96b66fece560d81e9816d1f373cab0e31e2cf5be
liukun06 <451564319@qq.com>
2017-05-24 18:01:15 +0800

    test file path

--- src/main/java/cn/edu/thu/tsfiledb/engine/filenode/Mi.java
+++ src/main/java/cn/edu/thu/tsfiledb/engine/filenode/Mi.java
@@ -6,13 +6,21 @@ public class Mi {
 
 	public static void main(String[] args) {
 		
-		String path = "/user/user/name";
-		String[] paths = path.split(File.separator);
-		System.out.println(paths.length);
-		for(String data :paths){
-			System.out.println(data);
-		}
+//		String path = "/user/user/name";
+//		String[] paths = path.split(File.separator);
+//		System.out.println(paths.length);
+//		for(String data :paths){
+//			System.out.println(data);
+//		}
 		// TODO Auto-generated method stub
+		String path = "fdafad\\fds";
+		System.out.println("\\"+File.separator);
+		System.out.println("/");
+		System.out.println("\");
+		String[] paths = path.split("\\"+File.separator);
+		for(String str:paths){
+			System.out.println(str);
+		}
 		System.out.println(File.separator);
 		System.out.println(File.pathSeparator);
 		
