7704a69ac356fdfbe7d99f3f4f8ce62462a46efd
Dark Templar <xuyi556677@163.com>
2017-07-09 16:38:40 +0800

    add jxm local mode

--- tsfiledb/conf/tsfile-env.sh
+++ tsfiledb/conf/tsfile-env.sh
@@ -2,7 +2,13 @@
 
 #export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.8.0_112.jdk/Contents/Home
 
+LOCAL_JMX=yes
+
 JMX_PORT="31999"
 
-TSFILEDB_JMX_OPTS="-Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.authenticate=false  -Dcom.sun.management.jmxremote.ssl=false "
-TSFILEDB_JMX_OPTS="$TSFILEDB_JMX_OPTS -Dcom.sun.management.jmxremote.port=$JMX_PORT "
+if [ "$LOCAL_JMX" = "yes" ]; then
+	echo "Only start local JMX service"
+else
+	TSFILEDB_JMX_OPTS="-Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.authenticate=false  -Dcom.sun.management.jmxremote.ssl=false "
+	TSFILEDB_JMX_OPTS="$TSFILEDB_JMX_OPTS -Dcom.sun.management.jmxremote.port=$JMX_PORT "
+fi
