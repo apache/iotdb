9d4d79715ed7f170988be4f848a9fc81b1be95dc
carlin <carlin@carlin>
2017-07-13 21:02:11 +0800

    modify command line

--- src/main/java/cn/edu/thu/tsfiledb/tool/CSVToTsfile.java
+++ src/main/java/cn/edu/thu/tsfiledb/tool/CSVToTsfile.java
@@ -14,13 +14,45 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
+import java.util.Scanner;
 
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.OptionBuilder;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import cn.edu.thu.tsfiledb.exception.ArgsErrorException;
 
 public class CSVToTsfile {
 	private static final Logger LOGGER = LoggerFactory.getLogger(CSVToTsfile.class);
+	private static final String HOST_ARGS = "h";
+	private static final String HOST_NAME = "host";
+
+	private static final String HELP_ARGS = "help";
+
+	private static final String PORT_ARGS = "p";
+	private static final String PORT_NAME = "port";
+
+	private static final String PASSWORD_ARGS = "pw";
+	private static final String PASSWORD_NAME = "password";
+
+	private static final String USERNAME_ARGS = "u";
+	private static final String USERNAME_NAME = "username";
+
+	private static final String FILE_ARGS = "f";
+	private static final String FILE_NAME = "file";
+
+	private static final String TIMEFORMAT_ARGS = "t";
+	private static final String TIMEFORMAT_NAME = "timeformat";
+
+	private static final int MAX_HELP_CONSOLE_WIDTH = 88;
+	private static final String TSFILEDB_CLI_PREFIX = "CSV_To_TsFile";
 
 	private static String host;
 	private static String port;
@@ -30,57 +62,145 @@ public class CSVToTsfile {
 	private static String timeformat;
 
 	private static HashMap<String, String> hm = null;
-	private static HashMap<String, ArrayList<Integer>> hm_timeseries; //storage device info
-	private static ArrayList<String> headInfo;   //storage csv head info
-	private static ArrayList<String> colInfo;	//storage csv  sensor info 
+	private static HashMap<String, ArrayList<Integer>> hm_timeseries; // storage
+																		// device
+																		// info
+	private static ArrayList<String> headInfo; // storage csv head info
+	private static ArrayList<String> colInfo; // storage csv sensor info
 
 	private static Connection connection = null;
 	private static Statement statement = null;
 
+	private static Options createOptions() {
+		Options options = new Options();
+		Option help = new Option(HELP_ARGS, false, "Display help information");
+		help.setRequired(false);
+		options.addOption(help);
+
+		// Option timeFormat = new Option(TIMEFORMAT_ARGS, false, "Display
+		// timestamp in number");
+		// timeFormat.setRequired(false);
+		// options.addOption(timeFormat);
+
+		Option op_host = OptionBuilder.withArgName(HOST_NAME).hasArg().withDescription("Host Name (required)")
+				.create(HOST_ARGS);
+		options.addOption(op_host);
+
+		Option os_port = OptionBuilder.withArgName(PORT_NAME).hasArg().withDescription("Port (required)")
+				.create(PORT_ARGS);
+		options.addOption(os_port);
+
+		Option os_username = OptionBuilder.withArgName(USERNAME_NAME).hasArg().withDescription("User name (required)")
+				.create(USERNAME_ARGS);
+		options.addOption(os_username);
+
+		Option os_password = OptionBuilder.withArgName(PASSWORD_NAME).hasArg().withDescription("Password (required)")
+				.create(PASSWORD_ARGS);
+		options.addOption(os_password);
+
+		Option os_file = OptionBuilder.withArgName(FILE_NAME).hasArg().withDescription("csv file path (required)")
+				.create(FILE_ARGS);
+		options.addOption(os_file);
+
+		Option os_timeformat = OptionBuilder.withArgName(TIMEFORMAT_NAME).hasArg()
+				.withDescription("timeFormat  (not required)").create(TIMEFORMAT_ARGS);
+		options.addOption(os_timeformat);
+
+		return options;
+	}
+
+	private static String checkRequiredArg(String arg, String name, CommandLine commandLine) throws ArgsErrorException {
+		String str = commandLine.getOptionValue(arg);
+		if (str == null) {
+			String msg = String.format("%s: Required values for option '%s' not provided", TSFILEDB_CLI_PREFIX, name);
+			System.out.println(msg);
+			System.out.println("Use -help for more information");
+			throw new ArgsErrorException(msg);
+		}
+		return str;
+	}
+
 	public static void main(String[] args) throws ClassNotFoundException, SQLException, IOException {
 
+		// if (args == null || args.length == 0) {
+		// System.out.println("usage: host port username password csv_filename
+		// timeformat");
+		// return;
+		// }
+		// host = args[0];
+		// port = args[1];
+		// username = args[2];
+		// password = args[3];
+		// filename = args[4];
+		// timeformat = args[5];
+		// SetTimeFormat(timeformat);
+		Options options = createOptions();
+		HelpFormatter hf = new HelpFormatter();
+		hf.setWidth(MAX_HELP_CONSOLE_WIDTH);
+		CommandLine commandLine = null;
+		CommandLineParser parser = new DefaultParser();
+		Scanner scanner = new Scanner(System.in);
+
 		if (args == null || args.length == 0) {
-			System.out.println("usage: host  port  username  password  csv_filename timeformat");
+			hf.printHelp(TSFILEDB_CLI_PREFIX, options, true);
 			return;
 		}
-		host = args[0];
-		port = args[1];
-		username = args[2];
-		password = args[3];
-		filename = args[4];
-		timeformat = args[5];
-		//SetTimeFormat(timeformat);
+
+		try {
+			commandLine = parser.parse(options, args);
+			if (commandLine.hasOption(HELP_ARGS)) {
+				hf.printHelp(TSFILEDB_CLI_PREFIX, options, true);
+				return;
+			}
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		try {
+			host = checkRequiredArg(HOST_ARGS, HOST_NAME, commandLine);
+			port = checkRequiredArg(PORT_ARGS, PORT_NAME, commandLine);
+			username = checkRequiredArg(USERNAME_ARGS, USERNAME_NAME, commandLine);
+			password = commandLine.getOptionValue(PASSWORD_ARGS);
+			if (password == null) {
+				System.out.print(TSFILEDB_CLI_PREFIX + "> please input password: ");
+				password = scanner.nextLine();
+			}
+			filename = checkRequiredArg(FILE_ARGS, FILE_NAME, commandLine);
+			timeformat = checkRequiredArg(TIMEFORMAT_ARGS, TIMEFORMAT_NAME, commandLine);
+		} catch (ArgsErrorException e) {
+			e.printStackTrace();
+		}
+
 		Class.forName("cn.edu.thu.tsfiledb.jdbc.TsfileDriver");
 		connection = DriverManager.getConnection("jdbc:tsfile://" + host + ":" + port + "/", username, password);
-		
+
 		loadDataFromCSV();
 
 		connection.close();
+		scanner.close();
 
 	}
 
 	private static String setTimeFormat(String tf, String words) {
-		if(tf.equals("ISO8601")) {
-			return  "\'" + words + "\'";
-		}else if(tf.equals("timestamps")) {
+		if (tf.equals("ISO8601")) {
+			return "\'" + words + "\'";
+		} else if (tf.equals("timestamps")) {
 			return words;
-		}else {
+		} else {
 			return "\'" + words + "\'";
 		}
 	}
 
-	public static void loadDataFromCSV()  {
+	public static void loadDataFromCSV() {
 		try {
 			statement = connection.createStatement();
-			BufferedReader br = new BufferedReader(new InputStreamReader
-					(new FileInputStream(new File(filename))));
+			BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(new File(filename))));
 			String line = "";
 			String[] str_headInfo = br.readLine().split(",");
 			hm_timeseries = new HashMap<String, ArrayList<Integer>>();
 			colInfo = new ArrayList<String>();
 			headInfo = new ArrayList<String>();
 
-			createType();  //create metadata info
+			createType(); // create metadata info
 
 			for (int i = 1; i < str_headInfo.length; i++) {
 				if (hm.containsKey(str_headInfo[i]) == false) {
@@ -90,34 +210,42 @@ public class CSVToTsfile {
 				headInfo.add(str_headInfo[i]);
 				String deviceInfo = str_headInfo[i].substring(0, str_headInfo[i].lastIndexOf("."));
 				if (hm_timeseries.containsKey(deviceInfo)) {
-					hm_timeseries.get(deviceInfo).add(i - 1); //storage every device's sensor index info
+					hm_timeseries.get(deviceInfo).add(i - 1); // storage every
+																// device's
+																// sensor index
+																// info
 					colInfo.add(str_headInfo[i].substring(str_headInfo[i].lastIndexOf(".") + 1));
 				} else {
 					hm_timeseries.put(deviceInfo, new ArrayList<Integer>());
-					hm_timeseries.get(deviceInfo).add(i - 1); //storage every device's sensor index info
+					hm_timeseries.get(deviceInfo).add(i - 1); // storage every
+																// device's
+																// sensor index
+																// info
 					colInfo.add(str_headInfo[i].substring(str_headInfo[i].lastIndexOf(".") + 1));
 				}
 
 			}
-//			for (Entry<String, ArrayList<Integer>> entry : hm_timeseries.entrySet()) {
-//				System.out.println(entry.getKey() + " " + entry.getValue());
-//			}
-//			System.out.println(colInfo);
+			// for (Entry<String, ArrayList<Integer>> entry :
+			// hm_timeseries.entrySet()) {
+			// System.out.println(entry.getKey() + " " + entry.getValue());
+			// }
+			// System.out.println(colInfo);
 			int successCount = 0;
-			int failCount =0;
+			int failCount = 0;
 			while ((line = br.readLine()) != null) {
 				ArrayList<String> sqls = createInsertSQL(line);
 				for (String str : sqls) {
-					if(statement.execute(str.toString())) {
+					if (statement.execute(str.toString())) {
 						successCount++;
-					}else {
+					} else {
 						failCount++;
 					}
 				}
-//				ResultSet rs = statement.executeQuery("select d1.s1 from root.fit");
-//				while (rs.next()) {
-//					System.out.println(rs.getString(0) + " " +  rs.getString(1));
-//				}
+				// ResultSet rs = statement.executeQuery("select d1.s1 from
+				// root.fit");
+				// while (rs.next()) {
+				// System.out.println(rs.getString(0) + " " + rs.getString(1));
+				// }
 			}
 			System.out.println("Success Count : " + successCount);
 			System.out.println("fail Count : " + failCount);
@@ -147,8 +275,8 @@ public class CSVToTsfile {
 	}
 
 	private static ArrayList<String> createInsertSQL(String line) {
-		String[] words = line.split(",", headInfo.size()+1);
-		
+		String[] words = line.split(",", headInfo.size() + 1);
+
 		ArrayList<String> sqls = new ArrayList<String>();
 		Iterator<Map.Entry<String, ArrayList<Integer>>> it = hm_timeseries.entrySet().iterator();
 		while (it.hasNext()) {
@@ -167,14 +295,18 @@ public class CSVToTsfile {
 			}
 			if (skipcount == entry.getValue().size())
 				continue;
-			sbd.append(") values(" + setTimeFormat(timeformat, words[0])); //accord to time set sql
+			sbd.append(") values(" + setTimeFormat(timeformat, words[0])); // accord
+																			// to
+																			// time
+																			// set
+																			// sql
 			for (int j = 0; j < colIndex.size(); ++j) {
 				if (words[entry.getValue().get(j) + 1].equals(""))
 					continue;
 				if (typeIdentify(headInfo.get(colIndex.get(j))) == "BYTE_ARRAY") {
 					sbd.append(", \'" + words[colIndex.get(j) + 1] + "\'");
 				} else {
-					sbd.append("," + words[colIndex.get(j) + 1]) ;
+					sbd.append("," + words[colIndex.get(j) + 1]);
 				}
 			}
 			sbd.append(")");
