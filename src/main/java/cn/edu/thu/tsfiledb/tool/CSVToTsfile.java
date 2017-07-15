8f21f2e87b67bd21bd5af4c28b75bf6b819a754d
carlin <carlin@carlin>
2017-07-15 09:47:06 +0800

    add time handle function

--- src/main/java/cn/edu/thu/tsfiledb/tool/CSVToTsfile.java
+++ src/main/java/cn/edu/thu/tsfiledb/tool/CSVToTsfile.java
@@ -10,7 +10,9 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
@@ -59,7 +61,7 @@ public class CSVToTsfile {
 	private static String username;
 	private static String password;
 	private static String filename;
-	private static String timeformat;
+	private static String timeformat = "timestamps";
 
 	private static HashMap<String, String> hm = null;
 	private static HashMap<String, ArrayList<Integer>> hm_timeseries; // storage
@@ -115,6 +117,145 @@ public class CSVToTsfile {
 		return str;
 	}
 
+	
+
+	private static String setTimeFormat(String tf, String words) {
+		if (tf.equals("ISO8601")) {
+			return "\'" + words + "\'";
+		} else if (tf.equals("timestamps")) {
+			return words;
+		} else {
+			// return "\'" + words + "\'";
+			SimpleDateFormat sdf = new SimpleDateFormat(tf);
+			Date date = null;
+			try {
+				date = sdf.parse(words);
+
+			} catch (java.text.ParseException e) {
+				LOGGER.debug("input time format error please re-input, Unparseable date: " + tf);
+			}
+			if (date != null)
+				return date.getTime() + "";
+			return "";
+		}
+	}
+
+	public static void loadDataFromCSV() {
+		try {
+			statement = connection.createStatement();
+			BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(new File(filename))));
+			String line = "";
+			String[] str_headInfo = br.readLine().split(",");
+			hm_timeseries = new HashMap<String, ArrayList<Integer>>();
+			colInfo = new ArrayList<String>();
+			headInfo = new ArrayList<String>();
+
+			createType(); // create metadata info
+
+			for (int i = 1; i < str_headInfo.length; i++) {
+				if (hm.containsKey(str_headInfo[i]) == false) {
+					System.out.println("data colum not exist!");
+					System.exit(1);
+				}
+				headInfo.add(str_headInfo[i]);
+				String deviceInfo = str_headInfo[i].substring(0, str_headInfo[i].lastIndexOf("."));
+				if (hm_timeseries.containsKey(deviceInfo)) {
+					hm_timeseries.get(deviceInfo).add(i - 1); // storage every
+																// device's
+																// sensor index
+																// info
+					colInfo.add(str_headInfo[i].substring(str_headInfo[i].lastIndexOf(".") + 1));
+				} else {
+					hm_timeseries.put(deviceInfo, new ArrayList<Integer>());
+					hm_timeseries.get(deviceInfo).add(i - 1); // storage every
+																// device's
+																// sensor index
+																// info
+					colInfo.add(str_headInfo[i].substring(str_headInfo[i].lastIndexOf(".") + 1));
+				}
+
+			}
+			while ((line = br.readLine()) != null) {
+				ArrayList<String> sqls = createInsertSQL(line);
+				for (String str : sqls) {
+					try {
+						statement.execute(str);
+
+					} catch (SQLException e) {
+						LOGGER.error(str + " excuted fail");
+					}
+				}
+
+			}
+			br.close();
+			statement.close();
+		} catch (FileNotFoundException e) {
+			LOGGER.error(e.getMessage());
+		} catch (IOException e) {
+			LOGGER.error(e.getMessage());
+		} catch (SQLException e1) {
+			LOGGER.error(e1.getMessage());
+		}
+	}
+
+	private static String typeIdentify(String key_timeseries) {
+		return hm.get(key_timeseries);
+
+	}
+
+	private static void createType() {
+		hm = new HashMap<String, String>();
+		hm.put("root.fit.d1.s1", "INT32");
+		hm.put("root.fit.d1.s2", "BYTE_ARRAY");
+		hm.put("root.fit.d2.s1", "INT32");
+		hm.put("root.fit.d2.s3", "INT32");
+		hm.put("root.fit.p.s1", "INT32");
+	}
+
+	private static ArrayList<String> createInsertSQL(String line) {
+		String[] words = line.split(",", headInfo.size() + 1);
+
+		ArrayList<String> sqls = new ArrayList<String>();
+		Iterator<Map.Entry<String, ArrayList<Integer>>> it = hm_timeseries.entrySet().iterator();
+		while (it.hasNext()) {
+			Map.Entry<String, ArrayList<Integer>> entry = it.next();
+			StringBuilder sbd = new StringBuilder();
+			ArrayList<Integer> colIndex = entry.getValue();
+			sbd.append("insert into " + entry.getKey() + "(timestamp");
+			int skipcount = 0;
+			for (int j = 0; j < colIndex.size(); ++j) {
+
+				if (words[entry.getValue().get(j) + 1].equals("")) {
+					skipcount++;
+					continue;
+				}
+				sbd.append(", " + colInfo.get(colIndex.get(j)));
+			}
+			if (skipcount == entry.getValue().size())
+				continue;
+
+			String str_timestamps = setTimeFormat(timeformat, words[0]);
+			if (str_timestamps == "")
+				continue;
+			sbd.append(") values(" + str_timestamps);
+
+			for (int j = 0; j < colIndex.size(); ++j) {
+				if (words[entry.getValue().get(j) + 1].equals(""))
+					continue;
+				if (typeIdentify(headInfo.get(colIndex.get(j))) == "BYTE_ARRAY") {
+					sbd.append(", \'" + words[colIndex.get(j) + 1] + "\'");
+				} else {
+					sbd.append("," + words[colIndex.get(j) + 1]);
+				}
+			}
+			sbd.append(")");
+			sqls.add(sbd.toString());
+			System.out.println(sbd.toString());
+		}
+
+		return sqls;
+	}
+	
 	public static void main(String[] args) throws ClassNotFoundException, SQLException, IOException {
 
 		Options options = createOptions();
@@ -162,135 +303,7 @@ public class CSVToTsfile {
 		scanner.close();
 
 	}
-
-	private static String setTimeFormat(String tf, String words) {
-		if (tf.equals("ISO8601")) {
-			return "\'" + words + "\'";
-		} else if (tf.equals("timestamps")) {
-			return words;
-		} else {
-			return "\'" + words + "\'";
-		}
-	}
-
-	public static void loadDataFromCSV() {
-		try {
-			statement = connection.createStatement();
-			BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(new File(filename))));
-			String line = "";
-			String[] str_headInfo = br.readLine().split(",");
-			hm_timeseries = new HashMap<String, ArrayList<Integer>>();
-			colInfo = new ArrayList<String>();
-			headInfo = new ArrayList<String>();
-
-			createType(); // create metadata info
-
-			for (int i = 1; i < str_headInfo.length; i++) {
-				if (hm.containsKey(str_headInfo[i]) == false) {
-					System.out.println("data colum not exist!");
-					System.exit(1);
-				}
-				headInfo.add(str_headInfo[i]);
-				String deviceInfo = str_headInfo[i].substring(0, str_headInfo[i].lastIndexOf("."));
-				if (hm_timeseries.containsKey(deviceInfo)) {
-					hm_timeseries.get(deviceInfo).add(i - 1); // storage every
-																// device's
-																// sensor index
-																// info
-					colInfo.add(str_headInfo[i].substring(str_headInfo[i].lastIndexOf(".") + 1));
-				} else {
-					hm_timeseries.put(deviceInfo, new ArrayList<Integer>());
-					hm_timeseries.get(deviceInfo).add(i - 1); // storage every
-																// device's
-																// sensor index
-																// info
-					colInfo.add(str_headInfo[i].substring(str_headInfo[i].lastIndexOf(".") + 1));
-				}
-
-			}
-			// for (Entry<String, ArrayList<Integer>> entry :
-			// hm_timeseries.entrySet()) {
-			// System.out.println(entry.getKey() + " " + entry.getValue());
-			// }
-			// System.out.println(colInfo);
-			int successCount = 0;
-			int failCount = 0;
-			while ((line = br.readLine()) != null) {
-				ArrayList<String> sqls = createInsertSQL(line);
-				for (String str : sqls) {
-					if (statement.execute(str.toString())) {
-						successCount++;
-					} else {
-						failCount++;
-					}
-				}
-
-			}
-			System.out.println("Success Count : " + successCount);
-			System.out.println("fail Count : " + failCount);
-			br.close();
-			statement.close();
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		} catch (SQLException e) {
-			LOGGER.error(e.getMessage());
-		}
-	}
-
-	private static String typeIdentify(String key_timeseries) {
-		return hm.get(key_timeseries);
-
-	}
-
-	private static void createType() {
-		hm = new HashMap<String, String>();
-		hm.put("root.fit.d1.s1", "INT32");
-		hm.put("root.fit.d1.s2", "BYTE_ARRAY");
-		hm.put("root.fit.d2.s1", "INT32");
-		hm.put("root.fit.d2.s3", "INT32");
-		hm.put("root.fit.p.s1", "INT32");
-	}
-
-	private static ArrayList<String> createInsertSQL(String line) {
-		String[] words = line.split(",", headInfo.size() + 1);
-
-		ArrayList<String> sqls = new ArrayList<String>();
-		Iterator<Map.Entry<String, ArrayList<Integer>>> it = hm_timeseries.entrySet().iterator();
-		while (it.hasNext()) {
-			Map.Entry<String, ArrayList<Integer>> entry = it.next();
-			StringBuilder sbd = new StringBuilder();
-			ArrayList<Integer> colIndex = entry.getValue();
-			sbd.append("insert into " + entry.getKey() + "(timestamp");
-			int skipcount = 0;
-			for (int j = 0; j < colIndex.size(); ++j) {
-
-				if (words[entry.getValue().get(j) + 1].equals("")) {
-					skipcount++;
-					continue;
-				}
-				sbd.append(", " + colInfo.get(colIndex.get(j)));
-			}
-			if (skipcount == entry.getValue().size())
-				continue;
-			sbd.append(") values(" + setTimeFormat(timeformat, words[0]));
-
-			for (int j = 0; j < colIndex.size(); ++j) {
-				if (words[entry.getValue().get(j) + 1].equals(""))
-					continue;
-				if (typeIdentify(headInfo.get(colIndex.get(j))) == "BYTE_ARRAY") {
-					sbd.append(", \'" + words[colIndex.get(j) + 1] + "\'");
-				} else {
-					sbd.append("," + words[colIndex.get(j) + 1]);
-				}
-			}
-			sbd.append(")");
-			sqls.add(sbd.toString());
-			System.out.println(sbd.toString());
-		}
-
-		return sqls;
-	}
-
 }
+
+
+
