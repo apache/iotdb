5260c53d28def3679394f6a8990d9a302dbe98d6
Dark Templar <xuyi556677@163.com>
2017-07-19 00:48:04 +0800

    add get all sensor jdbc interface

--- src/test/java/cn/edu/thu/tsfiledb/jdbc/JDBCClientTest.java
+++ src/test/java/cn/edu/thu/tsfiledb/jdbc/JDBCClientTest.java
@@ -14,10 +14,16 @@ public class JDBCClientTest {
 		try {
 			connection =  DriverManager.getConnection("jdbc:tsfile://127.0.0.1:6667/", "root", "root");
 			DatabaseMetaData databaseMetaData = connection.getMetaData();
-			ResultSet resultSet = databaseMetaData.getColumns(null, null, "root.vehicle.d0.s0", null);
+//			ResultSet resultSet = databaseMetaData.getColumns(null, null, "root.vehicle.d1.s0", null);
+//			while(resultSet.next()){
+//				//System.out.println(String.format("column %s, type %s", resultSet.getString("COLUMN_NAME"), resultSet.getString("COLUMN_TYPE")));
+//				System.out.println(String.format("column %s, type %s", resultSet.getString(0), resultSet.getString(1)));
+//			}
+			
+			ResultSet resultSet = databaseMetaData.getColumns(null, null, "root.*", null);
 			while(resultSet.next()){
 				//System.out.println(String.format("column %s, type %s", resultSet.getString("COLUMN_NAME"), resultSet.getString("COLUMN_TYPE")));
-				System.out.println(String.format("column %s, type %s", resultSet.getString(0), resultSet.getString(1)));
+				System.out.println(String.format("column %s", resultSet.getString(0)));
 			}
 		} finally {
 			connection.close();
