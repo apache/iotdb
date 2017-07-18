5fa11b743deac1e7edcba7118191b8ab36ed0ca1
CGF <cgf1993@foxmail.com>
2017-07-18 10:09:22 +0800

    add dump.sh

---
+++ tsfiledb/bin/dump.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+if [ -z "${TSFILE_HOME}" ];then
+   export TSFILE_HOME="$(cd "`dirname "$0"`"/..; pwd)"
+fi
+
+MAIN_CLASS=cn.edu.thu.tsfiledb.jdbc.TsfileDump
+
+CLASSPATH="."
+
+for f in ${TSFILE_HOME}/lib/*.jar; do
+    CLASSPATH=${CLASSPATH}":"$f
+done
+
+echo $JAVA_HOME
+if [ -n "$JAVA_HOME" ];then
+   for java in "$JAVA_HOME"/bin/amd64/java "$JAVA_HOME"/bin/java; do
+       if [ -x "$java" ]; then
+          JAVA="$java"
+	     break
+	   fi
+    done
+else
+    JAVA=java
+	
+fi
+
+echo $@
+exec "$JAVA" -DTSFILE_HOME=${TSFILE_HOME}  -cp "$CLASSPATH" "$MAIN_CLASS" "$@"
+
+exit $?
