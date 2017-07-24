50b7dc762efd054745b3be9ea1c2235122c37eb0
liukun06 <451564319@qq.com>
2017-07-24 22:26:40 +0800

    add test for create kv index

--- src/test/java/cn/edu/thu/tsfiledb/qp/cud/IndexTest.java
+++ src/test/java/cn/edu/thu/tsfiledb/qp/cud/IndexTest.java
@@ -2,6 +2,8 @@ package cn.edu.thu.tsfiledb.qp.cud;
 
 import static org.junit.Assert.*;
 
+import java.util.Map;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -9,11 +11,11 @@ import org.junit.Test;
 import cn.edu.thu.tsfiledb.exception.ArgsErrorException;
 import cn.edu.thu.tsfiledb.qp.QueryProcessor;
 import cn.edu.thu.tsfiledb.qp.exception.QueryProcessorException;
+import cn.edu.thu.tsfiledb.qp.physical.crud.IndexPlan;
 import cn.edu.thu.tsfiledb.qp.utils.MemIntQpExecutor;
 
 public class IndexTest {
 	
-	private QueryProcessor processor = new QueryProcessor(new MemIntQpExecutor());
 
 	@Before
 	public void setUp() throws Exception {
@@ -27,16 +29,28 @@ public class IndexTest {
 	public void testCreateIndex() throws QueryProcessorException, ArgsErrorException {
 		
 		String createIndex = "create index on root.laptop.d1.s1 using kv-match";
-		String sql = "delete from root.laptop.d1.s1 where time<100";
-		processor.parseSQLToPhysicalPlan(createIndex);
-		
+		QueryProcessor processor = new QueryProcessor(new MemIntQpExecutor());
+		IndexPlan indexPlan =  (IndexPlan) processor.parseSQLToPhysicalPlan(createIndex);
+		assertEquals("root.laptop.d1.s1", indexPlan.getPaths().get(0).getFullPath());
+		assertEquals(0, indexPlan.getParameters().keySet().size());
+		assertEquals(0, indexPlan.getStartTime());
 	}
 	
 	@Test
 	public void testCreateIndex2() throws QueryProcessorException, ArgsErrorException{
-		String sql = "create timeseries root.laptop.d1.s1 with datatype=INT32,encoding=RLE";
-		String createIndex = "create index on root.laptop.d1.s1 using kv-match with window_length=20,a=50 where time>=100";
-		processor.parseSQLToPhysicalPlan(createIndex);
+		String createIndex = "create index on root.laptop.d1.s1 using kv-match with b=20,a=50 where time>=100";
+		QueryProcessor processor = new QueryProcessor(new MemIntQpExecutor());
+		IndexPlan indexPlan = (IndexPlan) processor.parseSQLToPhysicalPlan(createIndex);
+		assertEquals("root.laptop.d1.s1", indexPlan.getPaths().get(0).getFullPath());
+		assertEquals(2, indexPlan.getParameters().keySet().size());
+		Map<String, Integer> map = indexPlan.getParameters();
+		assertEquals((long)20, (long)map.get("b"));
+		assertEquals((long)50, (long)map.get("a"));
+		assertEquals(100, indexPlan.getStartTime());
+		createIndex = "create index on root.laptop.d1.s1 using kv-match with b=20,a=50 where time>100";
+		processor = new QueryProcessor(new MemIntQpExecutor());
+		indexPlan = (IndexPlan) processor.parseSQLToPhysicalPlan(createIndex);
+		assertEquals(101, indexPlan.getStartTime());
 	}
 
 }
