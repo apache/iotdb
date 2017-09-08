050b1cfee589fbedc5652beea50b6f738efe75eb
Dark Templar <xuyi556677@163.com>
2017-09-08 22:37:59 +0800

    change class name path

--- iotdb/bin/import-csv.sh
+++ iotdb/bin/import-csv.sh
@@ -25,7 +25,7 @@ for f in ${TSFILE_HOME}/lib/*.jar; do
     CLASSPATH=${CLASSPATH}":"$f
 done
 
-MAIN_CLASS=cn.edu.thu.tsfiledb.tool.ImportCsv
+MAIN_CLASS=cn.edu.tsinghua.iotdb.tool.ImportCsv
 
 "$JAVA" -DTSFILE_HOME=${TSFILE_HOME} -cp "$CLASSPATH" "$MAIN_CLASS" "$@"
 exit $?
