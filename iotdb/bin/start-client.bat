599cf78d4c9304ea48b66afdb22464caafe5966d
XuYi <MyXOF@users.noreply.github.com>
2017-12-21 23:51:48 +0800

    fix a bug when client start and cannot find IOTDB_HOME (#228)

--- iotdb/bin/start-client.bat
+++ iotdb/bin/start-client.bat
@@ -6,26 +6,19 @@ echo ````````````````````````
 if "%OS%" == "Windows_NT" setlocal
 
 pushd %~dp0..
-if NOT DEFINED TSFILE_HOME set TSFILE_HOME=%CD%
+if NOT DEFINED IOTDB_HOME set IOTDB_HOME=%CD%
 popd
 
-set TSFILE_CONF=%TSFILE_HOME%\conf
-set TSFILE_LOGS=%TSFILE_HOME%\logs
 if NOT DEFINED MAIN_CLASS set MAIN_CLASS=cn.edu.tsinghua.iotdb.client.Client
 if NOT DEFINED JAVA_HOME goto :err
 
 @REM -----------------------------------------------------------------------------
 @REM JVM Opts we'll use in legacy run or installation
 set JAVA_OPTS=-ea^
- -Dlogback.configurationFile="%TSFILE_CONF%\logback.xml"^
- -DTSFILE_HOME=%TSFILE_HOME%
+ -DIOTDB_HOME=%IOTDB_HOME%
 
-@REM ***** CLASSPATH library setting *****
-@REM Ensure that any user defined CLASSPATH variables are not used on startup
-set CLASSPATH="TSFILE_HOME%\conf"
-
-REM For each jar in the TSFILE_HOME lib directory call append to build the CLASSPATH variable.
-for %%i in ("%TSFILE_HOME%\lib\*.jar") do call :append "%%i"
+REM For each jar in the IOTDB_HOME lib directory call append to build the CLASSPATH variable.
+for %%i in ("%IOTDB_HOME%\lib\*.jar") do call :append "%%i"
 goto okClasspath
 
 :append
@@ -35,7 +28,7 @@ goto :eof
 REM -----------------------------------------------------------------------------
 :okClasspath
 
-"%JAVA_HOME%\bin\java" -DTSFILE_HOME=%TSFILE_HOME% %JAVA_OPTS% -cp %CLASSPATH% %MAIN_CLASS% %*
+"%JAVA_HOME%\bin\java" %JAVA_OPTS% -cp %CLASSPATH% %MAIN_CLASS% %*
 
 goto finally
 
