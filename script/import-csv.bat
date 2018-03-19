4df23a56244edc14afd611ade4493fffc5310bd9
XuYi <MyXOF@users.noreply.github.com>
2018-03-19 12:03:25 +0800

    fix isse 344 (#351)

--- script/import-csv.bat
+++ script/import-csv.bat
@@ -3,7 +3,7 @@
 if "%OS%" == "Windows_NT" setlocal
 
 pushd %~dp0..
-if NOT DEFINED TSFILE_HOME set TSFILE_HOME=%CD%
+if NOT DEFINED IOTDB_HOME set IOTDB_HOME=%CD%
 popd
 
 if NOT DEFINED MAIN_CLASS set MAIN_CLASS=cn.edu.tsinghua.iotdb.tool.ImportCsv
@@ -12,14 +12,14 @@ if NOT DEFINED JAVA_HOME goto :err
 @REM -----------------------------------------------------------------------------
 @REM JVM Opts we'll use in legacy run or installation
 set JAVA_OPTS=-ea^
- -DTSFILE_HOME=%TSFILE_HOME%
+ -DIOTDB_HOME=%IOTDB_HOME%
 
 @REM ***** CLASSPATH library setting *****
 @REM Ensure that any user defined CLASSPATH variables are not used on startup
 set CLASSPATH=""
 
-REM For each jar in the CASSANDRA_HOME lib directory call append to build the CLASSPATH variable.
-for %%i in ("%TSFILE_HOME%\lib\*.jar") do call :append "%%i"
+REM For each jar in the IOTDB_HOME lib directory call append to build the CLASSPATH variable.
+for %%i in ("%IOTDB_HOME%\lib\*.jar") do call :append "%%i"
 goto okClasspath
 
 :append
@@ -29,7 +29,7 @@ goto :eof
 REM -----------------------------------------------------------------------------
 :okClasspath
 
-"%JAVA_HOME%\bin\java" -DTSFILE_HOME=%TSFILE_HOME% %JAVA_OPTS% -cp %CLASSPATH% %MAIN_CLASS% %*
+"%JAVA_HOME%\bin\java" -DIOTDB_HOME=%IOTDB_HOME% %JAVA_OPTS% -cp %CLASSPATH% %MAIN_CLASS% %*
 
 goto finally
 
