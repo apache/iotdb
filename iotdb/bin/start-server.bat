90e2b4181b65455a262e4876dedf967976694a5c
March250 <33376433+LeiRui@users.noreply.github.com>
2018-04-09 11:24:19 +0800

    Adjust data directories (#342)

--- iotdb/bin/start-server.bat
+++ iotdb/bin/start-server.bat
@@ -3,6 +3,7 @@ echo ````````````````````````
 echo Starting IoTDB
 echo ````````````````````````
 
+
 if "%OS%" == "Windows_NT" setlocal
 
 pushd %~dp0..
@@ -31,7 +32,7 @@ set JAVA_OPTS=-ea^
 
 @REM ***** CLASSPATH library setting *****
 @REM Ensure that any user defined CLASSPATH variables are not used on startup
-set CLASSPATH="IOTDB_HOME%\lib"
+set CLASSPATH="%IOTDB_HOME%\lib"
 
 REM For each jar in the IOTDB_HOME lib directory call append to build the CLASSPATH variable.
 for %%i in ("%IOTDB_HOME%\lib\*.jar") do call :append "%%i"
@@ -49,7 +50,6 @@ rem echo CLASSPATH: %CLASSPATH%
 
 "%JAVA_HOME%\bin\java" %JAVA_OPTS% %IOTDB_HEAP_OPTS% -cp %CLASSPATH% %IOTDB_DERBY_OPTS% %IOTDB_JMX_OPTS% %MAIN_CLASS%
 goto finally
-
 
 :err
 echo JAVA_HOME environment variable must be set!
