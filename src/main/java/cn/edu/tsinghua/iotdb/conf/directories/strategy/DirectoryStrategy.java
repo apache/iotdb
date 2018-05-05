85b778307bbee0634778d86dea0533998ec52500
East <952945925@qq.com>
2018-05-05 10:38:18 +0800

    add javadoc for strategy

--- src/main/java/cn/edu/tsinghua/iotdb/conf/directories/strategy/DirectoryStrategy.java
+++ src/main/java/cn/edu/tsinghua/iotdb/conf/directories/strategy/DirectoryStrategy.java
@@ -17,15 +17,25 @@ public abstract class DirectoryStrategy {
     protected List<String> folders;
 
     /**
-     * To init folders.
-     * @param folders
+     * To init folders. Do not recommend to overwrite.
+     * @param folders the folders from conf
      */
     public void init(List<String> folders){
         this.folders = folders;
     }
 
+    /**
+     * Choose a folder to allocate. The user should implement this method to define
+     * his own strategy.
+     * @return the index of folder that will be allocated
+     */
     public abstract int nextFolderIndex();
 
+    /**
+     * Return the actual string value of a folder by its index.
+     * @param index the index of the folder
+     * @return the string value of the folder
+     */
     public String getTsFileFolder(int index){
         return folders.get(index);
     }
