cda1909b1af5d5be0ec9705033fba0f7f997f8b9
CGF <cgf1993@foxmail.com>
2018-05-09 23:21:38 +0800

    add iotdb query basic framework with filter

--- src/main/java/cn/edu/tsinghua/iotdb/read/executor/DeltaQueryWithQueryFilterExecutorImpl.java
+++ src/main/java/cn/edu/tsinghua/iotdb/read/executor/DeltaQueryWithQueryFilterExecutorImpl.java
@@ -1,10 +1,19 @@
 package cn.edu.tsinghua.iotdb.read.executor;
 
+import cn.edu.tsinghua.iotdb.read.timegenerator.DeltaTimeGenerator;
+import cn.edu.tsinghua.tsfile.timeseries.read.support.Path;
+import cn.edu.tsinghua.tsfile.timeseries.readV2.common.EncodedSeriesChunkDescriptor;
 import cn.edu.tsinghua.tsfile.timeseries.readV2.query.QueryDataSet;
 import cn.edu.tsinghua.tsfile.timeseries.readV2.query.QueryExecutor;
 import cn.edu.tsinghua.tsfile.timeseries.readV2.query.QueryExpression;
+import cn.edu.tsinghua.tsfile.timeseries.readV2.query.impl.QueryDataSetForQueryWithQueryFilterImpl;
+import cn.edu.tsinghua.tsfile.timeseries.readV2.query.timegenerator.TimestampGenerator;
+import cn.edu.tsinghua.tsfile.timeseries.readV2.query.timegenerator.TimestampGeneratorByQueryFilterImpl;
+import cn.edu.tsinghua.tsfile.timeseries.readV2.reader.impl.SeriesReaderFromSingleFileByTimestampImpl;
 
 import java.io.IOException;
+import java.util.LinkedHashMap;
+import java.util.List;
 
 public class DeltaQueryWithQueryFilterExecutorImpl implements QueryExecutor {
 
@@ -12,7 +21,19 @@ public class DeltaQueryWithQueryFilterExecutorImpl implements QueryExecutor {
 
     @Override
     public QueryDataSet execute(QueryExpression queryExpression) throws IOException {
+        TimestampGenerator timestampGenerator = new DeltaTimeGenerator(queryExpression.getQueryFilter());
+        LinkedHashMap<Path, SeriesReaderFromSingleFileByTimestampImpl> readersOfSelectedSeries = new LinkedHashMap<>();
+        initReadersOfSelectedSeries(readersOfSelectedSeries, queryExpression.getSelectedSeries());
+        return new QueryDataSetForQueryWithQueryFilterImpl(timestampGenerator, readersOfSelectedSeries);
+    }
 
-        return null;
+    private void initReadersOfSelectedSeries(LinkedHashMap<Path, SeriesReaderFromSingleFileByTimestampImpl> readersOfSelectedSeries,
+                                             List<Path> selectedSeries) throws IOException {
+//        for (Path path : selectedSeries) {
+//            List<EncodedSeriesChunkDescriptor> encodedSeriesChunkDescriptorList = metadataQuerier.getSeriesChunkDescriptorList(path);
+//            SeriesReaderFromSingleFileByTimestampImpl seriesReader = new SeriesReaderFromSingleFileByTimestampImpl(
+//                    seriesChunkLoader, encodedSeriesChunkDescriptorList);
+//            readersOfSelectedSeries.put(path, seriesReader);
+//        }
     }
 }
--- src/main/java/cn/edu/tsinghua/iotdb/read/executor/DeltaQueryWithoutFilterExecutorImpl.java
+++ src/main/java/cn/edu/tsinghua/iotdb/read/executor/DeltaQueryWithoutFilterExecutorImpl.java
@@ -16,6 +16,8 @@ import java.util.List;
 
 public class DeltaQueryWithoutFilterExecutorImpl implements QueryExecutor{
 
+
+
     public DeltaQueryWithoutFilterExecutorImpl() {
     }
 
