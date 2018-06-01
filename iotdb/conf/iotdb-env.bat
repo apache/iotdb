7351e857ac45de29eccff0afe1a04c2a4955b4d5
XuYi <MyXOF@users.noreply.github.com>
2018-06-01 17:55:10 +0800

    fix issue 404 (#406)

--- iotdb/conf/iotdb-env.bat
+++ iotdb/conf/iotdb-env.bat
@@ -9,7 +9,6 @@ if "%LOCAL_JMX%" == "yes" (
 	) else (
 		set IOTDB_JMX_OPTS="-Dcom.sun.management.jmxremote" "-Dcom.sun.management.jmxremote.authenticate=false"  "-Dcom.sun.management.jmxremote.ssl=false" "-Dcom.sun.management.jmxremote.port=%JMX_PORT%"
 	)
-set IOTDB_DERBY_OPTS= "-Dderby.stream.error.field=cn.edu.tsinghua.iotdb.auth.dao.DerbyUtil.DEV_NULL"
 
 
 IF ["%IOTDB_HEAP_OPTS%"] EQU [""] (
@@ -18,10 +17,10 @@ IF ["%IOTDB_HEAP_OPTS%"] EQU [""] (
 	IF NOT ERRORLEVEL 1 (
 		rem 64-bit Java
 		echo Detect 64-bit Java, maximum memory allocation pool = 2GB, initial memory allocation pool = 2GB
-		set IOTDB_HEAP_OPTS=-Xmx2G -Xms2G -Xloggc:%IOTDB_HOME%\gc.log -XX:+PrintGCDateStamps -XX:+PrintGCDetails
+		set IOTDB_HEAP_OPTS=-Xmx2G -Xms2G -Xloggc:"%IOTDB_HOME%\gc.log" -XX:+PrintGCDateStamps -XX:+PrintGCDetails
 	) ELSE (
 		rem 32-bit Java
 		echo Detect 64-bit Java, maximum memory allocation pool = 512MB, initial memory allocation pool = 512MB
-		set IOTDB_HEAP_OPTS=-Xmx512M -Xms512M -Xloggc:%IOTDB_HOME%\gc.log -XX:+PrintGCDateStamps -XX:+PrintGCDetails
+		set IOTDB_HEAP_OPTS=-Xmx512M -Xms512M -Xloggc:"%IOTDB_HOME%\gc.log" -XX:+PrintGCDateStamps -XX:+PrintGCDetails
 	)
 )
