42e30d0d7a5cc2e142f61abbe056876f83a201fb
Xiangdong Huang <hxdreg@qq.com>
2018-10-25 21:34:08 +0800

    change WAL writer from RandomAccessFile to NIO (FileChannel) and add its UT (#441)

--- src/main/java/cn/edu/tsinghua/iotdb/writelog/io/LogWriter.java
+++ src/main/java/cn/edu/tsinghua/iotdb/writelog/io/LogWriter.java
@@ -1,27 +1,28 @@
 package cn.edu.tsinghua.iotdb.writelog.io;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.RandomAccessFile;
 import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
 import java.util.List;
 import java.util.zip.CRC32;
 
-public class RAFLogWriter implements ILogWriter {
+public class LogWriter implements ILogWriter {
 
     private File logFile;
-    private RandomAccessFile raf;
+    private FileChannel outputStream;
     private CRC32 checkSummer = new CRC32();
 
-    public RAFLogWriter(String logFilePath) {
+    public LogWriter(String logFilePath) {
         logFile = new File(logFilePath);
     }
 
     @Override
     public void write(List<byte[]> logCache) throws IOException {
-        if (raf == null)
-            raf = new RandomAccessFile(logFile, "rw");
-        raf.seek(raf.length());
+        if (outputStream == null)
+            outputStream = new FileOutputStream(logFile, true).getChannel();
         int totalSize = 0;
         for (byte[] bytes : logCache) {
             totalSize += 4 + 8 + bytes.length;
@@ -34,14 +35,16 @@ public class RAFLogWriter implements ILogWriter {
             buffer.putLong(checkSummer.getValue());
             buffer.put(bytes);
         }
-        raf.write(buffer.array());
+        buffer.flip();
+        outputStream.write(buffer);
+        outputStream.force(true);
     }
 
     @Override
     public void close() throws IOException {
-        if (raf != null) {
-            raf.close();
-            raf = null;
+        if (outputStream != null) {
+            outputStream.close();
+            outputStream = null;
         }
     }
 }
