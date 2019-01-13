bce8d499233b55b313df6b105e38448810f17ebb
XuYi <MyXOF@users.noreply.github.com>
2019-01-13 22:33:47 +0800

    fix format bug (#580)

--- .mvn/wrapper/MavenWrapperDownloader.java
+++ .mvn/wrapper/MavenWrapperDownloader.java
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 import java.net.*;
 import java.io.*;
 import java.nio.channels.*;
