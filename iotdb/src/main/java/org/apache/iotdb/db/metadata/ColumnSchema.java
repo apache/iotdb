8e1dae1b5df0aab966c43284c0cde6b9e90e1ba9
liuruiyiyang <245422695@qq.com>
2019-01-25 11:23:12 +0800

    change columnSchema name public to private

--- iotdb/src/main/java/org/apache/iotdb/db/metadata/ColumnSchema.java
+++ iotdb/src/main/java/org/apache/iotdb/db/metadata/ColumnSchema.java
@@ -28,7 +28,11 @@ public class ColumnSchema implements Serializable {
 
   private static final long serialVersionUID = -8257474930341487207L;
 
-  public String name;
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  private String name;
   public TSDataType dataType;
   public TSEncoding encoding;
   private Map<String, String> args;
