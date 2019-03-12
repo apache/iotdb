3e91de1100b0073d0d594eafdcd683bb4cda4ce1
qiaojialin <646274302@qq.com>
2019-03-12 19:29:45 +0800

    remove unnecessary headers

--- iotdb/src/main/java/org/apache/iotdb/db/sql/parse/AstNodeOrigin.java
+++ iotdb/src/main/java/org/apache/iotdb/db/sql/parse/AstNodeOrigin.java
@@ -31,27 +31,6 @@ package org.apache.iotdb.db.sql.parse;
  * So, when expanding the definition of v while analyzing the top-level query, we tag each AstNode
  * with a reference to an AstNodeOrign describing v and its usage within the query.
  */
-
-/**
- * Based on ASTNodeOrigin.java from Apache Hive
- *
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 public class AstNodeOrigin {
 
   private final String objectType;
