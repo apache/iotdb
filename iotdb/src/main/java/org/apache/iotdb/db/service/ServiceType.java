d7a2dfad7f8aa319f9e10de58703c6b9a947a349
liuruiyiyang <245422695@qq.com>
2019-03-13 11:04:06 +0800

    format ServiceType

--- iotdb/src/main/java/org/apache/iotdb/db/service/ServiceType.java
+++ iotdb/src/main/java/org/apache/iotdb/db/service/ServiceType.java
@@ -16,16 +16,20 @@
  * specific language governing permissions and limitations
  * under the License.
  */
+
 package org.apache.iotdb.db.service;
 
 public enum ServiceType {
-  FILE_NODE_SERVICE("File Node ServerService", ""), JMX_SERVICE("JMX ServerService",
-      "JMX ServerService"), JDBC_SERVICE("JDBC ServerService", "JDBCService"), MONITOR_SERVICE(
-      "Monitor ServerService", "Monitor"), STAT_MONITOR_SERVICE("Statistics ServerService",
-      ""), WAL_SERVICE("WAL ServerService", ""), CLOSE_MERGE_SERVICE("Close&Merge ServerService",
-      ""), JVM_MEM_CONTROL_SERVICE("Memory Controller", ""), AUTHORIZATION_SERVICE(
-      "Authorization ServerService",
-      ""), FILE_READER_MANAGER_SERVICE("File reader manager ServerService", "");
+  FILE_NODE_SERVICE("File Node ServerService", ""),
+  JMX_SERVICE("JMX ServerService", "JMX ServerService"),
+  JDBC_SERVICE("JDBC ServerService", "JDBCService"),
+  MONITOR_SERVICE("Monitor ServerService", "Monitor"),
+  STAT_MONITOR_SERVICE("Statistics ServerService", ""),
+  WAL_SERVICE("WAL ServerService", ""),
+  CLOSE_MERGE_SERVICE("Close&Merge ServerService", ""),
+  JVM_MEM_CONTROL_SERVICE("Memory Controller", ""),
+  AUTHORIZATION_SERVICE("Authorization ServerService", ""),
+  FILE_READER_MANAGER_SERVICE("File reader manager ServerService", "");
   private String name;
   private String jmxName;
 
