10a5709a5dc3c22de3ada417900c20f89e5479ff
MyXOF <xuyi556677@163.com>
2019-03-22 16:36:13 +0800

    add cluster module

---
+++ cluster/src/main/java/org/apache/iotdb/cluster/App.java
@@ -0,0 +1,10 @@
+package org.apache.iotdb.cluster;
+
+public class App {
+  public App(){
+  }
+
+  public static void main(String[] args){
+    System.out.println(123);
+  }
+}
---
+++ cluster/src/test/java/org/apache/iotdb/cluster/AppTest.java
@@ -0,0 +1,12 @@
+package org.apache.iotdb.cluster;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AppTest {
+  @Test
+  public void Test(){
+    assertEquals("123", "123");
+  }
+}
