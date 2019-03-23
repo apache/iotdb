2218cc65e56d42d95d9036cef41dcfa61c9e066c
RuiLei <ruileisurprise@gmail.com>
2019-03-23 09:51:28 +0800

    reformat license

--- tsfile/src/test/java/org/apache/iotdb/tsfile/read/common/TimeRangeTest.java
+++ tsfile/src/test/java/org/apache/iotdb/tsfile/read/common/TimeRangeTest.java
@@ -1,15 +1,19 @@
 /**
- * Licensed to the Apache Software Foundation (ASF) under one or more contributor license
- * agreements.  See the NOTICE file distributed with this work for additional information regarding
- * copyright ownership.  The ASF licenses this file to you under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with the License.  You may obtain
- * a copy of the License at
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software distributed under the License
- * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
- * or implied.  See the License for the specific language governing permissions and limitations
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
  * under the License.
  */
 package org.apache.iotdb.tsfile.read.common;
@@ -20,6 +24,7 @@ import java.util.ArrayList;
 import org.junit.Test;
 
 public class TimeRangeTest {
+
   @Test
   /*
      no overlap
@@ -28,7 +33,7 @@ public class TimeRangeTest {
     TimeRange r = new TimeRange(1, 10);
 
     ArrayList<TimeRange> prevRanges = new ArrayList<>();
-    prevRanges.add(new TimeRange(20,25));
+    prevRanges.add(new TimeRange(20, 25));
 
     ArrayList<TimeRange> remainRanges = r.getRemains(prevRanges);
     assertEquals(1, remainRanges.size());
