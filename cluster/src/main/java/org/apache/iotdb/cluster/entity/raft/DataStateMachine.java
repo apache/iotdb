a3391f413a054022ac9566b56908961ff2c20603
lta <litiananfa@163.com>
2019-04-08 18:54:52 +0800

    fix a bug of execute batch

--- cluster/src/main/java/org/apache/iotdb/cluster/entity/raft/DataStateMachine.java
+++ cluster/src/main/java/org/apache/iotdb/cluster/entity/raft/DataStateMachine.java
@@ -116,7 +116,7 @@ public class DataStateMachine extends StateMachineAdapter {
           PhysicalPlan plan = PhysicalPlanLogTransfer.logToOperator(planByte);
 
           /** If the request is to set path and sg of the path doesn't exist, it needs to run null-read in meta group to avoid out of data sync **/
-          if (plan.getOperatorType() == OperatorType.CREATE_TIMESERIES && checkPathExistence(
+          if (plan.getOperatorType() == OperatorType.CREATE_TIMESERIES && !checkPathExistence(
               ((MetadataPlan) plan).getPath().getFullPath())) {
             SingleQPTask nullReadTask = new SingleQPTask(false, null);
             handleNullReadToMetaGroup(nullReadTask, status, nullReadTask);
