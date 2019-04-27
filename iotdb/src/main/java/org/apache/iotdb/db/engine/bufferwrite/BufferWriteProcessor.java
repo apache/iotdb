e6d6b82c8bc7569f9f38e238ccf1edb3da801928
江天 <jt2594838@163.com>
2019-04-27 14:09:50 +0800

    fix in proper hashcode methods in processors

--- iotdb/src/main/java/org/apache/iotdb/db/engine/bufferwrite/BufferWriteProcessor.java
+++ iotdb/src/main/java/org/apache/iotdb/db/engine/bufferwrite/BufferWriteProcessor.java
@@ -550,23 +550,12 @@ public class BufferWriteProcessor extends Processor {
 
   @Override
   public boolean equals(Object o) {
-    if (this == o) {
-      return true;
-    }
-    if (o == null || getClass() != o.getClass()) {
-      return false;
-    }
-    if (!super.equals(o)) {
-      return false;
-    }
-    BufferWriteProcessor that = (BufferWriteProcessor) o;
-    return Objects.equals(baseDir, that.baseDir) &&
-        Objects.equals(fileName, that.fileName);
+    return this == o;
   }
 
   @Override
   public int hashCode() {
-    return Objects.hash(super.hashCode(), baseDir, fileName);
+    return Objects.hash(super.hashCode());
   }
 
   @Override
