b7cabbae87ae5e78cb56c426ae3cd466855db443
RuiLei <ruileisurprise@gmail.com>
2019-06-22 14:30:27 +0800

    format code

--- iotdb/src/main/java/org/apache/iotdb/db/engine/querycontext/QueryDataSourceV2.java
+++ iotdb/src/main/java/org/apache/iotdb/db/engine/querycontext/QueryDataSourceV2.java
@@ -28,9 +28,10 @@ public class QueryDataSourceV2 {
   private Path seriesPath;
   private List<TsFileResourceV2> seqResources;
   private List<TsFileResourceV2> unseqResources;
-  public QueryDataSourceV2(Path seriesPath, List<TsFileResourceV2> seqResources,List<TsFileResourceV2> unseqResources) {
-    this.seriesPath =seriesPath;
-    this.seqResources= seqResources;
+
+  public QueryDataSourceV2(Path seriesPath, List<TsFileResourceV2> seqResources, List<TsFileResourceV2> unseqResources) {
+    this.seriesPath = seriesPath;
+    this.seqResources = seqResources;
     this.unseqResources = unseqResources;
   }
 
