58cfd99ec5d9dbd1fdc212bb86c17c97b0bce71d
Jialin Qiao <qjl16@mails.tsinghua.edu.cn>
2019-07-22 17:25:34 +0800

    Release folder (#282)
    * generate sbin,conf,lib,tools in root folder when packaging
    
    * add license to RELEASE_NOTES.md

--- iotdb-cli/src/test/java/org/apache/iotdb/cli/client/StartClientScriptIT.java
+++ iotdb-cli/src/test/java/org/apache/iotdb/cli/client/StartClientScriptIT.java
@@ -52,7 +52,7 @@ public class StartClientScriptIT extends AbstractScript {
         "IoTDB> Connection Error, please check whether the network is available or the server has started. Host is 127.0.0.1, port is 6668."};
     String dir = getCurrentPath("cmd.exe", "/c", "echo %cd%");
     ProcessBuilder builder = new ProcessBuilder("cmd.exe", "/c",
-        dir + File.separator + "cli" + File.separator + "bin" + File.separator + "start-client.bat",
+        dir + File.separator + "cli" + File.separator + "sbin" + File.separator + "start-client.bat",
         "-h",
         "127.0.0.1", "-p", "6668", "-u", "root", "-pw", "root");
     testOutput(builder, output);
@@ -66,7 +66,7 @@ public class StartClientScriptIT extends AbstractScript {
     String dir = getCurrentPath("pwd");
     System.out.println(dir);
     ProcessBuilder builder = new ProcessBuilder("sh",
-        dir + File.separator + "cli" + File.separator + "bin" + File.separator + "start-client.sh",
+        dir + File.separator + "cli" + File.separator + "sbin" + File.separator + "start-client.sh",
         "-h",
         "127.0.0.1", "-p", "6668", "-u", "root", "-pw", "root");
     testOutput(builder, output);
