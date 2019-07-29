fad18a7b37af947b335de4859f33fca197e62201
Yuyuan KANG <675550750@qq.com>
2019-07-29 10:37:03 +0800

    change default configuration

--- jdbc/src/main/java/org/apache/iotdb/jdbc/Config.java
+++ jdbc/src/main/java/org/apache/iotdb/jdbc/Config.java
@@ -6,9 +6,9 @@
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
- * <p>
- * http://www.apache.org/licenses/LICENSE-2.0
- * <p>
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
@@ -20,8 +20,7 @@ package org.apache.iotdb.jdbc;
 
 public class Config {
 
-  private Config() {
-  }
+  private Config() {}
 
   /**
    * The required prefix for the connection URL.
@@ -53,6 +52,6 @@ public class Config {
 
   public static final String JDBC_DRIVER_NAME = "org.apache.iotdb.jdbc.IoTDBDriver";
 
-  public static boolean rpcThriftCompressionEnable = true;
+  public static boolean rpcThriftCompressionEnable = false;
 
 }
