ca5fe35d8c4b70cdf2279951c7ab3cd411fa38ab
151250176 <151250176@smail.nju.edu.cn>
2019-08-28 14:49:26 +0800

    add license

--- spark-tsfile/src/main/java/org/apache/iotdb/tsfile/qp/common/BasicOperator.java
+++ spark-tsfile/src/main/java/org/apache/iotdb/tsfile/qp/common/BasicOperator.java
@@ -1,7 +1,25 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package org.apache.iotdb.tsfile.qp.common;
 
 
-import  org.apache.iotdb.tsfile.qp.exception.BasicOperatorException;
+import org.apache.iotdb.tsfile.qp.exception.BasicOperatorException;
 
 /**
  * basic operators include < > >= <= !=.
@@ -13,20 +31,20 @@ public class BasicOperator extends FilterOperator {
     private String seriesPath;
     private String seriesValue;
 
-    public String getSeriesPath() {
-        return seriesPath;
-    }
-
-    public String getSeriesValue() {
-        return seriesValue;
-    }
-
     public BasicOperator(int tokenIntType, String path, String value) {
         super(tokenIntType);
         this.seriesPath = this.singlePath = path;
         this.seriesValue = value;
         this.isLeaf = true;
         this.isSingle = true;
+    }
+
+    public String getSeriesPath() {
+        return seriesPath;
+    }
+
+    public String getSeriesValue() {
+        return seriesValue;
     }
 
     public void setReversedTokenIntType() throws BasicOperatorException {
@@ -44,7 +62,7 @@ public class BasicOperator extends FilterOperator {
     public BasicOperator clone() {
         BasicOperator ret;
         ret = new BasicOperator(this.tokenIntType, seriesPath, seriesValue);
-        ret.tokenSymbol=tokenSymbol;
+        ret.tokenSymbol = tokenSymbol;
         ret.isLeaf = isLeaf;
         ret.isSingle = isSingle;
         return ret;
