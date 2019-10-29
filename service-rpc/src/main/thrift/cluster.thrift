0ee412143b69d803384268af136e9fb1d776c0ac
qiaojialin <646274302@qq.com>
2019-10-29 11:34:44 +0800

    add append entry and start vote interface

--- service-rpc/src/main/thrift/cluster.thrift
+++ service-rpc/src/main/thrift/cluster.thrift
@@ -23,13 +23,60 @@ typedef i32 int
 typedef i16 short
 typedef i64 long
 
-struct HeartBeat {
-  1: required long term
-  2: required long appliedID
+// leader -> follower
+struct HeartBeatRequest {
+  1: required long term // leader's
+  2: required long commitLogIndex  // leader's
 }
 
+// follower -> leader
+struct HeartBeatResponse {
+  1: required long term // follower's
+  2: required long lastLogIndex // follower's
+}
+
+// node -> node
+struct VoteRequest {
+  1: required long term
+  2: required long lastLogTerm
+  3: required long lastLogIndex
+}
+
+// leader -> follower
+struct AppendEntryRequest {
+  1: required long term // leader's
+  2: required binary entry // data
+  3: required long previousLogTerm // leader's previous log term
+  4: required long previousLogIndex // leader's
+  5: required long leaderID
+}
+
+// follower -> leader
+struct AppendEntryResponse {
+  1: required long  // -1 means you are leader, otherwise return the voter's term
+  2: required bool success
+}
+
+// leader -> follower
+struct AppendEntriesRequest {
+  1: required long term // leader's
+  2: required list<binary> entries // data
+  3: required long previousLogTerm // leader's previous log term
+  4: required long previousLogIndex // leader's
+  5: required long leaderID
+}
 
 service TSIService {
-	HeartBeat sendHeartBeat(1:long term);
-	
+
+	HeartBeatResponse sendHeartBeat(1:HeartBeatRequest request);
+
+	// -1 means agree, otherwise return the voter's term
+	long startVote(1:VoteRequest voteRequest);
+
+  // -1: agree, -2: log index mismatch , otherwise return the follower's term
+  long appendEntry(1:AppendEntryRequest request)
+
+  // -1: agree, -2: log index mismatch , otherwise return the follower's term
+  long appendEntries(1:AppendEntriesRequest request)
+
 }
