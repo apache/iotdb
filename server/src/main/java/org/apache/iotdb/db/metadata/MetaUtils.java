88a3d6851148c64957faf80f588004b9fe402840
Ring-k <675550750@qq.com>
2019-10-30 17:25:59 +0800

    add license

--- server/src/main/java/org/apache/iotdb/db/metadata/MetaUtils.java
+++ server/src/main/java/org/apache/iotdb/db/metadata/MetaUtils.java
@@ -1,16 +1,32 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package org.apache.iotdb.db.metadata;
-
-import org.apache.iotdb.db.metadata.MTree;
 
 public class MetaUtils {
   public static String[] getNodeNames(String path, String separator) {
     String[] nodeNames;
     path = path.trim();
-    if(path.contains("\"") || path.contains("\'")){
+    if (path.contains("\"") || path.contains("\'")) {
       String[] deviceAndMeasurement;
-      if(path.contains("\"")){
+      if (path.contains("\"")) {
         deviceAndMeasurement = path.split("\"");
-      }else {
+      } else {
         deviceAndMeasurement = path.split("\'");
       }
       String device = deviceAndMeasurement[0];
@@ -20,7 +36,7 @@ public class MetaUtils {
       nodeNames = new String[nodeNumber];
       System.arraycopy(deviceNodeName, 0, nodeNames, 0, nodeNumber - 1);
       nodeNames[nodeNumber - 1] = measurement;
-    }else{
+    } else {
       nodeNames = path.split(separator);
     }
     return nodeNames;
