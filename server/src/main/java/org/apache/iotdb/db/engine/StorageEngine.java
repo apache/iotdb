5a061155cc2f65f0525f2516e470c8b6657dfceb
EJTTianyu <1653503269@qq.com>
2019-11-12 14:52:43 +0800

    [fix] fix name details

--- server/src/main/java/org/apache/iotdb/db/engine/StorageEngine.java
+++ server/src/main/java/org/apache/iotdb/db/engine/StorageEngine.java
@@ -28,6 +28,7 @@ import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
