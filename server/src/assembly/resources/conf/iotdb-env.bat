ec55edbf78d7b45759606275dd0d21472850a718
lta <litiananfa@163.com>
2019-11-14 09:28:42 +0800

    update iotdb-env.bat

--- server/src/assembly/resources/conf/iotdb-env.bat
+++ server/src/assembly/resources/conf/iotdb-env.bat
@@ -54,22 +54,21 @@ for /f "tokens=1-3" %%j in ('java -version 2^>^&1') do (
 	set BIT_VERSION=%%l
 )
 IF "%BIT_VERSION%" == "64-Bit" (
-  @REM Maximum heap size
+  rem 64bit, Maximum heap size
   MAX_HEAP_SIZE="2G"
-  @REM Minimum heap size
+  rem 64bit, Minimum heap size
   HEAP_NEWSIZE="2G"
-	rem 64-bit Java
-	echo Detect 64-bit Java, maximum memory allocation pool = %MAX_HEAP_SIZE%B, initial memory allocation pool = %HEAP_NEWSIZE%B
-	set IOTDB_HEAP_OPTS=-Xmx%HEAP_NEWSIZE% -Xms%MAX_HEAP_SIZE%
 ) ELSE (
-	@REM Maximum heap size
+	rem 32bit, Maximum heap size
   MAX_HEAP_SIZE="512M"
-  @REM Minimum heap size
+  rem 32bit, Minimum heap size
   HEAP_NEWSIZE="512M"
-	rem 32-bit Java
-	echo Detect 32-bit Java, maximum memory allocation pool = %MAX_HEAP_SIZE%B, initial memory allocation pool = %HEAP_NEWSIZE%B
-	set IOTDB_HEAP_OPTS=-Xmx%HEAP_NEWSIZE% -Xms%MAX_HEAP_SIZE%
 )
+
+@REM MAX_HEAP_SIZE="2G"
+@REM HEAP_NEWSIZE="2G"
+
+set IOTDB_HEAP_OPTS=-Xmx%MAX_HEAP_SIZE% -Xms%HEAP_NEWSIZE%
 
 @REM You can put your env variable here
 @REM set JAVA_HOME=%JAVA_HOME%
