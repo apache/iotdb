06b7c6dae4eabeb19072397826c7d46a5e2898ec
xiangdong huang <sainthxd@gmail.com>
2019-12-04 21:59:49 +0800

    fix wrong apache header; optimize long and Long boxing

--- server/src/main/java/org/apache/iotdb/db/metrics/server/ServerArgument.java
+++ server/src/main/java/org/apache/iotdb/db/metrics/server/ServerArgument.java
@@ -1,16 +1,20 @@
-/**
- * Licensed to the Apache Software Foundation (ASF) under one or more contributor license
- * agreements. See the NOTICE file distributed with this work for additional information regarding
- * copyright ownership. The ASF licenses this file to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance with the License. You may obtain a
- * copy of the License at
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software distributed under the License
- * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
- * or implied. See the License for the specific language governing permissions and limitations under
- * the License.
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
  */
 package org.apache.iotdb.db.metrics.server;
 
@@ -242,7 +246,7 @@ public class ServerArgument {
         continue;
       }
       String cmd = line.substring(cmdidx, kmtidx).trim();
-      if (cmd.indexOf("wmic.exe") >= 0) {
+      if (cmd.contains("wmic.exe")) {
         continue;
       }
       String caption = line.substring(capidx, cmdidx).trim();
@@ -255,24 +259,24 @@ public class ServerArgument {
       if (caption.equals("System Idle Process") || caption.equals("System")) {
         if (s1.length() > 0) {
           if (!digitS1.get(0).equals("") && digitS1.get(0) != null) {
-            idletime += Long.valueOf(digitS1.get(0)).longValue();
+            idletime += Long.parseLong(digitS1.get(0));
           }
         }
         if (s2.length() > 0) {
           if (!digitS2.get(0).equals("") && digitS2.get(0) != null) {
-            idletime += Long.valueOf(digitS2.get(0)).longValue();
+            idletime += Long.parseLong(digitS2.get(0));
           }
         }
         continue;
       }
       if (s1.length() > 0) {
         if (!digitS1.get(0).equals("") && digitS1.get(0) != null) {
-          kneltime += Long.valueOf(digitS1.get(0)).longValue();
+          kneltime += Long.parseLong(digitS1.get(0));
         }
       }
       if (s2.length() > 0) {
         if (!digitS2.get(0).equals("") && digitS2.get(0) != null) {
-          kneltime += Long.valueOf(digitS2.get(0)).longValue();
+          kneltime += Long.parseLong(digitS2.get(0));
         }
       }
     }
