2cfaeede55f1af5907ac1c553cf6319fd916ae7c
HTHou <hhaonan@outlook.com>
2019-12-25 19:30:01 +0800

    update rocketmq-example with session API

--- example/rocketmq/src/main/java/org/apache/iotdb/rocketmq/Constant.java
+++ example/rocketmq/src/main/java/org/apache/iotdb/rocketmq/Constant.java
@@ -18,7 +18,6 @@
  */
 package org.apache.iotdb.rocketmq;
 
-import org.apache.iotdb.jdbc.Config;
 
 public class Constant {
 
@@ -26,38 +25,41 @@ public class Constant {
   public static final String PRODUCER_GROUP = "IoTDBConsumer";
   public static final String CONSUMER_GROUP = "IoTDBProducer";
   public static final String SERVER_ADDRESS = "localhost:9876";
-  public static final String IOTDB_CONNECTION_URL = Config.IOTDB_URL_PREFIX + "localhost:6667/";
+  public static final String IOTDB_CONNECTION_HOST = "localhost";
+  public static final int IOTDB_CONNECTION_PORT = 6667;
   public static final String IOTDB_CONNECTION_USER = "root";
   public static final String IOTDB_CONNECTION_PASSWORD = "root";
   public static final String[] STORAGE_GROUP = {"root.vehicle", "root.test"};
-  public static final String[] CREATE_TIMESERIES = {
-      "CREATE TIMESERIES root.vehicle.d0.s0 WITH DATATYPE=INT32, ENCODING=PLAIN",
-      "CREATE TIMESERIES root.vehicle.d0.s1 WITH DATATYPE=TEXT, ENCODING=PLAIN",
-      "CREATE TIMESERIES root.vehicle.d0.s2 WITH DATATYPE=FLOAT, ENCODING=PLAIN",
-      "CREATE TIMESERIES root.vehicle.d0.s3 WITH DATATYPE=BOOLEAN, ENCODING=PLAIN",
-      "CREATE TIMESERIES root.test.d0.s0 WITH DATATYPE=INT32, ENCODING=PLAIN",
-      "CREATE TIMESERIES root.test.d0.s1 WITH DATATYPE=TEXT, ENCODING=PLAIN",
-      "CREATE TIMESERIES root.test.d1.s0 WITH DATATYPE=INT32, ENCODING=PLAIN"};
+  public static final String ADDITIONAL_STORAGE_GROUP = "root.addition";
+  public static final String[][] CREATE_TIMESERIES = {
+      {"root.vehicle.d0.s0", "INT32", "PLAIN", "SNAPPY"},
+      {"root.vehicle.d0.s1", "TEXT", "PLAIN", "SNAPPY"},
+      {"root.vehicle.d1.s2", "FLOAT", "PLAIN", "SNAPPY"},
+      {"root.vehicle.d1.s3", "BOOLEAN", "PLAIN", "SNAPPY"},
+      {"root.test.d0.s0", "INT32", "PLAIN", "SNAPPY"},
+      {"root.test.d0.s1", "TEXT", "PLAIN", "SNAPPY"},
+      {"root.test.d1.s0", "INT32", "PLAIN", "SNAPPY"},
+  };
   public static final String[] ALL_DATA = {
-      "insert into root.vehicle.d0(timestamp,s0) values(10,100)",
-      "insert into root.vehicle.d0(timestamp,s0,s1) values(12,101,'102')",
-      "insert into root.vehicle.d0(timestamp,s1) values(19,'103')",
-      "insert into root.vehicle.d1(timestamp,s2) values(11,104.0)",
-      "insert into root.vehicle.d1(timestamp,s2,s3) values(15,105.0,true)",
-      "insert into root.vehicle.d1(timestamp,s3) values(17,false)",
-      "insert into root.vehicle.d0(timestamp,s0) values(20,1000)",
-      "insert into root.vehicle.d0(timestamp,s0,s1) values(22,1001,'1002')",
-      "insert into root.vehicle.d0(timestamp,s1) values(29,'1003')",
-      "insert into root.vehicle.d1(timestamp,s2) values(21,1004.0)",
-      "insert into root.vehicle.d1(timestamp,s2,s3) values(25,1005.0,true)",
-      "insert into root.vehicle.d1(timestamp,s3) values(27,true)",
-      "insert into root.test.d0(timestamp,s0) values(10,106)",
-      "insert into root.test.d0(timestamp,s0,s1) values(14,107,'108')",
-      "insert into root.test.d0(timestamp,s1) values(16,'109')",
-      "insert into root.test.d1(timestamp,s0) values(1,110)",
-      "insert into root.test.d0(timestamp,s0) values(30,1006)",
-      "insert into root.test.d0(timestamp,s0,s1) values(34,1007,'1008')",
-      "insert into root.test.d0(timestamp,s1) values(36,'1090')",
-      "insert into root.test.d1(timestamp,s0) values(10,1100)"
+      "root.vehicle.d0,10,s0,100",
+      "root.vehicle.d0,12,s0:s1,101:'102'",
+      "root.vehicle.d0,19,s1,'103'",
+      "root.vehicle.d1,11,s2,104.0",
+      "root.vehicle.d1,15,s2:s3,105.0:true",
+      "root.vehicle.d1,17,s3,false",
+      "root.vehicle.d0,20,s0,1000",
+      "root.vehicle.d0,22,s0:s1,1001:'1002'",
+      "root.vehicle.d0,29,s1,'1003'",
+      "root.vehicle.d1,21,s2,1004.0",
+      "root.vehicle.d1,25,s2:s3,1005.0:true",
+      "root.vehicle.d1,27,s3,true",
+      "root.test.d0,10,s0,106",
+      "root.test.d0,14,s0:s1,107:'108'",
+      "root.test.d0,16,s1,'109'",
+      "root.test.d1,1,s0,110",
+      "root.test.d0,30,s0,1006",
+      "root.test.d0,34,s0:s1,1007:'1008'",
+      "root.test.d0,36,s1,'1090'",
+      "root.test.d1,10,s0,1100",
   };
 }
