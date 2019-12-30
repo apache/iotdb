89e30d5daefaa080da612136fb4f8bdde8b5eabf
Xiangwei Wei <526213833@qq.com>
2019-12-30 13:12:28 +0800

    fix

--- server/src/test/java/org/apache/iotdb/db/metadata/MManagerBasicTest.java
+++ server/src/test/java/org/apache/iotdb/db/metadata/MManagerBasicTest.java
@@ -23,9 +23,8 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
+
 import org.apache.iotdb.db.conf.IoTDBDescriptor;
 import org.apache.iotdb.db.exception.metadata.MetadataException;
 import org.apache.iotdb.db.exception.path.PathException;
