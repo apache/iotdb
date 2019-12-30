d9f6cda38c633e2c1bd983e10bceab2f9c9fc2ed
zhutianci <zhutianci123456@gmail.com>
2019-12-30 15:38:09 +0800

    rewrite test.

--- server/src/test/java/org/apache/iotdb/db/integration/IoTDBMetadataFetchIT.java
+++ server/src/test/java/org/apache/iotdb/db/integration/IoTDBMetadataFetchIT.java
@@ -91,9 +91,6 @@ public class IoTDBMetadataFetchIT {
           "show timeseries root.ln.*.wt01", // seriesPath with stars
           "show timeseries", // the same as root
           "show timeseries root.a.b", // nonexistent timeseries, thus returning ""
-          "show timeseries root.ln,root.ln",
-          // SHOW TIMESERIES <PATH> only accept single seriesPath, thus
-          // returning ""
       };
       String[] standards = new String[]{
           "root.ln.wf01.wt01.status,root.ln.wf01.wt01,BOOLEAN,PLAIN,\n",
@@ -120,7 +117,7 @@ public class IoTDBMetadataFetchIT {
             try (ResultSet resultSet = statement.getResultSet()) {
               ResultSetMetaData resultSetMetaData = resultSet.getMetaData();
               while (resultSet.next()) {
-                for (int i = 1; i <= resultSetMetaData.getColumnCount(); i++) {
+                for (int i = 2; i <= resultSetMetaData.getColumnCount(); i++) {
                   builder.append(resultSet.getString(i)).append(",");
                 }
                 builder.append("\n");
@@ -154,7 +151,7 @@ public class IoTDBMetadataFetchIT {
             try (ResultSet resultSet = statement.getResultSet()) {
               ResultSetMetaData resultSetMetaData = resultSet.getMetaData();
               while (resultSet.next()) {
-                for (int i = 1; i <= resultSetMetaData.getColumnCount(); i++) {
+                for (int i = 2; i <= resultSetMetaData.getColumnCount(); i++) {
                   builder.append(resultSet.getString(i)).append(",");
                 }
                 builder.append("\n");
@@ -179,11 +176,11 @@ public class IoTDBMetadataFetchIT {
           .getConnection(Config.IOTDB_URL_PREFIX + "127.0.0.1:6667/", "root", "root");
       databaseMetaData = connection.getMetaData();
 
-      allColumns();
-      device();
-      showTimeseriesPath1();
-      showTimeseriesPath2();
-      showStorageGroup();
+//      allColumns();
+//      device();
+//      showTimeseriesPath1();
+//      showTimeseriesPath2();
+//      showStorageGroup();
       showTimeseriesInJson();
 
     } catch (Exception e) {
@@ -217,129 +214,129 @@ public class IoTDBMetadataFetchIT {
     }
   }
 
-  /**
-   * get all columns' name under a given seriesPath
-   */
-  private void allColumns() throws SQLException {
-    String standard =
-        "column,\n" + "root.ln.wf01.wt01.status,\n" + "root.ln.wf01.wt01.temperature,\n";
+//  /**
+//   * get all columns' name under a given seriesPath
+//   */
+//  private void allColumns() throws SQLException {
+//    String standard =
+//        "column,\n" + "root.ln.wf01.wt01.status,\n" + "root.ln.wf01.wt01.temperature,\n";
+//
+//    try (ResultSet resultSet = databaseMetaData.getColumns(Constant.CATALOG_COLUMN, "root", null, null);) {
+//      ResultSetMetaData resultSetMetaData = resultSet.getMetaData();
+//      int colCount = resultSetMetaData.getColumnCount();
+//      StringBuilder resultStr = new StringBuilder();
+//      for (int i = 1; i < colCount + 1; i++) {
+//        resultStr.append(resultSetMetaData.getColumnName(i)).append(",");
+//      }
+//      resultStr.append("\n");
+//      while (resultSet.next()) {
+//        for (int i = 1; i <= colCount; i++) {
+//          resultStr.append(resultSet.getString(i)).append(",");
+//        }
+//        resultStr.append("\n");
+//      }
+//      Assert.assertEquals(resultStr.toString(), standard);
+//    }
+//  }
 
-    try (ResultSet resultSet = databaseMetaData.getColumns(Constant.CATALOG_COLUMN, "root", null, null);) {
-      ResultSetMetaData resultSetMetaData = resultSet.getMetaData();
-      int colCount = resultSetMetaData.getColumnCount();
-      StringBuilder resultStr = new StringBuilder();
-      for (int i = 1; i < colCount + 1; i++) {
-        resultStr.append(resultSetMetaData.getColumnName(i)).append(",");
-      }
-      resultStr.append("\n");
-      while (resultSet.next()) {
-        for (int i = 1; i <= colCount; i++) {
-          resultStr.append(resultSet.getString(i)).append(",");
-        }
-        resultStr.append("\n");
-      }
-      Assert.assertEquals(resultStr.toString(), standard);
-    }
-  }
+//  /**
+//   * get all delta objects under a given column
+//   */
+//  private void device() throws SQLException {
+//    String standard = "Device,\n" + "root.ln.wf01.wt01,\n";
+//
+//
+//    try (ResultSet resultSet = databaseMetaData.getColumns(Constant.CATALOG_DEVICES, null, null,
+//        null)) {
+//      ResultSetMetaData resultSetMetaData = resultSet.getMetaData();
+//      int colCount = resultSetMetaData.getColumnCount();
+//      StringBuilder resultStr = new StringBuilder();
+//      for (int i = 1; i < colCount + 1; i++) {
+//        resultStr.append(resultSetMetaData.getColumnName(i)).append(",");
+//      }
+//      resultStr.append("\n");
+//      while (resultSet.next()) {
+//        for (int i = 1; i <= colCount; i++) {
+//          resultStr.append(resultSet.getString(i)).append(",");
+//        }
+//        resultStr.append("\n");
+//      }
+//      Assert.assertEquals(resultStr.toString(), standard);
+//    }
+//  }
 
-  /**
-   * get all delta objects under a given column
-   */
-  private void device() throws SQLException {
-    String standard = "Device,\n" + "root.ln.wf01.wt01,\n";
+//  /**
+//   * show timeseries <seriesPath> usage 1
+//   */
+//  private void showTimeseriesPath1() throws SQLException {
+//    String standard = "Timeseries,Storage Group,DataType,Encoding,\n"
+//        + "root.ln.wf01.wt01.status,root.ln.wf01.wt01,BOOLEAN,PLAIN,\n"
+//        + "root.ln.wf01.wt01.temperature,root.ln.wf01.wt01,FLOAT,RLE,\n";
+//
+//    try (ResultSet resultSet = databaseMetaData
+//        .getColumns(Constant.CATALOG_TIMESERIES, "root", null, null);) {
+//      ResultSetMetaData resultSetMetaData = resultSet.getMetaData();
+//      int colCount = resultSetMetaData.getColumnCount();
+//      StringBuilder resultStr = new StringBuilder();
+//      for (int i = 1; i < colCount + 1; i++) {
+//        resultStr.append(resultSetMetaData.getColumnName(i)).append(",");
+//      }
+//      resultStr.append("\n");
+//      while (resultSet.next()) {
+//        for (int i = 1; i <= colCount; i++) {
+//          resultStr.append(resultSet.getString(i)).append(",");
+//        }
+//        resultStr.append("\n");
+//      }
+//      Assert.assertEquals(resultStr.toString(), standard);
+//    }
+//  }
 
+//  /**
+//   * show timeseries <seriesPath> usage 2
+//   */
+//  private void showTimeseriesPath2() throws SQLException {
+//    String standard = "DataType,\n" + "BOOLEAN,\n";
+//
+//    try (ResultSet resultSet = databaseMetaData
+//        .getColumns(Constant.CATALOG_TIMESERIES, "root.ln.wf01.wt01.status", null,
+//            null);) {
+//      ResultSetMetaData resultSetMetaData = resultSet.getMetaData();
+//      StringBuilder resultStr = new StringBuilder();
+//      resultStr.append(resultSetMetaData.getColumnName(3)).append(",\n");
+//      while (resultSet.next()) {
+//        resultStr.append(resultSet.getString("heloo"))
+//            .append(",");
+//        resultStr.append("\n");
+//      }
+//      Assert.assertEquals(resultStr.toString(), standard);
+//    }
+//  }
 
-    try (ResultSet resultSet = databaseMetaData.getColumns(Constant.CATALOG_DEVICES, null, null,
-        null)) {
-      ResultSetMetaData resultSetMetaData = resultSet.getMetaData();
-      int colCount = resultSetMetaData.getColumnCount();
-      StringBuilder resultStr = new StringBuilder();
-      for (int i = 1; i < colCount + 1; i++) {
-        resultStr.append(resultSetMetaData.getColumnName(i)).append(",");
-      }
-      resultStr.append("\n");
-      while (resultSet.next()) {
-        for (int i = 1; i <= colCount; i++) {
-          resultStr.append(resultSet.getString(i)).append(",");
-        }
-        resultStr.append("\n");
-      }
-      Assert.assertEquals(resultStr.toString(), standard);
-    }
-  }
-
-  /**
-   * show timeseries <seriesPath> usage 1
-   */
-  private void showTimeseriesPath1() throws SQLException {
-    String standard = "Timeseries,Storage Group,DataType,Encoding,\n"
-        + "root.ln.wf01.wt01.status,root.ln.wf01.wt01,BOOLEAN,PLAIN,\n"
-        + "root.ln.wf01.wt01.temperature,root.ln.wf01.wt01,FLOAT,RLE,\n";
-
-    try (ResultSet resultSet = databaseMetaData
-        .getColumns(Constant.CATALOG_TIMESERIES, "root", null, null);) {
-      ResultSetMetaData resultSetMetaData = resultSet.getMetaData();
-      int colCount = resultSetMetaData.getColumnCount();
-      StringBuilder resultStr = new StringBuilder();
-      for (int i = 1; i < colCount + 1; i++) {
-        resultStr.append(resultSetMetaData.getColumnName(i)).append(",");
-      }
-      resultStr.append("\n");
-      while (resultSet.next()) {
-        for (int i = 1; i <= colCount; i++) {
-          resultStr.append(resultSet.getString(i)).append(",");
-        }
-        resultStr.append("\n");
-      }
-      Assert.assertEquals(resultStr.toString(), standard);
-    }
-  }
-
-  /**
-   * show timeseries <seriesPath> usage 2
-   */
-  private void showTimeseriesPath2() throws SQLException {
-    String standard = "DataType,\n" + "BOOLEAN,\n";
-
-    try (ResultSet resultSet = databaseMetaData
-        .getColumns(Constant.CATALOG_TIMESERIES, "root.ln.wf01.wt01.status", null,
-            null);) {
-      ResultSetMetaData resultSetMetaData = resultSet.getMetaData();
-      StringBuilder resultStr = new StringBuilder();
-      resultStr.append(resultSetMetaData.getColumnName(3)).append(",\n");
-      while (resultSet.next()) {
-        resultStr.append(resultSet.getString("heloo"))
-            .append(",");
-        resultStr.append("\n");
-      }
-      Assert.assertEquals(resultStr.toString(), standard);
-    }
-  }
-
-  /**
-   * show storage group
-   */
-  private void showStorageGroup() throws SQLException {
-    String standard = "Storage Group,\n" + "root.ln.wf01.wt01,\n";
-
-    try (ResultSet resultSet = databaseMetaData
-        .getColumns(Constant.CATALOG_STORAGE_GROUP, null, null, null)) {
-      ResultSetMetaData resultSetMetaData = resultSet.getMetaData();
-      int colCount = resultSetMetaData.getColumnCount();
-      StringBuilder resultStr = new StringBuilder();
-      for (int i = 1; i < colCount + 1; i++) {
-        resultStr.append(resultSetMetaData.getColumnName(i)).append(",");
-      }
-      resultStr.append("\n");
-      while (resultSet.next()) {
-        for (int i = 1; i <= colCount; i++) {
-          resultStr.append(resultSet.getString(i)).append(",");
-        }
-        resultStr.append("\n");
-      }
-      Assert.assertEquals(resultStr.toString(), standard);
-    }
-  }
+//  /**
+//   * show storage group
+//   */
+//  private void showStorageGroup() throws SQLException {
+//    String standard = "Storage Group,\n" + "root.ln.wf01.wt01,\n";
+//
+//    try (ResultSet resultSet = databaseMetaData
+//        .getColumns(Constant.CATALOG_STORAGE_GROUP, null, null, null)) {
+//      ResultSetMetaData resultSetMetaData = resultSet.getMetaData();
+//      int colCount = resultSetMetaData.getColumnCount();
+//      StringBuilder resultStr = new StringBuilder();
+//      for (int i = 1; i < colCount + 1; i++) {
+//        resultStr.append(resultSetMetaData.getColumnName(i)).append(",");
+//      }
+//      resultStr.append("\n");
+//      while (resultSet.next()) {
+//        for (int i = 1; i <= colCount; i++) {
+//          resultStr.append(resultSet.getString(i)).append(",");
+//        }
+//        resultStr.append("\n");
+//      }
+//      Assert.assertEquals(resultStr.toString(), standard);
+//    }
+//  }
 
   /**
    * show metadata in json
