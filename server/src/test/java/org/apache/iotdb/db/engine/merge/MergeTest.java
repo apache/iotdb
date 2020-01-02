0734ec132e7f26b0c1cb2e00c603ca674bc11d63
Jiang Tian <jt2594838@163.com>
2020-01-02 16:26:01 +0800

    Track the merge history in TsFileResource (#683)
    * Track the merge history in TsFileResource

--- server/src/test/java/org/apache/iotdb/db/engine/merge/MergeTest.java
+++ server/src/test/java/org/apache/iotdb/db/engine/merge/MergeTest.java
@@ -123,6 +123,7 @@ abstract class MergeTest {
               + i + IoTDBConstant.TSFILE_NAME_SEPARATOR + 0
               + ".tsfile"));
       TsFileResource tsFileResource = new TsFileResource(file);
+      tsFileResource.setHistoricalVersions(Collections.singleton((long) i));
       seqResources.add(tsFileResource);
       prepareFile(tsFileResource, i * ptNum, ptNum, 0);
     }
@@ -133,12 +134,14 @@ abstract class MergeTest {
               + i + IoTDBConstant.TSFILE_NAME_SEPARATOR + 0
               + ".tsfile"));
       TsFileResource tsFileResource = new TsFileResource(file);
+      tsFileResource.setHistoricalVersions(Collections.singleton((long) (i + seqFileNum)));
       unseqResources.add(tsFileResource);
       prepareFile(tsFileResource, i * ptNum, ptNum * (i + 1) / unseqFileNum, 10000);
     }
     File file = new File(TestConstant.BASE_OUTPUT_PATH.concat(unseqFileNum + "unseq" + IoTDBConstant.TSFILE_NAME_SEPARATOR + unseqFileNum
         + IoTDBConstant.TSFILE_NAME_SEPARATOR + unseqFileNum + IoTDBConstant.TSFILE_NAME_SEPARATOR + 0 + ".tsfile"));
     TsFileResource tsFileResource = new TsFileResource(file);
+    tsFileResource.setHistoricalVersions(Collections.singleton((long) (seqFileNum + unseqFileNum)));
     unseqResources.add(tsFileResource);
     prepareFile(tsFileResource, 0, ptNum * unseqFileNum, 20000);
   }
