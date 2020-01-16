062d6a8c97a8e49c3f5e2705ffe1e2f6f8f0a991
samperson1997 <szs19@mails.tsinghua.edu.cn>
2020-01-16 20:12:42 +0800

    Exchange extend relationship of AVG and SUM

--- server/src/main/java/org/apache/iotdb/db/query/aggregation/impl/AvgAggrResult.java
+++ server/src/main/java/org/apache/iotdb/db/query/aggregation/impl/AvgAggrResult.java
@@ -19,26 +19,15 @@
 
 package org.apache.iotdb.db.query.aggregation.impl;
 
-import java.io.IOException;
-import org.apache.iotdb.db.query.aggregation.AggregateResult;
-import org.apache.iotdb.db.query.reader.IReaderByTimestamp;
 import org.apache.iotdb.tsfile.file.metadata.enums.TSDataType;
-import org.apache.iotdb.tsfile.file.metadata.statistics.Statistics;
-import org.apache.iotdb.tsfile.read.common.BatchData;
 
-public class AvgAggrResult extends AggregateResult {
+public class AvgAggrResult extends SumAggrResult {
 
-  protected double sum = 0.0;
-  private int cnt = 0;
-  private TSDataType seriesDataType;
   private static final String AVG_AGGR_NAME = "AVG";
 
-  public AvgAggrResult(TSDataType seriesDataType) {
+  public AvgAggrResult() {
     super(TSDataType.DOUBLE);
-    this.seriesDataType = seriesDataType;
     reset();
-    sum = 0.0;
-    cnt = 0;
   }
 
   @Override
@@ -49,70 +38,10 @@ public class AvgAggrResult extends AggregateResult {
     return hasResult() ? getDoubleRet() : null;
   }
 
-  @Override
-  public void updateResultFromStatistics(Statistics statistics) {
-    cnt += statistics.getCount();
-    sum += statistics.getSumValue();
-  }
-
-  @Override
-  public void updateResultFromPageData(BatchData dataInThisPage) throws IOException {
-    updateResultFromPageData(dataInThisPage, Long.MAX_VALUE);
-  }
-
-  @Override
-  public void updateResultFromPageData(BatchData dataInThisPage, long bound) throws IOException {
-    while (dataInThisPage.hasCurrent()) {
-      if (dataInThisPage.currentTime() >= bound) {
-        break;
-      }
-      updateAvg(seriesDataType, dataInThisPage.currentValue());
-      dataInThisPage.next();
-    }
-  }
-
-  @Override
-  public void updateResultUsingTimestamps(long[] timestamps, int length,
-      IReaderByTimestamp dataReader) throws IOException {
-    for (int i = 0; i < length; i++) {
-      Object value = dataReader.getValueInTimestamp(timestamps[i]);
-      if (value != null) {
-        updateAvg(seriesDataType, value);
-      }
-    }
-  }
-
-  @Override
-  public boolean isCalculatedAggregationResult() {
-    return false;
-  }
-
-  private void updateAvg(TSDataType type, Object sumVal) throws IOException {
-    switch (type) {
-      case INT32:
-        sum += (int) sumVal;
-        break;
-      case INT64:
-        sum += (long) sumVal;
-        break;
-      case FLOAT:
-        sum += (float) sumVal;
-        break;
-      case DOUBLE:
-        sum += (double) sumVal;
-        break;
-      case TEXT:
-      case BOOLEAN:
-      default:
-        throw new IOException(
-            String.format("Unsupported data type in aggregation %s : %s", getAggrTypeName(), type));
-    }
-    cnt++;
-  }
-
   /**
    * Return type name of aggregation
    */
+  @Override
   public String getAggrTypeName() {
     return AVG_AGGR_NAME;
   }
