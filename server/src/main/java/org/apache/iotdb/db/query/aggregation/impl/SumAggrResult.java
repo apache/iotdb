5d54d4b7167816c78746f29b6a2649cb58039a71
samperson1997 <szs19@mails.tsinghua.edu.cn>
2020-01-16 09:56:33 +0800

    Revert extend relationship of AVG and SUM

--- server/src/main/java/org/apache/iotdb/db/query/aggregation/impl/SumAggrResult.java
+++ server/src/main/java/org/apache/iotdb/db/query/aggregation/impl/SumAggrResult.java
@@ -19,82 +19,20 @@
 
 package org.apache.iotdb.db.query.aggregation.impl;
 
-import java.io.IOException;
-import org.apache.iotdb.db.query.aggregation.AggregateResult;
-import org.apache.iotdb.db.query.reader.IReaderByTimestamp;
 import org.apache.iotdb.tsfile.file.metadata.enums.TSDataType;
-import org.apache.iotdb.tsfile.file.metadata.statistics.Statistics;
-import org.apache.iotdb.tsfile.read.common.BatchData;
 
-public class SumAggrResult extends AggregateResult {
+public class SumAggrResult extends AvgAggrResult {
 
-  protected double sum = 0.0;
   private static final String SUM_AGGR_NAME = "SUM";
 
   public SumAggrResult(TSDataType seriesDataType) {
     super(seriesDataType);
     reset();
-    sum = 0.0;
   }
 
   @Override
-  public Object getResult() {
-    return getValue();
-  }
-
-  @Override
-  public void updateResultFromStatistics(Statistics statistics) {
-    sum += statistics.getSumValue();
-  }
-
-  @Override
-  public void updateResultFromPageData(BatchData dataInThisPage)
-      throws IOException {
-    updateResultFromPageData(dataInThisPage, Long.MAX_VALUE);
-  }
-
-  @Override
-  public void updateResultFromPageData(BatchData dataInThisPage, long bound) throws IOException {
-    while (dataInThisPage.hasCurrent()) {
-      if (dataInThisPage.currentTime() >= bound) {
-        break;
-      }
-      updateSum(getDataType(), dataInThisPage.currentValue());
-      dataInThisPage.next();
-    }
-  }
-
-  private void updateSum(TSDataType type, Object sumVal) throws IOException {
-    switch (type) {
-      case INT32:
-        sum += (int) sumVal;
-        break;
-      case INT64:
-        sum += (long) sumVal;
-        break;
-      case FLOAT:
-        sum += (float) sumVal;
-        break;
-      case DOUBLE:
-        sum += (double) sumVal;
-        break;
-      case TEXT:
-      case BOOLEAN:
-      default:
-        throw new IOException(
-            String.format("Unsupported data type in aggregation %s : %s", getAggrTypeName(), type));
-    }
-  }
-
-  @Override
-  public void updateResultUsingTimestamps(long[] timestamps, int length,
-      IReaderByTimestamp dataReader) throws IOException {
-    for (int i = 0; i < length; i++) {
-      Object value = dataReader.getValueInTimestamp(timestamps[i]);
-      if (value != null) {
-        updateSum(getDataType(), value);
-      }
-    }
+  public Double getResult() {
+    return super.sum;
   }
 
   @Override
@@ -105,6 +43,7 @@ public class SumAggrResult extends AggregateResult {
   /**
    * Return type name of aggregation
    */
+  @Override
   public String getAggrTypeName() {
     return SUM_AGGR_NAME;
   }
