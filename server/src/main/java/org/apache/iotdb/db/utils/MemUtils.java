b0eda6e528007fc4834cc3b4b40c5478b0abba02
SolomonAnn <thss15_anyz@163.com>
2020-02-20 17:23:05 +0800

    delete one unused import

--- server/src/main/java/org/apache/iotdb/db/utils/MemUtils.java
+++ server/src/main/java/org/apache/iotdb/db/utils/MemUtils.java
@@ -21,7 +21,6 @@ package org.apache.iotdb.db.utils;
 import org.apache.iotdb.db.conf.IoTDBConstant;
 import org.apache.iotdb.db.qp.physical.crud.BatchInsertPlan;
 import org.apache.iotdb.db.qp.physical.crud.InsertPlan;
-import org.apache.iotdb.tsfile.file.metadata.enums.TSDataType;
 import org.apache.iotdb.tsfile.utils.Binary;
 import org.apache.iotdb.tsfile.write.record.TSRecord;
 import org.apache.iotdb.tsfile.write.record.datapoint.BooleanDataPoint;
