26e37f2393d8bf2f72a7acf601c6b957aefa612f
jt2594838 <jt2594838@163.com>
2020-03-16 11:35:50 +0800

    add comments in ClientServer

--- cluster/src/main/java/org/apache/iotdb/cluster/server/ClientServer.java
+++ cluster/src/main/java/org/apache/iotdb/cluster/server/ClientServer.java
@@ -27,6 +27,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.SynchronousQueue;
@@ -64,6 +65,7 @@ import org.apache.thrift.server.ServerContext;
 import org.apache.thrift.server.THsHaServer;
 import org.apache.thrift.server.TServer;
 import org.apache.thrift.server.TServerEventHandler;
+import org.apache.thrift.transport.TFastFramedTransport;
 import org.apache.thrift.transport.TFramedTransport;
 import org.apache.thrift.transport.TNonblockingServerSocket;
 import org.apache.thrift.transport.TTransport;
@@ -71,14 +73,41 @@ import org.apache.thrift.transport.TTransportException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+/**
+ * ClientServer is the cluster version of TSServiceImpl, which is responsible for the processing of
+ * the user requests (sqls and session api). It inherits the basic procedures from TSServiceImpl,
+ * but redirect the queries of data and metadata to a MetaGroupMember of the local node.
+ */
 public class ClientServer extends TSServiceImpl {
 
   private static final Logger logger = LoggerFactory.getLogger(ClientServer.class);
+  /**
+   * The MetaGroupMember of the local node. Through this node ClientServer queries data and meta
+   * from the cluster and performs data manipulations to the cluster.
+   */
   private MetaGroupMember metaGroupMember;
 
+  /**
+   * The single thread pool that runs poolServer to unblock the main thread.
+   */
   private ExecutorService serverService;
+
+  /**
+   * Using the poolServer, ClientServer will listen to a socket to accept thrift requests like an
+   * HttpServer.
+   */
   private TServer poolServer;
-  private Map<Long, RemoteQueryContext> queryContextMap = new HashMap<>();
+
+  /**
+   * The socket poolServer will listen to. Async service requires nonblocking socket
+   */
+  private TNonblockingServerSocket serverTransport;
+
+  /**
+   * queryId -> queryContext map. When a query ends either normally or accidentally, the
+   * resources used by the query can be found in the context and then released.
+   */
+  private Map<Long, RemoteQueryContext> queryContextMap = new ConcurrentHashMap<>();
 
   public ClientServer(MetaGroupMember metaGroupMember) throws QueryProcessException {
     super();
@@ -87,6 +116,12 @@ public class ClientServer extends TSServiceImpl {
     this.executor = new ClusterPlanExecutor(metaGroupMember);
   }
 
+  /**
+   * Create a thrift server to listen to the client port and accept requests from clients. This
+   * server is run in a separate thread.
+   * Calling the method twice does not induce side effects.
+   * @throws TTransportException
+   */
   public void start() throws TTransportException {
     if (serverService != null) {
       return;
@@ -96,15 +131,17 @@ public class ClientServer extends TSServiceImpl {
         "ClusterClientServer"));
     ClusterConfig config = ClusterDescriptor.getINSTANCE().getConfig();
 
+    // this defines how thrift parse the requests bytes to a request
     TProtocolFactory protocolFactory;
     if(ClusterDescriptor.getINSTANCE().getConfig().isRpcThriftCompressionEnabled()) {
       protocolFactory = new TCompactProtocol.Factory();
-    }
-    else {
+    } else {
       protocolFactory = new TBinaryProtocol.Factory();
     }
-    TNonblockingServerSocket serverTransport = new TNonblockingServerSocket(new InetSocketAddress(config.getLocalIP(),
+    serverTransport = new TNonblockingServerSocket(new InetSocketAddress(config.getLocalIP(),
         config.getLocalClientPort()));
+    // async service also requires nonblocking server, and HsHaServer is basically more efficient a
+    // nonblocking server
     THsHaServer.Args poolArgs =
         new THsHaServer.Args(serverTransport).maxWorkerThreads(IoTDBDescriptor.
         getInstance().getConfig().getRpcMaxConcurrentClientNum()).minWorkerThreads(1);
@@ -117,32 +154,49 @@ public class ClientServer extends TSServiceImpl {
         return new Thread(r, "ClusterClient" + threadIndex.incrementAndGet());
       }
     }));
+    // ClientServer will do the following processing when the HsHaServer has parsed a request
     poolArgs.processor(new Processor<>(this));
     poolArgs.protocolFactory(protocolFactory);
-    poolArgs.transportFactory(new TFramedTransport.Factory());
+    // nonblocking server requests FramedTransport
+    poolArgs.transportFactory(new TFastFramedTransport.Factory());
 
     poolServer = new THsHaServer(poolArgs);
+    // mainly for handling client exit events
     poolServer.setServerEventHandler(new EventHandler());
 
     serverService.submit(() -> poolServer.serve());
     logger.info("Client service is set up");
   }
 
+  /**
+   * Stop the thrift server, close the socket and shutdown the thread pool.
+   * Calling the method twice does not induce side effects.
+   */
   public void stop() {
     if (serverService == null) {
       return;
     }
 
     poolServer.stop();
+    serverTransport.close();
     serverService.shutdownNow();
   }
 
 
+  /**
+   * Redirect the plan to the local MetaGroupMember so that it will be processed cluster-wide.
+   * @param plan
+   * @return
+   */
   @Override
   protected TSStatus executePlan(PhysicalPlan plan) {
     return metaGroupMember.executeNonQuery(plan);
   }
 
+  /**
+   * EventHandler handles the preprocess and postprocess of the thrift requests, but it currently
+   * only release resources when a client disconnects.
+   */
   class EventHandler implements TServerEventHandler {
 
     @Override
@@ -167,16 +221,40 @@ public class ClientServer extends TSServiceImpl {
     }
   }
 
+  /**
+   * Get the data types of each path in “paths”. If "aggregations" is not null, then it should be
+   * corresponding to "paths" one to one and the data type will be the type of the aggregation
+   * over the corresponding path.
+   * @param paths full timeseries paths
+   * @param aggregations if not null, it should be the same size as "paths"
+   * @return the data types of "paths" (using the aggregations)
+   * @throws MetadataException
+   */
+  @Override
   protected List<TSDataType> getSeriesTypesByPath(List<Path> paths, List<String> aggregations)
       throws MetadataException {
     return metaGroupMember.getSeriesTypesByPath(paths, aggregations);
   }
 
+  /**
+   * Get the data types of each path in “paths”. If "aggregation" is not null, all "paths" will
+   * use this aggregation.
+   * @param paths full timeseries paths
+   * @param aggregation if not null, it means "paths" all use this aggregation
+   * @return the data types of "paths" (using the aggregation)
+   * @throws MetadataException
+   */
   protected List<TSDataType> getSeriesTypesByString(List<String> paths, String aggregation)
       throws MetadataException {
     return metaGroupMember.getSeriesTypesByString(paths, aggregation);
   }
 
+  /**
+   * Generate and cache a QueryContext using "queryId". In the distributed version, the
+   * QueryContext is a RemoteQueryContext.
+   * @param queryId
+   * @return a RemoteQueryContext using queryId
+   */
   @Override
   protected QueryContext genQueryContext(long queryId) {
     RemoteQueryContext context = new RemoteQueryContext(queryId);
@@ -184,9 +262,16 @@ public class ClientServer extends TSServiceImpl {
     return context;
   }
 
+  /**
+   * Release the local and remote resources used by a query.
+   * @param queryId
+   * @throws StorageEngineException
+   */
   @Override
   protected void releaseQueryResource(long queryId) throws StorageEngineException {
+    // release resources locally
     super.releaseQueryResource(queryId);
+    // release resources remotely
     RemoteQueryContext context = queryContextMap.get(queryId);
     if (context != null) {
       // release the resources in every queried node
