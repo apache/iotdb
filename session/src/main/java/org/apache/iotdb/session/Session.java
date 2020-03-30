3498ae4701c68eb04a2d1fce2affe2a1d293e8c0
Ring-k <675550750@qq.com>
2020-03-30 22:52:53 +0800

    Chinese in session

--- session/src/main/java/org/apache/iotdb/session/Session.java
+++ session/src/main/java/org/apache/iotdb/session/Session.java
@@ -24,7 +24,6 @@ import java.time.ZoneId;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import org.apache.iotdb.rpc.BatchExecutionException;
