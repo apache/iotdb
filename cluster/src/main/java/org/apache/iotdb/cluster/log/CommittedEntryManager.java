6f03fa00c1bdabb1851af9b77b27767e5f4f20c5
LebronAl <TXYPotato@gmail.com>
2020-03-31 10:59:18 +0800

    add license

--- cluster/src/main/java/org/apache/iotdb/cluster/log/CommittedEntryManager.java
+++ cluster/src/main/java/org/apache/iotdb/cluster/log/CommittedEntryManager.java
@@ -1,3 +1,22 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 package org.apache.iotdb.cluster.log;
 
 import org.apache.iotdb.cluster.exception.EntryCompactedException;
@@ -81,7 +100,7 @@ public class CommittedEntryManager {
             throw new EntryCompactedException(low, offset);
         }
         if (high > getLastIndex() + 1) {
-            logger.error("entries high ({}) is out of bound lastIndex ({})", high, getLastIndex());
+            logger.debug("entries high ({}) is out of bound lastIndex ({})", high, getLastIndex());
             high = getLastIndex() + 1;
         }
         return entries.subList((int) (low - offset), (int) (high - offset));
@@ -90,7 +109,7 @@ public class CommittedEntryManager {
     public void compactEntries(long compactIndex) throws EntryUnavailableException {
         long offset = entries.get(0).getCurrLogIndex();
         if (compactIndex <= offset) {
-            logger.error("entries before request index ({}) have been compacted, and the compactIndex is ({})", compactIndex, offset);
+            logger.info("entries before request index ({}) have been compacted, and the compactIndex is ({})", compactIndex, offset);
             return;
         }
         if (compactIndex > getLastIndex()) {
@@ -130,7 +149,7 @@ public class CommittedEntryManager {
     }
 
     @TestOnly
-    public CommittedEntryManager(List<Log> entries,RaftSnapshot snapshot) {
+    public CommittedEntryManager(List<Log> entries, RaftSnapshot snapshot) {
         this.entries = entries;
         this.snapshot = snapshot;
     }
--- cluster/src/main/java/org/apache/iotdb/cluster/log/RaftLogManager.java
+++ cluster/src/main/java/org/apache/iotdb/cluster/log/RaftLogManager.java
@@ -1,3 +1,22 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 package org.apache.iotdb.cluster.log;
 
 import org.apache.iotdb.cluster.exception.*;
@@ -135,7 +154,7 @@ public class RaftLogManager {
         return term > getLastTerm() || (term == getLastTerm() && lastIndex >= getLastIndex());
     }
 
-    private long append(List<Log> entries) throws TruncateCommittedEntryException{
+    private long append(List<Log> entries) throws TruncateCommittedEntryException {
         if (entries.size() == 0) {
             return getLastIndex();
         }
