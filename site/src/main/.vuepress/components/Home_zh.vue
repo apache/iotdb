7669197ca5babcf9cb5aac8456fd293f3f64fc00
SailVR <16231015@bjtu.edu.cn>
2020-04-03 10:04:27 +0800

    modify vue home

--- site/src/main/.vuepress/components/Home_zh.vue
+++ site/src/main/.vuepress/components/Home_zh.vue
@@ -58,7 +58,7 @@ Apache IoTDB（孵化中）（物联网数据库）是一个集成数据专为�
 
     <div class="block">
         <el-carousel trigger="click" height="700px">
-          <el-carousel-item v-for="item in imgBlock" :key="item">
+          <el-carousel-item v-for="item in imgBlock" :key="item" style="text-align:center;">
             <img :src="item.src" height="500px">
             <h3 style="font-size: 30px;color: #fcac45;text-align: center;line-height: 0px;">{{item.des}}</h3>
             <p style="font-size: 18px;padding:10px;line-height: 22px;text-align:justify!important;font-weight:bold;">{{item.detail}}</p>
@@ -189,7 +189,7 @@ export default {
     },
     addRoutes2() {
       this.$router.push("/zh/UserGuide/V0.9.x/1-Overview/1-What%20is%20IoTDB.html");
-    }
+    },
   }
 };
 </script>
