bdf53d4b8c15dbc8e6f8a818c13a15ef7f1a1256
SailVR <16231015@bjtu.edu.cn>
2020-04-05 10:36:16 +0800

    modify website

--- site/src/main/.vuepress/config.js
+++ site/src/main/.vuepress/config.js
@@ -105,12 +105,12 @@ var config = {
 				  {
 					text: 'ASF',
 					items: [
-					  { text: 'Foundation', link: 'http://www.apache.org/foundation/'},
-					  { text: 'License', link: 'http://www.apache.org/licenses/'},
-					  { text: 'Security', link: 'http://www.apache.org/security/'},
-					  { text: 'Sponsorship', link: 'http://www.apache.org/foundation/thanks.html'},
-					  { text: 'Thanks', link: 'http://www.apache.org/foundation/thanks.html'},
-					  { text: 'Current Events', link: 'http://www.apache.org/events/current-event'},
+					  { text: 'Foundation', link: 'http://www.apache.org/foundation/',rel:''},
+					  { text: 'License', link: 'http://www.apache.org/licenses/',rel:''},
+					  { text: 'Security', link: 'http://www.apache.org/security/',rel:''},
+					  { text: 'Sponsorship', link: 'http://www.apache.org/foundation/sponsorship.html',rel:''},
+					  { text: 'Thanks', link: 'http://www.apache.org/foundation/thanks.html',rel:''},
+					  { text: 'Current Events', link: 'http://www.apache.org/events/current-event',rel:''},
 					]
 				  },
 				  {
@@ -446,7 +446,6 @@ var config = {
 							'5-DataQuery/5-GroupByQuery',
 							'5-DataQuery/6-LastQuery',
 							'5-DataQuery/7-AlignByDeviceQuery',
-							'5-DataQuery/8-ModificationHandle',
 						]
 					},
 					{
@@ -520,12 +519,12 @@ var config = {
 				  {
 					text: 'ASF',
 					items: [
-					  { text: '基础', link: 'http://www.apache.org/foundation/'},
-					  { text: '执照', link: 'http://www.apache.org/licenses/'},
-					  { text: '安全', link: 'http://www.apache.org/security/'},
-					  { text: '赞助', link: 'http://www.apache.org/foundation/thanks.html'},
-					  { text: '致谢', link: 'http://www.apache.org/foundation/thanks.html'},
-					  { text: '现在发生的事', link: 'http://www.apache.org/events/current-event'},
+					  { text: '基础', link: 'http://www.apache.org/foundation/',rel:''},
+					  { text: '执照', link: 'http://www.apache.org/licenses/',rel:''},
+					  { text: '安全', link: 'http://www.apache.org/security/',rel:''},
+					  { text: '赞助', link: 'http://www.apache.org/foundation/sponsorship.html',rel:''},
+					  { text: '致谢', link: 'http://www.apache.org/foundation/thanks.html',rel:''},
+					  { text: '现在发生的事', link: 'http://www.apache.org/events/current-event',rel:''},
 					]
 				  },
 				  {
@@ -861,7 +860,6 @@ var config = {
 							'5-DataQuery/5-GroupByQuery',
 							'5-DataQuery/6-LastQuery',
 							'5-DataQuery/7-AlignByDeviceQuery',
-							'5-DataQuery/8-ModificationHandle',
 						]
 					},
 					{
