e29bf6060c2355acb16b3aa27ef1ec6c55ae422f
HouliangQi <neuyilan@163.com>
2020-04-09 14:46:53 +0800

    [IOTDB-348] add SetTTLPlan

--- server/src/main/java/org/apache/iotdb/db/qp/physical/PhysicalPlan.java
+++ server/src/main/java/org/apache/iotdb/db/qp/physical/PhysicalPlan.java
@@ -29,6 +29,7 @@ import org.apache.iotdb.db.qp.physical.crud.DeletePlan;
 import org.apache.iotdb.db.qp.physical.crud.InsertPlan;
 import org.apache.iotdb.db.qp.physical.sys.CreateTimeSeriesPlan;
 import org.apache.iotdb.db.qp.physical.sys.SetStorageGroupPlan;
+import org.apache.iotdb.db.qp.physical.sys.SetTTLPlan;
 import org.apache.iotdb.tsfile.read.common.Path;
 import org.apache.iotdb.tsfile.utils.ReadWriteIOUtils;
 
@@ -175,6 +176,10 @@ public abstract class PhysicalPlan {
           plan = new CreateTimeSeriesPlan();
           plan.deserialize(buffer);
           break;
+        case TTL:
+          plan = new SetTTLPlan();
+          plan.deserialize(buffer);
+          break;
         default:
           throw new IOException("unrecognized log type " + type);
       }
@@ -183,7 +188,7 @@ public abstract class PhysicalPlan {
   }
 
   public enum PhysicalPlanType {
-    INSERT, DELETE, BATCHINSERT, SET_STORAGE_GROUP, CREATE_TIMESERIES
+    INSERT, DELETE, BATCHINSERT, SET_STORAGE_GROUP, CREATE_TIMESERIES, TTL
   }
 
 
