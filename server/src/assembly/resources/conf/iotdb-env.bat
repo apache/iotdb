dff018fed63efc11c529bfc0086df6ab0b4a4108
xiangdong huang <sainthxd@gmail.com>
2020-04-12 15:42:01 +0800

    fix win script error

--- server/src/assembly/resources/conf/iotdb-env.bat
+++ server/src/assembly/resources/conf/iotdb-env.bat
@@ -27,7 +27,7 @@ set JMX_PORT="31999"
 # 0.0.0.0 is not allowed
 set JMX_IP="127.0.0.1"
 
-if [ %JMX_LOCAL% = "false" ]; then
+if %JMX_LOCAL% == "false" (
   echo "setting remote JMX..."
   #you may have no permission to run chmod. If so, contact your system administrator.
   set IOTDB_JMX_OPTS="%IOTDB_JMX_OPTS% -Dcom.sun.management.jmxremote"
@@ -39,9 +39,9 @@ if [ %JMX_LOCAL% = "false" ]; then
   set IOTDB_JMX_OPTS="%IOTDB_JMX_OPTS% -Dcom.sun.management.jmxremote.password.file=%IOTDB_CONF%\jmx.password"
   set IOTDB_JMX_OPTS="%IOTDB_JMX_OPTS% -Dcom.sun.management.jmxremote.access.file=%IOTDB_CONF%\jmx.access"
   set IOTDB_JMX_OPTS="%IOTDB_JMX_OPTS% -Djava.rmi.server.hostname=%JMX_IP%"
-else
+) else (
   echo "setting local JMX..."
-fi
+)
 
 IF ["%IOTDB_HEAP_OPTS%"] EQU [""] (
 	rem detect Java 8 or 11
