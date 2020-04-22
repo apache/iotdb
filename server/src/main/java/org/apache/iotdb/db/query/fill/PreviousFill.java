11949b8957685abd98627ad7029ce3f9220be2c0
wshao08 <59866276+wshao08@users.noreply.github.com>
2020-04-22 14:36:53 +0800

    Add modified flag in Metadatas and remove canUseStatistics() (#1083)
    * Add hasModification() in Metadatas and remove canUseStatistics() in Statistics

--- server/src/main/java/org/apache/iotdb/db/query/fill/PreviousFill.java
+++ server/src/main/java/org/apache/iotdb/db/query/fill/PreviousFill.java
@@ -176,12 +176,12 @@ public class PreviousFill extends IFill {
           FileLoaderUtils.loadTimeSeriesMetadata(
               unseqFileResource.poll(), seriesPath, context, timeFilter, allSensors);
 
-      if (timeseriesMetadata == null || (timeseriesMetadata.getStatistics().canUseStatistics()
+      if (timeseriesMetadata == null || (!timeseriesMetadata.isModified()
           && timeseriesMetadata.getStatistics().getEndTime() < lBoundTime)) {
         continue;
       }
       unseqTimeseriesMetadataList.add(timeseriesMetadata);
-      if (timeseriesMetadata.getStatistics().canUseStatistics()) {
+      if (!timeseriesMetadata.isModified()) {
         if (endtimeContainedByTimeFilter(timeseriesMetadata.getStatistics())) {
           lBoundTime = Math.max(lBoundTime, timeseriesMetadata.getStatistics().getEndTime());
         } else {
@@ -198,7 +198,7 @@ public class PreviousFill extends IFill {
     }
     Statistics chunkStatistics = chunkMetaData.getStatistics();
 
-    if (chunkStatistics.canUseStatistics() && endtimeContainedByTimeFilter(chunkStatistics)) {
+    if (!chunkMetaData.isModified() && endtimeContainedByTimeFilter(chunkStatistics)) {
       return constructLastPair(
           chunkStatistics.getEndTime(), chunkStatistics.getLastValue(), dataType);
     }
@@ -206,7 +206,7 @@ public class PreviousFill extends IFill {
     for (int i = pageReaders.size() - 1; i >= 0; i--) {
       IPageReader pageReader = pageReaders.get(i);
       Statistics pageStatistics = pageReader.getStatistics();
-      if (pageStatistics.canUseStatistics() && endtimeContainedByTimeFilter(pageStatistics)) {
+      if (!pageReader.isModified() && endtimeContainedByTimeFilter(pageStatistics)) {
         lastPoint = constructLastPair(
             pageStatistics.getEndTime(), pageStatistics.getLastValue(), dataType);
       } else {
