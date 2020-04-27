1f967254381a1541df34d6aeb4e81421e301f727
chaow <xunmengzhuiyi@gmail.com>
2020-04-27 09:51:21 +0800

    [IOTDB-604] support more robust remote reader (#1090)
    Co-authored-by: mychaow <941210239@qq.com>

---
+++ cluster/src/main/java/org/apache/iotdb/cluster/query/reader/DataSourceInfo.java
@@ -0,0 +1,174 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.iotdb.cluster.query.reader;
+
+import org.apache.iotdb.cluster.client.DataClient;
+import org.apache.iotdb.cluster.partition.PartitionGroup;
+import org.apache.iotdb.cluster.query.RemoteQueryContext;
+import org.apache.iotdb.cluster.rpc.thrift.Node;
+import org.apache.iotdb.cluster.rpc.thrift.SingleSeriesQueryRequest;
+import org.apache.iotdb.cluster.server.handlers.caller.GenericHandler;
+import org.apache.iotdb.cluster.server.member.MetaGroupMember;
+import org.apache.iotdb.cluster.utils.SerializeUtils;
+import org.apache.iotdb.tsfile.file.metadata.enums.TSDataType;
+import org.apache.iotdb.tsfile.read.filter.TimeFilter;
+import org.apache.iotdb.tsfile.read.filter.basic.Filter;
+import org.apache.iotdb.tsfile.read.filter.factory.FilterFactory;
+import org.apache.iotdb.tsfile.read.filter.operator.AndFilter;
+import org.apache.thrift.TException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicReference;
+
+import static org.apache.iotdb.cluster.server.RaftServer.connectionTimeoutInMS;
+
+/**
+ * provide client which could connect to all nodes of the partitionGroup
+ */
+public class DataSourceInfo {
+  private static final Logger logger = LoggerFactory.getLogger(DataSourceInfo.class);
+
+  private long readerId;
+  private Node curSource;
+  private PartitionGroup partitionGroup;
+  private TSDataType dataType;
+  private SingleSeriesQueryRequest request;
+  private RemoteQueryContext context;
+  private MetaGroupMember metaGroupMember;
+  private List<Node> nodes;
+  private DataClient curClient;
+  private boolean isNoData;
+  private int curPos;
+
+  public DataSourceInfo(PartitionGroup group, TSDataType dataType,
+                        SingleSeriesQueryRequest request, RemoteQueryContext context,
+                        MetaGroupMember metaGroupMember, List<Node> nodes) {
+    this.readerId = -1;
+    this.partitionGroup = group;
+    this.dataType = dataType;
+    this.request = request;
+    this.context = context;
+    this.metaGroupMember = metaGroupMember;
+    this.nodes = nodes;
+    this.isNoData = false;
+    this.curPos = -1;
+    this.curSource = null;
+  }
+
+  public DataClient nextDataClient(boolean byTimestamp, long timestamp) {
+    if (this.nodes.isEmpty()) {
+      this.isNoData = false;
+      this.curClient = null;
+      return null;
+    }
+
+    AtomicReference<Long> result = new AtomicReference<>();
+
+    int nextNodePos = (this.curPos + 1) % this.nodes.size();
+    while (true) {
+      Node node = nodes.get(nextNodePos);
+      logger.debug("querying {} from {}", request.path, node);
+      GenericHandler<Long> handler = new GenericHandler<>(node, result);
+      try {
+        DataClient client = this.metaGroupMember.getDataClient(node);
+        synchronized (result) {
+          result.set(null);
+          if (byTimestamp) {
+            client.querySingleSeriesByTimestamp(request, handler);
+          } else {
+            // add timestamp to as a timeFilter to skip the data which has been read
+            Filter newFilter = null;
+            if (request.isSetTimeFilterBytes()) {
+              Filter timeFilter = FilterFactory.deserialize(request.timeFilterBytes);
+              newFilter = new AndFilter(timeFilter, TimeFilter.gt(timestamp));
+            } else {
+              newFilter = TimeFilter.gt(timestamp);
+            }
+            request.setTimeFilterBytes(SerializeUtils.serializeFilter(newFilter));
+            client.querySingleSeries(request, handler);
+          }
+
+          result.wait(connectionTimeoutInMS);
+        }
+        Long readerId = result.get();
+        if (readerId != null) {
+          if (readerId != -1) {
+            // register the node so the remote resources can be released
+            context.registerRemoteNode(partitionGroup.getHeader(), node);
+            logger.debug("get a readerId {} for {} from {}", readerId, request.path, node);
+            this.readerId = readerId;
+            this.curSource = node;
+            this.curPos = nextNodePos;
+            this.curClient = client;
+            return client;
+          } else {
+            // the id being -1 means there is no satisfying data on the remote node, create an
+            // empty reader to reduce further communication
+            this.curClient = null;
+            this.isNoData = true;
+            return null;
+          }
+        }
+      } catch (TException | InterruptedException | IOException e) {
+        logger.error("Cannot query {} from {}", this.request.path, node, e);
+      }
+      nextNodePos = (nextNodePos + 1) % this.nodes.size();
+      if (nextNodePos == this.curPos) {
+        // has iterate over all nodes
+        break;
+      }
+    }
+    // all nodes are failed
+    this.isNoData = false;
+    this.curClient = null;
+    return null;
+  }
+
+  public long getReaderId() {
+    return this.readerId;
+  }
+
+  public TSDataType getDataType() {
+    return this.dataType;
+  }
+
+  public Node getHeader() {
+    return partitionGroup.getHeader();
+  }
+
+  public Node getCurrentNode() {
+    return this.curSource;
+  }
+
+  public DataClient getCurClient() {
+    return this.curClient;
+  }
+
+  public boolean isNoData() {
+    return this.isNoData;
+  }
+
+  public void setMetaGroupMemberForTest(MetaGroupMember metaGroupMember) {
+    this.metaGroupMember = metaGroupMember;
+  }
+}
--- cluster/src/test/java/org/apache/iotdb/cluster/query/reader/RemoteSimpleSeriesReaderTest.java
+++ cluster/src/test/java/org/apache/iotdb/cluster/query/reader/RemoteSimpleSeriesReaderTest.java
@@ -22,6 +22,7 @@ package org.apache.iotdb.cluster.query.reader;
 import static junit.framework.TestCase.assertEquals;
 import static junit.framework.TestCase.assertFalse;
 import static junit.framework.TestCase.assertTrue;
+import static org.junit.Assert.fail;
 
 import java.io.ByteArrayOutputStream;
 import java.io.DataOutputStream;
@@ -30,58 +31,81 @@ import java.nio.ByteBuffer;
 import org.apache.iotdb.cluster.client.DataClient;
 import org.apache.iotdb.cluster.common.TestMetaGroupMember;
 import org.apache.iotdb.cluster.common.TestUtils;
+import org.apache.iotdb.cluster.partition.PartitionGroup;
+import org.apache.iotdb.cluster.query.RemoteQueryContext;
 import org.apache.iotdb.cluster.rpc.thrift.Node;
+import org.apache.iotdb.cluster.rpc.thrift.SingleSeriesQueryRequest;
 import org.apache.iotdb.cluster.server.member.MetaGroupMember;
 import org.apache.iotdb.cluster.utils.SerializeUtils;
 import org.apache.iotdb.tsfile.file.metadata.enums.TSDataType;
 import org.apache.iotdb.tsfile.read.TimeValuePair;
 import org.apache.iotdb.tsfile.read.common.BatchData;
+import org.apache.thrift.TException;
 import org.apache.thrift.async.AsyncMethodCallback;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
 public class RemoteSimpleSeriesReaderTest {
 
   private RemoteSimpleSeriesReader reader;
-  private DataClient client;
   private BatchData batchData;
   private boolean batchUsed;
+  private MetaGroupMember metaGroupMember;
 
   @Before
   public void setUp() throws IOException {
     batchData = TestUtils.genBatchData(TSDataType.DOUBLE, 0, 100);
     batchUsed = false;
-    client = new DataClient(null, null, TestUtils.getNode(0), null){
-      @Override
-      public void fetchSingleSeries(Node header, long readerId,
-          AsyncMethodCallback<ByteBuffer> resultHandler) {
-        new Thread(() -> {
-          if (batchUsed) {
-            resultHandler.onComplete(ByteBuffer.allocate(0));
-          } else {
-            ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-            DataOutputStream dataOutputStream = new DataOutputStream(byteArrayOutputStream);
-            SerializeUtils.serializeBatchData(batchData, dataOutputStream);
-            resultHandler.onComplete(ByteBuffer.wrap(byteArrayOutputStream.toByteArray()));
-            batchUsed = true;
-          }
-        }).start();
-      }
-    };
-    MetaGroupMember metaGroupMember = new TestMetaGroupMember() {
+    metaGroupMember = new TestMetaGroupMember() {
 
       @Override
-      public DataClient getDataClient(Node node) {
-        return client;
+      public DataClient getDataClient(Node node) throws IOException {
+        return new DataClient(null, null, TestUtils.getNode(0), null){
+          @Override
+          public void fetchSingleSeries(Node header, long readerId,
+                                        AsyncMethodCallback<ByteBuffer> resultHandler) {
+            new Thread(() -> {
+              if (batchUsed) {
+                resultHandler.onComplete(ByteBuffer.allocate(0));
+              } else {
+                ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+                DataOutputStream dataOutputStream = new DataOutputStream(byteArrayOutputStream);
+                SerializeUtils.serializeBatchData(batchData, dataOutputStream);
+                resultHandler.onComplete(ByteBuffer.wrap(byteArrayOutputStream.toByteArray()));
+                batchUsed = true;
+              }
+            }).start();
+          }
+
+          @Override
+          public void querySingleSeries(SingleSeriesQueryRequest request, AsyncMethodCallback<Long> resultHandler) throws TException {
+            new Thread(() -> {
+              resultHandler.onComplete(1L);
+            }).start();
+          }
+        };
       }
     };
-    reader = new RemoteSimpleSeriesReader(0, TestUtils.getNode(1), TestUtils.getNode(0),
-        metaGroupMember, TSDataType.DOUBLE);
   }
 
 
   @Test
   public void testSingle() throws IOException {
+    PartitionGroup group = new PartitionGroup();
+    group.add(TestUtils.getNode(0));
+    group.add(TestUtils.getNode(1));
+    group.add(TestUtils.getNode(2));
+
+    SingleSeriesQueryRequest request = new SingleSeriesQueryRequest();
+    RemoteQueryContext context = new RemoteQueryContext(1);
+
+    DataSourceInfo sourceInfo = new DataSourceInfo(group, TSDataType.DOUBLE,
+      request, context, metaGroupMember, group);
+    sourceInfo.nextDataClient(false, Long.MIN_VALUE);
+
+    reader = new RemoteSimpleSeriesReader(sourceInfo);
+
     for (int i = 0; i < 100; i++) {
       assertTrue(reader.hasNextTimeValuePair());
       TimeValuePair curr = reader.currentTimeValuePair();
@@ -91,5 +115,97 @@ public class RemoteSimpleSeriesReaderTest {
       assertEquals(i * 1.0, pair.getValue().getDouble(), 0.00001);
     }
     assertFalse(reader.hasNextTimeValuePair());
+  }
+
+  @Test
+  public void testFailedNode() throws IOException {
+    batchData = TestUtils.genBatchData(TSDataType.DOUBLE, 0, 100);
+    PartitionGroup group = new PartitionGroup();
+    group.add(TestUtils.getNode(0));
+    group.add(TestUtils.getNode(1));
+    group.add(TestUtils.getNode(2));
+
+    SingleSeriesQueryRequest request = new SingleSeriesQueryRequest();
+    RemoteQueryContext context = new RemoteQueryContext(1);
+
+    DataSourceInfo sourceInfo = new DataSourceInfo(group, TSDataType.DOUBLE,
+      request, context, metaGroupMember, group);
+    sourceInfo.nextDataClient(false, Long.MIN_VALUE);
+    reader = new RemoteSimpleSeriesReader(sourceInfo);
+
+    // normal read
+    Assert.assertEquals(TestUtils.getNode(0), sourceInfo.getCurrentNode());
+    for (int i = 0; i < 50; i++) {
+      assertTrue(reader.hasNextTimeValuePair());
+      TimeValuePair curr = reader.currentTimeValuePair();
+      TimeValuePair pair = reader.nextTimeValuePair();
+      assertEquals(pair, curr);
+      assertEquals(i, pair.getTimestamp());
+      assertEquals(i * 1.0, pair.getValue().getDouble(), 0.00001);
+    }
+
+    // a bad client, change to another node
+    DataClient badClient = new DataClient(null, null, TestUtils.getNode(0), null){
+      @Override
+      public void fetchSingleSeries(Node header, long readerId,
+                                    AsyncMethodCallback<ByteBuffer> resultHandler)  throws TException {
+        throw new TException("Good bye");
+      }
+
+      @Override
+      public void querySingleSeries(SingleSeriesQueryRequest request, AsyncMethodCallback<Long> resultHandler) throws TException {
+        new Thread(() -> {
+          resultHandler.onComplete(1L);
+        }).start();
+      }
+    };
+
+    this.batchUsed = false;
+    this.batchData = TestUtils.genBatchData(TSDataType.DOUBLE, 0, 100);
+    // a bad client, change to another node
+    reader.setClientForTest(badClient);
+    reader.clearCurDataForTest();
+    for (int i = 50; i < 80; i++) {
+      TimeValuePair pair = reader.nextTimeValuePair();
+      assertEquals(i - 50, pair.getTimestamp());
+      assertEquals((i - 50) * 1.0, pair.getValue().getDouble(), 0.00001);
+    }
+    Assert.assertEquals(TestUtils.getNode(1), sourceInfo.getCurrentNode());
+
+    this.batchUsed = false;
+    this.batchData = TestUtils.genBatchData(TSDataType.DOUBLE, 0, 100);
+    // a bad client, change to another node again
+    reader.setClientForTest(badClient);
+    reader.clearCurDataForTest();
+    for (int i = 80; i < 90; i++) {
+      TimeValuePair pair = reader.nextTimeValuePair();
+      assertEquals(i - 80, pair.getTimestamp());
+      assertEquals((i - 80) * 1.0, pair.getValue().getDouble(), 0.00001);
+    }
+    assertEquals(TestUtils.getNode(2), sourceInfo.getCurrentNode());
+
+    // all node failed
+    reader.setClientForTest(badClient);
+    reader.clearCurDataForTest();
+    {
+      MetaGroupMember tmpMetaGroupMember = new MetaGroupMember() {
+        @Override
+        public DataClient getDataClient(Node node) throws IOException {
+          return new DataClient(null, null, node, null) {
+            @Override
+            public void querySingleSeries(SingleSeriesQueryRequest request, AsyncMethodCallback<Long> resultHandler) throws TException {
+              throw new TException("Don't worry, this is the exception I constructed.");
+            }
+          };
+        }
+      };
+      sourceInfo.setMetaGroupMemberForTest(tmpMetaGroupMember);
+    }
+    try {
+      reader.nextTimeValuePair();
+      fail();
+    } catch (IOException e) {
+      assertEquals(e.getMessage(), "no available client.");
+    }
   }
 }
