b8e8d091a1592259873807121e4afdb184f85695
HTHou <hhaonan@outlook.com>
2020-05-13 12:05:41 +0800

    reformat

--- example/flink/src/main/java/org/apache/iotdb/flink/TsFileUtils.java
+++ example/flink/src/main/java/org/apache/iotdb/flink/TsFileUtils.java
@@ -53,51 +53,51 @@ public class TsFileUtils {
   private TsFileUtils() {
   }
 
-	public static void writeTsFile(String path) {
-		try {
-			File f = FSFactoryProducer.getFSFactory().getFile(path);
+  public static void writeTsFile(String path) {
+    try {
+      File f = FSFactoryProducer.getFSFactory().getFile(path);
 			Files.delete(f.toPath());
-			Schema schema = new Schema();
-			schema.extendTemplate(DEFAULT_TEMPLATE, new MeasurementSchema("sensor_1", TSDataType.FLOAT, TSEncoding.RLE));
-			schema.extendTemplate(DEFAULT_TEMPLATE, new MeasurementSchema("sensor_2", TSDataType.INT32, TSEncoding.TS_2DIFF));
-			schema.extendTemplate(DEFAULT_TEMPLATE, new MeasurementSchema("sensor_3", TSDataType.INT32, TSEncoding.TS_2DIFF));
+      Schema schema = new Schema();
+      schema.extendTemplate(DEFAULT_TEMPLATE, new MeasurementSchema("sensor_1", TSDataType.FLOAT, TSEncoding.RLE));
+      schema.extendTemplate(DEFAULT_TEMPLATE, new MeasurementSchema("sensor_2", TSDataType.INT32, TSEncoding.TS_2DIFF));
+      schema.extendTemplate(DEFAULT_TEMPLATE, new MeasurementSchema("sensor_3", TSDataType.INT32, TSEncoding.TS_2DIFF));
 
-			try (TsFileWriter tsFileWriter = new TsFileWriter(f, schema)) {
+      try (TsFileWriter tsFileWriter = new TsFileWriter(f, schema)) {
 
-  			// construct TSRecord
-  			for (int i = 0; i < 100; i++) {
-  				TSRecord tsRecord = new TSRecord(i, "device_" + (i % 4));
-  				DataPoint dPoint1 = new LongDataPoint("sensor_1", i);
-  				DataPoint dPoint2 = new LongDataPoint("sensor_2", i);
-  				DataPoint dPoint3 = new LongDataPoint("sensor_3", i);
-  				tsRecord.addTuple(dPoint1);
-  				tsRecord.addTuple(dPoint2);
-  				tsRecord.addTuple(dPoint3);
-  
-  				// write TSRecord
-  				tsFileWriter.write(tsRecord);
-  			}
-			}
+        // construct TSRecord
+        for (int i = 0; i < 100; i++) {
+          TSRecord tsRecord = new TSRecord(i, "device_" + (i % 4));
+          DataPoint dPoint1 = new LongDataPoint("sensor_1", i);
+          DataPoint dPoint2 = new LongDataPoint("sensor_2", i);
+          DataPoint dPoint3 = new LongDataPoint("sensor_3", i);
+          tsRecord.addTuple(dPoint1);
+          tsRecord.addTuple(dPoint2);
+          tsRecord.addTuple(dPoint3);
+          
+          // write TSRecord
+          tsFileWriter.write(tsRecord);
+        }
+      }
 
-		} catch (Exception e) {
-		  logger.error("Write {} failed. ", path, e);
-		}
-	}
+    } catch (Exception e) {
+      logger.error("Write {} failed. ", path, e);
+    }
+  }
 
-	public static String[] readTsFile(String tsFilePath, List<Path> paths) throws IOException {
-		QueryExpression expression = QueryExpression.create(paths, null);
-		TsFileSequenceReader reader = new TsFileSequenceReader(tsFilePath);
-		try (ReadOnlyTsFile readTsFile = new ReadOnlyTsFile(reader)) {
-		  QueryDataSet queryDataSet = readTsFile.query(expression);
-  		List<String> result = new ArrayList<>();
-  		while (queryDataSet.hasNext()) {
-  			RowRecord rowRecord = queryDataSet.next();
-  			String row = rowRecord.getFields().stream()
-  				.map(f -> f == null ? "null" : f.getStringValue())
-  				.collect(Collectors.joining(","));
-  			result.add(rowRecord.getTimestamp() + "," + row);
-  		}
-  		return result.toArray(new String[0]);
-		}
-	}
+  public static String[] readTsFile(String tsFilePath, List<Path> paths) throws IOException {
+    QueryExpression expression = QueryExpression.create(paths, null);
+    TsFileSequenceReader reader = new TsFileSequenceReader(tsFilePath);
+    try (ReadOnlyTsFile readTsFile = new ReadOnlyTsFile(reader)) {
+      QueryDataSet queryDataSet = readTsFile.query(expression);
+      List<String> result = new ArrayList<>();
+      while (queryDataSet.hasNext()) {
+        RowRecord rowRecord = queryDataSet.next();
+        String row = rowRecord.getFields().stream()
+            .map(f -> f == null ? "null" : f.getStringValue())
+            .collect(Collectors.joining(","));
+        result.add(rowRecord.getTimestamp() + "," + row);
+      }
+      return result.toArray(new String[0]);
+    }
+  }
 }
