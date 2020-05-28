94097074d3cbe5522ff71766aef0b5422ce2c433
HTHou <hhaonan@outlook.com>
2020-05-28 19:22:22 +0800

    update docs

--- example/tsfile/src/main/java/org/apache/iotdb/tsfile/TsFileForceAppendWrite.java
+++ example/tsfile/src/main/java/org/apache/iotdb/tsfile/TsFileForceAppendWrite.java
@@ -19,6 +19,8 @@
 package org.apache.iotdb.tsfile;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 
 import org.apache.iotdb.tsfile.file.metadata.enums.TSDataType;
 import org.apache.iotdb.tsfile.file.metadata.enums.TSEncoding;
@@ -37,14 +39,13 @@ public class TsFileForceAppendWrite {
 
   private static final Logger logger = LoggerFactory.getLogger(TsFileForceAppendWrite.class);
 
-  public static void main(String[] args) {
-    try {
-      String path = "test.tsfile";
-      File f = FSFactoryProducer.getFSFactory().getFile(path);
-      if (f.exists()) {
-        f.delete();
-      }
-      TsFileWriter tsFileWriter = new TsFileWriter(f);
+  public static void main(String[] args) throws IOException {
+    String path = "test.tsfile";
+    File f = FSFactoryProducer.getFSFactory().getFile(path);
+    if (f.exists()) {
+      Files.delete(f.toPath());
+    }
+    try (TsFileWriter tsFileWriter = new TsFileWriter(f)) {
 
       // add measurements into file schema
       for (int i = 0; i < 4; i++) {
@@ -69,38 +70,39 @@ public class TsFileForceAppendWrite {
         // write TSRecord
         tsFileWriter.write(tsRecord);
       }
+    } catch (Exception e) {
+      logger.error("meet error in TsFileWrite ", e);
+    }
 
-      tsFileWriter.close();
+    // open the closed file with ForceAppendTsFileWriter
+    ForceAppendTsFileWriter fwriter = new ForceAppendTsFileWriter(f);
+    fwriter.doTruncate();
+    try (TsFileWriter tsFileWriter1 = new TsFileWriter(fwriter)) {
+      // add measurements into file schema
+      for (int i = 0; i < 4; i++) {
+        tsFileWriter1.registerTimeseries(new Path(Constant.DEVICE_PREFIX + i, Constant.SENSOR_1),
+            new MeasurementSchema(Constant.SENSOR_1, TSDataType.INT64, TSEncoding.RLE));
+        tsFileWriter1.registerTimeseries(new Path(Constant.DEVICE_PREFIX + i, Constant.SENSOR_2),
+            new MeasurementSchema(Constant.SENSOR_2, TSDataType.INT64, TSEncoding.RLE));
+        tsFileWriter1.registerTimeseries(new Path(Constant.DEVICE_PREFIX + i, Constant.SENSOR_3),
+            new MeasurementSchema(Constant.SENSOR_3, TSDataType.INT64, TSEncoding.RLE));
+      }
+      // construct TSRecord
+      for (int i = 100; i < 120; i++) {
+        TSRecord tsRecord = new TSRecord(i, Constant.DEVICE_PREFIX + (i % 4));
+        DataPoint dPoint1 = new LongDataPoint(Constant.SENSOR_1, i);
+        DataPoint dPoint2 = new LongDataPoint(Constant.SENSOR_2, i);
+        DataPoint dPoint3 = new LongDataPoint(Constant.SENSOR_3, i);
+        tsRecord.addTuple(dPoint1);
+        tsRecord.addTuple(dPoint2);
+        tsRecord.addTuple(dPoint3);
 
-      // open the closed file with ForceAppendTsFileWriter
-      ForceAppendTsFileWriter fwriter = new ForceAppendTsFileWriter(f);
-      fwriter.doTruncate();
-      try (TsFileWriter tsFileWriter1 = new TsFileWriter(fwriter)) {
-        // add measurements into file schema
-        for (int i = 0; i < 4; i++) {
-          tsFileWriter1.registerTimeseries(new Path(Constant.DEVICE_PREFIX + i, Constant.SENSOR_1),
-              new MeasurementSchema(Constant.SENSOR_1, TSDataType.INT64, TSEncoding.RLE));
-          tsFileWriter1.registerTimeseries(new Path(Constant.DEVICE_PREFIX + i, Constant.SENSOR_2),
-              new MeasurementSchema(Constant.SENSOR_2, TSDataType.INT64, TSEncoding.RLE));
-          tsFileWriter1.registerTimeseries(new Path(Constant.DEVICE_PREFIX + i, Constant.SENSOR_3),
-              new MeasurementSchema(Constant.SENSOR_3, TSDataType.INT64, TSEncoding.RLE));
-        }
-        // construct TSRecord
-        for (int i = 100; i < 120; i++) {
-          TSRecord tsRecord = new TSRecord(i, Constant.DEVICE_PREFIX + (i % 4));
-          DataPoint dPoint1 = new LongDataPoint(Constant.SENSOR_1, i);
-          DataPoint dPoint2 = new LongDataPoint(Constant.SENSOR_2, i);
-          DataPoint dPoint3 = new LongDataPoint(Constant.SENSOR_3, i);
-          tsRecord.addTuple(dPoint1);
-          tsRecord.addTuple(dPoint2);
-          tsRecord.addTuple(dPoint3);
-
-          // write TSRecord
-          tsFileWriter1.write(tsRecord);
-        }
+        // write TSRecord
+        tsFileWriter1.write(tsRecord);
       }
     } catch (Exception e) {
       logger.error("meet error in TsFileWrite ", e);
     }
+    fwriter.close();
   }
 }
