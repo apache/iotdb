6d53987c9602522d20c31e384be662c3b4a803e6
HouliangQi <neuyilan@163.com>
2020-06-15 20:51:22 +0800

    [IOTDB-722] support three consistency levels: strong, mid and weak

--- cluster/src/main/java/org/apache/iotdb/cluster/exception/CheckConsistencyException.java
+++ cluster/src/main/java/org/apache/iotdb/cluster/exception/CheckConsistencyException.java
@@ -23,7 +23,6 @@ package org.apache.iotdb.cluster.exception;
  * syncLeader failed
  */
 public class CheckConsistencyException extends Exception {
-
   public CheckConsistencyException(String errMag) {
     super(String.format("check consistency failed, error message=%s ", errMag));
   }
