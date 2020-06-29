05d5c68408f17ad2fb7d1c8bc2f85226e21771dd
SailVR <16231015@bjtu.edu.cn>
2020-06-29 13:39:18 +0800

    showTimeseriesDataSet

--- server/src/main/java/org/apache/iotdb/db/query/dataset/ShowTimeseriesDataSet.java
+++ server/src/main/java/org/apache/iotdb/db/query/dataset/ShowTimeseriesDataSet.java
@@ -19,18 +19,17 @@
 
 package org.apache.iotdb.db.query.dataset;
 
+import static org.apache.iotdb.db.utils.QueryUtils.transferShowTimeSeriesResultToRecordList;
+
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 import org.apache.iotdb.db.metadata.MManager;
 import org.apache.iotdb.db.qp.physical.sys.ShowPlan;
 import org.apache.iotdb.db.qp.physical.sys.ShowTimeSeriesPlan;
 import org.apache.iotdb.tsfile.file.metadata.enums.TSDataType;
-import org.apache.iotdb.tsfile.read.common.Field;
 import org.apache.iotdb.tsfile.read.common.Path;
 import org.apache.iotdb.tsfile.read.common.RowRecord;
 import org.apache.iotdb.tsfile.read.query.dataset.QueryDataSet;
-import org.apache.iotdb.tsfile.utils.Binary;
 
 public class ShowTimeseriesDataSet extends QueryDataSet {
 
@@ -68,38 +67,4 @@ public class ShowTimeseriesDataSet extends QueryDataSet {
     result.add(newRecord);
   }
 
-  public List<RowRecord> transferShowTimeSeriesResultToRecordList(
-      List<ShowTimeSeriesResult> timeseriesList) {
-    List<RowRecord> records = new ArrayList<>();
-
-    for (ShowTimeSeriesResult result : timeseriesList) {
-      RowRecord record = new RowRecord(0);
-      updateRecord(record, result.getName());
-      updateRecord(record, result.getAlias());
-      updateRecord(record, result.getSgName());
-      updateRecord(record, result.getDataType());
-      updateRecord(record, result.getEncoding());
-      updateRecord(record, result.getCompressor());
-      updateRecord(record, result.getTagAndAttribute(), paths);
-      records.add(record);
-    }
-    return records;
-  }
-
-  private void updateRecord(
-      RowRecord record, Map<String, String> tagAndAttribute, List<Path> paths) {
-    for (int i = 6; i < paths.size(); i++) {
-      updateRecord(record, tagAndAttribute.get(paths.get(i).getFullPath()));
-    }
-  }
-
-  private void updateRecord(RowRecord record, String s) {
-    if (s == null) {
-      record.addField(null);
-      return;
-    }
-    Field field = new Field(TSDataType.TEXT);
-    field.setBinaryV(new Binary(s));
-    record.addField(field);
-  }
 }
