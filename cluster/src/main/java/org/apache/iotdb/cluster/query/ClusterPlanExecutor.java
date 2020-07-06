1f083900e6dc08a1c06404e6bac4a6c888059226
Ring-k <yuyuankang@hotmail.com>
2020-07-06 19:11:52 +0800

    remove import

--- cluster/src/main/java/org/apache/iotdb/cluster/query/ClusterPlanExecutor.java
+++ cluster/src/main/java/org/apache/iotdb/cluster/query/ClusterPlanExecutor.java
@@ -24,7 +24,6 @@ import java.nio.ByteBuffer;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
