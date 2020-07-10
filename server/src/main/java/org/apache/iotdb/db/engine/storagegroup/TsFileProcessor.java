a436db223b387253858c17b04f103432aeecee17
张凌哲 <zhanglingzhe@bytedance.com>
2020-07-10 09:12:45 +0800

    just end file

--- server/src/main/java/org/apache/iotdb/db/engine/storagegroup/TsFileProcessor.java
+++ server/src/main/java/org/apache/iotdb/db/engine/storagegroup/TsFileProcessor.java
@@ -687,6 +687,9 @@ public class TsFileProcessor {
         curWriter.mark();
         flushTask = new MemTableFlushTask(memTableToFlush, curWriter, storageGroupName);
         flushTask.syncFlushMemTable();
+        if (config.isEnableVm()) {
+          curWriter.endFile();
+        }
       } catch (Exception e) {
         logger.error("{}: {} meet error when flushing a memtable, change system mode to read-only",
             storageGroupName, tsFileResource.getTsFile().getName(), e);
