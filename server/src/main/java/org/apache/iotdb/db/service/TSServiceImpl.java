4df62c313bd48ad6e8fb0483092999ea7ec8a854
Xiangwei Wei <526213833@qq.com>
2020-07-22 19:41:03 +0800

    Fix bug

--- server/src/main/java/org/apache/iotdb/db/service/TSServiceImpl.java
+++ server/src/main/java/org/apache/iotdb/db/service/TSServiceImpl.java
@@ -20,7 +20,6 @@ package org.apache.iotdb.db.service;
 
 import static org.apache.iotdb.db.conf.IoTDBConfig.NODE_PATTERN1;
 import static org.apache.iotdb.db.conf.IoTDBConfig.NODE_PATTERN2;
-import static org.apache.iotdb.db.conf.IoTDBConfig.PATH_PATTERN;
 import static org.apache.iotdb.db.qp.physical.sys.ShowPlan.ShowContentType.TIMESERIES;
 
 import java.io.IOException;
@@ -1572,15 +1571,18 @@ public class TSServiceImpl implements TSIService.Iface, ServerContext {
   }
 
   private TSStatus checkPathValidityWithNodes(List<String> nodes) {
+    if (nodes == null) {
+      return RpcUtils.getStatus(TSStatusCode.PATH_ILLEGAL, "path is illegal");
+    }
     int index = 0;
     if (nodes.get(index).equals(IoTDBConstant.PATH_ROOT)) {
       for (index = 1; index < nodes.size(); index++) {
         if (!NODE_PATTERN1.matcher(nodes.get(index)).matches()) {
-            // support double quote on measurement only now
-            if (index == nodes.size() - 1 && NODE_PATTERN2.matcher(nodes.get(index)).matches()) {
-              continue;
-            }
-            break;
+          // support double quote on measurement only now
+          if (index == nodes.size() - 1 && NODE_PATTERN2.matcher(nodes.get(index)).matches()) {
+            continue;
+          }
+          break;
         }
       }
     }
