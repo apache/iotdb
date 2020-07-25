bb20d56d9e829e4a14f660260692c303d94112c9
qiaojialin <646274302@qq.com>
2020-07-25 15:55:45 +0800

    fix load file

--- server/src/main/java/org/apache/iotdb/db/qp/executor/PlanExecutor.java
+++ server/src/main/java/org/apache/iotdb/db/qp/executor/PlanExecutor.java
@@ -697,6 +697,10 @@ public class PlanExecutor implements IPlanExecutor {
       return;
     }
     TsFileResource tsFileResource = new TsFileResource(file);
+    long fileVersion =
+        Long.parseLong(
+            tsFileResource.getTsFile().getName().split(IoTDBConstant.FILE_NAME_SEPARATOR)[1]);
+    tsFileResource.setHistoricalVersions(Collections.singleton(fileVersion));
     tsFileResource.setClosed(true);
     try {
       // check file
