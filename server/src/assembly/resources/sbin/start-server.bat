8cf7ff6538044564b787735ddd6a848789215946
[wyh] <[1101967410@qq.com]>
2020-08-18 12:01:38 +0800

    Enable to stop iotdb server by iotdb.IoTDB

--- server/src/assembly/resources/sbin/start-server.bat
+++ server/src/assembly/resources/sbin/start-server.bat
@@ -95,7 +95,7 @@ set JAVA_OPTS=-ea^
 @REM ***** CLASSPATH library setting *****
 @REM Ensure that any user defined CLASSPATH variables are not used on startup
 set CLASSPATH="%IOTDB_HOME%\lib\*"
-
+set CLASSPATH=%CLASSPATH%;iotdb.IoTDB
 goto okClasspath
 
 :append
