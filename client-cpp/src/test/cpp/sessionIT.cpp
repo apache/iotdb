4bd6db7ab8b86f36a507d5bac0639e8a5403cb5e
wshao08 <kofshaow@163.com>
2020-08-24 07:21:08 +0800

    Fix session IT

--- client-cpp/src/test/cpp/sessionIT.cpp
+++ client-cpp/src/test/cpp/sessionIT.cpp
@@ -24,134 +24,148 @@ using namespace std;
 
 extern Session *session;
 
-static vector<string> testTimeseries =
-    {"root.test.d1.s1","root.test.d1.s2","root.test.d1.s3"};
+static vector<string> testTimeseries = {"root.test.d1.s1","root.test.d1.s2","root.test.d1.s3"};
 
-void prepareTimeseries(){
-    for (string ts : testTimeseries) {
-        session->createTimeseries(ts, TSDataType::INT64, TSEncoding::RLE, CompressionType::SNAPPY);
+void prepareTimeseries() {
+  for (string timeseries : testTimeseries) {
+    if (session->checkTimeseriesExists(timeseries)) {
+      session->deleteTimeseries(timeseries);
     }
-}
-
-void clearTimeseries() {
-    session->deleteTimeseries(testTimeseries);
+    session->createTimeseries(timeseries, TSDataType::INT64, TSEncoding::RLE, CompressionType::SNAPPY);
+  }
 }
 
 TEST_CASE( "Create timeseries success", "[createTimeseries]" ) {
-    if (!session->checkTimeseriesExists("root.test.d1.s1")) {
-        session->createTimeseries("root.test.d1.s1", TSDataType::INT64, TSEncoding::RLE, CompressionType::SNAPPY);
-    }
-    REQUIRE( session->checkTimeseriesExists("root.test.d1.s1") == true );
-    session->deleteTimeseries("root.test.d1.s1");
+  if (!session->checkTimeseriesExists("root.test.d1.s1")) {
+    session->createTimeseries("root.test.d1.s1", TSDataType::INT64, TSEncoding::RLE, CompressionType::SNAPPY);
+  }
+  REQUIRE( session->checkTimeseriesExists("root.test.d1.s1") == true );
+  session->deleteTimeseries("root.test.d1.s1");
+}
+
+TEST_CASE( "Delete timeseries success", "[deleteTimeseries]" ) {
+  if (!session->checkTimeseriesExists("root.test.d1.s1")) {
+    session->createTimeseries("root.test.d1.s1", TSDataType::INT64, TSEncoding::RLE, CompressionType::SNAPPY);
+  }
+  REQUIRE( session->checkTimeseriesExists("root.test.d1.s1") == true );
+  session->deleteTimeseries("root.test.d1.s1");
+  REQUIRE( session->checkTimeseriesExists("root.test.d1.s1") == false );
 }
 
 TEST_CASE( "Test insertRecord by string", "[testInsertRecord]") {
-    prepareTimeseries();
-    string deviceId = "root.test.d1";
-    vector<string> measurements;
-    measurements.push_back("s1");
-    measurements.push_back("s2");
-    measurements.push_back("s3");
+  prepareTimeseries();
+  string deviceId = "root.test.d1";
+  vector<string> measurements;
+  measurements.push_back("s1");
+  measurements.push_back("s2");
+  measurements.push_back("s3");
 
-    for (long time = 0; time < 100; time++) {
-        vector<string> values;
-        values.push_back("1");
-        values.push_back("2");
-        values.push_back("3");
-        session->insertRecord(deviceId, time, measurements, values);
+  for (long time = 0; time < 100; time++) {
+    vector<string> values;
+    values.push_back("1");
+    values.push_back("2");
+    values.push_back("3");
+    session->insertRecord(deviceId, time, measurements, values);
+  }
+
+  session->executeNonQueryStatement("insert into root.test.d1(timestamp,s1, s2, s3) values(100, 1,2,3)");
+
+  SessionDataSet *sessionDataSet = session->executeQueryStatement("select * from root.test.d1");
+  sessionDataSet->setBatchSize(1024);
+  int count = 0;
+  while (sessionDataSet->hasNext()) {
+    long index = 1;
+    count++;
+    for (Field *f : sessionDataSet->next()->fields) {
+      REQUIRE( f->longV == index );
+      index++;
     }
-
-    session->executeNonQueryStatement("insert into root.test.d1(timestamp,s1, s2, s3) values(100, 1,2,3)");
-
-    SessionDataSet *sessionDataSet = session->executeQueryStatement("select * from root.test.d1");
-    sessionDataSet->setBatchSize(1024);
-    int count = 0;
-    while (sessionDataSet->hasNext()) {
-        long index = 1;
-        count++;
-        for (Field *f : sessionDataSet->next()->fields) {
-            REQUIRE( f->longV == index );
-            index++;
-        }
-    }
-    REQUIRE( count == 101 );
-    clearTimeseries();
+  }
+  REQUIRE( count == 101 );
 }
 
 TEST_CASE( "Test insertRecords ", "[testInsertRecords]") {
-    prepareTimeseries();
-    string deviceId = "root.sg1.d1";
-    vector<string> measurements;
-    measurements.push_back("s1");
-    measurements.push_back("s2");
-    measurements.push_back("s3");
-    vector<string> deviceIds;
-    vector<vector<string>> measurementsList;
-    vector<vector<string>> valuesList;
-    vector<int64_t> timestamps;
+  prepareTimeseries();
+  string deviceId = "root.test.d1";
+  vector<string> measurements;
+  measurements.push_back("s1");
+  measurements.push_back("s2");
+  measurements.push_back("s3");
+  vector<string> deviceIds;
+  vector<vector<string>> measurementsList;
+  vector<vector<string>> valuesList;
+  vector<int64_t> timestamps;
 
-    for (int64_t time = 0; time < 500; time++) {
-        vector<string> values;
-        values.push_back("1");
-        values.push_back("2");
-        values.push_back("3");
+  for (int64_t time = 0; time < 500; time++) {
+    vector<string> values;
+    values.push_back("1");
+    values.push_back("2");
+    values.push_back("3");
 
-        deviceIds.push_back(deviceId);
-        measurementsList.push_back(measurements);
-        valuesList.push_back(values);
-        timestamps.push_back(time);
-        if (time != 0 && time % 100 == 0) {
-            session->insertRecords(deviceIds, timestamps, measurementsList, valuesList);
-            deviceIds.clear();
-            measurementsList.clear();
-            valuesList.clear();
-            timestamps.clear();
-        }
+    deviceIds.push_back(deviceId);
+    measurementsList.push_back(measurements);
+    valuesList.push_back(values);
+    timestamps.push_back(time);
+    if (time != 0 && time % 100 == 0) {
+      session->insertRecords(deviceIds, timestamps, measurementsList, valuesList);
+      deviceIds.clear();
+      measurementsList.clear();
+      valuesList.clear();
+      timestamps.clear();
     }
+  }
 
-    session->insertRecords(deviceIds, timestamps, measurementsList, valuesList);
+  session->insertRecords(deviceIds, timestamps, measurementsList, valuesList);
 
-    SessionDataSet *sessionDataSet = session->executeQueryStatement("select * from root.sg1.d2");
-    sessionDataSet->setBatchSize(1024);
-    int count = 0;
-    while (sessionDataSet->hasNext()) {
-        long index = 1;
-        count++;
-        for (Field *f : sessionDataSet->next()->fields) {
-            REQUIRE( f->longV == index );
-            index++;
-        }
+  SessionDataSet *sessionDataSet = session->executeQueryStatement("select * from root.test.d1");
+  sessionDataSet->setBatchSize(1024);
+  int count = 0;
+  while (sessionDataSet->hasNext()) {
+    long index = 1;
+    count++;
+    for (Field *f : sessionDataSet->next()->fields) {
+        REQUIRE( f->longV == index );
+        index++;
     }
-    REQUIRE( count == 500 );
-    clearTimeseries();
+  }
+  REQUIRE( count == 500 );
 }
 
 TEST_CASE( "Test insertTablet ", "[testInsertTablet]") {
-    vector<MeasurementSchema> schemaList;
-    schemaList.push_back(new MeasurementSchema("s1", TSDataType::INT64, TSEncoding::RLE));
-    schemaList.push_back(new MeasurementSchema("s2", TSDataType::INT64, TSEncoding::RLE));
-    schemaList.push_back(new MeasurementSchema("s3", TSDataType::INT64, TSEncoding::RLE));
+  prepareTimeseries();
+  string deviceId = "root.test.d1";
+  vector<pair<string, TSDataType::TSDataType>> schemaList;
+  schemaList.push_back(make_pair("s1", TSDataType::INT64));
+  schemaList.push_back(make_pair("s2", TSDataType::INT64));
+  schemaList.push_back(make_pair("s3", TSDataType::INT64));
 
-    Tablet *tablet = new Tablet(deviceId, schemaList, 100);
-
-    int64_t *timestamps = tablet->timestamps;
-    vector<vector<string>> values = tablet->values;
-
-    for (int64_t time = 0; time < 100; time++) {
-        int row = tablet->rowSize++;f
-        timestamps[row] = time;
-        for (int i = 0; i < 3; i++) {
-            long[] sensor = (long[]) values[i];
-            sensor[row] = i;
-        }
-        if (tablet.rowSize == tablet.getMaxRowNumber()) {
-            session.insertTablet(tablet);
-            tablet.reset();
-        }
+  Tablet tablet(deviceId, schemaList, 100);
+  for (int64_t time = 0; time < 100; time++) {
+    int row = tablet.rowSize++;
+    tablet.timestamps[row] = time;
+    for (int i = 0; i < 3; i++) {
+      tablet.values[i][row] = to_string(i);
     }
-
-    if (tablet.rowSize != 0) {
-        session.insertTablet(tablet);
-        tablet.reset();
+    if (tablet.rowSize == tablet.maxRowNumber) {
+      session->insertTablet(tablet);
+      tablet.reset();
     }
+  }
+
+  if (tablet.rowSize != 0) {
+    session->insertTablet(tablet);
+    tablet.reset();
+  }
+  SessionDataSet *sessionDataSet = session->executeQueryStatement("select * from root.test.d1");
+  sessionDataSet->setBatchSize(1024);
+  int count = 0;
+  while (sessionDataSet->hasNext()) {
+    long index = 0;
+    count++;
+    for (Field *f : sessionDataSet->next()->fields) {
+      REQUIRE( f->longV == index );
+      index++;
+    }
+  }
+  REQUIRE( count == 100 );
 }
