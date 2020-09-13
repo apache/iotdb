8c977af6ccea07dcf76cddbf62c94468792f4d95
zhutianci <zhutianci123456@gmail.com>
2020-09-13 19:58:32 +0800

    Update MTree.java

--- server/src/main/java/org/apache/iotdb/db/metadata/MTree.java
+++ server/src/main/java/org/apache/iotdb/db/metadata/MTree.java
@@ -628,7 +628,7 @@ public class MTree implements Serializable {
     List<Pair<PartialPath, String[]>> res = getAllMeasurementSchema(plan);
     List<PartialPath> paths = new ArrayList<>();
     for (Pair<PartialPath, String[]> p : res) {
-      if (p.right[0] != null) {
+      if (prePath.getMeasurement().equals(p.right[0])) {
         p.left.setMeasurementAlias(p.right[0]);
       }
       paths.add(p.left);
