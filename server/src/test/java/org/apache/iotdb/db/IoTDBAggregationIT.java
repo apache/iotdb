a99ac85ab1b67cb13ef2fd8ea89524ce8124431f
张凌哲 <445073309@qq.com>
2020-10-09 09:35:32 +0800

    remove log

--- server/src/test/java/org/apache/iotdb/db/IoTDBAggregationIT.java
+++ server/src/test/java/org/apache/iotdb/db/IoTDBAggregationIT.java
@@ -566,20 +566,6 @@ public class IoTDBAggregationIT {
         }
       }
 
-      statement.execute("SELECT sum(s2) FROM root.vehicle.d0 WHERE time >= 1000 AND time <= 2000");
-      try (ResultSet resultSet = statement.getResultSet()) {
-        while (resultSet.next()) {
-          System.out.println("sum(s2)=" + resultSet.getString(sum(d0s2)));
-        }
-      }
-
-      statement.execute("SELECT avg(s2) FROM root.vehicle.d0 WHERE time >= 1000 AND time <= 2000");
-      try (ResultSet resultSet = statement.getResultSet()) {
-        while (resultSet.next()) {
-          System.out.println("avg(s2)=" + resultSet.getString(avg(d0s2)));
-        }
-      }
-
       hasResultSet = statement.execute("SELECT sum(s0), avg(s2), avg(s0), sum(s2)" +
           "FROM root.vehicle.d0 WHERE time >= 1000 AND time <= 2000");
       Assert.assertTrue(hasResultSet);
