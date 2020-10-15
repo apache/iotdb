54760ee4a7cce5eea8bce8b75b92d56998a2189e
wshao08 <kofshaow@163.com>
2020-10-15 10:56:29 +0800

    Fix logreplayer test case

--- server/src/main/java/org/apache/iotdb/db/writelog/recover/LogReplayer.java
+++ server/src/main/java/org/apache/iotdb/db/writelog/recover/LogReplayer.java
@@ -121,7 +121,7 @@ public class LogReplayer {
   private void replayDelete(DeletePlan deletePlan) throws IOException, MetadataException {
     List<PartialPath> paths = deletePlan.getPaths();
     for (PartialPath path : paths) {
-      for (PartialPath device : IoTDB.metaManager.getDevices(path)) {
+      for (PartialPath device : IoTDB.metaManager.getDevices(path.getDevicePath())) {
         recoverMemTable
             .delete(path, device, deletePlan.getDeleteStartTime(),
                 deletePlan.getDeleteEndTime());
