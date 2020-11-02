e80416b70da8dee59692de3cabd1b66b50ff604e
CRZbulabula <crzbulabula@gmail.com>
2020-11-03 00:54:59 +0800

    update_start-cli

--- cli/src/assembly/resources/sbin/start-cli.bat
+++ cli/src/assembly/resources/sbin/start-cli.bat
@@ -45,10 +45,10 @@ set PARAMETERS=%*
 @REM if "%PARAMETERS%" == "" set PARAMETERS=-h 127.0.0.1 -p 6667 -u root -pw root
 
 @REM Added parameters when default parameters are missing
-echo %PARAMETERS% | find "-pw ">nul && (set PARAMETERS=%PARAMETERS%) || (set PARAMETERS=-pw root %PARAMETERS%)
-echo %PARAMETERS% | find "-u ">nul && (set PARAMETERS=%PARAMETERS%) || (set PARAMETERS=-u root %PARAMETERS%)
-echo %PARAMETERS% | find "-p ">nul && (set PARAMETERS=%PARAMETERS%) || (set PARAMETERS=-p 6667 %PARAMETERS%)
-echo %PARAMETERS% | find "-h ">nul && (set PARAMETERS=%PARAMETERS%) || (set PARAMETERS=-h 127.0.0.1 %PARAMETERS%)
+echo %PARAMETERS% | find "-pw">nul && (set PARAMETERS=%PARAMETERS%) || (set PARAMETERS=-pw root %PARAMETERS%)
+echo %PARAMETERS% | find "-u">nul && (set PARAMETERS=%PARAMETERS%) || (set PARAMETERS=-u root %PARAMETERS%)
+echo %PARAMETERS% | find "-p">nul && (set PARAMETERS=%PARAMETERS%) || (set PARAMETERS=-p 6667 %PARAMETERS%)
+echo %PARAMETERS% | find "-h">nul && (set PARAMETERS=%PARAMETERS%) || (set PARAMETERS=-h 127.0.0.1 %PARAMETERS%)
 
 @REM echo %PARAMETERS%
 
