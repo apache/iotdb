95c471a4a26acc2071eff32f45b0959abfc9af55
Boris <969032536@qq.com>
2020-11-04 12:00:31 +0800

    Update AbstractCli.java

--- cli/src/main/java/org/apache/iotdb/cli/AbstractCli.java
+++ cli/src/main/java/org/apache/iotdb/cli/AbstractCli.java
@@ -32,6 +32,7 @@ import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
