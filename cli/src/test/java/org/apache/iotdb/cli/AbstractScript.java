c934dde5dd74fd06e52e6ac0f2b80861f2d7ccc8
Boris <969032536@qq.com>
2020-11-04 19:00:51 +0800

    try to fix travis

--- cli/src/test/java/org/apache/iotdb/cli/AbstractScript.java
+++ cli/src/test/java/org/apache/iotdb/cli/AbstractScript.java
@@ -38,10 +38,9 @@ public abstract class AbstractScript {
       if (line == null) {
         break;
       } else {
-        // remove redundant "connection refused", only for test
-        if(line.contains("(Connection refused)")) {
-          line = line.replace("(Connection refused)", "");
-          line = line.trim();
+        // remove thing after "connection refused", only for test
+        if(line.contains("Connection refused")) {
+          line = line.substring(0, line.indexOf("Connection refused") + "Connection refused".length());
         }
         outputList.add(line);
       }
