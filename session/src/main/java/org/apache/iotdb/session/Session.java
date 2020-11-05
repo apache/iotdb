0201afe788be0a58ebddfe3c64bfc04823732193
wshao08 <kofshaow@163.com>
2020-11-05 09:12:16 +0800

    Refine code

--- session/src/main/java/org/apache/iotdb/session/Session.java
+++ session/src/main/java/org/apache/iotdb/session/Session.java
@@ -26,7 +26,6 @@ import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.Executor;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.function.BiConsumer;
--- session/src/main/java/org/apache/iotdb/session/async/AsyncThreadPool.java
+++ session/src/main/java/org/apache/iotdb/session/async/AsyncThreadPool.java
@@ -20,7 +20,6 @@ package org.apache.iotdb.session.async;
 
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.Callable;
-import java.util.concurrent.Executor;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Future;
 import java.util.concurrent.LinkedBlockingQueue;
