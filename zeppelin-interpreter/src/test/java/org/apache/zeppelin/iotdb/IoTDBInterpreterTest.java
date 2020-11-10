4503fa5a88486cabfc528a0fc0f5c5b1bcc89bc2
CRZbulabula <crzbulabula@gmail.com>
2020-11-10 13:32:59 +0800

    build and test success
    build success, pass the test in pr #1739

--- zeppelin-interpreter/src/test/java/org/apache/zeppelin/iotdb/IoTDBInterpreterTest.java
+++ zeppelin-interpreter/src/test/java/org/apache/zeppelin/iotdb/IoTDBInterpreterTest.java
@@ -1,4 +1,21 @@
-package org.apache.zeppelin.iotdb;
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.iotdb.zeppelin;
+
 import java.util.Properties;
 import org.apache.zeppelin.interpreter.InterpreterResult;
 import org.junit.After;
@@ -22,7 +39,7 @@ public class IoTDBInterpreterTest {
 
 	@Test
 	public void testNonQuery() {
-		for(int i=0;i<100;i++){
+		for(int i = 0; i < 100; i++) {
 			String script = String.format("INSERT INTO root.sg1.d1.test(timestamp,temperature) VALUES(%d,%f)",i,Math.random()*10);
 			InterpreterResult interpreterResult = interpreter.interpret(script,null);
 			System.out.println(interpreterResult.message());
