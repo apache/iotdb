6b2af7845d5c6f5ec0e8a0b83a93ef97f8cd630f
Alima777 <526213833@qq.com>
2020-11-10 15:05:03 +0800

    fix iotdb 994 to master

--- server/src/main/java/org/apache/iotdb/db/query/reader/chunk/MemChunkReader.java
+++ server/src/main/java/org/apache/iotdb/db/query/reader/chunk/MemChunkReader.java
@@ -41,7 +41,7 @@ public class MemChunkReader implements IChunkReader, IPointReader {
   private List<IPageReader> pageReaderList;
 
 
-  public MemChunkReader(ReadOnlyMemChunk readableChunk, Filter filter) {
+  public MemChunkReader(ReadOnlyMemChunk readableChunk, Filter filter) throws IOException{
     timeValuePairIterator = readableChunk.getPointReader();
     this.filter = filter;
     // we treat one ReadOnlyMemChunk as one Page
