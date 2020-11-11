6acf1d1e1765006a2b5dd47f43651eb3374def6b
JackieTien97 <JackieTien@foxmail.com>
2020-11-11 11:41:03 +0800

    fix bug

--- server/src/main/java/org/apache/iotdb/db/service/TSServiceImpl.java
+++ server/src/main/java/org/apache/iotdb/db/service/TSServiceImpl.java
@@ -27,7 +27,6 @@ import java.time.ZoneId;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.HashSet;
 import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
