671dd509f20943bd82ef2ac10f85a5094353a5c8
EJTTianyu <1653503269@qq.com>
2020-11-11 22:59:46 +0800

    tmp fix dead lock

--- server/src/main/java/org/apache/iotdb/db/engine/compaction/TsFileManagement.java
+++ server/src/main/java/org/apache/iotdb/db/engine/compaction/TsFileManagement.java
@@ -347,51 +347,46 @@ public abstract class TsFileManagement {
 
   public void mergeEndAction(List<TsFileResource> seqFiles, List<TsFileResource> unseqFiles,
       File mergeLog) {
-    writeLock();
-    try {
-      logger.info("{} a merge task is ending...", storageGroupName);
+    logger.info("{} a merge task is ending...", storageGroupName);
 
-      if (unseqFiles.isEmpty()) {
-        // merge runtime exception arose, just end this merge
-        isUnseqMerging = false;
-        logger.info("{} a merge task abnormally ends", storageGroupName);
-        return;
-      }
-
-      removeUnseqFiles(unseqFiles);
-
-      for (int i = 0; i < seqFiles.size(); i++) {
-        TsFileResource seqFile = seqFiles.get(i);
-        // get both seqFile lock and merge lock
-        doubleWriteLock(seqFile);
-
-        try {
-          File newMergeFile = seqFile.getTsFile();
-          if (!newMergeFile.delete()) {
-            throw new IOException(String.format("delete %s failed", newMergeFile.getAbsolutePath()));
-          }
-          File mergeFile = FSFactoryProducer.getFSFactory()
-              .getFile(seqFile.getTsFilePath() + MergeTask.MERGE_SUFFIX);
-          FSFactoryProducer.getFSFactory().moveFile(mergeFile, newMergeFile);
-          seqFile.setFile(newMergeFile);
-          updateMergeModification(seqFile);
-          if (i == seqFiles.size() - 1) {
-            //FIXME if there is an exception, the the modification file will be not closed.
-            removeMergingModification();
-            isUnseqMerging = false;
-            Files.delete(mergeLog.toPath());
-          }
-        } catch (IOException e) {
-          logger.error("{} a merge task ends but cannot delete log {}", storageGroupName,
-              mergeLog.toPath());
-        } finally {
-          doubleWriteUnlock(seqFile);
-        }
-      }
-      logger.info("{} a merge task ends", storageGroupName);
-    } finally {
-      writeUnlock();
+    if (unseqFiles.isEmpty()) {
+      // merge runtime exception arose, just end this merge
+      isUnseqMerging = false;
+      logger.info("{} a merge task abnormally ends", storageGroupName);
+      return;
     }
+
+    for (int i = 0; i < seqFiles.size(); i++) {
+      TsFileResource seqFile = seqFiles.get(i);
+      // get both seqFile lock and merge lock
+      doubleWriteLock(seqFile);
+
+      try {
+        File newMergeFile = seqFile.getTsFile();
+        if (!newMergeFile.delete()) {
+          throw new IOException(String.format("delete %s failed", newMergeFile.getAbsolutePath()));
+        }
+        File mergeFile = FSFactoryProducer.getFSFactory()
+            .getFile(seqFile.getTsFilePath() + MergeTask.MERGE_SUFFIX);
+        FSFactoryProducer.getFSFactory().moveFile(mergeFile, newMergeFile);
+        seqFile.setFile(newMergeFile);
+        updateMergeModification(seqFile);
+        if (i == seqFiles.size() - 1) {
+          //FIXME if there is an exception, the the modification file will be not closed.
+          removeMergingModification();
+          isUnseqMerging = false;
+          Files.delete(mergeLog.toPath());
+        }
+      } catch (IOException e) {
+        logger.error("{} a merge task ends but cannot delete log {}", storageGroupName,
+            mergeLog.toPath());
+      } finally {
+        doubleWriteUnlock(seqFile);
+      }
+    }
+    removeUnseqFiles(unseqFiles);
+
+    logger.info("{} a merge task ends", storageGroupName);
   }
 
 }
