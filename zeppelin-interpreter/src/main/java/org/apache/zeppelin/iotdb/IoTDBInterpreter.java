9980a5eaa0244c14d26237e61e723264c3d1e812
kr11 <3095717866@qq.com>
2020-11-19 20:17:06 +0800

    format, support muli-lines input

--- zeppelin-interpreter/src/main/java/org/apache/zeppelin/iotdb/IoTDBInterpreter.java
+++ zeppelin-interpreter/src/main/java/org/apache/zeppelin/iotdb/IoTDBInterpreter.java
@@ -17,12 +17,8 @@
 package org.apache.zeppelin.iotdb;
 
 
-import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
-import java.util.HashMap;
-import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.LinkedList;
 import java.util.List;
@@ -32,122 +28,124 @@ import java.util.Properties;
 import java.util.Set;
 import org.apache.iotdb.rpc.IoTDBConnectionException;
 import org.apache.iotdb.rpc.StatementExecutionException;
+import org.apache.iotdb.session.Session;
 import org.apache.iotdb.session.SessionDataSet;
-import org.apache.iotdb.tsfile.read.common.Field;
-import org.apache.iotdb.tsfile.read.common.RowRecord;
-import org.apache.thrift.TException;
 import org.apache.zeppelin.interpreter.Interpreter;
 import org.apache.zeppelin.interpreter.InterpreterContext;
 import org.apache.zeppelin.interpreter.InterpreterResult;
 import org.apache.zeppelin.interpreter.InterpreterResult.Code;
 import org.apache.zeppelin.interpreter.InterpreterResult.Type;
-import org.apache.iotdb.session.Session;
 
 public class IoTDBInterpreter extends Interpreter {
 
-	private IoTDBConnectionException connectionException;
-	private Session session;
+  private IoTDBConnectionException connectionException;
+  private Session session;
 
-	public IoTDBInterpreter(Properties property) {
-		super(property);
-	}
+  public IoTDBInterpreter(Properties property) {
+    super(property);
+  }
 
-	@Override
-	public void open() {
-		session = new Session("127.0.0.1", 6667, "root", "root");
-		try {
-			session.open();
-		} catch (IoTDBConnectionException e) {
-			connectionException = e;
-		}
-	}
+  @Override
+  public void open() {
+    session = new Session("127.0.0.1", 6667, "root", "root");
+    try {
+      session.open();
+    } catch (IoTDBConnectionException e) {
+      connectionException = e;
+    }
+  }
 
-	@Override
-	public void close() {
-		try {
-			session.close();
-		} catch (IoTDBConnectionException e) {
-			connectionException = e;
-		}
-	}
+  @Override
+  public void close() {
+    try {
+      session.close();
+    } catch (IoTDBConnectionException e) {
+      connectionException = e;
+    }
+  }
 
-	@Override
-	public FormType getFormType() {
-		return FormType.SIMPLE;
-	}
+  @Override
+  public FormType getFormType() {
+    return FormType.SIMPLE;
+  }
 
-	@Override
-	public InterpreterResult interpret(String script, InterpreterContext context) {
-		if(connectionException!=null){
-			return new InterpreterResult(Code.ERROR, "IoTDBConnectionException: " + connectionException.getMessage());
-		}
-		try {
-			if(script.toLowerCase().startsWith("select")){
-				//Execute query
-				String msg = null;
-				msg = getResultWithCols(session,script);
-				InterpreterResult interpreterResult = new InterpreterResult(Code.SUCCESS);
-				interpreterResult.add(Type.TABLE, msg);
-				return interpreterResult;
-			} else {
-				//Execute non query
-				session.executeNonQueryStatement(script);
-				return new InterpreterResult(Code.SUCCESS,"Sql executed.");
-			}
-		} catch (StatementExecutionException e) {
-			return new InterpreterResult(Code.ERROR, "StatementExecutionException: " + e.getMessage());
-		} catch (IoTDBConnectionException e) {
-			return new InterpreterResult(Code.ERROR, "IoTDBConnectionException: " + e.getMessage());
-		}
-	}
+  @Override
+  public InterpreterResult interpret(String script, InterpreterContext context) {
+    if (connectionException != null) {
+      return new InterpreterResult(Code.ERROR,
+          "IoTDBConnectionException: " + connectionException.getMessage());
+    }
+    try {
+      String[] scriptLines = script.split("\n");
+      InterpreterResult interpreterResult = null;
+      for (String scriptLine : scriptLines) {
+        if (scriptLine.toLowerCase().startsWith("select")) {
+          //Execute query
+          String msg = null;
+          msg = getResultWithCols(session, scriptLine);
+          interpreterResult = new InterpreterResult(Code.SUCCESS);
+          interpreterResult.add(Type.TABLE, msg);
+        } else {
+          //Execute non query
+          session.executeNonQueryStatement(scriptLine);
+          interpreterResult = new InterpreterResult(Code.SUCCESS, "Sql executed.");
+        }
+      }
+      return interpreterResult;
+    } catch (StatementExecutionException e) {
+      return new InterpreterResult(Code.ERROR, "StatementExecutionException: " + e.getMessage());
+    } catch (IoTDBConnectionException e) {
+      return new InterpreterResult(Code.ERROR, "IoTDBConnectionException: " + e.getMessage());
+    }
+  }
 
-	private String getResultWithCols(Session session, String sql)
-			throws StatementExecutionException, IoTDBConnectionException {
-		SessionDataSet sessionDataSet =session.executeQueryStatement(sql);
-		List<String> columnNames = sessionDataSet.getColumnNames();
-		StringBuilder stringBuilder = new StringBuilder();
-		for (String key : columnNames) {
-			stringBuilder.append(key);
-			stringBuilder.append('\t');
-		}
-		stringBuilder.deleteCharAt(stringBuilder.length() - 1);
-		stringBuilder.append('\n');
-		while (sessionDataSet.hasNext()){
-			stringBuilder.append(sessionDataSet.next()+"\n");
-		}
-		stringBuilder.deleteCharAt(stringBuilder.length() - 1);
-		return stringBuilder.toString();
-	}
+  private String getResultWithCols(Session session, String sql)
+      throws StatementExecutionException, IoTDBConnectionException {
+    SessionDataSet sessionDataSet = session.executeQueryStatement(sql);
+    List<String> columnNames = sessionDataSet.getColumnNames();
+    StringBuilder stringBuilder = new StringBuilder();
+    for (String key : columnNames) {
+      stringBuilder.append(key);
+      stringBuilder.append('\t');
+    }
+    stringBuilder.deleteCharAt(stringBuilder.length() - 1);
+    stringBuilder.append('\n');
+    while (sessionDataSet.hasNext()) {
+      stringBuilder.append(sessionDataSet.next() + "\n");
+    }
+    stringBuilder.deleteCharAt(stringBuilder.length() - 1);
+    return stringBuilder.toString();
+  }
 
-	@Override
-	public int getProgress(InterpreterContext context) {
-		return 0;
-	}
+  @Override
+  public int getProgress(InterpreterContext context) {
+    return 0;
+  }
 
-	@Override
-	public void cancel(InterpreterContext context) {
-		try {
-			session.close();
-		} catch (IoTDBConnectionException e) {
-			e.printStackTrace();
-		}
-	}
+  @Override
+  public void cancel(InterpreterContext context) {
+    try {
+      session.close();
+    } catch (IoTDBConnectionException e) {
+      e.printStackTrace();
+    }
+  }
 
-	private static Map<String, Integer> sortMapByValues(Map<String, Integer> map) {
-		Set<Entry<String,Integer>> mapEntries = map.entrySet();
-		List<Entry<String,Integer>> list = new LinkedList<Entry<String,Integer>>(mapEntries);
-		Collections.sort(list, new Comparator<Entry<String,Integer>>() {
-		@Override
-		public int compare(Entry<String, Integer> e1,Entry<String, Integer> e2) {
-			return e1.getValue().compareTo(e2.getValue());
-		}
-	});
-		Map<String,Integer> sortedMap = new LinkedHashMap<String, Integer>();
-    for(Entry<String,Integer> entry: list) {
-			sortedMap.put(entry.getKey(), entry.getValue());
-		}
+  private static Map<String, Integer> sortMapByValues(Map<String, Integer> map) {
+    Set<Entry<String, Integer>> mapEntries = map.entrySet();
+    List<Entry<String, Integer>> list = new LinkedList<Entry<String, Integer>>(mapEntries);
+    Collections.sort(list, new Comparator<Entry<String, Integer>>() {
+      @Override
+      public int compare(Entry<String, Integer> e1, Entry<String, Integer> e2) {
+        return e1.getValue().compareTo(e2.getValue());
+      }
+    });
+    Map<String, Integer> sortedMap = new LinkedHashMap<String, Integer>();
+    for (Entry<String, Integer> entry : list) {
+      sortedMap.put(entry.getKey(), entry.getValue());
+    }
     return sortedMap;
-	}
+  }
 
 }
 
--- zeppelin-interpreter/src/test/java/org/apache/zeppelin/iotdb/IoTDBInterpreterTest.java
+++ zeppelin-interpreter/src/test/java/org/apache/zeppelin/iotdb/IoTDBInterpreterTest.java
@@ -1,4 +1,5 @@
 package org.apache.zeppelin.iotdb;
+
 import java.util.Properties;
 import org.apache.zeppelin.interpreter.InterpreterResult;
 import org.junit.After;
@@ -6,53 +7,57 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class IoTDBInterpreterTest {
-	private IoTDBInterpreter interpreter;
 
-	@Before
-	public void open() {
-		Properties properties = new Properties();
-		interpreter = new IoTDBInterpreter(properties);
-		interpreter.open();
-	}
+  private IoTDBInterpreter interpreter;
 
-	@After
-	public void close() {
-		interpreter.close();
-	}
+  @Before
+  public void open() {
+    Properties properties = new Properties();
+    interpreter = new IoTDBInterpreter(properties);
+    interpreter.open();
+  }
 
-	@Test
-	public void testNonQuery() {
-		for(int i=0;i<100;i++){
-			String script = String.format("INSERT INTO root.sg1.d1.test(timestamp,temperature) VALUES(%d,%f)",i,Math.random()*10);
-			InterpreterResult interpreterResult = interpreter.interpret(script,null);
-			System.out.println(interpreterResult.message());
-		}
-	}
+  @After
+  public void close() {
+    interpreter.close();
+  }
 
-	@Test
-	public void testQuery() {
-		InterpreterResult interpreterResult = interpreter.interpret("select * from root.sg1.d1.test",null);
-		System.out.print(interpreterResult.message().get(0).getData());
-	}
+  @Test
+  public void testNonQuery() {
+    for (int i = 0; i < 100; i++) {
+      String script = String
+          .format("INSERT INTO root.sg1.d1.test(timestamp,temperature) VALUES(%d,%f)", i,
+              Math.random() * 10);
+      InterpreterResult interpreterResult = interpreter.interpret(script, null);
+      System.out.println(interpreterResult.message());
+    }
+  }
 
-	@Test
-	public void testException() {
-		InterpreterResult interpreterResult;
-		String wrongSql;
+  @Test
+  public void testQuery() {
+    InterpreterResult interpreterResult = interpreter
+        .interpret("select * from root.sg1.d1.test", null);
+    System.out.print(interpreterResult.message().get(0).getData());
+  }
 
-		wrongSql = "select * from";
-		System.out.println("input: " + wrongSql);
-		interpreterResult = interpreter.interpret(wrongSql,null);
-		System.out.println(interpreterResult.message().get(0).getData());
+  @Test
+  public void testException() {
+    InterpreterResult interpreterResult;
+    String wrongSql;
 
-		wrongSql = "select * from a";
-		System.out.println("input: " + wrongSql);
-		interpreterResult = interpreter.interpret(wrongSql,null);
-		System.out.println(interpreterResult.message().get(0).getData());
+    wrongSql = "select * from";
+    System.out.println("input: " + wrongSql);
+    interpreterResult = interpreter.interpret(wrongSql, null);
+    System.out.println(interpreterResult.message().get(0).getData());
 
-		wrongSql = "select * from root a";
-		System.out.println("input: " + wrongSql);
-		interpreterResult = interpreter.interpret(wrongSql,null);
-		System.out.println(interpreterResult.message().get(0).getData());
-	}
+    wrongSql = "select * from a";
+    System.out.println("input: " + wrongSql);
+    interpreterResult = interpreter.interpret(wrongSql, null);
+    System.out.println(interpreterResult.message().get(0).getData());
+
+    wrongSql = "select * from root a";
+    System.out.println("input: " + wrongSql);
+    interpreterResult = interpreter.interpret(wrongSql, null);
+    System.out.println(interpreterResult.message().get(0).getData());
+  }
 }
