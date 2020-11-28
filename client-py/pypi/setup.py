5d143e35e178c20b0c2f191909ec61abafd4fe1a
xiangdong huang <sainthxd@gmail.com>
2020-11-28 19:18:18 +0800

    upgrade version to v0.12

--- client-py/pypi/setup.py
+++ client-py/pypi/setup.py
@@ -31,7 +31,7 @@ print(long_description)
 
 setuptools.setup(
     name="apache-iotdb", # Replace with your own username
-    version="0.11.0",
+    version="0.12.0",
     author=" Apache Software Foundation",
     author_email="dev@iotdb.apache.org",
     description="Apache IoTDB client API",
