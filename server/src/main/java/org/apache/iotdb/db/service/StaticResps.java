9aae15d92f538a4ce731895eb7bf0ce995241a33
Boris <969032536@qq.com>
2020-12-02 18:01:24 +0800

    remove static resps

--- server/src/main/java/org/apache/iotdb/db/service/StaticResps.java
+++ server/src/main/java/org/apache/iotdb/db/service/StaticResps.java
@@ -19,30 +19,10 @@
 
 package org.apache.iotdb.db.service;
 
-import static org.apache.iotdb.db.conf.IoTDBConstant.COLUMN_CANCELLED;
-import static org.apache.iotdb.db.conf.IoTDBConstant.COLUMN_CHILD_PATHS;
-import static org.apache.iotdb.db.conf.IoTDBConstant.COLUMN_COLUMN;
-import static org.apache.iotdb.db.conf.IoTDBConstant.COLUMN_COUNT;
-import static org.apache.iotdb.db.conf.IoTDBConstant.COLUMN_CREATED_TIME;
-import static org.apache.iotdb.db.conf.IoTDBConstant.COLUMN_DEVICES;
-import static org.apache.iotdb.db.conf.IoTDBConstant.COLUMN_DONE;
-import static org.apache.iotdb.db.conf.IoTDBConstant.COLUMN_ITEM;
-import static org.apache.iotdb.db.conf.IoTDBConstant.COLUMN_PRIVILEGE;
-import static org.apache.iotdb.db.conf.IoTDBConstant.COLUMN_PROGRESS;
-import static org.apache.iotdb.db.conf.IoTDBConstant.COLUMN_ROLE;
-import static org.apache.iotdb.db.conf.IoTDBConstant.COLUMN_STORAGE_GROUP;
-import static org.apache.iotdb.db.conf.IoTDBConstant.COLUMN_TASK_NAME;
 import static org.apache.iotdb.db.conf.IoTDBConstant.COLUMN_TIMESERIES;
-import static org.apache.iotdb.db.conf.IoTDBConstant.COLUMN_TIMESERIES_COMPRESSION;
-import static org.apache.iotdb.db.conf.IoTDBConstant.COLUMN_TIMESERIES_DATATYPE;
-import static org.apache.iotdb.db.conf.IoTDBConstant.COLUMN_TIMESERIES_ENCODING;
-import static org.apache.iotdb.db.conf.IoTDBConstant.COLUMN_TTL;
-import static org.apache.iotdb.db.conf.IoTDBConstant.COLUMN_USER;
 import static org.apache.iotdb.db.conf.IoTDBConstant.COLUMN_VALUE;
-import static org.apache.iotdb.db.conf.IoTDBConstant.COLUMN_VERSION;
 
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 import org.apache.iotdb.rpc.RpcUtils;
 import org.apache.iotdb.rpc.TSStatusCode;
@@ -58,88 +38,12 @@ class StaticResps {
     // enum-like class
   }
 
-  static final TSExecuteStatementResp TTL_RESP = getNoTimeExecuteResp(
-      Arrays.asList(COLUMN_STORAGE_GROUP, COLUMN_TTL),
-      Arrays.asList(TSDataType.TEXT.toString(), TSDataType.INT64.toString()));
-
-  static final TSExecuteStatementResp FLUSH_INFO_RESP = getNoTimeExecuteResp(
-      Arrays.asList(COLUMN_ITEM, COLUMN_VALUE),
-      Arrays.asList(TSDataType.TEXT.toString(), TSDataType.TEXT.toString()));
-
-  static final TSExecuteStatementResp SHOW_VERSION_RESP = getNoTimeExecuteResp(
-      Collections.singletonList(COLUMN_VERSION),
-      Collections.singletonList(TSDataType.TEXT.toString()));
-
-  static final TSExecuteStatementResp SHOW_TIMESERIES_RESP = getNoTimeExecuteResp(
-      Arrays.asList(COLUMN_TIMESERIES, COLUMN_STORAGE_GROUP, COLUMN_TIMESERIES_DATATYPE,
-          COLUMN_TIMESERIES_ENCODING, COLUMN_TIMESERIES_COMPRESSION),
-      Arrays.asList(TSDataType.TEXT.toString(),
-          TSDataType.TEXT.toString(),
-          TSDataType.TEXT.toString(),
-          TSDataType.TEXT.toString(),
-          TSDataType.TEXT.toString()));
-
-  static final TSExecuteStatementResp SHOW_DEVICES = getNoTimeExecuteResp(
-      Collections.singletonList(COLUMN_DEVICES),
-      Collections.singletonList(TSDataType.TEXT.toString()));
-
-  static final TSExecuteStatementResp SHOW_STORAGE_GROUP = getNoTimeExecuteResp(
-      Collections.singletonList(COLUMN_STORAGE_GROUP),
-      Collections.singletonList(TSDataType.TEXT.toString()));
-
-  static final TSExecuteStatementResp SHOW_CHILD_PATHS = getNoTimeExecuteResp(
-      Collections.singletonList(COLUMN_CHILD_PATHS),
-      Collections.singletonList(TSDataType.TEXT.toString()));
-
-  static final TSExecuteStatementResp COUNT_TIMESERIES = getNoTimeExecuteResp(
-      Collections.singletonList(COLUMN_COUNT),
-      Collections.singletonList(TSDataType.INT32.toString()));
-
-  static final TSExecuteStatementResp COUNT_NODE_TIMESERIES = getNoTimeExecuteResp(
-      Arrays.asList(COLUMN_COLUMN, COLUMN_COUNT),
-      Arrays.asList(TSDataType.TEXT.toString(), TSDataType.INT32.toString()));
-
-  static final TSExecuteStatementResp COUNT_DEVICES = getNoTimeExecuteResp(
-      Collections.singletonList(COLUMN_COUNT),
-      Collections.singletonList(TSDataType.INT32.toString()));
-
-  static final TSExecuteStatementResp COUNT_STORAGE_GROUP = getNoTimeExecuteResp(
-      Collections.singletonList(COLUMN_COUNT),
-      Collections.singletonList(TSDataType.INT32.toString()));
-
-  static final TSExecuteStatementResp COUNT_NODES = getNoTimeExecuteResp(
-      Collections.singletonList(COLUMN_COUNT),
-      Collections.singletonList(TSDataType.INT32.toString()));
-
-  static final TSExecuteStatementResp LIST_ROLE_RESP = getNoTimeExecuteResp(
-      Collections.singletonList(COLUMN_ROLE),
-      Collections.singletonList(TSDataType.TEXT.toString()));
-
-  static final TSExecuteStatementResp LIST_USER_RESP = getNoTimeExecuteResp(
-      Collections.singletonList(COLUMN_USER),
-      Collections.singletonList(TSDataType.TEXT.toString()));
-
-  static final TSExecuteStatementResp LIST_USER_PRIVILEGE_RESP = getNoTimeExecuteResp(
-      Arrays.asList(COLUMN_ROLE, COLUMN_PRIVILEGE),
-      Arrays.asList(TSDataType.TEXT.toString(), TSDataType.TEXT.toString()));
-
-  static final TSExecuteStatementResp LIST_ROLE_PRIVILEGE_RESP = getNoTimeExecuteResp(
-      Collections.singletonList(COLUMN_PRIVILEGE),
-      Collections.singletonList(TSDataType.TEXT.toString()));
-
   static final TSExecuteStatementResp LAST_RESP = getExecuteResp(
       Arrays.asList(COLUMN_TIMESERIES, COLUMN_VALUE),
       Arrays.asList(TSDataType.TEXT.toString(), TSDataType.TEXT.toString()), false
   );
 
-  static final TSExecuteStatementResp MERGE_STATUS_RESP = getNoTimeExecuteResp(
-      Arrays.asList(COLUMN_STORAGE_GROUP, COLUMN_TASK_NAME, COLUMN_CREATED_TIME, COLUMN_PROGRESS,
-          COLUMN_CANCELLED, COLUMN_DONE),
-      Arrays.asList(TSDataType.TEXT.toString(), TSDataType.TEXT.toString(),
-          TSDataType.TEXT.toString(),
-          TSDataType.TEXT.toString(), TSDataType.BOOLEAN.toString(), TSDataType.BOOLEAN.toString()));
-
-  private static TSExecuteStatementResp getNoTimeExecuteResp(List<String> columns,
+  public static TSExecuteStatementResp getNoTimeExecuteResp(List<String> columns,
       List<String> dataTypes) {
     return getExecuteResp(columns, dataTypes, true);
   }
