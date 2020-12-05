f89a2222c8855d76a0d0c98f03e8fc6f90cc9a0e
张凌哲 <445073309@qq.com>
2020-12-05 17:13:29 +0800

    fix compaction bug after write data by deserialize merge

--- server/src/main/java/org/apache/iotdb/db/engine/compaction/utils/CompactionUtils.java
+++ server/src/main/java/org/apache/iotdb/db/engine/compaction/utils/CompactionUtils.java
@@ -36,6 +36,9 @@ import java.util.TreeMap;
 import org.apache.iotdb.db.conf.IoTDBDescriptor;
 import org.apache.iotdb.db.engine.merge.manage.MergeManager;
 import org.apache.iotdb.db.engine.storagegroup.TsFileResource;
+import org.apache.iotdb.db.exception.metadata.MetadataException;
+import org.apache.iotdb.db.metadata.PartialPath;
+import org.apache.iotdb.db.service.IoTDB;
 import org.apache.iotdb.tsfile.file.metadata.ChunkMetadata;
 import org.apache.iotdb.tsfile.read.TimeValuePair;
 import org.apache.iotdb.tsfile.read.TsFileSequenceReader;
@@ -141,8 +144,13 @@ public class CompactionUtils {
     if (chunkMetadataList.isEmpty()) {
       return maxVersion;
     }
-    IChunkWriter chunkWriter = new ChunkWriterImpl(
-        new MeasurementSchema(entry.getKey(), chunkMetadataList.get(0).getDataType()));
+    IChunkWriter chunkWriter;
+    try {
+      chunkWriter = new ChunkWriterImpl(
+          IoTDB.metaManager.getSeriesSchema(new PartialPath(device), entry.getKey()));
+    } catch (MetadataException e) {
+      throw new IOException(e);
+    }
     for (TimeValuePair timeValuePair : timeValuePairMap.values()) {
       writeTVPair(timeValuePair, chunkWriter);
       targetResource.updateStartTime(device, timeValuePair.getTimestamp());
