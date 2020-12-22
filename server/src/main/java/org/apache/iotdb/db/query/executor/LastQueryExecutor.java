f580a4d8ed2161911ce312363781a481dda6c17a
wshao08 <59866276+wshao08@users.noreply.github.com>
2020-12-22 16:52:26 +0800

    Fix incorrect last result after deleting all data (#2280)

--- server/src/main/java/org/apache/iotdb/db/query/executor/LastQueryExecutor.java
+++ server/src/main/java/org/apache/iotdb/db/query/executor/LastQueryExecutor.java
@@ -90,7 +90,7 @@ public class LastQueryExecutor {
             selectedSeries, dataTypes, context, expression, lastQueryPlan);
 
     for (int i = 0; i < lastPairList.size(); i++) {
-      if (lastPairList.get(i).left) {
+      if (lastPairList.get(i).right != null && lastPairList.get(i).right.getValue() != null) {
         TimeValuePair lastTimeValuePair = lastPairList.get(i).right;
         RowRecord resultRecord = new RowRecord(lastTimeValuePair.getTimestamp());
         Field pathField = new Field(TSDataType.TEXT);
@@ -106,12 +106,8 @@ public class LastQueryExecutor {
         resultRecord.addField(pathField);
 
         Field valueField = new Field(TSDataType.TEXT);
-        if (lastTimeValuePair.getValue() != null) {
-          valueField.setBinaryV(new Binary(lastTimeValuePair.getValue().getStringValue()));
-          resultRecord.addField(valueField);
-        } else {
-          resultRecord.addField(null);
-        }
+        valueField.setBinaryV(new Binary(lastTimeValuePair.getValue().getStringValue()));
+        resultRecord.addField(valueField);
 
         dataSet.putRecord(resultRecord);
       }
@@ -155,11 +151,13 @@ public class LastQueryExecutor {
 
     int index = 0;
     for (int i = 0; i < resultContainer.size(); i++) {
-      if (!resultContainer.get(i).left) {
-        resultContainer.get(i).left = true;
+      if (Boolean.FALSE.equals(resultContainer.get(i).left)) {
         resultContainer.get(i).right = readerList.get(index++).readLastPoint();
-        if (lastCacheEnabled) {
-          cacheAccessors.get(i).write(resultContainer.get(i).right);
+        if (resultContainer.get(i).right.getValue() != null) {
+          resultContainer.get(i).left = true;
+          if (lastCacheEnabled) {
+            cacheAccessors.get(i).write(resultContainer.get(i).right);
+          }
         }
       }
     }
