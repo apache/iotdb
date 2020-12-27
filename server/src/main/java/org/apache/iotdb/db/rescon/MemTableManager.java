3abacc3acb83c584c80851acf879c9b391cec0f8
HTHou <hhaonan@outlook.com>
2020-12-27 15:06:19 +0800

    code smell

--- server/src/main/java/org/apache/iotdb/db/rescon/MemTableManager.java
+++ server/src/main/java/org/apache/iotdb/db/rescon/MemTableManager.java
@@ -26,8 +26,6 @@ import org.apache.iotdb.db.engine.storagegroup.TsFileResource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.concurrent.TimeUnit;
-
 public class MemTableManager {
 
   private static final IoTDBConfig CONFIG = IoTDBDescriptor.getInstance().getConfig();
