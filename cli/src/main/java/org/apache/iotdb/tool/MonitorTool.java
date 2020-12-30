5aa040bc390d0daa4f1734559a4d5734af705e34
Boris <969032536@qq.com>
2020-12-30 17:42:47 +0800

    update according to sonnar

--- cli/src/main/java/org/apache/iotdb/tool/MonitorTool.java
+++ cli/src/main/java/org/apache/iotdb/tool/MonitorTool.java
@@ -47,23 +47,24 @@ import org.apache.iotdb.tsfile.read.common.RowRecord;
 public class MonitorTool {
 
   private static String url;
+  private static final String LINE = "---- ";
 
   public static void main(String[] args)
       throws ParseException, IoTDBConnectionException, IOException, StatementExecutionException {
     int port = 6667;
     String password = "root";
     Options options = new Options();
-    Option opt_help = new Option("help", "help", false, "print help message");
-    opt_help.setRequired(false);
-    options.addOption(opt_help);
+    Option optHelp = new Option("help", "help", false, "print help message");
+    optHelp.setRequired(false);
+    options.addOption(optHelp);
 
-    Option opt_port = new Option("p", "port", true, "port");
-    opt_port.setRequired(false);
-    options.addOption(opt_port);
+    Option optPort = new Option("p", "port", true, "port");
+    optPort.setRequired(false);
+    options.addOption(optPort);
 
-    Option opt_password = new Option("pw", "password", true, "password");
-    opt_password.setRequired(false);
-    options.addOption(opt_password);
+    Option optPassword = new Option("pw", "password", true, "password");
+    optPassword.setRequired(false);
+    options.addOption(optPassword);
 
     HelpFormatter hf=new HelpFormatter();
     hf.setWidth(110);
@@ -98,111 +99,111 @@ public class MonitorTool {
 
   public static void generateOsFile() throws IOException {
 
-    BufferedWriter writer = new BufferedWriter(new FileWriter(new File(url + "/os.txt")));
-    //operate system name
-    String osName = System.getProperty("os.name");
-    writer.write("operate system: " + osName + "\n");
+    try (BufferedWriter writer = new BufferedWriter(new FileWriter(new File(url + "/os.txt")))) {
+      //operate system name
+      String osName = System.getProperty("os.name");
+      writer.write("operate system: " + osName + "\n");
 
-    //jvm memory size in mb
-    long freeMemory = Runtime.getRuntime().freeMemory() / (1024 * 1024);
-    long maxMemory = Runtime.getRuntime().maxMemory() / (1024 * 1024);
-    long totalMemory = Runtime.getRuntime().totalMemory() / (1024 * 1024);
-    long usedMemory = totalMemory - freeMemory;
-    writer.write("jvm max memory: " + maxMemory + "mb"+ "\n");
-    writer.write("jvm total memory: " + totalMemory + "mb" + "\n");
-    writer.write("jvm used memory: " + usedMemory + "mb" + "\n");
-    writer.write("jvm free memory: " + freeMemory + "mb" + "\n");
+      //jvm memory size in mb
+      long freeMemory = Runtime.getRuntime().freeMemory() / (1024 * 1024);
+      long maxMemory = Runtime.getRuntime().maxMemory() / (1024 * 1024);
+      long totalMemory = Runtime.getRuntime().totalMemory() / (1024 * 1024);
+      long usedMemory = totalMemory - freeMemory;
+      writer.write("jvm max memory: " + maxMemory + "mb" + "\n");
+      writer.write("jvm total memory: " + totalMemory + "mb" + "\n");
+      writer.write("jvm used memory: " + usedMemory + "mb" + "\n");
+      writer.write("jvm free memory: " + freeMemory + "mb" + "\n");
 
-    // underlying file stores size in mb
-    for (FileStore store : FileSystems.getDefault().getFileStores()) {
-      writer.write("file store name: " + store.name() + "\n");
-      long total = store.getTotalSpace() / (1024 * 1024);
-      long used = (store.getTotalSpace() - store.getUnallocatedSpace()) / (1024 * 1024);
-      long avail = store.getUsableSpace() / (1024 * 1024);
-      writer.write("---- " + store.name() + "'s total space: " + total + "mb" + "\n");
-      writer.write("---- " + store.name() + "'s used space: " + used + "mb" + "\n");
-      writer.write("---- " + store.name() + "'s avail space: " + avail + "mb" + "\n");
+      // underlying file stores size in mb
+      for (FileStore store : FileSystems.getDefault().getFileStores()) {
+        writer.write("file store name: " + store.name() + "\n");
+        long total = store.getTotalSpace() / (1024 * 1024);
+        long used = (store.getTotalSpace() - store.getUnallocatedSpace()) / (1024 * 1024);
+        long avail = store.getUsableSpace() / (1024 * 1024);
+        writer.write(LINE + store.name() + "'s total space: " + total + "mb" + "\n");
+        writer.write(LINE + store.name() + "'s used space: " + used + "mb" + "\n");
+        writer.write(LINE + store.name() + "'s avail space: " + avail + "mb" + "\n");
+      }
+
+      //network interface name
+      Enumeration<NetworkInterface> interfaces = NetworkInterface.getNetworkInterfaces();
+
+      writer.write("network interface name: " + "\n");
+      while (interfaces.hasMoreElements()) {
+        NetworkInterface networkInterface = interfaces.nextElement();
+        String interfacesName = networkInterface.getDisplayName();
+        writer.write(LINE + interfacesName + "\n");
+      }
+
+      //jdk version
+      String jdk = System.getProperty("java.version");
+      writer.write("jdk version: " + jdk + "\n");
+
+      //operate system memory size in mb
+      OperatingSystemMXBean osBean = ManagementFactory.getPlatformMXBean(
+          OperatingSystemMXBean.class);
+
+      long freeSystemMemory = osBean.getFreePhysicalMemorySize() / (1024 * 1024);
+      long totalSystemMemory = osBean.getTotalPhysicalMemorySize() / (1024 * 1024);
+      long usedSystemMemory = totalSystemMemory - freeSystemMemory / (1024 * 1024);
+
+      writer.write("free system memory: " + freeSystemMemory + "\n");
+      writer.write("total system memory: " + totalSystemMemory + "\n");
+      writer.write("used system memory: " + usedSystemMemory + "\n");
+
+      writer.write("Thread info: " + "\n");
+      for (Thread thread : Thread.getAllStackTraces().keySet()) {
+        writer.write(LINE + thread.getName());
+      }
     }
-
-    //network interface name
-    Enumeration<NetworkInterface> interfaces = NetworkInterface.getNetworkInterfaces();
-
-    writer.write("network interface name: " + "\n");
-    while (interfaces.hasMoreElements())
-    {
-      NetworkInterface networkInterface = interfaces.nextElement();
-      String interfacesName = networkInterface.getDisplayName();
-      writer.write("---- " + interfacesName + "\n");
-    }
-
-    //jdk version
-    String jdk = System.getProperty("java.version");
-    writer.write("jdk version: " + jdk + "\n");
-
-    //operate system memory size in mb
-    OperatingSystemMXBean osBean = ManagementFactory.getPlatformMXBean(
-        OperatingSystemMXBean.class);
-
-    long freeSystemMemory = osBean.getFreePhysicalMemorySize() / (1024 * 1024);
-    long totalSystemMemory = osBean.getTotalPhysicalMemorySize() / (1024 * 1024);
-    long usedSystemMemory = totalSystemMemory - freeSystemMemory / (1024 * 1024);
-
-    writer.write("free system memory: " + freeSystemMemory + "\n");
-    writer.write("total system memory: " + totalSystemMemory + "\n");
-    writer.write("used system memory: " + usedSystemMemory + "\n");
-
-    writer.write("Thread info: " + "\n");
-    for(Thread thread : Thread.getAllStackTraces().keySet()) {
-      writer.write("---- " + thread.getName());
-    }
-
-    writer.close();
   }
 
   public static void generateIoTDBRuntimeFile(int port, String password)
       throws IoTDBConnectionException, StatementExecutionException, IOException {
-    BufferedWriter writer = new BufferedWriter(new FileWriter(new File(url + "/iotdb.txt")));
-    Session session = new Session("127.0.0.1", port, "root", password);
-    session.open();
-    SessionDataSet dataSet = session.executeQueryStatement("count timeseries root");
-    RowRecord record = dataSet.next();
-    writer.write("number of sensors: " + record.getFields().get(0).getLongV() + "\n");
-    dataSet = session.executeQueryStatement("count devices root");
-    record = dataSet.next();
-    writer.write("number of devices: " + record.getFields().get(0).getLongV() + "\n");
-    dataSet = session.executeQueryStatement("count storage group root");
-    record = dataSet.next();
-    writer.write("number of storage group: " + record.getFields().get(0).getLongV() + "\n");
-    dataSet = session.executeQueryStatement("count clients");
-    record = dataSet.next();
-    writer.write("number of clients: " + record.getFields().get(0).getLongV() + "\n");
-    dataSet = session.executeQueryStatement("show configuration");
-    while(dataSet.hasNext()) {
+
+    try (BufferedWriter writer = new BufferedWriter(new FileWriter(new File(url + "/iotdb.txt")))) {
+      Session session = new Session("127.0.0.1", port, "root", password);
+      session.open();
+      SessionDataSet dataSet = session.executeQueryStatement("count timeseries root");
+      RowRecord record = dataSet.next();
+      writer.write("number of sensors: " + record.getFields().get(0).getLongV() + "\n");
+      dataSet = session.executeQueryStatement("count devices root");
       record = dataSet.next();
-      writer.write(record.getFields().get(0).getBinaryV().toString() + ": ");
-      writer.write(record.getFields().get(1).getBinaryV().toString() + "\n");
+      writer.write("number of devices: " + record.getFields().get(0).getLongV() + "\n");
+      dataSet = session.executeQueryStatement("count storage group root");
+      record = dataSet.next();
+      writer.write("number of storage group: " + record.getFields().get(0).getLongV() + "\n");
+      dataSet = session.executeQueryStatement("count clients");
+      record = dataSet.next();
+      writer.write("number of clients: " + record.getFields().get(0).getLongV() + "\n");
+      dataSet = session.executeQueryStatement("show configuration");
+      while (dataSet.hasNext()) {
+        record = dataSet.next();
+        writer.write(record.getFields().get(0).getBinaryV().toString() + ": ");
+        writer.write(record.getFields().get(1).getBinaryV().toString() + "\n");
+      }
+      session.close();
     }
-    session.close();
-    writer.close();
   }
 
   public static void packageFilesToZip() throws IOException {
     File f = new File(url + "/monitor.zip");
-    ZipOutputStream out = new ZipOutputStream(new FileOutputStream(f));
-    ZipEntry os = new ZipEntry(url + "/os.txt");
-    ZipEntry iotdb = new ZipEntry(url + "/iotdb.txt");
-    out.putNextEntry(os);
-    out.putNextEntry(iotdb);
-    File f1 = new File(url + "/conf");
-    if(f1.isDirectory() && f1.exists()) {
-      ZipEntry properties = new ZipEntry(url + "/conf/iotdb-engine.properties");
-      out.putNextEntry(properties);
-      ZipEntry env = new ZipEntry(url + "/conf/iotdb-env.sh");
-      ZipEntry env1 = new ZipEntry(url + "/conf/iotdb-env.bat");
-      out.putNextEntry(env);
-      out.putNextEntry(env1);
+    
+    try (ZipOutputStream out = new ZipOutputStream(new FileOutputStream(f))) {
+      ZipEntry os = new ZipEntry(url + "/os.txt");
+      ZipEntry iotdb = new ZipEntry(url + "/iotdb.txt");
+      out.putNextEntry(os);
+      out.putNextEntry(iotdb);
+      File f1 = new File(url + "/conf");
+      if (f1.isDirectory() && f1.exists()) {
+        ZipEntry properties = new ZipEntry(url + "/conf/iotdb-engine.properties");
+        out.putNextEntry(properties);
+        ZipEntry env = new ZipEntry(url + "/conf/iotdb-env.sh");
+        ZipEntry env1 = new ZipEntry(url + "/conf/iotdb-env.bat");
+        out.putNextEntry(env);
+        out.putNextEntry(env1);
+      }
+      out.closeEntry();
     }
-    out.closeEntry();
-    out.close();
   }
 }
