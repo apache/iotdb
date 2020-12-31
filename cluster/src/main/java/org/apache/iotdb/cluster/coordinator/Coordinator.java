apache/iotdb

commit d98e150cbf80e928692071d3fdf88a3b445f02f6

Move coordinator from meta group member (#2334)

cluster/src/main/java/org/apache/iotdb/cluster/server/MetaClusterServer.java
@@ -21,6 +21,7 @@ package org.apache.iotdb.cluster.server;
 import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
 import org.apache.iotdb.cluster.config.ClusterDescriptor;
+import org.apache.iotdb.cluster.coordinator.Coordinator;
 import org.apache.iotdb.cluster.exception.ConfigInconsistentException;
 import org.apache.iotdb.cluster.exception.StartUpCheckFailureException;
 import org.apache.iotdb.cluster.metadata.CMManager;
@@ -68,6 +69,7 @@ public class MetaClusterServer extends RaftServer implements TSMetaService.Async
 
   // each node only contains one MetaGroupMember
   private MetaGroupMember member;
+  private Coordinator coordinator;
   // the single-node IoTDB instance
   private IoTDB ioTDB;
   // to register the ClusterMonitor that helps monitoring the cluster
@@ -80,6 +82,8 @@ public class MetaClusterServer extends RaftServer implements TSMetaService.Async
     super();
     metaHeartbeatServer = new MetaHeartbeatServer(thisNode, this);
     member = new MetaGroupMember(protocolFactory, thisNode);
+    coordinator = new Coordinator(member);
+    member.setCoordinator(coordinator);
     asyncService = new MetaAsyncService(member);
     syncService = new MetaSyncService(member);
     MetaPuller.getInstance().init(member);
@@ -99,6 +103,7 @@ public class MetaClusterServer extends RaftServer implements TSMetaService.Async
     ioTDB = new IoTDB();
     IoTDB.setMetaManager(CMManager.getInstance());
     ((CMManager) IoTDB.metaManager).setMetaGroupMember(member);
+    ((CMManager) IoTDB.metaManager).setCoordinator(coordinator);
     ioTDB.active();
     member.start();
     registerManager.register(ClusterMonitor.INSTANCE);

cluster/src/test/java/org/apache/iotdb/cluster/server/member/MetaGroupMemberTest.java
@@ -55,6 +55,7 @@ import org.apache.iotdb.cluster.common.TestPartitionedLogManager;
 import org.apache.iotdb.cluster.common.TestSnapshot;
 import org.apache.iotdb.cluster.common.TestUtils;
 import org.apache.iotdb.cluster.config.ClusterDescriptor;
+import org.apache.iotdb.cluster.coordinator.Coordinator;
 import org.apache.iotdb.cluster.exception.ConfigInconsistentException;
 import org.apache.iotdb.cluster.exception.EmptyIntervalException;
 import org.apache.iotdb.cluster.exception.LogExecutionException;
@@ -136,7 +137,7 @@ import org.junit.Test;
 public class MetaGroupMemberTest extends MemberTest {
 
   private DataClusterServer dataClusterServer;
-  private boolean mockDataClusterServer;
+  protected boolean mockDataClusterServer;
   private Node exiledNode;
 
   private int prevReplicaNum;
@@ -190,7 +191,7 @@ public class MetaGroupMemberTest extends MemberTest {
       }
 
       @Override
-      TSStatus executeNonQueryPlan(PhysicalPlan plan) {
+      public TSStatus executeNonQueryPlan(PhysicalPlan plan) {
         try {
           planExecutor.processNonQuery(plan);
           return StatusUtils.OK;
@@ -200,7 +201,7 @@ public class MetaGroupMemberTest extends MemberTest {
       }
 
       @Override
-      TSStatus forwardPlan(PhysicalPlan plan, Node node, Node header) {
+      public TSStatus forwardPlan(PhysicalPlan plan, Node node, Node header) {
         return executeNonQueryPlan(plan);
       }
 
@@ -466,6 +467,7 @@ public class MetaGroupMemberTest extends MemberTest {
         return new TestAsyncDataClient(node, dataGroupMemberMap);
       }
     });
+    metaGroupMember.setCoordinator(new Coordinator());
     return metaGroupMember;
   }
 
@@ -584,6 +586,7 @@ public class MetaGroupMemberTest extends MemberTest {
   public void testJoinCluster() throws QueryProcessException {
     System.out.println("Start testJoinCluster()");
     MetaGroupMember newMember = getMetaGroupMember(TestUtils.getNode(10));
+    newMember.setCoordinator(new Coordinator());
     newMember.start();
     try {
       newMember.joinCluster();
@@ -592,7 +595,7 @@ public class MetaGroupMemberTest extends MemberTest {
         // wait until character changes
       }
     } catch (Exception e) {
-      fail("The expected exception is not thrown");
+      fail("The expected exception is not thrown" + e);
     } finally {
       newMember.stop();
     }
@@ -722,18 +725,18 @@ public class MetaGroupMemberTest extends MemberTest {
     for (int i = 10; i < 20; i++) {
       // process a non partitioned plan
       SetStorageGroupPlan setStorageGroupPlan =
-          new SetStorageGroupPlan(new PartialPath(TestUtils.getTestSg(i)));
-      TSStatus status = testMetaMember.executeNonQueryPlan(setStorageGroupPlan);
+        new SetStorageGroupPlan(new PartialPath(TestUtils.getTestSg(i)));
+      TSStatus status = coordinator.executeNonQueryPlan(setStorageGroupPlan);
       assertEquals(TSStatusCode.SUCCESS_STATUS.getStatusCode(), status.code);
       assertTrue(IoTDB.metaManager.isPathExist(new PartialPath(TestUtils.getTestSg(i))));
 
       // process a partitioned plan
       TimeseriesSchema schema = TestUtils.getTestTimeSeriesSchema(i, 0);
       CreateTimeSeriesPlan createTimeSeriesPlan = new CreateTimeSeriesPlan(
-          new PartialPath(schema.getFullPath()), schema.getType(),
-          schema.getEncodingType(), schema.getCompressor(), schema.getProps(),
-          Collections.emptyMap(), Collections.emptyMap(), null);
-      status = testMetaMember.executeNonQueryPlan(createTimeSeriesPlan);
+        new PartialPath(schema.getFullPath()), schema.getType(),
+        schema.getEncodingType(), schema.getCompressor(), schema.getProps(),
+        Collections.emptyMap(), Collections.emptyMap(), null);
+      status = coordinator.executeNonQueryPlan(createTimeSeriesPlan);
       if (status.getCode() == TSStatusCode.NEED_REDIRECTION.getStatusCode()) {
         status.setCode(TSStatusCode.SUCCESS_STATUS.getStatusCode());
       }
@@ -865,6 +868,7 @@ public class MetaGroupMemberTest extends MemberTest {
   public void testProcessValidHeartbeatReq() throws QueryProcessException {
     System.out.println("Start testProcessValidHeartbeatReq()");
     MetaGroupMember testMetaMember = getMetaGroupMember(TestUtils.getNode(10));
+    testMetaMember.setCoordinator(new Coordinator());
     try {
       HeartBeatRequest request = new HeartBeatRequest();
       request.setRequireIdentifier(true);

cluster/src/main/java/org/apache/iotdb/cluster/config/ClusterConstant.java
@@ -22,8 +22,10 @@ import org.apache.iotdb.cluster.rpc.thrift.Node;
 
 public class ClusterConstant {
 
-  // a failed election will restart in 2s~5s, this should be at least as long as a heartbeat
-  // interval, or a stale node may frequently issue elections and thus makes the leader step down
+  /**
+   * a failed election will restart in 2s~5s, this should be at least as long as a heartbeat
+   * interval, or a stale node may frequently issue elections and thus makes the leader step down
+   */
   public static final long ELECTION_LEAST_TIME_OUT_MS = 2 * 1000L;
   public static final long ELECTION_RANDOM_TIME_OUT_MS = 3 * 1000L;
   public static final int SLOT_NUM = 10000;

cluster/src/main/java/org/apache/iotdb/cluster/metadata/CMManager.java
@@ -52,6 +52,7 @@ import org.apache.iotdb.cluster.client.async.AsyncDataClient;
 import org.apache.iotdb.cluster.client.sync.SyncClientAdaptor;
 import org.apache.iotdb.cluster.client.sync.SyncDataClient;
 import org.apache.iotdb.cluster.config.ClusterDescriptor;
+import org.apache.iotdb.cluster.coordinator.Coordinator;
 import org.apache.iotdb.cluster.exception.CheckConsistencyException;
 import org.apache.iotdb.cluster.exception.UnsupportedPlanException;
 import org.apache.iotdb.cluster.partition.PartitionGroup;
@@ -116,6 +117,7 @@ public class CMManager extends MManager {
   private RemoteMetaCache mRemoteMetaCache;
   private MetaPuller metaPuller;
   private MetaGroupMember metaGroupMember;
+  private Coordinator coordinator;
 
   private CMManager() {
     super();
@@ -542,7 +544,7 @@ public class CMManager extends MManager {
       // TODO-Cluster: add executeNonQueryBatch() to create the series in batch
       TSStatus result;
       try {
-        result = metaGroupMember.processPartitionedPlan(createTimeSeriesPlan);
+        result = coordinator.processPartitionedPlan(createTimeSeriesPlan);
       } catch (UnsupportedPlanException e) {
         logger.error("Failed to create timeseries {} automatically. Unsupported plan exception {} ",
             seriesPath, e.getMessage());
@@ -561,6 +563,10 @@ public class CMManager extends MManager {
 
   public void setMetaGroupMember(MetaGroupMember metaGroupMember) {
     this.metaGroupMember = metaGroupMember;
+  }
+
+  public void setCoordinator(Coordinator coordinator) {
+    this.coordinator = coordinator;
   }
 
   /**

cluster/src/main/java/org/apache/iotdb/cluster/server/ClientServer.java
@@ -37,6 +37,7 @@ import org.apache.iotdb.cluster.client.async.AsyncDataClient;
 import org.apache.iotdb.cluster.client.sync.SyncDataClient;
 import org.apache.iotdb.cluster.config.ClusterConfig;
 import org.apache.iotdb.cluster.config.ClusterDescriptor;
+import org.apache.iotdb.cluster.coordinator.Coordinator;
 import org.apache.iotdb.cluster.metadata.CMManager;
 import org.apache.iotdb.cluster.query.ClusterPlanExecutor;
 import org.apache.iotdb.cluster.query.ClusterPlanner;
@@ -85,10 +86,14 @@ public class ClientServer extends TSServiceImpl {
 
   private static final Logger logger = LoggerFactory.getLogger(ClientServer.class);
   /**
-   * The MetaGroupMember of the local node. Through this node ClientServer queries data and meta
+   * The Coordinator of the local node. Through this node ClientServer queries data and meta
    * from the cluster and performs data manipulations to the cluster.
    */
-  private MetaGroupMember metaGroupMember;
+  private Coordinator coordinator;
+
+  public void setCoordinator(Coordinator coordinator) {
+    this.coordinator = coordinator;
+  }
 
   /**
    * The single thread pool that runs poolServer to unblock the main thread.
@@ -114,7 +119,6 @@ public class ClientServer extends TSServiceImpl {
 
   public ClientServer(MetaGroupMember metaGroupMember) throws QueryProcessException {
     super();
-    this.metaGroupMember = metaGroupMember;
     this.processor = new ClusterPlanner();
     this.executor = new ClusterPlanExecutor(metaGroupMember);
   }
@@ -190,7 +194,7 @@ public class ClientServer extends TSServiceImpl {
 
 
   /**
-   * Redirect the plan to the local MetaGroupMember so that it will be processed cluster-wide.
+   * Redirect the plan to the local Coordinator so that it will be processed cluster-wide.
    *
    * @param plan
    * @return
@@ -203,7 +207,7 @@ public class ClientServer extends TSServiceImpl {
       logger.warn("Illegal plan detected： {}", plan);
       return RpcUtils.getStatus(TSStatusCode.EXECUTE_STATEMENT_ERROR, e.getMessage());
     }
-    return metaGroupMember.executeNonQueryPlan(plan);
+    return coordinator.executeNonQueryPlan(plan);
   }
 
 
@@ -302,15 +306,13 @@ public class ClientServer extends TSServiceImpl {
           GenericHandler<Void> handler = new GenericHandler<>(queriedNode, new AtomicReference<>());
           try {
             if (ClusterDescriptor.getInstance().getConfig().isUseAsyncServer()) {
-              AsyncDataClient client = metaGroupMember
-                  .getClientProvider().getAsyncDataClient(queriedNode,
+              AsyncDataClient client = coordinator.getAsyncDataClient(queriedNode,
                       RaftServer.getReadOperationTimeoutMS());
-              client.endQuery(header, metaGroupMember.getThisNode(), queryId, handler);
+              client.endQuery(header, coordinator.getThisNode(), queryId, handler);
             } else {
-              SyncDataClient syncDataClient = metaGroupMember
-                  .getClientProvider().getSyncDataClient(queriedNode,
+              SyncDataClient syncDataClient = coordinator.getSyncDataClient(queriedNode,
                       RaftServer.getReadOperationTimeoutMS());
-              syncDataClient.endQuery(header, metaGroupMember.getThisNode(), queryId);
+              syncDataClient.endQuery(header, coordinator.getThisNode(), queryId);
             }
           } catch (IOException | TException e) {
             logger.error("Cannot end query {} in {}", queryId, queriedNode);

cluster/src/main/java/org/apache/iotdb/cluster/server/Timer.java
@@ -20,16 +20,18 @@
 
 package org.apache.iotdb.cluster.server;
 
+import org.apache.iotdb.cluster.config.ClusterDescriptor;
+import org.apache.iotdb.cluster.server.member.RaftMember;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
-import org.apache.iotdb.cluster.config.ClusterDescriptor;
-import org.apache.iotdb.cluster.server.member.RaftMember;
 
 public class Timer {
 
   public static final boolean ENABLE_INSTRUMENTING = true;
 
+  private static final String COORDINATOR = "Coordinator";
   private static final String META_GROUP_MEMBER = "Meta group member";
   private static final String DATA_GROUP_MEMBER = "Data group member";
   private static final String RAFT_MEMBER_SENDER = " Raft member(sender)";
@@ -43,9 +45,14 @@ public class Timer {
     // A dummy root for the convenience of prints
     ROOT(
         "ClassName", "BlockName", TIME_SCALE, true, null),
+    // coordinator
+    COORDINATOR_EXECUTE_NON_QUERY(
+      COORDINATOR, "execute non query", TIME_SCALE, true, ROOT),
+
     // meta group member
     META_GROUP_MEMBER_EXECUTE_NON_QUERY(
-        META_GROUP_MEMBER, "execute non query", TIME_SCALE, true, ROOT),
+        META_GROUP_MEMBER, "execute non query", TIME_SCALE, true,
+      COORDINATOR_EXECUTE_NON_QUERY),
     META_GROUP_MEMBER_EXECUTE_NON_QUERY_IN_LOCAL_GROUP(
         META_GROUP_MEMBER, "execute in local group", TIME_SCALE, true,
         META_GROUP_MEMBER_EXECUTE_NON_QUERY),

commit 84d7527c652323ce31f128810dc42ca13c64cda3

fix coordinator not init bug after restart (#2383)
* fix  bug caused by #2334 (coordinator not init bug after restart)

cluster/src/main/java/org/apache/iotdb/cluster/coordinator/Coordinator.java
@@ -48,7 +48,6 @@ import org.apache.iotdb.db.qp.physical.sys.CreateMultiTimeSeriesPlan;
 import org.apache.iotdb.db.qp.physical.sys.CreateTimeSeriesPlan;
 import org.apache.iotdb.db.qp.physical.sys.DeleteTimeSeriesPlan;
 import org.apache.iotdb.db.service.IoTDB;
-import org.apache.iotdb.db.utils.TestOnly;
 import org.apache.iotdb.rpc.RpcUtils;
 import org.apache.iotdb.rpc.TSStatusCode;
 import org.apache.iotdb.service.rpc.thrift.EndPoint;
@@ -88,13 +87,14 @@ public class Coordinator {
     this.thisNode = metaGroupMember.getThisNode();
   }
 
-  @TestOnly
   public Coordinator() {
 
   }
 
   public void setMetaGroupMember(MetaGroupMember metaGroupMember) {
     this.metaGroupMember = metaGroupMember;
+    this.name = metaGroupMember.getName();
+    this.thisNode = metaGroupMember.getThisNode();
   }
 
   public void setRouter(ClusterPlanRouter router) {

cluster/src/main/java/org/apache/iotdb/cluster/server/MetaClusterServer.java
@@ -81,9 +81,9 @@ public class MetaClusterServer extends RaftServer implements TSMetaService.Async
   public MetaClusterServer() throws QueryProcessException {
     super();
     metaHeartbeatServer = new MetaHeartbeatServer(thisNode, this);
-    member = new MetaGroupMember(protocolFactory, thisNode);
-    coordinator = new Coordinator(member);
-    member.setCoordinator(coordinator);
+    coordinator = new Coordinator();
+    member = new MetaGroupMember(protocolFactory, thisNode, coordinator);
+    coordinator.setMetaGroupMember(member);
     asyncService = new MetaAsyncService(member);
     syncService = new MetaSyncService(member);
     MetaPuller.getInstance().init(member);

cluster/src/main/java/org/apache/iotdb/cluster/server/member/MetaGroupMember.java
@@ -263,7 +263,7 @@ public class MetaGroupMember extends RaftMember {
   public MetaGroupMember() {
   }
 
-  public MetaGroupMember(TProtocolFactory factory, Node thisNode) throws QueryProcessException {
+  public MetaGroupMember(TProtocolFactory factory, Node thisNode, Coordinator coordinator) throws QueryProcessException {
     super("Meta", new AsyncClientPool(new AsyncMetaClient.FactoryAsync(factory)),
         new SyncClientPool(new SyncMetaClient.FactorySync(factory)),
         new AsyncClientPool(new AsyncMetaHeartbeatClient.FactoryAsync(factory), false),
@@ -291,6 +291,7 @@ public class MetaGroupMember extends RaftMember {
     startUpStatus = getNewStartUpStatus();
 
     // try loading the partition table if there was a previous cluster
+    this.coordinator = coordinator;
     loadPartitionTable();
   }
 

cluster/src/test/java/org/apache/iotdb/cluster/server/member/MetaGroupMemberTest.java
@@ -287,7 +287,7 @@ public class MetaGroupMemberTest extends MemberTest {
   }
 
   protected MetaGroupMember getMetaGroupMember(Node node) throws QueryProcessException {
-    MetaGroupMember metaGroupMember = new MetaGroupMember(new Factory(), node) {
+    MetaGroupMember metaGroupMember = new MetaGroupMember(new Factory(), node, new Coordinator()) {
 
       @Override
       public DataClusterServer getDataClusterServer() {
@@ -467,7 +467,6 @@ public class MetaGroupMemberTest extends MemberTest {
         return new TestAsyncDataClient(node, dataGroupMemberMap);
       }
     });
-    metaGroupMember.setCoordinator(new Coordinator());
     return metaGroupMember;
   }
 