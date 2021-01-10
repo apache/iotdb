4c5e1cb796354829553ea6502ff75c9ca05a6496
HouliangQi <neuyilan@163.com>
2021-01-10 11:58:56 +0800

    remove useless debug log

--- server/src/main/java/org/apache/iotdb/db/qp/executor/PlanExecutor.java
+++ server/src/main/java/org/apache/iotdb/db/qp/executor/PlanExecutor.java
@@ -1082,7 +1082,6 @@ public class PlanExecutor implements IPlanExecutor {
   @Override
   public void insertTablet(InsertMultiTabletPlan insertMultiTabletPlan)
       throws QueryProcessException {
-    System.out.println("HAHAHAHAHAH=" + insertMultiTabletPlan.toString());
     for (int i = 0; i < insertMultiTabletPlan.getInsertTabletPlanList().size(); i++) {
       if (insertMultiTabletPlan.getResults().containsKey(i)) {
         continue;
