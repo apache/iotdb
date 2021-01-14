8207c2510274f58b7e24a60c9a66590eb673f9a9
Alima777 <526213833@qq.com>
2021-01-14 20:41:37 +0800

    modify catelog

--- jdbc/src/main/java/org/apache/iotdb/jdbc/IoTDBStatement.java
+++ jdbc/src/main/java/org/apache/iotdb/jdbc/IoTDBStatement.java
@@ -227,7 +227,7 @@ public class IoTDBStatement implements Statement {
     isCancelled = false;
     TSExecuteStatementReq execReq = new TSExecuteStatementReq(sessionId, sql, stmtId);
     execReq.setFetchSize(fetchSize);
-    execReq.setTimeout(1);
+    execReq.setTimeout((long) queryTimeout * 1000);
     TSExecuteStatementResp execResp = client.executeStatement(execReq);
     try {
       RpcUtils.verifySuccess(execResp.getStatus());
