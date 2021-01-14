73174798eac33cc29949dd80b12491e0f14b38d5
wshao08 <kofshaow@163.com>
2021-01-14 17:14:40 +0800

    remove imports

--- server/src/main/java/org/apache/iotdb/db/engine/storagegroup/TsFileProcessor.java
+++ server/src/main/java/org/apache/iotdb/db/engine/storagegroup/TsFileProcessor.java
@@ -27,7 +27,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.ConcurrentLinkedDeque;
-import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.locks.ReadWriteLock;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
 import org.apache.iotdb.db.conf.IoTDBConfig;
