0fac0022c59eed9914aaa08c4123342eb1564475
151250176 <151250176@smail.nju.edu.cn>
2021-01-15 14:12:23 +0800

    fix issue

--- server/src/main/java/org/apache/iotdb/db/tools/virtualsg/DeviceMappingViewer.java
+++ server/src/main/java/org/apache/iotdb/db/tools/virtualsg/DeviceMappingViewer.java
@@ -16,7 +16,7 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-package org.apache.iotdb.db.tools.virtualSG;
+package org.apache.iotdb.db.tools.virtualsg;
 
 import java.util.Set;
 import org.apache.iotdb.db.conf.IoTDBDescriptor;
