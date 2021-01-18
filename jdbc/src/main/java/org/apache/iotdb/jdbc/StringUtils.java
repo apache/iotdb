bc96875cf4cdf004a1456bca6a73554d687b20f8
CloudWise-Lukemiao <luke.miao@yunzhihui.com>
2021-01-18 11:02:57 +0800

    add header

--- jdbc/src/main/java/org/apache/iotdb/jdbc/StringUtils.java
+++ jdbc/src/main/java/org/apache/iotdb/jdbc/StringUtils.java
@@ -22,7 +22,6 @@ import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.math.BigDecimal;
 
-
 public class StringUtils {
   
   private static byte[] allBytes = new byte[256];
