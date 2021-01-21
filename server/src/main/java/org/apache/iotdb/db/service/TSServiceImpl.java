5837269823978216eb80b7496e337faa27ea1207
Haimei Guo <68632589+haimeiguo@users.noreply.github.com>
2021-01-21 08:33:37 +0800

    remove duplicated codes for error tips for query FILL,AGGREGATION, GROUPBYTIME sql  (#2525)

--- server/src/main/java/org/apache/iotdb/db/service/TSServiceImpl.java
+++ server/src/main/java/org/apache/iotdb/db/service/TSServiceImpl.java
@@ -544,14 +544,10 @@ public class TSServiceImpl implements TSIService.Iface, ServerContext {
       fetchSize = fetchSize == 0 ? DEFAULT_FETCH_SIZE : fetchSize;
 
       if (plan instanceof QueryPlan && !((QueryPlan) plan).isAlignByTime()) {
-        if (plan.getOperatorType() == OperatorType.AGGREGATION) {
-          throw new QueryProcessException("Aggregation doesn't support disable align clause.");
-        }
-        if (plan.getOperatorType() == OperatorType.FILL) {
-          throw new QueryProcessException("Fill doesn't support disable align clause.");
-        }
-        if (plan.getOperatorType() == OperatorType.GROUPBYTIME) {
-          throw new QueryProcessException("Group by doesn't support disable align clause.");
+        OperatorType operatorType = plan.getOperatorType();
+        if (operatorType == OperatorType.AGGREGATION || operatorType == OperatorType.FILL
+            || operatorType == OperatorType.GROUPBYTIME) {
+          throw new QueryProcessException(operatorType.name() + " doesn't support disable align clause.");
         }
       }
       if (plan.getOperatorType() == OperatorType.AGGREGATION) {
