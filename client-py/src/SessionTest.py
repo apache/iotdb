6953b9e3f0593f1bfe1e414c8e669d12c1cd473a
SilverNarcissus <151250176@smail.nju.edu.cn>
2021-01-21 21:40:22 +0800

    [IOTDB-1077] Add python test and insert one device interface (#2539)

--- client-py/src/SessionTest.py
+++ client-py/src/SessionTest.py
@@ -17,10 +17,22 @@
 #
 
 # Uncomment the following line to use apache-iotdb module installed by pip3
-
 from iotdb.Session import Session
 from iotdb.utils.Tablet import Tablet
 from iotdb.utils.IoTDBConstants import *
+
+# whether the test has passed
+final_flag = True
+failed_count = 0
+
+def test_fail(message):
+  global failed_count
+  global final_flag
+  print("*********")
+  print(message)
+  print("*********")
+  final_flag = False
+  failed_count += 1
 
 # creating session connection.
 ip = "127.0.0.1"
@@ -30,13 +42,21 @@ password_ = 'root'
 session = Session(ip, port_, username_, password_, fetch_size=1024, zone_id='UTC+8')
 session.open(False)
 
+if not session.is_open():
+  print("can't open session")
+  exit(1)
+
 # set and delete storage groups
 session.set_storage_group("root.sg_test_01")
 session.set_storage_group("root.sg_test_02")
 session.set_storage_group("root.sg_test_03")
 session.set_storage_group("root.sg_test_04")
-session.delete_storage_group("root.sg_test_02")
-session.delete_storage_groups(["root.sg_test_03", "root.sg_test_04"])
+
+if session.delete_storage_group("root.sg_test_02") < 0:
+  test_fail("delete storage group failed")
+
+if session.delete_storage_groups(["root.sg_test_03", "root.sg_test_04"]) < 0:
+  test_fail("delete storage groups failed")
 
 # setting time series.
 session.create_time_series("root.sg_test_01.d_01.s_01", TSDataType.BOOLEAN, TSEncoding.PLAIN, Compressor.SNAPPY)
@@ -53,18 +73,25 @@ compressor_lst_ = [Compressor.SNAPPY for _ in range(len(data_type_lst_))]
 session.create_multi_time_series(ts_path_lst_, data_type_lst_, encoding_lst_, compressor_lst_)
 
 # delete time series
-session.delete_time_series(["root.sg_test_01.d_01.s_07", "root.sg_test_01.d_01.s_08", "root.sg_test_01.d_01.s_09"])
+if session.delete_time_series(["root.sg_test_01.d_01.s_07", "root.sg_test_01.d_01.s_08", "root.sg_test_01.d_01.s_09"]) < 0:
+  test_fail("delete time series failed")
 
 # checking time series
-print("s_07 expecting False, checking result: ", session.check_time_series_exists("root.sg_test_01.d_01.s_07"))
-print("s_03 expecting True, checking result: ", session.check_time_series_exists("root.sg_test_01.d_01.s_03"))
+# s_07 expecting False
+if session.check_time_series_exists("root.sg_test_01.d_01.s_07"):
+  test_fail("root.sg_test_01.d_01.s_07 shouldn't exist")
+
+# s_03 expecting True
+if not session.check_time_series_exists("root.sg_test_01.d_01.s_03"):
+  test_fail("root.sg_test_01.d_01.s_03 should exist")
 
 # insert one record into the database.
 measurements_ = ["s_01", "s_02", "s_03", "s_04", "s_05", "s_06"]
 values_ = [False, 10, 11, 1.1, 10011.1, "test_record"]
 data_types_ = [TSDataType.BOOLEAN, TSDataType.INT32, TSDataType.INT64,
                TSDataType.FLOAT, TSDataType.DOUBLE, TSDataType.TEXT]
-session.insert_record("root.sg_test_01.d_01", 1, measurements_, data_types_, values_)
+if session.insert_record("root.sg_test_01.d_01", 1, measurements_, data_types_, values_) < 0:
+  test_fail("insert record failed")
 
 # insert multiple records into database
 measurements_list_ = [["s_01", "s_02", "s_03", "s_04", "s_05", "s_06"],
@@ -73,7 +100,8 @@ values_list_ = [[False, 22, 33, 4.4, 55.1, "test_records01"],
                 [True, 77, 88, 1.25, 8.125, "test_records02"]]
 data_type_list_ = [data_types_, data_types_]
 device_ids_ = ["root.sg_test_01.d_01", "root.sg_test_01.d_01"]
-session.insert_records(device_ids_, [2, 3], measurements_list_, data_type_list_, values_list_)
+if session.insert_records(device_ids_, [2, 3], measurements_list_, data_type_list_, values_list_) < 0:
+  test_fail("insert records failed")
 
 # insert one tablet into the database.
 values_ = [[False, 10, 11, 1.1, 10011.1, "test01"],
@@ -82,12 +110,14 @@ values_ = [[False, 10, 11, 1.1, 10011.1, "test01"],
            [True, 0, 0, 0, 6.25, "test04"]]  # Non-ASCII text will cause error since bytes can only hold 0-128 nums.
 timestamps_ = [4, 5, 6, 7]
 tablet_ = Tablet("root.sg_test_01.d_01", measurements_, data_types_, values_, timestamps_)
-session.insert_tablet(tablet_)
+if session.insert_tablet(tablet_) < 0:
+  test_fail("insert tablet failed")
 
 # insert multiple tablets into database
 tablet_01 = Tablet("root.sg_test_01.d_01", measurements_, data_types_, values_, [8, 9, 10, 11])
 tablet_02 = Tablet("root.sg_test_01.d_01", measurements_, data_types_, values_, [12, 13, 14, 15])
-session.insert_tablets([tablet_01, tablet_02])
+if session.insert_tablets([tablet_01, tablet_02]) < 0:
+  test_fail("insert tablets failed")
 
 # insert records of one device
 time_list = [1, 2, 3]
@@ -97,19 +127,32 @@ data_types_list = [[TSDataType.BOOLEAN, TSDataType.INT32, TSDataType.INT64],
                    [TSDataType.BOOLEAN, TSDataType.INT32, TSDataType.INT64]]
 values_list = [[False, 22, 33], [True, 1, 23], [False, 15, 26]]
 
-session.insert_records_of_one_device("root.sg_test_01.d_01", time_list, measurements_list, data_types_list, values_list)
+if session.insert_records_of_one_device("root.sg_test_01.d_01", time_list, measurements_list, data_types_list, values_list) < 0:
+  test_fail("insert records of one device failed")
 
 # execute non-query sql statement
-session.execute_non_query_statement("insert into root.sg_test_01.d_01(timestamp, s_02) values(16, 188)")
+if session.execute_non_query_statement("insert into root.sg_test_01.d_01(timestamp, s_02) values(16, 188)") < 0:
+  test_fail("execute 'insert into root.sg_test_01.d_01(timestamp, s_02) values(16, 188)' failed")
 
 # execute sql query statement
 session_data_set = session.execute_query_statement("select * from root.sg_test_01.d_01")
 session_data_set.set_fetch_size(1024)
+expect_count = 16
+actual_count = 0
 while session_data_set.has_next():
-  print(session_data_set.next())
+  actual_count += 1
 session_data_set.close_operation_handle()
+
+if actual_count != expect_count:
+  test_fail("query count mismatch: expect count: "
+            + str(expect_count) + " actual count: " + str(actual_count))
 
 # close session connection.
 session.close()
 
-print("All executions done!!")
+if final_flag:
+  print("All executions done!!")
+else:
+  print("Some test failed, please have a check")
+  print("failed count: ", failed_count)
+  exit(1)
