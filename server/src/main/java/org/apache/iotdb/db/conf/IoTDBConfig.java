adbce4426f9e3f6f44bc10f82db1e8e132d8fa42
HTHou <hhaonan@outlook.com>
2021-01-26 17:57:43 +0800

    try to fix test

--- server/src/main/java/org/apache/iotdb/db/conf/IoTDBConfig.java
+++ server/src/main/java/org/apache/iotdb/db/conf/IoTDBConfig.java
@@ -50,7 +50,7 @@ public class IoTDBConfig {
   private static final String DEFAULT_MULTI_DIR_STRATEGY = "MaxDiskUsableSpaceFirstStrategy";
 
   // e.g., a31+/$%#&[]{}3e4
-  private static final String ID_MATCHER = "([a-zA-Z0-9/\"\s*,:@#$%&{}\\[\\]\\-+\\u2E80-\\u9FFF_]+)";
+  private static final String ID_MATCHER = "([a-zA-Z0-9/\"[ ],:@#$%&{}\\[\\]\\-+\\u2E80-\\u9FFF_]+)";
 
   private static final String STORAGE_GROUP_MATCHER = "([a-zA-Z0-9_.\\u2E80-\\u9FFF]+)";
 
