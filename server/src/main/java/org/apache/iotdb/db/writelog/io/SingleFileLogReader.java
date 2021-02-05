6133c2cbdd89f8888f3e49e0933fbba0a9152227
151250176 <151250176@smail.nju.edu.cn>
2021-02-05 11:29:19 +0800

    add wal log

--- server/src/main/java/org/apache/iotdb/db/writelog/io/SingleFileLogReader.java
+++ server/src/main/java/org/apache/iotdb/db/writelog/io/SingleFileLogReader.java
@@ -92,6 +92,7 @@ public class SingleFileLogReader implements ILogReader {
       fileCorrupted = fileCorrupted || batchLogReader.isFileCorrupted();
     } catch (Exception e) {
       logger.error("Cannot read more PhysicalPlans from {} because", filepath, e);
+      logger.error("Successfully read index is {}", idx);
       fileCorrupted = true;
       return false;
     }
@@ -122,6 +123,7 @@ public class SingleFileLogReader implements ILogReader {
   public void open(File logFile) throws FileNotFoundException {
     close();
     logStream = new DataInputStream(new BufferedInputStream(new FileInputStream(logFile)));
+    logger.info("WAL file: {} size is {}", logFile.getName(), logFile.length());
     this.filepath = logFile.getPath();
     idx = 0;
   }
