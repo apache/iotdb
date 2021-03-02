f806ccc6522cd78185644b50e512970d7608c24e
SteveYurongSu <steveyurongsu@outlook.com>
2021-03-02 10:50:58 +0800

    trigger conf

--- server/src/main/java/org/apache/iotdb/db/conf/IoTDBConstant.java
+++ server/src/main/java/org/apache/iotdb/db/conf/IoTDBConstant.java
@@ -126,6 +126,7 @@ public class IoTDBConstant {
   public static final String TRACING_LOG = "tracing.txt";
   public static final String EXT_FOLDER_NAME = "ext";
   public static final String UDF_FOLDER_NAME = "udf";
+  public static final String TRIGGER_FOLDER_NAME = "trigger";
 
   // mqtt
   public static final String ENABLE_MQTT = "enable_mqtt_service";
