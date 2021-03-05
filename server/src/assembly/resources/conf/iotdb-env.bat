4de1f8bf7271eaaa6a3f5a0b105b56d29d6e0562
Haonan <hhaonan@outlook.com>
2021-03-05 18:12:06 +0800

    change the default max JVM heap size from 8G to 64G (#2771)

--- server/src/assembly/resources/conf/iotdb-env.bat
+++ server/src/assembly/resources/conf/iotdb-env.bat
@@ -68,7 +68,7 @@ set /a half_=%system_memory_in_mb%/2
 set /a quarter_=%half_%/2
 
 if ["%half_%"] GTR ["1024"] set half_=1024
-if ["%quarter_%"] GTR ["8192"] set quarter_=8192
+if ["%quarter_%"] GTR ["65536"] set quarter_=65536
 
 if ["%half_%"] GTR ["quarter_"] (
 	set max_heap_size_in_mb=%half_%
