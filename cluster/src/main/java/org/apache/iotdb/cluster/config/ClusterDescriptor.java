19ad435bc93c1ae8b71be1e9454bc4dad31018c6
wangchao316 <66939405+wangchao316@users.noreply.github.com>
2021-03-11 09:01:06 +0800

    [IOTDB-1204] set parameter in iotdb-cluster.properties (#2797)

--- cluster/src/main/java/org/apache/iotdb/cluster/config/ClusterDescriptor.java
+++ cluster/src/main/java/org/apache/iotdb/cluster/config/ClusterDescriptor.java
@@ -282,6 +282,17 @@ public class ClusterDescriptor {
         Long.parseLong(
             properties.getProperty("max_read_log_lag", String.valueOf(config.getMaxReadLogLag()))));
 
+    config.setMaxClientPerNodePerMember(
+        Integer.parseInt(
+            properties.getProperty(
+                "max_client_pernode_permember_number",
+                String.valueOf(config.getMaxClientPerNodePerMember()))));
+
+    config.setWaitClientTimeoutMS(
+        Long.parseLong(
+            properties.getProperty(
+                "wait_client_timeout_ms", String.valueOf(config.getWaitClientTimeoutMS()))));
+
     String consistencyLevel = properties.getProperty("consistency_level");
     if (consistencyLevel != null) {
       config.setConsistencyLevel(ConsistencyLevel.getConsistencyLevel(consistencyLevel));
