36ef81d3930f3ce468264d40759035d4560cff2a
HTHou <hhaonan@outlook.com>
2021-03-12 20:59:27 +0800

    implament flush interface

--- server/src/main/java/org/apache/iotdb/db/engine/flush/MemTableFlushTask.java
+++ server/src/main/java/org/apache/iotdb/db/engine/flush/MemTableFlushTask.java
@@ -26,7 +26,9 @@ import org.apache.iotdb.db.engine.memtable.IWritableMemChunk;
 import org.apache.iotdb.db.exception.runtime.FlushRunTimeException;
 import org.apache.iotdb.db.rescon.SystemInfo;
 import org.apache.iotdb.db.utils.datastructure.TVList;
+import org.apache.iotdb.db.utils.datastructure.VectorTVList;
 import org.apache.iotdb.tsfile.file.metadata.enums.TSDataType;
+import org.apache.iotdb.tsfile.utils.Binary;
 import org.apache.iotdb.tsfile.utils.Pair;
 import org.apache.iotdb.tsfile.write.chunk.ChunkWriterImpl;
 import org.apache.iotdb.tsfile.write.chunk.IChunkWriter;
@@ -36,7 +38,10 @@ import org.apache.iotdb.tsfile.write.writer.RestorableTsFileIOWriter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import static org.apache.iotdb.db.rescon.PrimitiveArrayManager.ARRAY_SIZE;
+
 import java.io.IOException;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
@@ -158,6 +163,12 @@ public class MemTableFlushTask {
       new Runnable() {
         private void writeOneSeries(
             TVList tvPairs, IChunkWriter seriesWriterImpl, TSDataType dataType) {
+
+          if (dataType == TSDataType.VECTOR) {
+            writeOneVectorSeries(tvPairs, seriesWriterImpl);
+            return;
+          }
+
           for (int i = 0; i < tvPairs.size(); i++) {
             long time = tvPairs.getTime(i);
 
@@ -190,16 +201,54 @@ public class MemTableFlushTask {
               case TEXT:
                 seriesWriterImpl.write(time, tvPairs.getBinary(i), false);
                 break;
-              case VECTOR:
-                // TODO:
-                //                for ( : tvPairs.getVector(i)) {
-                //                  seriesWriterImpl.write(time, tvPairs.getVector(i)[], get);
-                //                }
-                break;
               default:
                 LOGGER.error(
                     "Storage group {} does not support data type: {}", storageGroup, dataType);
                 break;
+            }
+          }
+        }
+
+        private void writeOneVectorSeries(TVList tvPairs, IChunkWriter seriesWriterImpl) {
+          VectorTVList tvList = (VectorTVList) tvPairs;
+          List<TSDataType> dataTypes = tvList.getTsDataTypes();
+          List<List<Object>> values = tvList.getValues();
+          for (int i = 0; i < dataTypes.size(); i++) {
+            List<Object> columnValues = values.get(i);
+            for (int j = 0; j < tvList.size(); j++) {
+              long time = tvList.getTime(j);
+              // skip duplicated data
+              if ((i + 1 < tvList.size() && (time == tvPairs.getTime(i + 1)))) {
+                continue;
+              }
+              int valueIndex = tvList.getValueIndex(j);
+              if (valueIndex >= tvList.size()) {
+                throw new ArrayIndexOutOfBoundsException(valueIndex);
+              }
+              int arrayIndex = valueIndex / ARRAY_SIZE;
+              int elementIndex = valueIndex % ARRAY_SIZE;
+              switch (dataTypes.get(i)) {
+                case TEXT:
+                  seriesWriterImpl.write(time, ((Binary[]) columnValues.get(arrayIndex))[elementIndex], false);
+                  break;
+                case FLOAT:
+                  seriesWriterImpl.write(time, ((float[]) columnValues.get(arrayIndex))[elementIndex], false);
+                  break;
+                case INT32:
+                  seriesWriterImpl.write(time, ((int[]) columnValues.get(arrayIndex))[elementIndex], false);
+                  break;
+                case INT64:
+                  seriesWriterImpl.write(time, ((long[]) columnValues.get(arrayIndex))[elementIndex], false);
+                  break;
+                case DOUBLE:
+                  seriesWriterImpl.write(time, ((double[]) columnValues.get(arrayIndex))[elementIndex], false);
+                  break;
+                case BOOLEAN:
+                  seriesWriterImpl.write(time, ((boolean[]) columnValues.get(arrayIndex))[elementIndex], false);
+                  break;
+                default:
+                  break;
+              }
             }
           }
         }
