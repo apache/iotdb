9412ba8165b2e10a407e9d660f91f471566cc8f9
THUMarkLau <liuxuxin@outlook.com>
2021-03-13 06:46:37 +0800

    test

--- server/src/main/java/org/apache/iotdb/db/engine/measurementorderoptimizer/MultiReplicaOrderOptimizer.java
+++ server/src/main/java/org/apache/iotdb/db/engine/measurementorderoptimizer/MultiReplicaOrderOptimizer.java
@@ -191,7 +191,7 @@ public class MultiReplicaOrderOptimizer {
 		int k = 0;
 		CostRecorder costRecorder = new CostRecorder();
 		long startTime = System.currentTimeMillis();
-		for (; k < maxIter && System.currentTimeMillis() - optimizeStartTime < 15l * 60l * 1000l; ++k) {
+		for (; k < maxIter && System.currentTimeMillis() - optimizeStartTime < 10l * 60l * 1000l; ++k) {
 			temperature = temperature * COOLING_RATE;
 			int selectedReplica = r.nextInt(replicaNum);
 			int operation = 1;
