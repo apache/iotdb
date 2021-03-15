94b5c8037e90a0ca1c925bc0307a8ce1df965aec
Steve Yurong Su <steveyurongsu@outlook.com>
2021-03-15 13:58:16 +0800

    support 'explain' clause (#2840)

--- server/src/main/java/org/apache/iotdb/db/qp/logical/Operator.java
+++ server/src/main/java/org/apache/iotdb/db/qp/logical/Operator.java
@@ -20,21 +20,22 @@ package org.apache.iotdb.db.qp.logical;
 
 import org.apache.iotdb.db.qp.constant.SQLConstant;
 
-/**
- * This class is a superclass of all operator.
- */
+/** This class is a superclass of all operator. */
 public abstract class Operator {
 
   // operator type in int format
   protected int tokenIntType;
   // operator type in String format
   protected String tokenName;
+  // flag of "explain"
+  protected boolean isDebug;
 
   protected OperatorType operatorType = OperatorType.NULL;
 
   public Operator(int tokenIntType) {
     this.tokenIntType = tokenIntType;
     this.tokenName = SQLConstant.tokenNames.get(tokenIntType);
+    this.isDebug = false;
   }
 
   public OperatorType getType() {
@@ -57,26 +58,92 @@ public abstract class Operator {
     this.operatorType = operatorType;
   }
 
+  public boolean isDebug() {
+    return isDebug;
+  }
+
+  public void setDebug(boolean debug) {
+    isDebug = debug;
+  }
+
   @Override
   public String toString() {
     return tokenName;
   }
 
-  /**
-   * If you want to add new OperatorType, you must add it in the last.
-   */
+  /** If you want to add new OperatorType, you must add it in the last. */
   public enum OperatorType {
-    SFW, JOIN, UNION, FILTER, GROUPBYTIME, ORDERBY, LIMIT, SELECT, SEQTABLESCAN, HASHTABLESCAN,
-    MERGEJOIN, FILEREAD, NULL, TABLESCAN, UPDATE, INSERT, BATCHINSERT, DELETE, BASIC_FUNC, IN, QUERY, MERGEQUERY,
-    AGGREGATION, AUTHOR, FROM, FUNC, LOADDATA, METADATA, INDEX, INDEXQUERY, FILL,
-    SET_STORAGE_GROUP, CREATE_TIMESERIES, DELETE_TIMESERIES, CREATE_USER, DELETE_USER, MODIFY_PASSWORD,
-    GRANT_USER_PRIVILEGE, REVOKE_USER_PRIVILEGE, GRANT_USER_ROLE, REVOKE_USER_ROLE, CREATE_ROLE,
-    DELETE_ROLE, GRANT_ROLE_PRIVILEGE, REVOKE_ROLE_PRIVILEGE, LIST_USER, LIST_ROLE,
-    LIST_USER_PRIVILEGE, LIST_ROLE_PRIVILEGE, LIST_USER_ROLES, LIST_ROLE_USERS,
-    GRANT_WATERMARK_EMBEDDING, REVOKE_WATERMARK_EMBEDDING,
-    TTL, DELETE_STORAGE_GROUP, LOAD_CONFIGURATION, SHOW, LOAD_FILES, REMOVE_FILE, MOVE_FILE, LAST, GROUP_BY_FILL,
-    ALTER_TIMESERIES, FLUSH, MERGE, FULL_MERGE, CLEAR_CACHE,
-    SHOW_MERGE_STATUS, CREATE_SCHEMA_SNAPSHOT, TRACING, DELETE_PARTITION,
+    SFW,
+    JOIN,
+    UNION,
+    FILTER,
+    GROUPBYTIME,
+    ORDERBY,
+    LIMIT,
+    SELECT,
+    SEQTABLESCAN,
+    HASHTABLESCAN,
+    MERGEJOIN,
+    FILEREAD,
+    NULL,
+    TABLESCAN,
+    UPDATE,
+    INSERT,
+    BATCHINSERT,
+    DELETE,
+    BASIC_FUNC,
+    IN,
+    QUERY,
+    MERGEQUERY,
+    AGGREGATION,
+    AUTHOR,
+    FROM,
+    FUNC,
+    LOADDATA,
+    METADATA,
+    INDEX,
+    INDEXQUERY,
+    FILL,
+    SET_STORAGE_GROUP,
+    CREATE_TIMESERIES,
+    DELETE_TIMESERIES,
+    CREATE_USER,
+    DELETE_USER,
+    MODIFY_PASSWORD,
+    GRANT_USER_PRIVILEGE,
+    REVOKE_USER_PRIVILEGE,
+    GRANT_USER_ROLE,
+    REVOKE_USER_ROLE,
+    CREATE_ROLE,
+    DELETE_ROLE,
+    GRANT_ROLE_PRIVILEGE,
+    REVOKE_ROLE_PRIVILEGE,
+    LIST_USER,
+    LIST_ROLE,
+    LIST_USER_PRIVILEGE,
+    LIST_ROLE_PRIVILEGE,
+    LIST_USER_ROLES,
+    LIST_ROLE_USERS,
+    GRANT_WATERMARK_EMBEDDING,
+    REVOKE_WATERMARK_EMBEDDING,
+    TTL,
+    DELETE_STORAGE_GROUP,
+    LOAD_CONFIGURATION,
+    SHOW,
+    LOAD_FILES,
+    REMOVE_FILE,
+    MOVE_FILE,
+    LAST,
+    GROUP_BY_FILL,
+    ALTER_TIMESERIES,
+    FLUSH,
+    MERGE,
+    FULL_MERGE,
+    CLEAR_CACHE,
+    SHOW_MERGE_STATUS,
+    CREATE_SCHEMA_SNAPSHOT,
+    TRACING,
+    DELETE_PARTITION,
     CREATE_MULTI_TIMESERIES,
     BATCH_INSERT_ONE_DEVICE,
   }
