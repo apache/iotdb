ce93af3e299b032192ccd0f65d375905780d8fb6
HTHou <hhaonan@outlook.com>
2021-03-18 13:52:00 +0800

    spotless apply

--- server/src/main/java/org/apache/iotdb/db/engine/memtable/AbstractMemTable.java
+++ server/src/main/java/org/apache/iotdb/db/engine/memtable/AbstractMemTable.java
@@ -43,7 +43,6 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
-import java.util.stream.Collectors;
 
 public abstract class AbstractMemTable implements IMemTable {
 
