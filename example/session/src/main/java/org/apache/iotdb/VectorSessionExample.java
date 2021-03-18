f566f7c1d6ee270cec6bbc4b1cdbae9e46a6f6c1
151250176 <151250176@smail.nju.edu.cn>
2021-03-18 12:17:51 +0800

    add select Test

--- example/session/src/main/java/org/apache/iotdb/VectorSessionExample.java
+++ example/session/src/main/java/org/apache/iotdb/VectorSessionExample.java
@@ -24,6 +24,7 @@ import java.util.Random;
 import org.apache.iotdb.rpc.IoTDBConnectionException;
 import org.apache.iotdb.rpc.StatementExecutionException;
 import org.apache.iotdb.session.Session;
+import org.apache.iotdb.session.SessionDataSet;
 import org.apache.iotdb.tsfile.file.metadata.enums.CompressionType;
 import org.apache.iotdb.tsfile.file.metadata.enums.TSDataType;
 import org.apache.iotdb.tsfile.file.metadata.enums.TSEncoding;
@@ -46,7 +47,26 @@ public class VectorSessionExample {
 
     createTemplate();
     insertTabletWithAlignedTimeseries();
+    selectTest();
     session.close();
+  }
+
+  private static void selectTest() throws StatementExecutionException, IoTDBConnectionException {
+    SessionDataSet dataSet = session.executeQueryStatement("select s1 from root.sg1.d1");
+    System.out.println(dataSet.getColumnNames());
+    while (dataSet.hasNext()) {
+      System.out.println(dataSet.next());
+    }
+
+    dataSet.closeOperationHandle();
+
+    dataSet = session.executeQueryStatement("select * from root.sg1.d1");
+    System.out.println(dataSet.getColumnNames());
+    while (dataSet.hasNext()) {
+      System.out.println(dataSet.next());
+    }
+
+    dataSet.closeOperationHandle();
   }
 
   // be sure template is coordinate with tablet
