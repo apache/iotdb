599826920096e3c39635e57a7f04a5a41fc6f732
yuqi1129 <yuqi4733@gmail.com>
2021-03-24 19:20:18 +0800

    Fix Run IoTDB main meets 'Wrong max_degree_of_index_node, please set as'

--- tsfile/src/main/java/org/apache/iotdb/tsfile/common/conf/TSFileConfig.java
+++ tsfile/src/main/java/org/apache/iotdb/tsfile/common/conf/TSFileConfig.java
@@ -75,7 +75,7 @@ public class TSFileConfig implements Serializable {
   /** The maximum number of data points in a page, default value is 1024 * 1024. */
   private int maxNumberOfPointsInPage = 1024 * 1024;
   /** The maximum degree of a metadataIndex node, default value is 1024 */
-  private int maxDegreeOfIndexNode = 256;
+  private int maxDegreeOfIndexNode = 1024;
   /** Data type for input timestamp, TsFile supports INT32 or INT64. */
   private String timeSeriesDataType = "INT64";
   /** Max length limitation of input string. */
