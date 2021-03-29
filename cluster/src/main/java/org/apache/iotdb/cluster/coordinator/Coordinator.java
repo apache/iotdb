191fffe84a0fb8e3747fb0c466bf118c509e8e38
LebronAl <TXYPotato@gmail.com>
2021-03-29 17:34:54 +0800

    do not use raft when replicatedNum == 1

--- cluster/src/main/java/org/apache/iotdb/cluster/coordinator/Coordinator.java
+++ cluster/src/main/java/org/apache/iotdb/cluster/coordinator/Coordinator.java
@@ -326,9 +326,6 @@ public class Coordinator {
         status = forwardToMultipleGroup(planGroupMap);
       }
     }
-    if (status == null) {
-      return null;
-    }
     if (plan instanceof InsertPlan
         && status.getCode() == TSStatusCode.TIMESERIES_NOT_EXIST.getStatusCode()
         && ClusterDescriptor.getInstance().getConfig().isEnableAutoCreateSchema()) {
@@ -368,22 +365,18 @@ public class Coordinator {
   private TSStatus forwardToSingleGroup(Map.Entry<PhysicalPlan, PartitionGroup> entry) {
     TSStatus result;
     if (entry.getValue().contains(thisNode)) {
-      if (entry.getKey() instanceof InsertPlan) {
-        return null;
-      } else {
-        // the query should be handled by a group the local node is in, handle it with in the group
-        long startTime =
-            Timer.Statistic.META_GROUP_MEMBER_EXECUTE_NON_QUERY_IN_LOCAL_GROUP
-                .getOperationStartTime();
-        logger.debug(
-            "Execute {} in a local group of {}", entry.getKey(), entry.getValue().getHeader());
-        result =
-            metaGroupMember
-                .getLocalDataMember(entry.getValue().getHeader())
-                .executeNonQueryPlan(entry.getKey());
-        Timer.Statistic.META_GROUP_MEMBER_EXECUTE_NON_QUERY_IN_LOCAL_GROUP
-            .calOperationCostTimeFromStart(startTime);
-      }
+      // the query should be handled by a group the local node is in, handle it with in the group
+      long startTime =
+          Timer.Statistic.META_GROUP_MEMBER_EXECUTE_NON_QUERY_IN_LOCAL_GROUP
+              .getOperationStartTime();
+      logger.debug(
+          "Execute {} in a local group of {}", entry.getKey(), entry.getValue().getHeader());
+      result =
+          metaGroupMember
+              .getLocalDataMember(entry.getValue().getHeader())
+              .executeNonQueryPlan(entry.getKey());
+      Timer.Statistic.META_GROUP_MEMBER_EXECUTE_NON_QUERY_IN_LOCAL_GROUP
+          .calOperationCostTimeFromStart(startTime);
     } else {
       // forward the query to the group that should handle it
       long startTime =
