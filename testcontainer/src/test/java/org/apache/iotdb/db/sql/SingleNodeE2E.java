bdf3150bd5824d36fe7cc6076928045928bda97e
xiangdong huang <sainthxd@gmail.com>
2021-04-07 01:35:58 +0800

    try test container

---
+++ testcontainer/src/test/java/org/apache/iotdb/db/sql/SingleNodeE2E.java
@@ -0,0 +1,85 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.iotdb.db.sql;
+
+import org.apache.iotdb.jdbc.Config;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.testcontainers.containers.BindMode;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.wait.strategy.Wait;
+import org.testcontainers.images.PullPolicy;
+import org.testcontainers.utility.DockerImageName;
+
+import java.io.File;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class SingleNodeE2E {
+  private Statement statement;
+  private Connection connection;
+
+  @Rule
+  public GenericContainer dslContainer =
+      new GenericContainer(DockerImageName.parse("apache/iotdb:maven-development"))
+          .withImagePullPolicy(PullPolicy.defaultPolicy())
+          // mount another properties for changing parameters, e.g., open 5555 port (sync module)
+          .withFileSystemBind(
+              new File("src/test/resources/iotdb-engine.properties").getAbsolutePath(),
+              "/iotdb/conf/iotdb-engine.properties",
+              BindMode.READ_ONLY)
+          .withExposedPorts(6667)
+          .waitingFor(Wait.forListeningPort());
+
+  int rpcPort = 6667;
+  int syncPort = 5555;
+
+  @Before
+  public void setUp() throws Exception {
+    rpcPort = dslContainer.getMappedPort(6667);
+
+    syncPort = dslContainer.getMappedPort(5555);
+    Class.forName(Config.JDBC_DRIVER_NAME);
+    connection = DriverManager.getConnection("jdbc:iotdb://127.0.0.1:" + rpcPort, "root", "root");
+    statement = connection.createStatement();
+  }
+
+  @After
+  public void tearDown() throws Exception {
+    statement.close();
+    connection.close();
+  }
+
+  @Test
+  public void testSimplePutAndGet() throws SQLException {
+    String[] timeSeriesArray = {"root.sg1.aa.bb", "root.sg1.aa.bb.cc", "root.sg1.aa"};
+
+    for (String timeSeries : timeSeriesArray) {
+      statement.execute(
+          String.format(
+              "create timeseries %s with datatype=INT64, encoding=PLAIN, compression=SNAPPY",
+              timeSeries));
+    }
+  }
+}
