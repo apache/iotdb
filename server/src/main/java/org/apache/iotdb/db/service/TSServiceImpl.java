15ad109084e564134b0e67f8f281af888ec2d542
MarkLau <liuxuxin@outlook.com>
2021-04-14 17:22:09 +0800

    test

--- server/src/main/java/org/apache/iotdb/db/service/TSServiceImpl.java
+++ server/src/main/java/org/apache/iotdb/db/service/TSServiceImpl.java
@@ -2008,21 +2008,6 @@ public class TSServiceImpl implements TSIService.Iface, ServerContext {
 
   @Override
   public void myTest() throws TException {
-    DiskEvaluator diskEvaluator = DiskEvaluator.getInstance();
-    String path = "/data/tmp";
-    File file = new File(path);
-    try {
-      if (!file.exists()) {
-        file = diskEvaluator.generateFile(8l * 1024l * 1024l * 1024l, path, 2);
-      }
-      double[] seekTime = new double[100];
-      diskEvaluator.performMultiSegmentSeek(512, 100, 50, file, 100, seekTime);
-      //      for (int i = 0; i < 100; ++i) {
-      //        System.out.println(
-      //            String.format("Size:%d bytes, seek time: %.2f ms", 512 * (i + 1), seekTime[i]));
-      //      }
-    } catch (Exception e) {
-      e.printStackTrace();
-    }
+    DiskEvaluator.main(null);
   }
 }
