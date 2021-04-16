c3fa222aa011e4cc5441670c4baf4fa7036de774
CloudWise-Lukemiao <luke.miao@yunzhihui.com>
2021-04-16 10:20:11 +0800

    It optimizes the need to execute clearbatch when using the executebatch method

--- jdbc/src/main/java/org/apache/iotdb/jdbc/IoTDBStatement.java
+++ jdbc/src/main/java/org/apache/iotdb/jdbc/IoTDBStatement.java
@@ -313,6 +313,8 @@ public class IoTDBStatement implements Statement {
         throw new SQLException(
             "Fail to reconnect to server when executing batch sqls. please check server status", e);
       }
+    }finally {
+        clearBatch();
     }
   }
 
