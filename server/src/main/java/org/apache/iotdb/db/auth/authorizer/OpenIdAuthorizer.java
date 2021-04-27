a2fe7ebc67263bd8a9573ad6e4384b0064fa993f
282583553 <282583553@qq.com>
2021-04-27 13:39:17 +0800

    Fix that when openid_url is http://xxxxx, iotdb starts to report an error

--- server/src/main/java/org/apache/iotdb/db/auth/authorizer/OpenIdAuthorizer.java
+++ server/src/main/java/org/apache/iotdb/db/auth/authorizer/OpenIdAuthorizer.java
@@ -99,7 +99,7 @@ public class OpenIdAuthorizer extends BasicAuthorizer {
     logger.debug("Using Provider Metadata: {}", providerMetadata);
 
     try {
-      URL url = new URI(providerMetadata.getJWKSetURI().toString().replace("http", "http")).toURL();
+      URL url = new URI(providerMetadata.getJWKSetURI().toString()).toURL();
       logger.debug("Using url {}", url);
       return getProviderRSAJWK(url.openStream());
     } catch (IOException e) {
@@ -188,7 +188,7 @@ public class OpenIdAuthorizer extends BasicAuthorizer {
         .setAllowedClockSkewSeconds(Long.MAX_VALUE / 1000)
         // .setSigningKey(DatatypeConverter.parseBase64Binary(secret))
         .setSigningKey(providerKey)
-        .parseClaimsJws(token.replaceAll("access_token:", ""))
+        .parseClaimsJws(token)
         .getBody();
   }
 
