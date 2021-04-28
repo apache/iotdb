apache/iotdb

commit 7148f155933ea97410f30d7e67ebf6e72acaf953

[IOTDB-1132] username, password and rolename shouldn't contains spaces (#3069)

server/src/main/java/org/apache/iotdb/db/utils/AuthUtils.java
@@ -57,6 +57,9 @@ public class AuthUtils {
       throw new AuthException(
           "Password's size must be greater than or equal to " + MIN_PASSWORD_LENGTH);
     }
+    if (password.contains(" ")) {
+      throw new AuthException("Password cannot contain spaces");
+    }
   }
 
   /**
@@ -70,6 +73,9 @@ public class AuthUtils {
       throw new AuthException(
           "Username's size must be greater than or equal to " + MIN_USERNAME_LENGTH);
     }
+    if (username.contains(" ")) {
+      throw new AuthException("Username cannot contain spaces");
+    }
   }
 
   /**
@@ -82,6 +88,9 @@ public class AuthUtils {
     if (rolename.length() < MIN_ROLENAME_LENGTH) {
       throw new AuthException(
           "Role name's size must be greater than or equal to " + MIN_ROLENAME_LENGTH);
+    }
+    if (rolename.contains(" ")) {
+      throw new AuthException("Rolename cannot contain spaces");
     }
   }
 

server/src/test/java/org/apache/iotdb/db/auth/role/LocalFileRoleManagerTest.java
@@ -87,6 +87,12 @@ public class LocalFileRoleManagerTest {
       caught = true;
     }
     assertTrue(caught);
+    try {
+      manager.createRole("rolename ");
+    } catch (AuthException e) {
+      caught = true;
+    }
+    assertTrue(caught);
 
     // delete
     assertFalse(manager.deleteRole("not a role"));

server/src/test/java/org/apache/iotdb/db/auth/user/LocalFileUserManagerTest.java
@@ -59,6 +59,22 @@ public class LocalFileUserManagerTest {
   }
 
   @Test
+  public void testIllegalInput() throws AuthException {
+    // Password contains space
+    try {
+      manager.createUser("username1", "password_ ");
+    } catch (AuthException e) {
+      assertTrue(e.getMessage().contains("cannot contain spaces"));
+    }
+    // Username contains space
+    try {
+      assertFalse(manager.createUser("username 2", "password_"));
+    } catch (AuthException e) {
+      assertTrue(e.getMessage().contains("cannot contain spaces"));
+    }
+  }
+
+  @Test
   public void test() throws AuthException {
     User[] users = new User[5];
     for (int i = 0; i < users.length; i++) {

commit 73dedd14112cff3f707ec1cccc12500e9031b7cb

Fix AuthorizationIT failed caused by #3069

server/src/test/java/org/apache/iotdb/db/integration/auth/IoTDBAuthorizationIT.java
@@ -199,6 +199,31 @@ public class IoTDBAuthorizationIT {
   }
 
   @Test
+  public void illegalPasswordTest() throws ClassNotFoundException, SQLException {
+    Class.forName(Config.JDBC_DRIVER_NAME);
+    boolean caught = false;
+    try (Connection adminCon =
+            DriverManager.getConnection(
+                Config.IOTDB_URL_PREFIX + "127.0.0.1:6667/", "root", "root");
+        Statement adminStmt = adminCon.createStatement()) {
+      adminStmt.execute("CREATE USER tempuser 'temppw '");
+    } catch (SQLException e) {
+      caught = true;
+    }
+    assertTrue(caught);
+
+    try (Connection adminCon =
+            DriverManager.getConnection(
+                Config.IOTDB_URL_PREFIX + "127.0.0.1:6667/", "root", "root");
+        Statement adminStmt = adminCon.createStatement()) {
+      adminStmt.execute("CREATE USER tempuser 'te'");
+    } catch (SQLException e) {
+      caught = true;
+    }
+    assertTrue(caught);
+  }
+
+  @Test
   public void updatePasswordTest() throws ClassNotFoundException, SQLException {
     Class.forName(Config.JDBC_DRIVER_NAME);
     try (Connection adminCon =
@@ -680,7 +705,7 @@ public class IoTDBAuthorizationIT {
         validateResultSet(resultSet, ans);
 
         for (int i = 0; i < 10; i++) {
-          adminStmt.execute("CREATE USER user" + i + " 'password " + i + "'");
+          adminStmt.execute("CREATE USER user" + i + " 'password" + i + "'");
         }
         resultSet = adminStmt.executeQuery("LIST USER");
         ans =
@@ -1005,7 +1030,7 @@ public class IoTDBAuthorizationIT {
     Statement adminStmt = adminCon.createStatement();
 
     for (int i = 0; i < 10; i++) {
-      adminStmt.execute("CREATE USER user" + i + " 'password " + i + "'");
+      adminStmt.execute("CREATE USER user" + i + " 'password" + i + "'");
     }
 
     adminStmt.execute("CREATE USER tempuser 'temppw'");