63595d3814bb4716712c7a73d00e11ebc11cd165
HTHou <hhaonan@outlook.com>
2021-05-13 14:15:12 +0800

    spotless

--- jdbc/src/main/java/org/apache/iotdb/jdbc/IoTDBStatement.java
+++ jdbc/src/main/java/org/apache/iotdb/jdbc/IoTDBStatement.java
@@ -19,18 +19,6 @@
 
 package org.apache.iotdb.jdbc;
 
-import java.nio.ByteBuffer;
-import java.sql.BatchUpdateException;
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.SQLWarning;
-import java.sql.Statement;
-import java.time.ZoneId;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
-import java.util.stream.Collectors;
 import org.apache.iotdb.rpc.RpcUtils;
 import org.apache.iotdb.rpc.StatementExecutionException;
 import org.apache.iotdb.rpc.TSStatusCode;
@@ -44,7 +32,21 @@ import org.apache.iotdb.service.rpc.thrift.TSQueryDataSet;
 import org.apache.iotdb.service.rpc.thrift.TSQueryNonAlignDataSet;
 import org.apache.iotdb.service.rpc.thrift.TSStatus;
 import org.apache.iotdb.tsfile.utils.ReadWriteIOUtils;
+
 import org.apache.thrift.TException;
+
+import java.nio.ByteBuffer;
+import java.sql.BatchUpdateException;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.SQLWarning;
+import java.sql.Statement;
+import java.time.ZoneId;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 public class IoTDBStatement implements Statement {
 
@@ -64,28 +66,20 @@ public class IoTDBStatement implements Statement {
   private List<String> batchSQLList;
   private static final String NOT_SUPPORT_EXECUTE = "Not support execute";
   private static final String NOT_SUPPORT_EXECUTE_UPDATE = "Not support executeUpdate";
-  /**
-   * Keep state so we can fail certain calls made after close().
-   */
+  /** Keep state so we can fail certain calls made after close(). */
   private boolean isClosed = false;
 
-  /**
-   * Keep state so we can fail certain calls made after cancel().
-   */
+  /** Keep state so we can fail certain calls made after cancel(). */
   private boolean isCancelled = false;
 
-  /**
-   * Add SQLWarnings to the warningChain if needed.
-   */
+  /** Add SQLWarnings to the warningChain if needed. */
   private SQLWarning warningChain = null;
 
   private long sessionId;
   private long stmtId = -1;
   private long queryId = -1;
 
-  /**
-   * Constructor of IoTDBStatement.
-   */
+  /** Constructor of IoTDBStatement. */
   IoTDBStatement(
       IoTDBConnection connection,
       TSIService.Iface client,
@@ -351,7 +345,7 @@ public class IoTDBStatement implements Statement {
         allSuccess =
             allSuccess
                 && (execResp.getCode() == TSStatusCode.SUCCESS_STATUS.getStatusCode()
-                || execResp.getCode() == TSStatusCode.NEED_REDIRECTION.getStatusCode());
+                    || execResp.getCode() == TSStatusCode.NEED_REDIRECTION.getStatusCode());
         result[i] = execResp.getCode();
         message.setLength(0);
         message.append(execResp.getMessage());
