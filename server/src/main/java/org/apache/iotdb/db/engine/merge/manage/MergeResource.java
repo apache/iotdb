5c072a198ea0ee82c42963ebd5bcded128ac20de
zhanglingzhe0820 <445073309@qq.com>
2021-05-15 21:02:33 +0800

    [To rel/0.11] Unseq compaction throws wrong exception if some paths are not in the file (#3197)
    * Unseq compaction throws wrong exception if some paths are not in the file
    
    * fix ci
    
    Co-authored-by: Zesong Sun <szs19@mails.tsinghua.edu.cn>
    Co-authored-by: zhanglingzhe <surevil@foxmail.com>

--- server/src/main/java/org/apache/iotdb/db/engine/merge/manage/MergeResource.java
+++ server/src/main/java/org/apache/iotdb/db/engine/merge/manage/MergeResource.java
@@ -135,7 +135,7 @@ public class MergeResource {
   public List<ChunkMetadata> queryChunkMetadata(PartialPath path, TsFileResource seqFile)
       throws IOException {
     TsFileSequenceReader sequenceReader = getFileReader(seqFile);
-    return sequenceReader.getChunkMetadataList(path);
+    return sequenceReader.getChunkMetadataList(path, true);
   }
 
   /**
