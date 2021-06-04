5b9ec415f914ef24b5e0ca8b38f0b0c6d2630329
chaow <xunmengzhuiyi@gmail.com>
2021-06-04 20:52:15 +0800

    Fix session ci bug(#3343)

--- session/src/test/java/org/apache/iotdb/session/IoTDBSessionSimpleIT.java
+++ session/src/test/java/org/apache/iotdb/session/IoTDBSessionSimpleIT.java
@@ -611,13 +611,13 @@ public class IoTDBSessionSimpleIT {
         });
     Assert.assertArrayEquals(
         dataSet.getColumnTypes().toArray(new String[0]),
-        new TSDataType[] {
-          TSDataType.INT64,
-          TSDataType.INT64,
-          TSDataType.FLOAT,
-          TSDataType.BOOLEAN,
-          TSDataType.INT32,
-          TSDataType.INT32
+        new String[] {
+          TSDataType.INT64.toString(),
+          TSDataType.INT64.toString(),
+          TSDataType.FLOAT.toString(),
+          TSDataType.BOOLEAN.toString(),
+          TSDataType.INT32.toString(),
+          TSDataType.INT32.toString()
         });
     long time = 1L;
     //
