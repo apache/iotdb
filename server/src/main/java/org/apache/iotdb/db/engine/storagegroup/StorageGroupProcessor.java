5819a5b2777ca39a23092772f6fd7b0bcdd3295b
samperson1997 <szs19@mails.tsinghua.edu.cn>
2021-06-06 01:36:19 +0800

    Fix last update

--- server/src/main/java/org/apache/iotdb/db/engine/storagegroup/StorageGroupProcessor.java
+++ server/src/main/java/org/apache/iotdb/db/engine/storagegroup/StorageGroupProcessor.java
@@ -1096,21 +1096,19 @@ public class StorageGroupProcessor {
     }
     MeasurementMNode[] mNodes = plan.getMeasurementMNodes();
     int columnIndex = 0;
-    for (int i = 0; i < mNodes.length; i++) {
+    for (MeasurementMNode mNode : mNodes) {
       // Don't update cached last value for vector type
-      if (mNodes[i] != null && plan.isAligned()) {
-        columnIndex += mNodes[i].getSchema().getValueMeasurementIdList().size();
-      } else {
+      if (!plan.isAligned()) {
         if (plan.getValues()[columnIndex] == null) {
           columnIndex++;
           continue;
         }
         // Update cached last value with high priority
-        if (mNodes[i] != null) {
+        if (mNode != null) {
           // in stand alone version, the seriesPath is not needed, just use measurementMNodes[i] to
           // update last cache
           IoTDB.metaManager.updateLastCache(
-              null, plan.composeTimeValuePair(columnIndex), true, latestFlushedTime, mNodes[i]);
+              null, plan.composeTimeValuePair(columnIndex), true, latestFlushedTime, mNode);
         } else {
           IoTDB.metaManager.updateLastCache(
               plan.getPrefixPath().concatNode(plan.getMeasurements()[columnIndex]),
