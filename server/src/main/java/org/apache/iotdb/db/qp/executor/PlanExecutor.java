b31715e782dbb6d4b5d9ba46e851d6116ebdbdfb
BigGreyBear <x_zhao16@163.com>
2021-06-08 03:08:29 +0800

    set ttl can be applied to root now

--- server/src/main/java/org/apache/iotdb/db/qp/executor/PlanExecutor.java
+++ server/src/main/java/org/apache/iotdb/db/qp/executor/PlanExecutor.java
@@ -1152,8 +1152,16 @@ public class PlanExecutor implements IPlanExecutor {
 
   private void operateTTL(SetTTLPlan plan) throws QueryProcessException {
     try {
-      IoTDB.metaManager.setTTL(plan.getStorageGroup(), plan.getDataTTL());
-      StorageEngine.getInstance().setTTL(plan.getStorageGroup(), plan.getDataTTL());
+      List<PartialPath> storageGroupPaths = new ArrayList<>();
+      if (plan.getStorageGroup().equals("root")) {
+        storageGroupPaths = IoTDB.metaManager.getStorageGroupPaths(plan.getStorageGroup());
+      } else {
+        storageGroupPaths.add(plan.getStorageGroup());
+      }
+      for (PartialPath storagePath : storageGroupPaths){
+        IoTDB.metaManager.setTTL(storagePath, plan.getDataTTL());
+        StorageEngine.getInstance().setTTL(storagePath, plan.getDataTTL());
+      }
     } catch (MetadataException e) {
       throw new QueryProcessException(e);
     } catch (IOException e) {
