62700735328691a7e348fe67ca3106a9e43d3a5c
samperson1997 <szs19@mails.tsinghua.edu.cn>
2021-06-11 10:38:36 +0800

    fix type

--- tsfile/src/test/java/org/apache/iotdb/tsfile/read/MeasurementChunkMetadataListMapIteratorTest.java
+++ tsfile/src/test/java/org/apache/iotdb/tsfile/read/MeasurementChunkMetadataListMapIteratorTest.java
@@ -153,8 +153,7 @@ public class MeasurementChunkMetadataListMapIteratorTest {
         checkCorrectness(expected, actual);
 
         // test not exist device
-        Iterator<Map<String, List<ChunkMetadata>>> iterator =
-            fileReader.getMeasurementChunkMetadataListMapIterator("dd");
+        iterator = fileReader.getMeasurementChunkMetadataListMapIterator("dd");
         Assert.assertFalse(iterator.hasNext());
       }
     }
