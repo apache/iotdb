a5525d10ee346cb40419aa60f9e533691c5a6540
HouliangQi <neuyilan@163.com>
2021-06-15 09:53:50 +0800

    spotless apply

--- server/src/test/java/org/apache/iotdb/db/tools/TsFileSketchToolTest.java
+++ server/src/test/java/org/apache/iotdb/db/tools/TsFileSketchToolTest.java
@@ -25,7 +25,6 @@ import org.apache.iotdb.tsfile.fileSystem.FSFactoryProducer;
 import org.apache.iotdb.tsfile.read.common.Path;
 import org.apache.iotdb.tsfile.write.TsFileWriter;
 import org.apache.iotdb.tsfile.write.record.Tablet;
-import org.apache.iotdb.tsfile.write.schema.IMeasurementSchema;
 import org.apache.iotdb.tsfile.write.schema.MeasurementSchema;
 import org.apache.iotdb.tsfile.write.schema.Schema;
 
@@ -61,10 +60,10 @@ public class TsFileSketchToolTest {
       // the number of values to include in the tablet
       int sensorNum = 10;
 
-      List<IMeasurementSchema> measurementSchemas = new ArrayList<>();
+      List<MeasurementSchema> measurementSchemas = new ArrayList<>();
       // add measurements into file schema (all with INT64 data type)
       for (int i = 0; i < sensorNum; i++) {
-        IMeasurementSchema measurementSchema =
+        MeasurementSchema measurementSchema =
             new MeasurementSchema(sensorPrefix + (i + 1), TSDataType.INT64, TSEncoding.TS_2DIFF);
         measurementSchemas.add(measurementSchema);
         schema.registerTimeseries(
