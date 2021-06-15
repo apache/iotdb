95f978b67a321c12e4e2f3b182415bd74388a332
xiangdong huang <sainthxd@gmail.com>
2021-06-15 11:17:28 +0800

    rename client-py version to 0.11.4

--- client-py/setup.py
+++ client-py/setup.py
@@ -31,7 +31,7 @@ print(long_description)
 
 setuptools.setup(
     name="apache-iotdb",  # Replace with your own username
-    version="0.11.3",
+    version="0.11.4",
     author=" Apache Software Foundation",
     author_email="dev@iotdb.apache.org",
     description="Apache IoTDB client API",
