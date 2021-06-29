24dc29b36a30e580ef92ce4fefc7e15e6ff6f21c
wangyanhong <1101967410@qq.com>
2021-06-29 11:22:43 +0800

    add audit log in PlanExecutor

---
+++ server/src/main/java/org/apache/iotdb/db/query/udf/builtin/UDTFSum.java
@@ -0,0 +1,115 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.iotdb.db.query.udf.builtin;
+
+import org.apache.iotdb.db.exception.metadata.MetadataException;
+import org.apache.iotdb.db.query.udf.api.UDTF;
+import org.apache.iotdb.db.query.udf.api.access.Row;
+import org.apache.iotdb.db.query.udf.api.collector.PointCollector;
+import org.apache.iotdb.db.query.udf.api.customizer.config.UDTFConfigurations;
+import org.apache.iotdb.db.query.udf.api.customizer.parameter.UDFParameters;
+import org.apache.iotdb.db.query.udf.api.customizer.strategy.RowByRowAccessStrategy;
+import org.apache.iotdb.db.query.udf.api.exception.UDFInputSeriesDataTypeNotValidException;
+import org.apache.iotdb.tsfile.file.metadata.enums.TSDataType;
+
+import java.io.IOException;
+
+public class UDTFSum implements UDTF {
+
+  protected int pathsNum;
+  protected TSDataType dataType;
+
+  @Override
+  public void beforeStart(UDFParameters parameters, UDTFConfigurations configurations)
+      throws MetadataException {
+    pathsNum = parameters.getPaths().size();
+    System.out.println("paths:"+parameters.getPaths());
+    dataType = parameters.getDataType(0);
+    configurations.setAccessStrategy(new RowByRowAccessStrategy()).setOutputDataType(dataType);
+  }
+
+  @Override
+  public void transform(Row row, PointCollector collector)
+      throws UDFInputSeriesDataTypeNotValidException, IOException {
+    long time = row.getTime();
+    switch (dataType) {
+      case INT32:
+        {
+          int result = 0;
+          for (int i = 0; i < pathsNum; i++) {
+            try {
+              result += row.getInt(i);
+            } catch (NullPointerException e) {
+              continue;
+            }
+          }
+          collector.putInt(time, result);
+        }
+        break;
+      case INT64:
+        {
+          long result = 0;
+          for (int i = 0; i < pathsNum; i++) {
+            try {
+              result += row.getLong(i);
+            } catch (NullPointerException e) {
+              continue;
+            }
+          }
+          collector.putLong(time, result);
+        }
+        break;
+      case FLOAT:
+        {
+          float result = 0;
+          for (int i = 0; i < pathsNum; i++) {
+            try {
+              result += row.getFloat(i);
+            } catch (NullPointerException e) {
+              continue;
+            }
+          }
+          collector.putFloat(time, result);
+        }
+        break;
+      case DOUBLE:
+        {
+          double result = 0;
+          for (int i = 0; i < pathsNum; i++) {
+            try {
+              result += row.getDouble(i);
+            } catch (NullPointerException e) {
+              continue;
+            }
+          }
+          collector.putDouble(time, result);
+        }
+        break;
+      default:
+        // This will not happen.
+        throw new UDFInputSeriesDataTypeNotValidException(
+            0, dataType, TSDataType.INT32, TSDataType.INT64, TSDataType.FLOAT, TSDataType.DOUBLE);
+    }
+  }
+
+  protected void setTransformer() {
+    throw new UnsupportedOperationException("UDTFSum#setTransformer()");
+  }
+}
