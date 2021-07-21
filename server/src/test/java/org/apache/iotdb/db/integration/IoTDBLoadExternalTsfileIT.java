f3721bdff767e59f49cb429f79b4cc3ce0143bea
yusicheng <928623089@qq.com>
2021-07-21 20:23:32 +0800

    improve import for test

--- server/src/test/java/org/apache/iotdb/db/integration/IoTDBLoadExternalTsfileIT.java
+++ server/src/test/java/org/apache/iotdb/db/integration/IoTDBLoadExternalTsfileIT.java
@@ -50,7 +50,10 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.fail;
+import static org.junit.Assert.assertTrue;
 
 public class IoTDBLoadExternalTsfileIT {
 
