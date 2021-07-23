704786740a0064098de0c8c8c8ea67f870e0b44b
Ring-k <yuyuankang@hotmail.com>
2021-07-23 16:00:24 +0800

    remove EXCEPTION

--- flink-iotdb-connector/src/main/java/org/apache/iotdb/flink/IoTDBSink.java
+++ flink-iotdb-connector/src/main/java/org/apache/iotdb/flink/IoTDBSink.java
@@ -76,7 +76,7 @@ public class IoTDBSink<IN> extends RichSinkFunction<IN> {
     initScheduler();
   }
 
-  void initSession() throws Exception {
+  void initSession() {
     pool =
         new SessionPool(
             options.getHost(),
