120678a960a709f3063dbb3bc09a4c0254bd7b26
Steve Yurong Su (宇荣) <rong@apache.org>
2021-08-18 00:21:06 -0500

    PrimitiveArrayManager: reduce log of array deque size update & add limitUpdateThreshold update log (#3778)

--- server/src/main/java/org/apache/iotdb/db/rescon/PrimitiveArrayManager.java
+++ server/src/main/java/org/apache/iotdb/db/rescon/PrimitiveArrayManager.java
@@ -179,7 +179,7 @@ public class PrimitiveArrayManager {
       int newLimit = (int) (limitBase * ratios[i]);
       LIMITS[i] = newLimit;
 
-      if (LOGGER.isInfoEnabled()) {
+      if (LOGGER.isInfoEnabled() && oldLimit != newLimit) {
         LOGGER.info(
             "limit of {} array deque size updated: {} -> {}",
             TSDataType.deserialize((byte) i).name(),
@@ -188,11 +188,16 @@ public class PrimitiveArrayManager {
       }
     }
 
+    long oldLimitUpdateThreshold = limitUpdateThreshold;
     // limitUpdateThreshold = ∑(LIMITS[i])
     limitUpdateThreshold = 0;
     for (int limit : LIMITS) {
       limitUpdateThreshold += limit;
     }
+    LOGGER.info(
+        "limitUpdateThreshold of PrimitiveArrayManager updated: {} -> {}",
+        oldLimitUpdateThreshold,
+        limitUpdateThreshold);
 
     for (AtomicLong allocationRequestCount : ALLOCATION_REQUEST_COUNTS) {
       allocationRequestCount.set(0);
