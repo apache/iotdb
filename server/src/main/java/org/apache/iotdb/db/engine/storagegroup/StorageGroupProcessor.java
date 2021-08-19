13afc5c5081499d906cddf6991060f541e90fe65
yusicheng <yscysc99@126.com>
2021-08-19 17:40:59 +0800

    spotless

--- server/src/main/java/org/apache/iotdb/db/engine/storagegroup/StorageGroupProcessor.java
+++ server/src/main/java/org/apache/iotdb/db/engine/storagegroup/StorageGroupProcessor.java
@@ -2444,11 +2444,19 @@ public class StorageGroupProcessor {
 
     long meanTime = preTime + ((subsequenceTime - preTime) >> 1);
     if (insertIndex != tsFileManagement.size(true) - 1 && meanTime == subsequenceTime) {
-      throw new LoadFileException("can not load TsFile because of can not find suitable location, " +
-              "preName: " + preTime + " " +
-              "subsequenceTime: " + subsequenceTime + " " +
-              "insertIndex: " + insertIndex + " " +
-              "tsFilemanagementSize: " + tsFileManagement.size(true));
+      throw new LoadFileException(
+          "can not load TsFile because of can not find suitable location, "
+              + "preName: "
+              + preTime
+              + " "
+              + "subsequenceTime: "
+              + subsequenceTime
+              + " "
+              + "insertIndex: "
+              + insertIndex
+              + " "
+              + "tsFilemanagementSize: "
+              + tsFileManagement.size(true));
     }
 
     return getNewTsFileName(
