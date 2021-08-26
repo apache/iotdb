23a875cff6ea5941fe5508f5e9b830a078b221d8
Jialin Qiao <qjl16@mails.tsinghua.edu.cn>
2021-08-26 11:56:58 +0800

    Update server/src/test/java/org/apache/iotdb/db/integration/IoTDBLikeIT.java

--- server/src/test/java/org/apache/iotdb/db/integration/IoTDBLikeIT.java
+++ server/src/test/java/org/apache/iotdb/db/integration/IoTDBLikeIT.java
@@ -135,6 +135,7 @@ public class IoTDBLikeIT {
     hasResultSet = st0.execute("select status from root.t1.wf01.wt01 where status like '6\\%%'");
     Assert.assertTrue(hasResultSet);
     Assert.assertEquals("6%16", outputResultStr(st0.getResultSet()));
+
     hasResultSet = st0.execute("select status from root.t1.wf01.wt01 where status like '\\%%'");
     Assert.assertTrue(hasResultSet);
     Assert.assertEquals("%123", outputResultStr(st0.getResultSet()));
