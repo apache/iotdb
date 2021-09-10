489a51c486c46d2d32bc2bc10dfc35f9394f8aae
Haonan <hhaonan@outlook.com>
2021-09-10 17:37:17 +0800

    [To rel/0.12] cherry-pick two CLI bug fix PRs (#3944)
    * [IOTDB-1275] Fix backgroup exec for cli -e function causes an infinite loop in writing output file (#3932)
    
    * [IOTDB-1659] Fix Windows CLI cannot set maxPRC less than or equal to 0 (#3936)

--- cli/src/main/java/org/apache/iotdb/cli/WinCli.java
+++ cli/src/main/java/org/apache/iotdb/cli/WinCli.java
@@ -99,20 +99,16 @@ public class WinCli extends AbstractCli {
         timeFormat = RpcUtils.setTimeFormat("long");
       }
       if (commandLine.hasOption(MAX_PRINT_ROW_COUNT_ARGS)) {
-        maxPrintRowCount = Integer.parseInt(commandLine.getOptionValue(MAX_PRINT_ROW_COUNT_ARGS));
-        if (maxPrintRowCount <= 0) {
-          println(
-              IOTDB_CLI_PREFIX
-                  + "> error format of max print row count, it should be a number greater than 0");
-          return false;
-        }
+        setMaxDisplayNumber(commandLine.getOptionValue(MAX_PRINT_ROW_COUNT_ARGS));
       }
     } catch (ParseException e) {
       println("Require more params input, please check the following hint.");
       hf.printHelp(IOTDB_CLI_PREFIX, options, true);
       return false;
     } catch (NumberFormatException e) {
-      println(IOTDB_CLI_PREFIX + "> error format of max print row count, it should be a number");
+      println(
+          IOTDB_CLI_PREFIX
+              + "> error format of max print row count, it should be an integer number");
       return false;
     }
     return true;
