779c9053c2e65b63b34c740fb93bd792d2e9978f
HTHou <hhaonan@outlook.com>
2021-09-10 14:47:08 +0800

    [IOTDB-1670] Fix cli -e mode didn't fetch timestamp_precision from server

--- cli/src/main/java/org/apache/iotdb/cli/WinCli.java
+++ cli/src/main/java/org/apache/iotdb/cli/WinCli.java
@@ -133,6 +133,7 @@ public class WinCli extends AbstractCli {
                 DriverManager.getConnection(
                     Config.IOTDB_URL_PREFIX + host + ":" + port + "/", username, password)) {
           properties = connection.getServerProperties();
+          timestampPrecision = properties.getTimestampPrecision();
           AGGREGRATE_TIME_LIST.addAll(properties.getSupportedTimeAggregationOperations());
           processCommand(execute, connection);
           return;
