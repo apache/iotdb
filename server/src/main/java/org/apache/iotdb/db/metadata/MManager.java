ea1d8662dd98891746b503bfc44d7ecaadad5c22
Xieqijun <44310040+jun0315@users.noreply.github.com>
2021-09-15 23:51:52 +0800

    [IOTDB-1690] Fix align by device type cast error (#3971)

--- server/src/main/java/org/apache/iotdb/db/metadata/MManager.java
+++ server/src/main/java/org/apache/iotdb/db/metadata/MManager.java
@@ -1292,8 +1292,10 @@ public class MManager {
       Template template = node.getUpperTemplate();
 
       for (IMNode child : node.getChildren().values()) {
-        IMeasurementMNode measurementMNode = (IMeasurementMNode) child;
-        res.add(measurementMNode.getSchema());
+        if (child.isMeasurement()) {
+          IMeasurementMNode measurementMNode = (IMeasurementMNode) child;
+          res.add(measurementMNode.getSchema());
+        }
       }
 
       // template
