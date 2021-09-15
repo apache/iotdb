9e4ee9cc6aeacb81bdf857dba6984ea662c4aeb4
MrQuansy <1051622353@qq.com>
2021-09-15 09:31:45 +0800

    fix a bug in processPlanWithTolerance

--- cluster/src/main/java/org/apache/iotdb/cluster/log/applier/BaseApplier.java
+++ cluster/src/main/java/org/apache/iotdb/cluster/log/applier/BaseApplier.java
@@ -219,9 +219,8 @@ abstract class BaseApplier implements LogApplier {
           pullTimeseriesSchema(plan, dataGroupMember.getHeader());
           plan.recoverFromFailure();
           getQueryExecutor().processNonQuery(plan);
-        }
-      }
-      throw e;
+        } else throw e;
+      } else throw e;
     }
   }
 
