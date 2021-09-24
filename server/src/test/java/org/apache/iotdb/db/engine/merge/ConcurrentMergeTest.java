0c6007536f6138798724073a7f0d895d1983adde
THUMarkLau <liuxuxin@outlook.com>
2021-09-24 23:42:19 +0800

    add apache header

--- server/src/test/java/org/apache/iotdb/db/engine/merge/ConcurrentMergeTest.java
+++ server/src/test/java/org/apache/iotdb/db/engine/merge/ConcurrentMergeTest.java
@@ -1,3 +1,22 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 package org.apache.iotdb.db.engine.merge;
 
 import org.apache.iotdb.db.conf.IoTDBConfig;
@@ -19,6 +38,7 @@ import org.apache.iotdb.db.utils.EnvironmentUtils;
 import org.apache.iotdb.tsfile.file.metadata.enums.TSDataType;
 import org.apache.iotdb.tsfile.write.record.TSRecord;
 import org.apache.iotdb.tsfile.write.record.datapoint.DataPoint;
+
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
@@ -27,86 +47,85 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class ConcurrentMergeTest {
-	private static final IoTDBConfig config = IoTDBDescriptor.getInstance().getConfig();
-	private static Logger logger = LoggerFactory.getLogger(StorageGroupProcessorTest.class);
+  private static final IoTDBConfig config = IoTDBDescriptor.getInstance().getConfig();
+  private static Logger logger = LoggerFactory.getLogger(StorageGroupProcessorTest.class);
 
-	private String storageGroup = "root.vehicle.d0";
-	private String systemDir = TestConstant.OUTPUT_DATA_DIR.concat("info");
-	private String deviceId = "root.vehicle.d0";
-	private String measurementId = "s0";
-	private StorageGroupProcessor processor;
-	private QueryContext context = EnvironmentUtils.TEST_QUERY_CONTEXT;
+  private String storageGroup = "root.vehicle.d0";
+  private String systemDir = TestConstant.OUTPUT_DATA_DIR.concat("info");
+  private String deviceId = "root.vehicle.d0";
+  private String measurementId = "s0";
+  private StorageGroupProcessor processor;
+  private QueryContext context = EnvironmentUtils.TEST_QUERY_CONTEXT;
 
-	@Before
-	public void setUp() throws Exception {
-		config.setCompactionStrategy(CompactionStrategy.LEVEL_COMPACTION);
-		MetadataManagerHelper.initMetadata();
-		EnvironmentUtils.envSetUp();
-		processor = new ConcurrentMergeTest.DummySGP(systemDir, storageGroup);
-		MergeManager.getINSTANCE().start();
-	}
+  @Before
+  public void setUp() throws Exception {
+    config.setCompactionStrategy(CompactionStrategy.LEVEL_COMPACTION);
+    MetadataManagerHelper.initMetadata();
+    EnvironmentUtils.envSetUp();
+    processor = new ConcurrentMergeTest.DummySGP(systemDir, storageGroup);
+    MergeManager.getINSTANCE().start();
+  }
 
-	@After
-	public void tearDown() throws Exception {
-		processor.syncDeleteDataFiles();
-		EnvironmentUtils.cleanEnv();
-		EnvironmentUtils.cleanDir(TestConstant.OUTPUT_DATA_DIR);
-		MergeManager.getINSTANCE().stop();
-		EnvironmentUtils.cleanEnv();
-		config.setCompactionStrategy(CompactionStrategy.LEVEL_COMPACTION);
-	}
+  @After
+  public void tearDown() throws Exception {
+    processor.syncDeleteDataFiles();
+    EnvironmentUtils.cleanEnv();
+    EnvironmentUtils.cleanDir(TestConstant.OUTPUT_DATA_DIR);
+    MergeManager.getINSTANCE().stop();
+    EnvironmentUtils.cleanEnv();
+    config.setCompactionStrategy(CompactionStrategy.LEVEL_COMPACTION);
+  }
 
-	@Test
-	public void testConcurrentMerge() throws Exception {
-		for (int j = 21; j <= 30; j++) {
-			TSRecord record = new TSRecord(j, deviceId);
-			record.addTuple(DataPoint.getDataPoint(TSDataType.INT32, measurementId, String.valueOf(j)));
-			processor.insert(new InsertRowPlan(record));
-			processor.asyncCloseAllWorkingTsFileProcessors();
-		}
-		processor.syncCloseAllWorkingTsFileProcessors();
+  @Test
+  public void testConcurrentMerge() throws Exception {
+    for (int j = 21; j <= 30; j++) {
+      TSRecord record = new TSRecord(j, deviceId);
+      record.addTuple(DataPoint.getDataPoint(TSDataType.INT32, measurementId, String.valueOf(j)));
+      processor.insert(new InsertRowPlan(record));
+      processor.asyncCloseAllWorkingTsFileProcessors();
+    }
+    processor.syncCloseAllWorkingTsFileProcessors();
 
-		for (int j = 10; j >= 1; j--) {
-			TSRecord record = new TSRecord(j, deviceId);
-			record.addTuple(DataPoint.getDataPoint(TSDataType.INT32, measurementId, String.valueOf(j)));
-			processor.insert(new InsertRowPlan(record));
-			processor.asyncCloseAllWorkingTsFileProcessors();
-		}
+    for (int j = 10; j >= 1; j--) {
+      TSRecord record = new TSRecord(j, deviceId);
+      record.addTuple(DataPoint.getDataPoint(TSDataType.INT32, measurementId, String.valueOf(j)));
+      processor.insert(new InsertRowPlan(record));
+      processor.asyncCloseAllWorkingTsFileProcessors();
+    }
 
-		processor.syncCloseAllWorkingTsFileProcessors();
-		Thread mergeThreadOne = new Thread(() -> processor.merge());
-		Thread mergeThreadTwo = new Thread(() -> processor.merge());
-		mergeThreadOne.start();
-		mergeThreadTwo.start();
-		try {
-			Thread.sleep(1000);
-		} catch (Exception e) {
-			
-		}
-		while (processor.getTsFileManagement().isSeqMerging) {
-			// wait
-		}
+    processor.syncCloseAllWorkingTsFileProcessors();
+    Thread mergeThreadOne = new Thread(() -> processor.merge());
+    Thread mergeThreadTwo = new Thread(() -> processor.merge());
+    mergeThreadOne.start();
+    mergeThreadTwo.start();
+    try {
+      Thread.sleep(1000);
+    } catch (Exception e) {
 
-		QueryDataSource queryDataSource =
-						processor.query(new PartialPath(deviceId, measurementId), context, null, null);
-		Assert.assertEquals(5, queryDataSource.getSeqResources().size());
-		for (TsFileResource resource : queryDataSource.getSeqResources()) {
-			Assert.assertTrue(resource.isClosed());
-		}
-		for (TsFileResource resource : queryDataSource.getUnseqResources()) {
-			Assert.assertTrue(resource.isClosed());
-		}
-	}
+    }
+    while (processor.getTsFileManagement().isSeqMerging) {
+      // wait
+    }
 
+    QueryDataSource queryDataSource =
+        processor.query(new PartialPath(deviceId, measurementId), context, null, null);
+    Assert.assertEquals(5, queryDataSource.getSeqResources().size());
+    for (TsFileResource resource : queryDataSource.getSeqResources()) {
+      Assert.assertTrue(resource.isClosed());
+    }
+    for (TsFileResource resource : queryDataSource.getUnseqResources()) {
+      Assert.assertTrue(resource.isClosed());
+    }
+  }
 
-	class DummySGP extends StorageGroupProcessor {
+  class DummySGP extends StorageGroupProcessor {
 
-		DummySGP(String systemInfoDir, String storageGroupName) throws StorageGroupProcessorException {
-			super(
-							systemInfoDir,
-							storageGroupName,
-							new TsFileFlushPolicy.DirectFlushPolicy(),
-							storageGroupName);
-		}
-	}
+    DummySGP(String systemInfoDir, String storageGroupName) throws StorageGroupProcessorException {
+      super(
+          systemInfoDir,
+          storageGroupName,
+          new TsFileFlushPolicy.DirectFlushPolicy(),
+          storageGroupName);
+    }
+  }
 }
