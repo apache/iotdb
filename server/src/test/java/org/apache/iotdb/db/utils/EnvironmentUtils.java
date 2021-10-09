4a9cb1fa7fdf3ce40a7958e341eb7894719ff09f
Xiangwei Wei <34242296+alima777@users.noreply.github.com>
2021-10-09 10:03:00 +0800

    Resolve conflicts

--- server/src/test/java/org/apache/iotdb/db/utils/EnvironmentUtils.java
+++ server/src/test/java/org/apache/iotdb/db/utils/EnvironmentUtils.java
@@ -37,6 +37,7 @@ import org.apache.iotdb.db.exception.UDFRegistrationException;
 import org.apache.iotdb.db.query.context.QueryContext;
 import org.apache.iotdb.db.query.control.FileReaderManager;
 import org.apache.iotdb.db.query.control.QueryResourceManager;
+import org.apache.iotdb.db.query.control.QueryTimeManager;
 import org.apache.iotdb.db.query.udf.service.UDFRegistrationService;
 import org.apache.iotdb.db.rescon.MemTableManager;
 import org.apache.iotdb.db.rescon.PrimitiveArrayManager;
@@ -143,6 +144,8 @@ public class EnvironmentUtils {
     }
     // close metadata
     IoTDB.metaManager.clear();
+
+    QueryTimeManager.getInstance().clear();
 
     // close array manager
     PrimitiveArrayManager.close();
