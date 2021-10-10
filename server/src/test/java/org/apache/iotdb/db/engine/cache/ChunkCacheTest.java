d00e2ad5ccbd2a82563e7157cc237b8548072f8a
liuminghui233 <liuminghui233@foxmail.com>
2021-10-10 23:03:41 +0800

    retry CI

--- server/src/test/java/org/apache/iotdb/db/engine/cache/ChunkCacheTest.java
+++ server/src/test/java/org/apache/iotdb/db/engine/cache/ChunkCacheTest.java
@@ -56,7 +56,6 @@ import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
 public class ChunkCacheTest {
-
   File tempSGDir;
   static final String TEST_SG = "root.sg1";
 
