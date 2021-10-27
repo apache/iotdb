57a73f23517fe993c493644b71de2ccc19219b09
LebronAl <TXYPotato@gmail.com>
2021-10-27 16:15:41 +0800

    mvn spotless:apply

--- server/src/test/java/org/apache/iotdb/db/utils/EnvironmentUtils.java
+++ server/src/test/java/org/apache/iotdb/db/utils/EnvironmentUtils.java
@@ -48,15 +48,16 @@ import org.apache.iotdb.rpc.TConfigurationConst;
 import org.apache.iotdb.rpc.TSocketWrapper;
 import org.apache.iotdb.tsfile.utils.FilePathUtils;
 
-import javax.management.remote.JMXConnector;
-import javax.management.remote.JMXConnectorFactory;
-import javax.management.remote.JMXServiceURL;
 import org.apache.commons.io.FileUtils;
 import org.apache.thrift.TConfiguration;
 import org.apache.thrift.transport.TTransport;
 import org.apache.thrift.transport.TTransportException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import javax.management.remote.JMXConnector;
+import javax.management.remote.JMXConnectorFactory;
+import javax.management.remote.JMXServiceURL;
 
 import java.io.File;
 import java.io.IOException;
