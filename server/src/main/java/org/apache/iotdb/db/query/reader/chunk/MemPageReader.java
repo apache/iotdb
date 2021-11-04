5c5bb200fdd3724fafae1a7e0d54c6b16a722536
JackieTien97 <JackieTien@foxmail.com>
2021-11-04 16:19:19 +0800

    change some

--- server/src/main/java/org/apache/iotdb/db/query/reader/chunk/MemPageReader.java
+++ server/src/main/java/org/apache/iotdb/db/query/reader/chunk/MemPageReader.java
@@ -18,7 +18,7 @@
  */
 package org.apache.iotdb.db.query.reader.chunk;
 
-import org.apache.iotdb.tsfile.file.metadata.AlignedChunkMetadata;
+import java.io.IOException;
 import org.apache.iotdb.tsfile.file.metadata.IChunkMetadata;
 import org.apache.iotdb.tsfile.file.metadata.enums.TSDataType;
 import org.apache.iotdb.tsfile.file.metadata.statistics.Statistics;
@@ -29,8 +29,6 @@ import org.apache.iotdb.tsfile.read.filter.basic.Filter;
 import org.apache.iotdb.tsfile.read.filter.operator.AndFilter;
 import org.apache.iotdb.tsfile.read.reader.IPageReader;
 import org.apache.iotdb.tsfile.read.reader.IPointReader;
-
-import java.io.IOException;
 
 public class MemPageReader implements IPageReader {
 
@@ -47,14 +45,7 @@ public class MemPageReader implements IPageReader {
 
   @Override
   public BatchData getAllSatisfiedPageData(boolean ascending) throws IOException {
-    TSDataType dataType;
-    if (chunkMetadata instanceof AlignedChunkMetadata
-        && ((AlignedChunkMetadata) chunkMetadata).getValueChunkMetadataList().size() == 1) {
-      dataType =
-          ((AlignedChunkMetadata) chunkMetadata).getValueChunkMetadataList().get(0).getDataType();
-    } else {
-      dataType = chunkMetadata.getDataType();
-    }
+    TSDataType  dataType = chunkMetadata.getDataType();
     BatchData batchData = BatchDataFactory.createBatchData(dataType, ascending, false);
     while (timeValuePairIterator.hasNextTimeValuePair()) {
       TimeValuePair timeValuePair = timeValuePairIterator.nextTimeValuePair();
