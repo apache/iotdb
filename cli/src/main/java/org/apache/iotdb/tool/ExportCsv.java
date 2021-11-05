f841f08006b86385631c538d40169b308857d105
xuanronaldo <xuanronaldo@qq.com>
2021-11-05 10:44:04 +0800

    fix 1913.

--- cli/src/main/java/org/apache/iotdb/tool/ExportCsv.java
+++ cli/src/main/java/org/apache/iotdb/tool/ExportCsv.java
@@ -36,11 +36,11 @@ import org.apache.commons.cli.HelpFormatter;
 import org.apache.commons.cli.Option;
 import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVPrinter;
+import org.apache.commons.csv.QuoteMode;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.time.Instant;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
@@ -286,68 +286,14 @@ public class ExportCsv extends AbstractCsvTool {
    * @param index use to create dump file name
    */
   private static void dumpResult(String sql, int index) {
-
     final String path = targetDirectory + targetFile + index + ".csv";
     try {
-      SessionDataSet sessionDataSet = session.executeQueryStatement(sql);
-      List<List<Object>> records = loadDataFromDataSet(sessionDataSet);
-      writeCsvFile(null, records, path);
+      SessionDataSet sessionDataSet = session.executeQueryStatement(sql, 10000);
+      writeCsvFile(sessionDataSet, path);
       System.out.println("Export completely!");
-    } catch (StatementExecutionException | IoTDBConnectionException e) {
+    } catch (StatementExecutionException | IoTDBConnectionException | IOException e) {
       System.out.println("Cannot dump result because: " + e.getMessage());
     }
-  }
-
-  /**
-   * Load data from the result of query command.
-   *
-   * @param sessionDataSet
-   * @return
-   * @throws IoTDBConnectionException
-   * @throws StatementExecutionException
-   */
-  public static List<List<Object>> loadDataFromDataSet(SessionDataSet sessionDataSet)
-      throws IoTDBConnectionException, StatementExecutionException {
-    List<List<Object>> records = new ArrayList<>();
-    List<Object> headers = new ArrayList<>();
-    List<String> names = sessionDataSet.getColumnNames();
-    List<String> types = sessionDataSet.getColumnTypes();
-
-    if (needDataTypePrinted == true) {
-      for (int i = 0; i < names.size(); i++) {
-        if (!names.get(i).equals("Time") && !names.get(i).equals("Device"))
-          headers.add(String.format("%s(%s)", names.get(i), types.get(i)));
-        else headers.add(names.get(i));
-      }
-    } else {
-      names.forEach(name -> headers.add(name));
-    }
-    records.add(headers);
-
-    while (sessionDataSet.hasNext()) {
-      RowRecord rowRecord = sessionDataSet.next();
-      ArrayList<Object> record = new ArrayList<>();
-      if (rowRecord.getTimestamp() != 0) {
-        record.add(timeTrans(rowRecord.getTimestamp()));
-      }
-      rowRecord
-          .getFields()
-          .forEach(
-              field -> {
-                String fieldStringValue = field.getStringValue();
-                if (!field.getStringValue().equals("null")) {
-                  if (field.getDataType() == TSDataType.TEXT
-                      && !fieldStringValue.startsWith("root.")) {
-                    fieldStringValue = "\"" + fieldStringValue + "\"";
-                  }
-                  record.add(fieldStringValue);
-                } else {
-                  record.add("");
-                }
-              });
-      records.add(record);
-    }
-    return records;
   }
 
   public static String timeTrans(Long time) {
@@ -364,5 +310,58 @@ public class ExportCsv extends AbstractCsvTool {
         return ZonedDateTime.ofInstant(Instant.ofEpochMilli(time), zoneId)
             .format(DateTimeFormatter.ofPattern(timeFormat));
     }
+  }
+
+  public static Boolean writeCsvFile(SessionDataSet sessionDataSet, String filePath)
+      throws IOException, IoTDBConnectionException, StatementExecutionException {
+    CSVPrinter printer =
+        CSVFormat.DEFAULT
+            .withFirstRecordAsHeader()
+            .withEscape('\\')
+            .withQuoteMode(QuoteMode.NONE)
+            .print(new PrintWriter(filePath));
+
+    List<Object> headers = new ArrayList<>();
+    List<String> names = sessionDataSet.getColumnNames();
+    List<String> types = sessionDataSet.getColumnTypes();
+
+    if (needDataTypePrinted) {
+      for (int i = 0; i < names.size(); i++) {
+        if (!names.get(i).equals("Time") && !names.get(i).equals("Device")) {
+          headers.add(String.format("%s(%s)", names.get(i), types.get(i)));
+        } else {
+          headers.add(names.get(i));
+        }
+      }
+    } else {
+      names.forEach(name -> headers.add(name));
+    }
+    printer.printRecord(headers);
+
+    while (sessionDataSet.hasNext()) {
+      RowRecord rowRecord = sessionDataSet.next();
+      ArrayList<Object> record = new ArrayList<>();
+      record.add(timeTrans(rowRecord.getTimestamp()));
+      rowRecord
+          .getFields()
+          .forEach(
+              field -> {
+                String fieldStringValue = field.getStringValue();
+                if (!field.getStringValue().equals("null")) {
+                  if (field.getDataType() == TSDataType.TEXT
+                      && !fieldStringValue.startsWith("root.")) {
+                    fieldStringValue = "\"" + fieldStringValue + "\"";
+                  }
+                  record.add(fieldStringValue);
+                } else {
+                  record.add("");
+                }
+              });
+      printer.printRecord(record);
+    }
+
+    printer.flush();
+    printer.close();
+    return true;
   }
 }
