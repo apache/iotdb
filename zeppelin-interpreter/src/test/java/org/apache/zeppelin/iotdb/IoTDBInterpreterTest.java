1be3335f0b967b417eba045c666eca2fdbee46c5
JackieTien97 <JackieTien@foxmail.com>
2021-11-15 13:32:21 +0800

    fix zepplin ci

--- zeppelin-interpreter/src/test/java/org/apache/zeppelin/iotdb/IoTDBInterpreterTest.java
+++ zeppelin-interpreter/src/test/java/org/apache/zeppelin/iotdb/IoTDBInterpreterTest.java
@@ -330,7 +330,7 @@ public class IoTDBInterpreterTest {
   @Test
   public void testShowDevices() {
     InterpreterResult actual = interpreter.internalInterpret("show devices", null);
-    String gt = "devices\n" + "root.test.wf01.wt01\n" + "root.test.wf02.wt02";
+    String gt = "devices\n" + "root.test.wf02.wt02\n" + "root.test.wf01.wt01";
     Assert.assertNotNull(actual);
     Assert.assertEquals(Code.SUCCESS, actual.code());
     Assert.assertEquals(gt, actual.message().get(0).getData());
@@ -342,8 +342,8 @@ public class IoTDBInterpreterTest {
         interpreter.internalInterpret("show devices with storage group", null);
     String gt =
         "devices\tstorage group\n"
-            + "root.test.wf01.wt01\troot.test.wf01\n"
-            + "root.test.wf02.wt02\troot.test.wf02";
+            + "root.test.wf02.wt02\troot.test.wf02\n"
+            + "root.test.wf01.wt01\troot.test.wf01";
     Assert.assertNotNull(actual);
     Assert.assertEquals(Code.SUCCESS, actual.code());
     System.out.println(actual.message().get(0).getData());
