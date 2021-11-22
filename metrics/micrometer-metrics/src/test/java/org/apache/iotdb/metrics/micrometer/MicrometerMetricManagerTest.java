38ba4c3829f8f4f6e6380b04a559c8cdaae751a4
还真 <huanzhen.gy@alibaba-inc.com>
2021-11-22 14:47:17 +0800

    mvn spotless:apply

--- metrics/micrometer-metrics/src/test/java/org/apache/iotdb/metrics/micrometer/MicrometerMetricManagerTest.java
+++ metrics/micrometer-metrics/src/test/java/org/apache/iotdb/metrics/micrometer/MicrometerMetricManagerTest.java
@@ -22,29 +22,31 @@ package org.apache.iotdb.metrics.micrometer;
 import org.apache.iotdb.metrics.MetricManager;
 import org.apache.iotdb.metrics.MetricService;
 import org.apache.iotdb.metrics.type.Timer;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertThrows;
+
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertThrows;
+
 public class MicrometerMetricManagerTest {
 
-    @BeforeClass
-    public static void init() {
-        System.setProperty("line.separator", "\n");
-        // set up path of yml
-        System.setProperty("IOTDB_CONF", "src/test/resources");
-    }
+  @BeforeClass
+  public static void init() {
+    System.setProperty("line.separator", "\n");
+    // set up path of yml
+    System.setProperty("IOTDB_CONF", "src/test/resources");
+  }
 
-    private void getOrCreateDifferentMetricsWithSameName() {
-        MetricManager metricManager = MetricService.getMetricManager();
-        Timer timer = metricManager.getOrCreateTimer("metric", "tag1", "tag2");
-        assertNotNull(timer);
-        metricManager.getOrCreateCounter("metric", "tag1", "tag2");
-    }
+  private void getOrCreateDifferentMetricsWithSameName() {
+    MetricManager metricManager = MetricService.getMetricManager();
+    Timer timer = metricManager.getOrCreateTimer("metric", "tag1", "tag2");
+    assertNotNull(timer);
+    metricManager.getOrCreateCounter("metric", "tag1", "tag2");
+  }
 
-    @Test
-    public void getOrCreateDifferentMetricsWithSameNameTest() {
-        assertThrows(IllegalArgumentException.class, this::getOrCreateDifferentMetricsWithSameName);
-    }
+  @Test
+  public void getOrCreateDifferentMetricsWithSameNameTest() {
+    assertThrows(IllegalArgumentException.class, this::getOrCreateDifferentMetricsWithSameName);
+  }
 }
