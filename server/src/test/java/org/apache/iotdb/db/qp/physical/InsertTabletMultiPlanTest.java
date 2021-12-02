311783804fddd58d53cfb37d49efc3599116d089
谢其骏 <xieqijun@mail.nwpu.edu.cn>
2021-12-02 17:20:00 +0800

    remove unless test

--- server/src/test/java/org/apache/iotdb/db/qp/physical/InsertTabletMultiPlanTest.java
+++ server/src/test/java/org/apache/iotdb/db/qp/physical/InsertTabletMultiPlanTest.java
@@ -46,56 +46,6 @@ public class InsertTabletMultiPlanTest extends InsertTabletPlanTest {
   private final Planner processor = new Planner();
 
   @Test
-  public void testInsertMultiTabletPlanSpeed()
-      throws QueryProcessException, MetadataException, InterruptedException,
-          QueryFilterOptimizationException, StorageEngineException, IOException {
-    long[] times = new long[] {110L, 111L, 112L, 113L};
-    List<Integer> dataTypes = new ArrayList<>();
-    dataTypes.add(TSDataType.DOUBLE.ordinal());
-    dataTypes.add(TSDataType.FLOAT.ordinal());
-    dataTypes.add(TSDataType.INT64.ordinal());
-    dataTypes.add(TSDataType.INT32.ordinal());
-    dataTypes.add(TSDataType.BOOLEAN.ordinal());
-    dataTypes.add(TSDataType.TEXT.ordinal());
-
-    Object[] columns = new Object[6];
-    columns[0] = new double[4];
-    columns[1] = new float[4];
-    columns[2] = new long[4];
-    columns[3] = new int[4];
-    columns[4] = new boolean[4];
-    columns[5] = new Binary[4];
-
-    for (int r = 0; r < 4; r++) {
-      ((double[]) columns[0])[r] = 1.0;
-      ((float[]) columns[1])[r] = 2;
-      ((long[]) columns[2])[r] = 10000;
-      ((int[]) columns[3])[r] = 100;
-      ((boolean[]) columns[4])[r] = false;
-      ((Binary[]) columns[5])[r] = new Binary("hh" + r);
-    }
-    List<InsertTabletPlan> insertTabletPlanList = new ArrayList<>();
-    for (int i = 0; i < 10; i++) {
-      InsertTabletPlan tabletPlan =
-          new InsertTabletPlan(
-              new PartialPath("root.multi.d" + i),
-              new String[] {"s1", "s2", "s3", "s4", "s5", "s6"},
-              dataTypes);
-      tabletPlan.setTimes(times);
-      tabletPlan.setColumns(columns);
-      tabletPlan.setRowCount(times.length);
-      insertTabletPlanList.add(tabletPlan);
-    }
-    PlanExecutor executor = new PlanExecutor();
-
-    long before = System.currentTimeMillis();
-
-    InsertMultiTabletPlan insertMultiTabletPlan = new InsertMultiTabletPlan(insertTabletPlanList);
-    executor.insertTablet(insertMultiTabletPlan);
-    System.out.println("result" + String.valueOf(System.currentTimeMillis() - before));
-  }
-
-  @Test
   public void testInsertMultiTabletPlan()
       throws QueryProcessException, MetadataException, InterruptedException,
           QueryFilterOptimizationException, StorageEngineException, IOException {
