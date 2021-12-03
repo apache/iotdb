38b98b42d27a540bed7a17c944683c04502fc8cd
xuanronaldo <xuanronaldo@qq.com>
2021-12-03 10:55:50 +0800

    update ITs.

--- spark-iotdb-connector/src/main/scala/org/apache/iotdb/spark/db/DefaultSource.scala
+++ spark-iotdb-connector/src/main/scala/org/apache/iotdb/spark/db/DefaultSource.scala
@@ -50,7 +50,7 @@ private[iotdb] class DefaultSource extends RelationProvider with DataSourceRegis
     }
     val iotdbOptions = new IoTDBOptions(parameters)
 
-    if (!data.columns.contains("Device")) {
+    if (!data.columns.contains("device_name")) {
       data.columns.foreach(column => if (!column.startsWith("root.") && column != "Time") sys.error("Invalidate column: " + column))
       val narrowDf = Transformer.toNarrowForm(sqlContext.sparkSession, data)
       DataFrameTools.insertDataFrame(iotdbOptions, narrowDf)
