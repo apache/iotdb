4e8e1be96a4c9f004cc5a8055967da87e1995d13
谢其骏 <xieqijun@mail.nwpu.edu.cn>
2021-12-06 11:26:10 +0800

    delete

--- influxdb-protocol/src/main/java/org/apache/iotdb/influxdb/session/InfluxDBSession.java
+++ influxdb-protocol/src/main/java/org/apache/iotdb/influxdb/session/InfluxDBSession.java
@@ -144,7 +144,6 @@ public class InfluxDBSession {
       transport.close();
       throw new IoTDBConnectionException(e);
     }
-    System.out.println(sessionId);
   }
 
   public void writePoints(TSWritePointsReq request)
