e4d9d77f4ae4aa6f9e9bcc9efc09fbed628b6b11
谢其骏 <xieqijun@mail.nwpu.edu.cn>
2021-12-07 21:04:45 +0800

    UT

---
+++ server/src/test/java/org/apache/iotdb/db/protocol/influxdb/sql/LogicalGeneratorTest.java
@@ -0,0 +1,61 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.iotdb.db.protocol.influxdb.sql;
+
+import org.apache.iotdb.db.protocol.influxdb.expression.ResultColumn;
+import org.apache.iotdb.db.protocol.influxdb.expression.unary.NodeExpression;
+import org.apache.iotdb.db.protocol.influxdb.operator.*;
+
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class LogicalGeneratorTest {
+  @Test
+  public void testParserSql1() {
+    QueryOperator operator = (QueryOperator) LogicalGenerator.generate("SELECT * FROM h2o_feet");
+    List<ResultColumn> resultColumnList = operator.getSelectComponent().getResultColumns();
+    assertEquals(resultColumnList.size(), 1);
+    NodeExpression nodeExpression = (NodeExpression) resultColumnList.get(0).getExpression();
+    assertEquals(nodeExpression.getName(), "*");
+    assertEquals(operator.getFromComponent().getNodeName().get(0), "h2o_feet");
+    assertNull(operator.getWhereComponent());
+  }
+
+  @Test
+  public void testParserSql2() {
+    QueryOperator operator =
+        (QueryOperator) LogicalGenerator.generate("SELECT a,b,c FROM h2o_feet where a>1 and b<1");
+    List<ResultColumn> resultColumnList = operator.getSelectComponent().getResultColumns();
+    assertEquals(resultColumnList.size(), 3);
+    NodeExpression nodeExpression = (NodeExpression) resultColumnList.get(0).getExpression();
+    assertEquals(nodeExpression.getName(), "a");
+    WhereComponent whereComponent = operator.getWhereComponent();
+    FilterOperator filterOperator = whereComponent.getFilterOperator();
+    assertEquals(filterOperator.getFilterType().toString(), "KW_AND");
+    assertEquals(filterOperator.getChildren().size(), 2);
+    BasicFunctionOperator basicFunctionOperator =
+        (BasicFunctionOperator) filterOperator.getChildren().get(0);
+    assertEquals(basicFunctionOperator.getValue(), "1");
+    assertEquals(basicFunctionOperator.getKeyName(), "a");
+    assertNull(basicFunctionOperator.getFilterType());
+  }
+}
