6c94dbc1474197374d6655f261c559394e858bac
xuanronaldo <xuanronaldo@qq.com>
2021-12-10 10:50:16 +0800

    modify the test and add examples.

---
+++ example/spark/src/main/java/org/apache/iotdb/Example.java
@@ -0,0 +1,54 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.iotdb;
+
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SparkSession;
+
+public class Example {
+  public static void main(String[] args) {
+    // create the spark session
+    SparkSession spark =
+        SparkSession.builder()
+            .appName("IoTDB Read And Write Example")
+            .master("local[*]")
+            .getOrCreate();
+
+    // read data from IoTDB
+    Dataset<Row> dataset =
+        spark
+            .read()
+            .format("org.apache.iotdb.spark.db")
+            .option("url", "jdbc:iotdb://127.0.0.1:6667/")
+            .option("sql", "select * from root")
+            .load();
+
+    // display the data
+    dataset.show();
+
+    // save to IoTDB
+    dataset
+        .write()
+        .format("org.apache.iotdb.spark.db")
+        .option("url", "jdbc:iotdb://127.0.0.1:6667/")
+        .save();
+  }
+}
--- spark-iotdb-connector/src/test/scala/org/apache/iotdb/spark/db/IoTDBWriteTest.scala
+++ spark-iotdb-connector/src/test/scala/org/apache/iotdb/spark/db/IoTDBWriteTest.scala
@@ -72,12 +72,12 @@ class IoTDBWriteTest extends FunSuite with BeforeAndAfterAll {
       (2L, 2, 2L, 2.0F, 2.0D, false, "world")))
 
     val dfWithColumn = df.withColumnRenamed("_1", "Time")
-      .withColumnRenamed("_2", "root.test.d0.int")
-      .withColumnRenamed("_3", "root.test.d0.long")
-      .withColumnRenamed("_4", "root.test.d0.float")
-      .withColumnRenamed("_5", "root.test.d0.double")
-      .withColumnRenamed("_6", "root.test.d0.boolean")
-      .withColumnRenamed("_7", "root.test.d0.text")
+      .withColumnRenamed("_2", "root.test.d0.s0")
+      .withColumnRenamed("_3", "root.test.d0.s1")
+      .withColumnRenamed("_4", "root.test.d0.s2")
+      .withColumnRenamed("_5", "root.test.d0.s3")
+      .withColumnRenamed("_6", "root.test.d0.s4")
+      .withColumnRenamed("_7", "root.test.d0.s5")
     dfWithColumn.write.format("org.apache.iotdb.spark.db")
       .option("url", "jdbc:iotdb://127.0.0.1:6667/")
       .save
@@ -97,17 +97,17 @@ class IoTDBWriteTest extends FunSuite with BeforeAndAfterAll {
 
     val dfWithColumn = df.withColumnRenamed("_1", "Time")
       .withColumnRenamed("_2", "device_name")
-      .withColumnRenamed("_3", "int")
-      .withColumnRenamed("_4", "long")
-      .withColumnRenamed("_5", "float")
-      .withColumnRenamed("_6", "double")
-      .withColumnRenamed("_7", "boolean")
-      .withColumnRenamed("_8", "text")
+      .withColumnRenamed("_3", "s0")
+      .withColumnRenamed("_4", "s1")
+      .withColumnRenamed("_5", "s2")
+      .withColumnRenamed("_6", "s3")
+      .withColumnRenamed("_7", "s4")
+      .withColumnRenamed("_8", "s5")
     dfWithColumn.write.format("org.apache.iotdb.spark.db")
       .option("url", "jdbc:iotdb://127.0.0.1:6667/")
       .save
 
-    val result = session.executeQueryStatement("select ** from root")
+    val result = session.executeQueryStatement("select * from root")
     var size = 0
     while (result.hasNext) {
       size += 1
