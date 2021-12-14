7e36caae91f1484c0bdce2ddb46e6a9ff1e05e72
Minghui Liu <liuminghui233@foxmail.com>
2021-12-14 17:07:09 +0800

    spotless apply

--- server/src/main/java/org/apache/iotdb/db/engine/storagegroup/TsFileResource.java
+++ server/src/main/java/org/apache/iotdb/db/engine/storagegroup/TsFileResource.java
@@ -47,7 +47,6 @@ import java.io.OutputStream;
 import java.nio.file.FileAlreadyExistsException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
