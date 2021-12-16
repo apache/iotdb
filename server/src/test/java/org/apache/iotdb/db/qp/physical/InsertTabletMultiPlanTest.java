1fa9fd1adf823e924e2d67a44372c59a87d3a4bf
谢其骏 <xieqijun@mail.nwpu.edu.cn>
2021-12-16 18:02:07 +0800

    tmp

--- server/src/test/java/org/apache/iotdb/db/qp/physical/InsertTabletMultiPlanTest.java
+++ server/src/test/java/org/apache/iotdb/db/qp/physical/InsertTabletMultiPlanTest.java
@@ -46,6 +46,67 @@ public class InsertTabletMultiPlanTest extends InsertTabletPlanTest {
   private final Planner processor = new Planner();
 
   @Test
+  public void testInsertMultiTabletPlanParallel()
+          throws QueryProcessException, MetadataException, InterruptedException,
+          QueryFilterOptimizationException, StorageEngineException, IOException {
+    int size = 50;
+    long[] times = new long[size];
+    for (int i =0 ;i<size;i++){
+      times[i]=i;
+    }
+    List<Integer> dataTypes = new ArrayList<>();
+    dataTypes.add(TSDataType.DOUBLE.ordinal());
+    dataTypes.add(TSDataType.FLOAT.ordinal());
+    dataTypes.add(TSDataType.INT64.ordinal());
+    dataTypes.add(TSDataType.INT32.ordinal());
+    dataTypes.add(TSDataType.BOOLEAN.ordinal());
+    dataTypes.add(TSDataType.TEXT.ordinal());
+
+    Object[] columns = new Object[6];
+    columns[0] = new double[4];
+    columns[1] = new float[4];
+    columns[2] = new long[4];
+    columns[3] = new int[4];
+    columns[4] = new boolean[4];
+    columns[5] = new Binary[4];
+
+    for (int r = 0; r < size; r++) {
+      ((double[]) columns[0])[r] = 1.0;
+      ((float[]) columns[1])[r] = 2;
+      ((long[]) columns[2])[r] = 10000;
+      ((int[]) columns[3])[r] = 100;
+      ((boolean[]) columns[4])[r] = false;
+      ((Binary[]) columns[5])[r] = new Binary("hh" + r);
+    }
+
+    List<InsertTabletPlan> insertTabletPlanList = new ArrayList<>();
+    for (int i = 0; i < 10; i++) {
+      InsertTabletPlan tabletPlan =
+              new InsertTabletPlan(
+                      new PartialPath("root.multi"+i/5+"d." + i),
+                      new String[] {"s1", "s2", "s3", "s4", "s5", "s6"},
+                      dataTypes);
+      tabletPlan.setTimes(times);
+      tabletPlan.setColumns(columns);
+      tabletPlan.setRowCount(times.length);
+      insertTabletPlanList.add(tabletPlan);
+    }
+    PlanExecutor executor = new PlanExecutor();
+
+    InsertMultiTabletPlan insertMultiTabletPlan = new InsertMultiTabletPlan(insertTabletPlanList);
+
+    executor.insertTablet(insertMultiTabletPlan);
+    QueryPlan queryPlan =
+            (QueryPlan) processor.parseSQLToPhysicalPlan("select * from root.multi.**");
+    QueryDataSet dataSet = executor.processQuery(queryPlan, EnvironmentUtils.TEST_QUERY_CONTEXT);
+    Assert.assertEquals(60, dataSet.getPaths().size());
+    while (dataSet.hasNext()) {
+      RowRecord record = dataSet.next();
+      Assert.assertEquals(60, record.getFields().size());
+    }
+  }
+
+  @Test
   public void testInsertMultiTabletPlan()
       throws QueryProcessException, MetadataException, InterruptedException,
           QueryFilterOptimizationException, StorageEngineException, IOException {
