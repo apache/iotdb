cef5f87076138ba5417026250195567abc5885a8
谢其骏 <xieqijun@mail.nwpu.edu.cn>
2021-12-17 10:54:43 +0800

    modify IT

--- server/src/test/java/org/apache/iotdb/db/qp/physical/InsertTabletMultiPlanTest.java
+++ server/src/test/java/org/apache/iotdb/db/qp/physical/InsertTabletMultiPlanTest.java
@@ -170,7 +170,7 @@ public class InsertTabletMultiPlanTest extends InsertTabletPlanTest {
     for (int i = 0; i < 10; i++) {
       InsertTabletPlan tabletPlan =
           new InsertTabletPlan(
-              new PartialPath("root.multi" + i / 2 + ".d" + i),
+              new PartialPath("root.multi" + i / 5 + ".d" + i),
               new String[] {
                 "s1", "s2", "s3", "s4", "s5", "s6", "s7", "s8", "s9", "s10", "s11", "s12", "s13",
                 "s14", "s15", "s16"
