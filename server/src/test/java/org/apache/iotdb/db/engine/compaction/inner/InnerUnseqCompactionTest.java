385f34b7ff266dd4468d140269bf47a3959a8baf
周沛辰 <45144903+choubenson@users.noreply.github.com>
2021-12-19 20:20:39 +0800

    [IOTDB-2112] Do not use .tsfile when compacting the target file (#4549)

--- server/src/test/java/org/apache/iotdb/db/engine/compaction/inner/InnerUnseqCompactionTest.java
+++ server/src/test/java/org/apache/iotdb/db/engine/compaction/inner/InnerUnseqCompactionTest.java
@@ -353,6 +353,7 @@ public class InnerUnseqCompactionTest {
                   new SizeTieredCompactionLogger("target", COMPACTION_TEST_SG);
               InnerSpaceCompactionUtils.compact(
                   targetTsFileResource, toMergeResources, COMPACTION_TEST_SG, false);
+              InnerSpaceCompactionUtils.moveTargetFile(targetTsFileResource, COMPACTION_TEST_SG);
               InnerSpaceCompactionUtils.combineModsInCompaction(
                   toMergeResources, targetTsFileResource);
               List<TsFileResource> targetTsFileResources = new ArrayList<>();
