ce4ac1af121fc42c5871027e6bf64a6ee96d654d
谢其骏 <xieqijun@mail.nwpu.edu.cn>
2021-12-20 17:11:41 +0800

    rename

--- server/src/main/java/org/apache/iotdb/db/protocol/influxdb/sql/InfluxDBLogicalGenerator.java
+++ server/src/main/java/org/apache/iotdb/db/protocol/influxdb/sql/InfluxDBLogicalGenerator.java
@@ -28,18 +28,18 @@ import org.antlr.v4.runtime.atn.PredictionMode;
 import org.antlr.v4.runtime.misc.ParseCancellationException;
 import org.antlr.v4.runtime.tree.ParseTree;
 
-public class LogicalGenerator {
+public class InfluxDBLogicalGenerator {
 
-  private LogicalGenerator() {}
+  private InfluxDBLogicalGenerator() {}
 
   public static Operator generate(String sql) throws ParseCancellationException {
     InfluxDBSqlVisitor influxDBSqlVisitor = new InfluxDBSqlVisitor();
     CharStream charStream1 = CharStreams.fromString(sql);
-    InfluxDBLexer lexer1 = new InfluxDBLexer(charStream1);
+    InfluxDBSqlLexer lexer1 = new InfluxDBSqlLexer(charStream1);
     lexer1.removeErrorListeners();
     lexer1.addErrorListener(SQLParseError.INSTANCE);
     CommonTokenStream tokens1 = new CommonTokenStream(lexer1);
-    InfluxDBParser parser1 = new InfluxDBParser(tokens1);
+    InfluxDBSqlParser parser1 = new InfluxDBSqlParser(tokens1);
     parser1.getInterpreter().setPredictionMode(PredictionMode.SLL);
     parser1.removeErrorListeners();
     parser1.addErrorListener(SQLParseError.INSTANCE);
@@ -48,11 +48,11 @@ public class LogicalGenerator {
       tree = parser1.singleStatement(); // STAGE 1
     } catch (Exception ex) {
       CharStream charStream2 = CharStreams.fromString(sql);
-      InfluxDBLexer lexer2 = new InfluxDBLexer(charStream2);
+      InfluxDBSqlLexer lexer2 = new InfluxDBSqlLexer(charStream2);
       lexer2.removeErrorListeners();
       lexer2.addErrorListener(SQLParseError.INSTANCE);
       CommonTokenStream tokens2 = new CommonTokenStream(lexer2);
-      InfluxDBParser parser2 = new InfluxDBParser(tokens2);
+      InfluxDBSqlParser parser2 = new InfluxDBSqlParser(tokens2);
       parser2.getInterpreter().setPredictionMode(PredictionMode.LL);
       parser2.removeErrorListeners();
       parser2.addErrorListener(SQLParseError.INSTANCE);
