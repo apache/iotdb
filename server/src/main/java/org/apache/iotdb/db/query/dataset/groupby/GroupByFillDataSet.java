50a310a5047433c049f32553187a459218541a46
CRZbulabula <crzbulabula@gmail.com>
2021-12-24 15:38:14 +0800

    bug fix

--- server/src/main/java/org/apache/iotdb/db/query/dataset/groupby/GroupByFillDataSet.java
+++ server/src/main/java/org/apache/iotdb/db/query/dataset/groupby/GroupByFillDataSet.java
@@ -1,3 +1,21 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package org.apache.iotdb.db.query.dataset.groupby;
 
 import org.apache.iotdb.db.exception.StorageEngineException;
@@ -40,8 +58,9 @@ public class GroupByFillDataSet extends GroupByEngineDataSet {
   private long[] previousTimes;
   private Object[] previousValues;
 
-  // the next value and time for each time series
+  // the next not null and unused rowId for each time series
   private int[] nextIds;
+  // the next value and time for each time series
   private List<ElasticSerializableTVList> nextTVLists;
 
   public GroupByFillDataSet(QueryContext context, GroupByTimeFillPlan groupByTimeFillPlan)
@@ -89,7 +108,7 @@ public class GroupByFillDataSet extends GroupByEngineDataSet {
     for (int i = 0; i < aggregations.size(); i++) {
       nextTVLists.add(
           ElasticSerializableTVList.newElasticSerializableTVList(
-              resultDataType[i], context.getQueryId(), 10, fetchSize));
+              resultDataType[i], context.getQueryId(), 10, 2));
     }
   }
 
@@ -293,6 +312,7 @@ public class GroupByFillDataSet extends GroupByEngineDataSet {
         previousTimes[i] = curTimestamp;
         previousValues[i] = getNextCacheValue(i);
         nextIds[i] += 1;
+        nextTVLists.get(i).setEvictionUpperBound(nextIds[i]);
         slideSet.set(i);
       }
     }
