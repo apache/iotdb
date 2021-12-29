c77f46555958c6938749698fb50c7af6e7e236d5
谢其骏 <xieqijun@mail.nwpu.edu.cn>
2021-12-29 12:33:23 +0800

    remove iotdb registry

--- server/src/main/java/org/apache/iotdb/db/service/metrics/MetricsService.java
+++ server/src/main/java/org/apache/iotdb/db/service/metrics/MetricsService.java
@@ -22,8 +22,6 @@ import org.apache.iotdb.db.conf.IoTDBConstant;
 import org.apache.iotdb.db.conf.IoTDBDescriptor;
 import org.apache.iotdb.db.conf.directories.DirectoryManager;
 import org.apache.iotdb.db.exception.StartupException;
-import org.apache.iotdb.db.metrics.micrometer.registry.IoTDBMeterRegistry;
-import org.apache.iotdb.db.metrics.micrometer.registry.IoTDBRegistryConfig;
 import org.apache.iotdb.db.service.IService;
 import org.apache.iotdb.db.service.JMXService;
 import org.apache.iotdb.db.service.ServiceType;
@@ -34,11 +32,9 @@ import org.apache.iotdb.metrics.Reporter;
 import org.apache.iotdb.metrics.config.MetricConfig;
 import org.apache.iotdb.metrics.config.MetricConfigDescriptor;
 import org.apache.iotdb.metrics.impl.DoNothingMetricManager;
-import org.apache.iotdb.metrics.micrometer.MicrometerMetricManager;
 import org.apache.iotdb.metrics.utils.PredefinedMetric;
 import org.apache.iotdb.metrics.utils.ReporterType;
 
-import io.micrometer.core.instrument.Clock;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -161,12 +157,13 @@ public class MetricsService implements MetricsServiceMBean, IService {
 
   private void metricManagerInit() {
     metricManager.init();
-    if (metricManager instanceof MicrometerMetricManager) {
-      if (metricConfig.getMetricReporterList().contains(ReporterType.iotdb)) {
-        ((MicrometerMetricManager) metricManager)
-            .addMeterRegistry(new IoTDBMeterRegistry(IoTDBRegistryConfig.DEFAULT, Clock.SYSTEM));
-      }
-    }
+    //    if (metricManager instanceof MicrometerMetricManager) {
+    //      if (metricConfig.getMetricReporterList().contains(ReporterType.iotdb)) {
+    //        ((MicrometerMetricManager) metricManager)
+    //            .addMeterRegistry(new IoTDBMeterRegistry(IoTDBRegistryConfig.DEFAULT,
+    // Clock.SYSTEM));
+    //      }
+    //    }
   }
 
   private void collectFileSystemInfo() {
