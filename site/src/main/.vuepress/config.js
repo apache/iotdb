c174f010e527cda5b5ae3b2c738af70f978a7d54
trin1t <48903014+trin1t@users.noreply.github.com>
2022-01-01 14:55:47 +0800

    Update config.js

--- site/src/main/.vuepress/config.js
+++ site/src/main/.vuepress/config.js
@@ -1531,7 +1531,7 @@ var config = {
           						children: [
           							['Library-UDF/Get-started', '快速上手']
           						]
-          					},
+          			},
           						{
 						title: '通信服务协议',
 						children: [
