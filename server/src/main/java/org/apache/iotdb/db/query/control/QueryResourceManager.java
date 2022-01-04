7cc606ff0692b301870a800b100c95f8bc3ca56a
Minghui Liu <liuminghui233@foxmail.com>
2022-01-04 14:39:19 +0800

    import opt

--- server/src/main/java/org/apache/iotdb/db/query/control/QueryResourceManager.java
+++ server/src/main/java/org/apache/iotdb/db/query/control/QueryResourceManager.java
@@ -32,7 +32,12 @@ import org.apache.iotdb.db.utils.QueryUtils;
 import org.apache.iotdb.tsfile.read.filter.basic.Filter;
 
 import java.io.IOException;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.stream.Collectors;
