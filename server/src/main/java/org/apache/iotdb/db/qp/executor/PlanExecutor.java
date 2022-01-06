ee9b5890e371a8de865597ceddb8c293c8b74974
yschengzi <yscysc99@126.com>
2022-01-06 16:50:56 +0800

    remove == null for time chunck

--- server/src/main/java/org/apache/iotdb/db/qp/executor/PlanExecutor.java
+++ server/src/main/java/org/apache/iotdb/db/qp/executor/PlanExecutor.java
@@ -1226,7 +1226,7 @@ public class PlanExecutor implements IPlanExecutor {
         continue;
       }
       for (TimeseriesMetadata metadata : entry.getValue()) {
-        if (metadata.getMeasurementId() == null || metadata.getMeasurementId().equals("")) {
+        if (metadata.getMeasurementId().equals("")) {
           continue;
         }
         PartialPath fullPath =
@@ -1279,7 +1279,7 @@ public class PlanExecutor implements IPlanExecutor {
         existSeriesSet.add(path.getMeasurementAlias());
       }
       for (ChunkMetadata chunkMetadata : chunkGroupMetadata.getChunkMetadataList()) {
-        if (chunkMetadata.getMeasurementUid() == null || chunkMetadata.getMeasurementUid().equals("")) {
+        if (chunkMetadata.getMeasurementUid().equals("")) {
           isAligned = true;
           continue;
         }
