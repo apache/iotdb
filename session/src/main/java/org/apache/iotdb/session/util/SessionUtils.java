7f89488c8fbd6c85297b327956325dcf90d45cd2
Minghui Liu <liuminghui233@foxmail.com>
2022-01-10 18:54:59 +0800

    add example for insert tablet with null value

--- session/src/main/java/org/apache/iotdb/session/util/SessionUtils.java
+++ session/src/main/java/org/apache/iotdb/session/util/SessionUtils.java
@@ -28,7 +28,6 @@ import org.apache.iotdb.tsfile.utils.BitMap;
 import org.apache.iotdb.tsfile.utils.BytesUtils;
 import org.apache.iotdb.tsfile.utils.ReadWriteIOUtils;
 import org.apache.iotdb.tsfile.write.record.Tablet;
-import org.apache.iotdb.tsfile.write.schema.IMeasurementSchema;
 import org.apache.iotdb.tsfile.write.schema.MeasurementSchema;
 
 import org.slf4j.Logger;
