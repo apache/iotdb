bfefb0da9896ebc34f3b4a100a095d85a4b9201e
jt <jt2594838@163.com>
2022-01-10 12:48:23 +0800

    revert replace ip

--- cluster/src/main/java/org/apache/iotdb/cluster/ClusterIoTDB.java
+++ cluster/src/main/java/org/apache/iotdb/cluster/ClusterIoTDB.java
@@ -288,6 +288,11 @@ public class ClusterIoTDB implements ClusterIoTDBMBean {
   }
 
   private String clusterConfigCheck() {
+    try {
+      ClusterDescriptor.getInstance().replaceHostnameWithIp();
+    } catch (Exception e) {
+      return String.format("replace hostname with ip failed, %s", e.getMessage());
+    }
     ClusterConfig config = ClusterDescriptor.getInstance().getConfig();
     // check the initial replicateNum and refuse to start when the replicateNum <= 0
     if (config.getReplicationNum() <= 0) {
