ffa217616f0027bdd6e0bc1f6ee95c322560d530
jt <jt2594838@163.com>
2022-01-13 18:23:07 +0800

    apply spotless

--- integration/src/main/java/org/apache/iotdb/integration/env/ClusterEnvBase.java
+++ integration/src/main/java/org/apache/iotdb/integration/env/ClusterEnvBase.java
@@ -30,7 +30,6 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
