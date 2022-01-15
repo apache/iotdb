c31903be1d811d9d28bc9c755815f7fc1e6c8d26
Steve Yurong Su <rong@apache.org>
2022-01-15 09:34:47 +0800

    [IOTDB-2353] CQ: support specifying first execution time (BOUNDARY) when creating CQ (#4831)
    Feature: BOUNDARY, new schema check logic
    Bug: wrong startup order in IoTDB.java
    Bug: wrong sql spliting in CreateContinuousQueryPlan.java

--- server/src/main/java/org/apache/iotdb/db/service/IoTDB.java
+++ server/src/main/java/org/apache/iotdb/db/service/IoTDB.java
@@ -136,8 +136,6 @@ public class IoTDB implements IoTDBMBean {
     registerManager.register(TemporaryQueryDataFileService.getInstance());
     registerManager.register(UDFClassLoaderManager.getInstance());
     registerManager.register(UDFRegistrationService.getInstance());
-    registerManager.register(TriggerRegistrationService.getInstance());
-    registerManager.register(ContinuousQueryService.getInstance());
     registerManager.register(MetricsService.getInstance());
 
     // in cluster mode, RPC service is not enabled.
@@ -170,6 +168,8 @@ public class IoTDB implements IoTDBMBean {
     registerManager.register(SyncServerManager.getInstance());
     registerManager.register(UpgradeSevice.getINSTANCE());
     registerManager.register(SettleService.getINSTANCE());
+    registerManager.register(TriggerRegistrationService.getInstance());
+    registerManager.register(ContinuousQueryService.getInstance());
 
     logger.info("Congratulation, IoTDB is set up successfully. Now, enjoy yourself!");
   }
