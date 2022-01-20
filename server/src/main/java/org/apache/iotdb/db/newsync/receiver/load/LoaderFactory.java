3057b3dc7badcfd4b6c97e875645782a67b2c812
Cpaulyz <chen3yz@gmail.com>
2022-01-20 20:44:27 +0800

    save

---
+++ server/src/main/java/org/apache/iotdb/db/newsync/receiver/load/LoaderFactory.java
@@ -0,0 +1,9 @@
+package org.apache.iotdb.db.newsync.receiver.load;
+
+import java.io.File;
+
+public class LoaderFactory {
+    public ILoader createLoader(File file,String type){
+        return null;
+    }
+}
