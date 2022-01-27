1bcec6d619bdb55481bb45a094478136b1a1b0e2
Alima777 <wxw19981014@gmail.com>
2022-01-27 17:16:01 +0800

    remove redundant code

--- server/src/main/java/org/apache/iotdb/db/query/reader/series/SeriesReader.java
+++ server/src/main/java/org/apache/iotdb/db/query/reader/series/SeriesReader.java
@@ -908,11 +908,6 @@ public class SeriesReader {
       currentPageEndpointTime = orderUtils.getOverlapCheckTime(firstPageReader.getStatistics());
     }
 
-    // unpack all overlapped data for currentPageEndpointTime
-    unpackAllOverlappedTsFilesToTimeSeriesMetadata(currentPageEndpointTime);
-    unpackAllOverlappedTimeSeriesMetadataToCachedChunkMetadata(currentPageEndpointTime, false);
-    unpackAllOverlappedChunkMetadataToPageReaders(currentPageEndpointTime, false);
-
     /*
      * put all currently directly overlapped unseq page reader to merge reader
      */
