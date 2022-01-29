63cced85defca0a3185958f9de7ec3d974345ddf
Liu Xuxin <liuxuxin@outlook.com>
2022-01-29 15:39:20 +0800

    recover CrossSpaceCompactionExceptionHandler

--- server/src/main/java/org/apache/iotdb/db/engine/compaction/cross/CrossSpaceCompactionExceptionHandler.java
+++ server/src/main/java/org/apache/iotdb/db/engine/compaction/cross/CrossSpaceCompactionExceptionHandler.java
@@ -129,11 +129,6 @@ public class CrossSpaceCompactionExceptionHandler {
       List<TsFileResource> unseqFileList,
       TsFileManager tsFileManager)
       throws IOException {
-    TsFileResourceList seqResourceList =
-        tsFileManager.getSequenceListByTimePartition(seqFileList.get(0).getTimePartition());
-    TsFileResourceList unseqResourceList =
-        tsFileManager.getUnsequenceListByTimePartition(unseqFileList.get(0).getTimePartition());
-
     for (TsFileResource seqFile : seqFileList) {
       ModificationFile compactionModFile = ModificationFile.getCompactionMods(seqFile);
       if (compactionModFile.exists()) {
@@ -145,9 +140,6 @@ public class CrossSpaceCompactionExceptionHandler {
         normalModification.close();
         compactionModFile.close();
         FileUtils.delete(new File(compactionModFile.getFilePath()));
-      }
-      if (!seqFile.isFileInList()) {
-        seqResourceList.keepOrderInsert(seqFile);
       }
     }
 
@@ -163,16 +155,10 @@ public class CrossSpaceCompactionExceptionHandler {
         compactionModFile.close();
         FileUtils.delete(new File(compactionModFile.getFilePath()));
       }
-      if (!unseqFile.isFileInList()) {
-        unseqResourceList.keepOrderInsert(unseqFile);
-      }
     }
 
     boolean removeAllTargetFile = true;
     for (TsFileResource targetTsFile : targetTsFiles) {
-      if (targetTsFile.isFileInList()) {
-        seqResourceList.remove(targetTsFile);
-      }
       if (!targetTsFile.remove()) {
         LOGGER.error(
             "{} [Compaction][Exception] failed to delete target tsfile {} when handling exception",
