2babdb4761e7338988ab25ef16eb4f65f01cc349
Liu Xuxin <liuxuxin@outlook.com>
2022-01-30 22:45:20 +0800

    run ci again

--- server/src/test/java/org/apache/iotdb/db/engine/compaction/CompactionSchedulerTest.java
+++ server/src/test/java/org/apache/iotdb/db/engine/compaction/CompactionSchedulerTest.java
@@ -95,6 +95,7 @@ public class CompactionSchedulerTest {
     if (!basicOutputDir.exists()) {
       assertTrue(basicOutputDir.mkdirs());
     }
+
     IoTDBDescriptor.getInstance().getConfig().setEnableSeqSpaceCompaction(true);
     IoTDBDescriptor.getInstance().getConfig().setEnableUnseqSpaceCompaction(true);
     IoTDBDescriptor.getInstance().getConfig().setEnableCrossSpaceCompaction(true);
