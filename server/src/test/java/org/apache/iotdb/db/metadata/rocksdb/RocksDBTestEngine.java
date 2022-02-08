67edffbac2cfb7e73c698619e3b2cd994528944f
chengjianyun <chengjianyun@360.cn>
2022-02-08 16:59:14 +0800

    [RocksDB] add integration test

---
+++ server/src/test/java/org/apache/iotdb/db/metadata/rocksdb/RocksDBTestEngine.java
@@ -0,0 +1,131 @@
+package org.apache.iotdb.db.metadata.rocksdb;
+
+import org.apache.iotdb.db.conf.IoTDBConfig;
+import org.apache.iotdb.db.conf.IoTDBDescriptor;
+import org.apache.iotdb.db.engine.fileSystem.SystemFileFactory;
+import org.apache.iotdb.db.exception.metadata.IllegalPathException;
+import org.apache.iotdb.db.metadata.MManager;
+import org.apache.iotdb.db.metadata.MetadataConstant;
+import org.apache.iotdb.db.metadata.logfile.MLogReader;
+import org.apache.iotdb.db.metadata.logfile.MLogWriter;
+import org.apache.iotdb.db.metadata.path.PartialPath;
+import org.apache.iotdb.db.qp.physical.PhysicalPlan;
+import org.apache.iotdb.db.qp.physical.sys.CreateTimeSeriesPlan;
+import org.apache.iotdb.db.qp.physical.sys.SetStorageGroupPlan;
+import org.apache.iotdb.db.utils.FileUtils;
+
+import org.apache.commons.lang3.ArrayUtils;
+import org.rocksdb.RocksDBException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import static org.apache.iotdb.db.metadata.rocksdb.MRocksDBManager.ROCKSDB_PATH;
+
+public class RocksDBTestEngine {
+  private static final Logger logger = LoggerFactory.getLogger(MManager.class);
+  private static final IoTDBConfig config = IoTDBDescriptor.getInstance().getConfig();
+
+  private static final int BIN_CAPACITY = 100 * 1000;
+
+  private MLogWriter logWriter;
+  private File logFile;
+  public static List<List<CreateTimeSeriesPlan>> timeSeriesSet = new ArrayList<>();
+  public static Set<String> measurementPathSet = new HashSet<>();
+  public static Set<String> innerPathSet = new HashSet<>();
+  public static List<SetStorageGroupPlan> storageGroups = new ArrayList<>();
+
+  public RocksDBTestEngine() {
+    String schemaDir = config.getSchemaDir();
+    String logFilePath = schemaDir + File.separator + MetadataConstant.METADATA_LOG;
+    logFile = SystemFileFactory.INSTANCE.getFile(logFilePath);
+  }
+
+  public void startTest() {
+    RocksDBTestUtils.printMemInfo("Benchmark rocksdb start");
+    try {
+      prepareBenchmark();
+      /** rocksdb benchmark * */
+      MRocksDBManager rocksDBManager = new MRocksDBManager();
+      MRocksDBTest mRocksDBTest = new MRocksDBTest(rocksDBManager);
+      mRocksDBTest.testStorageGroupCreation(storageGroups);
+      mRocksDBTest.testTimeSeriesCreation(timeSeriesSet);
+      RocksDBTestUtils.printReport(mRocksDBTest.benchmarkResults, "rocksDB");
+      RocksDBTestUtils.printMemInfo("Benchmark finished");
+
+    } catch (IOException | RocksDBException e) {
+      logger.error("Error happened when run benchmark", e);
+    }
+  }
+
+  public void prepareBenchmark() throws IOException {
+    long time = System.currentTimeMillis();
+    logWriter = new MLogWriter(config.getSchemaDir(), MetadataConstant.METADATA_LOG + ".temp");
+    logWriter.setLogNum(0);
+    if (!logFile.exists()) {
+      throw new FileNotFoundException("we need a mlog.bin to init the benchmark test");
+    }
+    try (MLogReader mLogReader =
+        new MLogReader(config.getSchemaDir(), MetadataConstant.METADATA_LOG)) {
+      parseForTestSet(mLogReader);
+      logger.info("spend {} ms to prepare dataset", System.currentTimeMillis() - time);
+    } catch (Exception e) {
+      throw new IOException("Failed to parser mlog.bin for err:" + e);
+    }
+  }
+
+  private static void parseForTestSet(MLogReader mLogReader) throws IllegalPathException {
+    List<CreateTimeSeriesPlan> currentList = null;
+    SetStorageGroupPlan setStorageGroupPlan = new SetStorageGroupPlan();
+    setStorageGroupPlan.setPath(new PartialPath("root.iotcloud"));
+    storageGroups.add(setStorageGroupPlan);
+    while (mLogReader.hasNext()) {
+      PhysicalPlan plan = null;
+      try {
+        plan = mLogReader.next();
+        if (plan == null) {
+          continue;
+        }
+        switch (plan.getOperatorType()) {
+          case CREATE_TIMESERIES:
+            CreateTimeSeriesPlan createTimeSeriesPlan = (CreateTimeSeriesPlan) plan;
+            PartialPath path = createTimeSeriesPlan.getPath();
+            if (currentList == null) {
+              currentList = new ArrayList<>(BIN_CAPACITY);
+              timeSeriesSet.add(currentList);
+            }
+            measurementPathSet.add(path.getFullPath());
+
+            innerPathSet.add(path.getDevice());
+            String[] subNodes = ArrayUtils.subarray(path.getNodes(), 0, path.getNodes().length - 2);
+            innerPathSet.add(String.join(".", subNodes));
+
+            currentList.add(createTimeSeriesPlan);
+            if (currentList.size() >= BIN_CAPACITY) {
+              currentList = null;
+            }
+            break;
+          default:
+            break;
+        }
+      } catch (Exception e) {
+        logger.error(
+            "Can not operate cmd {} for err:", plan == null ? "" : plan.getOperatorType(), e);
+      }
+    }
+  }
+
+  private void resetEnv() throws IOException {
+    File rockdDbFile = new File(ROCKSDB_PATH);
+    if (rockdDbFile.exists() && rockdDbFile.isDirectory()) {
+      FileUtils.deleteDirectory(rockdDbFile);
+    }
+  }
+}
---
+++ server/src/test/java/org/apache/iotdb/db/metadata/rocksdb/RocksDBTestUtils.java
@@ -0,0 +1,53 @@
+package org.apache.iotdb.db.metadata.rocksdb;
+
+import org.apache.iotdb.db.qp.physical.sys.CreateTimeSeriesPlan;
+
+import java.util.Collection;
+import java.util.List;
+
+public class RocksDBTestUtils {
+
+  public static int WRITE_CLIENT_NUM = 20;
+  public static int READ_CLIENT_NUM = 30;
+
+  public static void printBenchmarkBaseline(
+      List storageGroups,
+      List<List<CreateTimeSeriesPlan>> timeSeriesSet,
+      Collection queryTsSet,
+      Collection innerPathSet) {
+    System.out.println(
+        "#################################Benchmark configuration#################################");
+    System.out.println("-----------Configuration---------");
+    System.out.println("Write client num: " + WRITE_CLIENT_NUM);
+    System.out.println("Query client num: " + READ_CLIENT_NUM);
+    System.out.println("-----------Benchmark Data Set Size---------");
+    System.out.println("StorageGroup: " + storageGroups.size());
+    int count = 0;
+    for (List l : timeSeriesSet) {
+      count += l.size();
+    }
+    System.out.println("TimeSeries: " + count);
+    System.out.println("MeasurementNodeQuery: " + queryTsSet.size());
+    System.out.println("ChildrenNodeQuery: " + innerPathSet.size());
+  }
+
+  public static void printMemInfo(String stageInfo) {
+    System.out.printf(
+        "[%s] Memory used: %d%n",
+        stageInfo, Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory());
+  }
+
+  public static void printReport(List<RocksDBTestTask.BenchmarkResult> results, String category) {
+    System.out.println(
+        String.format(
+            "\n\n#################################%s benchmark statistics#################################",
+            category));
+    System.out.println(String.format("%25s %15s %10s %15s", "", "success", "fail", "cost-in-ms"));
+    for (RocksDBTestTask.BenchmarkResult result : results) {
+      System.out.println(
+          String.format(
+              "%25s %15d %10d %15d",
+              result.name, result.successCount, result.failCount, result.costInMs));
+    }
+  }
+}
