2651c808bb2a0396a61f469c9e02d5398c6d8b98
zyk990424 <1534661820@qq.com>
2022-02-15 09:38:07 +0800

    Fix tag info sync error during metadata sync

--- server/src/test/java/org/apache/iotdb/db/metadata/MManagerBasicTest.java
+++ server/src/test/java/org/apache/iotdb/db/metadata/MManagerBasicTest.java
@@ -1416,4 +1416,37 @@ public class MManagerBasicTest {
     resultTag = results.get(0).getTag();
     assertEquals("newValue", resultTag.get("description"));
   }
+
+  @Test
+  public void testTagCreationViaMLogPlanDuringMetadataSync() throws Exception {
+    MManager manager = IoTDB.metaManager;
+
+    PartialPath path = new PartialPath("root.sg.d.s");
+    Map<String, String> tags = new HashMap<>();
+    tags.put("type", "test");
+    CreateTimeSeriesPlan plan =
+        new CreateTimeSeriesPlan(
+            path,
+            TSDataType.valueOf("INT32"),
+            TSEncoding.valueOf("RLE"),
+            compressionType,
+            null,
+            tags,
+            null,
+            null);
+    // mock that the plan has already been executed on sender and receiver will redo this plan
+    plan.setTagOffset(10);
+
+    manager.operation(plan);
+
+    ShowTimeSeriesPlan showTimeSeriesPlan =
+        new ShowTimeSeriesPlan(new PartialPath("root.sg.d.s"), true, "type", "test", 0, 0, false);
+    List<ShowTimeSeriesResult> results =
+        manager.showTimeseries(showTimeSeriesPlan, new QueryContext());
+    assertEquals(1, results.size());
+    Map<String, String> resultTag = results.get(0).getTag();
+    assertEquals("test", resultTag.get("type"));
+
+    assertEquals(0, ((MeasurementMNode) manager.getNodeByPath(path)).getOffset());
+  }
 }
