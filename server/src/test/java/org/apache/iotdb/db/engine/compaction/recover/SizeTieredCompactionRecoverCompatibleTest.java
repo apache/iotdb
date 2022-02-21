748ccc3f460222a87493f07b447343ffe30f7215
Liu Xuxin <liuxuxin@outlook.com>
2022-02-21 20:17:04 +0800

    finish SizeTieredCompactionRecoverCompatibleTest

--- server/src/test/java/org/apache/iotdb/db/engine/compaction/recover/SizeTieredCompactionRecoverCompatibleTest.java
+++ server/src/test/java/org/apache/iotdb/db/engine/compaction/recover/SizeTieredCompactionRecoverCompatibleTest.java
@@ -20,23 +20,138 @@
 package org.apache.iotdb.db.engine.compaction.recover;
 
 import org.apache.iotdb.db.engine.compaction.AbstractCompactionTest;
+import org.apache.iotdb.db.engine.compaction.inner.sizetiered.SizeTieredCompactionRecoverTask;
+import org.apache.iotdb.db.engine.compaction.inner.utils.InnerSpaceCompactionUtils;
+import org.apache.iotdb.db.engine.storagegroup.TsFileManager;
+import org.apache.iotdb.db.engine.storagegroup.TsFileNameGenerator;
+import org.apache.iotdb.db.engine.storagegroup.TsFileResource;
+import org.apache.iotdb.db.exception.StorageEngineException;
 import org.apache.iotdb.db.exception.metadata.MetadataException;
 import org.apache.iotdb.tsfile.exception.write.WriteProcessException;
 
+import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.util.concurrent.atomic.AtomicInteger;
 
+/** This test is used to test the compatibility of compaction recovery with 0.12. */
 public class SizeTieredCompactionRecoverCompatibleTest extends AbstractCompactionTest {
+  @Override
   @Before
   public void setUp() throws IOException, MetadataException, WriteProcessException {
     super.setUp();
   }
 
-  @Test
-  public void testCompatibleWithFilePath() {}
+  @Override
+  @After
+  public void tearDown() throws StorageEngineException, IOException {
+    super.tearDown();
+  }
 
   @Test
-  public void testCompatibleWithFileInfo() {}
+  public void testCompatibleWithFilePath() throws Exception {
+    createFiles(6, 2, 3, 100, 0, 0, 50, 50, false, true);
+    registerTimeseriesInMManger(2, 3, false);
+    TsFileResource targetResource =
+        TsFileNameGenerator.getInnerCompactionTargetFileResource(seqResources, true);
+    InnerSpaceCompactionUtils.compact(targetResource, seqResources);
+    RandomAccessFile targetFile = new RandomAccessFile(targetResource.getTsFile(), "rw");
+    long fileLength = targetFile.length();
+    targetFile.getChannel().truncate(fileLength - 20);
+    targetFile.close();
+
+    File logFile =
+        new File(
+            targetResource.getTsFile().getParent(),
+            targetResource.getTsFile().getName() + ".compaction.log");
+    BufferedWriter logWriter = new BufferedWriter(new FileWriter(logFile));
+    for (TsFileResource tsFileResource : seqResources) {
+      logWriter.write(
+          String.format(
+              "info-source\nroot.compactionTest 0 0 %s sequence\n",
+              tsFileResource.getTsFile().getName()));
+    }
+    logWriter.write("sequence\n");
+    logWriter.write(
+        String.format(
+            "info-target\nroot.compactionTest 0 0 %s sequence\n",
+            targetResource.getTsFile().getName()));
+    logWriter.close();
+
+    TsFileManager tsFileManager =
+        new TsFileManager("root.compactionTest", "0", targetResource.getTsFile().getParent());
+    tsFileManager.addAll(seqResources, true);
+    SizeTieredCompactionRecoverTask recoverTask =
+        new SizeTieredCompactionRecoverTask(
+            "root.compactionTest",
+            "0",
+            0,
+            logFile,
+            targetResource.getTsFile().getParent(),
+            true,
+            new AtomicInteger(0),
+            tsFileManager);
+    recoverTask.call();
+
+    for (TsFileResource resource : seqResources) {
+      Assert.assertTrue(resource.getTsFile().exists());
+    }
+
+    Assert.assertFalse(targetResource.getTsFile().exists());
+    Assert.assertFalse(logFile.exists());
+  }
+
+  @Test
+  public void testCompatibleWithFileInfo() throws Exception {
+    createFiles(6, 2, 3, 100, 0, 0, 50, 50, false, true);
+    registerTimeseriesInMManger(2, 3, false);
+    TsFileResource targetResource =
+        TsFileNameGenerator.getInnerCompactionTargetFileResource(seqResources, true);
+    InnerSpaceCompactionUtils.compact(targetResource, seqResources);
+    RandomAccessFile targetFile = new RandomAccessFile(targetResource.getTsFile(), "rw");
+    long fileLength = targetFile.length();
+    targetFile.getChannel().truncate(fileLength - 20);
+    targetFile.close();
+
+    File logFile =
+        new File(
+            targetResource.getTsFile().getParent(),
+            targetResource.getTsFile().getName() + ".compaction.log");
+    BufferedWriter logWriter = new BufferedWriter(new FileWriter(logFile));
+    for (TsFileResource tsFileResource : seqResources) {
+      logWriter.write(String.format("source\n%s\n", tsFileResource.getTsFile().getAbsolutePath()));
+    }
+    logWriter.write("sequence\n");
+    logWriter.write(String.format("target\n%s\n", targetResource.getTsFile().getAbsolutePath()));
+    logWriter.close();
+
+    TsFileManager tsFileManager =
+        new TsFileManager("root.compactionTest", "0", targetResource.getTsFile().getParent());
+    tsFileManager.addAll(seqResources, true);
+    SizeTieredCompactionRecoverTask recoverTask =
+        new SizeTieredCompactionRecoverTask(
+            "root.compactionTest",
+            "0",
+            0,
+            logFile,
+            targetResource.getTsFile().getParent(),
+            true,
+            new AtomicInteger(0),
+            tsFileManager);
+    recoverTask.call();
+
+    for (TsFileResource resource : seqResources) {
+      Assert.assertTrue(resource.getTsFile().exists());
+    }
+
+    Assert.assertFalse(targetResource.getTsFile().exists());
+    Assert.assertFalse(logFile.exists());
+  }
 }
