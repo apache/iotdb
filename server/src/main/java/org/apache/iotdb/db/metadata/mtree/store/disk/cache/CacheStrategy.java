f38a8d1346504f9437b6cf7c4f1869c30c33b761
zyk990424 <1534661820@qq.com>
2022-03-01 14:36:33 +0800

    add comments to CacheStrategy

--- server/src/main/java/org/apache/iotdb/db/metadata/mtree/store/disk/cache/CacheStrategy.java
+++ server/src/main/java/org/apache/iotdb/db/metadata/mtree/store/disk/cache/CacheStrategy.java
@@ -30,10 +30,40 @@ import java.util.concurrent.ConcurrentHashMap;
 import static org.apache.iotdb.db.metadata.mtree.store.disk.ICachedMNodeContainer.getBelongedContainer;
 import static org.apache.iotdb.db.metadata.mtree.store.disk.ICachedMNodeContainer.getCachedMNodeContainer;
 
+/**
+ * This class implemented the cache management, involving the cache status management on per MNode
+ * and cache eviction. All the nodes in memory are still basically organized as a trie via the
+ * container and parent reference in each node. Some extra data structure is used to help manage the
+ * node cached in memory.
+ *
+ * <p>The cache eviction on node is actually evicting a subtree from the MTree.
+ *
+ * <p>All the cached nodes are divided into two parts by their cache status, evictable nodes and
+ * none evictable nodes.
+ *
+ * <ol>
+ *   <li>Evictable nodes are all placed in nodeCache, which prepares the nodes be selected by cache
+ *       eviction.
+ *   <li>None evictable nodes takes two parts:
+ *       <ol>
+ *         <li>The volatile nodes, new added or updated, which means the data has not been synced to
+ *             disk.
+ *         <li>The ancestors of the volatile nodes. If a node is not volatile but not in nodeCache,
+ *             it means this node is an ancestor of some volatile node. Any volatile node is
+ *             contained in the CachedContainer of its parent. The parent will be placed in
+ *             nodeBuffer. If the parent is volatile as well, then the parent of the parent will be
+ *             placed in nodeBuffer instead, which means the root node of a maximum volatile subtree
+ *             will be placed in node buffer.
+ *       </ol>
+ * </ol>
+ */
 public class CacheStrategy implements ICacheStrategy {
 
+  // The nodes in nodeCache are all evictable and may be selected to be evicted during cache
+  // eviction.
   private volatile Map<CacheEntry, IMNode> nodeCache = new ConcurrentHashMap<>();
 
+  // The nodeBuffer helps to quickly locate the volatile subtree
   private volatile Map<CacheEntry, IMNode> nodeBuffer = new ConcurrentHashMap<>();
 
   @Override
@@ -41,6 +71,12 @@ public class CacheStrategy implements ICacheStrategy {
     return node.getCacheEntry() != null;
   }
 
+  /**
+   * Some cache status of the given node may be updated based on concrete cache strategy after being
+   * read in memory.
+   *
+   * @param node
+   */
   @Override
   public void updateCacheStatusAfterMemoryRead(IMNode node) {
     if (nodeCache.containsKey(node.getCacheEntry())) {
@@ -48,6 +84,12 @@ public class CacheStrategy implements ICacheStrategy {
     }
   }
 
+  /**
+   * The node read from disk should be cached and added to nodeCache and the cache of its belonging
+   * container.
+   *
+   * @param node
+   */
   @Override
   public void updateCacheStatusAfterDiskRead(IMNode node) {
     CacheEntry cacheEntry = node.getCacheEntry();
@@ -55,6 +97,12 @@ public class CacheStrategy implements ICacheStrategy {
     nodeCache.putIfAbsent(cacheEntry, node);
   }
 
+  /**
+   * The new appended node should be cached and the volatile subtree it belonged should be added to
+   * nodeBuffer.
+   *
+   * @param node
+   */
   @Override
   public void updateCacheStatusAfterAppend(IMNode node) {
     CacheEntry cacheEntry = node.getCacheEntry();
@@ -63,6 +111,12 @@ public class CacheStrategy implements ICacheStrategy {
     addNodeToBuffer(node);
   }
 
+  /**
+   * The updated node should be marked volatile and removed from nodeCache if necessary and the
+   * volatile subtree it belonged should be added to nodeBuffer.
+   *
+   * @param node
+   */
   @Override
   public void updateCacheStatusAfterUpdate(IMNode node) {
     CacheEntry cacheEntry = node.getCacheEntry();
@@ -76,6 +130,12 @@ public class CacheStrategy implements ICacheStrategy {
     }
   }
 
+  /**
+   * look for the first none volatile ancestor of this node and add it to nodeBuffer. Through this
+   * the volatile subtree the given node belong to will be record in nodeBuffer.
+   *
+   * @param node
+   */
   private void addNodeToBuffer(IMNode node) {
     IMNode parent = node.getParent();
     CacheEntry cacheEntry;
@@ -106,6 +166,13 @@ public class CacheStrategy implements ICacheStrategy {
     }
   }
 
+  /**
+   * After flush the given node's container to disk, the cache status of the nodes in container and
+   * their ancestors should be updated. All the node should be added to nodeCache and the nodeBuffer
+   * should be cleared after finishing a MTree flush task.
+   *
+   * @param node
+   */
   @Override
   public void updateCacheStatusAfterPersist(IMNode node) {
     IMNode tmp = node;
@@ -132,6 +199,12 @@ public class CacheStrategy implements ICacheStrategy {
     nodeCache.put(cacheEntry, node);
   }
 
+  /**
+   * Collect nodes in all volatile subtrees. All volatile nodes' parents will be collected. The
+   * ancestor will be in front of the descendent in returned list.
+   *
+   * @return
+   */
   @Override
   public List<IMNode> collectVolatileMNodes() {
     List<IMNode> nodesToPersist = new ArrayList<>();
@@ -185,6 +258,12 @@ public class CacheStrategy implements ICacheStrategy {
     }
   }
 
+  /**
+   * Choose an evictable node from nodeCache and evicted all the cached node in the subtree it
+   * represented.
+   *
+   * @return
+   */
   @Override
   public synchronized List<IMNode> evict() {
     IMNode node = null;
@@ -192,6 +271,7 @@ public class CacheStrategy implements ICacheStrategy {
     for (CacheEntry cacheEntry : nodeCache.keySet()) {
       if (!cacheEntry.isPinned()) {
         node = nodeCache.get(cacheEntry);
+        // the operation that may change the cache status of a node should be synchronized
         synchronized (node) {
           if (!cacheEntry.isPinned() && nodeCache.containsKey(cacheEntry)) {
             getBelongedContainer(node).evictMNode(node.getName());
@@ -218,6 +298,14 @@ public class CacheStrategy implements ICacheStrategy {
     }
   }
 
+  /**
+   * Pin a node in memory, and it will not be evicted. The pin is implemented as a multi mayer lock.
+   * When a thread/task pin the node, one mayer lock will be added to the node and its parent. This
+   * help guarantees the cache assumption that if a node is cached/pinned, its parent should be
+   * cached/pinned.
+   *
+   * @param node
+   */
   @Override
   public void pinMNode(IMNode node) {
     CacheEntry cacheEntry = node.getCacheEntry();
@@ -234,6 +322,14 @@ public class CacheStrategy implements ICacheStrategy {
     cacheEntry.pin();
   }
 
+  /**
+   * Unpin a node, and if the lock mayer on this node is zero, it will be evictable. Unpin a node
+   * means decrease one mayer lock from the node. If the lock mayer reaches zero, the mayer lock of
+   * its parent should decrease.
+   *
+   * @param node
+   * @return
+   */
   @Override
   public List<IMNode> unPinMNode(IMNode node) {
     List<IMNode> releasedMNodes = new ArrayList<>();
