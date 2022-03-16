9117d64f1f4ddac870a49ee9fd161c454e33c20e
Cpaulyz <chen3yz@gmail.com>
2022-03-16 17:34:09 +0800

    add receiver IT

--- integration/src/test/java/org/apache/iotdb/db/integration/sync/IoTDBSyncReceiverIT.java
+++ integration/src/test/java/org/apache/iotdb/db/integration/sync/IoTDBSyncReceiverIT.java
@@ -19,18 +19,37 @@
 package org.apache.iotdb.db.integration.sync;
 
 import org.apache.iotdb.db.conf.IoTDBDescriptor;
+import org.apache.iotdb.db.engine.modification.Deletion;
 import org.apache.iotdb.db.exception.sync.PipeServerException;
+import org.apache.iotdb.db.metadata.path.PartialPath;
+import org.apache.iotdb.db.newsync.pipedata.DeletionPipeData;
+import org.apache.iotdb.db.newsync.pipedata.PipeData;
+import org.apache.iotdb.db.newsync.pipedata.SchemaPipeData;
+import org.apache.iotdb.db.newsync.pipedata.TsFilePipeData;
 import org.apache.iotdb.db.newsync.receiver.ReceiverService;
 import org.apache.iotdb.db.newsync.receiver.manager.PipeStatus;
+import org.apache.iotdb.db.newsync.sender.pipe.Pipe;
+import org.apache.iotdb.db.newsync.sender.pipe.TsFilePipe;
+import org.apache.iotdb.db.newsync.transport.client.TransportClient;
+import org.apache.iotdb.db.qp.physical.PhysicalPlan;
+import org.apache.iotdb.db.qp.physical.sys.CreateAlignedTimeSeriesPlan;
+import org.apache.iotdb.db.qp.physical.sys.CreateTimeSeriesPlan;
+import org.apache.iotdb.db.qp.physical.sys.SetStorageGroupPlan;
 import org.apache.iotdb.db.qp.physical.sys.ShowPipeServerPlan;
 import org.apache.iotdb.db.qp.utils.DatetimeUtils;
 import org.apache.iotdb.db.utils.EnvironmentUtils;
 import org.apache.iotdb.itbase.category.LocalStandaloneTest;
 import org.apache.iotdb.service.transport.thrift.RequestType;
+import org.apache.iotdb.service.transport.thrift.ResponseType;
 import org.apache.iotdb.service.transport.thrift.SyncRequest;
+import org.apache.iotdb.service.transport.thrift.SyncResponse;
+import org.apache.iotdb.tsfile.file.metadata.enums.CompressionType;
+import org.apache.iotdb.tsfile.file.metadata.enums.TSDataType;
+import org.apache.iotdb.tsfile.file.metadata.enums.TSEncoding;
 import org.apache.iotdb.tsfile.read.common.Field;
 import org.apache.iotdb.tsfile.read.common.RowRecord;
 import org.apache.iotdb.tsfile.read.query.dataset.QueryDataSet;
+import org.apache.iotdb.tsfile.write.schema.MeasurementSchema;
 
 import org.apache.commons.io.FileUtils;
 import org.junit.After;
@@ -40,9 +59,9 @@ import org.junit.Test;
 import org.junit.experimental.categories.Category;
 
 import java.io.File;
-import java.net.ConnectException;
 import java.net.Socket;
-import java.net.UnknownHostException;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @Category({LocalStandaloneTest.class})
@@ -56,12 +75,14 @@ public class IoTDBSyncReceiverIT {
   protected static boolean enableCrossSpaceCompaction;
 
   String pipeName1 = "pipe1";
-  String remoteIp1 = "192.168.0.11";
+  String remoteIp1 = "127.0.0.1";
   long createdTime1 = System.currentTimeMillis();
 
   String pipeName2 = "pipe2";
   String remoteIp2 = "192.168.0.22";
   long createdTime2 = System.currentTimeMillis();
+
+  TransportClient client;
 
   @Before
   public void setUp() throws Exception {
@@ -78,13 +99,25 @@ public class IoTDBSyncReceiverIT {
     SyncTestUtil.insertData();
     EnvironmentUtils.shutdownDaemon();
     File srcDir = new File(IoTDBDescriptor.getInstance().getConfig().getDataDirs()[0]);
+    if (tmpDir.exists()) {
+      tmpDir.delete();
+    }
     FileUtils.moveDirectory(srcDir, tmpDir);
     EnvironmentUtils.cleanEnv();
     EnvironmentUtils.envSetUp();
+    try {
+      ReceiverService.getInstance().startPipeServer();
+      new Socket("localhost", 5555);
+    } catch (Exception e) {
+      Assert.fail("Failed to start pipe server because " + e.getMessage());
+    }
+    Pipe pipe = new TsFilePipe(createdTime1, pipeName1, null, 0, false);
+    client = new TransportClient(pipe, "127.0.0.1", 5555);
   }
 
   @After
   public void tearDown() throws Exception {
+    client.close();
     IoTDBDescriptor.getInstance().getConfig().setEnableSeqSpaceCompaction(enableSeqSpaceCompaction);
     IoTDBDescriptor.getInstance()
         .getConfig()
@@ -96,41 +129,11 @@ public class IoTDBSyncReceiverIT {
     EnvironmentUtils.cleanEnv();
   }
 
-  /** normal case */
-  @Test
-  public void testStartAndStopPipeServer1() {
-    try {
-      ReceiverService.getInstance().startPipeServer();
-      try {
-        new Socket("localhost", 5555);
-      } catch (UnknownHostException | ConnectException e) {
-        Assert.fail(e.getMessage());
-      }
-      ReceiverService.getInstance().stopPipeServer();
-      try {
-        new Socket("localhost", 5555);
-        Assert.fail("Pipe Server is still running.");
-      } catch (UnknownHostException | ConnectException e) {
-        // nothing
-      }
-    } catch (Exception e) {
-      e.printStackTrace();
-      Assert.fail(e.getMessage());
-    }
-  }
-
   /** cannot stop */
   @Test
-  public void testStartAndStopPipeServer2() {
-    try {
-      ReceiverService.getInstance().startPipeServer();
-      new Socket("localhost", 5555);
-      ReceiverService.getInstance()
-          .recMsg(new SyncRequest(RequestType.CREATE, pipeName1, remoteIp1, createdTime1));
-    } catch (Exception e) {
-      e.printStackTrace();
-      Assert.fail(e.getMessage());
-    }
+  public void testStopPipeServerCheck() throws Exception {
+    ReceiverService.getInstance()
+        .recMsg(new SyncRequest(RequestType.CREATE, pipeName1, remoteIp1, createdTime1));
     try {
       ReceiverService.getInstance().stopPipeServer();
       Assert.fail("Should not stop pipe server");
@@ -140,9 +143,8 @@ public class IoTDBSyncReceiverIT {
   }
 
   @Test
-  public void testShowPipe() {
+  public void testPipeOperation() {
     try {
-      ReceiverService.getInstance().startPipeServer();
       ReceiverService.getInstance()
           .recMsg(new SyncRequest(RequestType.CREATE, pipeName1, remoteIp1, createdTime1));
       ReceiverService.getInstance()
@@ -179,6 +181,30 @@ public class IoTDBSyncReceiverIT {
       Assert.assertEquals(
           DatetimeUtils.convertLongToDate(createdTime2), fields.get(3).getStringValue());
       Assert.assertFalse(pipe2DataSet.hasNext());
+
+      ReceiverService.getInstance()
+          .recMsg(new SyncRequest(RequestType.DROP, pipeName1, remoteIp1, createdTime1));
+      ReceiverService.getInstance()
+          .recMsg(new SyncRequest(RequestType.START, pipeName2, remoteIp2, createdTime2));
+      allDataSet = ReceiverService.getInstance().showPipe(new ShowPipeServerPlan(""));
+      while (allDataSet.hasNext()) {
+        rowRecord = allDataSet.next();
+        fields = rowRecord.getFields();
+        Assert.assertEquals(4, fields.size());
+        if (fields.get(0).getStringValue().equals(pipeName1)) {
+          Assert.assertEquals(pipeName1, fields.get(0).getStringValue());
+          Assert.assertEquals(remoteIp1, fields.get(1).getStringValue());
+          Assert.assertEquals(PipeStatus.DROP.name(), fields.get(2).getStringValue());
+          Assert.assertEquals(
+              DatetimeUtils.convertLongToDate(createdTime1), fields.get(3).getStringValue());
+        } else {
+          Assert.assertEquals(pipeName2, fields.get(0).getStringValue());
+          Assert.assertEquals(remoteIp2, fields.get(1).getStringValue());
+          Assert.assertEquals(PipeStatus.RUNNING.name(), fields.get(2).getStringValue());
+          Assert.assertEquals(
+              DatetimeUtils.convertLongToDate(createdTime2), fields.get(3).getStringValue());
+        }
+      }
     } catch (Exception e) {
       e.printStackTrace();
       Assert.fail(e.getMessage());
@@ -186,17 +212,142 @@ public class IoTDBSyncReceiverIT {
   }
 
   @Test
-  public void testPipeOperation(){
+  public void testReceiveDataAndLoad() {
+    try {
+      // 1. create pipe
+      ReceiverService.getInstance()
+          .recMsg(new SyncRequest(RequestType.CREATE, pipeName1, remoteIp1, createdTime1));
 
-  }
+      // 2. send pipe data
+      int serialNum = 0;
+      List<PhysicalPlan> planList = new ArrayList<>();
+      planList.add(new SetStorageGroupPlan(new PartialPath("root.vehicle")));
+      planList.add(
+          new CreateTimeSeriesPlan(
+              new PartialPath("root.vehicle.d0.s0"),
+              new MeasurementSchema("s0", TSDataType.INT32, TSEncoding.RLE)));
+      planList.add(
+          new CreateTimeSeriesPlan(
+              new PartialPath("root.vehicle.d0.s1"),
+              new MeasurementSchema("s1", TSDataType.TEXT, TSEncoding.PLAIN)));
+      planList.add(
+          new CreateTimeSeriesPlan(
+              new PartialPath("root.vehicle.d1.s2"),
+              new MeasurementSchema("s2", TSDataType.FLOAT, TSEncoding.RLE)));
+      planList.add(
+          new CreateTimeSeriesPlan(
+              new PartialPath("root.vehicle.d1.s3"),
+              new MeasurementSchema("s3", TSDataType.BOOLEAN, TSEncoding.PLAIN)));
+      planList.add(new SetStorageGroupPlan(new PartialPath("root.sg1")));
+      planList.add(
+          new CreateAlignedTimeSeriesPlan(
+              new PartialPath("root.sg1.d1"),
+              Arrays.asList("s1", "s2", "s3", "s4", "s5"),
+              Arrays.asList(
+                  TSDataType.FLOAT,
+                  TSDataType.INT32,
+                  TSDataType.INT64,
+                  TSDataType.BOOLEAN,
+                  TSDataType.TEXT),
+              Arrays.asList(
+                  TSEncoding.RLE,
+                  TSEncoding.GORILLA,
+                  TSEncoding.RLE,
+                  TSEncoding.RLE,
+                  TSEncoding.PLAIN),
+              Arrays.asList(
+                  CompressionType.SNAPPY,
+                  CompressionType.SNAPPY,
+                  CompressionType.SNAPPY,
+                  CompressionType.SNAPPY,
+                  CompressionType.SNAPPY),
+              null));
+      planList.add(new SetStorageGroupPlan(new PartialPath("root.sg1")));
+      for (PhysicalPlan plan : planList) {
+        client.senderTransport(new SchemaPipeData(plan, serialNum++));
+      }
+      List<File> tsFiles = SyncTestUtil.getTsFilePaths(tmpDir);
+      for (File f : tsFiles) {
+        client.senderTransport(new TsFilePipeData(f.getPath(), serialNum++));
+      }
+      Deletion deletion = new Deletion(new PartialPath("root.vehicle.**"), 0, 33, 38);
+      PipeData pipeData = new DeletionPipeData(deletion, serialNum++);
+      client.senderTransport(pipeData);
 
-  @Test
-  public void testReceiveDataAndLoad(){
+      // wait collector to load pipe data
+      Thread.sleep(1000);
 
-  }
+      // 3. check result
+      String sql1 = "select * from root.vehicle.*";
+      String[] retArray1 =
+          new String[] {
+            "6,120,null,null,null",
+            "9,null,123,null,null",
+            "16,128,null,null,16.0",
+            "18,189,198,true,18.0",
+            "20,null,null,false,null",
+            "29,null,null,true,1205.0",
+            "99,null,1234,null,null"
+          };
+      String[] columnNames1 = {
+        "root.vehicle.d0.s0", "root.vehicle.d0.s1", "root.vehicle.d1.s3", "root.vehicle.d1.s2"
+      };
+      SyncTestUtil.checkResult(sql1, columnNames1, retArray1);
+      String sql2 = "select * from root.sg1.d1";
+      String[] retArray2 =
+          new String[] {
+            "1,1.0,1,null,true,aligned_test1",
+            "2,2.0,2,null,null,aligned_test2",
+            "3,3.0,null,null,false,aligned_test3",
+            "4,4.0,4,null,true,aligned_test4",
+            "5,130000.0,130000,130000,false,aligned_unseq_test1",
+            "6,6.0,6,6,true,null",
+            "7,7.0,7,7,false,aligned_test7",
+            "8,8.0,8,8,null,aligned_test8",
+            "9,9.0,9,9,false,aligned_test9",
+          };
+      String[] columnNames2 = {
+        "root.sg1.d1.s1", "root.sg1.d1.s2", "root.sg1.d1.s3", "root.sg1.d1.s4", "root.sg1.d1.s5",
+      };
+      SyncTestUtil.checkResult(sql2, columnNames2, retArray2);
 
-  @Test
-  public void testHeartbeat(){
+      // 4. stop pipe
+      ReceiverService.getInstance()
+          .recMsg(new SyncRequest(RequestType.STOP, pipeName1, remoteIp1, createdTime1));
+      client.senderTransport(
+          new DeletionPipeData(
+              new Deletion(new PartialPath("root.vehicle.**"), 0, 0, 99), serialNum++));
+      Thread.sleep(1000);
+      SyncTestUtil.checkResult(sql1, columnNames1, retArray1);
+      // check heartbeat
+      SyncResponse response1 =
+          ReceiverService.getInstance()
+              .recMsg(new SyncRequest(RequestType.HEARTBEAT, pipeName1, remoteIp1, createdTime1));
+      Assert.assertEquals(ResponseType.WARN, response1.type);
 
+      // 5. restart pipe
+      ReceiverService.getInstance()
+          .recMsg(new SyncRequest(RequestType.START, pipeName1, remoteIp1, createdTime1));
+      Thread.sleep(1000);
+      SyncTestUtil.checkResult(sql1, columnNames1, new String[] {});
+      // check heartbeat
+      SyncResponse response2 =
+          ReceiverService.getInstance()
+              .recMsg(new SyncRequest(RequestType.HEARTBEAT, pipeName1, remoteIp1, createdTime1));
+      Assert.assertEquals(ResponseType.INFO, response2.type);
+
+      // 6. drop pipe
+      ReceiverService.getInstance()
+          .recMsg(new SyncRequest(RequestType.DROP, pipeName1, remoteIp1, createdTime1));
+      client.senderTransport(
+          new DeletionPipeData(
+              new Deletion(new PartialPath("root.sg1.**"), 0, 0, 99), serialNum++));
+      Thread.sleep(1000);
+      SyncTestUtil.checkResult(sql2, columnNames2, retArray2);
+
+    } catch (Exception e) {
+      e.printStackTrace();
+      Assert.fail(e.getMessage());
+    }
   }
 }
