727c3e0dff29e236d2a26d678b8a91499847f0ed
JackieTien97 <jackietien97@gmail.com>
2022-03-21 11:58:01 +0800

    add write node and metadata node

---
+++ server/src/main/java/org/apache/iotdb/db/mpp/sql/planner/plan/node/write/InsertNode.java
@@ -0,0 +1,66 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.iotdb.db.mpp.sql.planner.plan.node.write;
+
+import org.apache.iotdb.db.metadata.idtable.entry.IDeviceID;
+import org.apache.iotdb.db.metadata.mnode.IMeasurementMNode;
+import org.apache.iotdb.db.metadata.path.PartialPath;
+import org.apache.iotdb.db.mpp.common.Analysis;
+import org.apache.iotdb.db.mpp.sql.planner.plan.node.PlanNode;
+import org.apache.iotdb.db.mpp.sql.planner.plan.node.PlanNodeId;
+import org.apache.iotdb.tsfile.file.metadata.enums.TSDataType;
+
+import java.util.List;
+
+public abstract class InsertNode extends PlanNode {
+
+  /**
+   * if use id table, this filed is id form of device path <br>
+   * if not, this filed is device path<br>
+   */
+  protected PartialPath devicePath;
+
+  protected boolean isAligned;
+  protected String[] measurements;
+  // get from client
+  protected TSDataType[] dataTypes;
+  // get from SchemaEngine
+  protected IMeasurementMNode[] measurementMNodes;
+
+  /**
+   * device id reference, for reuse device id in both id table and memtable <br>
+   * used in memtable
+   */
+  protected IDeviceID deviceID;
+
+  // record the failed measurements, their reasons, and positions in "measurements"
+  List<String> failedMeasurements;
+  private List<Exception> failedExceptions;
+  List<Integer> failedIndices;
+
+  protected InsertNode(PlanNodeId id) {
+    super(id);
+  }
+
+  public abstract List<InsertNode> splitByPartition(Analysis analysis);
+
+  public boolean needSplit() {
+    return true;
+  }
+}
