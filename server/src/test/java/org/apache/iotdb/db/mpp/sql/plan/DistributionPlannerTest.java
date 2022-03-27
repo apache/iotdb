d8d2989c9da2ed780ceee4386ba5b760273d38de
liuminghui233 <549208791@qq.com>
2022-03-27 22:35:50 +0800

    fix compile error

--- server/src/test/java/org/apache/iotdb/db/mpp/sql/plan/DistributionPlannerTest.java
+++ server/src/test/java/org/apache/iotdb/db/mpp/sql/plan/DistributionPlannerTest.java
@@ -26,7 +26,6 @@ import org.apache.iotdb.commons.partition.DeviceGroupId;
 import org.apache.iotdb.commons.partition.TimePartitionId;
 import org.apache.iotdb.db.exception.metadata.IllegalPathException;
 import org.apache.iotdb.db.metadata.path.PartialPath;
-import org.apache.iotdb.db.mpp.common.FilterNullPolicy;
 import org.apache.iotdb.db.mpp.common.MPPQueryContext;
 import org.apache.iotdb.db.mpp.common.QueryId;
 import org.apache.iotdb.db.mpp.sql.analyze.Analysis;
