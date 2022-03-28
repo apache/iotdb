ad2ada5f87cf8b881636cb9b1d2d6b72cd43ef8d
CRZbulabula <33111881+CRZbulabula@users.noreply.github.com>
2022-03-28 15:26:10 +0800

    move ConsensusType

--- consensus/src/main/java/org/apache/iotdb/consensus/common/ConsensusType.java
+++ consensus/src/main/java/org/apache/iotdb/consensus/common/ConsensusType.java
@@ -16,7 +16,7 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-package org.apache.iotdb.confignode.consensus;
+package org.apache.iotdb.consensus.common;
 
 import java.util.Arrays;
 
