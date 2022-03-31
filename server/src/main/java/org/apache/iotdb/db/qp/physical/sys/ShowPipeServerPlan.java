786eecbd9495a6a5d3f7ccfb1b08894a6ceb7172
Chen YZ <43774645+Cpaulyz@users.noreply.github.com>
2022-03-31 21:32:48 +0800

    [To new_sync][IOTDB-2830] SQL: SHOW PIPE and SHOW PIPESERVER STATUS (#5387)

--- server/src/main/java/org/apache/iotdb/db/qp/physical/sys/ShowPipeServerPlan.java
+++ server/src/main/java/org/apache/iotdb/db/qp/physical/sys/ShowPipeServerPlan.java
@@ -20,22 +20,7 @@ package org.apache.iotdb.db.qp.physical.sys;
 
 public class ShowPipeServerPlan extends ShowPlan {
 
-  private String pipeName;
-
-  public ShowPipeServerPlan(String pipeName) {
-    super(ShowContentType.PIPESERVER);
-    this.pipeName = pipeName;
-  }
-
   public ShowPipeServerPlan() {
     super(ShowContentType.PIPESERVER);
-  }
-
-  public String getPipeName() {
-    return pipeName;
-  }
-
-  public void setPipeName(String pipeName) {
-    this.pipeName = pipeName;
   }
 }
