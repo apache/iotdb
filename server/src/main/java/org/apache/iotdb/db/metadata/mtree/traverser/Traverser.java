55566b6eb6cc19640af622193f29649683224977
Marcos_Zyk <38524330+MarcosZyk@users.noreply.github.com>
2022-04-02 10:53:54 +0800

    refactor count node by level (#5398)

--- server/src/main/java/org/apache/iotdb/db/metadata/mtree/traverser/Traverser.java
+++ server/src/main/java/org/apache/iotdb/db/metadata/mtree/traverser/Traverser.java
@@ -78,8 +78,8 @@ public abstract class Traverser {
     }
     this.startNode = startNode;
     this.nodes = nodes;
-    initStartIndexAndLevel(path);
     this.traverseContext = new ArrayDeque<>();
+    initStartIndexAndLevel(path);
   }
 
   /**
@@ -94,6 +94,8 @@ public abstract class Traverser {
     startLevel = 0;
     while (parent != null) {
       startLevel++;
+      traverseContext.addLast(parent);
+
       ancestors.push(parent);
       parent = parent.getParent();
     }
@@ -335,11 +337,7 @@ public abstract class Traverser {
       nodeNames.add(nodes.next().getName());
     }
 
-    if (nodeNames.isEmpty()) {
-      nodeNames.addAll(Arrays.asList(currentNode.getPartialPath().getNodes()));
-    } else {
-      nodeNames.add(currentNode.getName());
-    }
+    nodeNames.add(currentNode.getName());
 
     return nodeNames.toArray(new String[0]);
   }
