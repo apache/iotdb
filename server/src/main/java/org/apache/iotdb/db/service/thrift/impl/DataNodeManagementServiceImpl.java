79634b013cab8572c750c3d74b6f0670f157c445
MrQuansy <1051622353@qq.com>
2022-04-13 17:43:00 +0800

    create data region

--- server/src/main/java/org/apache/iotdb/db/service/thrift/impl/DataNodeManagementServiceImpl.java
+++ server/src/main/java/org/apache/iotdb/db/service/thrift/impl/DataNodeManagementServiceImpl.java
@@ -24,17 +24,19 @@ import org.apache.iotdb.common.rpc.thrift.TRegionReplicaSet;
 import org.apache.iotdb.common.rpc.thrift.TSStatus;
 import org.apache.iotdb.commons.cluster.Endpoint;
 import org.apache.iotdb.commons.consensus.ConsensusGroupId;
+import org.apache.iotdb.commons.consensus.DataRegionId;
 import org.apache.iotdb.commons.consensus.SchemaRegionId;
 import org.apache.iotdb.consensus.IConsensus;
 import org.apache.iotdb.consensus.common.Peer;
 import org.apache.iotdb.consensus.common.response.ConsensusGenericResponse;
 import org.apache.iotdb.db.consensus.ConsensusImpl;
+import org.apache.iotdb.db.engine.StorageEngineV2;
+import org.apache.iotdb.db.exception.DataRegionException;
 import org.apache.iotdb.db.exception.metadata.IllegalPathException;
 import org.apache.iotdb.db.exception.metadata.MetadataException;
 import org.apache.iotdb.db.metadata.path.PartialPath;
 import org.apache.iotdb.db.metadata.schemaregion.SchemaEngine;
 import org.apache.iotdb.rpc.TSStatusCode;
-import org.apache.iotdb.service.rpc.thrift.CreateDataPartitionReq;
 import org.apache.iotdb.service.rpc.thrift.CreateDataRegionReq;
 import org.apache.iotdb.service.rpc.thrift.CreateSchemaRegionReq;
 import org.apache.iotdb.service.rpc.thrift.ManagementIService;
@@ -52,8 +54,9 @@ import java.util.List;
 
 public class DataNodeManagementServiceImpl implements ManagementIService.Iface {
   private static final Logger LOGGER = LoggerFactory.getLogger(DataNodeManagementServiceImpl.class);
-  private SchemaEngine schemaEngine = SchemaEngine.getInstance();
-  private IConsensus consensusImpl = ConsensusImpl.getInstance();
+  private final SchemaEngine schemaEngine = SchemaEngine.getInstance();
+  private final StorageEngineV2 storageEngine = StorageEngineV2.getInstance();
+  private final IConsensus consensusImpl = ConsensusImpl.getInstance();
 
   @Override
   public TSStatus createSchemaRegion(CreateSchemaRegionReq req) throws TException {
@@ -101,12 +104,40 @@ public class DataNodeManagementServiceImpl implements ManagementIService.Iface {
 
   @Override
   public TSStatus createDataRegion(CreateDataRegionReq req) throws TException {
-    return null;
-  }
-
-  @Override
-  public TSStatus createDataPartition(CreateDataPartitionReq req) throws TException {
-    return null;
+    TSStatus tsStatus;
+    try {
+      TRegionReplicaSet regionReplicaSet = req.getRegionReplicaSet();
+      DataRegionId dataRegionId =
+          (DataRegionId)
+              ConsensusGroupId.Factory.create(ByteBuffer.wrap(regionReplicaSet.getRegionId()));
+      storageEngine.createDataRegion(dataRegionId, req.storageGroup, req.ttl);
+      ConsensusGroupId consensusGroupId =
+          ConsensusGroupId.Factory.create(ByteBuffer.wrap(regionReplicaSet.getRegionId()));
+      List<Peer> peers = new ArrayList<>();
+      for (EndPoint endPoint : regionReplicaSet.getEndpoint()) {
+        Endpoint endpoint = new Endpoint(endPoint.getIp(), endPoint.getPort());
+        peers.add(new Peer(consensusGroupId, endpoint));
+      }
+      ConsensusGenericResponse consensusGenericResponse =
+          consensusImpl.addConsensusGroup(consensusGroupId, peers);
+      if (consensusGenericResponse.isSuccess()) {
+        tsStatus = new TSStatus(TSStatusCode.SUCCESS_STATUS.getStatusCode());
+      } else {
+        tsStatus = new TSStatus(TSStatusCode.INTERNAL_SERVER_ERROR.getStatusCode());
+        tsStatus.setMessage(consensusGenericResponse.getException().getMessage());
+      }
+    } catch (DataRegionException e1) {
+      LOGGER.error(
+          "Create Data Region {} failed because {}", req.getStorageGroup(), e1.getMessage());
+      tsStatus = new TSStatus(TSStatusCode.INTERNAL_SERVER_ERROR.getStatusCode());
+      tsStatus.setMessage(
+          String.format("Create Data Region failed because of %s", e1.getMessage()));
+    } catch (IOException e2) {
+      LOGGER.error("Can't deserialize regionId", e2);
+      tsStatus = new TSStatus(TSStatusCode.INTERNAL_SERVER_ERROR.getStatusCode());
+      tsStatus.setMessage(String.format("Can't deserialize regionId %s", e2));
+    }
+    return tsStatus;
   }
 
   @Override
@@ -120,6 +151,4 @@ public class DataNodeManagementServiceImpl implements ManagementIService.Iface {
   }
 
   public void handleClientExit() {}
-
-  // TODO: add Mpp interface
 }
