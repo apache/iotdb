4f95ef9f4166e301dc91368b27064c57ca2f49f3
Cpaulyz <chen3yz@gmail.com>
2022-04-13 21:51:01 +0800

    revert some code

--- server/src/main/java/org/apache/iotdb/db/exception/DataRegionException.java
+++ server/src/main/java/org/apache/iotdb/db/exception/DataRegionException.java
@@ -15,9 +15,7 @@
  * KIND, either express or implied.  See the License for the
  * specific language governing permissions and limitations
  * under the License.
- *
  */
-
 package org.apache.iotdb.db.exception;
 
 import org.apache.iotdb.commons.exception.IoTDBException;
