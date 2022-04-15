f2bc52c8d54e9cff04fe7bdced7d63a455d2253d
Cpaulyz <chen3yz@gmail.com>
2022-04-15 10:23:43 +0800

    no use *

--- server/src/main/java/org/apache/iotdb/db/sync/receiver/manager/ReceiverManager.java
+++ server/src/main/java/org/apache/iotdb/db/sync/receiver/manager/ReceiverManager.java
@@ -28,7 +28,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class ReceiverManager {
 
