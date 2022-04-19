4d34f363af5c7b5ab86fa8f95e98d1af3911fa71
ZhaoXin <x_zhao16@163.com>
2022-04-19 20:35:25 +0800

    [IOTDB-2925]Add Schema File sketch tool (#5570)

--- server/src/main/java/org/apache/iotdb/db/tools/schema/MLogParser.java
+++ server/src/main/java/org/apache/iotdb/db/tools/schema/MLogParser.java
@@ -16,7 +16,7 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-package org.apache.iotdb.db.tools.mlog;
+package org.apache.iotdb.db.tools.schema;
 
 import org.apache.iotdb.db.metadata.logfile.MLogReader;
 import org.apache.iotdb.db.metadata.logfile.MLogTxtWriter;
