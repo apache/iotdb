07661f1ec08f83ac8d08f664a5c893cf8042ce14
lisijia <lisijia@360.cn>
2022-04-22 09:59:14 +0800

    update test

--- integration/src/test/java/org/apache/iotdb/db/integration/IoTDBTtlIT.java
+++ integration/src/test/java/org/apache/iotdb/db/integration/IoTDBTtlIT.java
@@ -60,12 +60,12 @@ public class IoTDBTtlIT {
       try {
         statement.execute("SET TTL TO root.TTL_SG1 1000");
       } catch (SQLException e) {
-        assertEquals(TSStatusCode.TIMESERIES_NOT_EXIST.getStatusCode(), e.getErrorCode());
+        assertEquals(TSStatusCode.STORAGE_GROUP_NOT_EXIST.getStatusCode(), e.getErrorCode());
       }
       try {
         statement.execute("UNSET TTL TO root.TTL_SG1");
       } catch (SQLException e) {
-        assertEquals(TSStatusCode.TIMESERIES_NOT_EXIST.getStatusCode(), e.getErrorCode());
+        assertEquals(TSStatusCode.STORAGE_GROUP_NOT_EXIST.getStatusCode(), e.getErrorCode());
       }
 
       statement.execute("SET STORAGE GROUP TO root.TTL_SG1");
