0da10c50ae9941787da4b2fe59d023d267130238
ericpai <ericpai94@hotmail.com>
2022-04-24 16:44:40 +0800

    [IOTDB-2996] Fix wildcard import in test folders

--- cross-tests/src/test/java/org/apache/iotdb/cross/tests/tools/importCsv/AbstractScript.java
+++ cross-tests/src/test/java/org/apache/iotdb/cross/tests/tools/importCsv/AbstractScript.java
@@ -23,7 +23,12 @@ import org.apache.commons.csv.CSVParser;
 import org.apache.commons.lang3.ArrayUtils;
 import org.apache.thrift.annotation.Nullable;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
