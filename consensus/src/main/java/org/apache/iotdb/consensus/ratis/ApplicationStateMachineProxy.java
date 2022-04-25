94bded40942d6d7bef28f0c8af7120fcb6c53015
SzyWilliam <48054931+SzyWilliam@users.noreply.github.com>
2022-04-25 23:08:07 +0800

    [IOTDB-2968] RatisConsensus snapshot implementation (#5623)
    * ratis snapshot impl
    
    * format
    
    * close resource
    
    add warning
    
    add comments
    
    * fix reviews
    
    * add snapshot UT
    
    * add license
    
    * fix
    
    * fix
    
    * fix reviews
    
    * fix reviews
    
    * fix reviews

--- consensus/src/main/java/org/apache/iotdb/consensus/ratis/ApplicationStateMachineProxy.java
+++ consensus/src/main/java/org/apache/iotdb/consensus/ratis/ApplicationStateMachineProxy.java
@@ -20,32 +20,75 @@ package org.apache.iotdb.consensus.ratis;
 
 import org.apache.iotdb.common.rpc.thrift.TSStatus;
 import org.apache.iotdb.consensus.common.DataSet;
+import org.apache.iotdb.consensus.common.SnapshotMeta;
 import org.apache.iotdb.consensus.common.request.ByteBufferConsensusRequest;
 import org.apache.iotdb.consensus.common.request.IConsensusRequest;
 import org.apache.iotdb.consensus.statemachine.IStateMachine;
 
 import org.apache.ratis.proto.RaftProtos;
 import org.apache.ratis.protocol.Message;
+import org.apache.ratis.protocol.RaftGroupId;
+import org.apache.ratis.server.RaftServer;
+import org.apache.ratis.server.protocol.TermIndex;
+import org.apache.ratis.server.raftlog.RaftLog;
+import org.apache.ratis.server.storage.RaftStorage;
+import org.apache.ratis.statemachine.StateMachineStorage;
 import org.apache.ratis.statemachine.TransactionContext;
 import org.apache.ratis.statemachine.impl.BaseStateMachine;
+import org.apache.ratis.util.LifeCycle;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.File;
 import java.io.IOException;
+import java.nio.ByteBuffer;
 import java.util.concurrent.CompletableFuture;
 
 public class ApplicationStateMachineProxy extends BaseStateMachine {
-  private final IStateMachine applicationStateMachine;
   private final Logger logger = LoggerFactory.getLogger(ApplicationStateMachineProxy.class);
+  private final IStateMachine applicationStateMachine;
+
+  // Raft Storage sub dir for statemachine data, default (_sm)
+  private File statemachineDir;
+  private final SnapshotStorage snapshotStorage;
 
   public ApplicationStateMachineProxy(IStateMachine stateMachine) {
     applicationStateMachine = stateMachine;
+    snapshotStorage = new SnapshotStorage(applicationStateMachine);
     applicationStateMachine.start();
   }
 
   @Override
+  public void initialize(RaftServer raftServer, RaftGroupId raftGroupId, RaftStorage storage)
+      throws IOException {
+    getLifeCycle()
+        .startAndTransition(
+            () -> {
+              snapshotStorage.init(storage);
+              this.statemachineDir = snapshotStorage.getStateMachineDir();
+              loadSnapshot(applicationStateMachine.getLatestSnapshot(statemachineDir));
+            });
+  }
+
+  @Override
+  public void reinitialize() throws IOException {
+    setLastAppliedTermIndex(null);
+    loadSnapshot(applicationStateMachine.getLatestSnapshot(statemachineDir));
+    if (getLifeCycleState() == LifeCycle.State.PAUSED) {
+      getLifeCycle().transition(LifeCycle.State.STARTING);
+      getLifeCycle().transition(LifeCycle.State.RUNNING);
+    }
+  }
+
+  @Override
+  public void pause() {
+    getLifeCycle().transition(LifeCycle.State.PAUSING);
+    getLifeCycle().transition(LifeCycle.State.PAUSED);
+  }
+
+  @Override
   public void close() throws IOException {
-    applicationStateMachine.stop();
+    getLifeCycle().checkStateAndClose(applicationStateMachine::stop);
   }
 
   @Override
@@ -83,5 +126,39 @@ public class ApplicationStateMachineProxy extends BaseStateMachine {
     RequestMessage requestMessage = (RequestMessage) request;
     DataSet result = applicationStateMachine.read(requestMessage.getActualRequest());
     return CompletableFuture.completedFuture(new ResponseMessage(result));
+  }
+
+  @Override
+  public long takeSnapshot() throws IOException {
+    final TermIndex lastApplied = getLastAppliedTermIndex();
+    if (lastApplied.getTerm() <= 0 || lastApplied.getIndex() <= 0) {
+      return RaftLog.INVALID_LOG_INDEX;
+    }
+
+    // require the application statemachine to take the latest snapshot
+    ByteBuffer metadata = Utils.getMetadataFromTermIndex(lastApplied);
+    boolean success = applicationStateMachine.takeSnapshot(metadata, statemachineDir);
+    if (!success) {
+      return RaftLog.INVALID_LOG_INDEX;
+    }
+
+    return lastApplied.getIndex();
+  }
+
+  private void loadSnapshot(SnapshotMeta snapshot) {
+    if (snapshot == null) {
+      return;
+    }
+
+    // require the application statemachine to load the latest snapshot
+    applicationStateMachine.loadSnapshot(snapshot);
+    ByteBuffer metadata = snapshot.getMetadata();
+    TermIndex snapshotTermIndex = Utils.getTermIndexFromMetadata(metadata);
+    updateLastAppliedTermIndex(snapshotTermIndex.getTerm(), snapshotTermIndex.getIndex());
+  }
+
+  @Override
+  public StateMachineStorage getStateMachineStorage() {
+    return snapshotStorage;
   }
 }
