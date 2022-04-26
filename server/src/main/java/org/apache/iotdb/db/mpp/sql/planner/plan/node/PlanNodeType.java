648212e1c4d91896a1a2bc46cadae76086e2219e
任宇华 <79885238+RYH61@users.noreply.github.com>
2022-04-26 11:54:38 +0800

    [IOTDB-3005] Increase read and write permissions in ConfigExecution (#5664)

--- server/src/main/java/org/apache/iotdb/db/mpp/sql/planner/plan/node/PlanNodeType.java
+++ server/src/main/java/org/apache/iotdb/db/mpp/sql/planner/plan/node/PlanNodeType.java
@@ -24,7 +24,6 @@ import org.apache.iotdb.db.mpp.sql.planner.plan.node.metedata.read.SchemaFetchNo
 import org.apache.iotdb.db.mpp.sql.planner.plan.node.metedata.read.SchemaMergeNode;
 import org.apache.iotdb.db.mpp.sql.planner.plan.node.metedata.read.TimeSeriesSchemaScanNode;
 import org.apache.iotdb.db.mpp.sql.planner.plan.node.metedata.write.AlterTimeSeriesNode;
-import org.apache.iotdb.db.mpp.sql.planner.plan.node.metedata.write.AuthorNode;
 import org.apache.iotdb.db.mpp.sql.planner.plan.node.metedata.write.CreateAlignedTimeSeriesNode;
 import org.apache.iotdb.db.mpp.sql.planner.plan.node.metedata.write.CreateTimeSeriesNode;
 import org.apache.iotdb.db.mpp.sql.planner.plan.node.process.AggregateNode;
@@ -73,13 +72,12 @@ public enum PlanNodeType {
   DEVICES_SCHEMA_SCAN((short) 18),
   CREATE_TIME_SERIES((short) 19),
   EXCHANGE((short) 20),
-  AUTHOR((short) 21),
-  ALTER_TIME_SERIES((short) 22),
-  CREATE_ALIGNED_TIME_SERIES((short) 23),
-  TIME_SERIES_SCHEMA_SCAN((short) 24),
-  SCHEMA_FETCH((short) 25),
-  SCHEMA_MERGE((short) 26),
-  STORAGE_GROUP_SCHEMA_SCAN((short) 27);
+  ALTER_TIME_SERIES((short) 21),
+  CREATE_ALIGNED_TIME_SERIES((short) 22),
+  TIME_SERIES_SCHEMA_SCAN((short) 23),
+  SCHEMA_FETCH((short) 24),
+  SCHEMA_MERGE((short) 25),
+  STORAGE_GROUP_SCHEMA_SCAN((short) 26);
 
   private final short nodeType;
 
@@ -150,16 +148,14 @@ public enum PlanNodeType {
       case 20:
         return ExchangeNode.deserialize(buffer);
       case 21:
-        return AuthorNode.deserialize(buffer);
-      case 22:
         return AlterTimeSeriesNode.deserialize(buffer);
-      case 23:
+      case 22:
         return CreateAlignedTimeSeriesNode.deserialize(buffer);
-      case 24:
+      case 23:
         return TimeSeriesSchemaScanNode.deserialize(buffer);
-      case 25:
+      case 24:
         return SchemaFetchNode.deserialize(buffer);
-      case 26:
+      case 25:
         return SchemaMergeNode.deserialize(buffer);
       default:
         throw new IllegalArgumentException("Invalid node type: " + nodeType);
