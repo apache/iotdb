3c23ae3f39a4cc9754d1a972309e102376cff2df
Haonan <hhaonan@outlook.com>
2022-04-26 11:43:14 +0800

    [IOTDB-3007] Separate DataNode and IoTDB (#5668)

--- node-commons/src/main/java/org/apache/iotdb/commons/conf/IoTDBConstant.java
+++ node-commons/src/main/java/org/apache/iotdb/commons/conf/IoTDBConstant.java
@@ -25,6 +25,7 @@ public class IoTDBConstant {
   public static final String ENV_FILE_NAME = "iotdb-env";
   public static final String IOTDB_CONF = "IOTDB_CONF";
   public static final String GLOBAL_DB_NAME = "IoTDB";
+
   // when running the program in IDE, we can not get the version info using
   // getImplementationVersion()
   public static final String VERSION =
