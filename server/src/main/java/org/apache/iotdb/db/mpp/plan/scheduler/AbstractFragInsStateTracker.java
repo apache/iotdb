0af1aede628f5b986d0fb21c2dedec0a45474e12
JiaXin Zhang <37936282+zjx1230@users.noreply.github.com>
2022-05-02 20:58:57 +0800

    Add SyncThriftClientErrorHandler for proxy to clean the code (#5710)

--- server/src/main/java/org/apache/iotdb/db/mpp/plan/scheduler/AbstractFragInsStateTracker.java
+++ server/src/main/java/org/apache/iotdb/db/mpp/plan/scheduler/AbstractFragInsStateTracker.java
@@ -65,26 +65,13 @@ public abstract class AbstractFragInsStateTracker implements IFragInstanceStateT
 
   protected FragmentInstanceState fetchState(FragmentInstance instance)
       throws TException, IOException {
-    SyncDataNodeInternalServiceClient client = null;
-    try {
-      // TODO: (jackie tien) change the port
-      TEndPoint endPoint = instance.getHostDataNode().internalEndPoint;
-      client = internalServiceClientManager.borrowClient(endPoint);
-      if (client == null) {
-        throw new TException("Can't get client for node " + endPoint);
-      }
+    // TODO: (jackie tien) change the port
+    TEndPoint endPoint = instance.getHostDataNode().internalEndPoint;
+    try (SyncDataNodeInternalServiceClient client =
+        internalServiceClientManager.borrowClient(endPoint)) {
       TFragmentInstanceStateResp resp =
           client.fetchFragmentInstanceState(new TFetchFragmentInstanceStateReq(getTId(instance)));
       return FragmentInstanceState.valueOf(resp.state);
-    } catch (Throwable t) {
-      if (t instanceof TException && client != null) {
-        client.close();
-      }
-      throw t;
-    } finally {
-      if (client != null) {
-        client.returnSelf();
-      }
     }
   }
 
