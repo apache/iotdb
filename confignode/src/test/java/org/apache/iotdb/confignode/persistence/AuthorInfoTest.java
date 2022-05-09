67ceaa39a33f1f2c0627f47558b60d4aa0908f7e
stormbroken <181250184@smail.nju.edu.cn>
2022-05-09 17:05:37 +0800

    [snapshot] add authInfo snapshot.

---
+++ confignode/src/test/java/org/apache/iotdb/confignode/persistence/AuthorInfoTest.java
@@ -0,0 +1,88 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.iotdb.confignode.persistence;
+
+import org.apache.iotdb.common.rpc.thrift.TSStatus;
+import org.apache.iotdb.commons.auth.AuthException;
+import org.apache.iotdb.commons.conf.CommonConfig;
+import org.apache.iotdb.confignode.conf.ConfigNodeConf;
+import org.apache.iotdb.confignode.conf.ConfigNodeDescriptor;
+import org.apache.iotdb.confignode.consensus.request.ConfigRequestType;
+import org.apache.iotdb.confignode.consensus.request.auth.AuthorReq;
+import org.apache.iotdb.rpc.TSStatusCode;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.thrift.TException;
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.io.File;
+import java.io.IOException;
+
+import static org.apache.iotdb.db.constant.TestConstant.BASE_OUTPUT_PATH;
+
+public class AuthorInfoTest {
+
+  private static AuthorInfo authorInfo;
+  private static final File snapshotDir = new File(BASE_OUTPUT_PATH, "authorInfo-snapshot");
+  private static final ConfigNodeConf config = ConfigNodeDescriptor.getInstance().getConf();
+  private static final CommonConfig commonConfig = CommonConfig.getInstance();
+
+  @BeforeClass
+  public static void setup() {
+    authorInfo = AuthorInfo.getInstance();
+    if (!snapshotDir.exists()) {
+      snapshotDir.mkdirs();
+    }
+  }
+
+  @AfterClass
+  public static void cleanup() throws IOException, AuthException {
+    authorInfo.clear();
+    if (snapshotDir.exists()) {
+      FileUtils.deleteDirectory(snapshotDir);
+    }
+    // remove system dir
+    FileUtils.deleteDirectory(
+        new File(ConfigNodeDescriptor.getInstance().getConf().getSystemDir()));
+  }
+
+  @Test
+  public void takeSnapshot() throws TException, IOException, AuthException {
+    AuthorReq createRoleReq = new AuthorReq(ConfigRequestType.CreateRole);
+    createRoleReq.setRoleName("testRole");
+    TSStatus status = authorInfo.authorNonQuery(createRoleReq);
+    Assert.assertEquals(TSStatusCode.SUCCESS_STATUS.getStatusCode(), status.code);
+    AuthorReq createUserReq = new AuthorReq(ConfigRequestType.CreateUser);
+    createUserReq.setUserName("testUser");
+    createUserReq.setPassword("testPassword");
+    status = authorInfo.authorNonQuery(createUserReq);
+    Assert.assertEquals(TSStatusCode.SUCCESS_STATUS.getStatusCode(), status.code);
+    Assert.assertEquals(1, authorInfo.executeListRole().getPermissionInfo().get("role").size());
+    Assert.assertEquals(2, authorInfo.executeListUser().getPermissionInfo().get("user").size());
+    authorInfo.processTakeSnapshot(snapshotDir);
+    authorInfo.clear();
+    authorInfo.processLoadSnapshot(snapshotDir);
+    Assert.assertEquals(1, authorInfo.executeListRole().getPermissionInfo().get("role").size());
+    Assert.assertEquals(2, authorInfo.executeListUser().getPermissionInfo().get("user").size());
+  }
+}
