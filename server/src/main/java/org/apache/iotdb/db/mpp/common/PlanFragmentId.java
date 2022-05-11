5d59e7a9c5760191f6615f3b36c0d700df64bfe9
Jinrui.Zhang <xingtanzjr@gmail.com>
2022-05-11 13:33:44 +0800

    code smell change

--- server/src/main/java/org/apache/iotdb/db/mpp/common/PlanFragmentId.java
+++ server/src/main/java/org/apache/iotdb/db/mpp/common/PlanFragmentId.java
@@ -82,8 +82,12 @@ public class PlanFragmentId {
 
   @Override
   public boolean equals(Object o) {
-    if (this == o) return true;
-    if (o == null || getClass() != o.getClass()) return false;
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
     PlanFragmentId that = (PlanFragmentId) o;
     return id == that.id && Objects.equals(queryId, that.queryId);
   }
