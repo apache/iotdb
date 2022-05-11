684bdd032a9607e194d0d3a76eed240ba3a0753e
Pengyu Chen <48903014+trin1t@users.noreply.github.com>
2022-05-11 16:00:58 +0800

    Update library-udf/src/main/java/org/apache/iotdb/library/dprofile/UDTFPACF.java
    Co-authored-by: Steve Yurong Su <rong@apache.org>

--- library-udf/src/main/java/org/apache/iotdb/library/dprofile/UDTFPACF.java
+++ library-udf/src/main/java/org/apache/iotdb/library/dprofile/UDTFPACF.java
@@ -77,8 +77,7 @@ public class UDTFPACF implements UDTF {
     n = value.size();
     if (n > 1) {
       if (lag < 0 || lag > value.size() - 1) {
-        int lag1 = (int) (10 * Math.log10(value.size()));
-        lag = Math.min(lag1, value.size() - 1);
+        lag = Math.min((int) (10 * Math.log10(value.size())), value.size() - 1);
       }
       double[] x =
           Arrays.stream(value.toArray(new Double[0])).mapToDouble(Double::valueOf).toArray();
