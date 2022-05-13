a56198f9f93d400f2cba1b8fa4718f970905c169
MrQuansy <1051622353@qq.com>
2022-05-13 08:49:01 +0800

    Add script of stop and remove datanode

--- server/src/assembly/resources/sbin/stop-datanode.sh
+++ server/src/assembly/resources/sbin/stop-datanode.sh
@@ -18,22 +18,42 @@
 # under the License.
 #
 
+IOTDB_CONF="`dirname "$0"`/../conf"
+rpc_port=`sed '/^rpc_port=/!d;s/.*=//' ${IOTDB_CONF}/iotdb-engine.properties`
+PID=""
 
-PIDS=$(ps ax | grep -i 'DataNode' | grep java | grep -v grep | awk '{print $1}')
-sig=0
-for every_pid in ${PIDS}
-do
-  cwd_path=$(ls -l /proc/$every_pid | grep "cwd ->" | grep -v grep | awk '{print $NF}')
-  pwd_path=$(/bin/pwd)
-  if [[ $pwd_path =~ $cwd_path ]]; then
-    kill -s TERM $every_pid
-    echo "close IoTDB"
-    sig=1
+function getPid {
+  if  type lsof > /dev/null 2>&1 ; then
+    PID=$(lsof -t -i:${rpc_port} -sTCP:LISTEN)
+  elif type netstat > /dev/null 2>&1 ; then
+    PID=$(netstat -anp 2>/dev/null | grep ":${rpc_port} " | grep ' LISTEN ' | awk '{print $NF}' | sed "s|/.*||g" )
+  else
+    echo ""
+    echo " Error: No necessary tool."
+    echo " Please install 'lsof' or 'netstat'."
+    exit 1
   fi
-done
+}
 
-if [ $sig -eq 0 ]; then
-  echo "No IoTDB server to stop"
+getPid
+if [ -z "$PID" ]; then
+  echo "No DataNode to stop."
   exit 1
 fi
 
+echo -n "Begin to stop DataNode ..."
+kill -s TERM $PID
+for ((i=1; i<=1000;i++))   #check status in 100 sec
+do
+  if [ ! -d "/proc/$PID" ]; then
+    echo " closed gracefully."
+    exit 0
+  fi
+  if [ $((i%10)) -eq 0 ]; then
+    echo -n "."
+  fi
+  usleep 100000
+done
+
+kill -s KILL $PID
+echo " forced to kill."
