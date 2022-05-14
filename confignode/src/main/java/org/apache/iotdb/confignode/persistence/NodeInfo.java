8f5cad3e4af3ff32e675457a42a135a087f8f8d1
HTHou <hhaonan@outlook.com>
2022-05-14 11:51:17 +0800

    Fix config node compile error

--- confignode/src/main/java/org/apache/iotdb/confignode/persistence/NodeInfo.java
+++ confignode/src/main/java/org/apache/iotdb/confignode/persistence/NodeInfo.java
@@ -21,6 +21,7 @@ package org.apache.iotdb.confignode.persistence;
 import org.apache.iotdb.common.rpc.thrift.TConfigNodeLocation;
 import org.apache.iotdb.common.rpc.thrift.TDataNodeLocation;
 import org.apache.iotdb.common.rpc.thrift.TSStatus;
+import org.apache.iotdb.commons.snapshot.SnapshotProcessor;
 import org.apache.iotdb.commons.utils.NodeUrlUtils;
 import org.apache.iotdb.commons.utils.TestOnly;
 import org.apache.iotdb.confignode.conf.ConfigNodeConstant;
