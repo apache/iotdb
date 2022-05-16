b0e085d959f15bffefbe828d8b454280157767a6
cmlmakahts <cmlmakahts@126.com>
2022-05-16 11:31:39 +0800

    [IOTDB-3184] Feature/delete timeseries

--- server/src/test/java/org/apache/iotdb/db/mpp/scheduler/FragmentInstanceDispatcherImplTest.java
+++ server/src/test/java/org/apache/iotdb/db/mpp/scheduler/FragmentInstanceDispatcherImplTest.java
@@ -41,6 +41,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
 import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PowerMockIgnore;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 
@@ -53,6 +54,7 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
 @RunWith(PowerMockRunner.class)
+@PowerMockIgnore({"javax.*, com.sun.*"})
 @PrepareForTest({FragmentInstanceDispatcherImpl.class})
 public class FragmentInstanceDispatcherImplTest {
 
