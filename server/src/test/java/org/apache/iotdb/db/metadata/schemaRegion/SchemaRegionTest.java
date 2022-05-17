2da08a5559dab1d25938b14deb1656557b2cb45d
zyk990424 <1534661820@qq.com>
2022-05-17 12:03:11 +0800

    spotless apply

--- server/src/test/java/org/apache/iotdb/db/metadata/schemaRegion/SchemaRegionTest.java
+++ server/src/test/java/org/apache/iotdb/db/metadata/schemaRegion/SchemaRegionTest.java
@@ -23,10 +23,8 @@ import org.apache.iotdb.commons.consensus.SchemaRegionId;
 import org.apache.iotdb.commons.path.PartialPath;
 import org.apache.iotdb.db.conf.IoTDBConfig;
 import org.apache.iotdb.db.conf.IoTDBDescriptor;
-import org.apache.iotdb.db.exception.StorageEngineException;
 import org.apache.iotdb.db.metadata.schemaregion.ISchemaRegion;
 import org.apache.iotdb.db.metadata.schemaregion.SchemaEngine;
-import org.apache.iotdb.db.metadata.schemaregion.SchemaRegionMemoryImpl;
 import org.apache.iotdb.db.qp.physical.sys.CreateTimeSeriesPlan;
 import org.apache.iotdb.db.qp.physical.sys.ShowTimeSeriesPlan;
 import org.apache.iotdb.db.query.dataset.ShowTimeSeriesResult;
@@ -35,56 +33,68 @@ import org.apache.iotdb.tsfile.file.metadata.enums.CompressionType;
 import org.apache.iotdb.tsfile.file.metadata.enums.TSDataType;
 import org.apache.iotdb.tsfile.file.metadata.enums.TSEncoding;
 import org.apache.iotdb.tsfile.utils.Pair;
+
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
 import java.io.File;
-import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class SchemaRegionTest {
 
-    SchemaEngine schemaEngine = SchemaEngine.getInstance();
-    IoTDBConfig config = IoTDBDescriptor.getInstance().getConfig();
+  SchemaEngine schemaEngine = SchemaEngine.getInstance();
+  IoTDBConfig config = IoTDBDescriptor.getInstance().getConfig();
 
-    @Before
-    public void setUp(){
-        EnvironmentUtils.envSetUp();
-    }
+  @Before
+  public void setUp() {
+    EnvironmentUtils.envSetUp();
+  }
 
-    @After
-    public void tearDown() throws Exception{
-        EnvironmentUtils.cleanEnv();
-    }
+  @After
+  public void tearDown() throws Exception {
+    EnvironmentUtils.cleanEnv();
+  }
 
-    @Test
-    public void testSnapshot() throws Exception{
-        PartialPath storageGroup = new PartialPath("root.sg");
-        SchemaRegionId schemaRegionId = new SchemaRegionId(0);
-        schemaEngine.createSchemaRegion(storageGroup, schemaRegionId);
-        ISchemaRegion schemaRegion = SchemaEngine.getInstance().getSchemaRegion(schemaRegionId);
+  @Test
+  public void testSnapshot() throws Exception {
+    PartialPath storageGroup = new PartialPath("root.sg");
+    SchemaRegionId schemaRegionId = new SchemaRegionId(0);
+    schemaEngine.createSchemaRegion(storageGroup, schemaRegionId);
+    ISchemaRegion schemaRegion = SchemaEngine.getInstance().getSchemaRegion(schemaRegionId);
 
-        Map<String, String> tags = new HashMap<>();
-        tags.put("tag-key", "tag-value");
-        schemaRegion.createTimeseries(new CreateTimeSeriesPlan(new PartialPath("root.sg.d1.s1"), TSDataType.INT32, TSEncoding.PLAIN, CompressionType.UNCOMPRESSED, null, tags, null, null), -1);
+    Map<String, String> tags = new HashMap<>();
+    tags.put("tag-key", "tag-value");
+    schemaRegion.createTimeseries(
+        new CreateTimeSeriesPlan(
+            new PartialPath("root.sg.d1.s1"),
+            TSDataType.INT32,
+            TSEncoding.PLAIN,
+            CompressionType.UNCOMPRESSED,
+            null,
+            tags,
+            null,
+            null),
+        -1);
 
-        File snapshotDir = new File(config.getSchemaDir() + File.separator + "snapshot");
-        schemaRegion.createSnapshot(snapshotDir);
+    File snapshotDir = new File(config.getSchemaDir() + File.separator + "snapshot");
+    schemaRegion.createSnapshot(snapshotDir);
 
-        schemaRegion.loadSnapshot(snapshotDir);
+    schemaRegion.loadSnapshot(snapshotDir);
 
-        Pair<List<ShowTimeSeriesResult>, Integer> result = schemaRegion.showTimeseries(new ShowTimeSeriesPlan(
-                new PartialPath("root.sg.**"), false, "tag-key", "tag-value", 0, 0, false), null);
+    Pair<List<ShowTimeSeriesResult>, Integer> result =
+        schemaRegion.showTimeseries(
+            new ShowTimeSeriesPlan(
+                new PartialPath("root.sg.**"), false, "tag-key", "tag-value", 0, 0, false),
+            null);
 
-        ShowTimeSeriesResult seriesResult = result.left.get(0);
-        Assert.assertEquals(new PartialPath("root.sg.d1.s1").getFullPath(), seriesResult.getName());
-        Map<String, String> resultTagMap = seriesResult.getTag();
-        Assert.assertEquals(1, resultTagMap.size());
-        Assert.assertEquals("tag-value", resultTagMap.get("tag-key"));
-    }
-
+    ShowTimeSeriesResult seriesResult = result.left.get(0);
+    Assert.assertEquals(new PartialPath("root.sg.d1.s1").getFullPath(), seriesResult.getName());
+    Map<String, String> resultTagMap = seriesResult.getTag();
+    Assert.assertEquals(1, resultTagMap.size());
+    Assert.assertEquals("tag-value", resultTagMap.get("tag-key"));
+  }
 }
