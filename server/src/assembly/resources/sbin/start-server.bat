b8cd0c2cc09912e5e12bd783503a428f7987b044
Haonan <hhaonan@outlook.com>
2022-05-17 22:52:29 +0800

    Avoild using symlink when pack all-in-one package (#5935)

--- server/src/assembly/resources/sbin/start-server.bat
+++ server/src/assembly/resources/sbin/start-server.bat
@@ -96,7 +96,7 @@ set JAVA_OPTS=-ea^
 
 @REM ***** CLASSPATH library setting *****
 @REM Ensure that any user defined CLASSPATH variables are not used on startup
-set CLASSPATH="%IOTDB_HOME%\lib\*"
+if EXIST %IOTDB_HOME%\lib (set CLASSPATH="%IOTDB_HOME%\lib\*") else set CLASSPATH="%IOTDB_HOME%\..\lib\*"
 set CLASSPATH=%CLASSPATH%;iotdb.IoTDB
 goto okClasspath
 
