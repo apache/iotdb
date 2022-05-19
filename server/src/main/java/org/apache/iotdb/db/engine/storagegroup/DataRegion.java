1ebeac5b6a9a3bfcfee6825ff8bdf7d22ab04e6f
Liu Xuxin <37140360+THUMarkLau@users.noreply.github.com>
2022-05-19 09:24:14 +0800

    [IOTDB-2977] Take and load snapshot for DataRegionStateMachine (#5923)

--- server/src/main/java/org/apache/iotdb/db/engine/storagegroup/DataRegion.java
+++ server/src/main/java/org/apache/iotdb/db/engine/storagegroup/DataRegion.java
@@ -3486,6 +3486,10 @@ public class DataRegion {
         throws WriteProcessException;
   }
 
+  public List<Long> getTimePartitions() {
+    return new ArrayList<>(partitionMaxFileVersions.keySet());
+  }
+
   public String getInsertWriteLockHolder() {
     return insertWriteLockHolder;
   }
