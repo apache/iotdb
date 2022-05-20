6c53cff5dbddeb9f0f08a5239e4ddf9b9de363ef
renyuhua <ryh13233464493@163.com>
2022-05-20 15:15:59 +0800

    fix compile

--- server/src/test/java/org/apache/iotdb/db/auth/AuthorizerManagerTest.java
+++ server/src/test/java/org/apache/iotdb/db/auth/AuthorizerManagerTest.java
@@ -30,7 +30,12 @@ import org.apache.iotdb.confignode.rpc.thrift.TUserResp;
 import org.junit.Assert;
 import org.junit.Test;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 public class AuthorizerManagerTest {
 
