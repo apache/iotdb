72367974b672ed0aceaef55d2c6c77d17dcb4ca3
Weihao Li <18110526956@163.com>
2022-05-22 13:19:23 +0800

    [IOTDB-3173] Add License and change the value of ExpressionType
    Signed-off-by: Weihao Li <18110526956@163.com>

--- server/src/main/java/org/apache/iotdb/db/mpp/transformation/dag/transformer/ternary/TernaryTransformer.java
+++ server/src/main/java/org/apache/iotdb/db/mpp/transformation/dag/transformer/ternary/TernaryTransformer.java
@@ -1,3 +1,24 @@
+/*
+ *
+ *  * Licensed to the Apache Software Foundation (ASF) under one
+ *  * or more contributor license agreements.  See the NOTICE file
+ *  * distributed with this work for additional information
+ *  * regarding copyright ownership.  The ASF licenses this file
+ *  * to you under the Apache License, Version 2.0 (the
+ *  * "License"); you may not use this file except in compliance
+ *  * with the License.  You may obtain a copy of the License at
+ *  *
+ *  *     http://www.apache.org/licenses/LICENSE-2.0
+ *  *
+ *  * Unless required by applicable law or agreed to in writing,
+ *  * software distributed under the License is distributed on an
+ *  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  * KIND, either express or implied.  See the License for the
+ *  * specific language governing permissions and limitations
+ *  * under the License.
+ *
+ */
+
 package org.apache.iotdb.db.mpp.transformation.dag.transformer.ternary;
 
 import org.apache.iotdb.db.exception.query.QueryProcessException;
@@ -89,7 +110,7 @@ public abstract class TernaryTransformer extends Transformer {
           }
           secondTime = secondPointReader.currentTime();
         }
-      } else if (thirdTime < firstTime) {
+      } else {
         if (isThirdConstant) {
           firstTime = secondTime;
         } else {
@@ -99,15 +120,10 @@ public abstract class TernaryTransformer extends Transformer {
           }
           thirdTime = secondPointReader.currentTime();
         }
-      } else {
-        cachedTime = firstTime;
-        return true;
       }
     }
 
-    if (firstTime
-        != 0) // Value of firstTime, secondTime and thirdTime are same here, the value equals zero
-      // represents three LayerPointerReaders are all ConstantPointReader.
+    if (firstTime != 0) // Value of firstTime, secondTime and thirdTime are same here, the value equals zero represents three LayerPointerReaders are all ConstantPointReader.
       cachedTime = firstTime;
     return true;
   }
