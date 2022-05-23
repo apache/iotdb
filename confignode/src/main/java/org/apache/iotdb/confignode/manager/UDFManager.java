8fe3c0738326b570d889c9e0bb00342c277eb9f5
Steve Yurong Su <rong@apache.org>
2022-05-23 19:00:11 +0800

    basic structure of UDFManager

--- confignode/src/main/java/org/apache/iotdb/confignode/manager/UDFManager.java
+++ confignode/src/main/java/org/apache/iotdb/confignode/manager/UDFManager.java
@@ -20,18 +20,39 @@
 package org.apache.iotdb.confignode.manager;
 
 import org.apache.iotdb.common.rpc.thrift.TSStatus;
+import org.apache.iotdb.commons.udf.service.UDFClassLoaderManager;
+import org.apache.iotdb.commons.udf.service.UDFExecutableFetcher;
+import org.apache.iotdb.commons.udf.service.UDFRegistrationService;
+import org.apache.iotdb.confignode.conf.ConfigNodeConf;
+import org.apache.iotdb.confignode.conf.ConfigNodeDescriptor;
 import org.apache.iotdb.confignode.persistence.UDFInfo;
 
+import java.io.IOException;
 import java.util.List;
 
 public class UDFManager {
 
+  private static final ConfigNodeConf CONFIG_NODE_CONF =
+      ConfigNodeDescriptor.getInstance().getConf();
+
+  private final UDFExecutableFetcher udfExecutableFetcher;
+  private final UDFClassLoaderManager udfClassLoaderManager;
+  private final UDFRegistrationService udfRegistrationService;
+
   private final ConfigManager configManager;
   private final UDFInfo udfInfo;
 
-  public UDFManager(ConfigManager configManager, UDFInfo udfInfo) {
+  public UDFManager(ConfigManager configManager, UDFInfo udfInfo) throws IOException {
     this.configManager = configManager;
     this.udfInfo = udfInfo;
+
+    udfExecutableFetcher =
+        UDFExecutableFetcher.setupAndGetInstance(
+            CONFIG_NODE_CONF.getTemporaryLibDir(), CONFIG_NODE_CONF.getUdfLibDir());
+    udfClassLoaderManager =
+        UDFClassLoaderManager.setupAndGetInstance(CONFIG_NODE_CONF.getUdfLibDir());
+    udfRegistrationService =
+        UDFRegistrationService.setupAndGetInstance(CONFIG_NODE_CONF.getSystemUdfDir());
   }
 
   public TSStatus createFunction(String udfName, String className, List<String> uris) {
--- node-commons/src/main/java/org/apache/iotdb/commons/conf/IoTDBConstant.java
+++ node-commons/src/main/java/org/apache/iotdb/commons/conf/IoTDBConstant.java
@@ -172,6 +172,7 @@ public class IoTDBConstant {
   public static final String TRACING_LOG = "tracing.txt";
   public static final String EXT_FOLDER_NAME = "ext";
   public static final String UDF_FOLDER_NAME = "udf";
+  public static final String TMP_FOLDER_NAME = "temporary";
   public static final String TRIGGER_FOLDER_NAME = "trigger";
   public static final String MQTT_FOLDER_NAME = "mqtt";
   public static final String WAL_FOLDER_NAME = "wal";
