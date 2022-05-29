428a309c273847f0b4b8305020c5742eb67d1779
Beyyes <caogaofei@apache.org>
2022-05-29 18:11:05 +0800

    fix the line separator problem in ShowClusterStatement class

--- server/src/main/java/org/apache/iotdb/db/mpp/plan/statement/metadata/ShowClusterStatement.java
+++ server/src/main/java/org/apache/iotdb/db/mpp/plan/statement/metadata/ShowClusterStatement.java
@@ -1,1 +1,19 @@
-package org.apache.iotdb.db.mpp.plan.statement.metadata;import org.apache.iotdb.db.mpp.plan.analyze.QueryType;import org.apache.iotdb.db.mpp.plan.statement.IConfigStatement;import org.apache.iotdb.db.mpp.plan.statement.StatementVisitor;public class ShowClusterStatement extends ShowStatement implements IConfigStatement {  @Override  public QueryType getQueryType() {    return QueryType.READ;  }  @Override  public <R, C> R accept(StatementVisitor<R, C> visitor, C context) {    return visitor.visitShowCluster(this, context);  }}
+package org.apache.iotdb.db.mpp.plan.statement.metadata;
+
+import org.apache.iotdb.db.mpp.plan.analyze.QueryType;
+import org.apache.iotdb.db.mpp.plan.statement.IConfigStatement;
+import org.apache.iotdb.db.mpp.plan.statement.StatementVisitor;
+
+public class ShowClusterStatement extends ShowStatement implements IConfigStatement {
+
+  @Override
+  public QueryType getQueryType() {
+    return QueryType.READ;
+  }
+
+  @Override
+  public <R, C> R accept(StatementVisitor<R, C> visitor, C context) {
+    return visitor.visitShowCluster(this, context);
+  }
+
+}
