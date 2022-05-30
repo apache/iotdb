52da31d9b95f0905f74ec3c01cfd8a2e0e1fe8b0
liuminghui233 <549208791@qq.com>
2022-05-30 21:10:48 +0800

    spotless apply

--- server/src/main/java/org/apache/iotdb/db/metadata/utils/MetaUtils.java
+++ server/src/main/java/org/apache/iotdb/db/metadata/utils/MetaUtils.java
@@ -32,7 +32,6 @@ import org.apache.iotdb.tsfile.read.common.Path;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
