ed6a533cec9c7c8e51d84e21aa2024ae12c9f6d1
Timcross-WEI <timcross-wei@qq.com>
2022-05-30 20:45:18 +0800

    adjust ForwardSink directory

--- server/src/main/java/org/apache/iotdb/db/engine/trigger/sink/forward/http/HTTPForwardEvent.java
+++ server/src/main/java/org/apache/iotdb/db/engine/trigger/sink/forward/http/HTTPForwardEvent.java
@@ -17,7 +17,7 @@
  * under the License.
  */
 
-package org.apache.iotdb.db.engine.trigger.sink.http;
+package org.apache.iotdb.db.engine.trigger.sink.forward.http;
 
 import org.apache.iotdb.commons.path.PartialPath;
 import org.apache.iotdb.db.engine.trigger.sink.forward.ForwardEvent;
