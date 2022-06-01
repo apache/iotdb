b514353fb2dae92dcaa4756e93f82570a2ee8d03
刘威 <51618159+LIU-WEI-git@users.noreply.github.com>
2022-06-01 09:58:49 +0800

    [IOTDB-3251] Add ForwardTrigger doc and change api package (#6080)

--- server/src/main/java/org/apache/iotdb/db/engine/trigger/sink/forward/mqtt/MQTTForwardHandler.java
+++ server/src/main/java/org/apache/iotdb/db/engine/trigger/sink/forward/mqtt/MQTTForwardHandler.java
@@ -17,7 +17,7 @@
  * under the License.
  */
 
-package org.apache.iotdb.db.engine.trigger.sink.mqtt;
+package org.apache.iotdb.db.engine.trigger.sink.forward.mqtt;
 
 import org.apache.iotdb.db.engine.trigger.sink.api.Handler;
 import org.apache.iotdb.db.engine.trigger.sink.exception.SinkException;
