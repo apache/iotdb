6ccf750e89273c2f374a1306252b505f115369e7
Alima777 <wxw19981014@gmail.com>
2022-06-02 16:40:29 +0800

    handle one time range once

--- server/src/main/java/org/apache/iotdb/db/mpp/execution/operator/process/AggregationOperator.java
+++ server/src/main/java/org/apache/iotdb/db/mpp/execution/operator/process/AggregationOperator.java
@@ -98,7 +98,9 @@ public class AggregationOperator implements ProcessOperator {
   @Override
   public TsBlock next() {
     // update input tsBlock
-    curTimeRange = timeRangeIterator.nextTimeRange();
+    if (curTimeRange == null && timeRangeIterator.hasNextTimeRange()) {
+      curTimeRange = timeRangeIterator.nextTimeRange();
+    }
     for (int i = 0; i < inputOperatorsCount; i++) {
       if (inputTsBlocks[i] != null) {
         continue;
@@ -114,6 +116,7 @@ public class AggregationOperator implements ProcessOperator {
       aggregator.processTsBlocks(inputTsBlocks);
     }
     // output result from aggregator
+    curTimeRange = null;
     for (int i = 0; i < inputOperatorsCount; i++) {
       inputTsBlocks[i] = null;
     }
