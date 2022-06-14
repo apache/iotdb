f5cc05105c98cb16eb212f3c907c62ac0f146728
luke.miao <luke.miao@yunzhihui.com>
2022-06-14 16:02:09 +0800

    rest java example

--- example/rest-java-example/src/main/java/org/apache/iotdb/HttpsExample.java
+++ example/rest-java-example/src/main/java/org/apache/iotdb/HttpsExample.java
@@ -33,7 +33,6 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.util.EntityUtils;
 
 public class HttpsExample {
