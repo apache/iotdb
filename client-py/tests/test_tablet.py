23cdee340ebb72ab42d805b4f57b22d15f10ae34
Liu Xuxin <liuxuxin@outlook.com>
2022-06-15 12:46:01 +0800

    run ci

--- client-py/tests/test_tablet.py
+++ client-py/tests/test_tablet.py
@@ -61,6 +61,7 @@ def test_tablet_insertion():
             "select s_01, s_02, s_03, s_04, s_05, s_06 from root.sg_test_01.d_01"
         )
         df_output = session_data_set.todf()
+        print(df_output)
         df_output = df_output[df_input.columns.tolist()]
 
         session.close()
