a0031aece9069ba30ce400d1c640d1251b0ff929
William Song <48054931+SzyWilliam@users.noreply.github.com>
2022-06-17 11:18:33 +0800

    Consensus CI test fix transferLeader assert (#6264)
    * consensus CI test fix transferLeader assert
    
    * add comments
    
    * rerun CI
    
    * test CI params
    
    * test CI params
    
    * refactor ratis consensus unit test

--- consensus/src/main/java/org/apache/iotdb/consensus/ratis/RatisConsensus.java
+++ consensus/src/main/java/org/apache/iotdb/consensus/ratis/RatisConsensus.java
@@ -410,6 +410,7 @@ class RatisConsensus implements IConsensus {
   }
 
   /**
+   * NOTICE: transferLeader *does not guarantee* the leader be transferred to newLeader.
    * transferLeader is implemented by 1. modify peer priority 2. ask current leader to step down
    *
    * <p>1. call setConfiguration to upgrade newLeader's priority to 1 and degrade all follower peers
