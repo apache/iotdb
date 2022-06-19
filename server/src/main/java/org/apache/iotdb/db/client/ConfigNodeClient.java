77563a627304031c0a0148c895e3ae9769b12117
任宇华 <79885238+RYH61@users.noreply.github.com>
2022-06-19 09:16:21 +0800

    [IOTDB-3511] Show Region tool (#6315)

--- server/src/main/java/org/apache/iotdb/db/client/ConfigNodeClient.java
+++ server/src/main/java/org/apache/iotdb/db/client/ConfigNodeClient.java
@@ -59,7 +59,10 @@ import org.apache.iotdb.confignode.rpc.thrift.TSetSchemaReplicationFactorReq;
 import org.apache.iotdb.confignode.rpc.thrift.TSetStorageGroupReq;
 import org.apache.iotdb.confignode.rpc.thrift.TSetTTLReq;
 import org.apache.iotdb.confignode.rpc.thrift.TSetTimePartitionIntervalReq;
+import org.apache.iotdb.confignode.rpc.thrift.TShowRegionReq;
+import org.apache.iotdb.confignode.rpc.thrift.TShowRegionResp;
 import org.apache.iotdb.confignode.rpc.thrift.TStorageGroupSchemaResp;
+import org.apache.iotdb.db.conf.IoTDBConfig;
 import org.apache.iotdb.db.conf.IoTDBDescriptor;
 import org.apache.iotdb.rpc.RpcTransportFactory;
 import org.apache.iotdb.rpc.TSStatusCode;
@@ -97,7 +100,11 @@ public class ConfigNodeClient implements ConfigIService.Iface, SyncThriftClient,
 
   private List<TEndPoint> configNodes;
 
+  private TEndPoint configNode;
+
   private int cursor = 0;
+
+  private IoTDBConfig config = IoTDBDescriptor.getInstance().getConfig();
 
   ClientManager<PartitionRegionId, ConfigNodeClient> clientManager;
 
@@ -140,6 +147,7 @@ public class ConfigNodeClient implements ConfigIService.Iface, SyncThriftClient,
               // as there is a try-catch already, we do not need to use TSocket.wrap
               endpoint.getIp(), endpoint.getPort(), (int) connectionTimeout);
       transport.open();
+      configNode = endpoint;
     } catch (TTransportException e) {
       throw new TException(e);
     }
@@ -223,6 +231,10 @@ public class ConfigNodeClient implements ConfigIService.Iface, SyncThriftClient,
       } else {
         configLeader = null;
       }
+      logger.warn(
+          "Failed to connect to ConfigNode {} from DataNode {},because the current node is not leader,try next node",
+          configNode,
+          config.getAddressAndPort());
       return true;
     }
     return false;
@@ -648,6 +660,22 @@ public class ConfigNodeClient implements ConfigIService.Iface, SyncThriftClient,
         TSStatus status = client.flush(req);
         if (!updateConfigNodeLeader(status)) {
           return status;
+        }
+      } catch (TException e) {
+        configLeader = null;
+      }
+      reconnect();
+    }
+    throw new TException(MSG_RECONNECTION_FAIL);
+  }
+
+  @Override
+  public TShowRegionResp showRegion(TShowRegionReq req) throws TException {
+    for (int i = 0; i < RETRY_NUM; i++) {
+      try {
+        TShowRegionResp showRegionResp = client.showRegion(req);
+        if (!updateConfigNodeLeader(showRegionResp.getStatus())) {
+          return showRegionResp;
         }
       } catch (TException e) {
         configLeader = null;
