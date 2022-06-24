8dc398a011409e538b6fa22d207e22532e05da2a
Marcos_Zyk <38524330+MarcosZyk@users.noreply.github.com>
2022-06-24 21:33:44 +0800

    [IOTDB-3539] Adjust the capacity of SchemaCache according to schema memory (#6403)

--- server/src/main/java/org/apache/iotdb/db/metadata/cache/SchemaCacheEntry.java
+++ server/src/main/java/org/apache/iotdb/db/metadata/cache/SchemaCacheEntry.java
@@ -67,4 +67,27 @@ public class SchemaCacheEntry {
   public void setLastCacheContainer(ILastCacheContainer lastCacheContainer) {
     this.lastCacheContainer = lastCacheContainer;
   }
+
+  /**
+   * Total basic 100B
+   *
+   * <ul>
+   *   <li>SchemaCacheEntry Object header, 8B
+   *   <li>isAligned, 1B
+   *   <li>LastCacheContainer reference, 8B
+   *   <li>MeasurementSchema
+   *       <ul>
+   *         <li>Reference, 8B
+   *         <li>Object header, 8B
+   *         <li>String measurementId basic, 8 + 8 + 4 + 8 + 4 = 32B
+   *         <li>type, encoding, compressor, 3 B
+   *         <li>encodingConverter, 8 + 8 + 8 = 24B
+   *         <li>props, 8B
+   *       </ul>
+   * </ul>
+   */
+  public static int estimateSize(SchemaCacheEntry schemaCacheEntry) {
+    // each char takes 2B in Java
+    return 100 + 2 * schemaCacheEntry.getMeasurementSchema().getMeasurementId().length();
+  }
 }
