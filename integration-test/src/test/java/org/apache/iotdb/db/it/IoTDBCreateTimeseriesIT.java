1e0a624ba0087761882267f04e9ee2deda49132e
HTHou <hhaonan@outlook.com>
2022-06-24 12:22:28 +0800

    debug

--- integration-test/src/test/java/org/apache/iotdb/db/it/IoTDBCreateTimeseriesIT.java
+++ integration-test/src/test/java/org/apache/iotdb/db/it/IoTDBCreateTimeseriesIT.java
@@ -35,12 +35,8 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
-import java.util.stream.Collectors;
 
 import static org.junit.Assert.fail;
 
