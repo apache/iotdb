4708a4c45a289db5f9dd8fa2f4a8cc663aabe140
lance <181250082@smail.nju.edu.cn>
2022-06-27 15:06:00 +0800

    fix

--- integration-test/src/test/java/org/apache/iotdb/db/it/IoTDBQuotedPathIT.java
+++ integration-test/src/test/java/org/apache/iotdb/db/it/IoTDBQuotedPathIT.java
@@ -105,9 +105,7 @@ public class IoTDBQuotedPathIT {
         Statement statement = connection.createStatement()) {
       statement.execute("SET STORAGE GROUP TO root.`\"ln`");
     } catch (SQLException e) {
-      Assert.assertEquals(
-          "315: The storage group name can only be characters, numbers and underscores. root.`\"ln` is not a legal path",
-          e.getMessage());
+      Assert.assertTrue(e.getMessage().contains("Error StorageGroup name"));
     } catch (Exception e) {
       e.printStackTrace();
       Assert.fail();
