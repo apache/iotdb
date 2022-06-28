58a0a62fefc780d46e373e0a9b0ad9d412574ae2
Liao Lanyu <48237151+Plutooooooo@users.noreply.github.com>
2022-06-28 12:13:44 +0800

    [IOTDB-3606] Add path related IT in new cluster (#6458)

--- integration-test/src/test/java/org/apache/iotdb/db/it/IoTDBQuotedPathIT.java
+++ integration-test/src/test/java/org/apache/iotdb/db/it/IoTDBQuotedPathIT.java
@@ -7,7 +7,7 @@
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
@@ -16,39 +16,38 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-package org.apache.iotdb.db.integration;
+package org.apache.iotdb.db.it;
 
-import org.apache.iotdb.integration.env.EnvFactory;
-import org.apache.iotdb.itbase.category.ClusterTest;
-import org.apache.iotdb.itbase.category.LocalStandaloneTest;
-import org.apache.iotdb.itbase.category.RemoteTest;
-import org.apache.iotdb.jdbc.IoTDBSQLException;
+import org.apache.iotdb.it.env.EnvFactory;
+import org.apache.iotdb.it.env.IoTDBTestRunner;
+import org.apache.iotdb.itbase.category.ClusterIT;
+import org.apache.iotdb.itbase.category.LocalStandaloneIT;
+import org.apache.iotdb.itbase.constant.TestConstant;
 
-import org.junit.After;
+import org.junit.AfterClass;
 import org.junit.Assert;
-import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
+import org.junit.runner.RunWith;
 
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-import static org.apache.iotdb.db.constant.TestConstant.TIMESTAMP_STR;
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 
-@Category({LocalStandaloneTest.class, ClusterTest.class, RemoteTest.class})
+@RunWith(IoTDBTestRunner.class)
+@Category({LocalStandaloneIT.class, ClusterIT.class})
 public class IoTDBQuotedPathIT {
-
-  @Before
-  public void setUp() throws Exception {
+  @BeforeClass
+  public static void setUp() throws Exception {
     EnvFactory.getEnv().initBeforeClass();
   }
 
-  @After
-  public void tearDown() throws Exception {
+  @AfterClass
+  public static void tearDown() throws Exception {
     EnvFactory.getEnv().cleanAfterClass();
   }
 
@@ -76,31 +75,23 @@ public class IoTDBQuotedPathIT {
       statement.execute(
           "CREATE TIMESERIES root.ln.`wf+01`.wt02.`asd12` WITH DATATYPE=BOOLEAN, ENCODING=PLAIN");
 
-      boolean hasResultSet = statement.execute("SELECT * FROM root.ln.`wf+01`.wt01");
-      assertTrue(hasResultSet);
-      ResultSet resultSet = statement.getResultSet();
-      try {
-        int cnt = 0;
+      int cnt = 0;
+      try (ResultSet resultSet = statement.executeQuery("SELECT * FROM root.ln.`wf+01`.wt01")) {
         while (resultSet.next()) {
-          String result = resultSet.getString(TIMESTAMP_STR) + "," + resultSet.getString(2);
+          String result =
+              resultSet.getString(TestConstant.TIMESTAMP_STR) + "," + resultSet.getString(2);
           assertEquals(exp[cnt++], result);
         }
-
-        hasResultSet =
-            statement.execute("SELECT * FROM root.ln.`wf+01`.wt01 WHERE `status+2+3` = false");
-        assertTrue(hasResultSet);
+      }
+      try (ResultSet resultSet =
+          statement.executeQuery("SELECT * FROM root.ln.`wf+01`.wt01 WHERE `status+2+3` = false")) {
         exp = new String[] {"1509465600002,false", "1509465600003,false"};
         cnt = 0;
-        resultSet = statement.getResultSet();
         while (resultSet.next()) {
-          String result = resultSet.getString(TIMESTAMP_STR) + "," + resultSet.getString(2);
+          String result =
+              resultSet.getString(TestConstant.TIMESTAMP_STR) + "," + resultSet.getString(2);
           assertEquals(exp[cnt++], result);
         }
-
-        statement.execute(
-            "DELETE FROM root.ln.`wf+01`.wt01.`status+2+3` WHERE time < 1509465600001");
-      } finally {
-        resultSet.close();
       }
     } catch (Exception e) {
       e.printStackTrace();
@@ -109,14 +100,16 @@ public class IoTDBQuotedPathIT {
   }
 
   @Test
-  public void testIllegalStorageGroup() throws SQLException {
+  public void testIllegalStorageGroup() {
     try (Connection connection = EnvFactory.getEnv().getConnection();
         Statement statement = connection.createStatement()) {
       statement.execute("SET STORAGE GROUP TO root.`\"ln`");
-    } catch (IoTDBSQLException e) {
-      Assert.assertEquals(
-          "315: The storage group name can only be characters, numbers and underscores. root.`\"ln` is not a legal path",
-          e.getMessage());
+    } catch (SQLException e) {
+      Assert.assertTrue(
+          e.getMessage().contains("Error StorageGroup name")
+              || e.getMessage()
+                  .contains(
+                      "The storage group name can only be characters, numbers and underscores."));
     } catch (Exception e) {
       e.printStackTrace();
       Assert.fail();
