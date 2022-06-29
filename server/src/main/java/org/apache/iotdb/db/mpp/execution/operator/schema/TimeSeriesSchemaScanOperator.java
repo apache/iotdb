fa4721ae02940937374a754272376121b66f889b
Marccos <1534661820@qq.com>
2022-06-29 19:53:04 +0800

    fix show latest timeseries

--- server/src/main/java/org/apache/iotdb/db/mpp/execution/operator/schema/TimeSeriesSchemaScanOperator.java
+++ server/src/main/java/org/apache/iotdb/db/mpp/execution/operator/schema/TimeSeriesSchemaScanOperator.java
@@ -93,7 +93,7 @@ public class TimeSeriesSchemaScanOperator extends SchemaQueryScanOperator {
 
   // ToDo @xinzhongtianxia remove this temporary converter after mpp online
   private ShowTimeSeriesPlan convertToPhysicalPlan() {
-    return new ShowTimeSeriesPlan(partialPath, isContains, key, value, limit, offset, orderByHeat);
+    return new ShowTimeSeriesPlan(partialPath, isContains, key, value, limit, offset, false);
   }
 
   private void setColumns(ShowTimeSeriesResult series, TsBlockBuilder builder) {
