1c38d134ef1291d987526c81e57f358ec6a278a3
YongzaoDan <532741407@qq.com>
2022-07-14 12:17:40 +0800

    spotless

--- confignode/src/test/java/org/apache/iotdb/confignode/manager/load/balancer/router/LeaderRouterTest.java
+++ confignode/src/test/java/org/apache/iotdb/confignode/manager/load/balancer/router/LeaderRouterTest.java
@@ -33,8 +33,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import static org.junit.Assert.*;
-
 public class LeaderRouterTest {
 
   private static final TDataNodeLocation dataNodeLocation1 =
