b7ff1eb6ace27b6ac0f925ea1b7509eafb7375d0
wangchao316 <66939405+wangchao316@users.noreply.github.com>
2022-07-19 11:53:26 +0800

    [IOTDB-3869] refactoring remove confignode to Procedure and support rollback (#6696)
    * [IOTDB-3815] refactoring remove confignode to Procedure and support rollback

--- thrift-confignode/src/main/thrift/confignode.thrift
+++ thrift-confignode/src/main/thrift/confignode.thrift
@@ -386,6 +386,8 @@ service IConfigNodeRPCService {
 
   common.TSStatus removeConfigNode(common.TConfigNodeLocation configNodeLocation)
 
+  common.TSStatus removeConsensusGroup(common.TConfigNodeLocation configNodeLocation)
+
   common.TSStatus stopConfigNode(common.TConfigNodeLocation configNodeLocation)
 
   /* UDF */
