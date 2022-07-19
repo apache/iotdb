23856d20a22ae3c3260d7b318151307a3f277bd9
Marcos_Zyk <38524330+MarcosZyk@users.noreply.github.com>
2022-07-19 17:21:32 +0800

    [To rel/0.13][IOTDB-3742]Fix count nodes #6715
    [To rel/0.13][IOTDB-3742]Fix count nodes #6715

--- server/src/main/java/org/apache/iotdb/db/metadata/mtree/traverser/counter/EntityCounter.java
+++ server/src/main/java/org/apache/iotdb/db/metadata/mtree/traverser/counter/EntityCounter.java
@@ -27,6 +27,7 @@ public class EntityCounter extends CounterTraverser {
 
   public EntityCounter(IMNode startNode, PartialPath path) throws MetadataException {
     super(startNode, path);
+    shouldTraverseTemplate = true;
   }
 
   @Override
