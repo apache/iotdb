0313d9427501f1d21dccd61d512b01a3cf649112
lyssom <569810240@qq.com>
2022-07-20 14:27:38 +0800

    Lower compression ratio configuration to reduce memory usage

--- tsfile/src/main/java/org/apache/iotdb/tsfile/compress/IUnCompressor.java
+++ tsfile/src/main/java/org/apache/iotdb/tsfile/compress/IUnCompressor.java
@@ -316,7 +316,7 @@ public interface IUnCompressor {
   }
 
   class ZSTDUnCompressor implements IUnCompressor {
-    private static final int MAX_COMPRESS_RATIO = 255;
+    private static final int MAX_COMPRESS_RATIO = 25;
 
     @Override
     public int getUncompressedLength(byte[] array, int offset, int length) {
