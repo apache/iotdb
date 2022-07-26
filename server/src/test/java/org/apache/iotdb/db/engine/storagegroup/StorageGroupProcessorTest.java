0ca58726dfe8d174bd75a42d4a328d8e8214d141
周沛辰 <45144903+choubenson@users.noreply.github.com>
2022-07-26 16:39:44 +0800

    [To rel/0.13][IOTDB-3777]Avoid holding sg write lock in the whole processin of deletion (#6745)

--- server/src/test/java/org/apache/iotdb/db/engine/storagegroup/StorageGroupProcessorTest.java
+++ server/src/test/java/org/apache/iotdb/db/engine/storagegroup/StorageGroupProcessorTest.java
@@ -925,7 +925,7 @@ public class StorageGroupProcessorTest {
   }
 
   @Test
-  public void testDeleteDataInFlushingMemtable()
+  public void testDeleteDataNotInFlushingMemtable()
       throws IllegalPathException, WriteProcessException, TriggerExecutionException, IOException {
     for (int j = 0; j < 100; j++) {
       TSRecord record = new TSRecord(j, deviceId);
@@ -936,15 +936,96 @@ public class StorageGroupProcessorTest {
     TsFileProcessor tsFileProcessor = tsFileResource.getProcessor();
     tsFileProcessor.getFlushingMemTable().addLast(tsFileProcessor.getWorkMemTable());
 
-    // delete data which is in memtable
+    // delete data which is not in memtable
     processor.delete(new PartialPath("root.vehicle.d2.s0"), 50, 70, 0, null);
 
     // delete data which is not in memtable
     processor.delete(new PartialPath("root.vehicle.d200.s0"), 50, 70, 0, null);
 
     processor.syncCloseAllWorkingTsFileProcessors();
+    Assert.assertFalse(tsFileResource.getModFile().exists());
+  }
+
+  @Test
+  public void testDeleteDataInSeqFlushingMemtable()
+      throws IllegalPathException, WriteProcessException, TriggerExecutionException, IOException {
+    for (int j = 100; j < 200; j++) {
+      TSRecord record = new TSRecord(j, deviceId);
+      record.addTuple(DataPoint.getDataPoint(TSDataType.INT32, measurementId, String.valueOf(j)));
+      processor.insert(new InsertRowPlan(record));
+    }
+    TsFileResource tsFileResource = processor.getTsFileResourceManager().getTsFileList(true).get(0);
+    TsFileProcessor tsFileProcessor = tsFileResource.getProcessor();
+    tsFileProcessor.getFlushingMemTable().addLast(tsFileProcessor.getWorkMemTable());
+
+    // delete data which is not in flushing memtable
+    processor.delete(new PartialPath("root.vehicle.d0.s0"), 50, 99, 0, null);
+    processor.delete(new PartialPath("root.vehicle.d200.s0"), 50, 70, 0, null);
+
+    // delete data which is in flushing memtable
+    processor.delete(new PartialPath("root.vehicle.d0.s0"), 50, 100, 0, null);
+    processor.delete(new PartialPath("root.vehicle.d0.s0"), 50, 150, 0, null);
+    processor.delete(new PartialPath("root.vehicle.d0.s0"), 100, 300, 0, null);
+
+    processor.syncCloseAllWorkingTsFileProcessors();
     Assert.assertTrue(tsFileResource.getModFile().exists());
-    Assert.assertEquals(2, tsFileResource.getModFile().getModifications().size());
+    Assert.assertEquals(3, tsFileResource.getModFile().getModifications().size());
+  }
+
+  @Test
+  public void testDeleteDataInUnSeqFlushingMemtable()
+      throws IllegalPathException, WriteProcessException, TriggerExecutionException, IOException {
+    for (int j = 100; j < 200; j++) {
+      TSRecord record = new TSRecord(j, deviceId);
+      record.addTuple(DataPoint.getDataPoint(TSDataType.INT32, measurementId, String.valueOf(j)));
+      processor.insert(new InsertRowPlan(record));
+    }
+    TsFileResource tsFileResource = processor.getTsFileResourceManager().getTsFileList(true).get(0);
+
+    // delete data which is not in work memtable
+    processor.delete(new PartialPath("root.vehicle.d0.s0"), 50, 99, 0, null);
+    processor.delete(new PartialPath("root.vehicle.d200.s0"), 50, 70, 0, null);
+
+    // delete data which is in work memtable
+    processor.delete(new PartialPath("root.vehicle.d0.s0"), 50, 100, 0, null);
+    processor.delete(new PartialPath("root.vehicle.d0.s0"), 50, 150, 0, null);
+    processor.delete(new PartialPath("root.vehicle.d0.s0"), 100, 300, 0, null);
+
+    processor.syncCloseAllWorkingTsFileProcessors();
+    Assert.assertFalse(tsFileResource.getModFile().exists());
+
+    // insert unseq data points
+    for (int j = 50; j < 100; j++) {
+      TSRecord record = new TSRecord(j, deviceId);
+      record.addTuple(DataPoint.getDataPoint(TSDataType.INT32, measurementId, String.valueOf(j)));
+      processor.insert(new InsertRowPlan(record));
+    }
+    // delete data which is not in work memtable
+    processor.delete(new PartialPath("root.vehicle.d0.s0"), 200, 299, 0, null);
+    processor.delete(new PartialPath("root.vehicle.d200.s0"), 50, 70, 0, null);
+
+    // delete data which is in work memtable
+    processor.delete(new PartialPath("root.vehicle.d0.s0"), 80, 85, 0, null);
+
+    Assert.assertFalse(tsFileResource.getModFile().exists());
+
+    tsFileResource = processor.getTsFileResourceManager().getTsFileList(false).get(0);
+    TsFileProcessor tsFileProcessor = tsFileResource.getProcessor();
+    tsFileProcessor.getFlushingMemTable().addLast(tsFileProcessor.getWorkMemTable());
+
+    // delete data which is not in flushing memtable
+    processor.delete(new PartialPath("root.vehicle.d0.s0"), 0, 49, 0, null);
+    processor.delete(new PartialPath("root.vehicle.d0.s0"), 100, 200, 0, null);
+    processor.delete(new PartialPath("root.vehicle.d200.s0"), 50, 70, 0, null);
+
+    // delete data which is in flushing memtable
+    processor.delete(new PartialPath("root.vehicle.d0.s0"), 25, 50, 0, null);
+    processor.delete(new PartialPath("root.vehicle.d0.s0"), 50, 80, 0, null);
+    processor.delete(new PartialPath("root.vehicle.d0.s0"), 99, 150, 0, null);
+
+    processor.syncCloseAllWorkingTsFileProcessors();
+    Assert.assertTrue(tsFileResource.getModFile().exists());
+    Assert.assertEquals(3, tsFileResource.getModFile().getModifications().size());
   }
 
   class DummySGP extends VirtualStorageGroupProcessor {
