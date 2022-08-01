9e62a600124a7b87b1292b2c31459a249b1a756e
lyssom <569810240@qq.com>
2022-08-01 11:36:19 +0800

    fix it

--- integration-test/src/test/java/org/apache/iotdb/db/it/IoTDBDeletePartitionIT.java
+++ integration-test/src/test/java/org/apache/iotdb/db/it/IoTDBDeletePartitionIT.java
@@ -34,9 +34,6 @@ import org.junit.runner.RunWith;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.Statement;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 
@@ -49,33 +46,13 @@ public class IoTDBDeletePartitionIT {
   @Before
   public void setUp() throws Exception {
     ConfigFactory.getConfig().setEnablePartition(true);
-    ConfigFactory.getConfig().setPartitionInterval(100);
-    insertData();
+    ConfigFactory.getConfig().setPartitionInterval(partitionInterval);
   }
 
   @After
   public void tearDown() throws Exception {
     ConfigFactory.getConfig().setEnablePartition(false);
     ConfigFactory.getConfig().setPartitionInterval(-1);
-  }
-
-  @Test
-  public void testSQLRemovePartition() {
-    try (Connection connection = EnvFactory.getEnv().getConnection();
-        Statement statement = connection.createStatement()) {
-      statement.execute("DELETE PARTITION root.test2 0,1,2,3,4");
-      try (ResultSet resultSet = statement.executeQuery("SELECT * FROM root.test2")) {
-        int count = 0;
-        while (resultSet.next()) {
-          assertEquals(count / 2 * 100 + count % 2 * 50 + 500, resultSet.getLong(1));
-          assertEquals(count / 2 * 100 + count % 2 * 50 + 500, resultSet.getLong(2));
-          count++;
-        }
-        assertEquals(10, count);
-      }
-    } catch (Exception e) {
-      e.printStackTrace();
-    }
   }
 
   @Test
@@ -170,51 +147,6 @@ public class IoTDBDeletePartitionIT {
       statement.execute("flush");
       try (ResultSet resultSet = statement.executeQuery("select * from root.test.wf02.wt02")) {
         assertEquals(true, resultSet.next());
-      }
-    } catch (Exception e) {
-      e.printStackTrace();
-    }
-  }
-
-  private static void insertData() throws ClassNotFoundException {
-    List<String> sqls =
-        new ArrayList<>(
-            Arrays.asList(
-                "SET STORAGE GROUP TO root.test1",
-                "SET STORAGE GROUP TO root.test2",
-                "CREATE TIMESERIES root.test1.s0 WITH DATATYPE=INT64,ENCODING=PLAIN",
-                "CREATE TIMESERIES root.test2.s0 WITH DATATYPE=INT64,ENCODING=PLAIN"));
-    // 10 partitions, each one with one seq file and one unseq file
-    for (int i = 0; i < 10; i++) {
-      // seq files
-      for (int j = 1; j <= 2; j++) {
-        sqls.add(
-            String.format(
-                "INSERT INTO root.test%d(timestamp, s0) VALUES (%d, %d)",
-                j, i * partitionInterval + 50, i * partitionInterval + 50));
-      }
-      // last file is unclosed
-      if (i < 9) {
-        sqls.add("FLUSH");
-      }
-      // unseq files
-      for (int j = 1; j <= 2; j++) {
-        sqls.add(
-            String.format(
-                "INSERT INTO root.test%d(timestamp, s0) VALUES (%d, %d)",
-                j, i * partitionInterval, i * partitionInterval));
-      }
-      sqls.add("MERGE");
-      // last file is unclosed
-      if (i < 9) {
-        sqls.add("FLUSH");
-      }
-    }
-    try (Connection connection = EnvFactory.getEnv().getConnection();
-        Statement statement = connection.createStatement()) {
-
-      for (String sql : sqls) {
-        statement.execute(sql);
       }
     } catch (Exception e) {
       e.printStackTrace();
