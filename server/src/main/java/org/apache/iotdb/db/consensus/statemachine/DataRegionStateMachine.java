dc06fca1b9f270b304c8145182b13d2048b98e84
Jinrui.Zhang <xingtanzjr@gmail.com>
2022-08-09 15:01:59 +0800

    add another constraint to keep the write order as far as possible

--- server/src/main/java/org/apache/iotdb/db/consensus/statemachine/DataRegionStateMachine.java
+++ server/src/main/java/org/apache/iotdb/db/consensus/statemachine/DataRegionStateMachine.java
@@ -155,12 +155,21 @@ public class DataRegionStateMachine extends BaseStateMachine {
           boolean timeout =
               !queueSortCondition.await(CACHE_WINDOW_TIME_IN_MS, TimeUnit.MILLISECONDS);
           if (timeout) {
-            logger.info(
-                "waiting target request timeout. current index: {}, target index: {}",
-                insertNodeWrapper.getStartSyncIndex(),
-                nextSyncIndex);
-            requestCache.remove(insertNodeWrapper);
-            break;
+            if (requestCache.peek().getStartSyncIndex() == insertNodeWrapper.getStartSyncIndex()) {
+              // current thread hold the peek request thus it can write the peek immediately.
+              logger.info(
+                  "waiting target request timeout. current index: {}, target index: {}",
+                  insertNodeWrapper.getStartSyncIndex(),
+                  nextSyncIndex);
+              requestCache.remove(insertNodeWrapper);
+              break;
+            } else {
+              // although the timeout is triggered, current thread cannot write its request
+              // because there should be some other thread who hold the peek request.
+              // And current thread should signal all the other threads to let the thread
+              // who holds the peed request to execute write operation.
+              queueSortCondition.signalAll();
+            }
           }
         } catch (InterruptedException e) {
           logger.warn(
