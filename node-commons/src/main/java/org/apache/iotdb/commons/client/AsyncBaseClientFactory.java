92f0fc2b5361cbd55040e9f9ae0986f773403e91
renyuhua <ryh13233464493@163.com>
2022-08-09 18:35:09 +0800

    add synchronized

--- node-commons/src/main/java/org/apache/iotdb/commons/client/AsyncBaseClientFactory.java
+++ node-commons/src/main/java/org/apache/iotdb/commons/client/AsyncBaseClientFactory.java
@@ -39,15 +39,15 @@ public abstract class AsyncBaseClientFactory<K, V> extends BaseClientFactory<K, 
       ClientFactoryProperty clientFactoryProperty,
       String threadName) {
     super(clientManager, clientFactoryProperty);
-    tManagers = new TAsyncClientManager[clientFactoryProperty.getSelectorNumOfAsyncClientPool()];
-    for (int i = 0; i < tManagers.length; i++) {
-      try {
-        tManagers[i] = new TAsyncClientManager();
-      } catch (IOException e) {
-        logger.error("Cannot create Async client factory", e);
-      }
-    }
     synchronized (this) {
+      tManagers = new TAsyncClientManager[clientFactoryProperty.getSelectorNumOfAsyncClientPool()];
+      for (int i = 0; i < tManagers.length; i++) {
+        try {
+          tManagers[i] = new TAsyncClientManager();
+        } catch (IOException e) {
+          logger.error("Cannot create Async client factory", e);
+        }
+      }
       Thread.getAllStackTraces().keySet().stream()
           .filter(thread -> thread.getName().contains(THRIFT_THREAD_NAME))
           .collect(Collectors.toList())
