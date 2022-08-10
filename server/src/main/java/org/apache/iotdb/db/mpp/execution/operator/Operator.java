4fc628898a0087f273b36357123806fca2504079
JackieTien97 <jackietien97@gmail.com>
2022-08-10 19:06:59 +0800

    add calculateRetainedSizeAfterCallingNext in Operator

--- server/src/main/java/org/apache/iotdb/db/mpp/execution/operator/Operator.java
+++ server/src/main/java/org/apache/iotdb/db/mpp/execution/operator/Operator.java
@@ -59,6 +59,8 @@ public interface Operator extends AutoCloseable {
    * be like: long estimatedOfCurrentOperator = XXXXX; return max(estimatedOfCurrentOperator,
    * child1.calculateMaxPeekMemory(), child2.calculateMaxPeekMemory(), ....)
    *
+   * Each operator's MaxPeekMemory should also take retained size of each child operator into account.
+   *
    * @return estimated max memory footprint that the Operator Tree(rooted from this operator) will
    *     use while doing its own query processing
    */
@@ -69,6 +71,15 @@ public interface Operator extends AutoCloseable {
   // TODO remove the default while completing all the operators
   /** @return estimated max memory footprint for returned TsBlock when calling operator.next() */
   default long calculateMaxReturnSize() {
+    return 0L;
+  }
+
+  // TODO remove the default while completing all the operators
+
+  /**
+   * @return each operator's retained size after calling its next() method
+   */
+  default long calculateRetainedSizeAfterCallingNext() {
     return 0L;
   }
 }
