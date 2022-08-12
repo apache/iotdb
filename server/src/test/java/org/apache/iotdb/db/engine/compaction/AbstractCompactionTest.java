bc8d6d0ed588213ac469c3e097308e72eca0fbbb
Liu Xuxin <liuxuxin@outlook.com>
2022-08-12 18:11:48 +0800

    fix test

--- server/src/test/java/org/apache/iotdb/db/engine/compaction/AbstractCompactionTest.java
+++ server/src/test/java/org/apache/iotdb/db/engine/compaction/AbstractCompactionTest.java
@@ -187,6 +187,12 @@ public class AbstractCompactionTest {
           isAlign,
           isSeq);
     }
+    // sleep a moment to avoid generating files with same timestamp
+    try {
+      Thread.sleep(10);
+    } catch (Exception e) {
+
+    }
   }
 
   private void addResource(
