992f9b7336262c19df0d052aa6b2c9579202f8cb
任宇华 <79885238+RYH61@users.noreply.github.com>
2022-08-15 10:39:45 +0800

    Revert "[TO rel/0.13] [IOTDB-4018] The path in the permissions contains wildcards * and ** path adaptation does not work" (#6992)

--- server/src/main/java/org/apache/iotdb/db/auth/entity/User.java
+++ server/src/main/java/org/apache/iotdb/db/auth/entity/User.java
@@ -18,7 +18,6 @@
  */
 package org.apache.iotdb.db.auth.entity;
 
-import org.apache.iotdb.db.auth.AuthException;
 import org.apache.iotdb.db.utils.AuthUtils;
 import org.apache.iotdb.db.utils.SerializeUtils;
 
@@ -134,11 +133,11 @@ public class User {
     return roleList.contains(roleName);
   }
 
-  public Set<Integer> getPrivileges(String path) throws AuthException {
+  public Set<Integer> getPrivileges(String path) {
     return AuthUtils.getPrivileges(path, privilegeList);
   }
 
-  public boolean checkPrivilege(String path, int privilegeId) throws AuthException {
+  public boolean checkPrivilege(String path, int privilegeId) {
     return AuthUtils.checkPrivilege(path, privilegeId, privilegeList);
   }
 
