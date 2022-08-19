235ebd5855203e131eb056bd184f19119a4b00c0
caozhijia <caozhijia@126.com>
2022-08-19 17:40:23 +0800

    [IOTDB-4154]TsFile export tool

--- cli/src/assembly/resources/tools/export-tsfile.bat
+++ cli/src/assembly/resources/tools/export-tsfile.bat
@@ -28,7 +28,7 @@ pushd %~dp0..
 if NOT DEFINED IOTDB_HOME set IOTDB_HOME=%CD%
 popd
 
-if NOT DEFINED MAIN_CLASS set MAIN_CLASS=org.apache.iotdb.tool.ExportTSFile
+if NOT DEFINED MAIN_CLASS set MAIN_CLASS=org.apache.iotdb.tool.ExportTsFile
 if NOT DEFINED JAVA_HOME goto :err
 
 @REM -----------------------------------------------------------------------------
