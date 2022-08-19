9e054a60e1f72f2bc1333a0581354bee3c887a9e
Onii Chan <46487694+sogood99@users.noreply.github.com>
2022-08-19 16:08:52 +0800

    add volatile to migrationtask

--- server/src/main/java/org/apache/iotdb/db/engine/migration/MigrationTask.java
+++ server/src/main/java/org/apache/iotdb/db/engine/migration/MigrationTask.java
@@ -24,12 +24,12 @@ import java.io.File;
 
 /** Data class for each Migration Task */
 public class MigrationTask {
-  private long taskId;
-  private PartialPath storageGroup;
-  private File targetDir;
-  private long startTime;
-  private long ttl;
-  private MigrationTaskStatus status = MigrationTaskStatus.READY;
+  private volatile long taskId;
+  private volatile PartialPath storageGroup;
+  private volatile File targetDir;
+  private volatile long startTime;
+  private volatile long ttl;
+  private volatile MigrationTaskStatus status = MigrationTaskStatus.READY;
 
   public MigrationTask(
       long taskId, PartialPath storageGroup, File targetDir, long ttl, long startTime) {
