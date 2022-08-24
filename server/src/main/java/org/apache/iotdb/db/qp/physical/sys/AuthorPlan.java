7b7d32d18375c9ded40c7e5c64deac74efe1997c
spricoder <spricoder@qq.com>
2022-08-24 11:20:42 +0800

    rename `GRANT_USER_ROLE` to `GRANT_ROLE_TO_USER` and `REVOKE_USER_ROLE` to `REVOKE_ROLE_TO_USER`

--- server/src/main/java/org/apache/iotdb/db/qp/physical/sys/AuthorPlan.java
+++ server/src/main/java/org/apache/iotdb/db/qp/physical/sys/AuthorPlan.java
@@ -105,10 +105,10 @@ public class AuthorPlan extends PhysicalPlan {
         this.setOperatorType(Operator.OperatorType.MODIFY_PASSWORD);
         break;
       case GRANT_ROLE_TO_USER:
-        this.setOperatorType(Operator.OperatorType.GRANT_USER_ROLE);
+        this.setOperatorType(Operator.OperatorType.GRANT_ROLE_TO_USER);
         break;
       case REVOKE_ROLE_FROM_USER:
-        this.setOperatorType(Operator.OperatorType.REVOKE_USER_ROLE);
+        this.setOperatorType(Operator.OperatorType.REVOKE_ROLE_TO_USER);
         break;
       case LIST_USER_PRIVILEGE:
         this.setQuery(true);
@@ -156,7 +156,7 @@ public class AuthorPlan extends PhysicalPlan {
       case CREATE_USER:
         type = AuthorType.CREATE_USER;
         break;
-      case REVOKE_USER_ROLE:
+      case REVOKE_ROLE_TO_USER:
         type = AuthorType.REVOKE_ROLE_FROM_USER;
         break;
       case REVOKE_ROLE_PRIVILEGE:
@@ -171,7 +171,7 @@ public class AuthorPlan extends PhysicalPlan {
       case GRANT_USER_PRIVILEGE:
         type = AuthorType.GRANT_USER;
         break;
-      case GRANT_USER_ROLE:
+      case GRANT_ROLE_TO_USER:
         type = AuthorType.GRANT_ROLE_TO_USER;
         break;
       case MODIFY_PASSWORD:
@@ -390,8 +390,8 @@ public class AuthorPlan extends PhysicalPlan {
       case CREATE_USER:
         type = PhysicalPlanType.CREATE_USER.ordinal();
         break;
-      case REVOKE_USER_ROLE:
-        type = PhysicalPlanType.REVOKE_USER_ROLE.ordinal();
+      case REVOKE_ROLE_TO_USER:
+        type = PhysicalPlanType.REVOKE_ROLE_TO_USER.ordinal();
         break;
       case REVOKE_ROLE_PRIVILEGE:
         type = PhysicalPlanType.REVOKE_ROLE_PRIVILEGE.ordinal();
@@ -405,8 +405,8 @@ public class AuthorPlan extends PhysicalPlan {
       case GRANT_USER_PRIVILEGE:
         type = PhysicalPlanType.GRANT_USER_PRIVILEGE.ordinal();
         break;
-      case GRANT_USER_ROLE:
-        type = PhysicalPlanType.GRANT_USER_ROLE.ordinal();
+      case GRANT_ROLE_TO_USER:
+        type = PhysicalPlanType.GRANT_ROLE_TO_USER.ordinal();
         break;
       case MODIFY_PASSWORD:
         type = PhysicalPlanType.MODIFY_PASSWORD.ordinal();
