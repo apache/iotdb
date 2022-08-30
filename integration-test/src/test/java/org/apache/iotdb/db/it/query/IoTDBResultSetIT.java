fc4cda7592abdd4fb614b875bdb8332b7b7e12f1
任宇华 <79885238+RYH61@users.noreply.github.com>
2022-08-30 09:10:49 +0800

    [IOTDB-4253] Modify the jdbc query time column to be empty (#7156)

--- integration-test/src/test/java/org/apache/iotdb/db/it/query/IoTDBResultSetIT.java
+++ integration-test/src/test/java/org/apache/iotdb/db/it/query/IoTDBResultSetIT.java
@@ -185,6 +185,45 @@ public class IoTDBResultSetIT {
   }
 
   @Test
+  public void timeWasNullTest() throws Exception {
+    try (Connection connection = EnvFactory.getEnv().getConnection();
+        Statement statement = connection.createStatement()) {
+
+      // create timeseries
+      statement.execute(
+          "CREATE TIMESERIES root.sg1.d1.s1 WITH DATATYPE=INT64, ENCODING=RLE, COMPRESSOR=SNAPPY");
+      statement.execute(
+          "CREATE TIMESERIES root.sg1.d1.s2 WITH DATATYPE=INT64, ENCODING=RLE, COMPRESSOR=SNAPPY");
+      statement.execute(
+          "CREATE TIMESERIES root.sg1.d1.s3 WITH DATATYPE=INT64, ENCODING=RLE, COMPRESSOR=SNAPPY");
+
+      for (int i = 0; i < 10; i++) {
+        statement.addBatch(
+            "insert into root.sg1.d1(timestamp, s1, s2) values(" + i + "," + 1 + "," + 1 + ")");
+      }
+
+      statement.execute("insert into root.sg1.d1(timestamp, s3) values(103, 1)");
+      statement.execute("insert into root.sg1.d1(timestamp, s3) values(104, 1)");
+      statement.execute("insert into root.sg1.d1(timestamp, s3) values(105, 1)");
+      statement.executeBatch();
+      try (ResultSet resultSet = statement.executeQuery("select * from root.**")) {
+        ResultSetMetaData metaData = resultSet.getMetaData();
+        int columnCount = metaData.getColumnCount();
+        while (resultSet.next()) {
+          for (int i = 1; i <= columnCount; i++) {
+            int ct = metaData.getColumnType(i);
+            if (ct == Types.TIMESTAMP) {
+              if (resultSet.wasNull()) {
+                fail();
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+
+  @Test
   public void emptyLastQueryTest() {
     String expectedHeader =
         ColumnHeaderConstant.COLUMN_TIME
