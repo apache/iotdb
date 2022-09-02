41f74a578f167f3b2d9c67bdb910ee6b54c6dc4d
Weihao Li <60659567+Wei-hao-Li@users.noreply.github.com>
2022-09-02 17:04:10 +0800

    [IOTDB-4304]Add TriggerTable and TriggerInformation (#7208)

--- thrift-confignode/src/main/thrift/confignode.thrift
+++ thrift-confignode/src/main/thrift/confignode.thrift
@@ -231,6 +231,17 @@ struct TDropFunctionReq {
 }
 
 // Trigger
+enum TTriggerState {
+  // The intermediate state of Create trigger, the trigger need to create has not yet activated on any DataNodes.
+  INACTIVE
+  // The intermediate state of Create trigger, the trigger need to create has activated on some DataNodes.
+  PARTIAL_ACTIVE
+  // Triggers on all DataNodes are available.
+  ACTIVE
+  // The intermediate state of Drop trigger, the cluster is in the process of removing the trigger.
+  DROPPING
+}
+
 struct TCreateTriggerReq {
   1: required string triggerName
   2: required binary triggerInformation
