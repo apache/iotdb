39cd5d18d1c14ec056eed7e0e62fc728d57a3be3
yschengzi <yscysc99@126.com>
2022-09-16 15:50:50 +0800

    add license

--- server/src/main/java/org/apache/iotdb/db/engine/load/DeletionData.java
+++ server/src/main/java/org/apache/iotdb/db/engine/load/DeletionData.java
@@ -1,3 +1,22 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 package org.apache.iotdb.db.engine.load;
 
 import org.apache.iotdb.commons.exception.IllegalPathException;
@@ -28,7 +47,7 @@ public class DeletionData implements TsFileData {
   public void writeToFileWriter(TsFileIOWriter writer) throws IOException {
     File tsFile = writer.getFile();
     try (ModificationFile modificationFile =
-        new ModificationFile(tsFile.getAbsolutePath() + ModificationFile.FILE_SUFFIX)) {
+                 new ModificationFile(tsFile.getAbsolutePath() + ModificationFile.FILE_SUFFIX)) {
       deletion.setFileOffset(tsFile.length());
       modificationFile.write(deletion);
     }
@@ -46,7 +65,7 @@ public class DeletionData implements TsFileData {
   }
 
   public static DeletionData deserialize(InputStream stream)
-      throws IllegalPathException, IOException {
+          throws IllegalPathException, IOException {
     return new DeletionData(Deletion.deserializeWithoutFileOffset(new DataInputStream(stream)));
   }
 }
