5f6e376e999295fb09baa81464e45cd0e849c4b8
Itami Sho <1036788120@qq.com>
2022-09-16 08:23:37 +0800

    spotless:apply

--- integration-test/src/test/java/org/apache/iotdb/session/IoTDBSessionDisableMemControlIT.java
+++ integration-test/src/test/java/org/apache/iotdb/session/IoTDBSessionDisableMemControlIT.java
@@ -38,8 +38,6 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 import org.junit.runner.RunWith;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
 import java.util.List;
