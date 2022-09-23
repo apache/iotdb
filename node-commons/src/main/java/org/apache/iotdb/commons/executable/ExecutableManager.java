0abbb910990f1940d8fccb30e198236baf7b6b02
Liao Lanyu <1435078631@qq.com>
2022-09-23 17:31:36 +0800

    Trigger test (#7419)

--- node-commons/src/main/java/org/apache/iotdb/commons/executable/ExecutableManager.java
+++ node-commons/src/main/java/org/apache/iotdb/commons/executable/ExecutableManager.java
@@ -59,27 +59,33 @@ public class ExecutableManager {
   public ExecutableResource request(List<String> uris) throws URISyntaxException, IOException {
     final long requestId = generateNextRequestId();
     downloadExecutables(uris, requestId);
-    return new ExecutableResource(requestId, getDirStringByRequestId(requestId));
+    return new ExecutableResource(requestId, getDirStringUnderTempRootByRequestId(requestId));
   }
 
-  public void moveToExtLibDir(ExecutableResource resource, String name) throws IOException {
-    FileUtils.moveDirectory(getDirByRequestId(resource.getRequestId()), getDirByName(name));
+  public void moveTempDirToExtLibDir(ExecutableResource resource, String name) throws IOException {
+    FileUtils.moveDirectory(
+        getDirUnderTempRootByRequestId(resource.getRequestId()), getDirUnderLibRootByName(name));
+  }
+
+  public void moveFileUnderTempRootToExtLibDir(ExecutableResource resource, String name)
+      throws IOException {
+    FileUtils.moveFileToDirectory(
+        getFileByFullPath(
+            getDirStringUnderTempRootByRequestId(resource.getRequestId()) + File.separator + name),
+        getFileByFullPath(libRoot),
+        false);
   }
 
   public void removeFromTemporaryLibRoot(ExecutableResource resource) {
     removeFromTemporaryLibRoot(resource.getRequestId());
   }
 
-  public void removeFromExtLibDir(String functionName) {
-    FileUtils.deleteQuietly(getDirByName(functionName));
-  }
-
   private synchronized long generateNextRequestId() throws IOException {
     long requestId = requestCounter.getAndIncrement();
-    while (FileUtils.isDirectory(getDirByRequestId(requestId))) {
+    while (FileUtils.isDirectory(getDirUnderTempRootByRequestId(requestId))) {
       requestId = requestCounter.getAndIncrement();
     }
-    FileUtils.forceMkdir(getDirByRequestId(requestId));
+    FileUtils.forceMkdir(getDirUnderTempRootByRequestId(requestId));
     return requestId;
   }
 
@@ -101,27 +107,40 @@ public class ExecutableManager {
   }
 
   private void removeFromTemporaryLibRoot(long requestId) {
-    FileUtils.deleteQuietly(getDirByRequestId(requestId));
+    FileUtils.deleteQuietly(getDirUnderTempRootByRequestId(requestId));
   }
 
   /////////////////////////////////////////////////////////////////////////////////////////////////
   // dir string and dir file generation
   /////////////////////////////////////////////////////////////////////////////////////////////////
 
-  public File getDirByRequestId(long requestId) {
-    return FSFactoryProducer.getFSFactory().getFile(getDirStringByRequestId(requestId));
+  public File getDirUnderTempRootByRequestId(long requestId) {
+    return FSFactoryProducer.getFSFactory()
+        .getFile(getDirStringUnderTempRootByRequestId(requestId));
   }
 
-  public String getDirStringByRequestId(long requestId) {
+  public String getDirStringUnderTempRootByRequestId(long requestId) {
     return temporaryLibRoot + File.separator + requestId + File.separator;
   }
 
-  public File getDirByName(String name) {
-    return FSFactoryProducer.getFSFactory().getFile(getDirStringByName(name));
+  public File getDirUnderLibRootByName(String name) {
+    return FSFactoryProducer.getFSFactory().getFile(getDirStringUnderLibRootByName(name));
   }
 
-  public String getDirStringByName(String name) {
+  public String getDirStringUnderLibRootByName(String name) {
     return libRoot + File.separator + name + File.separator;
+  }
+
+  public File getFileUnderLibRootByName(String name) {
+    return FSFactoryProducer.getFSFactory().getFile(getFileStringUnderLibRootByName(name));
+  }
+
+  public String getFileStringUnderLibRootByName(String name) {
+    return libRoot + File.separator + name;
+  }
+
+  private File getFileByFullPath(String path) {
+    return FSFactoryProducer.getFSFactory().getFile(path);
   }
 
   /////////////////////////////////////////////////////////////////////////////////////////////////
