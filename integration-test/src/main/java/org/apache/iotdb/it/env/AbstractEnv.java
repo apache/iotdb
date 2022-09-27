6d6a4ccac783eb2872be4436dea1cff50386b9fd
YongzaoDan <532741407@qq.com>
2022-09-27 11:31:00 +0800

    snapshot test code

--- integration-test/src/main/java/org/apache/iotdb/it/env/AbstractEnv.java
+++ integration-test/src/main/java/org/apache/iotdb/it/env/AbstractEnv.java
@@ -380,6 +380,7 @@ public abstract class AbstractEnv implements BaseEnv {
                 new DataNodeClientPoolFactory.SyncConfigNodeIServiceClientPoolFactory());
     for (int i = 0; i < 30; i++) {
       try {
+        // Return ConfigNode connection of the Seed-ConfigNode
         return clientManager.borrowClient(
             new TEndPoint(
                 configNodeWrapperList.get(0).getIp(), configNodeWrapperList.get(0).getPort()));
