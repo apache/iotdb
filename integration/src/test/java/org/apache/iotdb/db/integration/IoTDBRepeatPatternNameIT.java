d8c3429d19a8c482dc54dce58c7ed5e30c787276
LiuXuxin <liuxuxin@outlook.com>
2022-10-11 20:23:36 +0800

    reset the test

--- integration/src/test/java/org/apache/iotdb/db/integration/IoTDBRepeatPatternNameIT.java
+++ integration/src/test/java/org/apache/iotdb/db/integration/IoTDBRepeatPatternNameIT.java
@@ -20,6 +20,7 @@
 package org.apache.iotdb.db.integration;
 
 import org.apache.iotdb.integration.env.EnvFactory;
+import org.apache.iotdb.jdbc.IoTDBSQLException;
 
 import org.junit.After;
 import org.junit.Assert;
@@ -29,7 +30,7 @@ import org.junit.Test;
 import java.sql.Connection;
 import java.sql.Statement;
 
-public class IoTDBLargePatternNameIT {
+public class IoTDBRepeatPatternNameIT {
   @Before
   public void startUp() throws Exception {
     EnvFactory.getEnv().initBeforeClass();
@@ -46,20 +47,21 @@ public class IoTDBLargePatternNameIT {
     sb.append("insert into root.ln.wf01.wt01(timestamp,status,s) values(1509465780000,false,'");
     // we should make sure that the pattern is repeated enough time to make exception occurs
     // so that system can pass the test
-    for (int i = 0; i < 1024; ++i) {
+    for (int i = 0; i < 20; ++i) {
       sb.append('a');
     }
     sb.append("b');");
+    long startTime = System.currentTimeMillis();
     try (Connection connection = EnvFactory.getEnv().getConnection();
         Statement statement = connection.createStatement()) {
       statement.execute(sb.toString());
       try {
         statement.execute("select s from root.ln.wf01.wt01 where s REGEXP'(a+)+s'");
-      } catch (Exception e) {
+      } catch (IoTDBSQLException e) {
         Assert.assertTrue(e.getMessage().contains("Pattern access threshold exceeded"));
-        return;
       }
-      Assert.fail();
+      long timeCost = System.currentTimeMillis() - startTime;
+      Assert.assertTrue(timeCost < 5_000L);
     }
   }
 }
