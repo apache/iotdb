f2f33f684c82eb7d1cf8a0021aeb0ce91377b00b
Beyyes <cgf1993@foxmail.com>
2022-10-13 21:46:45 +0800

    remove comment code

--- confignode/src/main/java/org/apache/iotdb/confignode/service/thrift/ConfigNodeRPCServiceProcessor.java
+++ confignode/src/main/java/org/apache/iotdb/confignode/service/thrift/ConfigNodeRPCServiceProcessor.java
@@ -458,21 +458,6 @@ public class ConfigNodeRPCServiceProcessor implements IConfigNodeRPCService.Ifac
               "remove ConsensusGroup failed because internal failure. See other logs for more details");
     }
 
-    //    List<TConfigNodeLocation> registeredConfigNodes =
-    //            configManager.getNodeManager().getRegisteredConfigNodes();
-    //    registeredConfigNodes.remove(configNodeLocation);
-    //    try {
-    //      // The removed ConfigNode also need to update the confignode-system.properties file
-    //      // Because user may execute remove-confignode.sh in this node
-    //      SystemPropertiesUtils.storeConfigNodeList(new ArrayList<>(registeredConfigNodes));
-    //      LOGGER.info(
-    //              "Updated ConfigNode information files after deleteConfigNodePeer. {}.",
-    //              registeredConfigNodes);
-    //    } catch (IOException e) {
-    //      LOGGER.error("Current ConfigNode to be removed can't update ConfigNode information.",
-    // e);
-    //    }
-
     return new TSStatus(TSStatusCode.SUCCESS_STATUS.getStatusCode())
         .setMessage("remove ConsensusGroup success.");
   }
