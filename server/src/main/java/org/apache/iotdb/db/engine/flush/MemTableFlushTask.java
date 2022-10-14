8f92ecdf8f179778e4b072312e15ae440f2cc84f
Haonan <hhaonan@outlook.com>
2022-10-14 17:44:49 +0800

    [To rel/0.13][IOTDB-4636] Add check to avoid flush empty chunk group (#7598)

--- server/src/main/java/org/apache/iotdb/db/engine/flush/MemTableFlushTask.java
+++ server/src/main/java/org/apache/iotdb/db/engine/flush/MemTableFlushTask.java
@@ -122,12 +122,18 @@ public class MemTableFlushTask {
     // sort the IDeviceID in lexicographical order
     deviceIDList.sort(Comparator.comparing(IDeviceID::toStringID));
     for (IDeviceID deviceID : deviceIDList) {
-      encodingTaskQueue.put(new StartFlushGroupIOTask(deviceID.toStringID()));
-
       final Map<String, IWritableMemChunk> value = memTableMap.get(deviceID).getMemChunkMap();
+      // skip the empty device/chunk group
+      if (memTableMap.get(deviceID).count() == 0 || value.isEmpty()) {
+        continue;
+      }
+      encodingTaskQueue.put(new StartFlushGroupIOTask(deviceID.toStringID()));
       for (Map.Entry<String, IWritableMemChunk> iWritableMemChunkEntry : value.entrySet()) {
         long startTime = System.currentTimeMillis();
         IWritableMemChunk series = iWritableMemChunkEntry.getValue();
+        if (series.count() == 0) {
+          continue;
+        }
         /*
          * sort task (first task of flush pipeline)
          */
