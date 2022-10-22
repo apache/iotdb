b22fac39c49b48ee1b7465435c209c12942eb2cc
HTHou <hhaonan@outlook.com>
2022-10-22 22:06:31 +0800

    update

--- docker/src/main/DockerCompose/start-1c1d.sh
+++ docker/src/main/DockerCompose/start-1c1d.sh
@@ -17,5 +17,5 @@
 # specific language governing permissions and limitations
 # under the License.
 #
-. "sbin/start-confignode.sh" &
-. "sbin/start-datanode.sh" &
+./start-confignode.sh &
+./start-datanode.sh &
