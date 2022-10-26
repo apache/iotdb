2ff2f75eae6ac4fefff42aa9ff33ba2bd26b10dd
caozhijia <caozhijia@126.com>
2022-10-26 12:24:11 +0800

    feat file license header.

--- server/src/main/java/org/apache/iotdb/db/utils/AuditLogUtils.java
+++ server/src/main/java/org/apache/iotdb/db/utils/AuditLogUtils.java
@@ -1,3 +1,21 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package org.apache.iotdb.db.utils;
 
 import org.apache.iotdb.db.conf.IoTDBConfig;
@@ -50,7 +68,9 @@ public class AuditLogUtils {
       }
     } else if (LOG_LEVEL_LOGGER.equals(auditLogStorage)) {
       if (type.contains(TYPE_INSERT)) {
-        AUDIT_LOGGER.debug("user:{},type:{},action:{}", username, type, log);
+        if (config.isEnableAuditLogWrite()) {
+          AUDIT_LOGGER.debug("user:{},type:{},action:{}", username, type, log);
+        }
       } else {
         AUDIT_LOGGER.info("user:{},type:{},action:{}", username, type, log);
       }
