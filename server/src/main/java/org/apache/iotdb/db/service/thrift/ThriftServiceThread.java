778359bf34726cc269c6a305a3d3d0952dee2a67
Xiangdong Huang <hxd@apache.org>
2022-10-26 18:41:26 +0800

    [IOTDB-4741] [To rel/0.13] deprecate sessionId in all rpc requests (#7711)

--- server/src/main/java/org/apache/iotdb/db/service/thrift/ThriftServiceThread.java
+++ server/src/main/java/org/apache/iotdb/db/service/thrift/ThriftServiceThread.java
@@ -155,6 +155,7 @@ public class ThriftServiceThread extends Thread {
       TThreadPoolServer.Args poolArgs =
           initSyncedPoolArgs(processor, threadsName, maxWorkerThreads, timeoutSecond);
       poolServer = new TThreadPoolServer(poolArgs);
+      logger.warn("注册EventHandler");
       poolServer.setServerEventHandler(serverEventHandler);
     } catch (TTransportException e) {
       catchFailedInitialization(e);
