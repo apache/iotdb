55c1ea0edf5c4226b45b7521ff74e186553ebcd1
Weihao Li <60659567+Wei-hao-Li@users.noreply.github.com>
2022-10-26 16:09:09 +0800

    [IOTDB-4721] Modify Create UDF process on ConfigNode (#7730)

--- node-commons/src/main/java/org/apache/iotdb/commons/udf/service/UDFManagementService.java
+++ node-commons/src/main/java/org/apache/iotdb/commons/udf/service/UDFManagementService.java
@@ -22,7 +22,6 @@ package org.apache.iotdb.commons.udf.service;
 import org.apache.iotdb.commons.udf.UDFInformation;
 import org.apache.iotdb.commons.udf.UDFTable;
 import org.apache.iotdb.commons.udf.builtin.BuiltinAggregationFunction;
-import org.apache.iotdb.commons.udf.builtin.BuiltinTimeSeriesGeneratingFunction;
 import org.apache.iotdb.commons.utils.TestOnly;
 import org.apache.iotdb.udf.api.UDF;
 import org.apache.iotdb.udf.api.UDTF;
@@ -53,7 +52,6 @@ public class UDFManagementService {
   private UDFManagementService() {
     lock = new ReentrantLock();
     udfTable = new UDFTable();
-    registerBuiltinTimeSeriesGeneratingFunctions();
   }
 
   public void acquireLock() {
@@ -293,19 +291,6 @@ public class UDFManagementService {
     return Arrays.stream(getAllUDFInformation())
         .filter(UDFInformation::isBuiltin)
         .collect(Collectors.toList());
-  }
-
-  private void registerBuiltinTimeSeriesGeneratingFunctions() {
-    for (BuiltinTimeSeriesGeneratingFunction builtinTimeSeriesGeneratingFunction :
-        BuiltinTimeSeriesGeneratingFunction.values()) {
-      String functionName = builtinTimeSeriesGeneratingFunction.getFunctionName();
-      udfTable.addUDFInformation(
-          functionName,
-          new UDFInformation(
-              functionName, builtinTimeSeriesGeneratingFunction.getClassName(), true));
-      udfTable.addFunctionAndClass(
-          functionName.toUpperCase(), builtinTimeSeriesGeneratingFunction.getFunctionClass());
-    }
   }
 
   public boolean isUDTF(String functionName)
