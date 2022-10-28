b6ccaeb152b8254190b214da790851a88dfddac2
Liu Xuxin <liuxuxin@outlook.com>
2022-10-28 14:46:14 +0800

    add log when meets exception in sub-compaction-task

--- server/src/main/java/org/apache/iotdb/db/engine/compaction/cross/rewrite/task/SubCompactionTask.java
+++ server/src/main/java/org/apache/iotdb/db/engine/compaction/cross/rewrite/task/SubCompactionTask.java
@@ -73,25 +73,30 @@ public class SubCompactionTask implements Callable<Void> {
 
   @Override
   public Void call() throws Exception {
-    for (String measurement : measurementList) {
-      List<IMeasurementSchema> measurementSchemas =
-          Collections.singletonList(schemaMap.get(measurement));
+    try {
+      for (String measurement : measurementList) {
+        List<IMeasurementSchema> measurementSchemas =
+            Collections.singletonList(schemaMap.get(measurement));
 
-      IBatchReader dataBatchReader =
-          CompactionUtils.constructReader(
-              device,
-              Collections.singletonList(measurement),
-              measurementSchemas,
-              schemaMap.keySet(),
-              queryContext,
-              queryDataSource,
-              false);
+        IBatchReader dataBatchReader =
+            CompactionUtils.constructReader(
+                device,
+                Collections.singletonList(measurement),
+                measurementSchemas,
+                schemaMap.keySet(),
+                queryContext,
+                queryDataSource,
+                false);
 
-      if (dataBatchReader.hasNextBatch()) {
-        compactionWriter.startMeasurement(measurementSchemas, taskId);
-        CompactionUtils.writeWithReader(compactionWriter, dataBatchReader, taskId);
-        compactionWriter.endMeasurement(taskId);
+        if (dataBatchReader.hasNextBatch()) {
+          compactionWriter.startMeasurement(measurementSchemas, taskId);
+          CompactionUtils.writeWithReader(compactionWriter, dataBatchReader, taskId);
+          compactionWriter.endMeasurement(taskId);
+        }
       }
+    } catch (Throwable t) {
+      logger.error("Meets exception when executing sub-task", t);
+      throw t;
     }
     return null;
   }
