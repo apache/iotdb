87a6c26541bb29f7b0772c971f1fa8c075bcda44
spricoder <spricoder@qq.com>
2022-10-31 12:27:43 +0800

    move default metric sg to `root.__system`

--- metrics/interface/src/main/java/org/apache/iotdb/metrics/utils/IoTDBMetricsUtils.java
+++ metrics/interface/src/main/java/org/apache/iotdb/metrics/utils/IoTDBMetricsUtils.java
@@ -21,10 +21,6 @@ package org.apache.iotdb.metrics.utils;
 
 import org.apache.iotdb.metrics.config.MetricConfig;
 import org.apache.iotdb.metrics.config.MetricConfigDescriptor;
-import org.apache.iotdb.rpc.IoTDBConnectionException;
-import org.apache.iotdb.rpc.StatementExecutionException;
-import org.apache.iotdb.session.pool.SessionDataSetWrapper;
-import org.apache.iotdb.session.pool.SessionPool;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -35,13 +31,14 @@ public class IoTDBMetricsUtils {
   private static final Logger logger = LoggerFactory.getLogger(IoTDBMetricsUtils.class);
   private static final MetricConfig metricConfig =
       MetricConfigDescriptor.getInstance().getMetricConfig();
-  private static final String STORAGE_GROUP =
-      "root.system." + metricConfig.getIoTDBReporterConfig().getDatabase();
+  private static final String STORAGE_GROUP = "root.__system";
 
   public static String generatePath(String name, Map<String, String> labels) {
     StringBuilder stringBuilder = new StringBuilder();
     stringBuilder
         .append(STORAGE_GROUP)
+        .append(".")
+        .append(metricConfig.getIoTDBReporterConfig().getDatabase())
         .append(".\"")
         .append(metricConfig.getRpcAddress())
         .append(":")
@@ -61,18 +58,5 @@ public class IoTDBMetricsUtils {
           .append("\"");
     }
     return stringBuilder.toString();
-  }
-
-  public static void checkOrCreateStorageGroup(SessionPool session) {
-    try (SessionDataSetWrapper result =
-        session.executeQueryStatement("show storage group " + STORAGE_GROUP)) {
-      if (!result.hasNext()) {
-        session.setStorageGroup(STORAGE_GROUP);
-      }
-    } catch (IoTDBConnectionException e) {
-      logger.error("CheckOrCreateStorageGroup failed because ", e);
-    } catch (StatementExecutionException e) {
-      // do nothing
-    }
   }
 }
