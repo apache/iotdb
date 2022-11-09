c29f46ac03fe34b8ce47fb9abff2f778d6d25baf
Jackie Tien <jackietien97@gmail.com>
2022-11-09 09:02:53 +0800

    Change initBeforeClass to initBeforeTest (#7938)

--- integration-test/src/test/java/org/apache/iotdb/db/it/aligned/IoTDBInsertAlignedValues2IT.java
+++ integration-test/src/test/java/org/apache/iotdb/db/it/aligned/IoTDBInsertAlignedValues2IT.java
@@ -49,12 +49,12 @@ public class IoTDBInsertAlignedValues2IT {
     autoCreateSchemaEnabled = ConfigFactory.getConfig().isAutoCreateSchemaEnabled();
     ConfigFactory.getConfig().setAutoCreateSchemaEnabled(true);
     ConfigFactory.getConfig().setMaxNumberOfPointsInPage(2);
-    EnvFactory.getEnv().initBeforeClass();
+    EnvFactory.getEnv().initBeforeTest();
   }
 
   @After
   public void tearDown() throws Exception {
-    EnvFactory.getEnv().cleanAfterClass();
+    EnvFactory.getEnv().cleanAfterTest();
     ConfigFactory.getConfig().setAutoCreateSchemaEnabled(autoCreateSchemaEnabled);
     ConfigFactory.getConfig().setMaxNumberOfPointsInPage(numOfPointsPerPage);
   }
