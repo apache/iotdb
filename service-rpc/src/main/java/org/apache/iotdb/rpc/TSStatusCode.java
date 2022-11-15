3c41abf4ed0453ad33420a9fa715e543021f6be2
qiaojialin <646274302@qq.com>
2022-11-15 08:58:00 +0800

    spotless

--- service-rpc/src/main/java/org/apache/iotdb/rpc/TSStatusCode.java
+++ service-rpc/src/main/java/org/apache/iotdb/rpc/TSStatusCode.java
@@ -25,7 +25,9 @@ import java.util.Map;
 /**
  * When update this class, remember to update the following UserGuide
  *
- * <p>docs/UserGuide/API/Status-Codes.md docs/zh/UserGuide/API/Status-Codes.md
+ * <p>docs/UserGuide/API/Status-Codes.md
+ *
+ * <p>docs/zh/UserGuide/API/Status-Codes.md
  */
 public enum TSStatusCode {
   SUCCESS_STATUS(200),
