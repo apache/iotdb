3d310e5d4e2396a7a32c930b1d8c1d3dca943efc
Chen YZ <43774645+Cpaulyz@users.noreply.github.com>
2022-11-18 15:15:45 +0800

    [IOTDB-4957] Add check for create pipesink and optimize re-connection (#8019)

--- node-commons/src/main/java/org/apache/iotdb/commons/sync/utils/SyncConstant.java
+++ node-commons/src/main/java/org/apache/iotdb/commons/sync/utils/SyncConstant.java
@@ -57,15 +57,12 @@ public class SyncConstant {
   public static final String DEFAULT_PIPE_SINK_IP = "127.0.0.1";
   public static final int DEFAULT_PIPE_SINK_PORT = 6667;
 
-  public static final Long HEARTBEAT_DELAY_SECONDS = 30L;
   public static final int CONNECT_TIMEOUT_MILLISECONDS = 1_000;
   public static final int SOCKET_TIMEOUT_MILLISECONDS = 100_000;
 
   public static final Long DEFAULT_WAITING_FOR_TSFILE_CLOSE_MILLISECONDS = 500L;
   public static final Long DEFAULT_WAITING_FOR_TSFILE_RETRY_NUMBER = 10L;
   public static final Long DEFAULT_WAITING_FOR_STOP_MILLISECONDS = 1000L;
-
-  public static final int MESSAGE_NUMBER_LIMIT = 1; // do not support multi lines now
 
   /** transport */
 
@@ -74,6 +71,10 @@ public class SyncConstant {
       Math.min(16 * 1024 * 1024, RpcUtils.THRIFT_FRAME_MAX_SIZE);
 
   public static final String PATCH_SUFFIX = ".patch";
+  public static final String IPV4_PATTERN =
+      "^([1-9]|[1-9]\\d|1\\d{2}|2[0-4]\\d|25[0-5])(\\.(\\d|[1-9]\\d|1\\d{2}|2[0-4]\\d|25[0-5])){3}$";
+  public static final int HEARTBEAT_INTERVAL_MILLISECONDS = 5_000;
+  public static final int LOST_CONNECT_REPORT_MILLISECONDS = 30_000;
 
   /** receiver */
   public static final String RECEIVER_DIR_NAME = "receiver";
