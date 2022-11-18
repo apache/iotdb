f68016956b42546ae4d60273c6ff22307865d459
HTHou <hhaonan@outlook.com>
2022-11-18 14:30:52 +0800

    Add website compile CI

--- site/src/main/.vuepress/config.js
+++ site/src/main/.vuepress/config.js
@@ -964,7 +964,7 @@ var config = {
 							['Maintenance-Tools/NodeTool','Node Tool'],
 							['Maintenance-Tools/Watermark-Tool','Watermark Tool'],
 							['Maintenance-Tools/Sync-Tool','Sync Tool'],
-							['Maintenance-Tools/TsFile-Split-Tool','TsFile Split Tool']
+							['Maintenance-Tools/TsFile-Split-Tool','TsFile Split Tool'],
 						]
 					},
 					{
