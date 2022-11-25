b4ffc5f7d10f5f29b66b4fdcfb876ad12b2b0c49
YongzaoDan <532741407@qq.com>
2022-11-25 21:23:13 +0800

    Fix conversation

--- integration-test/src/main/java/org/apache/iotdb/itbase/env/BaseEnv.java
+++ integration-test/src/main/java/org/apache/iotdb/itbase/env/BaseEnv.java
@@ -204,7 +204,7 @@ public interface BaseEnv {
   /** Start an existed DataNode */
   void startDataNode(int index);
 
-  /** Start an existed DataNode */
+  /** Shutdown an existed DataNode */
   void shutdownDataNode(int index);
 
   int getMqttPort();
