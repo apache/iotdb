e512a9a7c062eb30388c1316cb2540f6f2e7cfa8
Liu Xuxin <liuxuxin@outlook.com>
2022-11-30 18:41:06 +0800

    set cross_compaction to true and add compaction validation

--- server/src/main/java/org/apache/iotdb/db/conf/IoTDBDescriptor.java
+++ server/src/main/java/org/apache/iotdb/db/conf/IoTDBDescriptor.java
@@ -673,6 +673,12 @@ public class IoTDBDescriptor {
                 "compaction_write_throughput_mb_per_sec",
                 Integer.toString(conf.getCompactionWriteThroughputMbPerSec()))));
 
+    conf.setEnableCompactionValidation(
+        Boolean.parseBoolean(
+            properties.getProperty(
+                "enable_compaction_validation",
+                Boolean.toString(conf.isEnableCompactionValidation()))));
+
     conf.setEnablePartialInsert(
         Boolean.parseBoolean(
             properties.getProperty(
