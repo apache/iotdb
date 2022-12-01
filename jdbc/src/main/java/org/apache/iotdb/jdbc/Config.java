984f0c0b9cd7f05d3a2ab7d9f33b7d4d879eaa09
Zhijia Cao <caozhijia@126.com>
2022-12-01 09:59:16 +0800

    Update jdbc/src/main/java/org/apache/iotdb/jdbc/Config.java
    Co-authored-by: Jackie Tien <jackietien97@gmail.com>

--- jdbc/src/main/java/org/apache/iotdb/jdbc/Config.java
+++ jdbc/src/main/java/org/apache/iotdb/jdbc/Config.java
@@ -57,7 +57,7 @@ public class Config {
   /** key of thrift max frame size */
   public static final String THRIFT_FRAME_MAX_SIZE = "thrift_max_frame_size";
 
-  static final String AUTH_ENABLE_AUDIT = "enableAudit";
+  public static final String AUTH_ENABLE_AUDIT = "enableAudit";
 
   public static final boolean DEFAULT_ENABLE_AUDIT = true;
 }
