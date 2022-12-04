96343c9c9ead2c6cf48ad0c3d05d90d189aef9c9
Liao Lanyu <1435078631@qq.com>
2022-12-04 18:16:10 +0800

    [To rel/1.0] [IOTDB-5026] Improve last query on aligned timeseries

--- server/src/main/java/org/apache/iotdb/db/mpp/plan/planner/SubPlanTypeExtractor.java
+++ server/src/main/java/org/apache/iotdb/db/mpp/plan/planner/SubPlanTypeExtractor.java
@@ -27,7 +27,12 @@ import org.apache.iotdb.db.mpp.plan.planner.plan.node.process.AggregationNode;
 import org.apache.iotdb.db.mpp.plan.planner.plan.node.process.GroupByLevelNode;
 import org.apache.iotdb.db.mpp.plan.planner.plan.node.process.GroupByTagNode;
 import org.apache.iotdb.db.mpp.plan.planner.plan.node.process.SlidingWindowAggregationNode;
+import org.apache.iotdb.db.mpp.plan.planner.plan.node.process.last.LastQueryCollectNode;
+import org.apache.iotdb.db.mpp.plan.planner.plan.node.process.last.LastQueryMergeNode;
+import org.apache.iotdb.db.mpp.plan.planner.plan.node.process.last.LastQueryNode;
+import org.apache.iotdb.db.mpp.plan.planner.plan.node.source.AlignedLastQueryScanNode;
 import org.apache.iotdb.db.mpp.plan.planner.plan.node.source.AlignedSeriesAggregationScanNode;
+import org.apache.iotdb.db.mpp.plan.planner.plan.node.source.LastQueryScanNode;
 import org.apache.iotdb.db.mpp.plan.planner.plan.node.source.SeriesAggregationScanNode;
 import org.apache.iotdb.db.mpp.plan.planner.plan.parameter.AggregationDescriptor;
 
@@ -104,6 +109,30 @@ public class SubPlanTypeExtractor {
           .forEach(this::updateTypeProviderByAggregationDescriptor);
       return visitPlan(node, context);
     }
+
+    // region PlanNode of last query
+    // No need to deal with type of last query
+    public Void visitLastQueryScan(LastQueryScanNode node, Void context) {
+      return null;
+    }
+
+    public Void visitAlignedLastQueryScan(AlignedLastQueryScanNode node, Void context) {
+      return null;
+    }
+
+    public Void visitLastQuery(LastQueryNode node, Void context) {
+      return null;
+    }
+
+    public Void visitLastQueryMerge(LastQueryMergeNode node, Void context) {
+      return null;
+    }
+
+    public Void visitLastQueryCollect(LastQueryCollectNode node, Void context) {
+      return null;
+    }
+
+    // end region PlanNode of last query
 
     private void updateTypeProviderByAggregationDescriptor(
         List<? extends AggregationDescriptor> aggregationDescriptorList) {
