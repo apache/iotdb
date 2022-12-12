d6fc072ac8987a9fe394c2a373fd3f94b212cee4
张正明 <876670773@qq.com>
2022-12-12 10:37:03 +0800

    change "

--- confignode/src/assembly/resources/sbin/stop-confignode.bat
+++ confignode/src/assembly/resources/sbin/stop-confignode.bat
@@ -27,7 +27,7 @@ for /f  "eol=; tokens=2,2 delims==" %%i in ('findstr /i "^cn_internal_port"
   set cn_internal_port=%%i
 )
 
-echo "check whether the cn_internal_port is used..., port is" %cn_internal_port%
+echo "check whether the cn_internal_port is used..., port is %cn_internal_port%"
 
 for /f  "eol=; tokens=2,2 delims==" %%i in ('findstr /i "cn_internal_address"
 %superior_dir%\conf\iotdb-confignode.properties') do (
