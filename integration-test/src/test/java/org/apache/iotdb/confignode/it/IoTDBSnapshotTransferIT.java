44d8cbbe640e2dd99fcd996c8e18b0fd154c3c10
szywilliam <szywilliam@163.com>
2022-12-20 16:51:07 +0800

    set it to 5

--- integration-test/src/test/java/org/apache/iotdb/confignode/it/IoTDBSnapshotTransferIT.java
+++ integration-test/src/test/java/org/apache/iotdb/confignode/it/IoTDBSnapshotTransferIT.java
@@ -37,7 +37,6 @@ import org.apache.iotdb.rpc.TSStatusCode;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 import org.junit.runner.RunWith;
@@ -50,7 +49,6 @@ import java.util.Optional;
 
 @RunWith(IoTDBTestRunner.class)
 @Category({ClusterIT.class})
-@Ignore
 public class IoTDBSnapshotTransferIT {
 
   private final long snapshotMagic = 99;
