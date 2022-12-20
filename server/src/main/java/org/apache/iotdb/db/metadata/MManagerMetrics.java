c3e278229519b9f8aa75f031480478552737ebdf
Marcos_Zyk <38524330+MarcosZyk@users.noreply.github.com>
2022-12-20 20:52:02 +0800

    [To rel/0.13][IOTDB-5232] Add schema memory metric (#8540)
    [To rel/0.13][IOTDB-5232] Add schema memory metric (#8540)

--- server/src/main/java/org/apache/iotdb/db/metadata/MManagerMetrics.java
+++ server/src/main/java/org/apache/iotdb/db/metadata/MManagerMetrics.java
@@ -19,6 +19,7 @@
 
 package org.apache.iotdb.db.metadata;
 
+import org.apache.iotdb.db.conf.IoTDBDescriptor;
 import org.apache.iotdb.db.service.metrics.MetricService;
 import org.apache.iotdb.db.service.metrics.enums.Metric;
 import org.apache.iotdb.db.service.metrics.enums.Tag;
@@ -82,12 +83,38 @@ public class MManagerMetrics implements IMetricSet {
 
     MetricService.getInstance()
         .getOrCreateAutoGauge(
+            Metric.QUANTITY.toString(),
+            MetricLevel.IMPORTANT,
+            mManager,
+            MManager::getTotalTemplateActivatedNumber,
+            Tag.NAME.toString(),
+            "deviceUsingTemplate",
+            Tag.TYPE.toString(),
+            "template");
+
+    MetricService.getInstance()
+        .getOrCreateAutoGauge(
             Metric.MEM.toString(),
             MetricLevel.IMPORTANT,
             mManager,
-            MManager::getMtreeSize,
+            MManager::getTotalEstimatedMemoryUsage,
             Tag.NAME.toString(),
-            "mtree");
+            "schemaUsage",
+            Tag.TYPE.toString(),
+            "total");
+
+    MetricService.getInstance()
+        .getOrCreateAutoGauge(
+            Metric.MEM.toString(),
+            MetricLevel.IMPORTANT,
+            mManager,
+            mManager ->
+                IoTDBDescriptor.getInstance().getConfig().getAllocateMemoryForSchema()
+                    - mManager.getTotalEstimatedMemoryUsage(),
+            Tag.NAME.toString(),
+            "schemaRemaining",
+            Tag.TYPE.toString(),
+            "total");
   }
 
   @Override
@@ -129,6 +156,30 @@ public class MManagerMetrics implements IMetricSet {
             "total");
 
     MetricService.getInstance()
-        .remove(MetricType.GAUGE, Metric.MEM.toString(), Tag.NAME.toString(), "mtree");
+        .remove(
+            MetricType.GAUGE,
+            Metric.QUANTITY.toString(),
+            Tag.NAME.toString(),
+            "deviceUsingTemplate",
+            Tag.TYPE.toString(),
+            "template");
+
+    MetricService.getInstance()
+        .remove(
+            MetricType.GAUGE,
+            Metric.MEM.toString(),
+            Tag.NAME.toString(),
+            "schemaUsage",
+            Tag.TYPE.toString(),
+            "total");
+
+    MetricService.getInstance()
+        .remove(
+            MetricType.GAUGE,
+            Metric.MEM.toString(),
+            Tag.NAME.toString(),
+            "schemaRemaining",
+            Tag.TYPE.toString(),
+            "total");
   }
 }
