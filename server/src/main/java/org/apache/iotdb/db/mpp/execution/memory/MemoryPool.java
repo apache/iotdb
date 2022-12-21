fba958e689431058b10ee3cdb91b97305a0b9611
Alima777 <wxw19981014@gmail.com>
2022-12-21 16:52:29 +0800

    remove memory limit for exchange

--- server/src/main/java/org/apache/iotdb/db/mpp/execution/memory/MemoryPool.java
+++ server/src/main/java/org/apache/iotdb/db/mpp/execution/memory/MemoryPool.java
@@ -25,11 +25,10 @@ import org.apache.iotdb.tsfile.utils.Pair;
 import com.google.common.util.concurrent.AbstractFuture;
 import com.google.common.util.concurrent.Futures;
 import com.google.common.util.concurrent.ListenableFuture;
+import javax.annotation.Nullable;
 import org.apache.commons.lang3.Validate;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import javax.annotation.Nullable;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -164,7 +163,7 @@ public class MemoryPool {
                       .getOrDefault(fragmentInstanceId, Collections.emptyMap())
                       .getOrDefault(planNodeId, 0L)
               < bytesToReserve) {
-        // LOGGER.info("Cannot reserve {} bytes memory from MemoryPool", bytesToReserve);
+        LOGGER.info("Cannot reserve {} bytes memory from MemoryPool", bytesToReserve);
         result =
             MemoryReservationFuture.create(
                 queryId, fragmentInstanceId, planNodeId, bytesToReserve, maxBytesCanReserve);
