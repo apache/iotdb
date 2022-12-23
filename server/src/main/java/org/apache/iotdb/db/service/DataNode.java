59a0b1732565398a7f15538bebf19684e5a52259
Beyyes <cgf1993@foxmail.com>
2022-12-23 17:36:54 +0800

    add constant

--- server/src/main/java/org/apache/iotdb/db/service/DataNode.java
+++ server/src/main/java/org/apache/iotdb/db/service/DataNode.java
@@ -97,6 +97,8 @@ import java.util.List;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
+import static org.apache.iotdb.db.conf.IoTDBStartCheck.DEFAULT_CLUSTER_NAME;
+
 public class DataNode implements DataNodeMBean {
   private static final Logger logger = LoggerFactory.getLogger(DataNode.class);
   private static final IoTDBConfig config = IoTDBDescriptor.getInstance().getConfig();
@@ -190,9 +192,6 @@ public class DataNode implements DataNodeMBean {
     // Set cluster mode
     config.setClusterMode(true);
 
-    // Notice: Consider this DataNode as first start if the system.properties file doesn't exist
-    boolean isFirstStart = !SYSTEM_PROPERTIES.exists();
-
     // Check target ConfigNodes
     for (TEndPoint endPoint : config.getTargetConfigNodeList()) {
       if (endPoint.getIp().equals("0.0.0.0")) {
@@ -212,7 +211,8 @@ public class DataNode implements DataNodeMBean {
     // Check system configurations
     IoTDBStartCheck.getInstance().checkSystemConfig();
 
-    return isFirstStart;
+    // Notice: Consider this DataNode as first start if the system.properties file doesn't exist
+    return !SYSTEM_PROPERTIES.exists();
   }
 
   /**
@@ -399,7 +399,7 @@ public class DataNode implements DataNodeMBean {
     int retry = DEFAULT_RETRY;
     TDataNodeRestartReq req = new TDataNodeRestartReq();
     req.setClusterName(
-        config.getClusterName() == null ? "defaultCluster" : config.getClusterName());
+        config.getClusterName() == null ? DEFAULT_CLUSTER_NAME : config.getClusterName());
     req.setDataNodeConfiguration(generateDataNodeConfiguration());
     TDataNodeRestartResp dataNodeRestartResp = null;
     while (retry > 0) {
