0e0eb4f435bd854b45a8d32ac07e000b2064ed95
HTHou <hhaonan@outlook.com>
2022-12-29 14:18:26 +0800

    fix_python_status_code

--- client-py/iotdb/Session.py
+++ client-py/iotdb/Session.py
@@ -72,6 +72,7 @@ logger = logging.getLogger("IoTDB")
 
 class Session(object):
     SUCCESS_CODE = 200
+    NEED_REDIRECT = 400
     DEFAULT_FETCH_SIZE = 10000
     DEFAULT_USER = "root"
     DEFAULT_PASSWORD = "root"
@@ -1046,7 +1047,7 @@ class Session(object):
         verify success of operation
         :param status: execution result status
         """
-        if status.code == Session.SUCCESS_CODE:
+        if status.code == Session.SUCCESS_CODE or status.code == Session.NEED_REDIRECT:
             return 0
 
         logger.error("error status is %s", status)
