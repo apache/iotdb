1c1b696f97815a00f0ea5c09630932da8be9b8dc
Alan Choo <43991780+heimingz@users.noreply.github.com>
2023-01-03 11:36:55 +0800

    [IOTDB-5285] TimePartition may be error when restarting with different time partition configuration (#8646)
    (cherry picked from commit e4cbca447910d70ebe139c8c5448af73abf1d972)

--- server/src/main/java/org/apache/iotdb/db/service/DataNode.java
+++ server/src/main/java/org/apache/iotdb/db/service/DataNode.java
@@ -193,7 +193,7 @@ public class DataNode implements DataNodeMBean {
     config.setClusterMode(true);
 
     // Notice: Consider this DataNode as first start if the system.properties file doesn't exist
-    boolean isFirstStart = !SYSTEM_PROPERTIES.exists();
+    boolean isFirstStart = IoTDBStartCheck.getInstance().checkIsFirstStart();
 
     // Check target ConfigNodes
     for (TEndPoint endPoint : config.getTargetConfigNodeList()) {
@@ -210,9 +210,6 @@ public class DataNode implements DataNodeMBean {
     // Startup checks
     StartupChecks checks = new StartupChecks(IoTDBConstant.DN_ROLE).withDefaultTest();
     checks.verify();
-
-    // Check system configurations
-    IoTDBStartCheck.getInstance().checkSystemConfig();
 
     return isFirstStart;
   }
@@ -285,6 +282,7 @@ public class DataNode implements DataNodeMBean {
 
     /* Check system configurations */
     try {
+      IoTDBStartCheck.getInstance().checkSystemConfig();
       IoTDBStartCheck.getInstance().checkDirectory();
       IoTDBStartCheck.getInstance().serializeGlobalConfig(configurationResp.globalConfig);
       IoTDBDescriptor.getInstance().initClusterSchemaMemoryAllocate();
