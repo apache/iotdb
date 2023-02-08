a77571f63ae57cd7633cb53adc9be30c4182fccb
Jackie Tien <jackietien97@gmail.com>
2023-02-08 19:43:26 +0800

    Correct DataPartiton Fetch request parameter contruction (#8953)
    Co-authored-by: Jinrui.Zhang <xingtanzjr@gmail.com>
    Co-authored-by: HeimingZ <zhuhmch@qq.com>
    Co-authored-by: Haonan <hhaonan@outlook.com>

--- server/src/main/java/org/apache/iotdb/db/conf/IoTDBConfig.java
+++ server/src/main/java/org/apache/iotdb/db/conf/IoTDBConfig.java
@@ -875,7 +875,7 @@ public class IoTDBConfig {
    * series partition
    */
   private String seriesPartitionExecutorClass =
-      "org.apache.iotdb.commons.partition.executor.hash.APHashExecutor";
+      "org.apache.iotdb.commons.partition.executor.hash.BKDRHashExecutor";
 
   /** The number of series partitions in a database */
   private int seriesPartitionSlotNum = 10000;
