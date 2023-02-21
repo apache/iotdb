b1363e81ac2593031b7edc6d4e043cf9fc7d12eb
Cpaulyz <chen3yz@gmail.com>
2023-02-21 10:49:36 +0800

    fix

--- metrics/interface/src/main/java/org/apache/iotdb/metrics/config/MetricConfig.java
+++ metrics/interface/src/main/java/org/apache/iotdb/metrics/config/MetricConfig.java
@@ -40,12 +40,10 @@ public class MetricConfig {
   private MetricFrameType metricFrameType = MetricFrameType.MICROMETER;
 
   /** The list of reporters provide metrics for external tool. */
-  private List<ReporterType> metricReporterList =
-      Collections.singletonList(ReporterType.PROMETHEUS);
-  //  private List<ReporterType> metricReporterList = Collections.emptyList();
+  private List<ReporterType> metricReporterList = Collections.emptyList();
 
   /** The level of metric service. */
-  private MetricLevel metricLevel = MetricLevel.IMPORTANT;
+  private MetricLevel metricLevel = MetricLevel.CORE;
 
   /** The period of async collection of some metrics in second. */
   private Integer asyncCollectPeriodInSecond = 5;
@@ -111,7 +109,7 @@ public class MetricConfig {
   }
 
   public void setMetricLevel(MetricLevel metricLevel) {
-    this.metricLevel = MetricLevel.IMPORTANT;
+    this.metricLevel = metricLevel;
   }
 
   public Integer getAsyncCollectPeriodInSecond() {
