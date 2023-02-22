4f05b93f7896422afa10fb0c9d8b3dd821aa85c6
马子坤 <1403569586@qq.com>
2023-02-22 20:23:42 +0800

    licence

--- server/src/main/java/org/apache/iotdb/db/engine/backup/BackupSystemFileTask.java
+++ server/src/main/java/org/apache/iotdb/db/engine/backup/BackupSystemFileTask.java
@@ -1,3 +1,22 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 package org.apache.iotdb.db.engine.backup;
 
 import org.apache.iotdb.db.concurrent.WrappedRunnable;
@@ -29,7 +48,8 @@ public class BackupSystemFileTask extends WrappedRunnable {
       String systemPath = IoTDBDescriptor.getInstance().getConfig().getSystemDir();
       File systemDir = new File(systemPath);
       if (sourceFile.getAbsolutePath().contains(systemDir.getAbsolutePath())) {
-        String relativeSourcePath = sourceFile.getAbsolutePath().replace(systemDir.getAbsolutePath(), "");
+        String relativeSourcePath =
+            sourceFile.getAbsolutePath().replace(systemDir.getAbsolutePath(), "");
         String outputFilePath =
             FilePathUtils.regularizePath(outputRootPath)
                 + IoTDBConstant.SYSTEM_FOLDER_NAME
