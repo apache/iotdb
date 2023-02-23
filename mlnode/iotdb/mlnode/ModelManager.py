db8c71f45c8c771721bff1bd74010897d27da96a
lichenyu <872166619@qq.com>
2023-02-24 00:06:48 +0800

    adding model caching mechanism

--- mlnode/iotdb/mlnode/ModelManager.py
+++ mlnode/iotdb/mlnode/ModelManager.py
@@ -4,15 +4,18 @@ import os
 from ModelInfo import ModelInfo
 from ThreadWithReturn import ThreadWithReturn
 
+from pylru import lrucache
 
 class ModelManger(object):
-    def __init__(self, root_path='ModelData'):
+    def __init__(self, root_path='ModelData', cache_size=10):
         self.root_path = root_path
         if not os.path.exists(root_path):
             os.mkdir(root_path)
         self.ModelMap = {}
         self.lastModelID = None
         self.lastModel = None
+        # store best trial of models
+        self.model_cache = lrucache(cache_size)
 
     def getModelinfobyID(self, model_id):
         return self.ModelMap.get(model_id)
@@ -28,8 +31,13 @@ class ModelManger(object):
         thread.start()
         thread.join()
         self.ModelMap.pop(model_id)
+        # delete model in cache
+        self.model_cache.pop(model_id)
 
     def loadBestModelbyID(self, model_id):
+        # if model in cache, return directly
+        if model_id in self.model_cache:
+            return self.model_cache[model_id]
         modelinfo = self.getModelinfobyID(model_id)
         thread = ThreadWithReturn(func=modelinfo.loadBestModel, args=())
         thread.start()
@@ -42,6 +50,9 @@ class ModelManger(object):
         else:
             modelinfo = self.getModelinfobyID(model_id)
         modelinfo.saveModel(model, trail_id, updateBestTrail)
+        # update best trial in cache
+        if updateBestTrail:
+            self.model_cache[model_id] = model
 
 
 modelManager = ModelManger()
