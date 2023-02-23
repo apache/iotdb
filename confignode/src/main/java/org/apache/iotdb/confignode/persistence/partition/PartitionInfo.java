e1abd57c722fd91e1c910a71fe6fc8e513aa95b8
YongzaoDan <532741407@qq.com>
2023-02-23 21:53:40 +0800

    Update PartitionInfo.java

--- confignode/src/main/java/org/apache/iotdb/confignode/persistence/partition/PartitionInfo.java
+++ confignode/src/main/java/org/apache/iotdb/confignode/persistence/partition/PartitionInfo.java
@@ -75,6 +75,7 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
