6491e05dcb0c5280c4cce897f07edfc68b4ff3c2
CritasWang <critas@outlook.com>
2023-03-02 16:42:45 +0800

    fix(home): fix home url error

--- site/src/main/.vuepress/theme.ts
+++ site/src/main/.vuepress/theme.ts
@@ -1,22 +1,3 @@
-/*
-  Licensed to the Apache Software Foundation (ASF) under one
-  or more contributor license agreements.  See the NOTICE file
-  distributed with this work for additional information
-  regarding copyright ownership.  The ASF licenses this file
-  to you under the Apache License, Version 2.0 (the
-  "License"); you may not use this file except in compliance
-  with the License.  You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-  Unless required by applicable law or agreed to in writing,
-  software distributed under the License is distributed on an
-  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-  KIND, either express or implied.  See the License for the
-  specific language governing permissions and limitations
-  under the License.
- */
-
 import { hopeTheme } from 'vuepress-theme-hope';
 import { enNavbar, zhNavbar } from './navbar/index.js';
 import { enSidebar, zhSidebar } from './sidebar/index.js';
@@ -24,7 +5,7 @@ import { enSidebar, zhSidebar } from './sidebar/index.js';
 export default hopeTheme({
   hostname: 'https://iotdb.apache.org/',
 
-  darkmode: 'disable',
+  darkmode: 'toggle',
 
   iconAssets: 'iconfont',
 
@@ -44,10 +25,12 @@ export default hopeTheme({
 
   breadcrumb: false,
 
+  navbarIcon: false,
+
   navbarLayout: {
     start: ['Brand'],
     center: [],
-    end: ['Search', 'Links', 'Language', 'Repo'],
+    end: ['Search', 'Links', 'Language', 'Outlook', 'Repo'],
   },
 
   locales: {
