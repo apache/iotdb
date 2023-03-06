68e2a409b3af661cf5e60647f9d4b1a46168d937
Alima777 <wxw19981014@gmail.com>
2023-03-06 11:27:11 +0800

    change fragmentInstanceId

--- server/src/main/java/org/apache/iotdb/db/mpp/execution/memory/MemoryPool.java
+++ server/src/main/java/org/apache/iotdb/db/mpp/execution/memory/MemoryPool.java
@@ -25,11 +25,10 @@ import org.apache.iotdb.tsfile.utils.Pair;
 import com.google.common.util.concurrent.AbstractFuture;
 import com.google.common.util.concurrent.Futures;
 import com.google.common.util.concurrent.ListenableFuture;
+import javax.annotation.Nullable;
 import org.apache.commons.lang3.Validate;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import javax.annotation.Nullable;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -351,7 +350,7 @@ public class MemoryPool {
         || planNodeIdToBytesReserved.get(planNodeId) == 0) {
       planNodeIdToBytesReserved.remove(planNodeId);
       instanceBytesReserved.computeIfPresent(
-          planNodeId,
+          fragmentInstanceId,
           (k, kPlanNodeBytesReserved) -> {
             if (kPlanNodeBytesReserved.isEmpty()) {
               return null;
