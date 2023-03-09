2d2539ce3bcc1e4e3726853caf0114e7f48255d8
Caideyipi <1336190078@qq.com>
2023-03-09 23:00:32 +0800

    Update CreatePipeStatement.java
    Deleted parseString and toString

--- server/src/main/java/org/apache/iotdb/db/mpp/plan/statement/sys/sync/CreatePipeStatement.java
+++ server/src/main/java/org/apache/iotdb/db/mpp/plan/statement/sys/sync/CreatePipeStatement.java
@@ -20,16 +20,13 @@
 package org.apache.iotdb.db.mpp.plan.statement.sys.sync;
 
 import org.apache.iotdb.commons.path.PartialPath;
-import org.apache.iotdb.commons.sync.utils.SyncConstant;
 import org.apache.iotdb.db.mpp.plan.analyze.QueryType;
 import org.apache.iotdb.db.mpp.plan.statement.IConfigStatement;
 import org.apache.iotdb.db.mpp.plan.statement.Statement;
 import org.apache.iotdb.db.mpp.plan.statement.StatementType;
 import org.apache.iotdb.db.mpp.plan.statement.StatementVisitor;
 
-import java.io.IOException;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -89,60 +86,5 @@ public class CreatePipeStatement extends Statement implements IConfigStatement {
   @Override
   public <R, C> R accept(StatementVisitor<R, C> visitor, C context) {
     return visitor.visitCreatePipe(this, context);
-  }
-
-  public static CreatePipeStatement parseString(String parsedString) throws IOException {
-    String[] split = parsedString.split(SyncConstant.PLAN_SERIALIZE_SPLIT_CHARACTER);
-    if (split.length < 4) {
-      throw new IOException("Parsing CreatePipePlan error. Attributes is less than expected.");
-    }
-    CreatePipeStatement statement = new CreatePipeStatement(StatementType.CREATE_PIPE);
-    statement.setPipeName(split[0]);
-    int collectorSize = (Integer.parseInt(split[1]) << 1);
-    int processorSize = (Integer.parseInt(split[2]) << 1);
-    int connectorSize = (Integer.parseInt(split[3]) << 1);
-    if (split.length != (collectorSize + processorSize + connectorSize + 4)) {
-      throw new IOException("Parsing CreatePipePlan error. Attributes number is wrong.");
-    }
-    Map<String, String> collectorAttributes = new HashMap<>();
-    for (int i = 4; i < collectorSize + 4; i += 2) {
-      collectorAttributes.put(split[i], split[i + 1]);
-    }
-    statement.setCollectorAttributes(collectorAttributes);
-    Map<String, String> processorAttributes = new HashMap<>();
-    for (int i = collectorSize + 4; i < collectorSize + processorSize + 4; i += 2) {
-      processorAttributes.put(split[i], split[i + 1]);
-    }
-    statement.setCollectorAttributes(processorAttributes);
-    Map<String, String> connectorAttributes = new HashMap<>();
-    for (int i = collectorSize + processorSize + 4;
-        i < collectorSize + processorSize + connectorSize + 4;
-        i += 2) {
-      connectorAttributes.put(split[i], split[i + 1]);
-    }
-    statement.setCollectorAttributes(connectorAttributes);
-    return statement;
-  }
-
-  @Override
-  public String toString() {
-    StringBuilder builder = new StringBuilder();
-    builder.append(pipeName).append(SyncConstant.PLAN_SERIALIZE_SPLIT_CHARACTER);
-    builder.append(collectorAttributes.size()).append(SyncConstant.PLAN_SERIALIZE_SPLIT_CHARACTER);
-    builder.append(processorAttributes.size()).append(SyncConstant.PLAN_SERIALIZE_SPLIT_CHARACTER);
-    builder.append(connectorAttributes.size()).append(SyncConstant.PLAN_SERIALIZE_SPLIT_CHARACTER);
-    for (Map.Entry<String, String> entry : collectorAttributes.entrySet()) {
-      builder.append(entry.getKey()).append(SyncConstant.PLAN_SERIALIZE_SPLIT_CHARACTER);
-      builder.append(entry.getValue()).append(SyncConstant.PLAN_SERIALIZE_SPLIT_CHARACTER);
-    }
-    for (Map.Entry<String, String> entry : processorAttributes.entrySet()) {
-      builder.append(entry.getKey()).append(SyncConstant.PLAN_SERIALIZE_SPLIT_CHARACTER);
-      builder.append(entry.getValue()).append(SyncConstant.PLAN_SERIALIZE_SPLIT_CHARACTER);
-    }
-    for (Map.Entry<String, String> entry : connectorAttributes.entrySet()) {
-      builder.append(entry.getKey()).append(SyncConstant.PLAN_SERIALIZE_SPLIT_CHARACTER);
-      builder.append(entry.getValue()).append(SyncConstant.PLAN_SERIALIZE_SPLIT_CHARACTER);
-    }
-    return builder.toString();
   }
 }
