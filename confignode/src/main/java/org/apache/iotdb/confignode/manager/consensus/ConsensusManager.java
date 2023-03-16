0c678d660b0f6414e9bd30deda35d63591e7fea8
BUAAserein <18376359@buaa.edu.cn>
2023-03-16 23:07:59 +0800

    merge master

--- confignode/src/main/java/org/apache/iotdb/confignode/manager/consensus/ConsensusManager.java
+++ confignode/src/main/java/org/apache/iotdb/confignode/manager/consensus/ConsensusManager.java
@@ -89,7 +89,7 @@ public class ConsensusManager {
                   ConsensusConfig.newBuilder()
                       .setThisNode(
                           new TEndPoint(CONF.getInternalAddress(), CONF.getConsensusPort()))
-                      .setStorageDir("target" + java.io.File.separator + "simple")
+                      .setStorageDir(CONF.getConsensusDir())
                       .build(),
                   gid -> stateMachine)
               .orElseThrow(
