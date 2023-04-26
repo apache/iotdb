0b127a52433bacd01e242c7b229fb29c07c47da6
lancelly <1435078631@qq.com>
2023-04-26 14:57:05 +0800

    fix

--- integration-test/src/test/java/org/apache/iotdb/db/it/last/IoTDBLastQueryLastCacheIT.java
+++ integration-test/src/test/java/org/apache/iotdb/db/it/last/IoTDBLastQueryLastCacheIT.java
@@ -47,9 +47,9 @@ import static org.junit.Assert.fail;
 public class IoTDBLastQueryLastCacheIT {
   protected static final String[] SQLs =
       new String[] {
-        "create aligned timeseries root.ln_1.tb_6141(machineExit_BOOLEAN BOOLEAN encoding=RLE,`waterNH4-N_DOUBLE` DOUBLE encoding=GORILLA,status_BOOLEAN BOOLEAN encoding=RLE,11_TEXT TEXT encoding=PLAIN,waterInterval_DOUBLE DOUBLE encoding=GORILLA,content_TEXT TEXT encoding=PLAIN, machineOn_BOOLEAN BOOLEAN encoding=RLE,enum_INT32 INT32 encoding=RLE,waterTP_DOUBLE DOUBLE encoding=GORILLA,fluidVelocity _DOUBLE DOUBLE encoding=GORILLA,CO2_DOUBLE DOUBLE encoding=GORILLA,`switch_BOOLEAN` BOOLEAN encoding=RLE,code_DOUBLE DOUBLE encoding=GORILLA);",
+        "create aligned timeseries root.ln_1.tb_6141(machineExit_BOOLEAN BOOLEAN encoding=RLE,`waterNH4-N_DOUBLE` DOUBLE encoding=GORILLA,status_BOOLEAN BOOLEAN encoding=RLE,11_TEXT TEXT encoding=PLAIN,waterInterval_DOUBLE DOUBLE encoding=GORILLA,content_TEXT TEXT encoding=PLAIN, machineOn_BOOLEAN BOOLEAN encoding=RLE,enum_INT32 INT32 encoding=RLE,waterTP_DOUBLE DOUBLE encoding=GORILLA,fluidVelocity_DOUBLE DOUBLE encoding=GORILLA,CO2_DOUBLE DOUBLE encoding=GORILLA,`switch_BOOLEAN` BOOLEAN encoding=RLE,code_DOUBLE DOUBLE encoding=GORILLA);",
         "alter timeseries root.ln_1.tb_6141.machineExit_BOOLEAN upsert alias=machineExit;",
-        "alter timeseries root.ln_1.tb_6141.fluidVelocity _DOUBLE upsert alias=fluidVelocity ;",
+        "alter timeseries root.ln_1.tb_6141.fluidVelocity_DOUBLE upsert alias=fluidVelocity;",
         "alter timeseries root.ln_1.tb_6141.CO2_DOUBLE upsert alias=CO2;",
         "alter timeseries root.ln_1.tb_6141.machineOn_BOOLEAN upsert alias=machineOn;",
         "alter timeseries root.ln_1.tb_6141.waterInterval_DOUBLE upsert alias=waterInterval;",
@@ -66,7 +66,7 @@ public class IoTDBLastQueryLastCacheIT {
         "insert into root.ln_1.tb_6141(time,code_DOUBLE) aligned values(1679477545000,2.0);",
         "insert into root.ln_1.tb_6141(time,content_TEXT) aligned values(1675995566000,52);",
         "insert into root.ln_1.tb_6141(time,enum_INT32) aligned values(1675995566000,2);",
-        "insert into root.ln_1.tb_6141(time,fluidVelocity _DOUBLE) aligned values(1679365910000,15.0);",
+        "insert into root.ln_1.tb_6141(time,fluidVelocity_DOUBLE) aligned values(1679365910000,15.0);",
         "insert into root.ln_1.tb_6141(time,status_BOOLEAN) aligned values(1677033625000,true);",
         "insert into root.ln_1.tb_6141(time,machineOn_BOOLEAN) aligned values(1675995566000,true);",
         "insert into root.ln_1.tb_6141(time,machineExit_BOOLEAN) aligned values(1675995566000,false);",
@@ -103,19 +103,19 @@ public class IoTDBLastQueryLastCacheIT {
         new String[] {TIMESTAMP_STR, TIMESEIRES_STR, VALUE_STR, DATA_TYPE_STR};
     String[] retArray =
         new String[] {
-          "1679365910000,root.ln_1.tb_6141.waterInterval_DOUBLE,10.0,DOUBLE,",
-          "1679365910000,root.ln_1.tb_6141.waterTP_DOUBLE,15.0,DOUBLE,",
-          "1679477545000,root.ln_1.tb_6141.code_DOUBLE,2.0,DOUBLE,",
-          "1675995566000,root.ln_1.tb_6141.content_TEXT,52,TEXT,",
-          "1675995566000,root.ln_1.tb_6141.enum_INT32,2,INT32,",
-          "1679365910000,root.ln_1.tb_6141.fluidVelocity _DOUBLE,15.0,DOUBLE,",
-          "1677033625000,root.ln_1.tb_6141.status_BOOLEAN,true,BOOLEAN,",
-          "1675995566000,root.ln_1.tb_6141.machineOn_BOOLEAN,true,BOOLEAN,",
-          "1675995566000,root.ln_1.tb_6141.machineExit_BOOLEAN,false,BOOLEAN,",
           "1679365910000,root.ln_1.tb_6141.11_TEXT,13,TEXT,",
           "1679365910000,root.ln_1.tb_6141.CO2_DOUBLE,12.0,DOUBLE,",
           "1679365910000,root.ln_1.tb_6141.`waterNH4-N_DOUBLE`,12.0,DOUBLE,",
-          "1675995566000,root.ln_1.tb_6141.`switch_BOOLEAN`,false,BOOLEAN,"
+          "1679477545000,root.ln_1.tb_6141.code_DOUBLE,2.0,DOUBLE,",
+          "1675995566000,root.ln_1.tb_6141.content_TEXT,52,TEXT,",
+          "1675995566000,root.ln_1.tb_6141.enum_INT32,2,INT32,",
+          "1679365910000,root.ln_1.tb_6141.fluidVelocity_DOUBLE,15.0,DOUBLE,",
+          "1675995566000,root.ln_1.tb_6141.machineExit_BOOLEAN,false,BOOLEAN,",
+          "1675995566000,root.ln_1.tb_6141.machineOn_BOOLEAN,true,BOOLEAN,",
+          "1677033625000,root.ln_1.tb_6141.status_BOOLEAN,true,BOOLEAN,",
+          "1675995566000,root.ln_1.tb_6141.switch_BOOLEAN,false,BOOLEAN,",
+          "1679365910000,root.ln_1.tb_6141.waterInterval_DOUBLE,10.0,DOUBLE,",
+          "1679365910000,root.ln_1.tb_6141.waterTP_DOUBLE,15.0,DOUBLE,",
         };
     resultSetEqualTest("select last * from root.ln_1.tb_6141;", expectedHeader, retArray);
   }
