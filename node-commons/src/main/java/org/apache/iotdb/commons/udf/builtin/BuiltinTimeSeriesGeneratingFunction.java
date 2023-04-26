521d33b27df87efe6b615a1490488a8541cd938b
Weihao Li <60659567+Wei-hao-Li@users.noreply.github.com>
2023-04-26 10:09:54 +0800

    [To rel/1.1][IOTDB-5735] Fix result of some UDF with align by device is incorrect

--- node-commons/src/main/java/org/apache/iotdb/commons/udf/builtin/BuiltinTimeSeriesGeneratingFunction.java
+++ node-commons/src/main/java/org/apache/iotdb/commons/udf/builtin/BuiltinTimeSeriesGeneratingFunction.java
@@ -30,6 +30,10 @@ import org.apache.iotdb.commons.udf.builtin.String.UDTFSubstr;
 import org.apache.iotdb.commons.udf.builtin.String.UDTFTrim;
 import org.apache.iotdb.commons.udf.builtin.String.UDTFUpper;
 
+import com.google.common.collect.ImmutableSet;
+
+import java.util.Set;
+
 /** All built-in UDFs need to register their function names and classes here. */
 public enum BuiltinTimeSeriesGeneratingFunction {
   CONST("CONST", UDTFConst.class),
@@ -95,6 +99,12 @@ public enum BuiltinTimeSeriesGeneratingFunction {
   private final String functionName;
   private final Class<?> functionClass;
   private final String className;
+
+  /**
+   * Set of functions are mappable but DeviceView of them also need special process. Now there is no
+   * function satisfies this.
+   */
+  public static final Set<String> DEVICE_VIEW_SPECIAL_PROCESS_FUNCTIONS = ImmutableSet.of();
 
   BuiltinTimeSeriesGeneratingFunction(String functionName, Class<?> functionClass) {
     this.functionName = functionName;
