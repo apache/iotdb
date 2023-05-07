77639a32156e37a51f74d1c3e6608c8b47da6b63
Liu Xuxin <liuxuxin@outlook.com>
2023-05-07 15:26:53 +0800

    throw exception when allocated memory size is greater than the total budget

--- server/src/main/java/org/apache/iotdb/db/rescon/SystemInfo.java
+++ server/src/main/java/org/apache/iotdb/db/rescon/SystemInfo.java
@@ -187,6 +187,13 @@ public class SystemInfo {
     if (!config.isEnableCompactionMemControl()) {
       return;
     }
+    if (memoryCost > memorySizeForCompaction) {
+      // required memory cost is greater than the total memory budget for compaction
+      throw new CompactionMemoryNotEnoughException(
+          String.format(
+              "Required memory cost %d bytes is greater than the total memory budget for compaction %d bytes",
+              memoryCost, memorySizeForCompaction));
+    }
     long startTime = System.currentTimeMillis();
     long originSize = this.compactionMemoryCost.get();
     while (originSize + memoryCost > memorySizeForCompaction
