165937ea196ca0b00dcb91898c12883b99e58e51
Tian Jiang <jt2594838@163.com>
2023-05-10 16:20:24 +0800

    avoid ByteBufferConsensusRequest being modified

--- consensus/src/main/java/org/apache/iotdb/consensus/common/request/ByteBufferConsensusRequest.java
+++ consensus/src/main/java/org/apache/iotdb/consensus/common/request/ByteBufferConsensusRequest.java
@@ -37,7 +37,7 @@ public class ByteBufferConsensusRequest implements IConsensusRequest {
 
   @Override
   public ByteBuffer serializeToByteBuffer() {
-    return byteBuffer;
+    return byteBuffer.slice();
   }
 
   @Override
