73d10ee0ef888df90fa8e3c1477b9ad178fd6551
citrusreticulata <1372722289@qq.com>
2023-05-12 17:42:14 +0800

    fix bug: can not deserialize ConstantViewOperand
    a `break` is lost!

--- node-commons/src/main/java/org/apache/iotdb/commons/schema/view/viewExpression/ViewExpression.java
+++ node-commons/src/main/java/org/apache/iotdb/commons/schema/view/viewExpression/ViewExpression.java
@@ -223,6 +223,7 @@ public abstract class ViewExpression {
       switch (type) {
         case -4:
           expression = new ConstantViewOperand(inputStream);
+          break;
         case -3:
           expression = new TimestampViewOperand(inputStream);
           break;
