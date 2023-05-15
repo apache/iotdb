1b1b95172f07cf9e39dc0ac6944f2d880f0cca50
Xiangpeng Hu <65238551+BUAAserein@users.noreply.github.com>
2023-05-15 09:08:46 +0800

    [IOTDB-5852] Add some IoTConsensus and RatisConsensus properties (#9797)

--- confignode/src/main/java/org/apache/iotdb/confignode/conf/ConfigNodeConfig.java
+++ confignode/src/main/java/org/apache/iotdb/confignode/conf/ConfigNodeConfig.java
@@ -228,6 +228,8 @@ public class ConfigNodeConfig {
   /** RatisConsensus protocol, allow flushing Raft Log asynchronously */
   private boolean dataRegionRatisLogUnsafeFlushEnable = false;
 
+  private int dataRegionRatisLogForceSyncNum = 128;
+
   private boolean configNodeRatisLogUnsafeFlushEnable = false;
   private boolean schemaRegionRatisLogUnsafeFlushEnable = false;
 
@@ -240,6 +242,8 @@ public class ConfigNodeConfig {
 
   /** RatisConsensus protocol, flow control window for ratis grpc log appender */
   private long dataRegionRatisGrpcFlowControlWindow = 4 * 1024 * 1024L;
+
+  private int dataRegionRatisGrpcLeaderOutstandingAppendsMax = 128;
 
   private long configNodeRatisGrpcFlowControlWindow = 4 * 1024 * 1024L;
   private long schemaRegionRatisGrpcFlowControlWindow = 4 * 1024 * 1024L;
@@ -773,6 +777,14 @@ public class ConfigNodeConfig {
     this.dataRegionRatisLogUnsafeFlushEnable = dataRegionRatisLogUnsafeFlushEnable;
   }
 
+  public int getDataRegionRatisLogForceSyncNum() {
+    return dataRegionRatisLogForceSyncNum;
+  }
+
+  public void setDataRegionRatisLogForceSyncNum(int dataRegionRatisLogForceSyncNum) {
+    this.dataRegionRatisLogForceSyncNum = dataRegionRatisLogForceSyncNum;
+  }
+
   public long getDataRegionRatisLogSegmentSizeMax() {
     return dataRegionRatisLogSegmentSizeMax;
   }
@@ -787,6 +799,16 @@ public class ConfigNodeConfig {
 
   public void setDataRegionRatisGrpcFlowControlWindow(long dataRegionRatisGrpcFlowControlWindow) {
     this.dataRegionRatisGrpcFlowControlWindow = dataRegionRatisGrpcFlowControlWindow;
+  }
+
+  public int getDataRegionRatisGrpcLeaderOutstandingAppendsMax() {
+    return dataRegionRatisGrpcLeaderOutstandingAppendsMax;
+  }
+
+  public void setDataRegionRatisGrpcLeaderOutstandingAppendsMax(
+      int dataRegionRatisGrpcLeaderOutstandingAppendsMax) {
+    this.dataRegionRatisGrpcLeaderOutstandingAppendsMax =
+        dataRegionRatisGrpcLeaderOutstandingAppendsMax;
   }
 
   public long getDataRegionRatisRpcLeaderElectionTimeoutMinMs() {
--- server/src/main/java/org/apache/iotdb/db/conf/IoTDBConfig.java
+++ server/src/main/java/org/apache/iotdb/db/conf/IoTDBConfig.java
@@ -1044,11 +1044,15 @@ public class IoTDBConfig {
   private boolean dataRatisConsensusLogUnsafeFlushEnable = false;
   private boolean schemaRatisConsensusLogUnsafeFlushEnable = false;
 
+  private int dataRatisConsensusLogForceSyncNum = 128;
+
   private long dataRatisConsensusLogSegmentSizeMax = 24 * 1024 * 1024L;
   private long schemaRatisConsensusLogSegmentSizeMax = 24 * 1024 * 1024L;
 
   private long dataRatisConsensusGrpcFlowControlWindow = 4 * 1024 * 1024L;
   private long schemaRatisConsensusGrpcFlowControlWindow = 4 * 1024 * 1024L;
+
+  private int dataRatisConsensusGrpcLeaderOutstandingAppendsMax = 128;
 
   private long dataRatisConsensusLeaderElectionTimeoutMinMs = 2000L;
   private long schemaRatisConsensusLeaderElectionTimeoutMinMs = 2000L;
@@ -1101,6 +1105,12 @@ public class IoTDBConfig {
   // customizedProperties, this should be empty by default.
   private Properties customizedProperties = new Properties();
 
+  // IoTConsensus Config
+  private int maxLogEntriesNumPerBatch = 1024;
+  private int maxSizePerBatch = 16 * 1024 * 1024;
+  private int maxPendingBatchesNum = 12;
+  private double maxMemoryRatioForQueue = 0.6;
+
   /** The maximum number of threads that can be used to execute subtasks in PipeSubtaskExecutor */
   private int pipeMaxThreadNum = 5;
 
@@ -1115,6 +1125,38 @@ public class IoTDBConfig {
   private String RateLimiterType = "FixedIntervalRateLimiter";
 
   IoTDBConfig() {}
+
+  public int getMaxLogEntriesNumPerBatch() {
+    return maxLogEntriesNumPerBatch;
+  }
+
+  public int getMaxSizePerBatch() {
+    return maxSizePerBatch;
+  }
+
+  public int getMaxPendingBatchesNum() {
+    return maxPendingBatchesNum;
+  }
+
+  public double getMaxMemoryRatioForQueue() {
+    return maxMemoryRatioForQueue;
+  }
+
+  public void setMaxLogEntriesNumPerBatch(int maxLogEntriesNumPerBatch) {
+    this.maxLogEntriesNumPerBatch = maxLogEntriesNumPerBatch;
+  }
+
+  public void setMaxSizePerBatch(int maxSizePerBatch) {
+    this.maxSizePerBatch = maxSizePerBatch;
+  }
+
+  public void setMaxPendingBatchesNum(int maxPendingBatchesNum) {
+    this.maxPendingBatchesNum = maxPendingBatchesNum;
+  }
+
+  public void setMaxMemoryRatioForQueue(double maxMemoryRatioForQueue) {
+    this.maxMemoryRatioForQueue = maxMemoryRatioForQueue;
+  }
 
   public float getUdfMemoryBudgetInMB() {
     return udfMemoryBudgetInMB;
@@ -3481,6 +3523,14 @@ public class IoTDBConfig {
     this.dataRatisConsensusLogUnsafeFlushEnable = dataRatisConsensusLogUnsafeFlushEnable;
   }
 
+  public int getDataRatisConsensusLogForceSyncNum() {
+    return dataRatisConsensusLogForceSyncNum;
+  }
+
+  public void setDataRatisConsensusLogForceSyncNum(int dataRatisConsensusLogForceSyncNum) {
+    this.dataRatisConsensusLogForceSyncNum = dataRatisConsensusLogForceSyncNum;
+  }
+
   public long getDataRatisConsensusLogSegmentSizeMax() {
     return dataRatisConsensusLogSegmentSizeMax;
   }
@@ -3496,6 +3546,16 @@ public class IoTDBConfig {
   public void setDataRatisConsensusGrpcFlowControlWindow(
       long dataRatisConsensusGrpcFlowControlWindow) {
     this.dataRatisConsensusGrpcFlowControlWindow = dataRatisConsensusGrpcFlowControlWindow;
+  }
+
+  public int getDataRatisConsensusGrpcLeaderOutstandingAppendsMax() {
+    return dataRatisConsensusGrpcLeaderOutstandingAppendsMax;
+  }
+
+  public void setDataRatisConsensusGrpcLeaderOutstandingAppendsMax(
+      int dataRatisConsensusGrpcLeaderOutstandingAppendsMax) {
+    this.dataRatisConsensusGrpcLeaderOutstandingAppendsMax =
+        dataRatisConsensusGrpcLeaderOutstandingAppendsMax;
   }
 
   public long getDataRatisConsensusLeaderElectionTimeoutMinMs() {
