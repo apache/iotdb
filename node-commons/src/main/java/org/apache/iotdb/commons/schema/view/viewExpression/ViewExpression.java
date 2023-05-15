c46e4e83a1bcee370eeef9515a0b51aac616d928
citrusreticulata <1372722289@qq.com>
2023-05-15 17:30:33 +0800

    fix bug: can not deserialize ConstantViewOperand using byteBuffer

--- node-commons/src/main/java/org/apache/iotdb/commons/schema/view/viewExpression/ViewExpression.java
+++ node-commons/src/main/java/org/apache/iotdb/commons/schema/view/viewExpression/ViewExpression.java
@@ -126,6 +126,7 @@ public abstract class ViewExpression {
     switch (type) {
       case -4:
         expression = new ConstantViewOperand(byteBuffer);
+        break;
       case -3:
         expression = new TimestampViewOperand(byteBuffer);
         break;
