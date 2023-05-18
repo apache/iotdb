ff78cbd42d0271ece84dc3e04f8d802f3316ef93
Caideyipi <87789683+Caideyipi@users.noreply.github.com>
2023-05-18 19:15:32 +0800

    [IOTDB-5845] Failed to register PipeRuntimeCoordinator to loadPublisher in LoadManager (#9882)
    Co-authored-by: Steve Yurong Su <rong@apache.org>

--- confignode/src/main/java/org/apache/iotdb/confignode/manager/load/LoadManager.java
+++ confignode/src/main/java/org/apache/iotdb/confignode/manager/load/LoadManager.java
@@ -88,8 +88,7 @@ public class LoadManager {
         new StatisticsService(configManager, routeBalancer, loadCache, loadPublisher);
 
     loadPublisher.register(statisticsService);
-    // TODO: enable
-    // loadPublisher.register(configManager.getPipeManager().getPipeRuntimeCoordinator());
+    loadPublisher.register(configManager.getPipeManager().getPipeRuntimeCoordinator());
   }
 
   /**
