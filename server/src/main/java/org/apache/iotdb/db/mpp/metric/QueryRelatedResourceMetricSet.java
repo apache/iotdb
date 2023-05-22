53de4d2867b367d7f524edce817c68c68bfc0ab0
Weihao Li <18110526956@163.com>
2023-05-22 09:31:06 +0800

    split
    Signed-off-by: Weihao Li <18110526956@163.com>

--- server/src/main/java/org/apache/iotdb/db/mpp/metric/QueryRelatedResourceMetricSet.java
+++ server/src/main/java/org/apache/iotdb/db/mpp/metric/QueryRelatedResourceMetricSet.java
@@ -21,11 +21,9 @@ package org.apache.iotdb.db.mpp.metric;
 
 import org.apache.iotdb.commons.service.metric.enums.Metric;
 import org.apache.iotdb.commons.service.metric.enums.Tag;
-import org.apache.iotdb.db.mpp.execution.exchange.MPPDataExchangeManager;
 import org.apache.iotdb.db.mpp.execution.exchange.MPPDataExchangeService;
 import org.apache.iotdb.db.mpp.execution.fragment.FragmentInstanceManager;
 import org.apache.iotdb.db.mpp.execution.memory.MemoryPool;
-import org.apache.iotdb.db.mpp.execution.schedule.DriverScheduler;
 import org.apache.iotdb.db.mpp.plan.Coordinator;
 import org.apache.iotdb.db.mpp.plan.planner.LocalExecutionPlanner;
 import org.apache.iotdb.metrics.AbstractMetricService;
@@ -47,29 +45,17 @@ public class QueryRelatedResourceMetricSet implements IMetricSet {
   private static final String INSTANCE_CONTEXT_SIZE = "instance_context_size";
   private static final String INSTANCE_EXECUTION_SIZE = "instance_execution_size";
 
-  // MPPDataExchangeManager
-  private static final MPPDataExchangeManager dataExchangeManager =
-      MPPDataExchangeService.getInstance().getMPPDataExchangeManager();
-  private static final String DATA_EXCHANGE_MANAGER = Metric.DATA_EXCHANGE_MANAGER.toString();
-  private static final String SHUFFLE_SINK_HANDLE_SIZE = "shuffle_sink_handle_size";
-  private static final String SOURCE_HANDLE_SIZE = "source_handle_size";
-
   // MemoryPool
   private static final MemoryPool memoryPool =
-      dataExchangeManager.getLocalMemoryManager().getQueryPool();
+      MPPDataExchangeService.getInstance()
+          .getMPPDataExchangeManager()
+          .getLocalMemoryManager()
+          .getQueryPool();
   private static final String MEMORY_POOL = Metric.MEMORY_POOL.toString();
   private static final String MAX_BYTES = "max_bytes";
   private static final String REMAINING_BYTES = "remaining_bytes";
   private static final String QUERY_MEMORY_RESERVATION_SIZE = "query_memory_reservation_size";
   private static final String MEMORY_RESERVATION_SIZE = "memory_reservation_size";
-
-  // DriverScheduler
-  private static final DriverScheduler driverScheduler = DriverScheduler.getInstance();
-  private static final String DRIVER_SCHEDULER = Metric.DRIVER_SCHEDULER.toString();
-  private static final String READY_QUEUE_SIZE = "ready_queue_size";
-  private static final String BLOCKED_TASK_SIZE = "blocked_task_size";
-  private static final String TIMEOUT_QUEUE_SIZE = "timeout_queue_size";
-  private static final String QUERY_MAP_SIZE = "query_map_size";
 
   // LocalExecutionPlanner
   private static final LocalExecutionPlanner localExecutionPlanner =
@@ -104,22 +90,6 @@ public class QueryRelatedResourceMetricSet implements IMetricSet {
         Tag.NAME.toString(),
         INSTANCE_EXECUTION_SIZE);
 
-    // MPPDataExchangeManager
-    metricService.createAutoGauge(
-        DATA_EXCHANGE_MANAGER,
-        MetricLevel.IMPORTANT,
-        dataExchangeManager,
-        MPPDataExchangeManager::getShuffleSinkHandleSize,
-        Tag.NAME.toString(),
-        SHUFFLE_SINK_HANDLE_SIZE);
-    metricService.createAutoGauge(
-        DATA_EXCHANGE_MANAGER,
-        MetricLevel.IMPORTANT,
-        dataExchangeManager,
-        MPPDataExchangeManager::getSourceHandleSize,
-        Tag.NAME.toString(),
-        SOURCE_HANDLE_SIZE);
-
     // MemoryPool
     metricService
         .getOrCreateGauge(MEMORY_POOL, MetricLevel.IMPORTANT, Tag.NAME.toString(), MAX_BYTES)
@@ -145,36 +115,6 @@ public class QueryRelatedResourceMetricSet implements IMetricSet {
         MemoryPool::getMemoryReservationSize,
         Tag.NAME.toString(),
         MEMORY_RESERVATION_SIZE);
-
-    // DriverScheduler
-    metricService.createAutoGauge(
-        DRIVER_SCHEDULER,
-        MetricLevel.IMPORTANT,
-        driverScheduler,
-        DriverScheduler::getReadyQueueTaskCount,
-        Tag.NAME.toString(),
-        READY_QUEUE_SIZE);
-    metricService.createAutoGauge(
-        DRIVER_SCHEDULER,
-        MetricLevel.IMPORTANT,
-        driverScheduler,
-        DriverScheduler::getBlockQueueTaskCount,
-        Tag.NAME.toString(),
-        BLOCKED_TASK_SIZE);
-    metricService.createAutoGauge(
-        DRIVER_SCHEDULER,
-        MetricLevel.IMPORTANT,
-        driverScheduler,
-        DriverScheduler::getTimeoutQueueTaskCount,
-        Tag.NAME.toString(),
-        TIMEOUT_QUEUE_SIZE);
-    metricService.createAutoGauge(
-        DRIVER_SCHEDULER,
-        MetricLevel.IMPORTANT,
-        driverScheduler,
-        DriverScheduler::getQueryMapSize,
-        Tag.NAME.toString(),
-        QUERY_MAP_SIZE);
 
     // LocalExecutionPlanner
     metricService.createAutoGauge(
@@ -204,15 +144,6 @@ public class QueryRelatedResourceMetricSet implements IMetricSet {
         Tag.NAME.toString(),
         INSTANCE_EXECUTION_SIZE);
 
-    // MPPDataExchangeManager
-    metricService.remove(
-        MetricType.AUTO_GAUGE,
-        DATA_EXCHANGE_MANAGER,
-        Tag.NAME.toString(),
-        SHUFFLE_SINK_HANDLE_SIZE);
-    metricService.remove(
-        MetricType.AUTO_GAUGE, DATA_EXCHANGE_MANAGER, Tag.NAME.toString(), SOURCE_HANDLE_SIZE);
-
     // MemoryPool
     metricService.remove(MetricType.GAUGE, MEMORY_POOL, Tag.NAME.toString(), MAX_BYTES);
     metricService.remove(MetricType.AUTO_GAUGE, MEMORY_POOL, Tag.NAME.toString(), REMAINING_BYTES);
@@ -220,16 +151,6 @@ public class QueryRelatedResourceMetricSet implements IMetricSet {
         MetricType.AUTO_GAUGE, MEMORY_POOL, Tag.NAME.toString(), QUERY_MEMORY_RESERVATION_SIZE);
     metricService.remove(
         MetricType.AUTO_GAUGE, MEMORY_POOL, Tag.NAME.toString(), MEMORY_RESERVATION_SIZE);
-
-    // DriverScheduler
-    metricService.remove(
-        MetricType.AUTO_GAUGE, DRIVER_SCHEDULER, Tag.NAME.toString(), READY_QUEUE_SIZE);
-    metricService.remove(
-        MetricType.AUTO_GAUGE, DRIVER_SCHEDULER, Tag.NAME.toString(), BLOCKED_TASK_SIZE);
-    metricService.remove(
-        MetricType.AUTO_GAUGE, DRIVER_SCHEDULER, Tag.NAME.toString(), TIMEOUT_QUEUE_SIZE);
-    metricService.remove(
-        MetricType.AUTO_GAUGE, DRIVER_SCHEDULER, Tag.NAME.toString(), QUERY_MAP_SIZE);
 
     // LocalExecutionPlanner
     metricService.remove(
