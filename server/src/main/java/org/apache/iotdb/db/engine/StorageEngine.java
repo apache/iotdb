024cc1b4c8d003b971f2cfb3111b2e20b99f9c0b
HeimingZ <zhuhmch@qq.com>
2023-05-24 16:42:56 +0800

    add cache recover

--- server/src/main/java/org/apache/iotdb/db/engine/StorageEngine.java
+++ server/src/main/java/org/apache/iotdb/db/engine/StorageEngine.java
@@ -63,6 +63,7 @@ import org.apache.iotdb.db.utils.UpgradeUtils;
 import org.apache.iotdb.db.wal.WALManager;
 import org.apache.iotdb.db.wal.exception.WALException;
 import org.apache.iotdb.db.wal.recover.WALRecoverManager;
+import org.apache.iotdb.os.cache.CacheRecoverTask;
 import org.apache.iotdb.rpc.RpcUtils;
 import org.apache.iotdb.rpc.TSStatusCode;
 import org.apache.iotdb.tsfile.exception.write.PageException;
@@ -272,6 +273,15 @@ public class StorageEngine implements IService {
             };
         futures.add(cachedThreadPool.submit(recoverDataRegionTask));
       }
+    }
+    // submit object storage cache recover task
+    if (config.isEnableObjectStorage()) {
+      futures.add(
+          cachedThreadPool.submit(
+              () -> {
+                new CacheRecoverTask().run();
+                return null;
+              }));
     }
   }
 
