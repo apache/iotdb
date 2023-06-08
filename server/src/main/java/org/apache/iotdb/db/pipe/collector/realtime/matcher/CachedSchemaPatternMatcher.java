9d9ce6b44ae63b97c40207a54c920f8ea7af2e9c
Caideyipi <1336190078@qq.com>
2023-06-08 18:26:48 +0800

    Ignore root.__system

--- server/src/main/java/org/apache/iotdb/db/pipe/collector/realtime/matcher/CachedSchemaPatternMatcher.java
+++ server/src/main/java/org/apache/iotdb/db/pipe/collector/realtime/matcher/CachedSchemaPatternMatcher.java
@@ -37,6 +37,7 @@ import java.util.concurrent.locks.ReentrantReadWriteLock;
 public class CachedSchemaPatternMatcher implements PipeDataRegionMatcher {
 
   private static final Logger LOGGER = LoggerFactory.getLogger(CachedSchemaPatternMatcher.class);
+  private static final String SYSTEM_DEVICE = "root.__system";
 
   private final ReentrantReadWriteLock lock;
 
@@ -84,8 +85,8 @@ public class CachedSchemaPatternMatcher implements PipeDataRegionMatcher {
     }
   }
 
-  // TODO: maximum the efficiency of matching when pattern is root
-  // TODO: memory control
+  // TODO: Maximum the efficiency of matching when pattern is root
+  // TODO: Memory control
   @Override
   public Set<PipeRealtimeDataRegionCollector> match(PipeRealtimeCollectEvent event) {
     final Set<PipeRealtimeDataRegionCollector> matchedCollectors = new HashSet<>();
@@ -100,55 +101,60 @@ public class CachedSchemaPatternMatcher implements PipeDataRegionMatcher {
         final String device = entry.getKey();
         final String[] measurements = entry.getValue();
 
-        // 1. try to get matched collectors from cache, if not success, match them by device
+        // Ignore device "root.__system"
+        if (device.equals(SYSTEM_DEVICE)) {
+          continue;
+        }
+
+        // 1. Try to get matched collectors from cache, if not success, match them by device
         final Set<PipeRealtimeDataRegionCollector> collectorsFilteredByDevice =
             deviceToCollectorsCache.get(device, this::filterCollectorsByDevice);
-        // this would not happen
+        // This would not happen
         if (collectorsFilteredByDevice == null) {
           LOGGER.warn(String.format("Match result NPE when handle device %s", device));
           continue;
         }
 
-        // 2. filter matched candidate collectors by measurements
+        // 2. Filter matched candidate collectors by measurements
         if (measurements.length == 0) {
           // `measurements` is empty (only in case of tsfile event). match all collectors.
           //
-          // case 1: for example, pattern is root.a.b, device is root.a.b.c, measurement can be any.
-          // in this case, the collector can be matched without checking the measurements.
+          // Case 1: for example, pattern is root.a.b, device is root.a.b.c, measurement can be any.
+          // In this case, the collector can be matched without checking the measurements.
           //
-          // case 2: for example, pattern is root.a.b.c, device is root.a.b.
-          // in this situation, the collector can not be matched in some cases, but we can not know
+          // Case 2: for example, pattern is root.a.b.c, device is root.a.b.
+          // In this situation, the collector can not be matched in some cases, but we can not know
           // all the measurements of the device in an efficient way, so we ASSUME that the collector
-          // can be matched. this is a trade-off between efficiency and accuracy. for most user's
+          // can be matched. This is a trade-off between efficiency and accuracy. For most user's
           // usage, this is acceptable, which may result in some unnecessary data processing and
           // transmission, but will not result in data loss.
           matchedCollectors.addAll(collectorsFilteredByDevice);
         } else {
           // `measurements` is not empty (only in case of tablet event). match collectors by
-          // measurements.
+          // Measurements.
           collectorsFilteredByDevice.forEach(
               collector -> {
                 final String pattern = collector.getPattern();
 
-                // case 1: for example, pattern is root.a.b and device is root.a.b.c
-                // in this case, the collector can be matched without checking the measurements
+                // Case 1: for example, pattern is root.a.b and device is root.a.b.c
+                // In this case, the collector can be matched without checking the measurements
                 if (pattern.length() <= device.length()) {
                   matchedCollectors.add(collector);
                 }
-                // case 2: for example, pattern is root.a.b.c and device is root.a.b
-                // in this case, we need to check the full path
+                // Case 2: for example, pattern is root.a.b.c and device is root.a.b
+                // In this case, we need to check the full path
                 else {
                   for (String measurement : measurements) {
-                    // for example, pattern is root.a.b.c, device is root.a.b and measurement is c
-                    // in this case, the collector can be matched. other cases are not matched.
-                    // please note that there should be a . between device and measurement.
+                    // For example, pattern is root.a.b.c, device is root.a.b and measurement is c
+                    // I this case, the collector can be matched. Other cases are not matched.
+                    // Please note that there should be a . between device and measurement.
                     if (
-                    // low cost check comes first
+                    // Low cost check comes first
                     pattern.length() == device.length() + measurement.length() + 1
-                        // high cost check comes later
+                        // High cost check comes later
                         && pattern.endsWith(TsFileConstant.PATH_SEPARATOR + measurement)) {
                       matchedCollectors.add(collector);
-                      // there would be no more matched collectors because the measurements are
+                      // There would be no more matched collectors because the measurements are
                       // unique
                       break;
                     }
