821277b10de802925fa61507f744cefdc83d3f2c
Marcos_Zyk <38524330+MarcosZyk@users.noreply.github.com>
2023-06-16 00:00:26 +0800

    [To rel/1.2] Fix show latest alias series (#10167)

--- server/src/main/java/org/apache/iotdb/db/mpp/plan/analyze/AnalyzeVisitor.java
+++ server/src/main/java/org/apache/iotdb/db/mpp/plan/analyze/AnalyzeVisitor.java
@@ -2644,6 +2644,7 @@ public class AnalyzeVisitor extends StatementVisitor<Analysis, MPPQueryContext> 
       // request schema fetch API
       logger.debug("[StartFetchSchema]");
       ISchemaTree schemaTree = schemaFetcher.fetchSchema(patternTree, context);
+      updateSchemaTreeByViews(analysis, schemaTree);
       logger.debug("[EndFetchSchema]]");
 
       analyzeLastSource(
