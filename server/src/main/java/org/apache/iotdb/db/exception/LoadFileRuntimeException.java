8b8548bd7ed081cc6d5b590eaa65871e89563f6b
Zhijia Cao <caozhijia@126.com>
2023-06-25 16:02:13 +0800

    fix bugs and code smells of dir:engine/load

---
+++ server/src/main/java/org/apache/iotdb/db/exception/LoadFileRuntimeException.java
@@ -0,0 +1,7 @@
+package org.apache.iotdb.db.exception;
+
+public class LoadFileRuntimeException extends RuntimeException {
+  public LoadFileRuntimeException(String message, Throwable cause) {
+    super(message, cause);
+  }
+}
