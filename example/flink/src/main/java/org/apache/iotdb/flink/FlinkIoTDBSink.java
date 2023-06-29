68614458774832c88d021268b1ef80ccaf0e36dc
Summer <43237967+2b3c511@users.noreply.github.com>
2023-06-29 16:52:34 +0800

    Sona:add space before package path and fix 9 bugs(close stmt) in IoTDBDatabaseMetadata (#10377)

--- example/flink/src/main/java/org/apache/iotdb/flink/FlinkIoTDBSink.java
+++ example/flink/src/main/java/org/apache/iotdb/flink/FlinkIoTDBSink.java
@@ -15,6 +15,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package org.apache.iotdb.flink;
 
 import org.apache.iotdb.flink.options.IoTDBSinkOptions;
--- iotdb-client/cli/src/main/java/org/apache/iotdb/exception/ArgsErrorException.java
+++ iotdb-client/cli/src/main/java/org/apache/iotdb/exception/ArgsErrorException.java
@@ -16,6 +16,7 @@
  * specific language governing permissions and limitations
  * under the License.
  */
+
 package org.apache.iotdb.exception;
 
 public class ArgsErrorException extends Exception {
