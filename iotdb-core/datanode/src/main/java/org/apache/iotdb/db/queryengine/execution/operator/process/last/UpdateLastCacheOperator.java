5a50f0df22ff8649a23a5139474cdede1736d95e
Weihao Li <60659567+Wei-hao-Li@users.noreply.github.com>
2023-07-16 18:34:34 +0800

    [IOTDB-6019] Fix concurrent update of last query

--- iotdb-core/datanode/src/main/java/org/apache/iotdb/db/queryengine/execution/operator/process/last/UpdateLastCacheOperator.java
+++ iotdb-core/datanode/src/main/java/org/apache/iotdb/db/queryengine/execution/operator/process/last/UpdateLastCacheOperator.java
@@ -24,7 +24,6 @@ import org.apache.iotdb.db.queryengine.execution.operator.Operator;
 import org.apache.iotdb.db.queryengine.execution.operator.OperatorContext;
 import org.apache.iotdb.db.queryengine.plan.analyze.cache.schema.DataNodeSchemaCache;
 import org.apache.iotdb.tsfile.file.metadata.enums.TSDataType;
-import org.apache.iotdb.tsfile.read.TimeValuePair;
 import org.apache.iotdb.tsfile.read.common.block.TsBlock;
 import org.apache.iotdb.tsfile.utils.TsPrimitiveType;
 
@@ -46,8 +45,9 @@ public class UpdateLastCacheOperator extends AbstractUpdateLastCacheOperator {
       MeasurementPath fullPath,
       TSDataType dataType,
       DataNodeSchemaCache dataNodeSchemaCache,
-      boolean needUpdateCache) {
-    super(operatorContext, child, dataNodeSchemaCache, needUpdateCache);
+      boolean needUpdateCache,
+      boolean isNeedUpdateNullEntry) {
+    super(operatorContext, child, dataNodeSchemaCache, needUpdateCache, isNeedUpdateNullEntry);
     this.fullPath = fullPath;
     this.dataType = dataType.name();
   }
@@ -66,16 +66,18 @@ public class UpdateLastCacheOperator extends AbstractUpdateLastCacheOperator {
 
     // last value is null
     if (res.getColumn(0).isNull(0)) {
+      // we still need to update last cache if there is no data for this time series to avoid
+      // scanning all files each time
+      if (needUpdateNullEntry) {
+        mayUpdateLastCache(Long.MIN_VALUE, null, fullPath);
+      }
       return LAST_QUERY_EMPTY_TSBLOCK;
     }
 
     long lastTime = res.getColumn(0).getLong(0);
     TsPrimitiveType lastValue = res.getColumn(1).getTsPrimitiveType(0);
 
-    if (needUpdateCache) {
-      TimeValuePair timeValuePair = new TimeValuePair(lastTime, lastValue);
-      lastCache.updateLastCache(getDatabaseName(), fullPath, timeValuePair, false, Long.MIN_VALUE);
-    }
+    mayUpdateLastCache(lastTime, lastValue, fullPath);
 
     tsBlockBuilder.reset();
     appendLastValueToTsBlockBuilder(lastTime, lastValue);
