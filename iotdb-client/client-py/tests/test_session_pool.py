a268bcfd9bb2df69dac0043f42b3e2bd1abe5b65
YangCaiyin <ycycse@gmail.com>
2023-07-22 14:39:41 +0800

    [IOTDB-6069] Add more checks for session pool in client-py

--- iotdb-client/client-py/tests/test_session_pool.py
+++ iotdb-client/client-py/tests/test_session_pool.py
@@ -20,20 +20,19 @@ from threading import Thread
 from iotdb.IoTDBContainer import IoTDBContainer
 from iotdb.SessionPool import create_session_pool, PoolConfig
 
+CONTAINER_NAME = "iotdb:dev"
+
 
 def test_session_pool():
-    with IoTDBContainer("iotdb:dev") as db:
+    with IoTDBContainer(CONTAINER_NAME) as db:
         db: IoTDBContainer
         max_pool_size = 2
         pool_config = PoolConfig(db.get_container_host_ip(), db.get_exposed_port(6667), "root", "root",
                                  [], 1024, "Asia/Shanghai", 3)
         session_pool = create_session_pool(pool_config, max_pool_size, 3000)
         session = session_pool.get_session()
-        session.open(False)
         assert session.is_open() is True
-
         session2 = session_pool.get_session()
-        assert session2 is not None
 
         timeout = False
         try:
@@ -45,7 +44,6 @@ def test_session_pool():
 
         Thread(target=lambda: session_pool.put_back(session2)).start()
         session3 = session_pool.get_session()
-        assert session3 is not None
 
         session_pool.close()
 
@@ -64,3 +62,40 @@ def test_session_pool():
             is_closed = True
             assert str(e) == "SessionPool has already been closed, please close the session manually."
         assert is_closed is True
+
+
+def test_session_pool_by_node_urls():
+    with IoTDBContainer(CONTAINER_NAME) as db:
+        db: IoTDBContainer
+        node_url = db.get_container_host_ip() + ":" + str(db.get_exposed_port(6667))
+        max_pool_size = 1
+        pool_config = PoolConfig(node_urls=[node_url], user_name="root", password="root")
+        session_pool = create_session_pool(pool_config, max_pool_size, 3000)
+
+        session = session_pool.get_session()
+
+        timeout = False
+        try:
+            session_pool.get_session()
+        except TimeoutError as e:
+            timeout = True
+            assert str(e) == "Wait to get session timeout in SessionPool, current pool size: " + str(max_pool_size)
+
+        assert timeout is True
+
+        session_pool.put_back(session)
+        session2 = session_pool.get_session()
+        assert session2.is_open() is True
+
+        session_pool.put_back(session2)
+
+        session_pool.close()
+        assert session2.is_open() is False
+
+        check = False
+        try:
+            session_pool.get_session()
+        except ConnectionError as e:
+            check = True
+            assert str(e) == "SessionPool has already been closed."
+        assert check is True
