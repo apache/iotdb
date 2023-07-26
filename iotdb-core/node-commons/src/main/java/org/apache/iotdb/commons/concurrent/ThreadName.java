bc6f0bf1c2b3b3ef6bcb255e0c800426a85ed18c
彭俊植 <pengjunzhi@meituan.com>
2023-07-26 22:35:13 +0800

    fix review of ThreadName

--- iotdb-core/node-commons/src/main/java/org/apache/iotdb/commons/concurrent/ThreadName.java
+++ iotdb-core/node-commons/src/main/java/org/apache/iotdb/commons/concurrent/ThreadName.java
@@ -136,6 +136,7 @@ public enum ThreadName {
   // NOTICE: The thread name of jvm cannot be edited here!
   // We list the thread name here just for distinguishing what module the thread belongs to.
   JVM_PAUSE_MONITOR("JvmPauseMonitor"),
+  JVM_GC_STATISTICS_MONITOR("JVM-GC-Statistics-Monitor"),
   PARALLEL_GC("GC task thread"),
   G1_GC("GC Thread"),
   G1_MAIN_MARKER("G1 Main Marker"),
@@ -169,7 +170,6 @@ public enum ThreadName {
   UPGRADE_TASK("UpgradeThread"),
   REGION_MIGRATE("Region-Migrate-Pool"),
   STORAGE_ENGINE_RECOVER_TRIGGER("StorageEngine-RecoverTrigger"),
-  JVM_GC_STATISTICS_MONITOR("JVM-GC-Statistics-Monitor"),
   // the unknown thread name is used for metrics
   UNKOWN("UNKNOWN");
 
@@ -269,6 +269,7 @@ public enum ThreadName {
       new HashSet<>(
           Arrays.asList(
               JVM_PAUSE_MONITOR,
+              JVM_GC_STATISTICS_MONITOR,
               PARALLEL_GC,
               G1_GC,
               G1_MAIN_MARKER,
