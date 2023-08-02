cdc722724ac8006eb0daa64ca19341588d1aed23
Beyyes <cgf1993@foxmail.com>
2023-08-02 09:22:16 +0800

    fix count time accumalator

--- iotdb-core/datanode/src/main/java/org/apache/iotdb/db/queryengine/plan/planner/OperatorTreeGenerator.java
+++ iotdb-core/datanode/src/main/java/org/apache/iotdb/db/queryengine/plan/planner/OperatorTreeGenerator.java
@@ -1562,14 +1562,14 @@ public class OperatorTreeGenerator extends PlanVisitor<Operator, LocalExecutionP
     for (List<String> inputColumnNamesOfOneInput : inputColumnNames) {
       // it may include double parts
       List<List<InputLocation>> inputLocationParts = new ArrayList<>();
-      // inputColumnNamesOfOneInput.forEach(o -> inputLocationParts.add(layout.get(o)));
-      for (String input : inputColumnNamesOfOneInput) {
-        if ("Time".equals(input)) {
-          inputLocationParts.addAll(layout.values());
-        } else {
-          inputLocationParts.add(layout.get(input));
-        }
-      }
+      inputColumnNamesOfOneInput.forEach(o -> inputLocationParts.add(layout.get(o)));
+      //      for (String input : inputColumnNamesOfOneInput) {
+      //        if ("Time".equals(input)) {
+      //          inputLocationParts.addAll(layout.values());
+      //        } else {
+      //          inputLocationParts.add(layout.get(input));
+      //        }
+      //      }
       for (int i = 0; i < inputLocationParts.get(0).size(); i++) {
         if (inputColumnNamesOfOneInput.size() == 1) {
           inputLocationList.add(new InputLocation[] {inputLocationParts.get(0).get(i)});
@@ -2405,7 +2405,7 @@ public class OperatorTreeGenerator extends PlanVisitor<Operator, LocalExecutionP
           .computeIfAbsent(TIMESTAMP_EXPRESSION_STRING, key -> new ArrayList<>())
           .add(new InputLocation(tsBlockIndex, -1));
       int valueColumnIndex = 0;
-      valueColumnIndex++;
+      // valueColumnIndex++;
       for (String columnName : childNode.getOutputColumnNames()) {
         outputMappings
             .computeIfAbsent(columnName, key -> new ArrayList<>())
