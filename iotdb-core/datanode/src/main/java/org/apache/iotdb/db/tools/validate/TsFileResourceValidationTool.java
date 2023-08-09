4d2e74aab22d7970ecf9fa1dbf71fdd06e1fe0e4
shuwenwei <s13979062948@gmail.com>
2023-08-09 11:44:59 +0800

    add confirm to repair overlap files

--- iotdb-core/datanode/src/main/java/org/apache/iotdb/db/tools/validate/TsFileResourceValidationTool.java
+++ iotdb-core/datanode/src/main/java/org/apache/iotdb/db/tools/validate/TsFileResourceValidationTool.java
@@ -19,6 +19,8 @@
 
 package org.apache.iotdb.db.tools.validate;
 
+import org.apache.iotdb.db.storageengine.dataregion.modification.Modification;
+import org.apache.iotdb.db.storageengine.dataregion.modification.ModificationFile;
 import org.apache.iotdb.db.storageengine.dataregion.tsfile.TsFileResource;
 import org.apache.iotdb.tsfile.common.constant.TsFileConstant;
 
@@ -26,13 +28,17 @@ import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
+import java.util.Scanner;
 import java.util.Set;
 
 public class TsFileResourceValidationTool {
 
+  private static final Set<File> toMoveFiles = new HashSet<>();
+  private static int overlapFileNum = 0;
   public static void main(String[] args) throws IOException {
     if (args.length == 0) {
       System.out.println("Please input sequence data dir path.");
@@ -41,6 +47,29 @@ public class TsFileResourceValidationTool {
     String sequenceDataPath = args[0];
     File sequenceData = new File(sequenceDataPath);
     validateSequenceDataDir(sequenceData);
+    if (!confirmMoveOverlapFilesToUnsequenceSpace()) {
+      return;
+    }
+
+  }
+
+  private static boolean confirmMoveOverlapFilesToUnsequenceSpace() {
+    System.out.println("Overlap tsfile num is " + overlapFileNum);
+    System.out.println("Corresponding file num is " + toMoveFiles.size());
+    System.out.println("Repair overlap tsfiles (y/n)");
+    Scanner scanner = new Scanner(System.in);
+    String input = scanner.nextLine();
+    return "y".equals(input);
+  }
+
+  private static void moveOverlapFiles() {
+    for (File f : toMoveFiles) {
+      if (!f.exists()) {
+        continue;
+      }
+      String filePath = f.getAbsolutePath();
+
+    }
   }
 
   public static void validateSequenceDataDir(File sequenceDataDir) throws IOException {
@@ -71,6 +100,7 @@ public class TsFileResourceValidationTool {
     Map<String, TsFileResource> deviceLastExistTsFileMap = new HashMap<>();
     for (TsFileResource resource : resources) {
       Set<String> devices = resource.getDevices();
+      boolean fileHasOverlap = false;
       for (String device : devices) {
         long deviceStartTimeInCurrentFile = resource.getStartTime(device);
         long deviceEndTimeInCurrentFile = resource.getEndTime(device);
@@ -89,12 +119,25 @@ public class TsFileResourceValidationTool {
               device,
               deviceEndTimeInPreviousFile,
               deviceStartTimeInCurrentFile);
-          continue;
+          if (!fileHasOverlap) {
+            recordOverlapTsFile(resource);
+          }
         }
         deviceEndTimeMap.put(device, deviceEndTimeInCurrentFile);
         deviceLastExistTsFileMap.put(device, resource);
       }
     }
+  }
+
+  private static void recordOverlapTsFile(TsFileResource overlapFile) {
+    String filePath = overlapFile.getTsFilePath();
+    toMoveFiles.add(overlapFile.getTsFile());
+    toMoveFiles.add(new File(filePath + TsFileResource.RESOURCE_SUFFIX));
+    ModificationFile modsFile = overlapFile.getModFile();
+    if (modsFile.exists()) {
+      toMoveFiles.add(new File(modsFile.getFilePath()));
+    }
+    overlapFileNum++;
   }
 
   public static List<TsFileResource> loadTsFileResources(File timePartitionDir) throws IOException {
@@ -105,6 +148,7 @@ public class TsFileResourceValidationTool {
       }
       String resourcePath = tsfile.getAbsolutePath() + TsFileResource.RESOURCE_SUFFIX;
       if (!new File(resourcePath).exists()) {
+        System.out.println(tsfile.getAbsolutePath() + " is skipped because resource file is not exist.");
         continue;
       }
       TsFileResource resource = new TsFileResource(tsfile);
