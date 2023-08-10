9dd896f2ff69a468b0d4370c1ac297a023795dfc
Zhijia Cao <caozhijia@126.com>
2023-08-10 15:19:55 +0800

    file async

--- iotdb-core/datanode/src/main/java/org/apache/iotdb/db/storageengine/dataregion/compaction/tool/reader/SingleSequenceFileTask.java
+++ iotdb-core/datanode/src/main/java/org/apache/iotdb/db/storageengine/dataregion/compaction/tool/reader/SingleSequenceFileTask.java
@@ -1,7 +1,12 @@
 package org.apache.iotdb.db.storageengine.dataregion.compaction.tool.reader;
 
+import org.apache.iotdb.db.storageengine.dataregion.compaction.tool.Interval;
+import org.apache.iotdb.db.storageengine.dataregion.compaction.tool.TsFileStatisticReader;
 import org.apache.iotdb.db.storageengine.dataregion.compaction.tool.UnseqSpaceStatistics;
+import org.apache.iotdb.tsfile.file.metadata.ChunkMetadata;
 
+import java.io.IOException;
+import java.util.List;
 import java.util.concurrent.Callable;
 
 public class SingleSequenceFileTask implements Callable<TaskSummary> {
@@ -19,7 +24,49 @@ public class SingleSequenceFileTask implements Callable<TaskSummary> {
   }
 
   private TaskSummary checkSeqFile(UnseqSpaceStatistics unseqSpaceStatistics, String seqFile) {
+    TaskSummary summary = new TaskSummary();
+    try (TsFileStatisticReader reader = new TsFileStatisticReader(seqFile)) {
+      // 统计顺序文件的信息并更新到 overlapStatistic
+      List<TsFileStatisticReader.ChunkGroupStatistics> chunkGroupStatisticsList =
+          reader.getChunkGroupStatistics();
+      for (TsFileStatisticReader.ChunkGroupStatistics chunkGroupStatistics :
+          chunkGroupStatisticsList) {
+        summary.totalChunks = chunkGroupStatistics.getTotalChunkNum();
+        String deviceId = chunkGroupStatistics.getDeviceID();
+        int overlapChunkNum = 0;
 
-    return new TaskSummary();
+        long deviceStartTime = Long.MAX_VALUE, deviceEndTime = Long.MIN_VALUE;
+
+        for (ChunkMetadata chunkMetadata : chunkGroupStatistics.getChunkMetadataList()) {
+          // skip empty chunk
+          if (chunkMetadata.getStartTime() > chunkMetadata.getEndTime()) {
+            continue;
+          }
+          // update device start time and end time
+          deviceStartTime = Math.min(deviceStartTime, chunkMetadata.getStartTime());
+          deviceEndTime = Math.max(deviceEndTime, chunkMetadata.getEndTime());
+          // check chunk overlap
+          Interval interval =
+              new Interval(chunkMetadata.getStartTime(), chunkMetadata.getEndTime());
+          String measurementId = chunkMetadata.getMeasurementUid();
+          if (unseqSpaceStatistics.chunkHasOverlap(deviceId, measurementId, interval)) {
+            summary.overlapChunk++;
+          }
+        }
+        // check device overlap
+        if (deviceStartTime > deviceEndTime) {
+          continue;
+        }
+        Interval deviceInterval = new Interval(deviceStartTime, deviceEndTime);
+        if (!unseqSpaceStatistics.chunkGroupHasOverlap(deviceId, deviceInterval)) {
+          continue;
+        }
+        summary.overlapChunkGroup++;
+      }
+      summary.totalChunkGroups = chunkGroupStatisticsList.size();
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+    return summary;
   }
 }
--- iotdb-core/datanode/src/main/java/org/apache/iotdb/db/storageengine/dataregion/compaction/tool/reader/TaskSummary.java
+++ iotdb-core/datanode/src/main/java/org/apache/iotdb/db/storageengine/dataregion/compaction/tool/reader/TaskSummary.java
@@ -1,40 +1,8 @@
 package org.apache.iotdb.db.storageengine.dataregion.compaction.tool.reader;
 
 public class TaskSummary {
-  private long overlapChunk;
-  private long overlapChunkGroup;
-  private long totalChunks;
-  private long totalChunkGroups;
-
-  public long getOverlapChunk() {
-    return overlapChunk;
-  }
-
-  public void setOverlapChunk(long overlapChunk) {
-    this.overlapChunk = overlapChunk;
-  }
-
-  public long getOverlapChunkGroup() {
-    return overlapChunkGroup;
-  }
-
-  public void setOverlapChunkGroup(long overlapChunkGroup) {
-    this.overlapChunkGroup = overlapChunkGroup;
-  }
-
-  public long getTotalChunks() {
-    return totalChunks;
-  }
-
-  public void setTotalChunks(long totalChunks) {
-    this.totalChunks = totalChunks;
-  }
-
-  public long getTotalChunkGroups() {
-    return totalChunkGroups;
-  }
-
-  public void setTotalChunkGroups(long totalChunkGroups) {
-    this.totalChunkGroups = totalChunkGroups;
-  }
+  public long overlapChunk;
+  public long overlapChunkGroup;
+  public long totalChunks;
+  public long totalChunkGroups;
 }
