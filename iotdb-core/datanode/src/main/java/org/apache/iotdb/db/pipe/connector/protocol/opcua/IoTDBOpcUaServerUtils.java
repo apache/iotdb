3d14a9b6961e7a6d95fe8815f82939e78b56e444
Caideyipi <1336190078@qq.com>
2023-08-28 20:31:02 +0800

    Debug

--- iotdb-core/datanode/src/main/java/org/apache/iotdb/db/pipe/connector/protocol/opcua/IoTDBOpcUaServerUtils.java
+++ iotdb-core/datanode/src/main/java/org/apache/iotdb/db/pipe/connector/protocol/opcua/IoTDBOpcUaServerUtils.java
@@ -56,18 +56,17 @@ import org.eclipse.milo.opcua.stack.server.security.DefaultServerCertificateVali
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.*;
+import java.io.File;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.security.*;
+import java.security.KeyPair;
 import java.security.cert.X509Certificate;
 import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Set;
 
 import static com.google.common.collect.Lists.newArrayList;
-import static org.apache.iotdb.tsfile.file.metadata.enums.TSDataType.INT32;
 import static org.eclipse.milo.opcua.sdk.server.api.config.OpcUaServerConfig.USER_TOKEN_POLICY_ANONYMOUS;
 import static org.eclipse.milo.opcua.sdk.server.api.config.OpcUaServerConfig.USER_TOKEN_POLICY_USERNAME;
 import static org.eclipse.milo.opcua.sdk.server.api.config.OpcUaServerConfig.USER_TOKEN_POLICY_X509;
