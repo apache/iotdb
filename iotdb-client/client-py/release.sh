2e2e541bafc35263b3b5e109e23a2e617f263c2a
HTHou <hhaonan@outlook.com>
2023-08-31 18:01:23 +0800

    update

--- iotdb-client/client-py/release.sh
+++ iotdb-client/client-py/release.sh
@@ -27,7 +27,7 @@ rm -Rf dist
 rm -Rf iotdb_session.egg_info
 
 # (Re-)build generated code
-(cd ..; mvn clean package -pl client-py -am)
+(cd ../..; mvn clean package -pl iotdb-client/client-py -am)
 
 # Run Linting
 black .
