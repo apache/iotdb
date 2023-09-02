d15087dbe1f430433c9727d2b96da9c9f0e2a185
Weihao Li <60659567+Wei-hao-Li@users.noreply.github.com>
2023-09-02 13:10:08 +0800

    Add IT for system permissions

--- iotdb-core/datanode/src/main/java/org/apache/iotdb/db/queryengine/plan/statement/metadata/pipe/ShowPipePluginsStatement.java
+++ iotdb-core/datanode/src/main/java/org/apache/iotdb/db/queryengine/plan/statement/metadata/pipe/ShowPipePluginsStatement.java
@@ -17,7 +17,7 @@
  * under the License.
  */
 
-package org.apache.iotdb.db.queryengine.plan.statement.metadata;
+package org.apache.iotdb.db.queryengine.plan.statement.metadata.pipe;
 
 import org.apache.iotdb.common.rpc.thrift.TSStatus;
 import org.apache.iotdb.commons.auth.entity.PrivilegeType;
@@ -25,43 +25,29 @@ import org.apache.iotdb.commons.path.PartialPath;
 import org.apache.iotdb.db.auth.AuthorityChecker;
 import org.apache.iotdb.db.queryengine.plan.analyze.QueryType;
 import org.apache.iotdb.db.queryengine.plan.statement.IConfigStatement;
-import org.apache.iotdb.db.queryengine.plan.statement.Statement;
 import org.apache.iotdb.db.queryengine.plan.statement.StatementType;
 import org.apache.iotdb.db.queryengine.plan.statement.StatementVisitor;
+import org.apache.iotdb.db.queryengine.plan.statement.metadata.ShowStatement;
 import org.apache.iotdb.rpc.TSStatusCode;
 
 import java.util.Collections;
 import java.util.List;
 
-public class CreatePipePluginStatement extends Statement implements IConfigStatement {
+public class ShowPipePluginsStatement extends ShowStatement implements IConfigStatement {
 
-  private final String pluginName;
-  private final String className;
-  private final String uriString;
-
-  public CreatePipePluginStatement(String pluginName, String className, String uriString) {
+  public ShowPipePluginsStatement() {
     super();
-    statementType = StatementType.CREATE_PIPEPLUGIN;
-    this.pluginName = pluginName;
-    this.className = className;
-    this.uriString = uriString;
+    statementType = StatementType.SHOW_PIPEPLUGINS;
   }
 
-  public String getPluginName() {
-    return pluginName;
-  }
-
-  public String getClassName() {
-    return className;
-  }
-
-  public String getUriString() {
-    return uriString;
+  @Override
+  public <R, C> R accept(StatementVisitor<R, C> visitor, C context) {
+    return visitor.visitShowPipePlugins(this, context);
   }
 
   @Override
   public QueryType getQueryType() {
-    return QueryType.WRITE;
+    return QueryType.READ;
   }
 
   @Override
@@ -77,10 +63,5 @@ public class CreatePipePluginStatement extends Statement implements IConfigState
     return AuthorityChecker.getTSStatus(
         AuthorityChecker.checkSystemPermission(userName, PrivilegeType.USE_PIPE.ordinal()),
         PrivilegeType.USE_PIPE);
-  }
-
-  @Override
-  public <R, C> R accept(StatementVisitor<R, C> visitor, C context) {
-    return visitor.visitCreatePipePlugin(this, context);
   }
 }
