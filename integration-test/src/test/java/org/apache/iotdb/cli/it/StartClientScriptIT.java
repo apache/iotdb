cb25b5c260ad036d3e63bf5375d0492b5bffaf0b
CritasWang <critas@outlook.com>
2023-09-05 15:40:05 +0800

    fix(cli-it): change display nums to fixed 1000

--- integration-test/src/test/java/org/apache/iotdb/cli/it/StartClientScriptIT.java
+++ integration-test/src/test/java/org/apache/iotdb/cli/it/StartClientScriptIT.java
@@ -103,8 +103,6 @@ public class StartClientScriptIT extends AbstractScript {
             ip,
             "-p",
             port,
-            "-maxPRC",
-            "0",
             "-e",
             "\"flush\"",
             "&",
@@ -121,8 +119,6 @@ public class StartClientScriptIT extends AbstractScript {
             "cmd.exe",
             "/c",
             sbinPath + File.separator + "start-cli.bat",
-            "-maxPRC",
-            "-1111111111111111111111111111",
             "&",
             "exit",
             "%^errorlevel%");
@@ -159,23 +155,9 @@ public class StartClientScriptIT extends AbstractScript {
             ip,
             "-p",
             port,
-            "-maxPRC",
-            "0",
             "-e",
             "\"flush\"");
     builder2.environment().put("CLASSPATH", libPath);
     testOutput(builder2, output2, 0);
-
-    final String[] output3 = {
-      "Error: error format of max print row count, it should be an integer number"
-    };
-    ProcessBuilder builder3 =
-        new ProcessBuilder(
-            "sh",
-            sbinPath + File.separator + "start-cli.sh",
-            "-maxPRC",
-            "-1111111111111111111111111111");
-    builder3.environment().put("CLASSPATH", libPath);
-    testOutput(builder3, output3, 1);
   }
 }
