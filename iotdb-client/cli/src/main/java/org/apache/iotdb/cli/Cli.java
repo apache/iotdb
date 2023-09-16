53ba5e812473fb2d43d9160659acb5e584937ea4
Peng Junzhi <201250214@smail.nju.edu.cn>
2023-09-16 13:18:53 +0800

    test gpg

--- iotdb-client/cli/src/main/java/org/apache/iotdb/cli/Cli.java
+++ iotdb-client/cli/src/main/java/org/apache/iotdb/cli/Cli.java
@@ -45,6 +45,7 @@ import static org.apache.iotdb.jdbc.Config.IOTDB_ERROR_PREFIX;
 
 /** args[]: -h 127.0.0.1 -p 6667 -u root -pw root */
 public class Cli extends AbstractCli {
+
   private static CommandLine commandLine;
   private static LineReader lineReader;
 
