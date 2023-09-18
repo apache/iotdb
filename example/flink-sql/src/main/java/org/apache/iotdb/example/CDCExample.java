6f60262d61fde15d9d090aae0e1c720255ee3890
Xuan Ronaldo <xuanronaldo@qq.com>
2023-09-18 22:50:42 +0800

    [IOTDB-6158] add examples and ITs for flink-sql-connector (#11163)

---
+++ example/flink-sql/src/main/java/org/apache/iotdb/example/CDCExample.java
@@ -0,0 +1,54 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.iotdb.example;
+
+import org.apache.flink.table.api.DataTypes;
+import org.apache.flink.table.api.EnvironmentSettings;
+import org.apache.flink.table.api.Schema;
+import org.apache.flink.table.api.TableDescriptor;
+import org.apache.flink.table.api.TableEnvironment;
+
+public class CDCExample {
+  public static void main(String[] args) {
+    // setup environment
+    EnvironmentSettings settings = EnvironmentSettings.newInstance().inStreamingMode().build();
+    TableEnvironment tableEnv = TableEnvironment.create(settings);
+    // setup schema
+    Schema iotdbTableSchema =
+        Schema.newBuilder()
+            .column("Time_", DataTypes.BIGINT())
+            .column("root.sg.d0.s0", DataTypes.FLOAT())
+            .column("root.sg.d1.s0", DataTypes.FLOAT())
+            .column("root.sg.d1.s1", DataTypes.FLOAT())
+            .build();
+
+    // register table
+    TableDescriptor iotdbDescriptor =
+        TableDescriptor.forConnector("IoTDB")
+            .schema(iotdbTableSchema)
+            .option("mode", "CDC")
+            .option("cdc.task.name", "test")
+            .option("cdc.pattern", "root.sg")
+            .build();
+    tableEnv.createTemporaryTable("iotdbTable", iotdbDescriptor);
+
+    // output table
+    tableEnv.from("iotdbTable").execute().print();
+  }
+}
---
+++ integration-test/src/test/java/org/apache/iotdb/flink/it/AbstractTest.java
@@ -0,0 +1,46 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.iotdb.flink.it;
+
+import org.apache.iotdb.it.env.EnvFactory;
+import org.apache.iotdb.rpc.IoTDBConnectionException;
+import org.apache.iotdb.rpc.StatementExecutionException;
+
+import org.apache.flink.table.api.EnvironmentSettings;
+import org.apache.flink.table.api.TableEnvironment;
+
+public class AbstractTest {
+  TableEnvironment tableEnv;
+
+  protected String ip;
+  protected int port;
+
+  public void before() {
+    EnvFactory.getEnv().initClusterEnvironment();
+    ip = EnvFactory.getEnv().getIP();
+    port = Integer.valueOf(EnvFactory.getEnv().getPort());
+
+    EnvironmentSettings settings = EnvironmentSettings.newInstance().inStreamingMode().build();
+    tableEnv = TableEnvironment.create(settings);
+  }
+
+  public void after() throws IoTDBConnectionException, StatementExecutionException {
+    EnvFactory.getEnv().cleanClusterEnvironment();
+  }
+}
