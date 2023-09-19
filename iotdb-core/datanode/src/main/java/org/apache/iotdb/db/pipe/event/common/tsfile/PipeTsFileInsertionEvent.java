38f755e25609d462be72765680e5413ec992dbab
Steve Yurong Su <rong@apache.org>
2023-09-19 19:49:29 +0800

    Update PipeTsFileInsertionEvent.java

--- iotdb-core/datanode/src/main/java/org/apache/iotdb/db/pipe/event/common/tsfile/PipeTsFileInsertionEvent.java
+++ iotdb-core/datanode/src/main/java/org/apache/iotdb/db/pipe/event/common/tsfile/PipeTsFileInsertionEvent.java
@@ -111,7 +111,7 @@ public class PipeTsFileInsertionEvent extends EnrichedEvent implements TsFileIns
   }
 
   public boolean hasTimeFilter() {
-    return !(startTime <= resource.getFileStartTime() && resource.getFileEndTime() <= endTime);
+    return startTime != Long.MIN_VALUE || endTime != Long.MAX_VALUE;
   }
 
   /////////////////////////// EnrichedEvent ///////////////////////////
