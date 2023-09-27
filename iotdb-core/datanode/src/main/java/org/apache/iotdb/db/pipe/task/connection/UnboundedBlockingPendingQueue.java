aa8e0d04b27aebe3e3ea0cb10c642da1fc13c34e
Caideyipi <87789683+Caideyipi@users.noreply.github.com>
2023-09-27 15:18:58 +0800

    [To rel/1.2] Pipe: Added tablet event count and tsfile event count to PipeHeartbeatEvent reports (#11225)(#11224)

--- iotdb-core/datanode/src/main/java/org/apache/iotdb/db/pipe/task/connection/UnboundedBlockingPendingQueue.java
+++ iotdb-core/datanode/src/main/java/org/apache/iotdb/db/pipe/task/connection/UnboundedBlockingPendingQueue.java
@@ -20,80 +20,20 @@
 package org.apache.iotdb.db.pipe.task.connection;
 
 import org.apache.iotdb.pipe.api.event.Event;
-import org.apache.iotdb.pipe.api.event.dml.insertion.TsFileInsertionEvent;
 
 import java.util.concurrent.BlockingDeque;
 import java.util.concurrent.LinkedBlockingDeque;
-import java.util.concurrent.atomic.AtomicInteger;
 
 public class UnboundedBlockingPendingQueue<E extends Event> extends BlockingPendingQueue<E> {
 
   private final BlockingDeque<E> pendingDeque;
 
-  private final AtomicInteger tsfileInsertionEventCount;
-
   public UnboundedBlockingPendingQueue() {
     super(new LinkedBlockingDeque<>());
     pendingDeque = (BlockingDeque<E>) pendingQueue;
-    tsfileInsertionEventCount = new AtomicInteger(0);
-  }
-
-  @Override
-  public boolean waitedOffer(E event) {
-    final boolean offered = super.waitedOffer(event);
-    if (offered && event instanceof TsFileInsertionEvent) {
-      tsfileInsertionEventCount.incrementAndGet();
-    }
-    return offered;
-  }
-
-  @Override
-  public boolean directOffer(E event) {
-    final boolean offered = super.directOffer(event);
-    if (offered && event instanceof TsFileInsertionEvent) {
-      tsfileInsertionEventCount.incrementAndGet();
-    }
-    return offered;
-  }
-
-  @Override
-  public boolean put(E event) {
-    final boolean putSuccessfully = super.put(event);
-    if (putSuccessfully && event instanceof TsFileInsertionEvent) {
-      tsfileInsertionEventCount.incrementAndGet();
-    }
-    return putSuccessfully;
-  }
-
-  @Override
-  public E directPoll() {
-    final E event = super.directPoll();
-    if (event instanceof TsFileInsertionEvent) {
-      tsfileInsertionEventCount.decrementAndGet();
-    }
-    return event;
-  }
-
-  @Override
-  public E waitedPoll() {
-    final E event = super.waitedPoll();
-    if (event instanceof TsFileInsertionEvent) {
-      tsfileInsertionEventCount.decrementAndGet();
-    }
-    return event;
-  }
-
-  @Override
-  public void clear() {
-    super.clear();
-    tsfileInsertionEventCount.set(0);
   }
 
   public E peekLast() {
     return pendingDeque.peekLast();
-  }
-
-  public int getTsfileInsertionEventCount() {
-    return tsfileInsertionEventCount.get();
   }
 }
