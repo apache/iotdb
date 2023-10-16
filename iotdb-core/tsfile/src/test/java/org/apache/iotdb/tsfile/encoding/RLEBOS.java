9504da07dc52e9935189b4c6e93f327ae47a7506
xjz17 <67282793+xjz17@users.noreply.github.com>
2023-10-16 15:17:47 +0800

    Update RLEBOS.java

--- iotdb-core/tsfile/src/test/java/org/apache/iotdb/tsfile/encoding/RLEBOS.java
+++ iotdb-core/tsfile/src/test/java/org/apache/iotdb/tsfile/encoding/RLEBOS.java
@@ -1245,9 +1245,8 @@ public class RLEBOS {
     }
 
     public static void main(@org.jetbrains.annotations.NotNull String[] args) throws IOException {
-        String parent_dir = "/Users/xiaojinzhao/Desktop/encoding-outlier/"; ///Users/xiaojinzhao/Desktop
-//        String parent_dir = "/Users/zihanguo/Downloads/outliier_code/encoding-outlier/";
-        String output_parent_dir = parent_dir + "vldb/compression_ratio/rle_bos";
+        String parent_dir = "iotdb/iotdb-core/tsfile/src/test/resources/"; // your data path
+        String output_parent_dir = parent_dir + "rle_bos";
         String input_parent_dir = parent_dir + "trans_data/";
         ArrayList<String> input_path_list = new ArrayList<>();
         ArrayList<String> output_path_list = new ArrayList<>();
