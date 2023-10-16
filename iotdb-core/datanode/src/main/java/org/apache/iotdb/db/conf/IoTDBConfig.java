c02389f1e96949971c3d86eeb5670db225867022
VGalaxies <dyc1904821183@gmail.com>
2023-10-16 15:51:20 +0800

    fix: missing `pipeReceiverFileDir` update after updating `systemDir` during conf initialization

--- iotdb-core/datanode/src/main/java/org/apache/iotdb/db/conf/IoTDBConfig.java
+++ iotdb-core/datanode/src/main/java/org/apache/iotdb/db/conf/IoTDBConfig.java
@@ -61,6 +61,7 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Objects;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 import java.util.regex.Pattern;
@@ -1078,8 +1079,8 @@ public class IoTDBConfig {
   private double maxMemoryRatioForQueue = 0.6;
 
   /** Pipe related */
-  private String pipeReceiverFileDir =
-      systemDir + File.separator + "pipe" + File.separator + "receiver";
+  /** initialized as null, updated based on the latest `systemDir` during querying */
+  private String pipeReceiverFileDir = null;
 
   /** Resource control */
   private boolean quotaEnable = false;
@@ -3711,7 +3712,9 @@ public class IoTDBConfig {
   }
 
   public String getPipeReceiverFileDir() {
-    return pipeReceiverFileDir;
+    return Objects.isNull(this.pipeReceiverFileDir)
+        ? (systemDir + File.separator + "pipe" + File.separator + "receiver")
+        : this.pipeReceiverFileDir;
   }
 
   public boolean isQuotaEnable() {
