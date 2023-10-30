6534cc86f280e1a809e9d4f02906e9f64a6a7470
JackieTien97 <jackietien97@gmail.com>
2023-10-30 17:00:01 +0800

    Add java doc for SessionDataSet.next() method to let users prefer to use SessionDataSet.DataIterator

--- iotdb-core/datanode/src/assembly/resources/conf/datanode-env.bat
+++ iotdb-core/datanode/src/assembly/resources/conf/datanode-env.bat
@@ -120,6 +120,9 @@ set IOTDB_HEAP_OPTS=-Xmx%ON_HEAP_MEMORY% -Xms%ON_HEAP_MEMORY%
 set IOTDB_HEAP_OPTS=%IOTDB_HEAP_OPTS% -XX:MaxDirectMemorySize=%OFF_HEAP_MEMORY%
 set IOTDB_HEAP_OPTS=%IOTDB_HEAP_OPTS% -Djdk.nio.maxCachedBufferSize=%MAX_CACHED_BUFFER_SIZE%
 
+@REM if you want to dump the heap memory while OOM happening, you can use the following command, remember to replace /tmp/heapdump.hprof with your own file path and the folder where this file is located needs to be created in advance
+@REM IOTDB_JMX_OPTS=%IOTDB_HEAP_OPTS% -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=\tmp\heapdump.hprof
+
 @REM You can put your env variable here
 @REM set JAVA_HOME=%JAVA_HOME%
 
