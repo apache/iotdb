a61b07f9b3d8a1bf8baaff9de0e031358760a317
Steve Yurong Su <rong@apache.org>
2023-10-30 21:54:07 +0800

    pipe-api: PipeExtractor -> PipeSource, PipeConnector -> PipeSink

--- iotdb-api/pipe-api/src/main/java/org/apache/iotdb/pipe/api/PipeSource.java
+++ iotdb-api/pipe-api/src/main/java/org/apache/iotdb/pipe/api/PipeSource.java
@@ -19,41 +19,40 @@
 
 package org.apache.iotdb.pipe.api;
 
-import org.apache.iotdb.pipe.api.customizer.configuration.PipeExtractorRuntimeConfiguration;
+import org.apache.iotdb.pipe.api.customizer.configuration.PipeSourceRuntimeConfiguration;
 import org.apache.iotdb.pipe.api.customizer.parameter.PipeParameterValidator;
 import org.apache.iotdb.pipe.api.customizer.parameter.PipeParameters;
 import org.apache.iotdb.pipe.api.event.Event;
 
 /**
- * PipeExtractor (Deprecated since v1.3.0, renamed to PipeSource)
+ * PipeSource
  *
- * <p>PipeExtractor is responsible for capturing events from sources.
+ * <p>PipeSource is responsible for capturing events from sources.
  *
- * <p>Various data sources can be supported by implementing different PipeExtractor classes.
+ * <p>Various data sources can be supported by implementing different PipeSource classes.
  *
- * <p>The lifecycle of a PipeExtractor is as follows:
+ * <p>The lifecycle of a PipeSource is as follows:
  *
  * <ul>
  *   <li>When a collaboration task is created, the KV pairs of `WITH EXTRACTOR` clause in SQL are
- *       parsed and the validation method {@link PipeExtractor#validate(PipeParameterValidator)}
- *       will be called to validate the parameters.
+ *       parsed and the validation method {@link PipeSource#validate(PipeParameterValidator)} will
+ *       be called to validate the parameters.
  *   <li>Before the collaboration task starts, the method {@link
- *       PipeExtractor#customize(PipeParameters, PipeExtractorRuntimeConfiguration)} will be called
- *       to config the runtime behavior of the PipeExtractor.
- *   <li>Then the method {@link PipeExtractor#start()} will be called to start the PipeExtractor.
- *   <li>While the collaboration task is in progress, the method {@link PipeExtractor#supply()} will
- *       be called to capture events from sources and then the events will be passed to the
+ *       PipeSource#customize(PipeParameters, PipeSourceRuntimeConfiguration)} will be called to
+ *       config the runtime behavior of the PipeSource.
+ *   <li>Then the method {@link PipeSource#start()} will be called to start the PipeSource.
+ *   <li>While the collaboration task is in progress, the method {@link PipeSource#supply()} will be
+ *       called to capture events from sources and then the events will be passed to the
  *       PipeProcessor.
- *   <li>The method {@link PipeExtractor#close()} will be called when the collaboration task is
+ *   <li>The method {@link PipeSource#close()} will be called when the collaboration task is
  *       cancelled (the `DROP PIPE` command is executed).
  * </ul>
  */
-@Deprecated // since v1.3.0, renamed to PipeSource
-public interface PipeExtractor extends PipePlugin {
+public interface PipeSource extends PipeExtractor {
 
   /**
    * This method is mainly used to validate {@link PipeParameters} and it is executed before {@link
-   * PipeExtractor#customize(PipeParameters, PipeExtractorRuntimeConfiguration)} is called.
+   * PipeSource#customize(PipeParameters, PipeSourceRuntimeConfiguration)} is called.
    *
    * @param validator the validator used to validate {@link PipeParameters}
    * @throws Exception if any parameter is not valid
@@ -61,28 +60,28 @@ public interface PipeExtractor extends PipePlugin {
   void validate(PipeParameterValidator validator) throws Exception;
 
   /**
-   * This method is mainly used to customize PipeExtractor. In this method, the user can do the
+   * This method is mainly used to customize PipeSource. In this method, the user can do the
    * following things:
    *
    * <ul>
    *   <li>Use PipeParameters to parse key-value pair attributes entered by the user.
-   *   <li>Set the running configurations in PipeExtractorRuntimeConfiguration.
+   *   <li>Set the running configurations in PipeSourceRuntimeConfiguration.
    * </ul>
    *
-   * <p>This method is called after the method {@link
-   * PipeExtractor#validate(PipeParameterValidator)} is called.
+   * <p>This method is called after the method {@link PipeSource#validate(PipeParameterValidator)}
+   * is called.
    *
    * @param parameters used to parse the input parameters entered by the user
-   * @param configuration used to set the required properties of the running PipeExtractor
+   * @param configuration used to set the required properties of the running PipeSource
    * @throws Exception the user can throw errors if necessary
    */
-  void customize(PipeParameters parameters, PipeExtractorRuntimeConfiguration configuration)
+  void customize(PipeParameters parameters, PipeSourceRuntimeConfiguration configuration)
       throws Exception;
 
   /**
    * Start the extractor. After this method is called, events should be ready to be supplied by
-   * {@link PipeExtractor#supply()}. This method is called after {@link
-   * PipeExtractor#customize(PipeParameters, PipeExtractorRuntimeConfiguration)} is called.
+   * {@link PipeSource#supply()}. This method is called after {@link
+   * PipeSource#customize(PipeParameters, PipeSourceRuntimeConfiguration)} is called.
    *
    * @throws Exception the user can throw errors if necessary
    */
@@ -90,7 +89,7 @@ public interface PipeExtractor extends PipePlugin {
 
   /**
    * Supply single event from the extractor and the caller will send the event to the processor.
-   * This method is called after {@link PipeExtractor#start()} is called.
+   * This method is called after {@link PipeSource#start()} is called.
    *
    * @return the event to be supplied. the event may be null if the extractor has no more events at
    *     the moment, but the extractor is still running for more events.
