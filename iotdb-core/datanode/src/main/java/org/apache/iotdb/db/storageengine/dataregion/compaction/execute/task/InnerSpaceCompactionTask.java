ef878ac7d8748cf6c65928147bf5f1bf3ee582b9
shuwenwei <s13979062948@gmail.com>
2023-11-01 14:35:19 +0800

    add compaction logs

--- iotdb-core/datanode/src/main/java/org/apache/iotdb/db/storageengine/dataregion/compaction/execute/task/InnerSpaceCompactionTask.java
+++ iotdb-core/datanode/src/main/java/org/apache/iotdb/db/storageengine/dataregion/compaction/execute/task/InnerSpaceCompactionTask.java
@@ -171,12 +171,13 @@ public class InnerSpaceCompactionTask extends AbstractCompactionTask {
     recoverMemoryStatus = true;
     LOGGER.info(
         "{}-{} [Compaction] {} InnerSpaceCompaction task starts with {} files, "
-            + "total file size is {} MB.",
+            + "total file size is {} MB, memory cost is {} MB",
         storageGroupName,
         dataRegionId,
         sequence ? "Sequence" : "Unsequence",
         selectedTsFileResourceList.size(),
-        selectedFileSize / 1024 / 1024);
+        selectedFileSize / 1024 / 1024,
+        memoryCost == 0 ? 0 : (double) memoryCost / 1024 / 1024);
     boolean isSuccess = true;
 
     try {
