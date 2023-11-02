0cabde0143bdaa752e2616c32c51d840e4a7bc94
wangchao316 <576749262@qq.com>
2023-11-02 12:01:26 +0800

    [IOTDB-1476] iotdb support IPV6

---
+++ iotdb-client/service-rpc/src/test/java/org/apache/iotdb/rpc/UrlUtilsTest.java
@@ -0,0 +1,45 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.iotdb.rpc;
+
+import org.apache.iotdb.common.rpc.thrift.TEndPoint;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class UrlUtilsTest {
+
+  @Test
+  public void testParseIPV6URL() {
+    String hostAndPoint = "D80:0000:0000:0000:ABAA:0000:00C2:0002:22227";
+    TEndPoint endPoint = UrlUtils.parseTEndPointIpv4AndIpv6Url(hostAndPoint);
+    assertEquals(endPoint.getIp(), "d80:0:0:0:abaa:0:c2:2");
+    assertEquals(endPoint.getPort(), 22227);
+  }
+
+  @Test
+  public void testParseIPV4URL() {
+    String hostAndPoint = "192.0.0.1:22227";
+    TEndPoint endPoint = UrlUtils.parseTEndPointIpv4AndIpv6Url(hostAndPoint);
+    assertEquals(endPoint.getIp(), "192.0.0.1");
+    assertEquals(endPoint.getPort(), 22227);
+  }
+}
