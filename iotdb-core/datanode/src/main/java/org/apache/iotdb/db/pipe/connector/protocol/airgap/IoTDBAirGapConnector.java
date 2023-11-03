05cb6d2af313471fc398e0f2bd8f36f1e01e6c26
Caideyipi <1336190078@qq.com>
2023-11-03 18:02:06 +0800

    Little bug

--- iotdb-core/datanode/src/main/java/org/apache/iotdb/db/pipe/connector/protocol/airgap/IoTDBAirGapConnector.java
+++ iotdb-core/datanode/src/main/java/org/apache/iotdb/db/pipe/connector/protocol/airgap/IoTDBAirGapConnector.java
@@ -100,17 +100,14 @@ public class IoTDBAirGapConnector extends IoTDBConnector {
         empty -> {
           try {
             // Ensure the sink doesn't point to the air gap receiver on DataNode itself
-
-            if (!pipeConfig.getPipeAirGapReceiverEnabled()) {
-              return true;
-            }
-
-            return !NodeUrlUtils.containsLocalAddress(
-                givenNodeUrls.stream()
-                    .filter(
-                        tEndPoint -> tEndPoint.getPort() == pipeConfig.getPipeAirGapReceiverPort())
-                    .map(TEndPoint::getIp)
-                    .collect(Collectors.toList()));
+            return !(pipeConfig.getPipeAirGapReceiverEnabled()
+                && NodeUrlUtils.containsLocalAddress(
+                    givenNodeUrls.stream()
+                        .filter(
+                            tEndPoint ->
+                                tEndPoint.getPort() == pipeConfig.getPipeAirGapReceiverPort())
+                        .map(TEndPoint::getIp)
+                        .collect(Collectors.toList())));
           } catch (UnknownHostException e) {
             LOGGER.warn("Unknown host when checking pipe sink IP.", e);
             return false;
