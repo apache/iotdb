c8feed0dd3888e35f6155ed27421832fae922761
VGalaxies <dyc1904821183@gmail.com>
2023-11-06 19:31:39 +0800

    fix: enable lruCache stats

--- iotdb-core/datanode/src/main/java/org/apache/iotdb/db/storageengine/dataregion/wal/utils/WALInsertNodeCache.java
+++ iotdb-core/datanode/src/main/java/org/apache/iotdb/db/storageengine/dataregion/wal/utils/WALInsertNodeCache.java
@@ -83,6 +83,7 @@ public class WALInsertNodeCache {
             .weigher(
                 (Weigher<WALEntryPosition, Pair<ByteBuffer, InsertNode>>)
                     (position, pair) -> position.getSize())
+            .recordStats()
             .build(new WALInsertNodeCacheLoader());
     PipeWALInsertNodeCacheMetrics.getInstance().register(this, dataRegionId);
   }
@@ -171,7 +172,7 @@ public class WALInsertNodeCache {
   }
 
   public double getCacheHitRate() {
-    return Objects.nonNull(lruCache) ? 1 - lruCache.stats().missRate() : 0;
+    return Objects.nonNull(lruCache) ? lruCache.stats().hitRate() : 0;
   }
 
   /////////////////////////// MemTable ///////////////////////////
