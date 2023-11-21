07b9a5b1913b46fa4282c04cfe191fe68bea0b80
Steve Yurong Su <rong@apache.org>
2023-11-21 16:14:13 +0800

    Update PipeEventCommitManager.java

--- iotdb-core/datanode/src/main/java/org/apache/iotdb/db/pipe/commit/PipeEventCommitManager.java
+++ iotdb-core/datanode/src/main/java/org/apache/iotdb/db/pipe/commit/PipeEventCommitManager.java
@@ -19,44 +19,24 @@
 
 package org.apache.iotdb.db.pipe.commit;
 
-import org.apache.iotdb.commons.pipe.plugin.builtin.BuiltinPipePlugin;
 import org.apache.iotdb.db.pipe.event.EnrichedEvent;
 import org.apache.iotdb.db.pipe.event.common.heartbeat.PipeHeartbeatEvent;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.HashSet;
 import java.util.Map;
-import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class PipeEventCommitManager {
 
   private static final Logger LOGGER = LoggerFactory.getLogger(PipeEventCommitManager.class);
 
-  private static final Set<String> COMMITTABLE_CONNECTORS =
-      Collections.unmodifiableSet(
-          new HashSet<>(
-              Arrays.asList(
-                  BuiltinPipePlugin.IOTDB_THRIFT_CONNECTOR.getPipePluginName(),
-                  BuiltinPipePlugin.IOTDB_THRIFT_SYNC_CONNECTOR.getPipePluginName(),
-                  BuiltinPipePlugin.IOTDB_THRIFT_ASYNC_CONNECTOR.getPipePluginName(),
-                  BuiltinPipePlugin.IOTDB_LEGACY_PIPE_CONNECTOR.getPipePluginName(),
-                  BuiltinPipePlugin.WRITE_BACK_CONNECTOR.getPipePluginName(),
-                  BuiltinPipePlugin.IOTDB_THRIFT_SINK.getPipePluginName(),
-                  BuiltinPipePlugin.IOTDB_THRIFT_SYNC_SINK.getPipePluginName(),
-                  BuiltinPipePlugin.IOTDB_THRIFT_ASYNC_SINK.getPipePluginName(),
-                  BuiltinPipePlugin.IOTDB_LEGACY_PIPE_SINK.getPipePluginName(),
-                  BuiltinPipePlugin.WRITE_BACK_SINK.getPipePluginName())));
-
   // key: pipeName_dataRegionId
   private final Map<String, PipeEventCommitter> eventCommitterMap = new ConcurrentHashMap<>();
 
   public void register(String pipeName, int dataRegionId, String pipePluginName) {
-    if (pipeName == null || !COMMITTABLE_CONNECTORS.contains(pipePluginName)) {
+    if (pipeName == null || pipePluginName == null) {
       return;
     }
 
