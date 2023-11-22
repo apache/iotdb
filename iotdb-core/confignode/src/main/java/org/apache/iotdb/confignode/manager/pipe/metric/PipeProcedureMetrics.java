7920691aa993de2cd1808227e4ed8cbb50eeb2f0
VGalaxies <dyc1904821183@gmail.com>
2023-11-22 16:12:06 +0800

    chore: add comment row

--- iotdb-core/confignode/src/main/java/org/apache/iotdb/confignode/manager/pipe/metric/PipeProcedureMetrics.java
+++ iotdb-core/confignode/src/main/java/org/apache/iotdb/confignode/manager/pipe/metric/PipeProcedureMetrics.java
@@ -1,3 +1,22 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 package org.apache.iotdb.confignode.manager.pipe.metric;
 
 import org.apache.iotdb.commons.service.metric.enums.Metric;
@@ -23,6 +42,8 @@ public class PipeProcedureMetrics implements IMetricSet {
 
   private final Map<String, Timer> timerMap = new HashMap<>();
 
+  //////////////////////////// bindTo & unbindFrom (metric framework) ////////////////////////////
+
   @Override
   public void bindTo(AbstractMetricService metricService) {
     Arrays.stream(PipeTaskOperation.values())
@@ -44,6 +65,8 @@ public class PipeProcedureMetrics implements IMetricSet {
             metricService.remove(
                 MetricType.TIMER, Metric.PIPE_PROCEDURE.toString(), Tag.NAME.toString(), name));
   }
+
+  //////////////////////////// pipe integration ////////////////////////////
 
   public void updateTimer(String name, long durationMillis) {
     Timer timer = timerMap.get(name);
