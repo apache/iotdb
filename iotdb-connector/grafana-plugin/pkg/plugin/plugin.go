d0cce78951ca69fcbaf4ddd31beb238061821f2f
Christofer Dutz <cdutz@apache.org>
2023-11-29 14:45:12 +0100

    fix: Fixed the imports in the go.mod and one minor code-adjustment to make the grafana plugin build again.

--- iotdb-connector/grafana-plugin/pkg/plugin/plugin.go
+++ iotdb-connector/grafana-plugin/pkg/plugin/plugin.go
@@ -57,7 +57,7 @@ func ApacheIoTDBDatasource(ctx context.Context, d backend.DataSourceInstanceSett
 	if err := json.Unmarshal(d.JSONData, &dm); err != nil {
 		return nil, err
 	}
-	ops, err := d.HTTPClientOptions()
+	ops, err := d.HTTPClientOptions(ctx)
 	if err != nil {
 		return nil, fmt.Errorf("http client options: %w", err)
 	}
