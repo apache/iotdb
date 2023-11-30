e9d8405f04a78d1b4e3f884980d564f155e99f8f
Caideyipi <1336190078@qq.com>
2023-11-30 19:34:16 +0800

    Added linkedlistqueue

---
+++ iotdb-core/node-commons/src/main/java/org/apache/iotdb/commons/pipe/schema/LinkedListMessageQueue.java
@@ -0,0 +1,222 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.iotdb.commons.pipe.schema;
+
+import java.util.Set;
+import java.util.concurrent.CopyOnWriteArraySet;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class LinkedListMessageQueue<E> {
+  LinkedListNode<E> pilot = new LinkedListNode<>(null);
+  LinkedListNode<E> first;
+  LinkedListNode<E> last;
+  ReentrantLock lock = new ReentrantLock();
+  Set<ConsumerItr> consumerItrSet = new CopyOnWriteArraySet<>();
+
+  Condition hasNext = lock.newCondition();
+
+  int firstIndex = 0;
+  int lastIndex = 0;
+
+  public LinkedListMessageQueue() {
+    // first == last == null
+  }
+
+  public boolean add(E e) {
+    lock.lock();
+    try {
+      final LinkedListNode<E> l = last;
+      final LinkedListNode<E> newNode = new LinkedListNode<>(e);
+      last = newNode;
+      if (l == null) {
+        first = newNode;
+        pilot.next = first;
+      } else {
+        l.next = newNode;
+      }
+      ++lastIndex;
+      hasNext.signalAll();
+    } finally {
+      lock.unlock();
+    }
+    return true;
+  }
+
+  public void removeUntil(int newFirst) {
+    lock.lock();
+    try {
+      if (newFirst <= firstIndex) {
+        return;
+      }
+      LinkedListNode<E> next;
+      LinkedListNode<E> x;
+      for (x = first; x != null && firstIndex < newFirst; ++firstIndex, x = next) {
+        next = x.next;
+        x.data = null;
+        x.next = null;
+      }
+      first = x;
+      pilot.next = first;
+      hasNext.signalAll();
+    } finally {
+      lock.unlock();
+    }
+  }
+
+  public void clear() {
+    lock.lock();
+    try {
+      removeUntil(lastIndex + 1);
+      first = null;
+      firstIndex = 0;
+      lastIndex = 0;
+      consumerItrSet.clear();
+    } finally {
+      lock.unlock();
+    }
+  }
+
+  public ConsumerItr subscribe(int offset) {
+    ConsumerItr itr = new ConsumerItr(offset);
+    consumerItrSet.add(itr);
+    return itr;
+  }
+
+  public int getFirstIndex() {
+    lock.lock();
+    try {
+      return firstIndex;
+    } finally {
+      lock.unlock();
+    }
+  }
+
+  public ConsumerItr subscribeEarliest() {
+    return subscribe(Integer.MIN_VALUE);
+  }
+
+  public ConsumerItr subscribeLatest() {
+    return subscribe(Integer.MAX_VALUE);
+  }
+
+  public int getSubscriptionNum() {
+    return consumerItrSet.size();
+  }
+
+  private static class LinkedListNode<E> {
+    E data;
+    LinkedListNode<E> next;
+
+    public LinkedListNode(E data) {
+      this.data = data;
+      this.next = null;
+    }
+  }
+
+  // Temporarily, we do not use read lock because read lock in java does not
+  // support condition. Besides, The pure park and un-park method is fairly slow,
+  // thus we use Reentrant lock here.
+  public class ConsumerItr {
+
+    private LinkedListNode<E> next;
+    int offset;
+
+    ConsumerItr(int offset) {
+      lock.lock();
+      try {
+        if (last != null && offset >= lastIndex) {
+          next = last;
+          offset = lastIndex;
+        } else {
+          next = pilot;
+          if (firstIndex > offset) {
+            for (int i = 0; i < offset - firstIndex; ++i) {
+              next();
+            }
+          } else {
+            offset = firstIndex;
+          }
+        }
+        this.offset = offset;
+      } finally {
+        lock.unlock();
+      }
+    }
+
+    public E next() {
+      lock.lock();
+      try {
+        while (!hasNext()) {
+          hasNext.await();
+        }
+
+        next = next.next;
+        ++offset;
+        return next.data;
+      } catch (InterruptedException e) {
+        Thread.currentThread().interrupt();
+      } catch (NullPointerException ignore) {
+        // NullPointerException means the "next" node is null, typically because the
+        // linked queue is cleared. Though we don't except a linked queue to be cleared
+        // when there are subscriptions alive, still we simply return null to notify the subscriber.
+      } finally {
+        lock.unlock();
+      }
+      return null;
+    }
+
+    private boolean hasNext() {
+      return next.next != null;
+    }
+
+    public boolean seek(int newOffset) {
+      lock.lock();
+      try {
+        if (newOffset < firstIndex && newOffset > lastIndex) {
+          return false;
+        }
+        if (newOffset < offset) {
+          next = pilot;
+          for (int i = 0; i < newOffset - firstIndex; ++i) {
+            next();
+          }
+        } else {
+          for (int i = 0; i < newOffset - offset; ++i) {
+            next();
+          }
+        }
+        offset = newOffset;
+        return true;
+      } finally {
+        lock.unlock();
+      }
+    }
+
+    public int position() {
+      lock.lock();
+      try {
+        return offset;
+      } finally {
+        lock.unlock();
+      }
+    }
+  }
+}
---
+++ iotdb-core/node-commons/src/test/java/org/apache/iotdb/commons/pipe/LinkedListMessageQueueTest.java
@@ -0,0 +1,125 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.iotdb.commons.pipe;
+
+import org.apache.iotdb.commons.pipe.schema.LinkedListMessageQueue;
+
+import org.awaitility.Awaitility;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import static org.junit.Assert.assertFalse;
+
+public class LinkedListMessageQueueTest {
+
+  @Test(timeout = 60000)
+  public void testRemoveAndSubscription() {
+    LinkedListMessageQueue<Integer> queue = new LinkedListMessageQueue<>();
+    queue.add(1);
+    queue.add(2);
+    queue.removeUntil(1);
+    LinkedListMessageQueue<Integer>.ConsumerItr it = queue.subscribeEarliest();
+    Assert.assertEquals(2, (int) it.next());
+
+    LinkedListMessageQueue<Integer>.ConsumerItr it2 = queue.subscribeLatest();
+    AtomicInteger value = new AtomicInteger(-1);
+    new Thread(() -> value.set(it2.next())).start();
+    queue.add(3);
+    Awaitility.await()
+        .atMost(10, TimeUnit.SECONDS)
+        .untilAsserted(() -> Assert.assertEquals(3, value.get()));
+
+    Assert.assertTrue(it.seek(1));
+    Assert.assertEquals(2, (int) it.next());
+
+    queue.clear();
+    Assert.assertEquals(0, queue.getFirstIndex());
+  }
+
+  @Test(timeout = 60000)
+  public void testConcurrentReadWrite() {
+    LinkedListMessageQueue<Integer> queue = new LinkedListMessageQueue<>();
+    AtomicBoolean failure = new AtomicBoolean(false);
+    List<Thread> threadList = new ArrayList<>(102);
+
+    Thread thread1 =
+        new Thread(
+            () -> {
+              try {
+                for (int i = 0; i < 1000000; ++i) {
+                  queue.add(1);
+                }
+              } catch (Exception e) {
+                failure.set(true);
+              }
+            });
+    threadList.add(thread1);
+    thread1.start();
+
+    Thread thread2 =
+        new Thread(
+            () -> {
+              try {
+                for (int i = 0; i < 1000000; ++i) {
+                  queue.add(2);
+                }
+              } catch (Exception e) {
+                failure.set(true);
+              }
+            });
+    threadList.add(thread2);
+    thread2.start();
+
+    for (int i = 0; i < 100; ++i) {
+      Thread thread =
+          new Thread(
+              () -> {
+                try {
+                  LinkedListMessageQueue<Integer>.ConsumerItr it = queue.subscribeEarliest();
+                  for (int j = 0; j < 2000000; ++j) {
+                    it.next();
+                  }
+                } catch (Exception e) {
+                  failure.set(true);
+                }
+              });
+      threadList.add(thread);
+      thread.start();
+    }
+
+    Awaitility.await()
+        .until(
+            () -> {
+              for (Thread thread : threadList) {
+                if (thread.isAlive()) {
+                  return false;
+                }
+              }
+              return true;
+            });
+    assertFalse(failure.get());
+  }
+}
