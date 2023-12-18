eced10e978242e8b071756bb6520aa21dc151f31
shuwenwei <s13979062948@gmail.com>
2023-12-18 19:04:52 +0800

    fix metric bug

--- iotdb-core/datanode/src/main/java/org/apache/iotdb/db/storageengine/dataregion/compaction/execute/utils/writer/AbstractInnerCompactionWriter.java
+++ iotdb-core/datanode/src/main/java/org/apache/iotdb/db/storageengine/dataregion/compaction/execute/utils/writer/AbstractInnerCompactionWriter.java
@@ -51,13 +51,15 @@ public abstract class AbstractInnerCompactionWriter extends AbstractCompactionWr
                 / IoTDBDescriptor.getInstance().getConfig().getCompactionThreadCount()
                 * IoTDBDescriptor.getInstance().getConfig().getChunkMetadataSizeProportion());
     boolean enableMemoryControl = IoTDBDescriptor.getInstance().getConfig().isEnableMemControl();
+    this.targetResource = targetFileResource;
     this.fileWriter =
         new CompactionTsFileWriter(
             targetFileResource.getTsFile(),
             enableMemoryControl,
             sizeForFileWriter,
-            CompactionType.INNER_UNSEQ_COMPACTION);
-    this.targetResource = targetFileResource;
+            targetResource.isSeq()
+                ? CompactionType.INNER_SEQ_COMPACTION
+                : CompactionType.INNER_UNSEQ_COMPACTION);
     isEmptyFile = true;
   }
 
