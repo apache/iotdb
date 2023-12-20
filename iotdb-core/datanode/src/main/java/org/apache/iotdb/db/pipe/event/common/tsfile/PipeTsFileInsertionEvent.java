86742dd132fd89e9aac0d3efb4c4964157eaede1
VGalaxies <dyc1904821183@gmail.com>
2023-12-20 22:08:34 +0800

    clean

--- iotdb-core/datanode/src/main/java/org/apache/iotdb/db/pipe/event/common/tsfile/PipeTsFileInsertionEvent.java
+++ iotdb-core/datanode/src/main/java/org/apache/iotdb/db/pipe/event/common/tsfile/PipeTsFileInsertionEvent.java
@@ -112,10 +112,6 @@ public class PipeTsFileInsertionEvent extends EnrichedEvent implements TsFileIns
     return tsFile;
   }
 
-  public TsFileResource getTsFileResource() {
-    return resource;
-  }
-
   public boolean getIsLoaded() {
     return isLoaded;
   }
