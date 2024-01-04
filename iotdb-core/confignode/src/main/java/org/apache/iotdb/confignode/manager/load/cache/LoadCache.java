148a1bf64a8caf8ffbd06cf5eb670f5a7e9d76ff
YongzaoDan <532741407@qq.com>
2024-01-04 11:52:13 +0800

    Update LoadCache.java

--- iotdb-core/confignode/src/main/java/org/apache/iotdb/confignode/manager/load/cache/LoadCache.java
+++ iotdb-core/confignode/src/main/java/org/apache/iotdb/confignode/manager/load/cache/LoadCache.java
@@ -559,6 +559,7 @@ public class LoadCache {
       try {
         TimeUnit.MILLISECONDS.sleep(HEARTBEAT_INTERVAL);
       } catch (InterruptedException e) {
+        Thread.currentThread().interrupt();
         LOGGER.warn("Interrupt when wait for leader election", e);
         return;
       }
