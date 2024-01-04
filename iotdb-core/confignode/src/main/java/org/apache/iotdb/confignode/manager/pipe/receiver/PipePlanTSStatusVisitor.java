33beebba704d56189edaa773aeee5d503af02c1e
Caideyipi <87789683+Caideyipi@users.noreply.github.com>
2024-01-04 20:42:57 +0800

    supplement

--- iotdb-core/confignode/src/main/java/org/apache/iotdb/confignode/manager/pipe/receiver/PipePlanTSStatusVisitor.java
+++ iotdb-core/confignode/src/main/java/org/apache/iotdb/confignode/manager/pipe/receiver/PipePlanTSStatusVisitor.java
@@ -22,11 +22,29 @@ package org.apache.iotdb.confignode.manager.pipe.receiver;
 import org.apache.iotdb.common.rpc.thrift.TSStatus;
 import org.apache.iotdb.confignode.consensus.request.ConfigPhysicalPlan;
 import org.apache.iotdb.confignode.consensus.request.PhysicalPlanVisitor;
+import org.apache.iotdb.confignode.consensus.request.write.database.DatabaseSchemaPlan;
+import org.apache.iotdb.rpc.TSStatusCode;
 
 public class PipePlanTSStatusVisitor extends PhysicalPlanVisitor<TSStatus, TSStatus> {
 
   @Override
   public TSStatus visitPlan(ConfigPhysicalPlan plan, TSStatus context) {
     return context;
+  }
+
+  @Override
+  public TSStatus visitCreateDatabase(DatabaseSchemaPlan plan, TSStatus context) {
+    if (context.getCode() == TSStatusCode.DATABASE_ALREADY_EXISTS.getStatusCode()) {
+      if (context.getMessage().contains("has already been created as database")) {
+        // The same database or lower level database has been created
+        return new TSStatus(
+                TSStatusCode.PIPE_RECEIVER_IDEMPOTENT_CONFLICT_EXCEPTION.getStatusCode())
+            .setMessage(context.getMessage());
+      }
+      // Lower level database has been created
+      return new TSStatus(TSStatusCode.PIPE_RECEIVER_USER_CONFLICT_EXCEPTION.getStatusCode())
+          .setMessage(context.getMessage());
+    }
+    return super.visitCreateDatabase(plan, context);
   }
 }
--- iotdb-core/datanode/src/main/java/org/apache/iotdb/db/conf/IoTDBConfig.java
+++ iotdb-core/datanode/src/main/java/org/apache/iotdb/db/conf/IoTDBConfig.java
@@ -630,10 +630,10 @@ public class IoTDBConfig {
   /** Switch of creating schema automatically */
   private boolean enableAutoCreateSchema = true;
 
-  /** register time series as which type when receiving boolean string "true" or "false" */
+  /** Register time series as which type when receiving boolean string "true" or "false" */
   private TSDataType booleanStringInferType = TSDataType.BOOLEAN;
 
-  /** register time series as which type when receiving an integer string "67" */
+  /** Register time series as which type when receiving an integer string "67" */
   private TSDataType integerStringInferType = TSDataType.FLOAT;
 
   /**
