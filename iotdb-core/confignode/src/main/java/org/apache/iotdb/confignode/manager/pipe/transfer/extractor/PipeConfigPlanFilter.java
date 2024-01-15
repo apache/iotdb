2604215d45dcc433870a93c8ea766f56c2f97ca3
Caideyipi <87789683+Caideyipi@users.noreply.github.com>
2024-01-16 00:59:56 +0800

    refactor

--- iotdb-core/confignode/src/main/java/org/apache/iotdb/confignode/manager/pipe/transfer/extractor/PipeConfigPlanFilter.java
+++ iotdb-core/confignode/src/main/java/org/apache/iotdb/confignode/manager/pipe/transfer/extractor/PipeConfigPlanFilter.java
@@ -21,8 +21,10 @@ package org.apache.iotdb.confignode.manager.pipe.transfer.extractor;
 
 import org.apache.iotdb.commons.exception.IllegalPathException;
 import org.apache.iotdb.commons.path.PartialPath;
+import org.apache.iotdb.commons.schema.SchemaConstant;
 import org.apache.iotdb.confignode.consensus.request.ConfigPhysicalPlan;
 import org.apache.iotdb.confignode.consensus.request.ConfigPhysicalPlanType;
+import org.apache.iotdb.confignode.consensus.request.write.database.DatabaseSchemaPlan;
 import org.apache.iotdb.confignode.consensus.request.write.template.CommitSetSchemaTemplatePlan;
 import org.apache.iotdb.pipe.api.customizer.parameter.PipeParameters;
 
@@ -126,8 +128,17 @@ public class PipeConfigPlanFilter {
 
   static boolean shouldBeListenedByQueue(ConfigPhysicalPlan plan) {
     ConfigPhysicalPlanType type = plan.getType();
+    // Do not transfer roll back set template plan
     if (type.equals(ConfigPhysicalPlanType.CommitSetSchemaTemplate)
         && ((CommitSetSchemaTemplatePlan) plan).isRollback()) {
+      return false;
+    }
+    // Do not transfer system database plan
+    if (type.equals(ConfigPhysicalPlanType.CreateDatabase)
+        && ((DatabaseSchemaPlan) plan)
+            .getSchema()
+            .getName()
+            .equals(SchemaConstant.SYSTEM_DATABASE)) {
       return false;
     }
     return type.equals(ConfigPhysicalPlanType.PipeEnriched)
