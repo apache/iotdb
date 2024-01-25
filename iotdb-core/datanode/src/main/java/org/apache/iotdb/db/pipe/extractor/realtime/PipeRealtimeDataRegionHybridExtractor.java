71b519d6346b1597a9d8b06e4b80c58b6b05c8f0
Caideyipi <87789683+Caideyipi@users.noreply.github.com>
2024-01-25 16:08:43 +0800

    Update PipeRealtimeDataRegionHybridExtractor.java

--- iotdb-core/datanode/src/main/java/org/apache/iotdb/db/pipe/extractor/realtime/PipeRealtimeDataRegionHybridExtractor.java
+++ iotdb-core/datanode/src/main/java/org/apache/iotdb/db/pipe/extractor/realtime/PipeRealtimeDataRegionHybridExtractor.java
@@ -145,6 +145,9 @@ public class PipeRealtimeDataRegionHybridExtractor extends PipeRealtimeDataRegio
                     // extractor
                     return TsFileEpoch.State.USING_BOTH;
                   } else {
+                    // If the file start time is larger than or equal to epoch's minTime,
+                    // it implies that the file does not contain information out of extracted
+                    // tablets, simply ignore
                     return TsFileEpoch.State.USING_TABLET;
                   }
                 case USING_BOTH:
