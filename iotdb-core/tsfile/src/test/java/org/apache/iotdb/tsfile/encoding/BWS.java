79a9c63ab34d72ecfbcd9ab8ca2db84ee8c813a8
gzh0806 <gzh23@mails.tsinghua.edu.cn>
2024-01-27 14:35:41 +0800

    update

--- iotdb-core/tsfile/src/test/java/org/apache/iotdb/tsfile/encoding/BWS.java
+++ iotdb-core/tsfile/src/test/java/org/apache/iotdb/tsfile/encoding/BWS.java
@@ -966,8 +966,8 @@ public class BWS {
 
     @Test
     public void BWSTest() throws IOException {
-        String parent_dir = "/Users/xiaojinzhao/Desktop/encoding-outlier/"; // your data path
-//        String parent_dir = "/Users/zihanguo/Downloads/R/outlier/outliier_code/encoding-outlier/";
+//        String parent_dir = "/Users/xiaojinzhao/Desktop/encoding-outlier/"; // your data path
+        String parent_dir = "/Users/zihanguo/Downloads/R/outlier/outliier_code/encoding-outlier/";
         String output_parent_dir = parent_dir + "vldb/compression_ratio/bws";
         String input_parent_dir = parent_dir + "trans_data/";
         ArrayList<String> input_path_list = new ArrayList<>();
@@ -1016,7 +1016,7 @@ public class BWS {
         output_path_list.add(output_parent_dir + "/EPM-Education_ratio.csv");//11
         dataset_block_size.add(1024);
 
-        for (int file_i = 0; file_i < 1; file_i++) {
+        for (int file_i = 0; file_i < input_path_list.size(); file_i++) {
 
 //        for (int file_i = 0; file_i < input_path_list.size(); file_i++) {
 
