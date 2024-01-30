3fb0b602a08fae2d568bbfcb7ff17b0f113ab59a
xjz17 <67282793+xjz17@users.noreply.github.com>
2024-01-31 02:18:45 +0800

    Update FinalPruneBOSTest.java

--- iotdb-core/tsfile/src/test/java/org/apache/iotdb/tsfile/encoding/FinalPruneBOSTest.java
+++ iotdb-core/tsfile/src/test/java/org/apache/iotdb/tsfile/encoding/FinalPruneBOSTest.java
@@ -981,6 +981,7 @@ public class FinalPruneBOSTest {
                 k2_end = max_delta_value - gap_gamma;
                 cur_k2 += gamma_count_list[gamma]; // += x_max - 2^{gamma-1}   // count[x_max - 2^{gamma-1}, x_max]
                 // ----------------
+
                 cur_group_gamma = groupU[gamma]; // (x_max - 2^{gamma}, x_max - 2^{gamma-1})
                 int gamma_value_count = cur_group_gamma.count;
                 if (gamma_value_count == 0) {
@@ -1039,11 +1040,11 @@ public class FinalPruneBOSTest {
 //                        countall++;
 
 
-//                        for(int beta = min_beta;beta <= max_beta;beta++){
-//                            int pow_2_beta = (int) pow(2,beta);
-                        if(min_beta >= alpha+max_lambda && max_beta <= gamma+1 && min_pow_2_beta+pow_2_alpha+gap_gamma >= max_delta_value){
+                    for(int beta = min_beta;beta <= max_beta;beta++) {
+                        int pow_2_beta = (int) pow(2, beta);
+                        if (beta >= alpha + max_lambda && beta <= gamma + 1 && pow_2_beta + pow_2_alpha + gap_gamma >= max_delta_value) {
                             // prop 5.4
-                            int cur_xl = k1_start + gap_alpha-1; // x_min+2^{alpha}-1
+                            int cur_xl = k1_start + gap_alpha - 1; // x_min+2^{alpha}-1
                             int cur_xu = k2_end; //x_max - 2^{gamma-1}
                             int prop_5_4_k1 = cur_k1 + groupL[alpha].count; //count[x_min, x_min+2^{alpha}-1]
                             int prop_5_4_k2 = cur_k2; // count[x_max - 2^{gamma-1},x_max}
@@ -1060,13 +1061,12 @@ public class FinalPruneBOSTest {
                             }
                             //continue;
 //                                count5_4++;
-                        }
-                        else if (max_beta <= alpha+1 && min_beta >= gamma+max_lambda && min_pow_2_beta+pow_2_alpha/2+pow_2_gamma >= max_delta_value) {
+                        } else if (beta <= alpha + 1 && beta >= gamma + max_lambda && pow_2_beta + pow_2_alpha / 2 + pow_2_gamma >= max_delta_value) {
                             // prop 5.5
                             int cur_xl = k1_start; // x_min+2^{alpha-1}
                             int cur_xu = k2_end - gap_gamma + 1; //x_max - 2^{gamma}+1
-                            int prop_5_5_k1 = cur_k1 ; //count[x_min, x_min+2^{alpha-1}]
-                            int prop_5_5_k2 = cur_k2+ groupU[gamma].count; // count[x_max - 2^{gamma}+1,x_max}
+                            int prop_5_5_k1 = cur_k1; //count[x_min, x_min+2^{alpha-1}]
+                            int prop_5_5_k2 = cur_k2 + groupU[gamma].count; // count[x_max - 2^{gamma}+1,x_max}
 
                             cur_bits = 0;
                             cur_bits += Math.min((prop_5_5_k1 + prop_5_5_k2) * lambda_0, block_size + prop_5_5_k1 + prop_5_5_k2);// (lambda * (prop_5_5_k1 + prop_5_5_k2) + zeta);
@@ -1080,12 +1080,11 @@ public class FinalPruneBOSTest {
                             }
 //                                count5_5++;
                             //continue;
-                        }
-                        else if (max_beta <= alpha+1 && max_beta <= gamma+1 && min_pow_2_beta+ pow_2_alpha/2+gap_gamma > max_delta_value) {
+                        } else if (beta <= alpha + 1 && beta <= gamma + 1 && pow_2_beta + pow_2_alpha / 2 + gap_gamma > max_delta_value) {
                             // prop 5.6
                             int cur_xl = k1_start; // x_min+2^{alpha-1}
                             int cur_xu = k2_end; //x_max - 2^{gamma-1}
-                            int prop_5_6_k1 = cur_k1 ; //count[x_min, x_min+2^{alpha-1}]
+                            int prop_5_6_k1 = cur_k1; //count[x_min, x_min+2^{alpha-1}]
                             int prop_5_6_k2 = cur_k2; // count[x_max - 2^{gamma-1},x_max}
 
                             cur_bits = 0;
@@ -1100,10 +1099,9 @@ public class FinalPruneBOSTest {
                             }
 //                                count5_6++;
                             //continue;
-                        }
-                        else{
+                        } else {
                             int cur_k1_start_of_lambda;
-                            for (int unique_j=0;unique_j<alpha_unique_number;unique_j++) {
+                            for (int unique_j = 0; unique_j < alpha_unique_number; unique_j++) {
                                 cur_k1_start_of_lambda = cur_k1 + cur_group_alpha.getCount(alpha_sorted[unique_j]);//alpha_unique_count_array[unique_j];// gamma_box_count_list[gamma];
                                 int x_l_i = cur_group_alpha.getUniqueValue(alpha_sorted[unique_j]);//alpha_unique_number_array[unique_j];
 
@@ -1111,21 +1109,24 @@ public class FinalPruneBOSTest {
 //                                    if (cur_count_alpha == 0)
 //                                        continue;
 //                                    cur_k1_start_of_lambda += cur_count_alpha;// gamma_box_count_list[gamma];
-                                int cur_cur_k2 ; // count[x_max - 2^{gamma-1}, x_max]
+                                int cur_cur_k2; // count[x_max - 2^{gamma-1}, x_max]
 
-                                int x_u_i_start = 0;// Math.max(0, k2_end - ( pow_2_beta + k1_start + x_l_i + 2));
-//                                    x_u_i_end = Math.min(k2_end -( pow_2_beta/2 + k1_start + x_l_i + 2),gap_gamma-1);
+                                int x_u_i_start =  Math.max(0, k2_end - ( pow_2_beta + k1_start + x_l_i + 2));
+                                    x_u_i_end = Math.min(k2_end -( pow_2_beta/2 + k1_start + x_l_i + 2),gap_gamma-1);
 
 
-                                for(int unique_i=0;unique_i<gamma_unique_number;unique_i++){
+                                for (int unique_i = 0; unique_i < gamma_unique_number; unique_i++) {
                                     int x_u_i = cur_group_gamma.getUniqueValue(gamma_sorted[unique_i]);//<=x_u_i_end
-//                                        if(x_u_i > x_u_i_end){
-//                                            break;
-//                                        }
+                                    if(x_u_i < x_u_i_start){
+                                        continue;
+                                    }
+                                    if(x_u_i > x_u_i_end){
+                                        break;
+                                    }
                                     cur_cur_k2 = cur_k2 + cur_group_gamma.getCount(gamma_sorted[unique_i]);
 
                                     cur_bits = 0;
-                                    cur_bits +=Math.min((cur_k1_start_of_lambda + cur_cur_k2) * lambda_0, block_size + cur_k1_start_of_lambda + cur_cur_k2);
+                                    cur_bits += Math.min((cur_k1_start_of_lambda + cur_cur_k2) * lambda_0, block_size + cur_k1_start_of_lambda + cur_cur_k2);
                                     // (lambda * (cur_k1_start_of_lambda + cur_cur_k2) + zeta); // Math.min((cur_k1 + cur_cur_k2) * getBitWith(block_size - 1), block_size + cur_k1 + cur_k2);
                                     cur_bits += cur_k1_start_of_lambda * alpha;
                                     if (cur_k1_start_of_lambda + cur_cur_k2 != block_size)
@@ -1142,8 +1143,7 @@ public class FinalPruneBOSTest {
                             }
                         }
 
-
-
+                    }
 
                 }
 
@@ -1825,7 +1825,7 @@ public class FinalPruneBOSTest {
         output_path_list.add(output_parent_dir + "/EPM-Education_ratio.csv");//11
         dataset_block_size.add(1024);
 
-        int repeatTime2 = 1;
+        int repeatTime2 = 100;
 //        for (int file_i = 0; file_i < 1; file_i++) {
 
         for (int file_i = 0; file_i < input_path_list.size(); file_i++) {
