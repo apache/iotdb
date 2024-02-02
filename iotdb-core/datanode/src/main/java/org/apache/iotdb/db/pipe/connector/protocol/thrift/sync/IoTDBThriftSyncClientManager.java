f9a1d717e36be217bd5849d09be16bb73d4fae53
Steve Yurong Su <rong@apache.org>
2024-02-02 12:26:05 +0800

    Update IoTDBThriftSyncClientManager.java

--- iotdb-core/datanode/src/main/java/org/apache/iotdb/db/pipe/connector/protocol/thrift/sync/IoTDBThriftSyncClientManager.java
+++ iotdb-core/datanode/src/main/java/org/apache/iotdb/db/pipe/connector/protocol/thrift/sync/IoTDBThriftSyncClientManager.java
@@ -30,13 +30,10 @@ import org.apache.iotdb.db.pipe.connector.payload.evolvable.request.PipeTransfer
 import org.apache.iotdb.db.pipe.connector.payload.evolvable.request.PipeTransferHandshakeV2Req;
 import org.apache.iotdb.db.pipe.connector.protocol.thrift.IoTDBThriftClientManager;
 import org.apache.iotdb.pipe.api.exception.PipeConnectionException;
-import org.apache.iotdb.pipe.api.exception.PipeException;
 import org.apache.iotdb.rpc.TSStatusCode;
 import org.apache.iotdb.service.rpc.thrift.TPipeTransferResp;
 import org.apache.iotdb.tsfile.utils.Pair;
 
-import org.apache.thrift.TException;
-import org.apache.thrift.transport.TTransportException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -135,7 +132,7 @@ public class IoTDBThriftSyncClientManager extends IoTDBThriftClientManager imple
               useSSL,
               trustStorePath,
               trustStorePwd));
-    } catch (TTransportException e) {
+    } catch (Exception e) {
       throw new PipeConnectionException(
           String.format(
               PipeConnectionException.CONNECTION_ERROR_FORMATTER,
@@ -148,7 +145,7 @@ public class IoTDBThriftSyncClientManager extends IoTDBThriftClientManager imple
   public void sendHandshakeReq(
       Pair<IoTDBThriftSyncConnectorClient, Boolean> clientAndStatus, TEndPoint endPoint) {
     try {
-      HashMap<String, String> params = new HashMap<>();
+      final HashMap<String, String> params = new HashMap<>();
       params.put(
           PipeTransferHandshakeConstant.HANDSHAKE_KEY_TIME_PRECISION,
           CommonDescriptor.getInstance().getConfig().getTimestampPrecision());
@@ -156,31 +153,28 @@ public class IoTDBThriftSyncClientManager extends IoTDBThriftClientManager imple
           PipeTransferHandshakeConstant.HANDSHAKE_KEY_CLUSTER_ID,
           PipeAgent.runtime().getClusterIdIfPossible());
 
+      // Try to handshake by PipeTransferHandshakeV2Req.
       TPipeTransferResp resp =
           clientAndStatus
               .getLeft()
               .pipeTransfer(PipeTransferHandshakeV2Req.toTPipeTransferReq(params));
-      if (resp.getStatus().getCode() != TSStatusCode.SUCCESS_STATUS.getStatusCode()) {
-        // Retry to handshake by PipeTransferHandshakeV1Req.
-        if (resp.getStatus().getCode() == TSStatusCode.PIPE_TYPE_ERROR.getStatusCode()) {
-          LOGGER.info(
-              "Handshake error by PipeTransferHandshakeV2Req. Retry to handshake by PipeTransferHandshakeV1Req.");
-          resp =
-              clientAndStatus
-                  .getLeft()
-                  .pipeTransfer(
-                      PipeTransferHandshakeV1Req.toTPipeTransferReq(
-                          CommonDescriptor.getInstance().getConfig().getTimestampPrecision()));
-        }
-
-        // Throw exception if the statusCode is PIPE_REJECT_ERROR
-        if (resp.getStatus().getCode() == TSStatusCode.PIPE_HANDSHAKE_ERROR.getStatusCode()) {
-          throw new PipeException(
-              String.format(
-                  "Receiver rejects the handshake request, because %s.",
-                  resp.getStatus().getMessage()));
-        }
+      // Receiver may be an old version, so we need to retry to handshake by
+      // PipeTransferHandshakeV1Req.
+      if (resp.getStatus().getCode() == TSStatusCode.PIPE_TYPE_ERROR.getStatusCode()) {
+        LOGGER.info(
+            "Handshake error with target server ip: {}, port: {}, because: {}. "
+                + "Retry to handshake by PipeTransferHandshakeV1Req.",
+            endPoint.getIp(),
+            endPoint.getPort(),
+            resp.getStatus());
+        resp =
+            clientAndStatus
+                .getLeft()
+                .pipeTransfer(
+                    PipeTransferHandshakeV1Req.toTPipeTransferReq(
+                        CommonDescriptor.getInstance().getConfig().getTimestampPrecision()));
       }
+
       if (resp.getStatus().getCode() != TSStatusCode.SUCCESS_STATUS.getStatusCode()) {
         LOGGER.warn(
             "Handshake error with target server ip: {}, port: {}, because: {}.",
@@ -197,15 +191,13 @@ public class IoTDBThriftSyncClientManager extends IoTDBThriftClientManager imple
             endPoint.getIp(),
             endPoint.getPort());
       }
-    } catch (TException e) {
+    } catch (Exception e) {
       LOGGER.warn(
           "Handshake error with target server ip: {}, port: {}, because: {}.",
           endPoint.getIp(),
           endPoint.getPort(),
           e.getMessage(),
           e);
-    } catch (IOException e) {
-      throw new RuntimeException(e);
     }
   }
 
