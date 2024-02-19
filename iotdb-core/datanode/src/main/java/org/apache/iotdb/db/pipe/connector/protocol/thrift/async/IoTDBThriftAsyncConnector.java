2520e9aa3b1741bb0daf7f34f584dea8b6bd32c2
Steve Yurong Su <rong@apache.org>
2024-02-19 15:12:44 +0800

    Update IoTDBThriftAsyncConnector.java

--- iotdb-core/datanode/src/main/java/org/apache/iotdb/db/pipe/connector/protocol/thrift/async/IoTDBThriftAsyncConnector.java
+++ iotdb-core/datanode/src/main/java/org/apache/iotdb/db/pipe/connector/protocol/thrift/async/IoTDBThriftAsyncConnector.java
@@ -330,7 +330,7 @@ public class IoTDBThriftAsyncConnector extends IoTDBConnector {
             peekedEvent,
             polledEvent);
       }
-      if (polledEvent != null) {
+      if (polledEvent != null && LOGGER.isDebugEnabled()) {
         LOGGER.debug("Polled event {} from retry queue.", polledEvent);
       }
     }
@@ -354,8 +354,9 @@ public class IoTDBThriftAsyncConnector extends IoTDBConnector {
    */
   public synchronized void addFailureEventToRetryQueue(Event event) {
     retryEventQueue.offer(event);
-
-    LOGGER.debug("Added event {} to retry queue.", event);
+    if (LOGGER.isDebugEnabled()) {
+      LOGGER.debug("Added event {} to retry queue.", event);
+    }
   }
 
   //////////////////////////// Operations for close ////////////////////////////
