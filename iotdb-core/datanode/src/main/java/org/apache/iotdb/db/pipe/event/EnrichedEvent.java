76702b0e2fadff7f919657efdd2c933d53fd6890
Caideyipi <87789683+Caideyipi@users.noreply.github.com>
2024-02-20 16:42:28 +0800

    Pipe: Pattern parsing pruning: When pattern is at the level below database, the parsing logic can be skipped if tsfiles / tablets completely match with the pattern (#12049)
    Co-authored-by: Steve Yurong Su <rong@apache.org>

--- iotdb-core/datanode/src/main/java/org/apache/iotdb/db/pipe/event/EnrichedEvent.java
+++ iotdb-core/datanode/src/main/java/org/apache/iotdb/db/pipe/event/EnrichedEvent.java
@@ -208,8 +208,12 @@ public abstract class EnrichedEvent implements Event {
     isTimeParsed = true;
   }
 
-  public boolean shouldParsePatternOrTime() {
-    return !isPatternParsed || !isTimeParsed;
+  public boolean shouldParseTimeOrPattern() {
+    return shouldParseTime() || shouldParsePattern();
+  }
+
+  public boolean shouldParsePattern() {
+    return !isPatternParsed;
   }
 
   public boolean shouldParseTime() {
