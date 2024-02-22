1f36e9dde2e2986b91037353f2875144c338225c
Caideyipi <87789683+Caideyipi@users.noreply.github.com>
2024-02-22 20:12:17 +0800

    Revert "bug fix"
    This reverts commit 942e52e7d0081acda1f08ec2179a8dce155fea97.

--- iotdb-core/datanode/src/main/java/org/apache/iotdb/db/pipe/extractor/IoTDBDataRegionExtractor.java
+++ iotdb-core/datanode/src/main/java/org/apache/iotdb/db/pipe/extractor/IoTDBDataRegionExtractor.java
@@ -198,8 +198,13 @@ public class IoTDBDataRegionExtractor implements PipeExtractor {
             String.join(".", Arrays.copyOfRange(pathNodes, 0, pathNodes.length - 1)));
         String lastNode = pathNodes[pathNodes.length - 1];
 
-        if (!lastNode.matches("^(`([^`]|`{2})*+`?)?$")) {
-          Double.parseDouble(lastNode);
+        // Check whether the last node is legal.
+        if (!"".equals(lastNode)) {
+          if (lastNode.startsWith("`") && lastNode.substring(1).replace("``", "").contains("`")) {
+            throw new IllegalArgumentException();
+          } else {
+            Double.parseDouble(lastNode);
+          }
         }
       } catch (Exception ignored) {
         throw new IllegalArgumentException(
--- iotdb-core/datanode/src/test/java/org/apache/iotdb/db/pipe/extractor/IoTDBDataRegionExtractorTest.java
+++ iotdb-core/datanode/src/test/java/org/apache/iotdb/db/pipe/extractor/IoTDBDataRegionExtractorTest.java
@@ -78,16 +78,10 @@ public class IoTDBDataRegionExtractorTest {
         testIoTDBDataRegionExtractorWithPattern("root.`1`2`").getClass(),
         IllegalArgumentException.class);
     Assert.assertEquals(
-        testIoTDBDataRegionExtractorWithPattern("root.``11").getClass(),
-        IllegalArgumentException.class);
-    Assert.assertEquals(
         testIoTDBDataRegionExtractorWithPattern("root.1``2").getClass(),
         IllegalArgumentException.class);
     Assert.assertEquals(
         testIoTDBDataRegionExtractorWithPattern("root.a.*").getClass(),
-        IllegalArgumentException.class);
-    Assert.assertEquals(
-        testIoTDBDataRegionExtractorWithPattern("root.a*.a").getClass(),
         IllegalArgumentException.class);
     Assert.assertNull(testIoTDBDataRegionExtractorWithPattern("root.ab."));
     Assert.assertNull(testIoTDBDataRegionExtractorWithPattern("root.a#b"));
@@ -100,7 +94,6 @@ public class IoTDBDataRegionExtractorTest {
     Assert.assertNull(testIoTDBDataRegionExtractorWithPattern("root.`a-b`"));
     Assert.assertNull(testIoTDBDataRegionExtractorWithPattern("root.1"));
     Assert.assertNull(testIoTDBDataRegionExtractorWithPattern("root.`1``2```"));
-    Assert.assertNull(testIoTDBDataRegionExtractorWithPattern("root.`1->"));
   }
 
   public Exception testIoTDBDataRegionExtractorWithPattern(String pattern) {
