aa547fb38895d5cc0d95212d5adef76bcd021909
Xuan Ronaldo <xuanronaldo@qq.com>
2024-03-14 17:12:22 +0800

    Pipe: implemented sdt-sampling-processor & refactored down-sampling-processor (#12114)
    Co-authored-by: Steve Yurong Su <rong@apache.org>

--- iotdb-core/datanode/src/main/java/org/apache/iotdb/db/pipe/processor/downsampling/DownSamplingProcessor.java
+++ iotdb-core/datanode/src/main/java/org/apache/iotdb/db/pipe/processor/downsampling/DownSamplingProcessor.java
@@ -24,7 +24,6 @@ import org.apache.iotdb.commons.pipe.config.plugin.env.PipeTaskProcessorRuntimeE
 import org.apache.iotdb.db.pipe.event.common.tablet.PipeInsertNodeTabletInsertionEvent;
 import org.apache.iotdb.db.pipe.event.common.tablet.PipeRawTabletInsertionEvent;
 import org.apache.iotdb.db.storageengine.StorageEngine;
-import org.apache.iotdb.db.utils.TimestampPrecisionUtils;
 import org.apache.iotdb.pipe.api.PipeProcessor;
 import org.apache.iotdb.pipe.api.access.Row;
 import org.apache.iotdb.pipe.api.collector.EventCollector;
@@ -37,72 +36,62 @@ import org.apache.iotdb.pipe.api.event.dml.insertion.TabletInsertionEvent;
 import org.apache.iotdb.pipe.api.event.dml.insertion.TsFileInsertionEvent;
 import org.apache.iotdb.tsfile.common.constant.TsFileConstant;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicReference;
 
-import static org.apache.iotdb.commons.pipe.config.constant.PipeProcessorConstant.PROCESSOR_DOWN_SAMPLING_INTERVAL_SECONDS_DEFAULT_VALUE;
-import static org.apache.iotdb.commons.pipe.config.constant.PipeProcessorConstant.PROCESSOR_DOWN_SAMPLING_INTERVAL_SECONDS_KEY;
 import static org.apache.iotdb.commons.pipe.config.constant.PipeProcessorConstant.PROCESSOR_DOWN_SAMPLING_MEMORY_LIMIT_IN_BYTES_DEFAULT_VALUE;
 import static org.apache.iotdb.commons.pipe.config.constant.PipeProcessorConstant.PROCESSOR_DOWN_SAMPLING_MEMORY_LIMIT_IN_BYTES_KEY;
 import static org.apache.iotdb.commons.pipe.config.constant.PipeProcessorConstant.PROCESSOR_DOWN_SAMPLING_SPLIT_FILE_DEFAULT_VALUE;
 import static org.apache.iotdb.commons.pipe.config.constant.PipeProcessorConstant.PROCESSOR_DOWN_SAMPLING_SPLIT_FILE_KEY;
 
-public class DownSamplingProcessor implements PipeProcessor {
+public abstract class DownSamplingProcessor implements PipeProcessor {
 
-  private static final Logger LOGGER = LoggerFactory.getLogger(DownSamplingProcessor.class);
+  protected long memoryLimitInBytes;
 
-  private String dataBaseNameWithPathSeparator;
-  private long intervalInCurrentPrecision;
-  private boolean shouldSplitFile;
+  protected boolean shouldSplitFile;
 
-  private PartialPathLastTimeCache partialPathLastTimeCache;
+  protected String dataBaseNameWithPathSeparator;
+
+  protected PartialPathLastObjectCache<?> pathLastObjectCache;
 
   @Override
   public void validate(PipeParameterValidator validator) throws Exception {
-    // No need to validate.
+    memoryLimitInBytes =
+        validator
+            .getParameters()
+            .getLongOrDefault(
+                PROCESSOR_DOWN_SAMPLING_MEMORY_LIMIT_IN_BYTES_KEY,
+                PROCESSOR_DOWN_SAMPLING_MEMORY_LIMIT_IN_BYTES_DEFAULT_VALUE);
+
+    validator.validate(
+        memoryLimitInBytes -> (Long) memoryLimitInBytes > 0,
+        String.format(
+            "%s must be > 0, but got %s",
+            PROCESSOR_DOWN_SAMPLING_MEMORY_LIMIT_IN_BYTES_KEY, memoryLimitInBytes),
+        memoryLimitInBytes);
   }
 
   @Override
-  public void customize(PipeParameters parameters, PipeProcessorRuntimeConfiguration configuration)
-      throws Exception {
-    final String dataBaseName =
-        StorageEngine.getInstance()
-            .getDataRegion(
-                new DataRegionId(
-                    ((PipeTaskProcessorRuntimeEnvironment) configuration.getRuntimeEnvironment())
-                        .getRegionId()))
-            .getDatabaseName();
-    final long intervalSeconds =
-        parameters.getLongOrDefault(
-            PROCESSOR_DOWN_SAMPLING_INTERVAL_SECONDS_KEY,
-            PROCESSOR_DOWN_SAMPLING_INTERVAL_SECONDS_DEFAULT_VALUE);
-    final long memoryLimitInBytes =
-        parameters.getLongOrDefault(
-            PROCESSOR_DOWN_SAMPLING_MEMORY_LIMIT_IN_BYTES_KEY,
-            PROCESSOR_DOWN_SAMPLING_MEMORY_LIMIT_IN_BYTES_DEFAULT_VALUE);
+  public void customize(
+      PipeParameters parameters, PipeProcessorRuntimeConfiguration configuration) {
     shouldSplitFile =
         parameters.getBooleanOrDefault(
             PROCESSOR_DOWN_SAMPLING_SPLIT_FILE_KEY,
             PROCESSOR_DOWN_SAMPLING_SPLIT_FILE_DEFAULT_VALUE);
-    LOGGER.info(
-        "DownSamplingProcessor in {} is initialized with {}: {}s, {}: {}, {}: {}.",
-        dataBaseName,
-        PROCESSOR_DOWN_SAMPLING_INTERVAL_SECONDS_KEY,
-        intervalSeconds,
-        PROCESSOR_DOWN_SAMPLING_MEMORY_LIMIT_IN_BYTES_KEY,
-        memoryLimitInBytes,
-        PROCESSOR_DOWN_SAMPLING_SPLIT_FILE_KEY,
-        shouldSplitFile);
 
-    dataBaseNameWithPathSeparator = dataBaseName + TsFileConstant.PATH_SEPARATOR;
-    intervalInCurrentPrecision =
-        TimestampPrecisionUtils.convertToCurrPrecision(intervalSeconds, TimeUnit.SECONDS);
+    dataBaseNameWithPathSeparator =
+        StorageEngine.getInstance()
+                .getDataRegion(
+                    new DataRegionId(
+                        ((PipeTaskProcessorRuntimeEnvironment)
+                                configuration.getRuntimeEnvironment())
+                            .getRegionId()))
+                .getDatabaseName()
+            + TsFileConstant.PATH_SEPARATOR;
 
-    partialPathLastTimeCache = new PartialPathLastTimeCache(memoryLimitInBytes);
+    pathLastObjectCache = initPathLastObjectCache(memoryLimitInBytes);
   }
+
+  protected abstract PartialPathLastObjectCache<?> initPathLastObjectCache(long memoryLimitInBytes);
 
   @Override
   public void process(TabletInsertionEvent tabletInsertionEvent, EventCollector eventCollector)
@@ -122,7 +111,8 @@ public class DownSamplingProcessor implements PipeProcessor {
               // To reduce the memory usage, we use the device suffix
               // instead of the full path as the key.
               if (deviceSuffix.get() == null) {
-                deviceSuffix.set(row.getDeviceId().replaceFirst(dataBaseNameWithPathSeparator, ""));
+                deviceSuffix.set(
+                    row.getDeviceId().replaceFirst(this.dataBaseNameWithPathSeparator, ""));
               }
 
               processRow(row, rowCollector, deviceSuffix.get(), exception);
@@ -141,41 +131,11 @@ public class DownSamplingProcessor implements PipeProcessor {
     }
   }
 
-  private void processRow(
+  protected abstract void processRow(
       Row row,
       RowCollector rowCollector,
       String deviceSuffix,
-      AtomicReference<Exception> exception) {
-    boolean hasNonNullMeasurements = false;
-
-    for (int index = 0, size = row.size(); index < size; ++index) {
-      if (row.isNull(index)) {
-        continue;
-      }
-
-      final String timeSeriesSuffix =
-          deviceSuffix + TsFileConstant.PATH_SEPARATOR + row.getColumnName(index);
-      final Long lastSampleTime = partialPathLastTimeCache.getPartialPathLastTime(timeSeriesSuffix);
-
-      if (lastSampleTime != null) {
-        if (Math.abs(row.getTime() - lastSampleTime) >= intervalInCurrentPrecision) {
-          hasNonNullMeasurements = true;
-          partialPathLastTimeCache.setPartialPathLastTime(timeSeriesSuffix, row.getTime());
-        }
-      } else {
-        hasNonNullMeasurements = true;
-        partialPathLastTimeCache.setPartialPathLastTime(timeSeriesSuffix, row.getTime());
-      }
-    }
-
-    if (hasNonNullMeasurements) {
-      try {
-        rowCollector.collectRow(row);
-      } catch (Exception e) {
-        exception.set(e);
-      }
-    }
-  }
+      AtomicReference<Exception> exception);
 
   /**
    * If data comes in {@link TsFileInsertionEvent}, we will not split it into {@link
@@ -207,8 +167,8 @@ public class DownSamplingProcessor implements PipeProcessor {
 
   @Override
   public void close() throws Exception {
-    if (partialPathLastTimeCache != null) {
-      partialPathLastTimeCache.close();
+    if (pathLastObjectCache != null) {
+      pathLastObjectCache.close();
     }
   }
 }
