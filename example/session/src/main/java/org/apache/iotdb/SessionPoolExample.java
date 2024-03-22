594f6a1359d22e279e7b7e7de76962edf8551fbd
lyf <1548150065@qq.com>
2024-03-22 19:15:50 +0800

    revert

--- example/session/src/main/java/org/apache/iotdb/SessionPoolExample.java
+++ example/session/src/main/java/org/apache/iotdb/SessionPoolExample.java
@@ -19,24 +19,20 @@
 
 package org.apache.iotdb;
 
+import org.apache.iotdb.isession.SessionDataSet.DataIterator;
+import org.apache.iotdb.isession.pool.SessionDataSetWrapper;
 import org.apache.iotdb.rpc.IoTDBConnectionException;
 import org.apache.iotdb.rpc.StatementExecutionException;
 import org.apache.iotdb.session.pool.SessionPool;
 import org.apache.iotdb.tsfile.file.metadata.enums.TSDataType;
-import org.apache.iotdb.tsfile.write.record.Tablet;
-import org.apache.iotdb.tsfile.write.schema.MeasurementSchema;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
 
 @SuppressWarnings({"squid:S106", "squid:S1144"})
 public class SessionPoolExample {
@@ -45,106 +41,114 @@ public class SessionPoolExample {
   private static SessionPool sessionPool;
   private static ExecutorService service;
 
-  private static int loop = 10;
+  /** Build a custom SessionPool for this example */
+  private static void constructCustomSessionPool() {
+    sessionPool =
+            new SessionPool.Builder()
+                    .host("127.0.0.1")
+                    .port(6667)
+                    .user("root")
+                    .password("root")
+                    .maxSize(3)
+                    .build();
+  }
 
   /** Build a redirect-able SessionPool for this example */
   private static void constructRedirectSessionPool() {
     List<String> nodeUrls = new ArrayList<>();
     nodeUrls.add("127.0.0.1:6667");
+    nodeUrls.add("127.0.0.1:6668");
     sessionPool =
-        new SessionPool.Builder()
-            .nodeUrls(nodeUrls)
-            .user("root")
-            .password("root")
-            .maxSize(10)
-            .build();
+            new SessionPool.Builder()
+                    .nodeUrls(nodeUrls)
+                    .user("root")
+                    .password("root")
+                    .maxSize(3)
+                    .build();
   }
 
-  public static void main(String[] args) {
+  public static void main(String[] args)
+          throws StatementExecutionException, IoTDBConnectionException, InterruptedException {
     // Choose the SessionPool you going to use
     constructRedirectSessionPool();
 
-    try {
-      loop = Integer.parseInt(args[0]);
-      System.out.println("loop is " + loop);
-      service = Executors.newFixedThreadPool(Integer.parseInt(args[1]));
-      long time = 0;
-      insertTablets(time, 1000, 1);
-      time += 1000L * loop;
-      Thread.sleep(10000);
+    service = Executors.newFixedThreadPool(10);
+    insertRecord();
+    queryByRowRecord();
+    Thread.sleep(1000);
+    queryByIterator();
+    sessionPool.close();
+    service.shutdown();
+  }
 
-      insertTablets(time, 1000, 10);
-      time += 1000L * loop;
-      Thread.sleep(10000);
+  // more insert example, see SessionExample.java
+  private static void insertRecord() throws StatementExecutionException, IoTDBConnectionException {
+    String deviceId = "root.sg1.d1";
+    List<String> measurements = new ArrayList<>();
+    List<TSDataType> types = new ArrayList<>();
+    measurements.add("s1");
+    measurements.add("s2");
+    measurements.add("s3");
+    types.add(TSDataType.INT64);
+    types.add(TSDataType.INT64);
+    types.add(TSDataType.INT64);
 
-      insertTablets(time, 1000, 100);
-      time += 1000L * loop;
-      Thread.sleep(10000);
-
-      insertTablets(time, 1000, 1000);
-    } catch (Throwable e) {
-      System.out.println("写入失败" + e.getMessage());
-    } finally {
-      sessionPool.close();
-      service.shutdown();
+    for (long time = 0; time < 10; time++) {
+      List<Object> values = new ArrayList<>();
+      values.add(1L);
+      values.add(2L);
+      values.add(3L);
+      sessionPool.insertRecord(deviceId, time, measurements, types, values);
     }
   }
 
-  private static void insertTablets(long timestamp, int batchSize, int deviceInBatch) {
-    // The schema of measurements of one device
-    // only measurementId and data type in MeasurementSchema take effects in Tablet
-    List<MeasurementSchema> schemaList = new ArrayList<>();
-    for (int measurement = 0; measurement < 200; measurement++) {
-      schemaList.add(new MeasurementSchema("s" + measurement, TSDataType.INT64));
-    }
-    List<String> allDevices = new ArrayList<>();
-    for (int device = 0; device < 1000; device++) {
-      allDevices.add("root.sg1.d" + device);
-    }
-    List<Future<?>> futures = new ArrayList<>();
-    long startTime = System.currentTimeMillis();
-    for (int no = 0; no < loop; no++) {
-      final int finalNo = no;
-      final long finalTimestamp = timestamp;
-      Future<?> future =
-          service.submit(
+  private static void queryByRowRecord() {
+    for (int i = 0; i < 1; i++) {
+      service.submit(
               () -> {
+                SessionDataSetWrapper wrapper = null;
                 try {
-                  long myTime = finalTimestamp + (long) finalNo * batchSize;
-                  Collections.shuffle(allDevices);
-                  Map<String, Tablet> tablets = new HashMap<>();
-                  List<String> loopDevice = allDevices.subList(0, deviceInBatch);
-                  for (String deviceId : loopDevice) {
-                    Tablet tablet = new Tablet(deviceId, schemaList, batchSize / deviceInBatch);
-                    tablets.put(deviceId, tablet);
+                  wrapper = sessionPool.executeQueryStatement("select * from root.sg1.d1");
+                  System.out.println(wrapper.getColumnNames());
+                  System.out.println(wrapper.getColumnTypes());
+                  while (wrapper.hasNext()) {
+                    System.out.println(wrapper.next());
                   }
-                  for (int batch = 0; batch < batchSize; batch++) {
-                    Tablet tablet = tablets.get(loopDevice.get(batch % deviceInBatch));
-                    int row = tablet.rowSize++;
-                    for (long measurement = 0; measurement < 200; measurement++) {
-                      tablet.addTimestamp(row, myTime + batch);
-                      tablet.addValue("s" + measurement, row, measurement);
-                    }
-                  }
-                  //                          for(Map.Entry<String,Tablet>
-                  // entry:tablets.entrySet()){
-                  //                            sessionPool.insertTablet(entry.getValue());
-                  //                          }
-                  sessionPool.insertTablets(tablets);
                 } catch (IoTDBConnectionException | StatementExecutionException e) {
-                  System.out.println("Failed to insert tablets because " + e.getMessage());
+                  LOGGER.error("Query by row record error", e);
+                } finally {
+                  // remember to close data set finally!
+                  sessionPool.closeResultSet(wrapper);
                 }
               });
-      futures.add(future);
     }
-    for (Future<?> future : futures) {
-      try {
-        future.get();
-      } catch (Exception e) {
-        System.out.println("Failed to execute future " + e.getMessage());
-        Thread.currentThread().interrupt();
-      }
+  }
+
+  private static void queryByIterator() {
+    for (int i = 0; i < 1; i++) {
+      service.submit(
+              () -> {
+                SessionDataSetWrapper wrapper = null;
+                try {
+                  wrapper = sessionPool.executeQueryStatement("select * from root.sg1.d1");
+                  // get DataIterator like JDBC
+                  DataIterator dataIterator = wrapper.iterator();
+                  System.out.println(wrapper.getColumnNames());
+                  System.out.println(wrapper.getColumnTypes());
+                  while (dataIterator.next()) {
+                    StringBuilder builder = new StringBuilder();
+                    for (String columnName : wrapper.getColumnNames()) {
+                      builder.append(dataIterator.getString(columnName) + " ");
+                    }
+                    System.out.println(builder);
+                  }
+                } catch (IoTDBConnectionException | StatementExecutionException e) {
+                  LOGGER.error("Query by Iterator error", e);
+                } finally {
+                  // remember to close data set finally!
+                  sessionPool.closeResultSet(wrapper);
+                }
+              });
     }
-    System.out.println("耗时" + (System.currentTimeMillis() - startTime) + "ms");
   }
 }
