3dcf9f2b19f3d2fc752ac325e7ce041cb1e4c915
Steve Yurong Su <rong@apache.org>
2024-03-28 12:46:45 +0800

    Update PipeHistoricalDataRegionTsFileExtractor.java

--- iotdb-core/datanode/src/main/java/org/apache/iotdb/db/pipe/extractor/dataregion/historical/PipeHistoricalDataRegionTsFileExtractor.java
+++ iotdb-core/datanode/src/main/java/org/apache/iotdb/db/pipe/extractor/dataregion/historical/PipeHistoricalDataRegionTsFileExtractor.java
@@ -352,7 +352,7 @@ public class PipeHistoricalDataRegionTsFileExtractor implements PipeHistoricalDa
                             "[DEBUG][tsfile] tsfile start time {}, isClosed: {}",
                             resource.getFileStartTime(),
                             false);
-                        // return false;
+                        return true;
                       }
                       boolean mayTsFileContainUnprocessedData =
                           mayTsFileContainUnprocessedData(resource);
@@ -392,10 +392,10 @@ public class PipeHistoricalDataRegionTsFileExtractor implements PipeHistoricalDa
                     resource ->
                         // Some resource may not be closed due to the control of
                         // PIPE_MIN_FLUSH_INTERVAL_IN_MS. We simply ignore them.
-                        // resource.isClosed() &&
-                        mayTsFileContainUnprocessedData(resource)
-                            && isTsFileResourceOverlappedWithTimeRange(resource)
-                            && isTsFileGeneratedAfterExtractionTimeLowerBound(resource))
+                        !resource.isClosed()
+                            || mayTsFileContainUnprocessedData(resource)
+                                && isTsFileResourceOverlappedWithTimeRange(resource)
+                                && isTsFileGeneratedAfterExtractionTimeLowerBound(resource))
                 .collect(Collectors.toList());
         resourceList.addAll(unsequenceTsFileResources);
 
