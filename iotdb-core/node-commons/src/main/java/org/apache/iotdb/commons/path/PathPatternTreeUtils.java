1f22a1ba037158867d8fad6433bb730dc3590584
Chen YZ <43774645+Cpaulyz@users.noreply.github.com>
2024-04-03 15:22:28 +0800

    Optimize time cost on rewrite pattern by authority pattern

--- iotdb-core/node-commons/src/main/java/org/apache/iotdb/commons/path/PathPatternTreeUtils.java
+++ iotdb-core/node-commons/src/main/java/org/apache/iotdb/commons/path/PathPatternTreeUtils.java
@@ -19,6 +19,8 @@
 
 package org.apache.iotdb.commons.path;
 
+import org.apache.iotdb.commons.schema.SchemaConstant;
+
 public class PathPatternTreeUtils {
   /**
    * Intersect the pattern tree with the full path prefix tree, and return the intersected pattern
@@ -31,23 +33,9 @@ public class PathPatternTreeUtils {
    */
   public static PathPatternTree intersectWithFullPathPrefixTree(
       PathPatternTree patternTree, PathPatternTree fullPathPrefixTree) {
-    PathPatternTree result = new PathPatternTree();
-    for (PartialPath pathPattern : patternTree.getAllPathPatterns()) {
-      for (PartialPath fullPathOrPrefix : fullPathPrefixTree.getAllPathPatterns()) {
-        if (fullPathOrPrefix.endWithMultiLevelWildcard()) {
-          // prefix
-          for (PartialPath temp : pathPattern.intersectWithPrefixPattern(fullPathOrPrefix)) {
-            result.appendPathPattern(temp);
-          }
-        } else {
-          // full path
-          if (pathPattern.matchFullPath(fullPathOrPrefix)) {
-            result.appendPathPattern(fullPathOrPrefix);
-          }
-        }
-      }
+    if (SchemaConstant.ALL_MATCH_SCOPE.equals(fullPathPrefixTree)) {
+      return patternTree;
     }
-    result.constructTree();
-    return result;
+    return patternTree.intersectWithFullPathPrefixTree(fullPathPrefixTree);
   }
 }
