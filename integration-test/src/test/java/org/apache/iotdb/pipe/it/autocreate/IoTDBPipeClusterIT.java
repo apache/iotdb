80ff23ff5d72566cab26e6a1839587e5b57447fe
Caideyipi <87789683+Caideyipi@users.noreply.github.com>
2024-04-10 20:15:42 +0800

    Pipe: Fixed the bug that config receivers may not be one-to-one match with the sender clients (#12280)
    Co-authored-by: Steve Yurong Su <rong@apache.org>

--- integration-test/src/test/java/org/apache/iotdb/pipe/it/autocreate/IoTDBPipeClusterIT.java
+++ integration-test/src/test/java/org/apache/iotdb/pipe/it/autocreate/IoTDBPipeClusterIT.java
@@ -725,9 +725,9 @@ public class IoTDBPipeClusterIT extends AbstractPipeDualAutoIT {
       final Thread t =
           new Thread(
               () -> {
-                try (SyncConfigNodeIServiceClient client =
+                try (final SyncConfigNodeIServiceClient client =
                     (SyncConfigNodeIServiceClient) senderEnv.getLeaderConfigNodeConnection()) {
-                  TSStatus status =
+                  final TSStatus status =
                       client.createPipe(
                           new TCreatePipeReq("p1", connectorAttributes)
                               .setExtractorAttributes(extractorAttributes)
@@ -759,9 +759,9 @@ public class IoTDBPipeClusterIT extends AbstractPipeDualAutoIT {
       final Thread t =
           new Thread(
               () -> {
-                try (SyncConfigNodeIServiceClient client =
+                try (final SyncConfigNodeIServiceClient client =
                     (SyncConfigNodeIServiceClient) senderEnv.getLeaderConfigNodeConnection()) {
-                  TSStatus status = client.dropPipe("p1");
+                  final TSStatus status = client.dropPipe("p1");
                   if (status.getCode() == TSStatusCode.SUCCESS_STATUS.getStatusCode()) {
                     successCount.incrementAndGet();
                   }
