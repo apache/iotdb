0234bfb253e55e3b6a83b7fbbc69be41c6ce4ef3
VGalaxies <vgalaxies@apache.org>
2024-04-12 12:42:23 +0800

    Revert "reset"
    This reverts commit 33d9fcac5ebf7c32de5f5550f19e3881df0ccbc5.

--- iotdb-core/datanode/src/main/java/org/apache/iotdb/db/pipe/connector/payload/evolvable/builder/PipeTransferBatchReqBuilder.java
+++ iotdb-core/datanode/src/main/java/org/apache/iotdb/db/pipe/connector/payload/evolvable/builder/PipeTransferBatchReqBuilder.java
@@ -43,6 +43,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 import static org.apache.iotdb.commons.pipe.config.constant.PipeConnectorConstant.CONNECTOR_IOTDB_BATCH_DELAY_DEFAULT_VALUE;
 import static org.apache.iotdb.commons.pipe.config.constant.PipeConnectorConstant.CONNECTOR_IOTDB_BATCH_DELAY_KEY;
@@ -69,6 +70,8 @@ public abstract class PipeTransferBatchReqBuilder implements AutoCloseable {
   // limit in buffer size
   protected final PipeMemoryBlock allocatedMemoryBlock;
   protected long totalBufferSize = 0;
+
+  private final AtomicBoolean isClosed = new AtomicBoolean(true);
 
   protected PipeTransferBatchReqBuilder(PipeParameters parameters) {
     maxDelayInMs =
@@ -104,6 +107,8 @@ public abstract class PipeTransferBatchReqBuilder implements AutoCloseable {
           requestMaxBatchSizeInBytes,
           getMaxBatchSizeInBytes());
     }
+
+    isClosed.set(false);
   }
 
   /**
@@ -209,11 +214,17 @@ public abstract class PipeTransferBatchReqBuilder implements AutoCloseable {
 
   @Override
   public synchronized void close() {
+    isClosed.set(true);
+
     for (final Event event : events) {
       if (event instanceof EnrichedEvent) {
         ((EnrichedEvent) event).clearReferenceCount(this.getClass().getName());
       }
     }
     allocatedMemoryBlock.close();
+  }
+
+  public boolean isClosed() {
+    return isClosed.get();
   }
 }
