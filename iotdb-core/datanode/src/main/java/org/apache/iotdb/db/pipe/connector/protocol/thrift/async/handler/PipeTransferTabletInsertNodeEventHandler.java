ccd134cfacfbfe92c15e47faacfa87dabfd43fe5
Peng Junzhi <201250214@smail.nju.edu.cn>
2024-04-16 22:59:39 +0800

    revert comment

--- iotdb-core/datanode/src/main/java/org/apache/iotdb/db/pipe/connector/protocol/thrift/async/handler/PipeTransferTabletInsertNodeEventHandler.java
+++ iotdb-core/datanode/src/main/java/org/apache/iotdb/db/pipe/connector/protocol/thrift/async/handler/PipeTransferTabletInsertNodeEventHandler.java
@@ -42,7 +42,6 @@ public class PipeTransferTabletInsertNodeEventHandler
   @Override
   protected void doTransfer(AsyncPipeDataTransferServiceClient client, TPipeTransferReq req)
       throws TException {
-    // 第二个参数是构造 callback
     client.pipeTransfer(req, this);
   }
 
