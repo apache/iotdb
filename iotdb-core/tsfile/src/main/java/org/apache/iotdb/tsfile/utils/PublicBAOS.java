58af313c76c6eafd5fad539e595b20eed0b262f1
shuwenwei <s13979062948@gmail.com>
2024-04-17 11:50:05 +0800

    modify code style

--- iotdb-core/tsfile/src/main/java/org/apache/iotdb/tsfile/utils/PublicBAOS.java
+++ iotdb-core/tsfile/src/main/java/org/apache/iotdb/tsfile/utils/PublicBAOS.java
@@ -45,21 +45,29 @@ public class PublicBAOS extends ByteArrayOutputStream {
 
   private void ensureCapacity(int minCapacity) {
     // overflow-conscious code
-    if (minCapacity - buf.length > 0) grow(minCapacity);
+    if (minCapacity - buf.length > 0) {
+      grow(minCapacity);
+    }
   }
 
   private void grow(int minCapacity) {
     // overflow-conscious code
     int oldCapacity = buf.length;
     int newCapacity = oldCapacity << 1;
-    if (newCapacity - minCapacity < 0) newCapacity = minCapacity;
-    if (newCapacity - MAX_ARRAY_SIZE > 0) newCapacity = hugeCapacity(minCapacity);
+    if (newCapacity - minCapacity < 0) {
+      newCapacity = minCapacity;
+    }
+    if (newCapacity - MAX_ARRAY_SIZE > 0) {
+      newCapacity = hugeCapacity(minCapacity);
+    }
     buf = Arrays.copyOf(buf, newCapacity);
   }
 
   private static int hugeCapacity(int minCapacity) {
-    if (minCapacity < 0) // overflow
-    throw new OutOfMemoryError();
+    if (minCapacity < 0) {
+      // overflow
+      throw new OutOfMemoryError();
+    }
     return (minCapacity > MAX_ARRAY_SIZE) ? Integer.MAX_VALUE : MAX_ARRAY_SIZE;
   }
 
