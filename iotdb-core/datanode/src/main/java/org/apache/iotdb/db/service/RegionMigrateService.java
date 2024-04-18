c82bcdb4ab2ae3e6bf151725027bba9ebdcfd5a9
Li Yu Heng <liyuheng55555@126.com>
2024-04-18 14:15:09 +0800

    Increase region migration pool size on DataNode (#12366)
    * done
    
    * use fixedThreadPool

--- iotdb-core/datanode/src/main/java/org/apache/iotdb/db/service/RegionMigrateService.java
+++ iotdb-core/datanode/src/main/java/org/apache/iotdb/db/service/RegionMigrateService.java
@@ -217,7 +217,8 @@ public class RegionMigrateService implements IService {
 
     private RegionMigratePool() {
       this.pool =
-          IoTDBThreadPoolFactory.newSingleThreadExecutor(ThreadName.REGION_MIGRATE.getName());
+          IoTDBThreadPoolFactory.newCachedThreadPool(
+              ThreadName.REGION_MIGRATE.getName(), Runtime.getRuntime().availableProcessors() / 2);
     }
 
     @Override
