b9e9aaa30e4c082553b56acd870853d62d47638a
shuwenwei <55970239+shuwenwei@users.noreply.github.com>
2024-04-19 17:27:05 +0800

    Compaction rate limit (#12312)
    * add compaction read rate limiter
    
    * modify method name
    
    * modify default compaction read rate limit
    
    * add config item to limit compaction read speed
    
    * add config item to limit compaction read speed
    
    * fix throughput rate is not correct
    
    * fix some issues
    
    * compaction read rate has no limit in default config
    
    * use correct unit when init rate limiter
    
    * modify maxSizePerWrite
    
    * remove useless code

--- iotdb-core/datanode/src/main/java/org/apache/iotdb/db/storageengine/dataregion/compaction/io/CompactionTsFileReader.java
+++ iotdb-core/datanode/src/main/java/org/apache/iotdb/db/storageengine/dataregion/compaction/io/CompactionTsFileReader.java
@@ -20,6 +20,7 @@
 package org.apache.iotdb.db.storageengine.dataregion.compaction.io;
 
 import org.apache.iotdb.db.service.metrics.CompactionMetrics;
+import org.apache.iotdb.db.storageengine.dataregion.compaction.schedule.CompactionTaskManager;
 import org.apache.iotdb.db.storageengine.dataregion.compaction.schedule.constant.CompactionIoDataType;
 import org.apache.iotdb.db.storageengine.dataregion.compaction.schedule.constant.CompactionType;
 import org.apache.iotdb.tsfile.file.IMetadataIndexEntry;
@@ -75,6 +76,7 @@ public class CompactionTsFileReader extends TsFileSequenceReader {
 
   @Override
   protected ByteBuffer readData(long position, int totalSize) throws IOException {
+    acquireReadDataSizeWithCompactionReadRateLimiter(totalSize);
     ByteBuffer buffer = super.readData(position, totalSize);
     readDataSize.addAndGet(totalSize);
     return buffer;
@@ -246,6 +248,11 @@ public class CompactionTsFileReader extends TsFileSequenceReader {
     long dataSize = readDataSize.get() - before;
     CompactionMetrics.getInstance()
         .recordReadInfo(compactionType, CompactionIoDataType.METADATA, dataSize);
+  }
+
+  private void acquireReadDataSizeWithCompactionReadRateLimiter(int readDataSize) {
+    CompactionTaskManager.getInstance().getCompactionReadOperationRateLimiter().acquire(1);
+    CompactionTaskManager.getInstance().getCompactionReadRateLimiter().acquire(readDataSize);
   }
 
   @Override
