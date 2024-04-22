eb592d1248c4c07d41887e168f226cc40324b785
HTHou <hhaonan@outlook.com>
2024-04-22 15:28:56 +0800

    fix review

--- iotdb-core/datanode/src/main/java/org/apache/iotdb/db/storageengine/dataregion/tsfile/timeindex/PlainDeviceTimeIndex.java
+++ iotdb-core/datanode/src/main/java/org/apache/iotdb/db/storageengine/dataregion/tsfile/timeindex/PlainDeviceTimeIndex.java
@@ -19,31 +19,19 @@
 
 package org.apache.iotdb.db.storageengine.dataregion.tsfile.timeindex;
 
-import org.apache.iotdb.commons.exception.IllegalPathException;
-import org.apache.iotdb.commons.path.PartialPath;
 import org.apache.iotdb.db.queryengine.plan.analyze.cache.schema.DataNodeDevicePathCache;
 
 import org.apache.tsfile.file.metadata.IDeviceID;
-import org.apache.tsfile.utils.Pair;
-import org.apache.tsfile.utils.RamUsageEstimator;
 import org.apache.tsfile.utils.ReadWriteIOUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.nio.ByteBuffer;
 import java.util.HashSet;
-import java.util.Map;
 import java.util.Set;
 
 public class PlainDeviceTimeIndex extends ArrayDeviceTimeIndex implements ITimeIndex {
-
-  private static final Logger logger = LoggerFactory.getLogger(PlainDeviceTimeIndex.class);
-
-  private static final long INSTANCE_SIZE =
-      RamUsageEstimator.shallowSizeOfInstance(ArrayDeviceTimeIndex.class);
 
   @Override
   public void serialize(OutputStream outputStream) throws IOException {
@@ -114,64 +102,6 @@ public class PlainDeviceTimeIndex extends ArrayDeviceTimeIndex implements ITimeI
       devices.add(IDeviceID.Factory.DEFAULT_FACTORY.create(path));
     }
     return devices;
-  }
-
-  @Override
-  public Pair<Long, Long> getPossibleStartTimeAndEndTime(
-      PartialPath devicePattern, Set<IDeviceID> deviceMatchInfo) {
-    boolean hasMatchedDevice = false;
-    long startTime = Long.MAX_VALUE;
-    long endTime = Long.MIN_VALUE;
-    for (Map.Entry<IDeviceID, Integer> entry : deviceToIndex.entrySet()) {
-      try {
-        if (deviceMatchInfo.contains(entry.getKey())) {
-          hasMatchedDevice = true;
-          if (startTimes[entry.getValue()] < startTime) {
-            startTime = startTimes[entry.getValue()];
-          }
-          if (endTimes[entry.getValue()] > endTime) {
-            endTime = endTimes[entry.getValue()];
-          }
-        } else {
-          if (devicePattern.matchFullPath(
-              DataNodeDevicePathCache.getInstance().getPartialPath(entry.getKey().toString()))) {
-            deviceMatchInfo.add(entry.getKey());
-            hasMatchedDevice = true;
-            if (startTimes[entry.getValue()] < startTime) {
-              startTime = startTimes[entry.getValue()];
-            }
-            if (endTimes[entry.getValue()] > endTime) {
-              endTime = endTimes[entry.getValue()];
-            }
-          }
-        }
-      } catch (IllegalPathException e) {
-        // won't reach here
-      }
-    }
-
-    return hasMatchedDevice ? new Pair<>(startTime, endTime) : null;
-  }
-
-  @Override
-  public int compareDegradePriority(ITimeIndex timeIndex) {
-    if (timeIndex instanceof ArrayDeviceTimeIndex) {
-      return Long.compare(getMinStartTime(), timeIndex.getMinStartTime());
-    } else if (timeIndex instanceof FileTimeIndex) {
-      return -1;
-    } else {
-      logger.error("Wrong timeIndex type {}", timeIndex.getClass().getName());
-      throw new RuntimeException("Wrong timeIndex type " + timeIndex.getClass().getName());
-    }
-  }
-
-  @Override
-  public long calculateRamSize() {
-    return INSTANCE_SIZE
-        + RamUsageEstimator.sizeOfMap(
-            deviceToIndex, RamUsageEstimator.shallowSizeOfInstance(Integer.class))
-        + RamUsageEstimator.sizeOf(startTimes)
-        + RamUsageEstimator.sizeOf(endTimes);
   }
 
   @Override
