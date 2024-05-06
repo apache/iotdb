5eca9e3fb46436255296b6ab9b5fe936fc109feb
shuwenwei <s13979062948@gmail.com>
2024-05-06 19:20:20 +0800

    delete old code

--- iotdb-core/datanode/src/main/java/org/apache/iotdb/db/storageengine/dataregion/compaction/execute/task/InnerSpaceCompactionTask.java
+++ iotdb-core/datanode/src/main/java/org/apache/iotdb/db/storageengine/dataregion/compaction/execute/task/InnerSpaceCompactionTask.java
@@ -33,7 +33,6 @@ import org.apache.iotdb.db.storageengine.dataregion.compaction.execute.utils.log
 import org.apache.iotdb.db.storageengine.dataregion.compaction.execute.utils.log.CompactionLogger;
 import org.apache.iotdb.db.storageengine.dataregion.compaction.execute.utils.log.SimpleCompactionLogger;
 import org.apache.iotdb.db.storageengine.dataregion.compaction.execute.utils.log.TsFileIdentifier;
-import org.apache.iotdb.db.storageengine.dataregion.compaction.schedule.CompactionTaskManager;
 import org.apache.iotdb.db.storageengine.dataregion.compaction.selector.estimator.AbstractInnerSpaceEstimator;
 import org.apache.iotdb.db.storageengine.dataregion.compaction.selector.estimator.FastCompactionInnerCompactionEstimator;
 import org.apache.iotdb.db.storageengine.dataregion.compaction.selector.estimator.ReadChunkInnerCompactionEstimator;
@@ -509,13 +508,6 @@ public class InnerSpaceCompactionTask extends AbstractCompactionTask {
   @Override
   public int getProcessedFileNum() {
     return selectedTsFileResourceList.size();
-  }
-
-  @Override
-  public void handleTaskCleanup() {
-    if (sumOfCompactionCount == 0) {
-      CompactionTaskManager.getInstance().getZeroLevelInnerCompactionTaskNum().decrementAndGet();
-    }
   }
 
   @Override
