2b55ea05197c478139f3f9878b5b1e1e3d2f8628
Caideyipi <87789683+Caideyipi@users.noreply.github.com>
2024-05-11 17:10:22 +0800

    report fixx

--- iotdb-core/node-commons/src/main/java/org/apache/iotdb/commons/pipe/event/PipeWritePlanEvent.java
+++ iotdb-core/node-commons/src/main/java/org/apache/iotdb/commons/pipe/event/PipeWritePlanEvent.java
@@ -23,12 +23,7 @@ import org.apache.iotdb.commons.consensus.index.ProgressIndex;
 import org.apache.iotdb.commons.pipe.pattern.PipePattern;
 import org.apache.iotdb.commons.pipe.task.meta.PipeTaskMeta;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 public abstract class PipeWritePlanEvent extends EnrichedEvent implements SerializableEvent {
-
-  private static final Logger LOGGER = LoggerFactory.getLogger(PipeWritePlanEvent.class);
 
   protected boolean isGeneratedByPipe;
 
@@ -43,26 +38,15 @@ public abstract class PipeWritePlanEvent extends EnrichedEvent implements Serial
     this.isGeneratedByPipe = isGeneratedByPipe;
   }
 
-  /**
-   * This event doesn't share resources with other events, so no need to maintain reference count.
-   * We just use a counter to prevent the reference count from being less than 0.
-   */
+  /** {@link PipeWritePlanEvent} does not share resources with other events. */
   @Override
   public boolean internallyIncreaseResourceReferenceCount(final String holderMessage) {
-    referenceCount.incrementAndGet();
     return true;
   }
 
-  /**
-   * This event doesn't share resources with other events, so no need to maintain reference count.
-   * We just use a counter to prevent the reference count from being less than 0.
-   */
+  /** This {@link PipeWritePlanEvent} does not share resources with other events. */
   @Override
   public boolean internallyDecreaseResourceReferenceCount(final String holderMessage) {
-    final long count = referenceCount.decrementAndGet();
-    if (count < 0) {
-      LOGGER.warn("The reference count is less than 0, may need to check the implementation.");
-    }
     return true;
   }
 
