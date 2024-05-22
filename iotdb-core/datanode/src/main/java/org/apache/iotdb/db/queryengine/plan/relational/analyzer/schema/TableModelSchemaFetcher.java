4de5ed1080b9363d050802080a05d08ed99a0deb
MarcosZyk <1534661820@qq.com>
2024-05-22 12:02:14 +0800

    use linkedHashSet for deduplicate

--- iotdb-core/datanode/src/main/java/org/apache/iotdb/db/queryengine/plan/relational/analyzer/schema/TableModelSchemaFetcher.java
+++ iotdb-core/datanode/src/main/java/org/apache/iotdb/db/queryengine/plan/relational/analyzer/schema/TableModelSchemaFetcher.java
@@ -70,6 +70,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
@@ -509,9 +510,7 @@ public class TableModelSchemaFetcher {
           cacheFetchedDevice);
     }
 
-    Set<DeviceEntry> set = new HashSet<>();
-    deviceEntryList.forEach(set::add);
-
+    Set<DeviceEntry> set = new LinkedHashSet<>(deviceEntryList);
     return new ArrayList<>(set);
   }
 
