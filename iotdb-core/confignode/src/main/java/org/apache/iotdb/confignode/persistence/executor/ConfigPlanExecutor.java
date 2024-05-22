ded3cea5d4921d374f452b1b6f9f04d08f6787c1
Caideyipi <87789683+Caideyipi@users.noreply.github.com>
2024-05-22 14:58:23 +0800

    Update ConfigPlanExecutor.java

--- iotdb-core/confignode/src/main/java/org/apache/iotdb/confignode/persistence/executor/ConfigPlanExecutor.java
+++ iotdb-core/confignode/src/main/java/org/apache/iotdb/confignode/persistence/executor/ConfigPlanExecutor.java
@@ -509,7 +509,7 @@ public class ConfigPlanExecutor {
       case PipeUnsetTemplate:
       case PipeSetTTL:
         // PipeUnsetTemplate/PipeSetTTL plan will not be written here, and exists only after pipe
-        // sender collects UnsetTemplatePlan and before receiver calls ConfigManager.
+        // sender collects UnsetTemplatePlan/SetTTLPlan and before receiver calls ConfigManager.
         throw new UnsupportedOperationException("PipeUnsetTemplate is not supported.");
       case TestOnly:
         return new TSStatus(TSStatusCode.SUCCESS_STATUS.getStatusCode());
