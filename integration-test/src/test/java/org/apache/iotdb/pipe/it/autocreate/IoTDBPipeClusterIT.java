43f224351bb0eb29331d3aa5cc8d52f5d2523583
Caideyipi <87789683+Caideyipi@users.noreply.github.com>
2024-05-22 11:24:38 +0800

    Update IoTDBPipeClusterIT.java

--- integration-test/src/test/java/org/apache/iotdb/pipe/it/autocreate/IoTDBPipeClusterIT.java
+++ integration-test/src/test/java/org/apache/iotdb/pipe/it/autocreate/IoTDBPipeClusterIT.java
@@ -915,9 +915,8 @@ public class IoTDBPipeClusterIT extends AbstractPipeDualAutoIT {
       if (!TestUtils.tryExecuteNonQueriesWithRetry(
           senderEnv,
           Arrays.asList(
-              "insert into root.db.d1(time, s1) values (-123, 3)",
-              "insert into root.db.d1(time, s1) values (now(), 3)",
-              "flush"))) {
+              // Test the correctness of insertRowsNode transmission
+              "insert into root.db.d1(time, s1) values (-123, 3), (now(), 3)", "flush"))) {
         return;
       }
 
