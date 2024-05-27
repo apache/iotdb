10df8348fc4dd04fa1ab76b6de0608102fc054ca
shuwenwei <s13979062948@gmail.com>
2024-05-27 11:53:54 +0800

    generate compaction target file with tier level

--- iotdb-core/datanode/src/main/java/org/apache/iotdb/db/storageengine/dataregion/tsfile/generator/TsFileNameGenerator.java
+++ iotdb-core/datanode/src/main/java/org/apache/iotdb/db/storageengine/dataregion/tsfile/generator/TsFileNameGenerator.java
@@ -75,6 +75,7 @@ public class TsFileNameGenerator {
         version,
         innerSpaceCompactionCount,
         crossSpaceCompactionCount,
+        0,
         TsFileConstant.TSFILE_SUFFIX);
   }
 
@@ -87,10 +88,12 @@ public class TsFileNameGenerator {
       long version,
       int innerSpaceCompactionCount,
       int crossSpaceCompactionCount,
+      int tierLevel,
       String customSuffix)
       throws DiskSpaceInsufficientException {
     String tsFileDir =
-        generateTsFileDir(sequence, logicalStorageGroup, virtualStorageGroup, timePartitionId);
+        generateTsFileDir(
+            sequence, logicalStorageGroup, virtualStorageGroup, timePartitionId, tierLevel);
     fsFactory.getFile(tsFileDir).mkdirs();
     return tsFileDir
         + File.separator
@@ -102,10 +105,11 @@ public class TsFileNameGenerator {
       boolean sequence,
       String logicalStorageGroup,
       String virtualStorageGroup,
-      long timePartitionId)
+      long timePartitionId,
+      int tierLevel)
       throws DiskSpaceInsufficientException {
     TierManager tierManager = TierManager.getInstance();
-    String baseDir = tierManager.getNextFolderForTsFile(0, sequence);
+    String baseDir = tierManager.getNextFolderForTsFile(tierLevel, sequence);
     return baseDir
         + File.separator
         + logicalStorageGroup
@@ -241,6 +245,7 @@ public class TsFileNameGenerator {
                       tsFileName.version,
                       tsFileName.innerCompactionCnt,
                       tsFileName.crossCompactionCnt,
+                      resource.getTierLevel(),
                       IoTDBConstant.CROSS_COMPACTION_TMP_FILE_SUFFIX)),
               TsFileResourceStatus.COMPACTING));
     }
@@ -265,6 +270,7 @@ public class TsFileNameGenerator {
     long maxVersion = Long.MIN_VALUE;
     long maxInnerMergeCount = Long.MIN_VALUE;
     long maxCrossMergeCount = Long.MIN_VALUE;
+    int maxTierLevel = Integer.MIN_VALUE;
     for (TsFileResource resource : tsFileResources) {
       TsFileName tsFileName = getTsFileName(resource.getTsFile().getName());
       minTime = Math.min(tsFileName.time, minTime);
@@ -273,6 +279,7 @@ public class TsFileNameGenerator {
       maxVersion = Math.max(tsFileName.version, maxVersion);
       maxInnerMergeCount = Math.max(tsFileName.innerCompactionCnt, maxInnerMergeCount);
       maxCrossMergeCount = Math.max(tsFileName.crossCompactionCnt, maxCrossMergeCount);
+      maxTierLevel = Math.max(resource.getTierLevel(), maxTierLevel);
     }
     // set target resource to COMPACTING until the end of this task
     TsFileResource resource =
@@ -288,6 +295,7 @@ public class TsFileNameGenerator {
                         minVersion,
                         (int) maxInnerMergeCount + 1,
                         (int) maxCrossMergeCount,
+                        maxTierLevel,
                         IoTDBConstant.INNER_COMPACTION_TMP_FILE_SUFFIX)),
                 TsFileResourceStatus.COMPACTING)
             : new TsFileResource(
@@ -301,6 +309,7 @@ public class TsFileNameGenerator {
                         maxVersion,
                         (int) maxInnerMergeCount + 1,
                         (int) maxCrossMergeCount,
+                        maxTierLevel,
                         IoTDBConstant.INNER_COMPACTION_TMP_FILE_SUFFIX)),
                 TsFileResourceStatus.COMPACTING);
     resource.setSeq(sequence);
