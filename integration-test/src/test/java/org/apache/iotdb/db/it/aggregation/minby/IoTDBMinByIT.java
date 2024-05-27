5ba07d84c49c52a3e304a29c83612a1fc57f4948
Liao Lanyu <1435078631@qq.com>
2024-05-27 22:14:53 +0800

    Fix New data type query IT

--- integration-test/src/test/java/org/apache/iotdb/db/it/aggregation/minby/IoTDBMinByIT.java
+++ integration-test/src/test/java/org/apache/iotdb/db/it/aggregation/minby/IoTDBMinByIT.java
@@ -58,6 +58,7 @@ public class IoTDBMinByIT {
         "CREATE TIMESERIES root.db.d1.x4 WITH DATATYPE=DOUBLE, ENCODING=PLAIN",
         "CREATE TIMESERIES root.db.d1.x5 WITH DATATYPE=BOOLEAN, ENCODING=PLAIN",
         "CREATE TIMESERIES root.db.d1.x6 WITH DATATYPE=TEXT, ENCODING=PLAIN",
+        "CREATE TIMESERIES root.db.d1.x7 WITH DATATYPE=STRING, ENCODING=PLAIN",
         "CREATE TIMESERIES root.db.d1.x8 WITH DATATYPE=BLOB, ENCODING=PLAIN",
         "CREATE TIMESERIES root.db.d1.x9 WITH DATATYPE=DATE, ENCODING=PLAIN",
         "CREATE TIMESERIES root.db.d1.x10 WITH DATATYPE=TIMESTAMP, ENCODING=PLAIN",
@@ -210,7 +211,7 @@ public class IoTDBMinByIT {
               new String[] {"x1", "x2", "x3", "x4", "x5", "x6", "x7", "x8", "x9", "x10"},
               new String[] {"y1", "y2", "y3", "y4", "y7", "y9", "y10"});
       String[] retArray =
-          new String[] {"3,3,3.0,3.0,false,3,3,0x33,2024-01-03,1970-01-01T08:00:00.003+08:00,"};
+          new String[] {"3,3,3.0,3.0,false,3,3,0x33,2024-01-03,1970-01-01T00:00:00.003Z,"};
       for (Map.Entry<String, String[]> expectedHeader : expectedHeaders.entrySet()) {
         String y = expectedHeader.getKey();
         resultSetEqualTest(
