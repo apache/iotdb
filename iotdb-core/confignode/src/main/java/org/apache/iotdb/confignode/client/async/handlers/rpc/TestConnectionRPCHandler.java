2d128414b504d191e7c83f422e8e63256ac95392
liyuheng <liyuheng55555@126.com>
2024-05-29 15:23:54 +0800

    fxxk, tooooo complicated

---
+++ iotdb-core/confignode/src/main/java/org/apache/iotdb/confignode/client/async/handlers/rpc/TestConnectionRPCHandler.java
@@ -0,0 +1,57 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.iotdb.confignode.client.async.handlers.rpc;
+
+import org.apache.iotdb.common.rpc.thrift.TDataNodeLocation;
+import org.apache.iotdb.common.rpc.thrift.TTestConnectionResp;
+import org.apache.iotdb.confignode.client.DataNodeRequestType;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Map;
+import java.util.concurrent.CountDownLatch;
+
+public class TestConnectionRPCHandler extends AbstractAsyncRPCHandler<TTestConnectionResp> {
+
+  private static final Logger LOGGER = LoggerFactory.getLogger(TestConnectionRPCHandler.class);
+
+  protected TestConnectionRPCHandler(
+      DataNodeRequestType requestType,
+      int requestId,
+      TDataNodeLocation targetDataNode,
+      Map<Integer, TDataNodeLocation> dataNodeLocationMap,
+      Map<Integer, TTestConnectionResp> responseMap,
+      CountDownLatch countDownLatch) {
+    super(requestType, requestId, targetDataNode, dataNodeLocationMap, responseMap, countDownLatch);
+  }
+
+  @Override
+  public void onComplete(TTestConnectionResp resp) {
+    responseMap.put(requestId, resp);
+    dataNodeLocationMap.remove(requestId);
+  }
+
+  // TODO: 什么情况下会error？
+  @Override
+  public void onError(Exception e) {
+    LOGGER.error("gg", e);
+  }
+}
