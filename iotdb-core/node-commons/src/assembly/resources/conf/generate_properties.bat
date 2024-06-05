924b9ac6f233bea77704dca1d395bfda00275318
shuwenwei <s13979062948@gmail.com>
2024-06-05 10:30:38 +0800

    modify script for windows

--- iotdb-core/node-commons/src/assembly/resources/conf/generate_properties.bat
+++ iotdb-core/node-commons/src/assembly/resources/conf/generate_properties.bat
@@ -1,12 +1,21 @@
 @echo off
-setlocal enabledelayedexpansion
 
-set "template_file=conf\iotdb-system.properties.template"
-set "output_file=conf\iotdb-system.properties"
-ren "%output_file%" "%template_file%"
+set "source_file=src\assembly\resources\conf\iotdb-system.properties"
+set "target_template_file=target\conf\iotdb-system.properties.template"
+set "target_properties_file=target\conf\iotdb-system.properties"
 
-(for /f "usebackq delims=" %%A in ("%template_file%") do (
-    set "line=%%A"
-    set "line=!line:~0,1!"
-    if not "!line!"=="#" if not "!line!"=="" echo %%A
-)) > "%output_file%"
+if exist "%target_template_file%" (
+    del "%target_template_file%"
+)
+
+if exist "%target_properties_file%" (
+    del "%target_properties_file%"
+)
+
+mkdir "%target_template_file%\.."
+
+copy "%source_file%" "%target_template_file%"
+
+for /f "tokens=* delims=" %%i in ('findstr /v "^[ ]*#" "%target_template_file%" ^| findstr /r /v "^$"') do (
+    echo %%i >> "%target_properties_file%"
+)
