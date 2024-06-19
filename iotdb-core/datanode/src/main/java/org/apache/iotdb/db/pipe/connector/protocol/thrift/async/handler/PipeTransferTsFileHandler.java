0abc2e05b583473b141fd2a5f4df4a93278f05d0
Steve Yurong Su <rong@apache.org>
2024-06-19 14:39:00 +0800

    refactor

--- iotdb-core/datanode/src/main/java/org/apache/iotdb/db/pipe/connector/protocol/thrift/async/handler/PipeTransferTsFileHandler.java
+++ iotdb-core/datanode/src/main/java/org/apache/iotdb/db/pipe/connector/protocol/thrift/async/handler/PipeTransferTsFileHandler.java
@@ -55,11 +55,19 @@ import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.stream.Collectors;
 
 public class PipeTransferTsFileHandler implements AsyncMethodCallback<TPipeTransferResp> {
+
   private static final Logger LOGGER = LoggerFactory.getLogger(PipeTransferTsFileHandler.class);
-  private final List<EnrichedEvent> events;
+
+  // Used to transfer the file
   private final IoTDBDataRegionAsyncConnector connector;
 
+  // Used to rate limit the transfer
   private final Map<String, Double> pipeName2WeightMap;
+
+  // The original events to be transferred, can be multiple events if
+  // the file is batched with other events
+  private final List<EnrichedEvent> events;
+
   private final File tsFile;
   private final File modFile;
   private File currentFile;
@@ -78,16 +86,16 @@ public class PipeTransferTsFileHandler implements AsyncMethodCallback<TPipeTrans
   private AsyncPipeDataTransferServiceClient client;
 
   public PipeTransferTsFileHandler(
+      final IoTDBDataRegionAsyncConnector connector,
       final Map<String, Double> pipeName2WeightMap,
       final List<EnrichedEvent> events,
       final File tsFile,
       final File modFile,
-      final boolean transferMod,
-      final IoTDBDataRegionAsyncConnector connector)
+      final boolean transferMod)
       throws FileNotFoundException {
+    this.connector = connector;
     this.pipeName2WeightMap = pipeName2WeightMap;
     this.events = events;
-    this.connector = connector;
 
     this.tsFile = tsFile;
     this.modFile = modFile;
