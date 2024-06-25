dea8eb6d95e65411fc7c10dadc3b97f6d29bda5d
Caideyipi <87789683+Caideyipi@users.noreply.github.com>
2024-06-25 17:22:23 +0800

    Update PipeConnectorSubtask.java

--- iotdb-core/datanode/src/main/java/org/apache/iotdb/db/pipe/task/subtask/connector/PipeConnectorSubtask.java
+++ iotdb-core/datanode/src/main/java/org/apache/iotdb/db/pipe/task/subtask/connector/PipeConnectorSubtask.java
@@ -234,30 +234,27 @@ public class PipeConnectorSubtask extends PipeAbstractConnectorSubtask {
         });
     synchronized (this) {
       // If exceptions to dropped pipe still emerge, it must be caused by the last event, because
-      // now
-      // we can reckon that the input pending queue does not contain any of the pipe's events
+      // now we can reckon that the input pending queue does not contain any of the pipe's events
       if (lastEvent instanceof EnrichedEvent
           && pipeNameToDrop.equals(((EnrichedEvent) lastEvent).getPipeName())) {
         // Do not clear last event's reference count because it may be on transferring
         lastEvent = null;
-        // Submit self to avoid that the lastEvent has been retried "max times" times and is stopped
+        // Submit self to avoid that the lastEvent has been retried "max times" times and has
+        // stopped
         // executing.
         // 1. If the last event is still on execution, the "submitSelf" cause nothing.
-        // 2. If the last event is on success, then the callback method skip that retry.
+        // 2. If the last event is on success, then the callback method will skip this turn of
+        //    submission.
         // 3. If the last event is on failure, then if it should retry, it's skipped. If it should
-        // not
-        //    retry, it's retried here and other "report" will wait for the lock to stop all the
-        // pipes
-        //    with critical exceptions. Note that this "discardEventsOfPipe" is under the "drop
-        // pipe"
-        //    lock, the report will see no pipes with critical exceptions and will do nothing.
+        //    not retry, it's retried here and other "report" will wait for the lock to stop all
+        //    the pipes with critical exceptions. Note that this "discardEventsOfPipe" is under
+        //    the "drop pipe" lock, the report will see no pipes with critical exceptions and will
+        //    do nothing.
         submitSelf();
       }
       // We only clear the lastEvent's reference count when it's already on failure. Namely, we
-      // clear
-      // the lastExceptionEvent. It's safe to potentially clear it twice because we have the
-      // "nonnull"
-      // detection.
+      // clear the lastExceptionEvent. It's safe to potentially clear it twice because we have the
+      // "nonnull" detection.
       if (lastExceptionEvent instanceof EnrichedEvent
           && pipeNameToDrop.equals(((EnrichedEvent) lastExceptionEvent).getPipeName())) {
         clearReferenceCountAndReleaseLastExceptionEvent();
