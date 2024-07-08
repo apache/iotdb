5f002831a4b5e6244ac3b89add5c4003d0d3d5dd
“y <2725843507@qq.com>
2024-07-08 11:12:41 +0800

    fixed the issue that show pipes prints too much garbage logs

--- iotdb-core/confignode/src/main/java/org/apache/iotdb/confignode/consensus/response/pipe/task/PipeTableResp.java
+++ iotdb-core/confignode/src/main/java/org/apache/iotdb/confignode/consensus/response/pipe/task/PipeTableResp.java
@@ -39,7 +39,11 @@ import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeSet;
 import java.util.stream.Collectors;
 
 public class PipeTableResp implements DataSet {
@@ -105,26 +109,44 @@ public class PipeTableResp implements DataSet {
     final List<TShowPipeInfo> showPipeInfoList = new ArrayList<>();
 
     for (final PipeMeta pipeMeta : allPipeMeta) {
+      final Map<String, Set<Integer>> pipeExceptionMessageRegion = new HashMap<>();
       final PipeStaticMeta staticMeta = pipeMeta.getStaticMeta();
       final PipeRuntimeMeta runtimeMeta = pipeMeta.getRuntimeMeta();
       final StringBuilder exceptionMessageBuilder = new StringBuilder();
-      for (final PipeRuntimeException e :
-          runtimeMeta.getNodeId2PipeRuntimeExceptionMap().values()) {
-        exceptionMessageBuilder
-            .append(DateTimeUtils.convertLongToDate(e.getTimeStamp(), "ms"))
-            .append(", ")
-            .append(e.getMessage())
-            .append("\n");
+
+      for (final Map.Entry<Integer, PipeRuntimeException> entry :
+          runtimeMeta.getNodeId2PipeRuntimeExceptionMap().entrySet()) {
+        final Integer regionId = entry.getKey();
+        final PipeRuntimeException e = entry.getValue();
+        final String exceptionMessage =
+            DateTimeUtils.convertLongToDate(e.getTimeStamp(), "ms") + ", " + e.getMessage();
+
+        pipeExceptionMessageRegion
+            .computeIfAbsent(exceptionMessage, k -> new TreeSet<>())
+            .add(regionId);
       }
-      for (final PipeTaskMeta pipeTaskMeta :
-          runtimeMeta.getConsensusGroupId2TaskMetaMap().values()) {
-        for (final PipeRuntimeException e : pipeTaskMeta.getExceptionMessages()) {
-          exceptionMessageBuilder
-              .append(DateTimeUtils.convertLongToDate(e.getTimeStamp(), "ms"))
-              .append(", ")
-              .append(e.getMessage())
-              .append("\n");
+
+      for (final Map.Entry<Integer, PipeTaskMeta> entry :
+          runtimeMeta.getConsensusGroupId2TaskMetaMap().entrySet()) {
+        final Integer regionId = entry.getKey();
+        for (final PipeRuntimeException e : entry.getValue().getExceptionMessages()) {
+          final String exceptionMessage =
+              DateTimeUtils.convertLongToDate(e.getTimeStamp(), "ms") + ", " + e.getMessage();
+          pipeExceptionMessageRegion
+              .computeIfAbsent(exceptionMessage, k -> new TreeSet<>())
+              .add(regionId);
         }
+      }
+
+      for (final Map.Entry<String, Set<Integer>> entry : pipeExceptionMessageRegion.entrySet()) {
+        final String exceptionMessage = entry.getKey();
+        final Set<Integer> regionIds = entry.getValue();
+        exceptionMessageBuilder
+            .append("regionId: ")
+            .append(regionIds)
+            .append(",")
+            .append(exceptionMessage)
+            .append(";");
       }
 
       final TShowPipeInfo showPipeInfo =
