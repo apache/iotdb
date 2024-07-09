b8dd5060b1c94011561aabb971a033a367a51976
luoluoyuyu <zhenyu@apache.org>
2024-07-09 22:21:11 +0800

    Modify the code format

--- integration-test/src/test/java/org/apache/iotdb/subscription/it/local/IoTDBSubscriptionBasicIT.java
+++ integration-test/src/test/java/org/apache/iotdb/subscription/it/local/IoTDBSubscriptionBasicIT.java
@@ -162,11 +162,12 @@ public class IoTDBSubscriptionBasicIT extends AbstractSubscriptionLocalIT {
     // Check row count
     try {
       // Keep retrying if there are execution failures
-      AWAIT.untilAsserted(() -> {
-        Assert.assertEquals(100, rowCount.get());
-        Assert.assertTrue(commitSuccessCount.get() > lastCommitSuccessCount.get());
-        Assert.assertEquals(0, commitFailureCount.get());
-      });
+      AWAIT.untilAsserted(
+          () -> {
+            Assert.assertEquals(100, rowCount.get());
+            Assert.assertTrue(commitSuccessCount.get() > lastCommitSuccessCount.get());
+            Assert.assertEquals(0, commitFailureCount.get());
+          });
     } catch (final Exception e) {
       e.printStackTrace();
       fail(e.getMessage());
@@ -190,11 +191,12 @@ public class IoTDBSubscriptionBasicIT extends AbstractSubscriptionLocalIT {
     // Check row count
     try {
       // Keep retrying if there are execution failures
-      AWAIT.untilAsserted(() -> {
-        Assert.assertEquals(200, rowCount.get());
-        Assert.assertTrue(commitSuccessCount.get() > lastCommitSuccessCount.get());
-        Assert.assertEquals(0, commitFailureCount.get());
-      });
+      AWAIT.untilAsserted(
+          () -> {
+            Assert.assertEquals(200, rowCount.get());
+            Assert.assertTrue(commitSuccessCount.get() > lastCommitSuccessCount.get());
+            Assert.assertEquals(0, commitFailureCount.get());
+          });
     } catch (final Exception e) {
       e.printStackTrace();
       fail(e.getMessage());
