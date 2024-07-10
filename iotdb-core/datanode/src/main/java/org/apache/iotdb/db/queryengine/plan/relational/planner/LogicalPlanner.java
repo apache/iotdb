9948542d4b0a715125f041d2f572b68949dfca75
JackieTien97 <jackietien97@gmail.com>
2024-07-10 11:55:25 +0800

    Delete SchemaQueryMergeNode in LogicalPlanner

--- iotdb-core/datanode/src/main/java/org/apache/iotdb/db/queryengine/plan/relational/planner/LogicalPlanner.java
+++ iotdb-core/datanode/src/main/java/org/apache/iotdb/db/queryengine/plan/relational/planner/LogicalPlanner.java
@@ -25,7 +25,6 @@ import org.apache.iotdb.db.queryengine.execution.warnings.WarningCollector;
 import org.apache.iotdb.db.queryengine.plan.analyze.QueryType;
 import org.apache.iotdb.db.queryengine.plan.planner.plan.LogicalQueryPlan;
 import org.apache.iotdb.db.queryengine.plan.planner.plan.node.PlanNode;
-import org.apache.iotdb.db.queryengine.plan.planner.plan.node.metedata.read.SchemaQueryMergeNode;
 import org.apache.iotdb.db.queryengine.plan.planner.plan.node.metedata.read.TableDeviceFetchNode;
 import org.apache.iotdb.db.queryengine.plan.planner.plan.node.metedata.read.TableDeviceQueryNode;
 import org.apache.iotdb.db.queryengine.plan.relational.analyzer.Analysis;
@@ -210,7 +209,6 @@ public class LogicalPlanner {
 
     analysis.setRespDatasetHeader(new DatasetHeader(columnHeaderList, true));
 
-    SchemaQueryMergeNode mergeNode = new SchemaQueryMergeNode(context.getQueryId().genPlanNodeId());
     TableDeviceFetchNode fetchNode =
         new TableDeviceFetchNode(
             context.getQueryId().genPlanNodeId(),
@@ -219,7 +217,6 @@ public class LogicalPlanner {
             statement.getDeviceIdList(),
             columnHeaderList,
             null);
-    mergeNode.addChild(fetchNode);
 
     SchemaPartition schemaPartition =
         metadata.getSchemaPartition(statement.getDatabase(), statement.getPartitionKeyList());
@@ -229,7 +226,7 @@ public class LogicalPlanner {
       analysis.setFinishQueryAfterAnalyze();
     }
 
-    return mergeNode;
+    return fetchNode;
   }
 
   private PlanNode planShowDevice(ShowDevice statement, Analysis analysis) {
@@ -238,7 +235,6 @@ public class LogicalPlanner {
     List<ColumnHeader> columnHeaderList =
         getColumnHeaderList(statement.getDatabase(), statement.getTableName());
 
-    SchemaQueryMergeNode mergeNode = new SchemaQueryMergeNode(context.getQueryId().genPlanNodeId());
     TableDeviceQueryNode queryNode =
         new TableDeviceQueryNode(
             context.getQueryId().genPlanNodeId(),
@@ -248,7 +244,6 @@ public class LogicalPlanner {
             statement.getIdFuzzyPredicate(),
             columnHeaderList,
             null);
-    mergeNode.addChild(queryNode);
 
     SchemaPartition schemaPartition =
         statement.isIdDetermined()
@@ -260,7 +255,7 @@ public class LogicalPlanner {
       analysis.setFinishQueryAfterAnalyze();
     }
 
-    return mergeNode;
+    return queryNode;
   }
 
   private List<ColumnHeader> getColumnHeaderList(String database, String tableName) {
