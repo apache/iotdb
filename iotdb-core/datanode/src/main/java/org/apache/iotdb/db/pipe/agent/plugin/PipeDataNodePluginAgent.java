c12cf6d31e2c080e52f441c01d6b650bbd590f17
Zhenyu Luo <luoluoyuyu2001@gmail.com>
2024-07-11 16:59:14 +0800

    Pipe Plugin: Pipe Task Safe Deletion, Plugin Directory Structure Reorganization, and Isolated Pipe Plugin ClassLoader Implementation (#12868)
    Currently, IOTDB allows PipePlugins that are being used by Pipes to be deleted. When users drop a PipePlugin and then create a new PipePlugin with the same fully qualified class name, IOTDB is unable to properly load the newly created PipePlugin. The reason is that after IOTDB drops a PipePlugin, the corresponding jar file is not deleted, and the class loader behavior in loading the same fully qualified class name in the Install directory is random, as there is no unified standard, which leads to the above error.
    
    Solutions:
    
    1. **New Directory Structure**: Adopt a new directory structure `install->pluginName->jar`. The advantage of using this new directory is that the class loader does not need to load all jars. However, using this form will also not support the case where different jars reference each other. For example, if plugin A references some utility classes in the jar of plugin B, this situation will not be allowed.
    
    2. **Improve Drop Plugin Process**: For the process of dropping a plugin, it is necessary to verify whether it can be deleted during the Lock process. Therefore, it is possible to check in the Lock step whether there is a Pipe using this plugin. If there is a Pipe using this plugin, the check fails; if not, continue with the following process.
    
    3. **ClassLoader for Each Create Plugin**: Create a new ClassLoader each time a plugin is created. Each ClassLoader can only load a class with the same fully qualified name, so using the same class loader may lead to exceptions. However, the current strategy is to save the plugin's bytecode object class, and we can implement on-demand loading, that is, only load into memory when it is necessary to run the corresponding bytecode. Each plugin saves a ClassLoader, and the one needed is loaded through this classLoader.
    
    ------
    
    Co-authored-by: Steve Yurong Su <rong@apache.org>
    (cherry picked from commit 66634eab443fcd0210e97a3466e6fc0e180074f8)

--- iotdb-core/datanode/src/main/java/org/apache/iotdb/db/pipe/agent/plugin/PipeDataNodePluginAgent.java
+++ iotdb-core/datanode/src/main/java/org/apache/iotdb/db/pipe/agent/plugin/PipeDataNodePluginAgent.java
@@ -77,7 +77,7 @@ public class PipeDataNodePluginAgent {
 
       // register process from here
       checkIfRegistered(pipePluginMeta);
-      saveJarFileIfNeeded(pipePluginMeta.getJarName(), jarFile);
+      saveJarFileIfNeeded(pipePluginMeta.getPluginName(), pipePluginMeta.getJarName(), jarFile);
       doRegister(pipePluginMeta);
     } finally {
       lock.unlock();
@@ -102,7 +102,8 @@ public class PipeDataNodePluginAgent {
     }
 
     if (PipePluginExecutableManager.getInstance()
-            .hasFileUnderInstallDir(pipePluginMeta.getJarName())
+            .hasPluginFileUnderInstallDir(
+                pipePluginMeta.getPluginName(), pipePluginMeta.getJarName())
         && !PipePluginExecutableManager.getInstance().isLocalJarMatched(pipePluginMeta)) {
       String errMsg =
           String.format(
@@ -118,9 +119,11 @@ public class PipeDataNodePluginAgent {
     // we allow users to register the same pipe plugin multiple times without any error
   }
 
-  private void saveJarFileIfNeeded(String jarName, ByteBuffer byteBuffer) throws IOException {
+  private void saveJarFileIfNeeded(String pluginName, String jarName, ByteBuffer byteBuffer)
+      throws IOException {
     if (byteBuffer != null) {
-      PipePluginExecutableManager.getInstance().saveToInstallDir(byteBuffer, jarName);
+      PipePluginExecutableManager.getInstance()
+          .savePluginToInstallDir(byteBuffer, pluginName, jarName);
     }
   }
 
@@ -136,17 +139,19 @@ public class PipeDataNodePluginAgent {
     final String className = pipePluginMeta.getClassName();
 
     try {
-      final PipePluginClassLoader currentActiveClassLoader =
-          PipePluginClassLoaderManager.getInstance().updateAndGetActiveClassLoader();
-      updateAllRegisteredClasses(currentActiveClassLoader);
+      PipePluginClassLoaderManager classLoaderManager = PipePluginClassLoaderManager.getInstance();
+      String pluginDirPath =
+          PipePluginExecutableManager.getInstance().getPluginsDirPath(pluginName);
+      final PipePluginClassLoader pipePluginClassLoader =
+          classLoaderManager.createPipePluginClassLoader(pluginDirPath);
 
-      final Class<?> pluginClass = Class.forName(className, true, currentActiveClassLoader);
+      final Class<?> pluginClass = Class.forName(className, true, pipePluginClassLoader);
 
       @SuppressWarnings("unused") // ensure that it is a PipePlugin class
       final PipePlugin ignored = (PipePlugin) pluginClass.getDeclaredConstructor().newInstance();
 
       pipePluginMetaKeeper.addPipePluginMeta(pluginName, pipePluginMeta);
-      pipePluginMetaKeeper.addPluginAndClass(pluginName, pluginClass);
+      classLoaderManager.addPluginAndClassLoader(pluginName, pipePluginClassLoader);
     } catch (IOException
         | InstantiationException
         | InvocationTargetException
@@ -164,13 +169,6 @@ public class PipeDataNodePluginAgent {
     }
   }
 
-  private void updateAllRegisteredClasses(PipePluginClassLoader activeClassLoader)
-      throws ClassNotFoundException {
-    for (PipePluginMeta information : pipePluginMetaKeeper.getAllPipePluginMeta()) {
-      pipePluginMetaKeeper.updatePluginClass(information, activeClassLoader);
-    }
-  }
-
   public void deregister(String pluginName, boolean needToDeleteJar) throws PipeException {
     lock.lock();
     try {
@@ -185,11 +183,12 @@ public class PipeDataNodePluginAgent {
 
       // remove anyway
       pipePluginMetaKeeper.removePipePluginMeta(pluginName);
-      pipePluginMetaKeeper.removePluginClass(pluginName);
+      PipePluginClassLoaderManager.getInstance().removePluginClassLoader(pluginName);
 
       // if it is needed to delete jar file of the pipe plugin, delete both jar file and md5
       if (information != null && needToDeleteJar) {
-        PipePluginExecutableManager.getInstance().removeFileUnderLibRoot(information.getJarName());
+        PipePluginExecutableManager.getInstance()
+            .removePluginFileUnderLibRoot(information.getPluginName(), information.getJarName());
         PipePluginExecutableManager.getInstance()
             .removeFileUnderTemporaryRoot(pluginName.toUpperCase() + ".txt");
       }
