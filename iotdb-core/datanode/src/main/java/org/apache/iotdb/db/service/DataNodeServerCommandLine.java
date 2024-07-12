d84aa58fcb4ddaad5622da1ebeb70f37baaf903e
Christofer Dutz <cdutz@apache.org>
2024-07-12 10:22:11 +0200

    feat: Implement support for deleting a node by id.

--- iotdb-core/datanode/src/main/java/org/apache/iotdb/db/service/DataNodeServerCommandLine.java
+++ iotdb-core/datanode/src/main/java/org/apache/iotdb/db/service/DataNodeServerCommandLine.java
@@ -25,23 +25,23 @@ import org.apache.iotdb.commons.ServerCommandLine;
 import org.apache.iotdb.commons.client.exception.ClientManagerException;
 import org.apache.iotdb.commons.exception.BadNodeUrlException;
 import org.apache.iotdb.commons.exception.IoTDBException;
-import org.apache.iotdb.commons.utils.NodeUrlUtils;
 import org.apache.iotdb.confignode.rpc.thrift.TDataNodeRemoveReq;
 import org.apache.iotdb.confignode.rpc.thrift.TDataNodeRemoveResp;
 import org.apache.iotdb.db.protocol.client.ConfigNodeClient;
 import org.apache.iotdb.db.protocol.client.ConfigNodeClientManager;
 import org.apache.iotdb.db.protocol.client.ConfigNodeInfo;
 import org.apache.iotdb.rpc.TSStatusCode;
+import org.apache.iotdb.rpc.UrlUtils;
 
+import org.apache.commons.lang3.math.NumberUtils;
 import org.apache.thrift.TException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
-
-import static org.apache.commons.lang3.StringUtils.isNumeric;
 
 public class DataNodeServerCommandLine extends ServerCommandLine {
 
@@ -146,46 +146,72 @@ public class DataNodeServerCommandLine extends ServerCommandLine {
     }
 
     // Below supports multiple datanode deletion, split by ',', and is reserved for extension
-    try {
-      List<TEndPoint> endPoints = NodeUrlUtils.parseTEndPointUrls(args);
-      try (ConfigNodeClient client =
-          ConfigNodeClientManager.getInstance().borrowClient(ConfigNodeInfo.CONFIG_REGION_ID)) {
-        dataNodeLocations =
-            client.getDataNodeConfiguration(-1).getDataNodeConfigurationMap().values().stream()
-                .map(TDataNodeConfiguration::getLocation)
-                .filter(location -> endPoints.contains(location.getClientRpcEndPoint()))
-                .collect(Collectors.toList());
-      } catch (TException | ClientManagerException e) {
-        LOGGER.error("Get data node locations failed", e);
-      }
-    } catch (BadNodeUrlException e) {
-      try (ConfigNodeClient client =
-          ConfigNodeClientManager.getInstance().borrowClient(ConfigNodeInfo.CONFIG_REGION_ID)) {
-        for (String id : args.split(",")) {
-          if (!isNumeric(id)) {
-            LOGGER.warn("Incorrect id format {}, skipped...", id);
-            continue;
-          }
-          List<TDataNodeLocation> nodeLocationResult =
-              client
-                  .getDataNodeConfiguration(Integer.parseInt(id))
-                  .getDataNodeConfigurationMap()
-                  .values()
-                  .stream()
-                  .map(TDataNodeConfiguration::getLocation)
-                  .collect(Collectors.toList());
-          if (nodeLocationResult.isEmpty()) {
-            LOGGER.warn("DataNode {} is not in cluster, skipped...", id);
-            continue;
-          }
-          if (!dataNodeLocations.contains(nodeLocationResult.get(0))) {
-            dataNodeLocations.add(nodeLocationResult.get(0));
-          }
-        }
-      } catch (TException | ClientManagerException e1) {
-        LOGGER.error("Get data node locations failed", e);
+    List<NodeCoordinate> endPoints = parseCoordinates(args);
+    try (ConfigNodeClient client =
+        ConfigNodeClientManager.getInstance().borrowClient(ConfigNodeInfo.CONFIG_REGION_ID)) {
+      dataNodeLocations =
+          client.getDataNodeConfiguration(-1).getDataNodeConfigurationMap().values().stream()
+              .map(TDataNodeConfiguration::getLocation)
+              .filter(
+                  location ->
+                      endPoints.stream()
+                          .anyMatch(nodeCoordinate -> nodeCoordinate.matches(location)))
+              .collect(Collectors.toList());
+    } catch (TException | ClientManagerException e) {
+      LOGGER.error("Get data node locations failed", e);
+    }
+
+    return dataNodeLocations;
+  }
+
+  protected List<NodeCoordinate> parseCoordinates(String coordinatesString) {
+    if (coordinatesString == null) {
+      return Collections.emptyList();
+    }
+
+    String[] coordinateStrings = coordinatesString.split(":");
+    List<NodeCoordinate> coordinates = new ArrayList<>(coordinateStrings.length);
+    for (String coordinate : coordinateStrings) {
+      if (coordinate.contains(":")) {
+        coordinates.add(new NodeCoordinateIP(UrlUtils.parseTEndPointIpv4AndIpv6Url(coordinate)));
+      } else if (NumberUtils.isCreatable(coordinate)) {
+        coordinates.add(new NodeCoordinateNodeId(Integer.parseInt(coordinate)));
       }
     }
-    return dataNodeLocations;
+    return coordinates;
+  }
+
+  protected interface NodeCoordinate {
+    // Returns true if the given location matches this coordinate
+    boolean matches(TDataNodeLocation location);
+  }
+
+  /** Implementation of a NodeCoordinate that uses ip and port to match. */
+  protected static class NodeCoordinateIP implements NodeCoordinate {
+    private final TEndPoint endPoint;
+
+    public NodeCoordinateIP(TEndPoint endPoint) {
+      this.endPoint = endPoint;
+    }
+
+    @Override
+    public boolean matches(TDataNodeLocation location) {
+      return endPoint.ip.equals(location.getClientRpcEndPoint().getIp())
+          && endPoint.port == location.getClientRpcEndPoint().getPort();
+    }
+  }
+
+  /** Implementation of a NodeCoordinate that uses the node id to match. */
+  protected static class NodeCoordinateNodeId implements NodeCoordinate {
+    private final int nodeId;
+
+    public NodeCoordinateNodeId(int nodeId) {
+      this.nodeId = nodeId;
+    }
+
+    @Override
+    public boolean matches(TDataNodeLocation location) {
+      return location.isSetDataNodeId() && location.dataNodeId == nodeId;
+    }
   }
 }
