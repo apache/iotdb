21a8a77a3013fe812cf5bc7c5ce2bd68417f4dc7
shuwenwei <55970239+shuwenwei@users.noreply.github.com>
2024-07-12 16:54:37 +0800

    Insertion compaction may cause new data lost (#12908)
    * fix insertion compaction
    
    * fix is valid check
    
    * fix ut
    
    * add comment
    
    (cherry picked from commit e0222284300f840a3bd95ebb101db30638af73b1)

--- iotdb-core/datanode/src/main/java/org/apache/iotdb/db/storageengine/dataregion/tsfile/TsFileResource.java
+++ iotdb-core/datanode/src/main/java/org/apache/iotdb/db/storageengine/dataregion/tsfile/TsFileResource.java
@@ -31,6 +31,7 @@ import org.apache.iotdb.db.conf.IoTDBDescriptor;
 import org.apache.iotdb.db.exception.PartitionViolationException;
 import org.apache.iotdb.db.schemaengine.schemaregion.utils.ResourceByPathUtils;
 import org.apache.iotdb.db.storageengine.dataregion.DataRegion;
+import org.apache.iotdb.db.storageengine.dataregion.compaction.selector.utils.InsertionCompactionCandidateStatus;
 import org.apache.iotdb.db.storageengine.dataregion.memtable.ReadOnlyMemChunk;
 import org.apache.iotdb.db.storageengine.dataregion.memtable.TsFileProcessor;
 import org.apache.iotdb.db.storageengine.dataregion.modification.ModificationFile;
@@ -162,7 +163,8 @@ public class TsFileResource {
 
   private ProgressIndex maxProgressIndex;
 
-  private boolean isInsertionCompactionTaskCandidate = true;
+  private InsertionCompactionCandidateStatus insertionCompactionCandidateStatus =
+      InsertionCompactionCandidateStatus.NOT_CHECKED;
 
   @TestOnly
   public TsFileResource() {
@@ -1136,10 +1138,15 @@ public class TsFileResource {
   }
 
   public boolean isInsertionCompactionTaskCandidate() {
-    return !isSeq && isInsertionCompactionTaskCandidate;
+    return !isSeq
+        && insertionCompactionCandidateStatus != InsertionCompactionCandidateStatus.NOT_VALID;
   }
 
-  public void setInsertionCompactionTaskCandidate(boolean insertionCompactionTaskCandidate) {
-    isInsertionCompactionTaskCandidate = insertionCompactionTaskCandidate;
+  public InsertionCompactionCandidateStatus getInsertionCompactionCandidateStatus() {
+    return insertionCompactionCandidateStatus;
+  }
+
+  public void setInsertionCompactionTaskCandidate(InsertionCompactionCandidateStatus status) {
+    insertionCompactionCandidateStatus = status;
   }
 }
