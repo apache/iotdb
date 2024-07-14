6e5f82d318bdedee8a0aaf3475ce8c4553c948fa
Lei Rui <1010953854@qq.com>
2024-07-14 10:11:23 +0800

    add

--- server/src/main/java/org/apache/iotdb/db/query/dataset/groupby/GroupByWithoutValueFilterDataSet.java
+++ server/src/main/java/org/apache/iotdb/db/query/dataset/groupby/GroupByWithoutValueFilterDataSet.java
@@ -441,9 +441,6 @@ public class GroupByWithoutValueFilterDataSet extends GroupByEngineDataSet {
     } else if (CONFIG.getEnableTri().equals("SC")) {
       return new LocalGroupByExecutorTri_SC(
           path, allSensors, dataType, context, timeFilter, fileFilter, ascending);
-    } else if (CONFIG.getEnableTri().equals("Visval")) {
-      return new LocalGroupByExecutorTri_Visval(
-          path, allSensors, dataType, context, timeFilter, fileFilter, ascending);
     } else {
       logger.info("No matched enable_tri!");
       return new LocalGroupByExecutor(
