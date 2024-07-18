83116396ae76c0040f064c53f1da3d3fbec90323
Peng Junzhi <78788603+Pengzna@users.noreply.github.com>
2024-07-18 01:13:42 -0500

    PipeConsensus: Fix circular replicate in kill -9/restart cases (#12937)

--- iotdb-core/datanode/src/main/java/org/apache/iotdb/db/storageengine/dataregion/tsfile/TsFileResource.java
+++ iotdb-core/datanode/src/main/java/org/apache/iotdb/db/storageengine/dataregion/tsfile/TsFileResource.java
@@ -161,6 +161,9 @@ public class TsFileResource {
 
   private ProgressIndex maxProgressIndex;
 
+  /** used to prevent circular replication in PipeConsensus */
+  private boolean isGeneratedByPipeConsensus = false;
+
   private InsertionCompactionCandidateStatus insertionCompactionCandidateStatus =
       InsertionCompactionCandidateStatus.NOT_CHECKED;
 
@@ -508,6 +511,14 @@ public class TsFileResource {
 
   public TsFileProcessor getProcessor() {
     return processor;
+  }
+
+  public boolean isGeneratedByPipeConsensus() {
+    return isGeneratedByPipeConsensus;
+  }
+
+  public void setGeneratedByPipeConsensus(boolean generatedByPipeConsensus) {
+    isGeneratedByPipeConsensus = generatedByPipeConsensus;
   }
 
   public void writeLock() {
