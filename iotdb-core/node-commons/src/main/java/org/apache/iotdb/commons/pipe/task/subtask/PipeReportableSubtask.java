a94dbd55e6f07cd19aab668a322b0ad18dc72ac8
Peng Junzhi <201250214@smail.nju.edu.cn>
2024-07-22 16:32:01 +0800

    spotless

--- iotdb-core/node-commons/src/main/java/org/apache/iotdb/commons/pipe/task/subtask/PipeReportableSubtask.java
+++ iotdb-core/node-commons/src/main/java/org/apache/iotdb/commons/pipe/task/subtask/PipeReportableSubtask.java
@@ -19,7 +19,6 @@
 
 package org.apache.iotdb.commons.pipe.task.subtask;
 
-import org.apache.iotdb.commons.exception.pipe.PipeRuntimeConnectorCriticalException;
 import org.apache.iotdb.commons.exception.pipe.PipeRuntimeConnectorRetryTimesConfigurableException;
 import org.apache.iotdb.commons.exception.pipe.PipeRuntimeCriticalException;
 import org.apache.iotdb.commons.exception.pipe.PipeRuntimeException;
@@ -54,18 +53,6 @@ public abstract class PipeReportableSubtask extends PipeSubtask {
     // Because we need to keep it for the next retry. If user wants to restart the task,
     // the last event will be processed again. The last event will be released when the task
     // is dropped or the process is running normally.
-  }
-
-  public static void main(String[] args) {
-    PipeRuntimeConnectorRetryTimesConfigurableException test =
-        new PipeRuntimeConnectorRetryTimesConfigurableException("test", Integer.MAX_VALUE);
-    final int maxRetryTimes =
-        test instanceof PipeRuntimeConnectorRetryTimesConfigurableException
-            ? ((PipeRuntimeConnectorRetryTimesConfigurableException) test).getRetryTimes()
-            : MAX_RETRY_TIMES;
-    if (test instanceof PipeRuntimeConnectorCriticalException) {
-      System.out.println(maxRetryTimes);
-    }
   }
 
   private void onEnrichedEventFailure(final Throwable throwable) {
