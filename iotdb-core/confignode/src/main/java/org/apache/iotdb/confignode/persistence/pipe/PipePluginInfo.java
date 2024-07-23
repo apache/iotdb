2ba0cf6bf63f94b4fdfc5186768fff1a55fe51fc
luoluoyuyu <zhenyu@apache.org>
2024-07-23 21:00:14 +0800

    Modify variable name

--- iotdb-core/confignode/src/main/java/org/apache/iotdb/confignode/persistence/pipe/PipePluginInfo.java
+++ iotdb-core/confignode/src/main/java/org/apache/iotdb/confignode/persistence/pipe/PipePluginInfo.java
@@ -243,8 +243,8 @@ public class PipePluginInfo implements SnapshotProcessor {
 
         String jarPath = manager.getPluginInstallPathV2(pluginName, jarName);
 
-        boolean needFlush = !Files.exists(Paths.get(jarPath));
-        if (needFlush) {
+        boolean isJarExistedInV2Dir = !Files.exists(Paths.get(jarPath));
+        if (isJarExistedInV2Dir) {
           jarPath = manager.getPluginInstallPathV1(jarName);
         }
 
@@ -253,7 +253,7 @@ public class PipePluginInfo implements SnapshotProcessor {
         }
 
         ByteBuffer byteBuffer = ExecutableManager.transferToBytebuffer(jarPath);
-        if (needFlush) {
+        if (isJarExistedInV2Dir) {
           pipePluginExecutableManager.savePluginToInstallDir(
               byteBuffer.duplicate(), pluginName, jarName);
         }
