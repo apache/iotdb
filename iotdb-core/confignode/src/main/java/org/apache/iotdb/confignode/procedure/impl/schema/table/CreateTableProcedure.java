d834eea75732e10b47f14b2ba13b58f54b3c823a
Caideyipi <87789683+Caideyipi@users.noreply.github.com>
2024-07-29 18:23:27 +0800

    Revert "Update CreateTableProcedure.java"
    This reverts commit d599a09f94c3c7841f8611a497f0a2a40dac7360.

--- iotdb-core/confignode/src/main/java/org/apache/iotdb/confignode/procedure/impl/schema/table/CreateTableProcedure.java
+++ iotdb-core/confignode/src/main/java/org/apache/iotdb/confignode/procedure/impl/schema/table/CreateTableProcedure.java
@@ -336,13 +336,15 @@ public class CreateTableProcedure
       throws IOException, InterruptedException, ProcedureException {
     final long startTime = System.currentTimeMillis();
     try {
-      if (state == CreateTableState.PRE_RELEASE) {
-        // There is no need to rollback at the preCreate state
-        // Because if preCreate fails, ratis and preCreate impl will guarantee that
-        // The state machine's state won't be changed
-        LOGGER.info("Start rollback pre release table {}.{}", database, table.getTableName());
-        rollbackCreate(env);
-        rollbackPreRelease(env);
+      switch (state) {
+        case PRE_CREATE:
+          LOGGER.info("Start rollback pre create table {}.{}", database, table.getTableName());
+          rollbackCreate(env);
+          break;
+        case PRE_RELEASE:
+          LOGGER.info("Start rollback pre release table {}.{}", database, table.getTableName());
+          rollbackPreRelease(env);
+          break;
       }
     } finally {
       LOGGER.info(
