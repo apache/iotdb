dbd468bcf7022b44386968c3258f9b861e52ec5f
V_Galaxy <vgalaxies@apache.org>
2024-07-29 12:19:34 +0800

    Subscription: fix unexpected cancellation of workers during consumer startup & optimize server-side subscription logs & add synchronized modifier (#13032)
    (cherry picked from commit 99769d1a6a0a9e4aec5b5f4f0cd3d486b0d628c7)

--- iotdb-core/datanode/src/main/java/org/apache/iotdb/db/subscription/agent/SubscriptionBrokerAgent.java
+++ iotdb-core/datanode/src/main/java/org/apache/iotdb/db/subscription/agent/SubscriptionBrokerAgent.java
@@ -116,6 +116,7 @@ public class SubscriptionBrokerAgent {
   public void createBroker(final String consumerGroupId) {
     final SubscriptionBroker broker = new SubscriptionBroker(consumerGroupId);
     consumerGroupIdToSubscriptionBroker.put(consumerGroupId, broker);
+    LOGGER.info("Subscription: create broker bound to consumer group [{}]", consumerGroupId);
   }
 
   /**
@@ -140,6 +141,7 @@ public class SubscriptionBrokerAgent {
       return false;
     }
     consumerGroupIdToSubscriptionBroker.remove(consumerGroupId);
+    LOGGER.info("Subscription: drop broker bound to consumer group [{}]", consumerGroupId);
     return true;
   }
 
