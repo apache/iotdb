73b00338e3f16e5a5d9da819b75d188a83ebb7f0
shuwenwei <s13979062948@gmail.com>
2024-08-14 18:42:34 +0800

    add ut

--- iotdb-core/datanode/src/test/java/org/apache/iotdb/db/storageengine/dataregion/compaction/utils/MergeReaderPriorityTest.java
+++ iotdb-core/datanode/src/test/java/org/apache/iotdb/db/storageengine/dataregion/compaction/utils/MergeReaderPriorityTest.java
@@ -19,4 +19,78 @@
 
 package org.apache.iotdb.db.storageengine.dataregion.compaction.utils;
 
-public class MergeReaderPriorityTest {}
+import org.apache.iotdb.db.storageengine.dataregion.read.reader.common.MergeReaderPriority;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.PriorityQueue;
+
+public class MergeReaderPriorityTest {
+  @Test
+  public void test() {
+    List<MergeReaderPriority> priorities = new ArrayList<>();
+    // Create MergeReaderPriority objects with different attributes
+    priorities.add(new MergeReaderPriority(1000L, 2L, 10L, true));
+    priorities.add(new MergeReaderPriority(1000L, 2L, 5L, true));
+    priorities.add(new MergeReaderPriority(1000L, 1L, 10L, true));
+    priorities.add(new MergeReaderPriority(1000L, 3L, 15L, false));
+    priorities.add(new MergeReaderPriority(1000L, 2L, 15L, false));
+
+    // Add these objects to a PriorityQueue with the comparison logic
+    PriorityQueue<MergeReaderPriority> queue = new PriorityQueue<>(priorities);
+
+    // Test the order of elements in the queue
+    Assert.assertEquals(queue.poll(), priorities.get(2)); // seq, lowest version
+    Assert.assertEquals(queue.poll(), priorities.get(1)); // seq, same version, lower offset
+    Assert.assertEquals(queue.poll(), priorities.get(0)); // seq, higher version, higher offset
+    Assert.assertEquals(queue.poll(), priorities.get(4)); // unseq, lower version, higher offset
+    Assert.assertEquals(queue.poll(), priorities.get(3)); // unseq, highest version, highest offset
+  }
+
+  @Test
+  public void testCompactionQueue() {
+    List<PriorityPoint> priorities = new ArrayList<>();
+    // Create MergeReaderPriority objects with different attributes
+
+    // Add these objects to a PriorityQueue with the comparison logic
+    PriorityQueue<PriorityPoint> queue =
+        new PriorityQueue<>(
+            (o1, o2) -> {
+              int timeCompare = Long.compare(o1.time, o2.time);
+              return timeCompare != 0 ? timeCompare : o2.priority.compareTo(o1.priority);
+            });
+    priorities.add(new PriorityPoint(1, new MergeReaderPriority(1000L, 2L, 10L, true)));
+    priorities.add(new PriorityPoint(1, new MergeReaderPriority(1000L, 2L, 5L, true)));
+    priorities.add(new PriorityPoint(1, new MergeReaderPriority(1000L, 1L, 10L, true)));
+    priorities.add(new PriorityPoint(1, new MergeReaderPriority(1000L, 3L, 15L, false)));
+    priorities.add(new PriorityPoint(1, new MergeReaderPriority(1000L, 2L, 15L, false)));
+    priorities.add(new PriorityPoint(11, new MergeReaderPriority(1000L, 2L, 10L, true)));
+    queue.addAll(priorities);
+
+    // Test the order of elements in the queue
+    Assert.assertEquals(queue.poll(), priorities.get(3)); // unseq, highest version, highest offset
+    Assert.assertEquals(queue.poll(), priorities.get(4)); // unseq, lower version, higher offset
+    Assert.assertEquals(queue.poll(), priorities.get(0)); // seq, higher version, higher offset
+    Assert.assertEquals(queue.poll(), priorities.get(1)); // seq, same version, lower offset
+    Assert.assertEquals(queue.poll(), priorities.get(2)); // seq, lowest version
+    Assert.assertEquals(queue.poll(), priorities.get(5)); // largest timestamp
+  }
+
+  private static class PriorityPoint {
+    long time;
+    MergeReaderPriority priority;
+
+    public PriorityPoint(long time, MergeReaderPriority priority) {
+      this.time = time;
+      this.priority = priority;
+    }
+
+    @Override
+    public String toString() {
+      return "PriorityPoint{" + "time=" + time + ", priority=" + priority + '}';
+    }
+  }
+}
