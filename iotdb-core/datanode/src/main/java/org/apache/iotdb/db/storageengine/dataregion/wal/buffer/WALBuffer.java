d876131fd79263bcd6b5dd822d5b559cbc733fbc
Peng Junzhi <201250214@smail.nju.edu.cn>
2024-08-30 09:34:33 +0800

    minor fix WAL close time

--- iotdb-core/datanode/src/main/java/org/apache/iotdb/db/storageengine/dataregion/wal/buffer/WALBuffer.java
+++ iotdb-core/datanode/src/main/java/org/apache/iotdb/db/storageengine/dataregion/wal/buffer/WALBuffer.java
@@ -669,6 +669,7 @@ public class WALBuffer extends AbstractWALBuffer {
   @Override
   public void close() {
     // first waiting serialize and sync tasks finished, then release all resources
+    isClosed = true;
     if (serializeThread != null) {
       // add close signal WALEntry to notify serializeThread
       try {
@@ -677,7 +678,6 @@ public class WALBuffer extends AbstractWALBuffer {
         logger.error("Fail to put CLOSE_SIGNAL to walEntries.", e);
         Thread.currentThread().interrupt();
       }
-      isClosed = true;
       shutdownThread(serializeThread, ThreadName.WAL_SERIALIZE);
     }
     if (syncBufferThread != null) {
