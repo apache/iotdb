7e331a0f2fe7b572ef03441a6e98661552e4645b
Caideyipi <87789683+Caideyipi@users.noreply.github.com>
2024-08-30 17:55:22 +0800

    Update TableDeviceSchemaCache.java

--- iotdb-core/datanode/src/main/java/org/apache/iotdb/db/queryengine/plan/relational/metadata/fetcher/cache/TableDeviceSchemaCache.java
+++ iotdb-core/datanode/src/main/java/org/apache/iotdb/db/queryengine/plan/relational/metadata/fetcher/cache/TableDeviceSchemaCache.java
@@ -196,10 +196,9 @@ public class TableDeviceSchemaCache {
   }
 
   /**
-   * Update the last cache in writing. The input {@link TimeValuePair} shall never be or contain
-   * {@code null}.If a measurement is with all {@code null}s or is an id/attribute column, its
-   * timeValuePair shall be {@link TableDeviceLastCache#EMPTY_TIME_VALUE_PAIR}. For correctness,
-   * this will put the cache lazily and only update the existing last caches of measurements.
+   * Update the last cache in writing. If a measurement is with all {@code null}s or is an
+   * id/attribute column, its timeValuePair shall be {@code null}. For correctness, this will put
+   * the cache lazily and only update the existing last caches of measurements.
    *
    * @param database the device's database, without "root"
    * @param deviceId IDeviceID
