d2ffea3fe4c5fd83cac6bbe1c7b2b4c9acfdaec1
Jiang Tian <jt2594838@163.com>
2024-09-03 10:43:04 +0800

    [To: rc/1.3.3]Fix WAL Exception catch & support new DataTypes in QueryDataSetUtils (#13375)
    * Fix WAL Exception catch & support new DataTypes in QueryDataSetUtils
    
    * fix merge conflict
    
    * fix review
    
    (cherry picked from commit 07fd6ed42e1ab5b773e34df354f9c6c610355c7f)
    
    * fix review

--- iotdb-core/datanode/src/main/java/org/apache/iotdb/db/queryengine/execution/aggregation/SumAccumulator.java
+++ iotdb-core/datanode/src/main/java/org/apache/iotdb/db/queryengine/execution/aggregation/SumAccumulator.java
@@ -56,7 +56,11 @@ public class SumAccumulator implements Accumulator {
         addDoubleInput(columns, bitMap);
         return;
       case TEXT:
+      case BLOB:
       case BOOLEAN:
+      case TIMESTAMP:
+      case DATE:
+      case STRING:
       default:
         throw new UnSupportedDataTypeException(
             String.format("Unsupported data type in aggregation AVG : %s", seriesDataType));
