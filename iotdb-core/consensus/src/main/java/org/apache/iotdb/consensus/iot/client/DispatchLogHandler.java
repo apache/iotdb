c44fb684fc046de14d350074085118448541ec87
Xiangpeng Hu <hxp23@mails.tsinghua.edu.cn>
2024-09-03 21:08:02 +0800

    enhance iot retry

--- iotdb-core/consensus/src/main/java/org/apache/iotdb/consensus/iot/client/DispatchLogHandler.java
+++ iotdb-core/consensus/src/main/java/org/apache/iotdb/consensus/iot/client/DispatchLogHandler.java
@@ -80,9 +80,7 @@ public class DispatchLogHandler implements AsyncMethodCallback<TSyncLogEntriesRe
   }
 
   public static boolean needRetry(int statusCode) {
-    return statusCode == TSStatusCode.INTERNAL_SERVER_ERROR.getStatusCode()
-        || statusCode == TSStatusCode.SYSTEM_READ_ONLY.getStatusCode()
-        || statusCode == TSStatusCode.WRITE_PROCESS_REJECT.getStatusCode();
+    return statusCode != TSStatusCode.SUCCESS_STATUS.getStatusCode();
   }
 
   @Override
