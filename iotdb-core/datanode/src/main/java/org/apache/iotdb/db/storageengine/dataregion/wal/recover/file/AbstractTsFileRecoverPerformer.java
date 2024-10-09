e65203e9c47e9d2011d7ce192c42cf917eebb196
Tian Jiang <jt2594838@163.com>
2024-10-09 10:56:55 +0800

    fix that recovery may generate TsFile with V3 head and V4 tail

--- iotdb-core/datanode/src/main/java/org/apache/iotdb/db/storageengine/dataregion/wal/recover/file/AbstractTsFileRecoverPerformer.java
+++ iotdb-core/datanode/src/main/java/org/apache/iotdb/db/storageengine/dataregion/wal/recover/file/AbstractTsFileRecoverPerformer.java
@@ -25,7 +25,6 @@ import org.apache.iotdb.db.storageengine.dataregion.tsfile.TsFileResource;
 import org.apache.iotdb.db.storageengine.dataregion.utils.TsFileResourceUtils;
 
 import org.apache.commons.io.FileUtils;
-import org.apache.tsfile.exception.NotCompatibleTsFileException;
 import org.apache.tsfile.read.TsFileSequenceReader;
 import org.apache.tsfile.write.writer.RestorableTsFileIOWriter;
 import org.apache.tsfile.write.writer.TsFileIOWriter;
@@ -35,7 +34,6 @@ import org.slf4j.LoggerFactory;
 import java.io.Closeable;
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
 
 /** This class is used to help recover TsFile. */
 public abstract class AbstractTsFileRecoverPerformer implements Closeable {
@@ -79,31 +77,10 @@ public abstract class AbstractTsFileRecoverPerformer implements Closeable {
       return;
     }
 
-    // try to remove corrupted part of the TsFile
-    try {
-      writer = new RestorableTsFileIOWriter(tsFile);
-    } catch (NotCompatibleTsFileException e) {
-      boolean result = tsFile.delete();
-      logger.warn(
-          "TsFile {} is incompatible. Try to delete it and delete result is {}", tsFile, result);
-      // if the broken TsFile is v3, we can recover the all data from wal
-      // to support it, we can regenerate an empty file here
-      Files.createFile(tsFile.toPath());
-      writer = new RestorableTsFileIOWriter(tsFile);
-      throw new DataRegionException(e);
-    } catch (IOException e) {
-      throw new DataRegionException(e);
-    }
-
-    // reconstruct .resource file when TsFile is complete
-    if (!writer.hasCrashed()) {
-      try {
-        reconstructResourceFile();
-      } catch (IOException e) {
-        throw new DataRegionException(
-            "Failed recover the resource file: " + tsFile + TsFileResource.RESOURCE_SUFFIX + e);
-      }
-    }
+    // If the resource does not exist, then the flush is not complete and the associated WAL should
+    // all exist. We can simply start a new file and recover from WAL.
+    tsFile.delete();
+    writer = new RestorableTsFileIOWriter(tsFile);
   }
 
   private void loadResourceFile() throws IOException {
