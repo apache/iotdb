e09a98b4a0b0ffb4aaa2670abf1c84d3b99f2af2
Li Yu Heng <liyuheng55555@126.com>
2024-10-09 12:00:39 +0800

    Fix the order of stop schema region and clear schema engine #13678

--- iotdb-core/datanode/src/main/java/org/apache/iotdb/db/service/DataNode.java
+++ iotdb-core/datanode/src/main/java/org/apache/iotdb/db/service/DataNode.java
@@ -1193,7 +1193,6 @@ public class DataNode extends ServerCommandLine implements DataNodeMBean {
     stopTriggerRelatedServices();
     registerManager.deregisterAll();
     JMXService.deregisterMBean(mbeanName);
-    SchemaEngine.getInstance().clear();
     MetricService.getInstance().stop();
     if (schemaRegionConsensusStarted) {
       try {
@@ -1202,6 +1201,7 @@ public class DataNode extends ServerCommandLine implements DataNodeMBean {
         logger.warn("Exception during SchemaRegionConsensusImpl stopping", e);
       }
     }
+    SchemaEngine.getInstance().clear();
     if (dataRegionConsensusStarted) {
       try {
         DataRegionConsensusImpl.getInstance().stop();
