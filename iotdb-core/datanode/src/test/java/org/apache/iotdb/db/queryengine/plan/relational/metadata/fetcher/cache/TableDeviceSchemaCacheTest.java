9069e82c50f7d66241923e3a9a80af6dc462e449
Caideyipi <87789683+Caideyipi@users.noreply.github.com>
2024-10-11 12:11:40 +0800

    Update TableDeviceSchemaCacheTest.java

--- iotdb-core/datanode/src/test/java/org/apache/iotdb/db/queryengine/plan/relational/metadata/fetcher/cache/TableDeviceSchemaCacheTest.java
+++ iotdb-core/datanode/src/test/java/org/apache/iotdb/db/queryengine/plan/relational/metadata/fetcher/cache/TableDeviceSchemaCacheTest.java
@@ -66,6 +66,7 @@ public class TableDeviceSchemaCacheTest {
 
   @After
   public void rollback() {
+    TableDeviceSchemaCache.getInstance().invalidateAll();
     config.setAllocateMemoryForSchemaCache(originMemConfig);
   }
 
