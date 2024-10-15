cb236a23dbdd442190882f0cbff1b1ccfcab7cdf
shuwenwei <s13979062948@gmail.com>
2024-10-15 16:04:37 +0800

    fix bug

--- iotdb-core/datanode/src/main/java/org/apache/iotdb/db/storageengine/dataregion/compaction/execute/utils/executor/batch/BatchedReadChunkAlignedSeriesCompactionExecutor.java
+++ iotdb-core/datanode/src/main/java/org/apache/iotdb/db/storageengine/dataregion/compaction/execute/utils/executor/batch/BatchedReadChunkAlignedSeriesCompactionExecutor.java
@@ -328,7 +328,7 @@ public class BatchedReadChunkAlignedSeriesCompactionExecutor
     @Override
     protected ChunkLoader getChunkLoader(TsFileSequenceReader reader, ChunkMetadata chunkMetadata)
         throws IOException {
-      if (chunkMetadata.getMeasurementUid().isEmpty()) {
+      if (chunkMetadata != null && chunkMetadata.getMeasurementUid().isEmpty()) {
         Chunk timeChunk = batchCompactionPlan.getTimeChunkFromCache(reader, chunkMetadata);
         return new InstantChunkLoader(reader.getFileName(), chunkMetadata, timeChunk);
       }
