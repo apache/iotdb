efab4d26a280ef73764635d5692e5a5ff31b8312
VGalaxies <vgalaxies@apache.org>
2024-10-16 22:13:21 +0800

    setup

--- iotdb-core/node-commons/src/main/java/org/apache/iotdb/commons/pipe/config/constant/PipeExtractorConstant.java
+++ iotdb-core/node-commons/src/main/java/org/apache/iotdb/commons/pipe/config/constant/PipeExtractorConstant.java
@@ -101,6 +101,16 @@ public class PipeExtractorConstant {
   public static final String EXTRACTOR_REALTIME_LOOSE_RANGE_ALL_VALUE = "all";
   public static final String EXTRACTOR_REALTIME_LOOSE_RANGE_DEFAULT_VALUE = "";
 
+  public static final String EXTRACTOR_MODE_STREAMING_KEY = "extractor.mode.streaming";
+  public static final String SOURCE_MODE_STREAMING_KEY = "source.mode.streaming";
+  public static final boolean EXTRACTOR_MODE_STREAMING_DEFAULT_VALUE = true;
+  public static final String EXTRACTOR_MODE_STRICT_KEY = "extractor.mode.strict";
+  public static final String SOURCE_MODE_STRICT_KEY = "source.mode.strict";
+  public static final boolean EXTRACTOR_MODE_STRICT_DEFAULT_VALUE = true;
+  public static final String EXTRACTOR_MODE_SNAPSHOT_KEY = "extractor.mode.snapshot";
+  public static final String SOURCE_MODE_SNAPSHOT_KEY = "source.mode.snapshot";
+  public static final boolean EXTRACTOR_MODE_SNAPSHOT_DEFAULT_VALUE = false;
+
   public static final String EXTRACTOR_START_TIME_KEY = "extractor.start-time";
   public static final String SOURCE_START_TIME_KEY = "source.start-time";
   public static final String EXTRACTOR_END_TIME_KEY = "extractor.end-time";
@@ -111,6 +121,7 @@ public class PipeExtractorConstant {
   public static final long EXTRACTOR_WATERMARK_INTERVAL_DEFAULT_VALUE = -1; // -1 means no watermark
 
   ///////////////////// pipe consensus /////////////////////
+
   public static final String EXTRACTOR_CONSENSUS_GROUP_ID_KEY = "extractor.consensus.group-id";
   public static final String EXTRACTOR_CONSENSUS_SENDER_DATANODE_ID_KEY =
       "extractor.consensus.sender-dn-id";
