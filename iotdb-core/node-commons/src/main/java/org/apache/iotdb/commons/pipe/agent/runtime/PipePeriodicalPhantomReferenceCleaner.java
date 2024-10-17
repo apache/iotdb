d066bf335e97d2e8e64bb700e414110c8dc79099
VGalaxies <vgalaxies@apache.org>
2024-10-17 22:33:54 +0800

    refact

--- iotdb-core/node-commons/src/main/java/org/apache/iotdb/commons/pipe/agent/runtime/PipePeriodicalPhantomReferenceCleaner.java
+++ iotdb-core/node-commons/src/main/java/org/apache/iotdb/commons/pipe/agent/runtime/PipePeriodicalPhantomReferenceCleaner.java
@@ -21,67 +21,14 @@ package org.apache.iotdb.commons.pipe.agent.runtime;
 
 import org.apache.iotdb.commons.concurrent.IoTDBThreadPoolFactory;
 import org.apache.iotdb.commons.concurrent.ThreadName;
-import org.apache.iotdb.commons.concurrent.WrappedRunnable;
-import org.apache.iotdb.commons.concurrent.threadpool.ScheduledExecutorUtil;
 
-import org.apache.tsfile.utils.Pair;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+/** The shortest scheduling cycle for these jobs is 1, suitable for jobs that are time-critical. */
+public class PipePeriodicalPhantomReferenceCleaner extends AbstractPipePeriodicalJobExecutor {
 
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
-
-/**
- * Single thread to execute pipe periodical phantom referenced clean job on DataNode or ConfigNode.
- * The shortest scheduling cycle for these jobs is {@link
- * PipePeriodicalPhantomReferenceCleaner#MIN_INTERVAL_SECONDS}, suitable for jobs that are
- * time-critical.
- */
-public class PipePeriodicalPhantomReferenceCleaner extends PipePeriodicalJobExecutor {
-
-  private static final Logger LOGGER =
-      LoggerFactory.getLogger(PipePeriodicalPhantomReferenceCleaner.class);
-
-  private static final ScheduledExecutorService PERIODICAL_JOB_EXECUTOR =
-      IoTDBThreadPoolFactory.newSingleThreadScheduledExecutor(
-          ThreadName.PIPE_RUNTIME_PERIODICAL_PHANTOM_REFERENCE_CLEANER.getName());
-
-  private static final long MIN_INTERVAL_SECONDS = 1L;
-
-  @Override
-  public void register(String id, Runnable periodicalJob, long intervalInSeconds) {
-    periodicalJobs.add(
-        new Pair<>(
-            new WrappedRunnable() {
-              @Override
-              public void runMayThrow() {
-                try {
-                  periodicalJob.run();
-                } catch (Exception e) {
-                  LOGGER.warn("Periodical phantom referenced clean job {} failed.", id, e);
-                }
-              }
-            },
-            Math.max(intervalInSeconds, MIN_INTERVAL_SECONDS)));
-    LOGGER.info(
-        "Pipe periodical phantom referenced clean job {} is registered successfully. Interval: {} seconds.",
-        id,
-        Math.max(intervalInSeconds, MIN_INTERVAL_SECONDS));
-  }
-
-  @Override
-  public synchronized void start() {
-    if (executorFuture == null) {
-      rounds = 0;
-
-      executorFuture =
-          ScheduledExecutorUtil.safelyScheduleWithFixedDelay(
-              PERIODICAL_JOB_EXECUTOR,
-              super::execute,
-              MIN_INTERVAL_SECONDS,
-              MIN_INTERVAL_SECONDS,
-              TimeUnit.SECONDS);
-      LOGGER.info("Pipe periodical job executor is started successfully.");
-    }
+  public PipePeriodicalPhantomReferenceCleaner() {
+    super(
+        IoTDBThreadPoolFactory.newSingleThreadScheduledExecutor(
+            ThreadName.PIPE_RUNTIME_PERIODICAL_PHANTOM_REFERENCE_CLEANER.getName()),
+        1L);
   }
 }
