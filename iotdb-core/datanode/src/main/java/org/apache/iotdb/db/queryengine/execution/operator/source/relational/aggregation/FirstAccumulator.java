45e3af3b59fcac01c6df468dd8cb8a1f3c8ac89f
Beyyes <cgf1993@foxmail.com>
2024-10-18 18:22:37 +0800

    fix statistics

--- iotdb-core/datanode/src/main/java/org/apache/iotdb/db/queryengine/execution/operator/source/relational/aggregation/FirstAccumulator.java
+++ iotdb-core/datanode/src/main/java/org/apache/iotdb/db/queryengine/execution/operator/source/relational/aggregation/FirstAccumulator.java
@@ -201,7 +201,7 @@ public class FirstAccumulator implements TableAccumulator {
 
   @Override
   public void addStatistics(Statistics[] statistics) {
-    if (statistics[0] == null) {
+    if (statistics == null || statistics[0] == null) {
       return;
     }
     switch (seriesDataType) {
