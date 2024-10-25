64bf6d82a107066cf0e6c3b577edaa31e3bfc4a8
Xiangpeng Hu <65238551+HxpSerein@users.noreply.github.com>
2024-10-25 11:58:14 +0800

    [region migration] Enable write retry for ratis local write (#13901) (#13913)
    (cherry picked from commit fe23762840ce5be521f7dd8391ab4d9b2db23fb3)

--- iotdb-core/consensus/src/main/java/org/apache/iotdb/consensus/ratis/RatisConsensus.java
+++ iotdb-core/consensus/src/main/java/org/apache/iotdb/consensus/ratis/RatisConsensus.java
@@ -332,6 +332,8 @@ class RatisConsensus implements IConsensus {
         if (ex != null) {
           suggestedLeader = ex.getSuggestedLeader();
         }
+      } catch (GroupMismatchException e) {
+        throw new ConsensusGroupNotExistException(groupId);
       } catch (Exception e) {
         throw new RatisRequestFailedException(e);
       }
@@ -347,6 +349,8 @@ class RatisConsensus implements IConsensus {
         throw new RatisRequestFailedException(reply.getException());
       }
       writeResult = Utils.deserializeFrom(reply.getMessage().getContent().asReadOnlyByteBuffer());
+    } catch (GroupMismatchException e) {
+      throw new ConsensusGroupNotExistException(groupId);
     } catch (Exception e) {
       throw new RatisRequestFailedException(e);
     }
