9069eb40e0ff2e56f77fa3178e349b6b1a6b6676
Beyyes <cgf1993@foxmail.com>
2024-10-29 10:04:22 +0800

    Fix the case that there is nonexist measurement in having clause

--- iotdb-core/datanode/src/main/java/org/apache/iotdb/db/queryengine/plan/expression/visitor/ColumnTransformerVisitor.java
+++ iotdb-core/datanode/src/main/java/org/apache/iotdb/db/queryengine/plan/expression/visitor/ColumnTransformerVisitor.java
@@ -81,6 +81,7 @@ import java.util.Map;
 import java.util.stream.Collectors;
 
 import static org.apache.iotdb.db.queryengine.plan.expression.ExpressionType.BETWEEN;
+import static org.apache.tsfile.enums.TSDataType.UNKNOWN;
 
 /** Responsible for constructing {@link ColumnTransformer} through Expression. */
 public class ColumnTransformerVisitor
@@ -581,7 +582,19 @@ public class ColumnTransformerVisitor
       if (typeProvider != null) {
         return typeProvider.getType(expression.getOutputSymbol());
       }
-      return expressionTypes.get(NodeRef.of(expression));
+      if (expressionTypes.get(NodeRef.of(expression)) != UNKNOWN) {
+        return expressionTypes.get(NodeRef.of(expression));
+      } else {
+        for (Map.Entry<NodeRef<Expression>, TSDataType> entry : expressionTypes.entrySet()) {
+          if (entry.getKey().getNode().equals(expression) && entry.getValue() != UNKNOWN) {
+            return entry.getValue();
+          }
+        }
+        throw new IllegalStateException(
+            String.format(
+                "Unknown expression type: %s, perhaps it has non existent measurement.",
+                expression.getOutputSymbol()));
+      }
     }
 
     public TypeProvider getTypeProvider() {
