c928a93b38ae5b6b1385652cdf8626a44a2938c1
Caideyipi <87789683+Caideyipi@users.noreply.github.com>
2024-11-06 16:43:38 +0800

    Update TableDistributedPlanGenerator.java

--- iotdb-core/datanode/src/main/java/org/apache/iotdb/db/queryengine/plan/relational/planner/distribute/TableDistributedPlanGenerator.java
+++ iotdb-core/datanode/src/main/java/org/apache/iotdb/db/queryengine/plan/relational/planner/distribute/TableDistributedPlanGenerator.java
@@ -71,6 +71,7 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.Optional;
 import java.util.Set;
 import java.util.function.Function;
@@ -935,17 +936,19 @@ public class TableDistributedPlanGenerator
       for (int i = 0; i < node.getPartitionKeyList().size(); ++i) {
         final TRegionReplicaSet regionReplicaSet =
             databaseMap.get(schemaPartition.calculateDeviceGroupId(partitionKeyList.get(i)));
-        tableDeviceFetchMap
-            .computeIfAbsent(
-                regionReplicaSet,
-                k -> {
-                  final TableDeviceFetchNode clonedNode =
-                      (TableDeviceFetchNode) node.cloneForDistribution();
-                  clonedNode.setPlanNodeId(queryId.genPlanNodeId());
-                  clonedNode.setRegionReplicaSet(regionReplicaSet);
-                  return clonedNode;
-                })
-            .addDeviceId(deviceIDArray.get(i));
+        if (Objects.nonNull(regionReplicaSet)) {
+          tableDeviceFetchMap
+              .computeIfAbsent(
+                  regionReplicaSet,
+                  k -> {
+                    final TableDeviceFetchNode clonedNode =
+                        (TableDeviceFetchNode) node.cloneForDistribution();
+                    clonedNode.setPlanNodeId(queryId.genPlanNodeId());
+                    clonedNode.setRegionReplicaSet(regionReplicaSet);
+                    return clonedNode;
+                  })
+              .addDeviceId(deviceIDArray.get(i));
+        }
       }
 
       final List<PlanNode> res = new ArrayList<>();
