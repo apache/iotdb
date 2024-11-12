9f5b832136bd8812137c4909e1e301ccd1f3d50c
VGalaxies <vgalaxies@apache.org>
2024-11-13 07:59:05 +0800

    rename

--- iotdb-core/datanode/src/main/java/org/apache/iotdb/db/pipe/resource/tsfile/PipeTsFileResourceSegmentLock.java
+++ iotdb-core/datanode/src/main/java/org/apache/iotdb/db/pipe/resource/tsfile/PipeTsFileResourceSegmentLock.java
@@ -51,13 +51,13 @@ public class PipeTsFileResourceSegmentLock {
           lockSegmentSize = Math.min(SEGMENT_LOCK_MAX_SIZE, lockSegmentSize);
           lockSegmentSize = Math.max(SEGMENT_LOCK_MIN_SIZE, lockSegmentSize);
 
-          final ReentrantLock[] newLocks = new ReentrantLock[lockSegmentSize];
-          for (int i = 0; i < newLocks.length; i++) {
-            newLocks[i] = new ReentrantLock();
+          final ReentrantLock[] tmpLocks = new ReentrantLock[lockSegmentSize];
+          for (int i = 0; i < tmpLocks.length; i++) {
+            tmpLocks[i] = new ReentrantLock();
           }
 
           // publish this variable
-          locks = newLocks;
+          locks = tmpLocks;
         }
       }
     }
