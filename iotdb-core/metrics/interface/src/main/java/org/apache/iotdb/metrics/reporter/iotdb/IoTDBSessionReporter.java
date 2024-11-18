9562a594a0f9322d9494c39e1d1878194a55693e
liyuheng <liyuheng55555@126.com>
2024-11-18 18:11:17 +0800

    seems done

--- iotdb-core/metrics/interface/src/main/java/org/apache/iotdb/metrics/reporter/iotdb/IoTDBSessionReporter.java
+++ iotdb-core/metrics/interface/src/main/java/org/apache/iotdb/metrics/reporter/iotdb/IoTDBSessionReporter.java
@@ -76,8 +76,7 @@ public class IoTDBSessionReporter extends IoTDBReporter {
         try (SessionDataSetWrapper result2 =
             this.sessionPool.executeQueryStatement(
                 "CREATE DATABASE "
-                    + metricConfig.getInternalDatabase()
-                    + " WITH SCHEMA_REPLICATION_FACTOR=1, DATA_REPLICATION_FACTOR=1, SCHEMA_REGION_GROUP_NUM=1, DATA_REGION_GROUP_NUM=1")) {
+                    + metricConfig.getInternalDatabase())) {
           if (!result2.hasNext()) {
             LOGGER.error("IoTDBSessionReporter checkOrCreateDatabase failed.");
           }
