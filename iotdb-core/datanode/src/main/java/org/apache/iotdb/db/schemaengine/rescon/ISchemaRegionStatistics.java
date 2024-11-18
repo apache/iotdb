6c155f9ea56b8d4ecad62fec3e0c51760c823045
Caideyipi <87789683+Caideyipi@users.noreply.github.com>
2024-11-18 17:15:04 +0800

    may final

--- iotdb-core/datanode/src/main/java/org/apache/iotdb/db/schemaengine/rescon/ISchemaRegionStatistics.java
+++ iotdb-core/datanode/src/main/java/org/apache/iotdb/db/schemaengine/rescon/ISchemaRegionStatistics.java
@@ -19,6 +19,8 @@
 
 package org.apache.iotdb.db.schemaengine.rescon;
 
+import java.util.Map;
+
 public interface ISchemaRegionStatistics {
 
   boolean isAllowToCreateNewSeries();
@@ -30,6 +32,8 @@ public interface ISchemaRegionStatistics {
   long getSeriesNumber(final boolean includeView);
 
   long getDevicesNumber();
+
+  Map<String, Long> getTable2DevicesNumMap();
 
   long getTableDevicesNumber(final String table);
 
