b1c0227490947a1346773bf9f7f05cbfe56705ab
Caideyipi <87789683+Caideyipi@users.noreply.github.com>
2024-11-20 14:10:37 +0800

    next

--- iotdb-core/confignode/src/main/java/org/apache/iotdb/confignode/manager/pipe/connector/payload/PipeTransferConfigSnapshotSealReq.java
+++ iotdb-core/confignode/src/main/java/org/apache/iotdb/confignode/manager/pipe/connector/payload/PipeTransferConfigSnapshotSealReq.java
@@ -95,7 +95,11 @@ public class PipeTransferConfigSnapshotSealReq extends PipeTransferFileSealReqV2
   /////////////////////////////// Air Gap ///////////////////////////////
 
   public static byte[] toTPipeTransferBytes(
-      final String pattern,
+      final String treePattern,
+      final String tablePatternDatabase,
+      final String tablePatternTable,
+      final boolean isTreeCaptured,
+      final boolean isTableCaptured,
       final String snapshotName,
       final long snapshotLength,
       final String templateFileName,
@@ -104,7 +108,15 @@ public class PipeTransferConfigSnapshotSealReq extends PipeTransferFileSealReqV2
       final String typeString)
       throws IOException {
     final Map<String, String> parameters = new HashMap<>();
-    parameters.put(ColumnHeaderConstant.PATH_PATTERN, pattern);
+    parameters.put(ColumnHeaderConstant.PATH_PATTERN, treePattern);
+    parameters.put(ColumnHeaderConstant.DATABASE, tablePatternDatabase);
+    parameters.put(ColumnHeaderConstant.TABLE_NAME, tablePatternTable);
+    if (isTreeCaptured) {
+      parameters.put(TREE, "");
+    }
+    if (isTableCaptured) {
+      parameters.put(TABLE, "");
+    }
     parameters.put(FILE_TYPE, Byte.toString(fileType.getType()));
     parameters.put(ColumnHeaderConstant.TYPE, typeString);
     return new PipeTransferConfigSnapshotSealReq()
