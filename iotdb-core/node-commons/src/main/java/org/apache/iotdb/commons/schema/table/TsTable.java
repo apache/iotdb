680eab9fb269e8cf09fa2cb8280afae2b56e4eb7
Caideyipi <87789683+Caideyipi@users.noreply.github.com>
2024-11-25 16:49:05 +0800

    Update TsTable.java

--- iotdb-core/node-commons/src/main/java/org/apache/iotdb/commons/schema/table/TsTable.java
+++ iotdb-core/node-commons/src/main/java/org/apache/iotdb/commons/schema/table/TsTable.java
@@ -49,6 +49,8 @@ import java.util.Set;
 import java.util.concurrent.locks.ReadWriteLock;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
 
+import static org.apache.iotdb.commons.conf.IoTDBConstant.TTL_INFINITE;
+
 @ThreadSafe
 public class TsTable {
 
@@ -197,7 +199,10 @@ public class TsTable {
   }
 
   public long getTableTTLInMS() {
-    return Long.parseLong(getPropValue(TTL_PROPERTY).orElse(Long.MAX_VALUE + ""));
+    final Optional<String> ttl = getPropValue(TTL_PROPERTY);
+    return ttl.isPresent() && !ttl.get().equalsIgnoreCase(TTL_INFINITE)
+        ? Long.parseLong(ttl.get())
+        : Long.MAX_VALUE;
   }
 
   public Optional<String> getPropValue(final String propKey) {
