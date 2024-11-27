e8ea11cabaa49c15a259269a98ded7982f20871f
Chen YZ <chen3yz@gmail.com>
2024-11-27 17:16:40 +0800

    add IT

--- integration-test/src/main/java/org/apache/iotdb/db/query/udf/example/relational/AllSum.java
+++ integration-test/src/main/java/org/apache/iotdb/db/query/udf/example/relational/AllSum.java
@@ -25,49 +25,80 @@ import org.apache.iotdb.udf.api.relational.ScalarFunction;
 import org.apache.iotdb.udf.api.relational.access.Record;
 import org.apache.iotdb.udf.api.type.Type;
 
-/**
- * Calculate the sum of all parameters. Only support inputs of INT32,INT64,DOUBLE,FLOAT type.
- */
+import java.util.HashSet;
+import java.util.Set;
+
+/** Calculate the sum of all parameters. Only support inputs of INT32,INT64,DOUBLE,FLOAT type. */
 public class AllSum implements ScalarFunction {
 
-    @Override
-    public void validate(FunctionParameters parameters) throws Exception {
-        if (parameters.getChildExpressionsSize() < 1) {
-            throw new UDFParameterNotValidException("At least one parameter is required.");
-        }
-        for(int i = 0; i < parameters.getChildExpressionsSize(); i++) {
-            if(parameters.getDataType(i)!=Type.INT32 && parameters.getDataType(i)!=Type.INT64&&parameters.getDataType(i)!=Type.FLOAT&&parameters.getDataType(i)!=Type.DOUBLE){
-                throw new UDFParameterNotValidException("Only support inputs of INT32,INT64,DOUBLE,FLOAT type.");
-            }
-        }
-    }
+  private Type outputDataType;
 
-    @Override
-    public Type inferOutputType(FunctionParameters parameters) {
-        return Type.DOUBLE;
+  @Override
+  public void validate(FunctionParameters parameters) throws Exception {
+    if (parameters.getChildExpressionsSize() < 1) {
+      throw new UDFParameterNotValidException("At least one parameter is required.");
     }
+    for (int i = 0; i < parameters.getChildExpressionsSize(); i++) {
+      if (parameters.getDataType(i) != Type.INT32
+          && parameters.getDataType(i) != Type.INT64
+          && parameters.getDataType(i) != Type.FLOAT
+          && parameters.getDataType(i) != Type.DOUBLE) {
+        throw new UDFParameterNotValidException(
+            "Only support inputs of INT32,INT64,DOUBLE,FLOAT type.");
+      }
+    }
+  }
 
-    @Override
-    public Object evaluate(Record input) throws Exception {
-        double res = 0;
-        for (int i = 0; i < input.size(); i++) {
-            if(!input.isNull(i)) {
-                switch (input.getDataType(i)) {
-                    case INT32:
-                        res += input.getInt(i);
-                        break;
-                    case INT64:
-                        res += input.getLong(i);
-                        break;
-                    case FLOAT:
-                        res += input.getFloat(i);
-                        break;
-                    case DOUBLE:
-                        res += input.getDouble(i);
-                        break;
-                }
-            }
+  @Override
+  public Type inferOutputType(FunctionParameters parameters) {
+    Set<Type> inputTypeSet = new HashSet<>();
+    for (int i = 0; i < parameters.getChildExpressionsSize(); i++) {
+      inputTypeSet.add(parameters.getDataType(i));
+    }
+    if (inputTypeSet.contains(Type.DOUBLE)) {
+      outputDataType = Type.DOUBLE;
+    } else if (inputTypeSet.contains(Type.FLOAT)) {
+      outputDataType = Type.FLOAT;
+    } else if (inputTypeSet.contains(Type.INT64)) {
+      outputDataType = Type.INT64;
+    } else {
+      outputDataType = Type.INT32;
+    }
+    return outputDataType;
+  }
+
+  @Override
+  public Object evaluate(Record input) throws Exception {
+    double res = 0;
+    for (int i = 0; i < input.size(); i++) {
+      if (!input.isNull(i)) {
+        switch (input.getDataType(i)) {
+          case INT32:
+            res += input.getInt(i);
+            break;
+          case INT64:
+            res += input.getLong(i);
+            break;
+          case FLOAT:
+            res += input.getFloat(i);
+            break;
+          case DOUBLE:
+            res += input.getDouble(i);
+            break;
         }
+      }
+    }
+    switch (outputDataType) {
+      case INT32:
+        return (int) res;
+      case INT64:
+        return (long) res;
+      case FLOAT:
+        return (float) res;
+      case DOUBLE:
         return res;
+      default:
+        throw new RuntimeException("Unexpected output type.");
     }
+  }
 }
--- integration-test/src/test/java/org/apache/iotdb/relational/it/db/it/udf/scalar/IoTDBScalarFunctionIT.java
+++ integration-test/src/test/java/org/apache/iotdb/relational/it/db/it/udf/scalar/IoTDBScalarFunctionIT.java
@@ -23,67 +23,163 @@ import org.apache.iotdb.it.env.EnvFactory;
 import org.apache.iotdb.it.framework.IoTDBTestRunner;
 import org.apache.iotdb.itbase.category.TableClusterIT;
 import org.apache.iotdb.itbase.category.TableLocalStandaloneIT;
-import org.apache.iotdb.itbase.env.BaseEnv;
+
 import org.junit.After;
-import org.junit.AfterClass;
+import org.junit.Assert;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 import org.junit.runner.RunWith;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.Statement;
+import java.sql.Types;
+import java.text.DecimalFormat;
+import java.util.Arrays;
+import java.util.List;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 
 @RunWith(IoTDBTestRunner.class)
 @Category({TableLocalStandaloneIT.class, TableClusterIT.class})
 public class IoTDBScalarFunctionIT {
-    private static String[] sqls =
-            new String[] {
-                    "CREATE DATABASE test",
-                    "USE \"test\"",
-                    "CREATE TABLE vehicle (device_id string id, s1 INT32 measurement, s2 INT64 measurement, s3 FLOAT MEASUREMENT, s4 DOUBLE MEASUREMENT, s5 BOOLEAN MEASUREMENT)",
-                    "insert into vehicle(time, device_id, s1, s2, s3, s4, s5) values (1, 'd0', 1, 1, 1.1, 1.1, true)",
-                    "insert into vehicle(time, device_id, s1, s2, s3, s4, s5) values (2, 'd0', null, 2, 2.2, 2.2, true)",
-                    "insert into vehicle(time, device_id, s1, s2, s3, s4, s5) values (3, 'd0', 3, 3, null, null, false)",
-                    "insert into vehicle(time, device_id, s5) values (5, 'd0', true)",
-            };
+  private static String[] sqls =
+      new String[] {
+        "CREATE DATABASE test",
+        "USE test",
+        "CREATE TABLE vehicle (device_id string id, s1 INT32 measurement, s2 INT64 measurement, s3 FLOAT MEASUREMENT, s4 DOUBLE MEASUREMENT, s5 BOOLEAN MEASUREMENT)",
+        "insert into vehicle(time, device_id, s1, s2, s3, s4, s5) values (1, 'd0', 1, 1, 1.1, 1.1, true)",
+        "insert into vehicle(time, device_id, s1, s2, s3, s4, s5) values (2, 'd0', null, 2, 2.2, 2.2, true)",
+        "insert into vehicle(time, device_id, s1, s2, s3, s4, s5) values (3, 'd0', 3, 3, null, null, false)",
+        "insert into vehicle(time, device_id, s5) values (5, 'd0', true)",
+        "CREATE FUNCTION contain_null as 'org.apache.iotdb.db.query.udf.example.relational.ContainNull'",
+        "CREATE FUNCTION all_sum as 'org.apache.iotdb.db.query.udf.example.relational.AllSum'"
+      };
 
-    @Before
-    public static void setUp() throws Exception {
-        EnvFactory.getEnv().initClusterEnvironment();
-        insertData();
+  @Before
+  public void setUp() throws Exception {
+    EnvFactory.getEnv().initClusterEnvironment();
+    insertData();
+  }
+
+  @After
+  public void tearDown() throws Exception {
+    EnvFactory.getEnv().cleanClusterEnvironment();
+  }
+
+  private static void insertData() {
+    try (Connection connection = EnvFactory.getEnv().getTableConnection();
+        Statement statement = connection.createStatement()) {
+      for (String sql : sqls) {
+        System.out.println(sql);
+        statement.execute(sql);
+      }
+    } catch (Exception e) {
+      fail("insertData failed.");
     }
+  }
 
-    @After
-    public static void tearDown() throws Exception {
-        EnvFactory.getEnv().cleanClusterEnvironment();
+  @Test
+  public void testIllegalInput() {
+    try (Connection connection = EnvFactory.getEnv().getTableConnection();
+        Statement statement = connection.createStatement()) {
+      statement.execute("USE test");
+      try {
+        statement.execute("select contain_null() from vehicle");
+        fail();
+      } catch (Exception e) {
+        Assert.assertTrue(e.getMessage().contains("At least one parameter is required"));
+      }
+      try {
+        statement.execute("select all_sum(s1,s2,s3,s4,s5) from vehicle");
+        fail();
+      } catch (Exception e) {
+        Assert.assertTrue(
+            e.getMessage().contains("Only support inputs of INT32,INT64,DOUBLE,FLOAT type"));
+      }
+
+    } catch (Exception e) {
+      fail(e.getMessage());
     }
+  }
 
-    private static void insertData() {
-        try (Connection connection = EnvFactory.getEnv().getTableConnection();
-             Statement statement = connection.createStatement()) {
-            for (String sql : sqls) {
-                System.out.println(sql);
-                statement.execute(sql);
-            }
-        } catch (Exception e) {
-            fail("insertData failed.");
+  @Test
+  public void testNormalQuery() {
+    try (Connection connection = EnvFactory.getEnv().getTableConnection();
+        Statement statement = connection.createStatement()) {
+      statement.execute("USE test");
+      List<String> expectedResult =
+          Arrays.asList("1,false,false", "2,true,true", "3,true,false", "5,true,true");
+      int row = 0;
+      try (ResultSet resultSet =
+          statement.executeQuery(
+              "select time, contain_null(s1,s2,s3,s4,s5) as contain_null, contain_null(s1) as s1_null from vehicle")) {
+        while (resultSet.next()) {
+          Assert.assertEquals(
+              expectedResult.get(row),
+              resultSet.getLong(1) + "," + resultSet.getBoolean(2) + "," + resultSet.getBoolean(3));
+          row++;
         }
+        assertEquals(4, row);
+      }
+    } catch (Exception e) {
+      fail(e.getMessage());
     }
+  }
 
-    @Test
-    public void testIllegalInput(){
-        try (Connection connection = EnvFactory.getEnv().getTableConnection();
-             Statement statement = connection.createStatement()) {
-            statement.execute("CREATE FUNCTION contain_null as 'org.apache.iotdb.db.query.udf.example.relational.ContainNull'");
-            statement.execute("CREATE FUNCTION all_sum as 'org.apache.iotdb.db.query.udf.example.relational.AllSum'");
-        } catch (Exception e) {
-            fail("insertData failed.");
+  @Test
+  public void testPolymorphicQuery() {
+    try (Connection connection = EnvFactory.getEnv().getTableConnection();
+        Statement statement = connection.createStatement()) {
+      statement.execute("USE test");
+      List<String> expectedResult =
+          Arrays.asList(
+              "1,1,1,1.1,1.1,2,3.1,4.2,2.1",
+              "2,0,2,2.2,2.2,2,4.2,6.4,4.2",
+              "3,3,3,.0,.0,6,6.0,6.0,3.0",
+              "5,0,0,.0,.0,0,.0,.0,.0");
+      int row = 0;
+      try (ResultSet resultSet =
+          statement.executeQuery(
+              "select time, all_sum(s1) as s1, all_sum(s2) as s2,  all_sum(s3) as s3,  all_sum(s4) as s4, all_sum(s1,s2) as s12,  all_sum(s1,s2,s3) as s123, all_sum(s1,s2,s3,s4) as s1234, all_sum(s2,s3) as s23 from vehicle")) {
+        Assert.assertEquals(Types.TIMESTAMP, resultSet.getMetaData().getColumnType(1));
+        Assert.assertEquals(Types.INTEGER, resultSet.getMetaData().getColumnType(2));
+        Assert.assertEquals(Types.BIGINT, resultSet.getMetaData().getColumnType(3));
+        Assert.assertEquals(Types.FLOAT, resultSet.getMetaData().getColumnType(4));
+        Assert.assertEquals(Types.DOUBLE, resultSet.getMetaData().getColumnType(5));
+        Assert.assertEquals(Types.BIGINT, resultSet.getMetaData().getColumnType(6));
+        Assert.assertEquals(Types.FLOAT, resultSet.getMetaData().getColumnType(7));
+        Assert.assertEquals(Types.DOUBLE, resultSet.getMetaData().getColumnType(8));
+        Assert.assertEquals(Types.FLOAT, resultSet.getMetaData().getColumnType(9));
+        DecimalFormat df = new DecimalFormat("#.0");
+        while (resultSet.next()) {
+          Assert.assertEquals(
+              expectedResult.get(row),
+              resultSet.getLong(1)
+                  + ","
+                  + resultSet.getInt(2)
+                  + ","
+                  + resultSet.getLong(3)
+                  + ","
+                  + df.format(resultSet.getFloat(4))
+                  + ","
+                  + df.format(resultSet.getDouble(5))
+                  + ","
+                  + resultSet.getLong(6)
+                  + ","
+                  + df.format(resultSet.getFloat(7))
+                  + ","
+                  + df.format(resultSet.getDouble(8))
+                  + ","
+                  + df.format(resultSet.getFloat(9)));
+          row++;
         }
+        assertEquals(4, row);
+      }
+    } catch (Exception e) {
+      fail(e.getMessage());
     }
-
-
+  }
 }
