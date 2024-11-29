3dd5a9560abf9d56cc9d1f455ef1b6e578a9d5a4
shuwenwei <s13979062948@gmail.com>
2024-11-29 09:54:12 +0800

    fix bug

--- iotdb-core/datanode/src/main/java/org/apache/iotdb/db/storageengine/dataregion/DataRegion.java
+++ iotdb-core/datanode/src/main/java/org/apache/iotdb/db/storageengine/dataregion/DataRegion.java
@@ -3063,9 +3063,6 @@ public class DataRegion implements IDataRegionForQuery {
     PipeInsertionDataNodeListener.getInstance()
         .listenToTsFile(dataRegionId, databaseName, tsFileResource, true, isGeneratedByPipe);
 
-    // help tsfile resource degrade
-    tsFileResourceManager.registerSealedTsFileResource(tsFileResource);
-
     tsFileManager.add(tsFileResource, false);
 
     return true;
