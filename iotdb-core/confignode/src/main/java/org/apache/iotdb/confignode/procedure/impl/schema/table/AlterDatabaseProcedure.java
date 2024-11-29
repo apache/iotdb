60461d479db27e334cd52edbe7adc273e7548afc
Caideyipi <87789683+Caideyipi@users.noreply.github.com>
2024-11-29 11:49:46 +0800

    Update AlterDatabaseProcedure.java

--- iotdb-core/confignode/src/main/java/org/apache/iotdb/confignode/procedure/impl/schema/table/AlterDatabaseProcedure.java
+++ iotdb-core/confignode/src/main/java/org/apache/iotdb/confignode/procedure/impl/schema/table/AlterDatabaseProcedure.java
@@ -19,17 +19,24 @@
 
 package org.apache.iotdb.confignode.procedure.impl.schema.table;
 
+import org.apache.iotdb.common.rpc.thrift.TSStatus;
+import org.apache.iotdb.commons.exception.IoTDBException;
 import org.apache.iotdb.commons.exception.runtime.ThriftSerDeException;
 import org.apache.iotdb.commons.schema.table.TsTable;
 import org.apache.iotdb.commons.utils.ThriftConfigNodeSerDeUtils;
+import org.apache.iotdb.confignode.consensus.request.ConfigPhysicalPlanType;
+import org.apache.iotdb.confignode.consensus.request.write.database.DatabaseSchemaPlan;
+import org.apache.iotdb.confignode.consensus.request.write.pipe.payload.PipeEnrichedPlan;
 import org.apache.iotdb.confignode.procedure.env.ConfigNodeProcedureEnv;
 import org.apache.iotdb.confignode.procedure.exception.ProcedureException;
 import org.apache.iotdb.confignode.procedure.exception.ProcedureSuspendedException;
 import org.apache.iotdb.confignode.procedure.exception.ProcedureYieldException;
 import org.apache.iotdb.confignode.procedure.impl.StateMachineProcedure;
+import org.apache.iotdb.confignode.procedure.impl.schema.SchemaUtils;
 import org.apache.iotdb.confignode.procedure.state.schema.AlterDatabaseState;
 import org.apache.iotdb.confignode.procedure.store.ProcedureType;
 import org.apache.iotdb.confignode.rpc.thrift.TDatabaseSchema;
+import org.apache.iotdb.rpc.TSStatusCode;
 
 import org.apache.tsfile.utils.ReadWriteIOUtils;
 import org.slf4j.Logger;
@@ -40,6 +47,12 @@ import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
+
+import static org.apache.iotdb.confignode.procedure.state.schema.AlterDatabaseState.ALTER_DATABASE;
+import static org.apache.iotdb.confignode.procedure.state.schema.AlterDatabaseState.CHECK_ALTERED_TABLES;
+import static org.apache.iotdb.confignode.procedure.state.schema.AlterDatabaseState.COMMIT_RELEASE;
+import static org.apache.iotdb.confignode.procedure.state.schema.AlterDatabaseState.PRE_RELEASE;
 
 public class AlterDatabaseProcedure
     extends StateMachineProcedure<ConfigNodeProcedureEnv, AlterDatabaseState> {
@@ -58,10 +71,60 @@ public class AlterDatabaseProcedure
   }
 
   @Override
-  protected Flow executeFromState(
-      final ConfigNodeProcedureEnv env, final AlterDatabaseState alterDatabaseState)
+  protected Flow executeFromState(final ConfigNodeProcedureEnv env, final AlterDatabaseState state)
       throws ProcedureSuspendedException, ProcedureYieldException, InterruptedException {
+    final long startTime = System.currentTimeMillis();
+    try {
+      switch (state) {
+        case CHECK_ALTERED_TABLES:
+          validateTable(env);
+          LOGGER.info(
+              "Checking altered tables for database {} when altering database", schema.getName());
+          if (!isFailed() && Objects.isNull(table)) {
+            LOGGER.info(
+                "The updated table has the same properties with the original one. Skip the procedure.");
+            return Flow.NO_MORE_STATE;
+          }
+          break;
+        case PRE_RELEASE:
+          preRelease(env);
+          LOGGER.info("Pre release info for tables {} when altering database", tables);
+          break;
+        case ALTER_DATABASE:
+          alterDatabase(env);
+          LOGGER.info("Altering database to {}", schema);
+          break;
+        case COMMIT_RELEASE:
+          commitRelease(env);
+          LOGGER.info(
+              "Commit release info of database {} when altering database", schema.getName());
+          return Flow.NO_MORE_STATE;
+        default:
+          setFailure(new ProcedureException("Unrecognized AddTableColumnState " + state));
+          return Flow.NO_MORE_STATE;
+      }
+      return Flow.HAS_MORE_STATE;
+    } finally {
+      LOGGER.info(
+          "AlterDatabase-{}-{} costs {}ms",
+          schema.getName(),
+          state,
+          (System.currentTimeMillis() - startTime));
+    }
     return null;
+  }
+
+  private void alterDatabase(final ConfigNodeProcedureEnv env) {
+    final DatabaseSchemaPlan plan =
+        new DatabaseSchemaPlan(ConfigPhysicalPlanType.AlterDatabase, schema);
+    final TSStatus status =
+        SchemaUtils.executeInConsensusLayer(
+            isGeneratedByPipe ? new PipeEnrichedPlan(plan) : plan, env, LOGGER);
+    if (status.getCode() == TSStatusCode.SUCCESS_STATUS.getStatusCode()) {
+      setNextState(AlterDatabaseState.COMMIT_RELEASE);
+    } else {
+      setFailure(new ProcedureException(new IoTDBException(status.getMessage(), status.getCode())));
+    }
   }
 
   @Override
