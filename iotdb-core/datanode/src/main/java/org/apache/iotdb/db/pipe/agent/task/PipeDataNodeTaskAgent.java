399a33412d06af30e118e55c592f6c578b9fbe58
Steve Yurong Su <rong@apache.org>
2024-12-03 19:00:09 +0800

    Pipe: fix too many warn logs from findAllStuckPipes() (#14287)

--- iotdb-core/datanode/src/main/java/org/apache/iotdb/db/pipe/agent/task/PipeDataNodeTaskAgent.java
+++ iotdb-core/datanode/src/main/java/org/apache/iotdb/db/pipe/agent/task/PipeDataNodeTaskAgent.java
@@ -499,8 +499,11 @@ public class PipeDataNodeTaskAgent extends PipeTaskAgent {
       for (final PipeMeta pipeMeta : pipeMetaKeeper.getPipeMetaList()) {
         stuckPipes.add(pipeMeta);
       }
-      LOGGER.warn(
-          "All {} pipe(s) will be restarted because of forced restart policy.", stuckPipes.size());
+      if (!stuckPipes.isEmpty()) {
+        LOGGER.warn(
+            "All {} pipe(s) will be restarted because of forced restart policy.",
+            stuckPipes.size());
+      }
       return stuckPipes;
     }
 
@@ -509,9 +512,11 @@ public class PipeDataNodeTaskAgent extends PipeTaskAgent {
       for (final PipeMeta pipeMeta : pipeMetaKeeper.getPipeMetaList()) {
         stuckPipes.add(pipeMeta);
       }
-      LOGGER.warn(
-          "All {} pipe(s) will be restarted because linked tsfiles' resource size exceeds memory limit.",
-          stuckPipes.size());
+      if (!stuckPipes.isEmpty()) {
+        LOGGER.warn(
+            "All {} pipe(s) will be restarted because linked tsfiles' resource size exceeds memory limit.",
+            stuckPipes.size());
+      }
       return stuckPipes;
     }
 
