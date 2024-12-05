cae4b926654c8dcc5a53c9e86e8b878d1b42fe6f
MrQuansy <siyiquan2021@gmail.com>
2024-12-05 09:59:32 +0800

    fix compile error

--- iotdb-core/node-commons/src/main/java/org/apache/iotdb/commons/conf/CommonDescriptor.java
+++ iotdb-core/node-commons/src/main/java/org/apache/iotdb/commons/conf/CommonDescriptor.java
@@ -737,7 +737,7 @@ public class CommonDescriptor {
                     "enable_retry_for_unknown_error"))));
   }
 
-  public void loadBinaryAllocatorProps(Properties properties) {
+  public void loadBinaryAllocatorProps(TrimProperties properties) {
     config.setEnableBinaryAllocator(
         Boolean.parseBoolean(
             properties.getProperty(
