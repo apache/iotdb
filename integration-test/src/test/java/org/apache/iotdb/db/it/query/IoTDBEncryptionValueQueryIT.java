839e2221dce2b1ea9e72610f6dcb3bf11c9b781d
shuwenwei <55970239+shuwenwei@users.noreply.github.com>
2024-12-12 16:08:34 +0800

    Move model annotations to tsfile (#14358)
    * move model annotations to tsfile
    
    * recover some change
    
    * update tsfile version
    
    * ignore IoTDBEncryptionValueQueryIT

--- integration-test/src/test/java/org/apache/iotdb/db/it/query/IoTDBEncryptionValueQueryIT.java
+++ integration-test/src/test/java/org/apache/iotdb/db/it/query/IoTDBEncryptionValueQueryIT.java
@@ -26,6 +26,7 @@ import org.apache.iotdb.itbase.category.LocalStandaloneIT;
 import org.junit.AfterClass;
 import org.junit.Assert;
 import org.junit.BeforeClass;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 import org.junit.runner.RunWith;
@@ -45,6 +46,7 @@ import static org.junit.Assert.fail;
 
 @RunWith(IoTDBTestRunner.class)
 @Category({LocalStandaloneIT.class, ClusterIT.class})
+@Ignore
 public class IoTDBEncryptionValueQueryIT {
   private static String[] sqls =
       new String[] {
