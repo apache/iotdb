ed821dd26cd33dab629fbf6b3f2cc849e4168b60
Peng Junzhi <201250214@smail.nju.edu.cn>
2024-12-16 23:23:59 +0800

    add switch to control filter

--- iotdb-core/datanode/src/main/java/org/apache/iotdb/db/pipe/processor/pipeconsensus/PipeConsensusProcessor.java
+++ iotdb-core/datanode/src/main/java/org/apache/iotdb/db/pipe/processor/pipeconsensus/PipeConsensusProcessor.java
@@ -36,9 +36,11 @@ import org.apache.iotdb.pipe.api.event.dml.insertion.TabletInsertionEvent;
 import org.apache.iotdb.pipe.api.event.dml.insertion.TsFileInsertionEvent;
 
 import java.util.Map;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 public class PipeConsensusProcessor implements PipeProcessor {
   private static final int DATA_NODE_ID = IoTDBDescriptor.getInstance().getConfig().getDataNodeId();
+  private final AtomicBoolean filterSwitch = new AtomicBoolean(true);
 
   @Override
   public void validate(PipeParameterValidator validator) throws Exception {}
