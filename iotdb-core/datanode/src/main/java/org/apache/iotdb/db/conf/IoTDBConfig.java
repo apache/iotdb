391d153759086ff947e238b196021e71aafbce1b
Tian Jiang <jt2594838@163.com>
2024-12-18 12:06:59 +0800

    Fix setting illegal default_storage_group_level does not report an error

--- iotdb-core/datanode/src/main/java/org/apache/iotdb/db/conf/IoTDBConfig.java
+++ iotdb-core/datanode/src/main/java/org/apache/iotdb/db/conf/IoTDBConfig.java
@@ -2479,6 +2479,11 @@ public class IoTDBConfig {
   }
 
   void setDefaultStorageGroupLevel(int defaultStorageGroupLevel) {
+    if (defaultStorageGroupLevel < 1) {
+      throw new IllegalArgumentException(
+          String.format(
+              "Illegal defaultStorageGroupLevel: %d, should >= 1", defaultStorageGroupLevel));
+    }
     this.defaultStorageGroupLevel = defaultStorageGroupLevel;
   }
 
