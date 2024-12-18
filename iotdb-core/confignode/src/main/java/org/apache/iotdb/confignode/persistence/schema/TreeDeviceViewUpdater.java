6dc7e9015d5f79cfae4e6196035c743a81d5b879
Caideyipi <87789683+Caideyipi@users.noreply.github.com>
2024-12-18 10:04:06 +0800

    Refactor

--- iotdb-core/confignode/src/main/java/org/apache/iotdb/confignode/persistence/schema/TreeDeviceViewUpdater.java
+++ iotdb-core/confignode/src/main/java/org/apache/iotdb/confignode/persistence/schema/TreeDeviceViewUpdater.java
@@ -21,74 +21,18 @@ package org.apache.iotdb.confignode.persistence.schema;
 
 import org.apache.iotdb.commons.concurrent.IoTDBThreadPoolFactory;
 import org.apache.iotdb.commons.concurrent.ThreadName;
+import org.apache.iotdb.commons.service.AbstractPeriodicalServiceWithAdvance;
 import org.apache.iotdb.confignode.conf.ConfigNodeDescriptor;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+public class TreeDeviceViewUpdater extends AbstractPeriodicalServiceWithAdvance {
 
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.locks.Condition;
-import java.util.concurrent.locks.ReentrantLock;
-
-public class TreeDeviceViewUpdater {
-
-  private static final Logger LOGGER = LoggerFactory.getLogger(TreeDeviceViewUpdater.class);
-
-  private static final long UPDATE_INTERVAL_SECONDS =
-      ConfigNodeDescriptor.getInstance().getConf().getTreeDeviceViewUpdateIntervalInMs();
-
-  private static final ScheduledExecutorService DEVICE_VIEW_UPDATER_EXECUTOR =
-      IoTDBThreadPoolFactory.newSingleThreadScheduledExecutor(
-          ThreadName.TREE_DEVICE_VIEW_UPDATER.getName());
-
-  private final ReentrantLock lock = new ReentrantLock();
-  private final Condition condition = lock.newCondition();
-  private volatile boolean skipNextSleep = false;
-  private volatile boolean allowSubmitListen = false;
-
-  public void notifyUpdate() {
-    if (lock.tryLock()) {
-      try {
-        condition.signalAll();
-      } finally {
-        lock.unlock();
-      }
-    } else {
-      skipNextSleep = true;
-    }
+  protected TreeDeviceViewUpdater() {
+    super(
+        IoTDBThreadPoolFactory.newSingleThreadScheduledExecutor(
+            ThreadName.TREE_DEVICE_VIEW_UPDATER.getName()),
+        ConfigNodeDescriptor.getInstance().getConf().getTreeDeviceViewUpdateIntervalInMs());
   }
 
-  private void execute() {
-    lock.lock();
-    try {
-      if (!skipNextSleep) {
-        condition.await(UPDATE_INTERVAL_SECONDS, TimeUnit.MILLISECONDS);
-      }
-      skipNextSleep = false;
-    } catch (final InterruptedException e) {
-      Thread.currentThread().interrupt();
-      LOGGER.warn("Interrupted when waiting for the next device view update: {}", e.getMessage());
-    } finally {
-      lock.unlock();
-
-      if (allowSubmitListen) {
-        DEVICE_VIEW_UPDATER_EXECUTOR.submit(this::execute);
-      }
-    }
-  }
-
-  public void start() {
-    allowSubmitListen = true;
-    DEVICE_VIEW_UPDATER_EXECUTOR.submit(this::execute);
-
-    LOGGER.info("Tree device view updater is started successfully.");
-  }
-
-  public void stop() {
-    allowSubmitListen = false;
-    DEVICE_VIEW_UPDATER_EXECUTOR.shutdown();
-
-    LOGGER.info("Tree device view updater is stopped successfully.");
-  }
+  @Override
+  protected void executeTask() {}
 }
