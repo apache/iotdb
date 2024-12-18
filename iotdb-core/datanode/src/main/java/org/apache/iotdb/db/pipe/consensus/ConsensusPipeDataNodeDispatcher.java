9842445c9a68a3464897e061bf1571d0ebfc8272
Peng Junzhi <201250214@smail.nju.edu.cn>
2024-12-18 15:56:29 +0800

    fix review

--- iotdb-core/datanode/src/main/java/org/apache/iotdb/db/pipe/consensus/ConsensusPipeDataNodeDispatcher.java
+++ iotdb-core/datanode/src/main/java/org/apache/iotdb/db/pipe/consensus/ConsensusPipeDataNodeDispatcher.java
@@ -44,9 +44,9 @@ public class ConsensusPipeDataNodeDispatcher implements ConsensusPipeDispatcher 
   private static final IClientManager<ConfigRegionId, ConfigNodeClient> CONFIG_NODE_CLIENT_MANAGER =
       ConfigNodeClientManager.getInstance();
 
-  private static final String PIPE_ALREADY_EXIST_MSG =
+  public static final String PIPE_ALREADY_EXIST_MSG =
       "the pipe with the same name has been created";
-  private static final String PIPE_NOT_EXIST_MGS = "does not exist";
+  public static final String PIPE_NOT_EXIST_MSG = "the pipe with the same name does not exist";
 
   @Override
   public void createPipe(
@@ -120,7 +120,7 @@ public class ConsensusPipeDataNodeDispatcher implements ConsensusPipeDispatcher 
       if (TSStatusCode.SUCCESS_STATUS.getStatusCode() != status.getCode()) {
         LOGGER.warn("Failed to drop consensus pipe-{}, status: {}", pipeName, status);
         // ignore idempotence logic
-        if (status.getMessage().contains(PIPE_NOT_EXIST_MGS)) {
+        if (status.getMessage().contains(PIPE_NOT_EXIST_MSG)) {
           return;
         }
         throw new PipeException(status.getMessage());
