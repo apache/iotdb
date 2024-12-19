4328bd4434b5a4251c248adf1dcb3e1c194cd05b
Caideyipi <87789683+Caideyipi@users.noreply.github.com>
2024-12-19 18:13:27 +0800

    Update TreeDeviceViewUpdater.java

--- iotdb-core/confignode/src/main/java/org/apache/iotdb/confignode/persistence/schema/TreeDeviceViewUpdater.java
+++ iotdb-core/confignode/src/main/java/org/apache/iotdb/confignode/persistence/schema/TreeDeviceViewUpdater.java
@@ -25,6 +25,7 @@ import org.apache.iotdb.common.rpc.thrift.TRegionReplicaSet;
 import org.apache.iotdb.common.rpc.thrift.TSStatus;
 import org.apache.iotdb.commons.concurrent.IoTDBThreadPoolFactory;
 import org.apache.iotdb.commons.concurrent.ThreadName;
+import org.apache.iotdb.commons.schema.table.TsTable;
 import org.apache.iotdb.commons.service.AbstractPeriodicalServiceWithAdvance;
 import org.apache.iotdb.confignode.client.async.CnToDnAsyncRequestType;
 import org.apache.iotdb.confignode.conf.ConfigNodeDescriptor;
@@ -40,11 +41,13 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.function.BiFunction;
 
 public class TreeDeviceViewUpdater extends AbstractPeriodicalServiceWithAdvance {
 
   private static final Logger LOGGER = LoggerFactory.getLogger(TreeDeviceViewUpdater.class);
+  private final Map<String, TsTable> newViewTables = new ConcurrentHashMap<>();
 
   public TreeDeviceViewUpdater() {
     super(
@@ -56,10 +59,12 @@ public class TreeDeviceViewUpdater extends AbstractPeriodicalServiceWithAdvance 
   @Override
   protected void executeTask() {}
 
+  public Map<String, TsTable> getNewViewTables() {
+    return newViewTables;
+  }
+
   protected class TreeDeviceUpdateTaskExecutor<Q>
       extends DataNodeRegionTaskExecutor<Q, TDeviceViewResp> {
-
-    private final String taskName;
 
     protected TreeDeviceUpdateTaskExecutor(
         final String taskName,
@@ -68,7 +73,6 @@ public class TreeDeviceViewUpdater extends AbstractPeriodicalServiceWithAdvance 
         final CnToDnAsyncRequestType dataNodeRequestType,
         final BiFunction<TDataNodeLocation, List<TConsensusGroupId>, Q> dataNodeRequestGenerator) {
       super(env, targetRegionGroup, false, dataNodeRequestType, dataNodeRequestGenerator);
-      this.taskName = taskName;
     }
 
     @Override
