4efb7181e9bda129a50e0ba9ab8f92ede22785da
Caideyipi <87789683+Caideyipi@users.noreply.github.com>
2025-01-02 22:59:51 +0800

    rename

--- iotdb-core/datanode/src/test/java/org/apache/iotdb/db/queryengine/plan/relational/analyzer/TestMetadata.java
+++ iotdb-core/datanode/src/test/java/org/apache/iotdb/db/queryengine/plan/relational/analyzer/TestMetadata.java
@@ -93,7 +93,7 @@ import static org.apache.tsfile.read.common.type.DoubleType.DOUBLE;
 import static org.apache.tsfile.read.common.type.LongType.INT64;
 import static org.apache.tsfile.read.common.type.TimestampType.TIMESTAMP;
 
-public class TestMatadata implements Metadata {
+public class TestMetadata implements Metadata {
 
   private final TypeManager typeManager = new InternalTypeManager();
 
