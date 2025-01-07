582555509bbd02aa66e6f65f20c57de34c133c09
Caideyipi <87789683+Caideyipi@users.noreply.github.com>
2025-01-07 17:56:40 +0800

    reopen

--- iotdb-core/datanode/src/main/java/org/apache/iotdb/db/queryengine/plan/relational/sql/ast/DropColumn.java
+++ iotdb-core/datanode/src/main/java/org/apache/iotdb/db/queryengine/plan/relational/sql/ast/DropColumn.java
@@ -19,8 +19,6 @@
 
 package org.apache.iotdb.db.queryengine.plan.relational.sql.ast;
 
-import org.apache.iotdb.db.exception.sql.SemanticException;
-
 import com.google.common.collect.ImmutableList;
 
 import java.util.List;
@@ -38,6 +36,18 @@ public class DropColumn extends Statement {
   private final boolean columnIfExists;
 
   public DropColumn(
+      final QualifiedName table,
+      final Identifier field,
+      final boolean tableIfExists,
+      final boolean columnIfExists) {
+    super(null);
+    this.table = requireNonNull(table, "table is null");
+    this.field = requireNonNull(field, "field is null");
+    this.tableIfExists = tableIfExists;
+    this.columnIfExists = columnIfExists;
+  }
+
+  public DropColumn(
       final NodeLocation location,
       final QualifiedName table,
       final Identifier field,
@@ -48,7 +58,6 @@ public class DropColumn extends Statement {
     this.field = requireNonNull(field, "field is null");
     this.tableIfExists = tableIfExists;
     this.columnIfExists = columnIfExists;
-    throw new SemanticException("Drop column is unsupported yet.");
   }
 
   public QualifiedName getTable() {
