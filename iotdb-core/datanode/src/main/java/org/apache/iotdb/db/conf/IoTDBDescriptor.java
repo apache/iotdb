7fe1f855f43e4924f9772a8c4996a70f08c2e1df
shizy <shizy04@gmail.com>
2025-01-14 14:16:24 +0800

    fix: hot-load tvlist_sort_threshold setting

--- iotdb-core/datanode/src/main/java/org/apache/iotdb/db/conf/IoTDBDescriptor.java
+++ iotdb-core/datanode/src/main/java/org/apache/iotdb/db/conf/IoTDBDescriptor.java
@@ -2069,11 +2069,9 @@ public class IoTDBDescriptor {
       // tvlist_sort_threshold
       conf.setTVListSortThreshold(
           Integer.parseInt(
-              Optional.ofNullable(
-                      properties.getProperty(
-                          "tvlist_sort_threshold", String.valueOf(conf.getTvListSortThreshold())))
-                  .map(String::trim)
-                  .orElse(String.valueOf(conf.getTvListSortThreshold()))));
+              properties.getProperty(
+                  "tvlist_sort_threshold",
+                  ConfigurationFileUtils.getConfigurationDefaultValue("tvlist_sort_threshold"))));
     } catch (Exception e) {
       if (e instanceof InterruptedException) {
         Thread.currentThread().interrupt();
