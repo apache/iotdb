042ac03b838c45a2bc7e149ba55dfa12e8636dc9
Caideyipi <87789683+Caideyipi@users.noreply.github.com>
2025-01-20 12:27:09 +0800

    Update InformationSchemaContentSupplierFactory.java

--- iotdb-core/datanode/src/main/java/org/apache/iotdb/db/queryengine/execution/operator/source/relational/InformationSchemaContentSupplierFactory.java
+++ iotdb-core/datanode/src/main/java/org/apache/iotdb/db/queryengine/execution/operator/source/relational/InformationSchemaContentSupplierFactory.java
@@ -310,9 +310,15 @@ public class InformationSchemaContentSupplierFactory {
           new Binary(
               preDeletedColumns.contains(schema.getColumnName()) ? "PRE_DELETE" : "USING",
               TSFileConfig.STRING_CHARSET));
-      columnBuilders[6].writeBinary(
-          new Binary(
-              schema.getProps().get(TsTableColumnSchema.COMMENT_KEY), TSFileConfig.STRING_CHARSET));
+
+      if (schema.getProps().containsKey(TsTableColumnSchema.COMMENT_KEY)) {
+        columnBuilders[6].writeBinary(
+            new Binary(
+                schema.getProps().get(TsTableColumnSchema.COMMENT_KEY),
+                TSFileConfig.STRING_CHARSET));
+      } else {
+        columnBuilders[6].appendNull();
+      }
       resultBuilder.declarePosition();
     }
 
