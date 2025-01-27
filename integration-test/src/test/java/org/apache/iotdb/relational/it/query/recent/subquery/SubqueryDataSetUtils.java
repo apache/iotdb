27e721b51e19009d79ea0ae8dac5fbe3ae9ec495
lancelly <1435078631@qq.com>
2025-01-27 16:50:39 +0800

    add some ITs

--- integration-test/src/test/java/org/apache/iotdb/relational/it/query/recent/subquery/SubqueryDataSetUtils.java
+++ integration-test/src/test/java/org/apache/iotdb/relational/it/query/recent/subquery/SubqueryDataSetUtils.java
@@ -113,10 +113,4 @@ public class SubqueryDataSetUtils {
         "FLUSH",
         "CLEAR ATTRIBUTE CACHE",
       };
-
-    public static void main(String[] args) {
-      for(String sql :CREATE_SQLS){
-        System.out.println(sql+";");
-      }
-    }
 }
