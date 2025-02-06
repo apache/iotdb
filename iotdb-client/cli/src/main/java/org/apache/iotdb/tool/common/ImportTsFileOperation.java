2c5ab2a9d830ab48b5f682c5fc649133b849abda
2b3c511 <rong.li@timecho.com>
2025-02-06 12:03:35 +0800

    add license and Test

--- iotdb-client/cli/src/main/java/org/apache/iotdb/tool/common/ImportTsFileOperation.java
+++ iotdb-client/cli/src/main/java/org/apache/iotdb/tool/common/ImportTsFileOperation.java
@@ -1,4 +1,25 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 package org.apache.iotdb.tool.common;
+
+import org.apache.iotdb.cli.utils.IoTPrinter;
 
 public enum ImportTsFileOperation {
   NONE,
@@ -30,7 +51,7 @@ public enum ImportTsFileOperation {
       case "delete":
         return ImportTsFileOperation.DELETE;
       default:
-        // ioTPrinter.println("Args error: os/of must be one of none, mv, cp, delete");
+        new IoTPrinter(System.out).println("Args error: os/of must be one of none, mv, cp, delete");
         System.exit(Constants.CODE_ERROR);
         return null;
     }
