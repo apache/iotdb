8980c5eb5427a0f1f0dbb295bea3a73e22a5506d
Haonan <hhaonan@outlook.com>
2025-02-13 15:37:34 +0800

    Check time precision for negative timestamps (#14828)

--- integration-test/src/test/java/org/apache/iotdb/db/it/IoTDBInsertMultiRowIT.java
+++ integration-test/src/test/java/org/apache/iotdb/db/it/IoTDBInsertMultiRowIT.java
@@ -171,6 +171,13 @@ public class IoTDBInsertMultiRowIT {
     } catch (SQLException e) {
       assertTrue(e.getMessage().contains("Current system timestamp precision is ms"));
     }
+    try (Statement st1 = connection.createStatement()) {
+      st1.execute(
+          "insert into root.t1.d99.wt01(timestamp, s1, s2) values(-1618283005586000, 1, 1), (-1618283005586001, 1, 2)");
+      fail();
+    } catch (SQLException e) {
+      assertTrue(e.getMessage().contains("Current system timestamp precision is ms"));
+    }
   }
 
   @Test
