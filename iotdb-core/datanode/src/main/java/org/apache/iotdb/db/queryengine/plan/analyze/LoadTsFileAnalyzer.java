8ebfcb99883be1f9f61b56d0b7c9056d54b01414
Zikun Ma <55695098+DanielWang2035@users.noreply.github.com>
2025-02-14 18:00:45 +0800

    Load: convert to insert tablet on region replica set changes (#14717) (#14833)
    * Load: convert to insert tablet on region replica set changes (#14717)
    
    (cherry picked from commit 7ac71fb2a33fdaae4fe56804ce0a2c48640dbed8)
    
    * resolve

--- iotdb-core/datanode/src/main/java/org/apache/iotdb/db/queryengine/plan/analyze/LoadTsFileAnalyzer.java
+++ iotdb-core/datanode/src/main/java/org/apache/iotdb/db/queryengine/plan/analyze/LoadTsFileAnalyzer.java
@@ -236,9 +236,16 @@ public class LoadTsFileAnalyzer implements AutoCloseable {
             : null;
 
     if (status == null) {
+      LOGGER.warn(
+          "Load: Failed to convert to tablets from statement {}. Status is null.",
+          loadTsFileStatement);
       analysis.setFailStatus(
           new TSStatus(TSStatusCode.LOAD_FILE_ERROR.getStatusCode()).setMessage(e.getMessage()));
     } else if (!loadTsFileDataTypeConverter.isSuccessful(status)) {
+      LOGGER.warn(
+          "Load: Failed to convert to tablets from statement {}. Status: {}",
+          loadTsFileStatement,
+          status);
       analysis.setFailStatus(status);
     }
 
