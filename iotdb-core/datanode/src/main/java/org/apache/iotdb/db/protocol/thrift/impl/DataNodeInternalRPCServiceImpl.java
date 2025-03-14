b892e5d8a7369dfea2cba2f1db83562b8c07ea7b
Steve Yurong Su <rong@apache.org>
2025-03-14 11:11:04 +0800

    Update DataNodeInternalRPCServiceImpl.java

--- iotdb-core/datanode/src/main/java/org/apache/iotdb/db/protocol/thrift/impl/DataNodeInternalRPCServiceImpl.java
+++ iotdb-core/datanode/src/main/java/org/apache/iotdb/db/protocol/thrift/impl/DataNodeInternalRPCServiceImpl.java
@@ -304,6 +304,7 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.Optional;
 import java.util.Set;
 import java.util.concurrent.TimeUnit;
