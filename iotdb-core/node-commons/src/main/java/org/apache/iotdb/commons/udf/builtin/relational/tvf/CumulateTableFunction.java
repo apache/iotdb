7228d9722931000d6b66d38a279a647905a82de5
shizy <shizy04@gmail.com>
2025-04-18 16:23:16 +0800

    license & cumulate process

--- iotdb-core/node-commons/src/main/java/org/apache/iotdb/commons/udf/builtin/relational/tvf/CumulateTableFunction.java
+++ iotdb-core/node-commons/src/main/java/org/apache/iotdb/commons/udf/builtin/relational/tvf/CumulateTableFunction.java
@@ -1,3 +1,22 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 package org.apache.iotdb.commons.udf.builtin.relational.tvf;
 
 import org.apache.iotdb.udf.api.exception.UDFException;
@@ -118,14 +137,12 @@ public class CumulateTableFunction implements TableFunction {
       // find the first windows
       long timeValue = input.getLong(0);
       long window_start = (timeValue - start) / size * size;
-      long steps = step;
-      while (window_start <= timeValue && window_start + steps <= window_start + size) {
-        if (window_start + steps > timeValue) {
-          properColumnBuilders.get(0).writeLong(window_start);
-          properColumnBuilders.get(1).writeLong(window_start + steps);
-          passThroughIndexBuilder.writeLong(curIndex);
-        }
-        steps += step;
+      for (long steps = (timeValue - window_start + step) / step * step;
+          steps <= size;
+          steps += step) {
+        properColumnBuilders.get(0).writeLong(window_start);
+        properColumnBuilders.get(1).writeLong(window_start + steps);
+        passThroughIndexBuilder.writeLong(curIndex);
       }
       curIndex++;
     }
