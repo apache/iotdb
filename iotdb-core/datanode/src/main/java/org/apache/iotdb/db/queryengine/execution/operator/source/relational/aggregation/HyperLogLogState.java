4aca10952c62352faa95cd22a289dc5ef027abe1
FearfulTomcat27 <1471335448@qq.com>
2025-04-24 11:42:20 +0800

    fix: Add HyperLogLogBigArray and delete unused class

--- iotdb-core/datanode/src/main/java/org/apache/iotdb/db/queryengine/execution/operator/source/relational/aggregation/HyperLogLogState.java
+++
@@ -1,7 +0,0 @@
-package org.apache.iotdb.db.queryengine.execution.operator.source.relational.aggregation;
-
-public interface HyperLogLogState {
-  HyperLogLog getHyperLogLog();
-
-  void setHyperLogLog(HyperLogLog value);
-}
