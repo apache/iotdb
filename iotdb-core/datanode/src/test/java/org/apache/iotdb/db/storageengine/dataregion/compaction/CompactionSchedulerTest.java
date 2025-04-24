786286f9188a70ba77e52b211c75746cf73bdd9b
spricoder <spricoder@qq.com>
2025-04-24 09:32:17 +0800

    fix ut test6

--- iotdb-core/datanode/src/test/java/org/apache/iotdb/db/storageengine/dataregion/compaction/CompactionSchedulerTest.java
+++ iotdb-core/datanode/src/test/java/org/apache/iotdb/db/storageengine/dataregion/compaction/CompactionSchedulerTest.java
@@ -673,7 +673,7 @@ public class CompactionSchedulerTest {
   @Test
   public void test6() throws IOException, MetadataException, InterruptedException {
     BatchCompactionPlan.setMaxCachedTimeChunksSize(2 * 1024 * 1024 / 30);
-    TSFileDescriptor.getInstance().getConfig().setPageSizeInByte(1024);
+    TSFileDescriptor.getInstance().getConfig().setPageSizeInByte(100);
     logger.warn("Running test6");
     boolean prevEnableSeqSpaceCompaction =
         IoTDBDescriptor.getInstance().getConfig().isEnableSeqSpaceCompaction();
