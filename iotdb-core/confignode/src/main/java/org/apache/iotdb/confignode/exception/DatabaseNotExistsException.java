905e5d04e0bf4d6b4e674c0ca28a12ff40c9a877
Caideyipi <87789683+Caideyipi@users.noreply.github.com>
2025-04-25 18:22:01 +0800

    Update DatabaseNotExistsException.java

--- iotdb-core/confignode/src/main/java/org/apache/iotdb/confignode/exception/DatabaseNotExistsException.java
+++ iotdb-core/confignode/src/main/java/org/apache/iotdb/confignode/exception/DatabaseNotExistsException.java
@@ -19,9 +19,16 @@
 
 package org.apache.iotdb.confignode.exception;
 
+import org.apache.iotdb.db.protocol.session.IClientSession;
+import org.apache.iotdb.db.protocol.session.SessionManager;
+
 public class DatabaseNotExistsException extends ConfigNodeException {
 
-  public DatabaseNotExistsException(String database) {
+  public DatabaseNotExistsException(final String database) {
     super(String.format("Database: %s doesn't exist.", database));
+    final IClientSession session = SessionManager.getInstance().getCurrSession();
+    if (database.equals(session.getDatabaseName())) {
+      session.setDatabaseName(null);
+    }
   }
 }
