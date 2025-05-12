93c6199bd80e6ccaeb3961f5092731929dc3ccaa
Caideyipi <87789683+Caideyipi@users.noreply.github.com>
2025-05-12 17:59:42 +0800

    Update OpcDaServerHandle.java

--- iotdb-core/datanode/src/main/java/org/apache/iotdb/db/pipe/connector/protocol/opcda/OpcDaServerHandle.java
+++ iotdb-core/datanode/src/main/java/org/apache/iotdb/db/pipe/connector/protocol/opcda/OpcDaServerHandle.java
@@ -181,7 +181,7 @@ public class OpcDaServerHandle implements Closeable {
   }
 
   void transfer(final Tablet tablet) {
-    new PipeTreeModelTabletEventSorter(tablet).deduplicateAndSortTimestampsIfNecessary();
+    new PipeTreeModelTabletEventSorter(tablet).deDuplicateAndSortTimestampsIfNecessary();
     final List<IMeasurementSchema> schemas = tablet.getSchemas();
 
     for (int i = 0; i < schemas.size(); ++i) {
