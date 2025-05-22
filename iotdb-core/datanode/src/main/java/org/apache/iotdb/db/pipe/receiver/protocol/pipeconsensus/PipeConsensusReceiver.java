5700de6b398dd863cb86f58aa82cbe87a6d729c3
Peng Junzhi <78788603+Pengzna@users.noreply.github.com>
2025-05-22 09:33:00 +0800

    reset metric (#15556)

--- iotdb-core/datanode/src/main/java/org/apache/iotdb/db/pipe/receiver/protocol/pipeconsensus/PipeConsensusReceiver.java
+++ iotdb-core/datanode/src/main/java/org/apache/iotdb/db/pipe/receiver/protocol/pipeconsensus/PipeConsensusReceiver.java
@@ -1659,6 +1659,8 @@ public class PipeConsensusReceiver {
       try {
         this.reqExecutionOrderBuffer.clear();
         this.tsFileWriterPool.releaseAllWriters(consensusPipeName);
+        this.tsFileEventCount.set(0);
+        this.WALEventCount.set(0);
         if (resetSyncIndex) {
           this.onSyncedReplicateIndex = 0;
         }
