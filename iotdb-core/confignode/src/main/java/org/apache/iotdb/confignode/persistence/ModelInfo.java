df052b59044bd5f7a365ed77b3eaf4e58940659a
YangCaiyin <ycycse@gmail.com>
2025-05-26 22:02:44 +0800

    set the max line number of timer_XL to 2880

--- iotdb-core/confignode/src/main/java/org/apache/iotdb/confignode/persistence/ModelInfo.java
+++ iotdb-core/confignode/src/main/java/org/apache/iotdb/confignode/persistence/ModelInfo.java
@@ -74,6 +74,8 @@ public class ModelInfo implements SnapshotProcessor {
 
   private static final Set<String> builtInAnomalyDetectionModel = new HashSet<>();
 
+  private static final int timerXLInputLength = 2880;
+
   static {
     builtInForecastModel.add("_TimerXL");
     builtInForecastModel.add("_ARIMA");
@@ -270,6 +272,9 @@ public class ModelInfo implements SnapshotProcessor {
       // check if it's a built-in model
       if ((modelType = checkModelType(modelName)) != ModelType.USER_DEFINED) {
         modelInformation = new ModelInformation(modelType, modelName);
+        if (modelName.equalsIgnoreCase("_timerxl")) {
+          modelInformation.setInputLength(timerXLInputLength);
+        }
       } else {
         modelInformation = modelTable.getModelInformationById(modelName);
       }
