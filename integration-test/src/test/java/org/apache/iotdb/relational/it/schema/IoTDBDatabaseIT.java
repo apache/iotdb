a8a7e533a50dabd6936fccf96f73b6d1a20fdfc6
Yongzao <532741407@qq.com>
2025-05-29 18:41:46 +0800

    Update IoTDBDatabaseIT.java

--- integration-test/src/test/java/org/apache/iotdb/relational/it/schema/IoTDBDatabaseIT.java
+++ integration-test/src/test/java/org/apache/iotdb/relational/it/schema/IoTDBDatabaseIT.java
@@ -699,7 +699,6 @@ public class IoTDBDatabaseIT {
           "model_id,",
           new HashSet<>(
               Arrays.asList(
-                  "_TimerXL,",
                   "_STLForecaster,",
                   "_NaiveForecaster,",
                   "_HoltWinters,",
