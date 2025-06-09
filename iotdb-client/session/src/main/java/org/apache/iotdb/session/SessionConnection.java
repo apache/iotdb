53121ed18608a29ebc04fd92065c7fa423b07f8d
HTHou <haonan@apache.org>
2025-06-09 17:08:41 +0800

    Optimize the implement

--- iotdb-client/session/src/main/java/org/apache/iotdb/session/SessionConnection.java
+++ iotdb-client/session/src/main/java/org/apache/iotdb/session/SessionConnection.java
@@ -1053,18 +1053,18 @@ public class SessionConnection {
         if (session.endPointToSessionConnection == null) {
           session.endPointToSessionConnection = new ConcurrentHashMap<>();
         }
-        SessionConnection previousConnection =
-            session.endPointToSessionConnection.get(this.endPoint);
-        if (previousConnection != null
-            && previousConnection.transport != null
-            && previousConnection.transport.isOpen()) {
-          try {
-            previousConnection.close();
-          } catch (IoTDBConnectionException e) {
-            logger.warn("close connection failed, {}", e.getMessage());
-          }
-        }
-        session.endPointToSessionConnection.put(session.defaultEndPoint, this);
+        session.endPointToSessionConnection.compute(
+            session.defaultEndPoint,
+            (k, v) -> {
+              if (v != null && v.transport != null && v.transport.isOpen()) {
+                try {
+                  v.close();
+                } catch (IoTDBConnectionException e) {
+                  logger.warn("close connection failed, {}", e.getMessage());
+                }
+              }
+              return this;
+            });
         break;
       }
     }
