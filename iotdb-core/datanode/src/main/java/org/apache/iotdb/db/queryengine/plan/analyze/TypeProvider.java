03759c1405acc715df95a6c53942b9284f02f4d3
Weihao Li <18110526956@163.com>
2025-06-17 17:36:45 +0800

    fix CI
    Signed-off-by: Weihao Li <18110526956@163.com>

--- iotdb-core/datanode/src/main/java/org/apache/iotdb/db/queryengine/plan/analyze/TypeProvider.java
+++ iotdb-core/datanode/src/main/java/org/apache/iotdb/db/queryengine/plan/analyze/TypeProvider.java
@@ -21,6 +21,7 @@ package org.apache.iotdb.db.queryengine.plan.analyze;
 
 import org.apache.tsfile.enums.TSDataType;
 import org.apache.tsfile.utils.ReadWriteIOUtils;
+import org.apache.tsfile.write.schema.IMeasurementSchema;
 
 import java.io.DataOutputStream;
 import java.io.IOException;
@@ -53,7 +54,8 @@ public class TypeProvider {
     if (templatedInfo == null || type != null) {
       return type;
     }
-    return templatedInfo.getSchemaMap().get(symbol).getType();
+    IMeasurementSchema schema = templatedInfo.getSchemaMap().get(symbol);
+    return schema == null ? null : schema.getType();
   }
 
   public void setType(String symbol, TSDataType dataType) {
