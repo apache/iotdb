c0ed35b85555384f580c73d18691efe860b0a28d
shuwenwei <s13979062948@gmail.com>
2025-06-18 11:28:23 +0800

    remove debug code

--- iotdb-core/datanode/src/main/java/org/apache/iotdb/db/queryengine/plan/planner/OperatorTreeGenerator.java
+++ iotdb-core/datanode/src/main/java/org/apache/iotdb/db/queryengine/plan/planner/OperatorTreeGenerator.java
@@ -3007,7 +3007,6 @@ public class OperatorTreeGenerator extends PlanVisitor<Operator, LocalExecutionP
   @Override
   public Operator visitDeviceLastQueryScan(
       DeviceLastQueryScanNode node, LocalExecutionPlanContext context) {
-    DATA_NODE_SCHEMA_CACHE.cleanUp();
     final PartialPath devicePath = node.getDevicePath();
     List<IMeasurementSchema> measurementSchemas = node.getMeasurementSchemas();
     List<Integer> unCachedMeasurementIndexes = new ArrayList<>();
