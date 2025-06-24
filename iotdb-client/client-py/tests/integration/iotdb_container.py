36f72a8c2b0b23c810a6a3bc2432a08facd10528
Tian Jiang <jt2594838@163.com>
2025-06-24 10:54:09 +0800

    format

--- iotdb-client/client-py/tests/integration/iotdb_container.py
+++ iotdb-client/client-py/tests/integration/iotdb_container.py
@@ -39,8 +39,10 @@ class IoTDBContainer(DockerContainer):
     @wait_container_is_ready()
     def _connect(self):
         session = Session(
-            self.get_container_host_ip(), self.get_exposed_port(6667), "root"
-            , "IoTDB@2017"
+            self.get_container_host_ip(),
+            self.get_exposed_port(6667),
+            "root",
+            "IoTDB@2017",
         )
         session.open(False)
         with session.execute_statement("SHOW CLUSTER") as session_data_set:
