cfc38e25681263ebcde76af922609ef84f1f3b66
Caideyipi <87789683+Caideyipi@users.noreply.github.com>
2025-06-27 18:31:49 +0800

    Update PipeResourceMetrics.java

--- iotdb-core/datanode/src/main/java/org/apache/iotdb/db/pipe/metric/overview/PipeResourceMetrics.java
+++ iotdb-core/datanode/src/main/java/org/apache/iotdb/db/pipe/metric/overview/PipeResourceMetrics.java
@@ -22,6 +22,7 @@ package org.apache.iotdb.db.pipe.metric.overview;
 import org.apache.iotdb.commons.pipe.resource.ref.PipePhantomReferenceManager;
 import org.apache.iotdb.commons.service.metric.enums.Metric;
 import org.apache.iotdb.commons.service.metric.enums.Tag;
+import org.apache.iotdb.db.pipe.agent.PipeDataNodeAgent;
 import org.apache.iotdb.db.pipe.resource.PipeDataNodeResourceManager;
 import org.apache.iotdb.db.pipe.resource.memory.PipeMemoryManager;
 import org.apache.iotdb.db.pipe.resource.tsfile.PipeTsFileResourceManager;
@@ -33,12 +34,15 @@ import org.apache.iotdb.metrics.utils.MetricType;
 public class PipeResourceMetrics implements IMetricSet {
 
   private static final String PIPE_USED_MEMORY = "PipeUsedMemory";
+  private static final String PIPE_USED_FLOATING_MEMORY = "PipeUsedFloatingMemory";
 
   private static final String PIPE_TABLET_USED_MEMORY = "PipeTabletUsedMemory";
 
   private static final String PIPE_TS_FILE_USED_MEMORY = "PipeTsFileUsedMemory";
 
   private static final String PIPE_TOTAL_MEMORY = "PipeTotalMemory";
+
+  private static final String PIPE_FLOATING_MEMORY = "PipeFloatingMemory";
 
   //////////////////////////// bindTo & unbindFrom (metric framework) ////////////////////////////
 
@@ -73,6 +77,20 @@ public class PipeResourceMetrics implements IMetricSet {
         o -> PipeMemoryManager.getTotalNonFloatingMemorySizeInBytes(),
         Tag.NAME.toString(),
         PIPE_TOTAL_MEMORY);
+    metricService.createAutoGauge(
+        Metric.PIPE_MEM.toString(),
+        MetricLevel.IMPORTANT,
+        PipeDataNodeResourceManager.memory(),
+        o -> PipeMemoryManager.getTotalFloatingMemorySizeInBytes(),
+        Tag.NAME.toString(),
+        PIPE_FLOATING_MEMORY);
+    metricService.createAutoGauge(
+        Metric.PIPE_MEM.toString(),
+        MetricLevel.IMPORTANT,
+        PipeDataNodeResourceManager.memory(),
+        o -> PipeDataNodeAgent.task().getAllFloatingMemoryUsageInByte(),
+        Tag.NAME.toString(),
+        PIPE_USED_FLOATING_MEMORY);
     // resource reference count
     metricService.createAutoGauge(
         Metric.PIPE_LINKED_TSFILE_COUNT.toString(),
@@ -109,6 +127,16 @@ public class PipeResourceMetrics implements IMetricSet {
         PIPE_TS_FILE_USED_MEMORY);
     metricService.remove(
         MetricType.AUTO_GAUGE, Metric.PIPE_MEM.toString(), Tag.NAME.toString(), PIPE_TOTAL_MEMORY);
+    metricService.remove(
+        MetricType.AUTO_GAUGE,
+        Metric.PIPE_MEM.toString(),
+        Tag.NAME.toString(),
+        PIPE_FLOATING_MEMORY);
+    metricService.remove(
+        MetricType.AUTO_GAUGE,
+        Metric.PIPE_MEM.toString(),
+        Tag.NAME.toString(),
+        PIPE_USED_FLOATING_MEMORY);
     // resource reference count
     metricService.remove(MetricType.AUTO_GAUGE, Metric.PIPE_LINKED_TSFILE_COUNT.toString());
     metricService.remove(MetricType.AUTO_GAUGE, Metric.PIPE_LINKED_TSFILE_SIZE.toString());
