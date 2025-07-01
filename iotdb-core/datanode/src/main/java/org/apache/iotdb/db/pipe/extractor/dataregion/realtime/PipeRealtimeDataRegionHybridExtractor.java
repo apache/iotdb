ebf297d65c287ed96d83851bbab922b5d2b259f8
Caideyipi <87789683+Caideyipi@users.noreply.github.com>
2025-07-01 14:24:32 +0800

    Update PipeRealtimeDataRegionHybridExtractor.java

--- iotdb-core/datanode/src/main/java/org/apache/iotdb/db/pipe/extractor/dataregion/realtime/PipeRealtimeDataRegionHybridExtractor.java
+++ iotdb-core/datanode/src/main/java/org/apache/iotdb/db/pipe/extractor/dataregion/realtime/PipeRealtimeDataRegionHybridExtractor.java
@@ -298,14 +298,14 @@ public class PipeRealtimeDataRegionHybridExtractor extends PipeRealtimeDataRegio
       return false;
     }
     final boolean mayTsFileLinkedCountReachDangerousThreshold =
-        PipeDataNodeResourceManager.tsfile().getLinkedTsFileCount()
+        PipeDataNodeResourceManager.tsfile().getLinkedTsFileCount(pipeName)
             >= PipeConfig.getInstance().getPipeMaxAllowedLinkedTsFileCount();
     if (mayTsFileLinkedCountReachDangerousThreshold && event.mayExtractorUseTablets(this)) {
       LOGGER.info(
           "Pipe task {}@{} canNotUseTabletAnymoreDeprecated(4): The number of linked tsFiles {} has reached the dangerous threshold {}",
           pipeName,
           dataRegionId,
-          PipeDataNodeResourceManager.tsfile().getLinkedTsFileCount(),
+          PipeDataNodeResourceManager.tsfile().getLinkedTsFileCount(pipeName),
           PipeConfig.getInstance().getPipeMaxAllowedLinkedTsFileCount());
     }
     return mayTsFileLinkedCountReachDangerousThreshold;
