07e0136231f08c082dcd298a3cbfc952404e33ba
Caideyipi <87789683+Caideyipi@users.noreply.github.com>
2025-07-04 12:19:14 +0800

    Update PipeEventCommitter.java

--- iotdb-core/node-commons/src/main/java/org/apache/iotdb/commons/pipe/agent/task/progress/PipeEventCommitter.java
+++ iotdb-core/node-commons/src/main/java/org/apache/iotdb/commons/pipe/agent/task/progress/PipeEventCommitter.java
@@ -88,12 +88,11 @@ public class PipeEventCommitter {
       final EnrichedEvent e = commitQueue.peek();
 
       if (e.getCommitId() <= lastCommitId.get()) {
-        LOGGER.warn(
-            "commit id must be monotonically increasing, current commit id: {}, last commit id: {}, event: {}, stack trace: {}",
+        LOGGER.info(
+            "commit id is not monotonically increasing, current commit id: {}, last commit id: {}, event: {}, may be because the tsFile has been compacted",
             e.getCommitId(),
             lastCommitId.get(),
-            e.coreReportMessage(),
-            Thread.currentThread().getStackTrace());
+            e.coreReportMessage());
         commitQueue.poll();
         continue;
       }
