99492cd50e3ea2f0eda11cdef8f34c8f1bef4f16
XNX02 <202100130044@mail.sdu.edu.cn>
2025-07-09 00:56:14 +0800

    update scripts

--- library-pipe/src/assembly/tools/register-pipeplugin.bat
+++ library-pipe/src/assembly/tools/register-pipeplugin.bat
@@ -17,12 +17,36 @@
 @REM under the License.
 @REM
 
-@REM Parameters
-@set host=127.0.0.1
-@set rpcPort=6667
-@set user=root
-@set pass=root
+@echo off
+setlocal enabledelayedexpansion
 
+REM ======================== Configuration ========================
+set host=127.0.0.1
+set rpcPort=6667
+set user=root
+set pass=root
 
-@REM Data Profiling
-call ../sbin/windows/start-cli.bat -h %host% -p %rpcPort% -u %user% -pw %pass% -e "create pipeplugin if not exists mqtt-extractor as 'org.apache.iotdb.libpipe.extractor.mqtt.MQTTExtractor' USING "
+REM ===================== Resolve JAR Path ========================
+set jar_abs_path=
+if not "%~1"=="" (
+  set jar_abs_path=%~1
+)
+if "%jar_abs_path%" == "" (
+  for %%I in ("..\ext\pipe\library-pipe.jar") do set jar_abs_path=%%~fI
+  echo [INFO] No jar path provided, using default: !jar_abs_path!
+)
+
+REM ======================= Convert to URI ========================
+if "!jar_abs_path:~0,8!"! == "https://" (
+    set uri_jar_path=!jar_abs_path!
+) else (
+    set uri_jar_path=!jar_abs_path:\=/!
+    set uri_jar_path=file:/!uri_jar_path!
+)
+
+echo [INFO] Using jar URI: !uri_jar_path!
+
+REM ================= Register Pipe Plugin ========================
+call ..\sbin\windows\start-cli.bat -h %host% -p %rpcPort% -u %user% -pw %pass% -e "create pipeplugin if not exists MQTTExtractor as 'org.apache.iotdb.libpipe.extractor.mqtt.MQTTExtractor' USING URI '!uri_jar_path!'"
+
+endlocal
