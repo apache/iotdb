977086e33bfd3e3a4fad5140e19eb2458e55a45a
Caideyipi <87789683+Caideyipi@users.noreply.github.com>
2025-07-10 12:07:50 +0800

    [To dev/1.3] Pipe: Enabled waiting for pipes to finish & progress index persist to config node in shutdown hook (#15896)(#15901)
    * Pipe: Enabled waiting for pipes to finish & progress index persist to config node in shutdown hook (#15896)
    
    * persist in shutdown hook
    
    * Update PipeTaskAgent.java
    
    * Update PipeConfigNodeTaskAgent.java
    
    * Update DataNodeShutdownHook.java
    
    * Fix
    
    * Fix2
    
    * Fix3
    
    * Update PipeHeartbeatScheduler.java
    
    * Update ShowPipeTask.java

--- iotdb-core/node-commons/src/main/java/org/apache/iotdb/commons/pipe/config/PipeConfig.java
+++ iotdb-core/node-commons/src/main/java/org/apache/iotdb/commons/pipe/config/PipeConfig.java
@@ -147,6 +147,10 @@ public class PipeConfig {
     return COMMON_CONFIG.getPipeSubtaskExecutorForcedRestartIntervalMs();
   }
 
+  public long getPipeMaxWaitFinishTime() {
+    return COMMON_CONFIG.getPipeMaxWaitFinishTime();
+  }
+
   /////////////////////////////// Extractor ///////////////////////////////
 
   public int getPipeExtractorAssignerDisruptorRingBufferSize() {
@@ -515,6 +519,7 @@ public class PipeConfig {
     LOGGER.info(
         "PipeSubtaskExecutorForcedRestartIntervalMs: {}",
         getPipeSubtaskExecutorForcedRestartIntervalMs());
+    LOGGER.info("PipeMaxWaitFinishTime: {}", getPipeMaxWaitFinishTime());
 
     LOGGER.info(
         "PipeExtractorAssignerDisruptorRingBufferSize: {}",
