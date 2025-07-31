c1c339c43113b6fda3baaad77c095ff02e103a37
Liu Zhengyun <lzy8006@outlook.com>
2025-07-31 17:07:00 +0800

    add stop interface

--- iotdb-core/ainode/ainode/core/ainode.py
+++ iotdb-core/ainode/ainode/core/ainode.py
@@ -159,7 +159,7 @@ class AINode:
     def stop(self):
         if not self._stop_event.is_set():
             self._stop_event.set()
-            self._rpc_handler._inference_manager.shutdown()
+            self._rpc_handler.stop()
             if self._rpc_service:
                 self._rpc_service.stop()
                 self._rpc_service.join(1)
