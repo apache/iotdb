4e60d4a7ba4b76ed5901a3878a2f06384df8cbde
shuwenwei <s13979062948@gmail.com>
2025-08-01 16:19:38 +0800

    fix compile

--- iotdb-core/datanode/src/test/java/org/apache/iotdb/db/storageengine/buffer/BloomFilterCacheTest.java
+++ iotdb-core/datanode/src/test/java/org/apache/iotdb/db/storageengine/buffer/BloomFilterCacheTest.java
@@ -97,7 +97,7 @@ public class BloomFilterCacheTest {
         TsFileID tsFileID = new TsFileID(filePath);
         BloomFilter bloomFilter =
             bloomFilterCache.get(new BloomFilterCache.BloomFilterCacheKey(filePath, tsFileID));
-        TsFileSequenceReader reader = FileReaderManager.getInstance().get(filePath, true);
+        TsFileSequenceReader reader = FileReaderManager.getInstance().get(filePath, tsFileID, true);
         BloomFilter bloomFilter1 = reader.readBloomFilter();
         Assert.assertEquals(bloomFilter1, bloomFilter);
         reader.close();
@@ -116,7 +116,7 @@ public class BloomFilterCacheTest {
       BloomFilterCache.BloomFilterCacheKey key =
           new BloomFilterCache.BloomFilterCacheKey(path, tsFileID);
       BloomFilter bloomFilter = bloomFilterCache.get(key);
-      TsFileSequenceReader reader = FileReaderManager.getInstance().get(path, true);
+      TsFileSequenceReader reader = FileReaderManager.getInstance().get(path, tsFileID, true);
       BloomFilter bloomFilter1 = reader.readBloomFilter();
       Assert.assertEquals(bloomFilter1, bloomFilter);
       bloomFilterCache.remove(key);
@@ -137,7 +137,7 @@ public class BloomFilterCacheTest {
         BloomFilterCache.BloomFilterCacheKey key =
             new BloomFilterCache.BloomFilterCacheKey(path, tsFileID);
         BloomFilter bloomFilter = bloomFilterCache.get(key);
-        TsFileSequenceReader reader = FileReaderManager.getInstance().get(path, true);
+        TsFileSequenceReader reader = FileReaderManager.getInstance().get(path, tsFileID, true);
         BloomFilter bloomFilter1 = reader.readBloomFilter();
         Assert.assertEquals(bloomFilter1, bloomFilter);
         reader.close();
