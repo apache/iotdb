6c737ae3f77c13e9d7a23d9dd28a1c9f66fa7303
luoluoyuyu <zhenyu@apache.org>
2025-08-01 19:20:55 +0800

    spotless

--- iotdb-core/datanode/src/main/java/org/apache/iotdb/db/pipe/receiver/protocol/thrift/IoTDBDataNodeReceiver.java
+++ iotdb-core/datanode/src/main/java/org/apache/iotdb/db/pipe/receiver/protocol/thrift/IoTDBDataNodeReceiver.java
@@ -908,12 +908,12 @@ public class IoTDBDataNodeReceiver extends IoTDBFileReceiver {
 
   @Override
   protected TSStatus login() {
-    final IClientSession session = SessionManager.getInstance().getCurrSession();
+    final IClientSession session = SESSION_MANAGER.getCurrSession();
 
     if (session != null && !session.isLogin()) {
       final BasicOpenSessionResp openSessionResp =
           SESSION_MANAGER.login(
-              SESSION_MANAGER.getCurrSession(),
+              session,
               username,
               password,
               ZoneId.systemDefault().toString(),
