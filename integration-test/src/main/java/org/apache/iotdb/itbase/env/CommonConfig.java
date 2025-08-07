e90df52fea5fc9525c89d2823750afee965f2c45
luoluoyuyu <zhenyu@apache.org>
2025-08-07 15:11:44 +0800

    add it

--- integration-test/src/main/java/org/apache/iotdb/itbase/env/CommonConfig.java
+++ integration-test/src/main/java/org/apache/iotdb/itbase/env/CommonConfig.java
@@ -175,4 +175,6 @@ public interface CommonConfig {
   default CommonConfig setDefaultStorageGroupLevel(int defaultStorageGroupLevel) {
     return this;
   }
+
+  CommonConfig setDatanodeMemoryProportion(String datanodeMemoryProportion);
 }
