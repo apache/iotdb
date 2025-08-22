232302446083a3c089cad08f5fc2fa89fb882789
彭俊植 <junzhi.peng@pingcap.com>
2025-08-22 10:06:17 +0800

    fix

--- iotdb-core/datanode/src/main/java/org/apache/iotdb/db/pipe/agent/task/stage/PipeTaskSinkStage.java
+++ iotdb-core/datanode/src/main/java/org/apache/iotdb/db/pipe/agent/task/stage/PipeTaskSinkStage.java
@@ -19,9 +19,7 @@
 
 package org.apache.iotdb.db.pipe.agent.task.stage;
 
-import org.apache.iotdb.commons.consensus.iotv2.container.IoTV2GlobalComponentContainer;
 import org.apache.iotdb.commons.pipe.agent.task.connection.UnboundedBlockingPendingQueue;
-import org.apache.iotdb.commons.pipe.agent.task.meta.PipeStaticMeta;
 import org.apache.iotdb.commons.pipe.agent.task.stage.PipeTaskStage;
 import org.apache.iotdb.commons.pipe.config.plugin.env.PipeTaskSinkRuntimeEnvironment;
 import org.apache.iotdb.db.pipe.agent.task.execution.PipeSinkSubtaskExecutor;
@@ -61,11 +59,7 @@ public class PipeTaskSinkStage extends PipeTaskStage {
     this.connectorSubtaskId =
         PipeSinkSubtaskManager.instance()
             .register(
-                // IoTV2 uses global singleton executor pool.
-                pipeName.startsWith(PipeStaticMeta.CONSENSUS_PIPE_PREFIX)
-                    ? (PipeSinkSubtaskExecutor)
-                        IoTV2GlobalComponentContainer.getInstance().getConsensusExecutor()
-                    : executor.get(),
+                executor,
                 pipeConnectorParameters,
                 new PipeTaskSinkRuntimeEnvironment(pipeName, creationTime, regionId));
   }
