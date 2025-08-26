d6b1e7f86186811656a26673b70107fd6f61b8ad
Liu Zhengyun <lzy8006@outlook.com>
2025-08-26 07:27:53 +0000

    Add request count for pools

--- iotdb-core/ainode/ainode/core/inference/pool_group.py
+++ iotdb-core/ainode/ainode/core/inference/pool_group.py
@@ -26,6 +26,7 @@ from ainode.core.exception import (
 from ainode.core.inference.dispatcher.basic_dispatcher import BasicDispatcher
 from ainode.core.inference.inference_request_pool import InferenceRequestPool, PoolState
 from ainode.core.log import Logger
+from ainode.core.util.atmoic_int import AtomicInt
 
 logger = Logger()
 
@@ -43,6 +44,8 @@ class PoolGroup:
         self.pool_group: Dict[int, Tuple[InferenceRequestPool, mp.Queue]] = {}
         # structure: {pool_id: PoolState}
         self.pool_states: Dict[int, PoolState] = {}
+        # structure: {pool_id: current_load}
+        self._loads: Dict[int, AtomicInt] = {}
         self.model_id = model_id
         self.request_dispatcher = BasicDispatcher(self.pool_states)
 
@@ -53,21 +56,32 @@ class PoolGroup:
         self, pool_id: int, request_pool: InferenceRequestPool, request_queue: mp.Queue
     ):
         self.pool_group[pool_id] = (request_pool, request_queue)
+        self._loads[pool_id] = AtomicInt(0)
 
     def remove_pool(self, pool_id: int):
         self.pool_group.pop(pool_id, None)
         self.pool_states.pop(pool_id, None)
+        self._loads.pop(pool_id, None)
 
     def get_pool_ids(self) -> list[int]:
         return list(self.pool_group.keys())
 
     def dispatch_request(self, req):
         pool_idx = self.request_dispatcher.dispatch_request(req, self.get_pool_ids())
+        req.assigned_pool_id = pool_idx
+        self._loads[pool_idx].incr()
         req_q = self.pool_group[pool_idx][1]
         req_q.put(req)
         logger.debug(
             f"[Inference][Device-{self.DEFAULT_DEVICE}][Pool-{pool_idx}][ID-{req.req_id}] Request is queued for inference"
         )
+
+    def remove_request(self, pool_id: int):
+        """
+        remove the request from the pool's load count
+        """
+        if pool_id in self._loads:
+            self._loads[pool_id].decr()
 
     def get_request_pool(self, pool_id) -> InferenceRequestPool:
         if pool_id not in self.pool_group:
@@ -90,4 +104,8 @@ class PoolGroup:
         self.pool_states[pool_id] = state
 
     def get_load(self, pool_id) -> int:
-        pass
+        if pool_id not in self._loads:
+            raise InferenceModelInternalError(
+                f"Pool ID {pool_id} not found for model {self.model_id}"
+            )
+        return self._loads[pool_id].get()
