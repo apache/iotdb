2ff5ae3e8d27b96e823685b333c6819d6957d024
Peng Junzhi <201250214@smail.nju.edu.cn>
2025-09-03 22:45:12 +0800

    fix

--- iotdb-core/datanode/src/main/java/org/apache/iotdb/db/pipe/consensus/PipeConsensusSubtaskExecutor.java
+++ iotdb-core/datanode/src/main/java/org/apache/iotdb/db/pipe/consensus/PipeConsensusSubtaskExecutor.java
@@ -26,28 +26,34 @@ import org.apache.iotdb.db.storageengine.dataregion.DataRegion;
 public class PipeConsensusSubtaskExecutor extends PipeSinkSubtaskExecutor {
   public PipeConsensusSubtaskExecutor() {
     super(
-        // The number of data regions for a datanode is limited by offHeapMemory. At the same time,
-        // in order to ensure multicore performance, the number of data regions usually does not
-        // exceed the number of cores. To prevent the thread from exploding, we take the min of
-        // both.
-        (int)
-            Math.min(
-                // NOTE1: The number of data regions is limited by the number of cores.
-                Runtime.getRuntime().availableProcessors(),
-                // NOTE2: The number of data regions is also limited by the offHeapMemory. In fact,
-                // schema regions also take up off-heap memory. So in fact, the soft cap of
-                // data region will be smaller than what is calculated here, but that's okay, we can
-                // set the core size quota of the Executor pool a little bit higher slightly
-                Math.max(
-                        IoTDBDescriptor.getInstance()
-                            .getMemoryConfig()
-                            .getDirectBufferMemoryManager()
-                            .getTotalMemorySizeInBytes(),
-                        IoTDBDescriptor.getInstance()
-                            .getMemoryConfig()
-                            .getOffHeapMemoryManager()
-                            .getTotalMemorySizeInBytes())
-                    / DataRegion.getAcquireDirectBufferMemCost()),
+        DataRegion.getAcquireDirectBufferMemCost() == 0
+            ? Runtime.getRuntime().availableProcessors()
+            :
+            // The number of data regions for a datanode is limited by offHeapMemory. At the same
+            // time,
+            // in order to ensure multicore performance, the number of data regions usually does not
+            // exceed the number of cores. To prevent the thread from exploding, we take the min of
+            // both.
+            (int)
+                Math.min(
+                    // NOTE1: The number of data regions is limited by the number of cores.
+                    Runtime.getRuntime().availableProcessors(),
+                    // NOTE2: The number of data regions is also limited by the offHeapMemory. In
+                    // fact,
+                    // schema regions also take up off-heap memory. So in fact, the soft cap of
+                    // data region will be smaller than what is calculated here, but that's okay, we
+                    // can
+                    // set the core size quota of the Executor pool a little bit higher slightly
+                    Math.max(
+                            IoTDBDescriptor.getInstance()
+                                .getMemoryConfig()
+                                .getDirectBufferMemoryManager()
+                                .getTotalMemorySizeInBytes(),
+                            IoTDBDescriptor.getInstance()
+                                .getMemoryConfig()
+                                .getOffHeapMemoryManager()
+                                .getTotalMemorySizeInBytes())
+                        / DataRegion.getAcquireDirectBufferMemCost()),
         ThreadName.PIPE_CONSENSUS_EXECUTOR_POOL.getName());
   }
 }
