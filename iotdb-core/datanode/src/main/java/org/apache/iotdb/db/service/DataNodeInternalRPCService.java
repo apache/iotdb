daa37d7f716340e0916040837eb9b51d3a33c212
Caideyipi <87789683+Caideyipi@users.noreply.github.com>
2025-09-10 12:31:38 +0800

    Fixed the potential NPE in clear all cache on local (#16378)

--- iotdb-core/datanode/src/main/java/org/apache/iotdb/db/service/DataNodeInternalRPCService.java
+++ iotdb-core/datanode/src/main/java/org/apache/iotdb/db/service/DataNodeInternalRPCService.java
@@ -33,10 +33,12 @@ import org.apache.iotdb.db.service.metrics.DataNodeInternalRPCServiceMetrics;
 import org.apache.iotdb.mpp.rpc.thrift.IDataNodeRPCService.Processor;
 import org.apache.iotdb.rpc.DeepCopyRpcTransportFactory;
 
+import java.util.concurrent.atomic.AtomicReference;
+
 public class DataNodeInternalRPCService extends ThriftService
     implements DataNodeInternalRPCServiceMBean {
 
-  private DataNodeInternalRPCServiceImpl impl;
+  private final AtomicReference<DataNodeInternalRPCServiceImpl> impl = new AtomicReference<>();
 
   private DataNodeInternalRPCService() {}
 
@@ -46,11 +48,10 @@ public class DataNodeInternalRPCService extends ThriftService
   }
 
   @Override
-  public void initTProcessor()
-      throws ClassNotFoundException, IllegalAccessException, InstantiationException {
-    impl = new DataNodeInternalRPCServiceImpl();
+  public void initTProcessor() {
+    impl.compareAndSet(null, new DataNodeInternalRPCServiceImpl());
     initSyncedServiceImpl(null);
-    processor = new Processor<>(impl);
+    processor = new Processor<>(impl.get());
   }
 
   @Override
@@ -89,7 +90,8 @@ public class DataNodeInternalRPCService extends ThriftService
   }
 
   public DataNodeInternalRPCServiceImpl getImpl() {
-    return impl;
+    impl.compareAndSet(null, new DataNodeInternalRPCServiceImpl());
+    return impl.get();
   }
 
   private static class DataNodeInternalRPCServiceHolder {
