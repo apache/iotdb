8e6907dd4e1ba6456a9657f6c3afeb76609d2e84
Zhenyu Luo <zhenyu@apache.org>
2025-09-12 10:04:31 +0800

    Pipe: Avoid throwing null pointer during Close process (#16391)
    * Pipe: Avoid throwing null pointer during Close process
    
    * fix
    
    * fix
    
    (cherry picked from commit 1bc2c7f0f615600b52e3ee20ef2f6ca44f67df0c)

--- iotdb-core/datanode/src/main/java/org/apache/iotdb/db/pipe/sink/client/IoTDBDataNodeSyncClientManager.java
+++ iotdb-core/datanode/src/main/java/org/apache/iotdb/db/pipe/sink/client/IoTDBDataNodeSyncClientManager.java
@@ -93,6 +93,7 @@ public class IoTDBDataNodeSyncClientManager extends IoTDBSyncClientManager
             && endPoint != null
             && endPoint2ClientAndStatus.containsKey(endPoint)
             && Boolean.TRUE.equals(endPoint2ClientAndStatus.get(endPoint).getRight())
+            && endPoint2ClientAndStatus.get(endPoint).getLeft() != null
         ? endPoint2ClientAndStatus.get(endPoint)
         : getClient();
   }
@@ -102,6 +103,7 @@ public class IoTDBDataNodeSyncClientManager extends IoTDBSyncClientManager
             && endPoint != null
             && endPoint2ClientAndStatus.containsKey(endPoint)
             && Boolean.TRUE.equals(endPoint2ClientAndStatus.get(endPoint).getRight())
+            && endPoint2ClientAndStatus.get(endPoint).getLeft() != null
         ? endPoint2ClientAndStatus.get(endPoint)
         : getClient();
   }
