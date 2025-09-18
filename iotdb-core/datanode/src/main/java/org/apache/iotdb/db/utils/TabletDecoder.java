c7b459c43d0200fe75f7b2fb6c2b781bb1f1cb07
CritasWang <critas@outlook.com>
2025-09-18 14:27:54 +0800

    move TabletDecoder to org.apache.iotdb.db.utils (#16433)

--- iotdb-core/datanode/src/main/java/org/apache/iotdb/db/utils/TabletDecoder.java
+++ iotdb-core/datanode/src/main/java/org/apache/iotdb/db/utils/TabletDecoder.java
@@ -16,7 +16,7 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-package org.apache.iotdb.util;
+package org.apache.iotdb.db.utils;
 
 import org.apache.iotdb.commons.exception.IoTDBRuntimeException;
 import org.apache.iotdb.db.protocol.thrift.handler.RPCServiceThriftHandlerMetrics;
