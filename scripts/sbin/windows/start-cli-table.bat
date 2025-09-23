7b0a2d4c9344ac68aa8bdaab611d9a6d3e895b45
761417898 <761417898@qq.com>
2025-09-23 17:01:53 +0800

    client hide password

--- scripts/sbin/windows/start-cli-table.bat
+++ scripts/sbin/windows/start-cli-table.bat
@@ -1,3 +1,4 @@
+@echo off
 @REM
 @REM Licensed to the Apache Software Foundation (ASF) under one
 @REM or more contributor license agreements.  See the NOTICE file
@@ -17,139 +18,109 @@
 @REM under the License.
 @REM
 
-@echo off
-@REM set cmd format
-powershell -NoProfile -Command "$v=(Get-ItemProperty 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion').CurrentMajorVersionNumber; if($v -gt 6) { cmd /c 'chcp 65001' }"
 
+REM -------------------------------
+REM Default SQL dialect
+if "%DEFAULT_SQL_DIALECT%"=="" set DEFAULT_SQL_DIALECT=table
 
-@REM DEFAULT_SQL_DIALECT is used to set the default SQL dialect for the CLI.
-@REM empty value means using "tree".
-@REM Optional values: "table" or "tree"
-set DEFAULT_SQL_DIALECT=table
+REM Default connection parameters
+set user_param=-u root
+set passwd_param=-pw root
+set host_param=-h 127.0.0.1
+set port_param=-p 6667
+set sql_dialect_param=-sql_dialect %DEFAULT_SQL_DIALECT%
+set PARAMETERS=
 
-@REM You can put your env variable here
-@REM set JAVA_HOME=%JAVA_HOME%
+REM -------------------------------
+REM Parse command-line arguments
+:parse_args
+if "%~1"=="" goto after_parse
 
-title IoTDB CLI
-
-set PATH="%JAVA_HOME%\bin\";%PATH%
-set "FULL_VERSION="
-set "MAJOR_VERSION="
-set "MINOR_VERSION="
-
-
-for /f tokens^=2-5^ delims^=.-_+^" %%j in ('java -fullversion 2^>^&1') do (
-	set "FULL_VERSION=%%j-%%k-%%l-%%m"
-	IF "%%j" == "1" (
-	    set "MAJOR_VERSION=%%k"
-	    set "MINOR_VERSION=%%l"
-	) else (
-	    set "MAJOR_VERSION=%%j"
-	    set "MINOR_VERSION=%%k"
-	)
-)
-
-set JAVA_VERSION=%MAJOR_VERSION%
-
-if "%OS%" == "Windows_NT" setlocal
-
-pushd %~dp0..\..
-if NOT DEFINED IOTDB_HOME set IOTDB_HOME=%CD%
-popd
-
-if NOT DEFINED MAIN_CLASS set MAIN_CLASS=org.apache.iotdb.cli.Cli
-if NOT DEFINED JAVA_HOME goto :err
-
-@REM -----------------------------------------------------------------------------
-@REM JVM Opts we'll use in legacy run or installation
-set JAVA_OPTS=-ea^
- -DIOTDB_HOME="%IOTDB_HOME%"
-
-@REM ***** CLASSPATH library setting *****
-@REM Ensure that any user defined CLASSPATH variables are not used on startup
-if EXIST "%IOTDB_HOME%\lib" (set CLASSPATH="%IOTDB_HOME%\lib\*") else set CLASSPATH="%IOTDB_HOME%\..\lib\*"
-goto okClasspath
-
-:append
-set CLASSPATH=%CLASSPATH%;%1
-
-goto :eof
-
-REM -----------------------------------------------------------------------------
-:okClasspath
-set PARAMETERS=%*
-
-@REM -----------------  -pw  -----------------
-set pw_flag=
-set new_parameters=
-
-:process_params
-if "%1"=="" goto done_params
-
-if /I "%1"=="-pw" (
+if /I "%~1"=="-u" (
+    set user_param=-u %~2
     shift
-    if "%1"=="" (
-        set pw_flag=-pw
+    shift
+    goto parse_args
+)
+if /I "%~1"=="-pw" (
+    if "%~2"=="" (
+        set passwd_param=-pw
+        shift
     ) else (
-        REM If the next parameter is another option, then the password is empty.
-        echo %1 | findstr /B /R "^-">nul
-        if %ERRORLEVEL%==0 (
-            set pw_flag=-pw
-        ) else (
-            set pw_flag=-pw %1
-            shift
-        )
+        set passwd_param=-pw %~2
+        shift
+        shift
     )
-    set new_parameters=%new_parameters% %pw_flag%
-) else (
-    set new_parameters=%new_parameters% %1
+    goto parse_args
+)
+if /I "%~1"=="-h" (
+    set host_param=-h %~2
     shift
+    shift
+    goto parse_args
 )
-goto process_params
+if /I "%~1"=="-p" (
+    set port_param=-p %~2
+    shift
+    shift
+    goto parse_args
+)
+if /I "%~1"=="-sql_dialect" (
+    set sql_dialect_param=-sql_dialect %~2
+    shift
+    shift
+    goto parse_args
+)
 
-:done_params
-set PARAMETERS=%new_parameters%
+REM Any other arguments
+set PARAMETERS=%PARAMETERS% %~1
+shift
+goto parse_args
 
-@REM if DEFAULT_SQL_DIALECT is empty, set it to "tree"
-if "%DEFAULT_SQL_DIALECT%" == "" set DEFAULT_SQL_DIALECT=tree
+:after_parse
 
-@REM set default parameters
-set pw_parameter=-pw root
-set u_parameter=-u root
-set p_parameter=-p 6667
-set h_parameter=-h 127.0.0.1
-set sql_dialect__parameter=-sql_dialect %DEFAULT_SQL_DIALECT%
+REM Combine all parameters
+set PARAMETERS=%host_param% %port_param% %user_param% %passwd_param% %sql_dialect_param% %PARAMETERS%
 
-@REM Added parameters when default parameters are missing
-echo %PARAMETERS% | findstr /c:"-sql_dialect ">nul && (set PARAMETERS=%PARAMETERS%) || (set PARAMETERS=%sql_dialect__parameter% %PARAMETERS%)
-echo %PARAMETERS% | findstr /c:"-pw ">nul && (set PARAMETERS=%PARAMETERS%) || (set PARAMETERS=%pw_parameter% %PARAMETERS%)
-echo %PARAMETERS% | findstr /c:"-u ">nul && (set PARAMETERS=%PARAMETERS%) || (set PARAMETERS=%u_parameter% %PARAMETERS%)
-echo %PARAMETERS% | findstr /c:"-p ">nul && (set PARAMETERS=%PARAMETERS%) || (set PARAMETERS=%p_parameter% %PARAMETERS%)
-echo %PARAMETERS% | findstr /c:"-h ">nul && (set PARAMETERS=%PARAMETERS%) || (set PARAMETERS=%h_parameter% %PARAMETERS%)
+REM -------------------------------
+REM Set IOTDB_HOME
+if not defined IOTDB_HOME (
+    pushd %~dp0..\..
+    set IOTDB_HOME=%CD%
+    popd
+)
 
+REM CLI configuration
+set IOTDB_CLI_CONF=%IOTDB_HOME%\conf
+set MAIN_CLASS=org.apache.iotdb.cli.Cli
+
+REM -------------------------------
+REM CLASSPATH setup
+if exist "%IOTDB_HOME%\lib" (
+    set CLASSPATH=%IOTDB_HOME%\lib\*
+) else (
+    set CLASSPATH=%IOTDB_HOME%\..\lib\*
+)
+
+REM -------------------------------
+REM JAVA executable
+if defined JAVA_HOME (
+    if exist "%JAVA_HOME%\bin\java.exe" (
+        set JAVA=%JAVA_HOME%\bin\java.exe
+    ) else (
+        set JAVA=java
+    )
+) else (
+    set JAVA=java
+)
+
+REM -------------------------------
+REM JVM options
+set JVM_OPTS=-Dsun.jnu.encoding=UTF-8 -Dfile.encoding=UTF-8
+set IOTDB_CLI_PARAMS=-Dlogback.configurationFile=%IOTDB_CLI_CONF%\logback-cli.xml
+
+REM -------------------------------
+REM Run CLI
 echo %PARAMETERS%
-
-@REM Add args for Java 11 and above, due to [JEP 396: Strongly Encapsulate JDK Internals by Default] (https://openjdk.java.net/jeps/396)
-IF "%JAVA_VERSION%" == "8" (
-    set ILLEGAL_ACCESS_PARAMS=
-) ELSE (
-    set ILLEGAL_ACCESS_PARAMS=--add-opens=java.base/java.lang=ALL-UNNAMED
-)
-set JAVA_OPTS=%JAVA_OPTS% -Dsun.jnu.encoding=UTF-8 -Dfile.encoding=UTF-8
-java %ILLEGAL_ACCESS_PARAMS% %JAVA_OPTS% -cp %CLASSPATH% %MAIN_CLASS% %PARAMETERS%
-set ret_code=%ERRORLEVEL%
-goto finally
-
-
-:err
-echo JAVA_HOME environment variable must be set!
-set ret_code=1
-pause
-
-
-@REM -----------------------------------------------------------------------------
-:finally
-
-ENDLOCAL
-
-EXIT /B %ret_code%
+"%JAVA%" %JVM_OPTS% %IOTDB_CLI_PARAMS% -cp "%CLASSPATH%" %MAIN_CLASS% %PARAMETERS%
+exit /b %ERRORLEVEL%
