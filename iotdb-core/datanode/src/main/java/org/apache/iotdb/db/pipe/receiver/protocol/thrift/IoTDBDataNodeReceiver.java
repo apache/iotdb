4bdba5c953d7a2764141d884b9bb45d3bd1957bd
Caideyipi <87789683+Caideyipi@users.noreply.github.com>
2025-09-28 11:46:28 +0800

    Pipe: Reduced the conversion logger & Fixed the illegal formats of PipeLogger (#16503)
    * fix-grass
    
    * fix

--- iotdb-core/datanode/src/main/java/org/apache/iotdb/db/pipe/receiver/protocol/thrift/IoTDBDataNodeReceiver.java
+++ iotdb-core/datanode/src/main/java/org/apache/iotdb/db/pipe/receiver/protocol/thrift/IoTDBDataNodeReceiver.java
@@ -669,7 +669,7 @@ public class IoTDBDataNodeReceiver extends IoTDBFileReceiver {
       if (status.getCode() != TSStatusCode.SUCCESS_STATUS.getStatusCode()) {
         PipeLogger.log(
             LOGGER::warn,
-            "Receiver id = {}: Failed to check authority for statement {}, username = {}, response = {}.",
+            "Receiver id = %s: Failed to check authority for statement %s, username = %s, response = %s.",
             receiverId.get(),
             StatementType.ALTER_LOGICAL_VIEW.name(),
             username,
@@ -821,7 +821,7 @@ public class IoTDBDataNodeReceiver extends IoTDBFileReceiver {
       } else {
         PipeLogger.log(
             LOGGER::warn,
-            "Receiver id = {}: Failure status encountered while executing statement {}: {}",
+            "Receiver id = %s: Failure status encountered while executing statement %s: %s",
             receiverId.get(),
             statement,
             result);
@@ -830,7 +830,7 @@ public class IoTDBDataNodeReceiver extends IoTDBFileReceiver {
     } catch (final Exception e) {
       PipeLogger.log(
           LOGGER::warn,
-          "Receiver id = {}: Exception encountered while executing statement {}: ",
+          "Receiver id = %s: Exception encountered while executing statement %s: ",
           receiverId.get(),
           statement,
           e);
@@ -884,7 +884,7 @@ public class IoTDBDataNodeReceiver extends IoTDBFileReceiver {
       if (permissionCheckStatus.getCode() != TSStatusCode.SUCCESS_STATUS.getStatusCode()) {
         PipeLogger.log(
             LOGGER::warn,
-            "Receiver id = {}: Failed to check authority for statement {}, username = {}, response = {}.",
+            "Receiver id = %s: Failed to check authority for statement %s, username = %s, response = %s.",
             receiverId.get(),
             statement.getType().name(),
             username,
