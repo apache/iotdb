0cd563ad0e6c0ff33cbc9b5027db5bcc0a55928e
Nero <zerolbsony@163.com>
2025-09-30 08:49:02 +0800

    Adjust encrypt config file.

--- iotdb-core/datanode/src/main/java/org/apache/iotdb/db/service/DataNode.java
+++ iotdb-core/datanode/src/main/java/org/apache/iotdb/db/service/DataNode.java
@@ -249,9 +249,18 @@ public class DataNode extends ServerCommandLine implements DataNodeMBean {
         }
         // Send restart request of this DataNode
         sendRestartRequestToConfigNode();
+      }
+      try {
         loadSecretKey();
         loadHardwareCode();
+        initEncryptProps();
+      } catch (IOException e) {
+        initSecretKey();
+        loadSecretKey();
+        loadHardwareCode();
+        initEncryptProps();
       }
+      encryptConfigFile();
       // TierManager need DataNodeId to do some operations so the reset method need to be invoked
       // after DataNode adding
       TierManager.getInstance().resetFolders();
@@ -567,6 +576,18 @@ public class DataNode extends ServerCommandLine implements DataNodeMBean {
   }
 
   protected void loadHardwareCode() throws IOException {
+    // Do nothing
+  }
+
+  protected void initEncryptProps() {
+    // Do nothing
+  }
+
+  protected void initSecretKey() throws IOException {
+    // Do nothing
+  }
+
+  protected void encryptConfigFile() {
     // Do nothing
   }
 
