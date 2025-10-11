3d1b688adb059767ef52fed188b2450d78208a46
Yongzao <532741407@qq.com>
2025-10-11 22:24:39 +0800

    Update DNAuditLogger.java

--- iotdb-core/datanode/src/main/java/org/apache/iotdb/db/audit/DNAuditLogger.java
+++ iotdb-core/datanode/src/main/java/org/apache/iotdb/db/audit/DNAuditLogger.java
@@ -354,7 +354,7 @@ public class DNAuditLogger extends AbstractAuditLogger {
   }
 
   @Override
-  public synchronized void log(IAuditEntity auditLogFields, Supplier<String> log) {
+  public void log(IAuditEntity auditLogFields, Supplier<String> log) {
     if (!IS_AUDIT_LOG_ENABLED) {
       return;
     }
