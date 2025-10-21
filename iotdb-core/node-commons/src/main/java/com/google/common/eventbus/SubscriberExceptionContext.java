07c58269cb84b668d5907a81dce03515f2e2a4b7
Yongzao <532741407@qq.com>
2025-10-21 12:22:26 +0800

    stash eventbus changes

---
+++ iotdb-core/node-commons/src/main/java/com/google/common/eventbus/SubscriberExceptionContext.java
@@ -0,0 +1,81 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package com.google.common.eventbus;
+
+import java.lang.reflect.Method;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+/**
+ * Context for an exception thrown by a subscriber.
+ *
+ * @since 16.0
+ */
+@ElementTypesAreNonnullByDefault
+public class SubscriberExceptionContext {
+  private final EventBus eventBus;
+  private final Object event;
+  private final Object subscriber;
+  private final Method subscriberMethod;
+
+  /**
+   * @param eventBus The {@link EventBus} that handled the event and the subscriber. Useful for
+   *     broadcasting a new event based on the error.
+   * @param event The event object that caused the subscriber to throw.
+   * @param subscriber The source subscriber context.
+   * @param subscriberMethod the subscribed method.
+   */
+  SubscriberExceptionContext(
+      EventBus eventBus, Object event, Object subscriber, Method subscriberMethod) {
+    this.eventBus = checkNotNull(eventBus);
+    this.event = checkNotNull(event);
+    this.subscriber = checkNotNull(subscriber);
+    this.subscriberMethod = checkNotNull(subscriberMethod);
+  }
+
+  /**
+   * @return The {@link EventBus} that handled the event and the subscriber. Useful for broadcasting
+   *     a new event based on the error.
+   */
+  public EventBus getEventBus() {
+    return eventBus;
+  }
+
+  /**
+   * @return The event object that caused the subscriber to throw.
+   */
+  public Object getEvent() {
+    return event;
+  }
+
+  /**
+   * @return The object context that the subscriber was called on.
+   */
+  public Object getSubscriber() {
+    return subscriber;
+  }
+
+  /**
+   * @return The subscribed method that threw the exception.
+   */
+  public Method getSubscriberMethod() {
+    return subscriberMethod;
+  }
+}
