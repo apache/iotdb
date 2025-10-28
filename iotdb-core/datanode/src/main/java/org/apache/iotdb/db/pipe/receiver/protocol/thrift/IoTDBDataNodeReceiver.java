aa899ccf8f50584100cd169d17c81b6679d06db2
Caideyipi <87789683+Caideyipi@users.noreply.github.com>
2025-10-28 11:47:27 +0800

    Pipe: Added log control for pipe receiver auth exception (#16659)

--- iotdb-core/datanode/src/main/java/org/apache/iotdb/db/pipe/receiver/protocol/thrift/IoTDBDataNodeReceiver.java
+++ iotdb-core/datanode/src/main/java/org/apache/iotdb/db/pipe/receiver/protocol/thrift/IoTDBDataNodeReceiver.java
@@ -831,7 +831,7 @@ public class IoTDBDataNodeReceiver extends IoTDBFileReceiver {
             LOGGER::warn,
             "Receiver id = %s: Failure status encountered while executing statement %s: %s",
             receiverId.get(),
-            statement,
+            statement.getPipeLoggingString(),
             result);
         return statement.accept(STATEMENT_STATUS_VISITOR, result);
       }
@@ -840,7 +840,7 @@ public class IoTDBDataNodeReceiver extends IoTDBFileReceiver {
           LOGGER::warn,
           "Receiver id = %s: Exception encountered while executing statement %s: ",
           receiverId.get(),
-          statement,
+          statement.getPipeLoggingString(),
           e);
       return statement.accept(STATEMENT_EXCEPTION_VISITOR, e);
     } finally {
