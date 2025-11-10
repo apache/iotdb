e081126ad204bb6be3aed1026e4449bcd64e2567
Caideyipi <87789683+Caideyipi@users.noreply.github.com>
2025-11-10 14:35:56 +0800

    bug-fix

--- integration-test/src/test/java/org/apache/iotdb/db/it/IoTDBFlushQueryIT.java
+++ integration-test/src/test/java/org/apache/iotdb/db/it/IoTDBFlushQueryIT.java
@@ -174,7 +174,7 @@ public class IoTDBFlushQueryIT {
             "FLUSH root.noexist.nodatagroup1,root.notExistGroup1,root.notExistGroup2");
       } catch (SQLException sqe) {
         String expectedMsg =
-            "322: 322: database root.notExistGroup1,root.notExistGroup2 does not exist";
+            "500: Database root.notExistGroup1,root.notExistGroup2 does not exist";
         sqe.printStackTrace();
         assertTrue(sqe.getMessage().contains(expectedMsg));
       }
