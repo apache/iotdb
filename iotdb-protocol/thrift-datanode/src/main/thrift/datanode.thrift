096d9eb8965cd41a8277da51692030e723377ab4
Caideyipi <87789683+Caideyipi@users.noreply.github.com>
2025-11-11 19:26:28 +0800

    Optimized the lock for encoding & compressor's invalidate cache (#16733)
    * fix
    
    * jr

--- iotdb-protocol/thrift-datanode/src/main/thrift/datanode.thrift
+++ iotdb-protocol/thrift-datanode/src/main/thrift/datanode.thrift
@@ -436,6 +436,7 @@ struct TRollbackSchemaBlackListReq {
 
 struct TInvalidateMatchedSchemaCacheReq {
   1: required binary pathPatternTree
+  2: optional bool needLock
 }
 
 struct TFetchSchemaBlackListReq {
