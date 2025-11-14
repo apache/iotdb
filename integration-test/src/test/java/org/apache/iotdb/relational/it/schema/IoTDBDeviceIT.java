45e4ac007c47f9282ddf2982d8cd60c0b1a51adc
Caideyipi <87789683+Caideyipi@users.noreply.github.com>
2025-11-14 16:06:36 +0800

    fixfix

--- integration-test/src/test/java/org/apache/iotdb/relational/it/schema/IoTDBDeviceIT.java
+++ integration-test/src/test/java/org/apache/iotdb/relational/it/schema/IoTDBDeviceIT.java
@@ -168,7 +168,7 @@ public class IoTDBDeviceIT {
         statement.executeQuery("count devices from table0 where a = 1");
         fail("Count devices shall fail for non-exist column");
       } catch (final Exception e) {
-        assertTrue(e.getMessage().contains("Column 'humidity' is not an attribute or tag column"));
+        assertTrue(e.getMessage().contains("Column 'a' is not an attribute or tag column"));
       }
 
       // Test fully qualified name
