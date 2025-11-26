fead66e2cc7caad2d51bc7fdf0524d7c635d04eb
Caideyipi <87789683+Caideyipi@users.noreply.github.com>
2025-11-26 15:01:16 +0800

    Pipe: Fixed the check for no permission 2 (#16804)
    * edge
    
    * sonar
    
    * emperor-cloth
    
    * emperor-patch

--- iotdb-core/confignode/src/main/java/org/apache/iotdb/confignode/manager/pipe/sink/protocol/IoTDBConfigRegionSink.java
+++ iotdb-core/confignode/src/main/java/org/apache/iotdb/confignode/manager/pipe/sink/protocol/IoTDBConfigRegionSink.java
@@ -179,7 +179,8 @@ public class IoTDBConfigRegionSink extends IoTDBSslSyncSink {
           String.format(
               "Transfer config region write plan %s error, result status %s.",
               pipeConfigRegionWritePlanEvent.getConfigPhysicalPlan().getType(), status),
-          pipeConfigRegionWritePlanEvent.getConfigPhysicalPlan().toString());
+          pipeConfigRegionWritePlanEvent.getConfigPhysicalPlan().toString(),
+          true);
     }
 
     if (LOGGER.isDebugEnabled()) {
@@ -265,7 +266,8 @@ public class IoTDBConfigRegionSink extends IoTDBSslSyncSink {
           String.format(
               "Seal config region snapshot file %s error, result status %s.",
               snapshotFile, resp.getStatus()),
-          snapshotFile.toString());
+          snapshotFile.toString(),
+          true);
     }
 
     LOGGER.info("Successfully transferred config region snapshot {}.", snapshotFile);
