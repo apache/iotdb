4f23e5e16dec9ec05a1598944948c6e998644b82
Caideyipi <87789683+Caideyipi@users.noreply.github.com>
2025-12-02 10:55:16 +0800

    unwebbed

--- iotdb-core/confignode/src/main/java/org/apache/iotdb/confignode/client/async/handlers/rpc/DataNodeAsyncRequestRPCHandler.java
+++ iotdb-core/confignode/src/main/java/org/apache/iotdb/confignode/client/async/handlers/rpc/DataNodeAsyncRequestRPCHandler.java
@@ -232,7 +232,6 @@ public abstract class DataNodeAsyncRequestRPCHandler<Response>
       case INVALIDATE_MATCHED_TABLE_DEVICE_CACHE:
       case DELETE_DATA_FOR_TABLE_DEVICE:
       case DELETE_TABLE_DEVICE_IN_BLACK_LIST:
-      case CHECK_DEVICE_ID_FOR_OBJECT:
       default:
         return new DataNodeTSStatusRPCHandler(
             requestType,
