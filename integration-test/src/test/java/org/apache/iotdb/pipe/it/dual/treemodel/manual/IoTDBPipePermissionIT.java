3d6ff40210697481f6700e18d36e08c647f443a4
Caideyipi <87789683+Caideyipi@users.noreply.github.com>
2025-12-04 09:52:27 +0800

    fix

--- integration-test/src/test/java/org/apache/iotdb/pipe/it/dual/treemodel/manual/IoTDBPipePermissionIT.java
+++ integration-test/src/test/java/org/apache/iotdb/pipe/it/dual/treemodel/manual/IoTDBPipePermissionIT.java
@@ -270,9 +270,8 @@ public class IoTDBPipePermissionIT extends AbstractPipeDualTreeModelManualIT {
               receiverEnv.getDataNodeWrapperList().get(0).getIpAndPortString()));
       fail("Shall fail if password is wrong.");
     } catch (final SQLException e) {
-      Assert.assertEquals(
-          "1107: ProcedureId 97: Fail to CREATE_PIPE because Authentication failed.",
-          e.getMessage());
+      Assert.assertTrue(
+          e.getMessage().contains("Fail to CREATE_PIPE because Authentication failed."));
     }
 
     // Use current session, user is root
