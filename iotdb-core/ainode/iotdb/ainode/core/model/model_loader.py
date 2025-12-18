a55b5f5d877eb7891faec8dd4d4456d2700c9525
Yongzao <532741407@qq.com>
2025-12-18 09:43:41 +0800

    revert updates

--- iotdb-core/ainode/iotdb/ainode/core/model/model_loader.py
+++ iotdb-core/ainode/iotdb/ainode/core/model/model_loader.py
@@ -43,7 +43,7 @@ logger = Logger()
 
 
 def load_model(model_info: ModelInfo, **model_kwargs) -> Any:
-    if model_info.category == ModelCategory.BUILTIN or model_info.auto_map is not None:
+    if model_info.auto_map is not None:
         model = load_model_from_transformers(model_info, **model_kwargs)
     else:
         if model_info.model_type == "sktime":
