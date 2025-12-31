222b57d5b8f4a07b4f475b6bcf6ad837cd6a6b0b
Caideyipi <87789683+Caideyipi@users.noreply.github.com>
2025-12-31 10:45:35 +0800

    fix

--- integration-test/src/test/java/org/apache/iotdb/pipe/it/single/IoTDBPipeOPCUAIT.java
+++ integration-test/src/test/java/org/apache/iotdb/pipe/it/single/IoTDBPipeOPCUAIT.java
@@ -48,8 +48,6 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 import org.junit.runner.RunWith;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.io.File;
 import java.net.ConnectException;
