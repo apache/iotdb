5a446e28037d8f18c7a3dae3808bca4649520ab0
shuwenwei <s13979062948@gmail.com>
2026-01-04 11:50:44 +0800

    spotless

--- iotdb-core/datanode/src/test/java/org/apache/iotdb/db/storageengine/dataregion/memtable/WritableMemChunkRegionScanTest.java
+++ iotdb-core/datanode/src/test/java/org/apache/iotdb/db/storageengine/dataregion/memtable/WritableMemChunkRegionScanTest.java
@@ -49,7 +49,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
-import java.util.Scanner;
 
 @RunWith(Parameterized.class)
 public class WritableMemChunkRegionScanTest {
