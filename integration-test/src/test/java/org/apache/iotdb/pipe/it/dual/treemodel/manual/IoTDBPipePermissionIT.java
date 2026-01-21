2ebb6005d00915c1c31c60a35c4ba1e7e7c33ce4
Caideyipi <87789683+Caideyipi@users.noreply.github.com>
2026-01-21 10:45:47 +0800

    it

--- integration-test/src/test/java/org/apache/iotdb/pipe/it/dual/treemodel/manual/IoTDBPipePermissionIT.java
+++ integration-test/src/test/java/org/apache/iotdb/pipe/it/dual/treemodel/manual/IoTDBPipePermissionIT.java
@@ -323,6 +323,13 @@ public class IoTDBPipePermissionIT extends AbstractPipeDualTreeModelManualIT {
             "create database root.db", "create timeSeries root.db.device.measurement int32"),
         null);
 
+    // Write some aligned historical data
+    TestUtils.executeNonQueries(
+        senderEnv,
+        Arrays.asList(
+            "create aligned timeSeries root.vehicle.plane(temperature DOUBLE, pressure INT32)",
+            "insert into root.vehicle.plane(temperature, pressure) values (36.5, 1103)"));
+
     // Transfer snapshot
     try (final Connection connection = senderEnv.getConnection();
         final Statement statement = connection.createStatement()) {
@@ -381,6 +388,13 @@ public class IoTDBPipePermissionIT extends AbstractPipeDualTreeModelManualIT {
     TestUtils.assertDataAlwaysOnEnv(
         receiverEnv,
         "select count(temperature) from root.vehicle.car",
+        "count(root.vehicle.car.pressure),",
+        Collections.singleton("0,"));
+
+    // Exception, skip
+    TestUtils.assertDataAlwaysOnEnv(
+        receiverEnv,
+        "select count(temperature) from root.vehicle.plane",
         "count(root.vehicle.car.pressure),",
         Collections.singleton("0,"));
 
