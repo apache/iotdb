23c6b85157f88a7519233d9ed55f5d6cd028d183
Caideyipi <87789683+Caideyipi@users.noreply.github.com>
2026-02-28 16:42:44 +0800

    fxi

--- iotdb-core/node-commons/src/main/java/org/apache/iotdb/commons/pipe/sink/protocol/IoTDBAirGapSink.java
+++ iotdb-core/node-commons/src/main/java/org/apache/iotdb/commons/pipe/sink/protocol/IoTDBAirGapSink.java
@@ -242,7 +242,7 @@ public abstract class IoTDBAirGapSink extends IoTDBSink {
     } else {
       supportModsIfIsDataNodeReceiver = true;
     }
-    socket.setSoTimeout(PIPE_CONFIG.getPipeAirGapNonTsFileTabletTimeoutMs());
+    socket.setSoTimeout(PIPE_CONFIG.getPipeAirGapSinkTabletTimeoutMs());
     LOGGER.info("Handshake success. Socket: {}", socket);
   }
 
