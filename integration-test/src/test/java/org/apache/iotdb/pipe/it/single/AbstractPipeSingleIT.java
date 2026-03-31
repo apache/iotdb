2222b9c51972f5310be413fd96fd775bd22dcce6
Caideyipi <87789683+Caideyipi@users.noreply.github.com>
2026-03-31 10:56:08 +0800

    fix

--- integration-test/src/test/java/org/apache/iotdb/pipe/it/single/AbstractPipeSingleIT.java
+++ integration-test/src/test/java/org/apache/iotdb/pipe/it/single/AbstractPipeSingleIT.java
@@ -38,8 +38,10 @@ abstract class AbstractPipeSingleIT {
         .setAutoCreateSchemaEnabled(true)
         .setPipeMemoryManagementEnabled(false)
         .setIsPipeEnableMemoryCheck(false)
-        .setPipeAutoSplitFullEnabled(false);
-    env.initClusterEnvironment();
+        .setPipeAutoSplitFullEnabled(false)
+        .setDataReplicationFactor(1)
+        .setSchemaReplicationFactor(1);
+    env.initClusterEnvironment(1, 1);
   }
 
   @After
