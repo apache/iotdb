bfb4856ef987e088baed916519cf25867d23491c
libo <zerolbsony@163.com>
2026-04-16 19:17:31 +0800

    Do some operations when some config params are invalid (#17498)

--- iotdb-core/datanode/src/main/java/org/apache/iotdb/db/conf/IoTDBDescriptor.java
+++ iotdb-core/datanode/src/main/java/org/apache/iotdb/db/conf/IoTDBDescriptor.java
@@ -1139,16 +1139,34 @@ public class IoTDBDescriptor {
     // update trusted_uri_pattern
     loadTrustedUriPattern(properties);
 
-    conf.setPartitionTableRecoverWorkerNum(
+    int partitionTableRecoverWorkerNum =
         Integer.parseInt(
             properties.getProperty(
                 "partition_table_recover_worker_num",
-                String.valueOf(conf.getPartitionTableRecoverWorkerNum()))));
-    conf.setPartitionTableRecoverMaxReadMBsPerSecond(
+                String.valueOf(conf.getPartitionTableRecoverWorkerNum())));
+    if (partitionTableRecoverWorkerNum <= 0) {
+      LOGGER.warn(
+          "partition_table_recover_worker_num should be greater than 0, "
+              + "but current value is {}, ignore that and use the default value {}",
+          partitionTableRecoverWorkerNum,
+          conf.getPartitionTableRecoverWorkerNum());
+      partitionTableRecoverWorkerNum = conf.getPartitionTableRecoverWorkerNum();
+    }
+    conf.setPartitionTableRecoverWorkerNum(partitionTableRecoverWorkerNum);
+    int partitionTableRecoverMaxReadMBsPerSecond =
         Integer.parseInt(
             properties.getProperty(
                 "partition_table_recover_max_read_megabytes_per_second",
-                String.valueOf(conf.getPartitionTableRecoverMaxReadMBsPerSecond()))));
+                String.valueOf(conf.getPartitionTableRecoverMaxReadMBsPerSecond())));
+    if (partitionTableRecoverMaxReadMBsPerSecond <= 0) {
+      LOGGER.warn(
+          "partition_table_recover_max_read_megabytes_per_second should be greater than 0, "
+              + "but current value is {}, ignore that and use the default value {}",
+          partitionTableRecoverMaxReadMBsPerSecond,
+          conf.getPartitionTableRecoverMaxReadMBsPerSecond());
+      partitionTableRecoverMaxReadMBsPerSecond = conf.getPartitionTableRecoverMaxReadMBsPerSecond();
+    }
+    conf.setPartitionTableRecoverMaxReadMBsPerSecond(partitionTableRecoverMaxReadMBsPerSecond);
 
     conf.setIncludeNullValueInWriteThroughputMetric(
         Boolean.parseBoolean(
