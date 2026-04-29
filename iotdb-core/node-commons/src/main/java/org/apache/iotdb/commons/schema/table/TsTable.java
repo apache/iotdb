fcb0228a8e0a071f11e631ca0f19370f0adf5d50
Caideyipi <87789683+Caideyipi@users.noreply.github.com>
2026-04-29 17:27:05 +0800

    fix

--- iotdb-core/node-commons/src/main/java/org/apache/iotdb/commons/schema/table/TsTable.java
+++ iotdb-core/node-commons/src/main/java/org/apache/iotdb/commons/schema/table/TsTable.java
@@ -230,16 +230,31 @@ public class TsTable {
   }
 
   public void renameColumnSchema(final String oldName, final String newName) {
-    executeWrite(
-        () -> {
-          // Ensures idempotency
-          if (columnSchemaMap.containsKey(oldName)) {
-            final TsTableColumnSchema schema = columnSchemaMap.get(oldName);
-            final Map<String, String> oldProps = schema.getProps();
-            oldProps.computeIfAbsent(TreeViewSchema.ORIGINAL_NAME, k -> schema.getColumnName());
-            schema.setColumnName(newName);
-          }
-        });
+    executeWrite(() -> renameColumnSchemaInternal(oldName, newName));
+  }
+
+  protected void renameColumnSchemaInternal(final String oldName, final String newName) {
+    if (Objects.equals(oldName, newName)
+        || !columnSchemaMap.containsKey(oldName) && columnSchemaMap.containsKey(newName)) {
+      return;
+    }
+
+    final TsTableColumnSchema schema = columnSchemaMap.remove(oldName);
+    if (Objects.isNull(schema)) {
+      return;
+    }
+
+    final Map<String, String> oldProps = schema.getProps();
+    oldProps.computeIfAbsent(TreeViewSchema.ORIGINAL_NAME, k -> schema.getColumnName());
+    schema.setColumnName(newName);
+    columnSchemaMap.put(newName, schema);
+
+    if (TsTableColumnCategory.TAG.equals(schema.getColumnCategory())) {
+      final Integer ordinal = tagColumnIndexMap.remove(oldName);
+      if (Objects.nonNull(ordinal)) {
+        tagColumnIndexMap.put(newName, ordinal);
+      }
+    }
   }
 
   public void removeColumnSchema(final String columnName) {
