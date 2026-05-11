60e5f419d28b761a093ab1858a0ada7f868230b5
Weihao Li <60659567+Wei-hao-Li@users.noreply.github.com>
2026-05-11 15:17:28 +0800

    Move TableScaNode to node-commons (#17627)

--- iotdb-core/datanode/src/main/java/org/apache/iotdb/db/queryengine/plan/planner/plan/node/PlanVisitor.java
+++ iotdb-core/datanode/src/main/java/org/apache/iotdb/db/queryengine/plan/planner/plan/node/PlanVisitor.java
@@ -133,7 +133,6 @@ import org.apache.iotdb.db.queryengine.plan.relational.planner.node.CteScanNode;
 import org.apache.iotdb.db.queryengine.plan.relational.planner.node.DeviceTableScanNode;
 import org.apache.iotdb.db.queryengine.plan.relational.planner.node.InformationSchemaTableScanNode;
 import org.apache.iotdb.db.queryengine.plan.relational.planner.node.NonAlignedAggregationTreeDeviceViewScanNode;
-import org.apache.iotdb.db.queryengine.plan.relational.planner.node.TableScanNode;
 import org.apache.iotdb.db.queryengine.plan.relational.planner.node.TreeAlignedDeviceViewScanNode;
 import org.apache.iotdb.db.queryengine.plan.relational.planner.node.TreeDeviceViewScanNode;
 import org.apache.iotdb.db.queryengine.plan.relational.planner.node.TreeNonAlignedDeviceViewScanNode;
@@ -680,10 +679,6 @@ public interface PlanVisitor<R, C> extends ICoreQueryPlanVisitor<R, C> {
   }
 
   // =============================== Used for Table Model ====================================
-
-  default R visitTableScan(TableScanNode node, C context) {
-    return visitPlan(node, context);
-  }
 
   default R visitDeviceTableScan(DeviceTableScanNode node, C context) {
     return visitTableScan(node, context);
