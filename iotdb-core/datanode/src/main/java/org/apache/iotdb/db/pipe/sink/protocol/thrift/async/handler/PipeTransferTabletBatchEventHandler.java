0b0a81d95e370b0e6dc670ef9a9aed65af1ac5a6
Caideyipi <87789683+Caideyipi@users.noreply.github.com>
2026-05-14 23:16:42 +0800

    Pipe: Implemented slice logic for async sink (#17668)
    * sl
    
    * chew
    
    (cherry picked from commit 0d382d85ddf03a5911e391a6a1dff12bb8340443)

--- iotdb-core/datanode/src/main/java/org/apache/iotdb/db/pipe/sink/protocol/thrift/async/handler/PipeTransferTabletBatchEventHandler.java
+++ iotdb-core/datanode/src/main/java/org/apache/iotdb/db/pipe/sink/protocol/thrift/async/handler/PipeTransferTabletBatchEventHandler.java
@@ -129,7 +129,7 @@ public class PipeTransferTabletBatchEventHandler extends PipeTransferTrackableHa
   protected void doTransfer(
       final AsyncPipeDataTransferServiceClient client, final TPipeTransferReq req)
       throws TException {
-    client.pipeTransfer(req, this);
+    transferWithOptionalRequestSlicing(client, req);
   }
 
   @Override
