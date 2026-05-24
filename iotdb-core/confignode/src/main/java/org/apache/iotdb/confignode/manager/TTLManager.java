c12504347adeccf0d625f3f28f742136dfb733fc
Caideyipi <87789683+Caideyipi@users.noreply.github.com>
2026-05-24 14:00:46 +0800

    Fixed TTL problems (#17735)
    (cherry picked from commit cc108e78ec81b2b7d003305b88218fa3327f581e)

--- iotdb-core/confignode/src/main/java/org/apache/iotdb/confignode/manager/TTLManager.java
+++ iotdb-core/confignode/src/main/java/org/apache/iotdb/confignode/manager/TTLManager.java
@@ -127,6 +127,10 @@ public class TTLManager {
     return ((ShowTTLResp) showTTL(new ShowTTLPlan())).getPathTTLMap();
   }
 
+  public long getTTL(final String[] pathPattern) {
+    return ttlInfo.getTTL(pathPattern);
+  }
+
   public int getTTLCount() {
     return ttlInfo.getTTLCount();
   }
