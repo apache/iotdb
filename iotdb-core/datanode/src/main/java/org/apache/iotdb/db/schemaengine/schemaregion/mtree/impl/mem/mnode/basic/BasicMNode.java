fbc110d5bc2dbed68aae284887f4bdf1799ccb4a
Caideyipi <87789683+Caideyipi@users.noreply.github.com>
2026-06-01 10:58:03 +0800

    Added flags to mark whether a device has device descendants to optimize query like select xx from xxx.** (#17672)
    * flag-
    
    * Fix
    
    * Address device descendant flag review comments
    
    (cherry picked from commit a565a06825317e5f39393e619fb44e3fbbb88a9b)

--- iotdb-core/datanode/src/main/java/org/apache/iotdb/db/schemaengine/schemaregion/mtree/impl/mem/mnode/basic/BasicMNode.java
+++ iotdb-core/datanode/src/main/java/org/apache/iotdb/db/schemaengine/schemaregion/mtree/impl/mem/mnode/basic/BasicMNode.java
@@ -46,6 +46,12 @@ public class BasicMNode implements IMemMNode {
   private IMemMNode parent;
   private final BasicMNodeInfo basicMNodeInfo;
 
+  /** Cached count of measurements in this node's subtree, rebuilt on restart. */
+  private long subtreeMeasurementCount = 0L;
+
+  /** Cached flag showing whether there is any device in the subtree below this node. */
+  private boolean hasDeviceDescendant = false;
+
   /** from root to this node, only be set when used once for InternalMNode */
   private String fullPath;
 
@@ -53,6 +59,11 @@ public class BasicMNode implements IMemMNode {
   public BasicMNode(IMemMNode parent, String name) {
     this.parent = parent;
     this.basicMNodeInfo = new BasicMNodeInfo(name);
+  }
+
+  @Override
+  public BasicMNode getBasicMNode() {
+    return this;
   }
 
   @Override
@@ -96,6 +107,25 @@ public class BasicMNode implements IMemMNode {
   @Override
   public void setFullPath(String fullPath) {
     this.fullPath = fullPath;
+  }
+
+  public long getSubtreeMeasurementCount() {
+    return subtreeMeasurementCount;
+  }
+
+  @Override
+  public void setSubtreeMeasurementCount(final long subtreeMeasurementCount) {
+    this.subtreeMeasurementCount = subtreeMeasurementCount;
+  }
+
+  @Override
+  public boolean hasDeviceDescendant() {
+    return hasDeviceDescendant;
+  }
+
+  @Override
+  public void setHasDeviceDescendant(final boolean hasDeviceDescendant) {
+    this.hasDeviceDescendant = hasDeviceDescendant;
   }
 
   @Override
@@ -224,6 +254,8 @@ public class BasicMNode implements IMemMNode {
    *         <li>basicMNodeInfo reference, 8B
    *         <li>parent reference, 8B
    *         <li>fullPath reference, 8B
+   *         <li>subtreeMeasurementCount, 8B
+   *         <li>hasDeviceDescendant, 1B
    *       </ol>
    *   <li>MapEntry in parent
    *       <ol>
@@ -235,7 +267,7 @@ public class BasicMNode implements IMemMNode {
    */
   @Override
   public int estimateSize() {
-    return 8 + 8 + 8 + 8 + 8 + 8 + 28 + basicMNodeInfo.estimateSize();
+    return 8 + 8 + 8 + 8 + 8 + 1 + 8 + 8 + 28 + basicMNodeInfo.estimateSize();
   }
 
   @Override
