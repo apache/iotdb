6a71c1bd5a7afd9ebebaf4ed842fc607e63a2472
761417898 <761417898@qq.com>
2026-06-05 16:39:18 +0800

    Set CXX11 ABI for glibc217 C++ package

--- .github/scripts/package-client-cpp-manylinux-glibc217.sh
+++ .github/scripts/package-client-cpp-manylinux-glibc217.sh
@@ -68,7 +68,8 @@ java -version
 cd "${GITHUB_WORKSPACE:?GITHUB_WORKSPACE is not set}"
 ./mvnw clean package -P with-cpp -pl iotdb-client/client-cpp -am -DskipTests \
   -Dspotless.skip=true \
-  -Dclient.cpp.package.classifier="${PACKAGE_CLASSIFIER}"
+  -Dclient.cpp.package.classifier="${PACKAGE_CLASSIFIER}" \
+  -Diotdb.extra.cxx.flags=-D_GLIBCXX_USE_CXX11_ABI=1
 
 SO="iotdb-client/client-cpp/target/install/lib/libiotdb_session.so"
 test -f "${SO}"
