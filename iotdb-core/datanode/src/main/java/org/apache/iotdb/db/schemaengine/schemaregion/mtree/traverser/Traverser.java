2d844de0adc1b1e9f6777ef78aa3171250655118
Caideyipi <87789683+Caideyipi@users.noreply.github.com>
2026-06-05 13:52:50 +0800

    Fix PBTree schema cache release after pre-delete (#17834)

--- iotdb-core/datanode/src/main/java/org/apache/iotdb/db/schemaengine/schemaregion/mtree/traverser/Traverser.java
+++ iotdb-core/datanode/src/main/java/org/apache/iotdb/db/schemaengine/schemaregion/mtree/traverser/Traverser.java
@@ -185,6 +185,7 @@ public abstract class Traverser<R, N extends IMNode<N>> extends AbstractTreeVisi
           && skipPreDeletedSchema
           && child.isMeasurement()
           && child.getAsMeasurementMNode().isPreDeleted()) {
+        releaseNode(child);
         child = null;
       }
     }
