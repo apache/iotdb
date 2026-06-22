eed40fd71e1212852ab55016e56f713b23208a64
shuwenwei <55970239+shuwenwei@users.noreply.github.com>
2026-06-22 10:22:30 +0800

    Update iotdb-core/datanode/src/main/java/org/apache/iotdb/db/queryengine/execution/operator/source/relational/ExternalTsFileAggTableScanOperator.java
    Co-authored-by: Jackie Tien <jackietien97@gmail.com>

--- iotdb-core/datanode/src/main/java/org/apache/iotdb/db/queryengine/execution/operator/source/relational/ExternalTsFileAggTableScanOperator.java
+++ iotdb-core/datanode/src/main/java/org/apache/iotdb/db/queryengine/execution/operator/source/relational/ExternalTsFileAggTableScanOperator.java
@@ -148,11 +148,29 @@ public class ExternalTsFileAggTableScanOperator extends DefaultAggTableScanOpera
 
   @Override
   public void close() throws Exception {
+  Exception exception = null;
+  try {
     if (deviceTaskReader != null) {
       deviceTaskReader.close();
-      deviceTaskReader = null;
     }
-    super.close();
+  } catch (Exception e) {
+    exception = e;
+  } finally {
+    deviceTaskReader = null;
+    try {
+      super.close();
+    } catch (Exception e) {
+      if (exception == null) {
+        exception = e;
+      } else {
+        exception.addSuppressed(e);
+      }
+    }
+  }
+
+  if (exception != null) {
+    throw exception;
+  }
   }
 
   @Override
