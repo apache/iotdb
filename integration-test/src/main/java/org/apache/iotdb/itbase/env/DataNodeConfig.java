c4afcc599b4b856e02a9f5c950209ad56db997e0
Caideyipi <87789683+Caideyipi@users.noreply.github.com>
2026-06-30 18:30:57 +0800

    Fix multi data dir region migration IT compile

--- integration-test/src/main/java/org/apache/iotdb/itbase/env/DataNodeConfig.java
+++ integration-test/src/main/java/org/apache/iotdb/itbase/env/DataNodeConfig.java
@@ -33,6 +33,8 @@ public interface DataNodeConfig {
 
   DataNodeConfig setConnectionTimeoutInMS(int connectionTimeoutInMS);
 
+  DataNodeConfig setDnDataDirs(String dnDataDirs);
+
   DataNodeConfig setLoadTsFileAnalyzeSchemaMemorySizeInBytes(
       long loadTsFileAnalyzeSchemaMemorySizeInBytes);
 
