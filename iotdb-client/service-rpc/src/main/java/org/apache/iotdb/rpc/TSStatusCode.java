5daaf2474da30e47be1de8cc6156855c331eb3ec
Yaobin Chen <alpass163@163.com>
2026-07-27 09:32:19 +0800

    fix01

--- iotdb-client/service-rpc/src/main/java/org/apache/iotdb/rpc/TSStatusCode.java
+++ iotdb-client/service-rpc/src/main/java/org/apache/iotdb/rpc/TSStatusCode.java
@@ -90,7 +90,7 @@ public enum TSStatusCode {
   TYPE_NOT_FOUND(528),
   DATABASE_CONFLICT(529),
   DATABASE_MODEL(530),
-  // the range [531, 534] has been occupied by timecho DB
+  // the range [531, 534] has been occupied
   METADATA_LEASE_FENCED(535),
   METADATA_LEASE_FENCED_RETRY_REQUIRED(536),
 
