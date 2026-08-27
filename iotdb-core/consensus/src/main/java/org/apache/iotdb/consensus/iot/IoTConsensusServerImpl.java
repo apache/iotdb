3ceb360dde241adb2e94f9b13dfba9e6203d293c
Caideyipi <87789683+Caideyipi@users.noreply.github.com>
2026-08-27 17:17:31 +0800

    Fix snapshot pipe startup before IoTConsensus catch-up

--- iotdb-core/consensus/src/main/java/org/apache/iotdb/consensus/iot/IoTConsensusServerImpl.java
+++ iotdb-core/consensus/src/main/java/org/apache/iotdb/consensus/iot/IoTConsensusServerImpl.java
@@ -812,6 +812,15 @@ public class IoTConsensusServerImpl {
     }
   }
 
+  /** Wait until every remote peer has synchronized its writes to all peers. */
+  public void waitUntilAllRemotePeersSyncLogCompleted() throws ConsensusGroupModifyPeerException {
+    for (Peer peer : getConfiguration()) {
+      if (!peer.equals(thisNode)) {
+        waitTargetPeerUntilSyncLogCompleted(peer);
+      }
+    }
+  }
+
   public boolean hasReleaseAllRegionRelatedResource(ConsensusGroupId groupId) {
     return stateMachine.hasReleaseAllRegionRelatedResource(groupId);
   }
