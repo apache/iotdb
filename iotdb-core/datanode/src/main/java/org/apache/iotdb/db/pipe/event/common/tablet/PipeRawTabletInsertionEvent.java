bd6305b6a4c3bba50ccfc0fdd1e514eef5c9a90d
Caideyipi <87789683+Caideyipi@users.noreply.github.com>
2026-09-24 17:26:00 +0800

    [Pipe] Remove redundant progress index local

--- iotdb-core/datanode/src/main/java/org/apache/iotdb/db/pipe/event/common/tablet/PipeRawTabletInsertionEvent.java
+++ iotdb-core/datanode/src/main/java/org/apache/iotdb/db/pipe/event/common/tablet/PipeRawTabletInsertionEvent.java
@@ -333,8 +333,7 @@ public class PipeRawTabletInsertionEvent extends PipeInsertionEvent
 
   protected void eliminateProgressIndex() {
     if (sourceEvent instanceof PipeTsFileInsertionEvent) {
-      final PipeTsFileInsertionEvent tsFileInsertionEvent = (PipeTsFileInsertionEvent) sourceEvent;
-      tsFileInsertionEvent.eliminateProgressIndex();
+      ((PipeTsFileInsertionEvent) sourceEvent).eliminateProgressIndex();
     }
   }
 
